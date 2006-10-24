3d089575071675ebf8b8102dc72a615417a192fd
rambaut <rambaut@ca793f91-a31e-0410-b540-2769d408b6a1>
2006-10-24 11:23:51 +0000

    Solved the problem of the missing integrator by copying out of the relevant source code. The numericalMethods library is GPL'ed.

--- trunk/src/dr/evolution/coalescent/DemographicFunction.java
+++ trunk/src/dr/evolution/coalescent/DemographicFunction.java
@@ -28,6 +28,7 @@ package dr.evolution.coalescent;
 import dr.evolution.util.Units;
 import dr.math.Binomial;
 import dr.math.MathUtils;
+import org.apache.commons.math.analysis.UnivariateRealFunction;
 
 /**
  * This interface provides methods that describe a demographic function.
@@ -40,7 +41,7 @@ import dr.math.MathUtils;
  * @author Alexei Drummond
  * @author Korbinian Strimmer
  */
-public interface DemographicFunction extends Units {
+public interface DemographicFunction extends UnivariateRealFunction, Units {
 
 	/**
 	 * Gets the value of the demographic function N(t) at time t.
@@ -101,14 +102,17 @@ public interface DemographicFunction extends Units {
 	
 	public abstract class Abstract implements DemographicFunction
 	{
-			
-		/**
+        // With the release of commons-maths 1.2 we will be able to use this...
+        // RombergIntegrator numericalIntegrator;
+        
+        /**
 		 * Construct demographic model with default settings
 		 */
 		public Abstract(int units) {
 		
 			setUnits(units);
-		}
+
+        }
 
 		// general functions
 
@@ -127,20 +131,77 @@ public interface DemographicFunction extends Units {
 
 			int slices = 99;
 			double[] intensities = new double[slices];
-			
+
 			for (int i =0; i < intensities.length; i++) {
 				double time = start + (i*finish / (slices-1));
 				intensities[i] = 1.0 / getDemographic(time);
 			}
-			/* FIXME removed this call until we can find equivalent with compatible license
-			double integral = numericalMethods.calculus.integration.NewtonCotes.simpsonSum(intensities) * (finish-start);
+			double integral = simpsonSum(intensities) * (finish-start);
 			return integral;
-			*/
-			throw new RuntimeException("Demographic model numerical integration not yet implemented");		
 		}
-		
-	
+        
+        /**
+         * This function is copied from the numericalMethods library which is distributed under
+         * the following licence:
+         *
+         JTEM - Java Tools for Experimental Mathematics
+         Copyright (C) 2001 JTEM-Group
+
+         This program is free software; you can redistribute it and/or modify
+         it under the terms of the GNU General Public License as published by
+         the Free Software Foundation; either version 2 of the License, or
+         any later version.
+
+         This program is distributed in the hope that it will be useful,
+         but WITHOUT ANY WARRANTY; without even the implied warranty of
+         MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+         GNU General Public License for more details.
+
+         You should have received a copy of the GNU General Public License
+         along with this program; if not, write to the Free Software
+         Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+         *
+         * Computes Newton-Cotes formula using Simpson's rule.
+         * Order 5 method; integrates polynoms up to order 3 exactly.
+         * @param value table of legnth 1 + 2n with n>0, equally spaced interval of length one
+         * @return integral of tabled (equally spaced, interval length 1) function
+         */
+        public static double simpsonSum(double[] value) {
+
+          if (value.length % 2 != 1) {
+            throw new IllegalArgumentException("length of array have to be odd");
+          }
+
+          int n = value.length - 1;
+
+          double mod0Sum = value[n];
+          double mod1Sum = 0;
+
+          for (int i = 0; i < n; ) {
+            mod0Sum += value[i++];
+            mod1Sum += value[i++];
+          }
+
+          mod0Sum = 2 * mod0Sum - value[0] - value[n];
+
+          return (mod0Sum + 4 * mod1Sum) / 6 / (n / 2);
+        }
+
+
+        // **************************************************************
+	    // UnivariateRealFunction IMPLEMENTATION
 	    // **************************************************************
+
+        /**
+         * Return the intensity at a given time for numerical integration
+         * @param x the time
+         * @return the intensity
+         */
+        public double value(double x) {
+            return 1.0 / getDemographic(x);
+        }
+
+        // **************************************************************
 	    // Units IMPLEMENTATION
 	    // **************************************************************
 

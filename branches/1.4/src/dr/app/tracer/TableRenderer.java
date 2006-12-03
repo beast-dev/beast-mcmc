9f284cd9792e13520b2d585d513d24a19c15593c
rambaut <rambaut@ca793f91-a31e-0410-b540-2769d408b6a1>
2006-12-03 22:52:58 +0000

    A few modifications from Marcs ARG branch

--- branches/1.4/src/dr/app/tracer/TableRenderer.java
+++ branches/1.4/src/dr/app/tracer/TableRenderer.java
@@ -31,17 +31,17 @@ import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.*;
 
 
-class TableRenderer extends DefaultTableCellRenderer { 
+public class TableRenderer extends DefaultTableCellRenderer { 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = -3815955688412224510L;
 	Color bg1 = Color.white;
 	Color bg2 = new Color(0xEE, 0xEE, 0xFF);
 	int alignment;
-	
-	public TableRenderer(int alignment, int inset) { 
-		super(); 
+
+	public TableRenderer(int alignment, int inset) {
+		super();
 		this.alignment = alignment;
 		if (alignment == SwingConstants.LEFT) {
 			setBorder(new BorderUIResource.EmptyBorderUIResource(new java.awt.Insets(0, inset, 0, 0)));
@@ -49,38 +49,38 @@ class TableRenderer extends DefaultTableCellRenderer {
 			setBorder(new BorderUIResource.EmptyBorderUIResource(new java.awt.Insets(0, 0, 0, inset)));
 		}
 	}
-	
-	public Component getTableCellRendererComponent( 
-		JTable table, Object value, boolean isSelected, 
-		boolean hasFocus, int row, int column) { 
-		
-		setOpaque(true); 
-		setText(value.toString()); 
-		
 
-		// if cell is selected, set background color to default cell selection background color 
-		if (isSelected) { 
-			setBackground( table.getSelectionBackground() ) ; 
-			setForeground( table.getSelectionForeground() ); 
-		} else { 
-			// otherwise, set cell background color to our custom color 
-			if (row % 2 == 0) { 
-				setBackground(bg1); 
-			} else { 
-				setBackground(bg2); 
+	public Component getTableCellRendererComponent(
+		JTable table, Object value, boolean isSelected,
+		boolean hasFocus, int row, int column) {
+
+		setOpaque(true);
+		setText(value.toString());
+
+
+		// if cell is selected, set background color to default cell selection background color
+		if (isSelected) {
+			setBackground( table.getSelectionBackground() ) ;
+			setForeground( table.getSelectionForeground() );
+		} else {
+			// otherwise, set cell background color to our custom color
+			if (row % 2 == 0) {
+				setBackground(bg1);
+			} else {
+				setBackground(bg2);
 			}
-			
-			// set cell's foreground to default cell foreground color 
-			setForeground(table.getForeground()); 
-		} 
 
-		// draw border on cell if it has focus 
-		//if (hasFocus) { 
-		//	setBorder( UIManager.getBorder("Table.focusCellHighlightBorder") ); 
-		//} 
+			// set cell's foreground to default cell foreground color
+			setForeground(table.getForeground());
+		}
+
+		// draw border on cell if it has focus
+		//if (hasFocus) {
+		//	setBorder( UIManager.getBorder("Table.focusCellHighlightBorder") );
+		//}
 
 		// position cell text at center
-		setHorizontalAlignment(alignment); 
-		return this; 
+		setHorizontalAlignment(alignment);
+		return this;
 	}
 }

a0f5fc2c2886744c79e8aa2ddfa0867b820cf4af
Marc Suchard <msuchard@ucla.edu>
2007-01-02 23:11:50 +0000

    Converted GeneralizedLinearModel into an abstract class for implementing different linear models.  LogisticRegression provides support for the logistic family of GLMs.

---
+++ src/dr/inference/distribution/LogisticRegression.java
@@ -0,0 +1,47 @@
+package dr.inference.distribution;
+
+import dr.inference.model.DesignMatrix;
+import dr.inference.model.Parameter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: msuchard
+ * Date: Jan 2, 2007
+ * Time: 2:43:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LogisticRegression extends GeneralizedLinearModel {
+
+
+    public LogisticRegression(Parameter dependentParam, Parameter independentParam, DesignMatrix designMatrix) {
+        super(dependentParam, independentParam, designMatrix);
+    }
+
+
+    protected double calculateLogLikelihood() {
+        // logLikelihood calculation for logistic regression
+        double logLikelihood = 0;
+
+        final int K = independentParam.getDimension();
+        final int N = dependentParam.getDimension();
+
+        for (int i = 0; i < N; i++) {
+            // for each "pseudo"-datum
+            double xBeta = 0;
+            for (int k = 0; k < K; k++) {
+                xBeta += designMatrix.getParameterValue(i, k) * independentParam.getParameterValue(k);
+            }
+
+            logLikelihood += dependentParam.getParameterValue(i) * xBeta
+                    - Math.log(1.0 + Math.exp(xBeta));
+
+        }
+        return logLikelihood;
+    }
+
+
+    public boolean confirmIndependentParameters() {
+        // todo -- check that independent parameters \in {0,1} only
+        return true;
+    }
+}

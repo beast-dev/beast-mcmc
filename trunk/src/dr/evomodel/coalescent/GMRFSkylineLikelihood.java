78ca47b963f56245006ebff3863ed1d0cec39a86
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2007-03-20 23:36:49 +0000

    Added GMRFSkylineLikelihood and GMRFSkylineGibbsOperator classes to begin work on implementing a GMRF smoother on population size trajectories.  The classes currently do not do anything.

---
+++ trunk/src/dr/evomodel/coalescent/GMRFSkylineLikelihood.java
@@ -0,0 +1,178 @@
+/*
+ * SkylineLikelihood.java
+ *
+ * Copyright (C) 2002-2006 Alexei Drummond and Andrew Rambaut
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package dr.evomodel.coalescent;
+
+import dr.evolution.coalescent.ConstantPopulation;
+import dr.evolution.tree.Tree;
+import dr.evolution.util.Units;
+import dr.evomodel.tree.TreeModel;
+import dr.inference.model.Likelihood;
+import dr.inference.model.Parameter;
+import dr.xml.*;
+
+/**
+ * A likelihood function for the Gaussian Markov random field population trajectory.
+ * *
+ *
+ * @author Vladimir Minin
+ * @author Marc Suchard
+ * @version $Id: GMRFSkylineLikelihood.java,v 1.3 2007/03/20 22:40:04 msuchard Exp $
+ */
+public class GMRFSkylineLikelihood extends CoalescentLikelihood {
+
+    // PUBLIC STUFF
+
+    public static final String SKYLINE_LIKELIHOOD = "gmrfSkyLineLikelihood";
+    public static final String POPULATION_PARAMETER = "populationSizes";
+    public static final String PRECISION_PARAMETER = "precisionParameter";
+
+    // PRIVATE STUFF
+
+    private Parameter popSizeParameter;
+    private Parameter precisionParameter;
+
+    public GMRFSkylineLikelihood(Tree tree, Parameter popParameter, Parameter precParameter) {
+        super(SKYLINE_LIKELIHOOD);
+
+        this.popSizeParameter = popParameter;
+        this.precisionParameter = precParameter;
+        int tips = tree.getExternalNodeCount();
+        int params = popSizeParameter.getDimension();
+        if (tips - params != 1) {
+            throw new IllegalArgumentException("Number of tips (" + tips + ") must be one greater than number of pop sizes (" + params + ")");
+        }
+
+        this.tree = tree;
+        if (tree instanceof TreeModel) {
+            addModel((TreeModel) tree);
+        }
+        addParameter(popSizeParameter);
+        addParameter(precisionParameter);
+        setupIntervals();
+
+        addStatistic(new DeltaStatistic());
+    }
+
+    // **************************************************************
+    // Likelihood IMPLEMENTATION
+    // **************************************************************
+
+    /**
+     * Calculates the log likelihood of this set of coalescent intervals,
+     * given a demographic model.
+     */
+    public double calculateLogLikelihood() {
+
+        if (!intervalsKnown) setupIntervals();
+
+        double logL = 0.0;
+
+        double currentTime = 0.0;
+
+        int popIndex = 0;
+
+        ConstantPopulation cp = new ConstantPopulation(Units.Type.YEARS);
+
+        for (int j = 0; j < intervalCount; j++) {
+
+            cp.setN0(popSizeParameter.getParameterValue(popIndex));
+            if (getIntervalType(j) == COALESCENT) {
+                popIndex += 1;
+            }
+
+            //		logL += calculateIntervalLikelihood(cp, intervals[j], currentTime, lineageCounts[j], getIntervalType(j));
+
+            // insert zero-length coalescent intervals
+            int diff = getCoalescentEvents(j) - 1;
+            for (int k = 0; k < diff; k++) {
+                cp.setN0(popSizeParameter.getParameterValue(popIndex));
+                logL += calculateIntervalLikelihood(cp, 0.0, currentTime, lineageCounts[j] - k - 1, COALESCENT);
+                popIndex += 1;
+            }
+
+            currentTime += intervals[j];
+
+
+        }
+
+        return logL;
+    }
+
+    // ****************************************************************
+    // Private and protected stuff
+    // ****************************************************************
+
+    public static XMLObjectParser PARSER = new AbstractXMLObjectParser() {
+
+        public String getParserName() {
+            return SKYLINE_LIKELIHOOD;
+        }
+
+        public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+
+            XMLObject cxo = (XMLObject) xo.getChild(POPULATION_PARAMETER);
+            Parameter popParameter = (Parameter) cxo.getChild(Parameter.class);
+
+            cxo = (XMLObject) xo.getChild(PRECISION_PARAMETER);
+            Parameter precParameter = (Parameter) cxo.getChild(Parameter.class);
+
+            cxo = (XMLObject) xo.getChild(POPULATION_TREE);
+            TreeModel treeModel = (TreeModel) cxo.getChild(TreeModel.class);
+
+            return new GMRFSkylineLikelihood(treeModel, popParameter, precParameter);
+        }
+
+        //************************************************************************
+        // AbstractXMLObjectParser implementation
+        //************************************************************************
+
+        public String getParserDescription() {
+            return "This element represents the likelihood of the tree given the population size vector.";
+        }
+
+        public Class getReturnType() {
+            return Likelihood.class;
+        }
+
+        public XMLSyntaxRule[] getSyntaxRules() {
+            return rules;
+        }
+
+        private XMLSyntaxRule[] rules = new XMLSyntaxRule[]{
+                new ElementRule(POPULATION_PARAMETER, new XMLSyntaxRule[]{
+                        new ElementRule(Parameter.class)
+                }),
+                new ElementRule(PRECISION_PARAMETER, new XMLSyntaxRule[]{
+                        new ElementRule(Parameter.class)
+                }),
+                new ElementRule(POPULATION_TREE, new XMLSyntaxRule[]{
+                        new ElementRule(TreeModel.class)
+                }),
+        };
+    };
+
+
+}

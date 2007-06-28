31ef0717d89dd5015bcbfddfc483ceb801c48a07
jheled <jheled@ca793f91-a31e-0410-b540-2769d408b6a1>
2007-06-28 23:59:37 +0000

    Fix MixedDistributionLikelihood to work with none constant distributions. Involved changing compund likelihood to accept anothe compund as a component.

--- src/dr/inference/distribution/MixedDistributionLikelihood.java
+++ src/dr/inference/distribution/MixedDistributionLikelihood.java
@@ -2,6 +2,8 @@ package dr.inference.distribution;
 
 import dr.inference.model.Statistic;
 import dr.inference.model.Likelihood;
+import dr.inference.model.Model;
+import dr.inference.model.CompoundModel;
 import dr.xml.*;
 import org.w3c.dom.Element;
 import org.w3c.dom.Document;
@@ -25,7 +27,17 @@ public class MixedDistributionLikelihood extends Likelihood.Abstract {
     public static final String INDICATORS = "indicators";
 
     public MixedDistributionLikelihood(ParametricDistributionModel[] distributions, Statistic data, Statistic indicators) {
-        super(null);
+        // Mixed Distribution Likelihood contains two distribution models, not necessarily constant.
+        // To cater for that, they need to be returned as the "Model" of this likelyhood so that their state is correctly
+        // restored when an operation involving their parameters fails.
+
+        super(new CompoundModel("MixedeDistributions"));
+
+        final CompoundModel cm = (CompoundModel)this.getModel();
+        for( ParametricDistributionModel m : distributions ) {
+          cm.addModel(m);
+        }
+
         this.distributions = distributions;
         this.data = data;
         this.indicators = indicators;
@@ -64,6 +76,7 @@ public class MixedDistributionLikelihood extends Likelihood.Abstract {
 
             logL += distributions[index].logPdf(data.getStatisticValue(j));
         }
+        //System.err.println("mixed: " + logL);
         return logL;
     }
 
@@ -133,4 +146,10 @@ public class MixedDistributionLikelihood extends Likelihood.Abstract {
     private Statistic indicators;
     private boolean impliedOne = false;
 
+    public Model[] getUniqueModels() {
+        Model[] m = new Model[distributions[0] == distributions[1] ? 1 : 2];
+        m[0] = distributions[0];
+        if( m.length > 1 ) m[1] = distributions[1];
+        return m;
+    }
 }

1dae339dd6c2ceb29c689aa0f202004791e8c6e2
rambaut <rambaut@ca793f91-a31e-0410-b540-2769d408b6a1>
2007-08-06 12:27:19 +0000

    Removed vestigal 'adoptState' methods that had been proliferating like endogenous retroviruses dispite that the method had been removed from the interface.

--- trunk/src/dr/inference/distribution/UniformDistributionModel.java
+++ trunk/src/dr/inference/distribution/UniformDistributionModel.java
@@ -99,7 +99,6 @@ public class UniformDistributionModel extends AbstractModel implements Parametri
 	protected void storeState() {} // no additional state needs storing
 	protected void restoreState() {} // no additional state needs restoring
 	protected void acceptState() {} // no additional state needs accepting
-	protected void adoptState(Model source) {} // no additional state needs adopting
 
 	/**
 	 * Reads a normal distribution model from a DOM Document element.

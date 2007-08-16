932e468723c7a25d74f196316b74ef25f715b118
rambaut <rambaut@ca793f91-a31e-0410-b540-2769d408b6a1>
2007-08-16 08:10:59 +0000

    Trying to add BFs to tracer. Opens a frame with a table of BFs

--- trunk/src/dr/app/tracer/analysis/BayesFactorsDialog.java
+++ trunk/src/dr/app/tracer/analysis/BayesFactorsDialog.java
@@ -1,172 +1,215 @@
 package dr.app.tracer.analysis;
 
 import dr.inference.trace.TraceList;
+import dr.inference.trace.MarginalLikelihoodAnalysis;
 import org.virion.jam.framework.DocumentFrame;
 import org.virion.jam.panels.OptionsPanel;
 import org.virion.jam.util.LongTask;
 
 import javax.swing.*;
+import javax.swing.Timer;
 import javax.swing.border.EmptyBorder;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.*;
 
 public class BayesFactorsDialog {
 
-    private JFrame frame;
+	private JFrame frame;
 
-    private JComboBox likelihoodCombo;
+	private JComboBox likelihoodCombo;
 
-    private String[] likelihoodGuesses = {
-            "likelihood", "treelikelihood", "lnl", "lik"
-    };
+	private String[] likelihoodGuesses = {
+			"likelihood", "treelikelihood", "lnl", "lik"
+	};
 
-    private String likelihoodTrace = "None selected";
+	private String likelihoodTrace = "None selected";
 
-    private OptionsPanel optionPanel;
+	private OptionsPanel optionPanel;
 
-    public BayesFactorsDialog(JFrame frame) {
-        this.frame = frame;
+	public BayesFactorsDialog(JFrame frame) {
+		this.frame = frame;
 
-        likelihoodCombo = new JComboBox();
+		likelihoodCombo = new JComboBox();
 
-        optionPanel = new OptionsPanel(12, 12);
-    }
+		optionPanel = new OptionsPanel(12, 12);
+	}
 
-    private int findArgument(JComboBox comboBox, String argument) {
-        for (int i = 0; i < comboBox.getItemCount(); i++) {
-            String item = ((String) comboBox.getItemAt(i)).toLowerCase();
-            if (item.indexOf(argument) != -1) return i;
-        }
-        return -1;
-    }
+	private int findArgument(JComboBox comboBox, String argument) {
+		for (int i = 0; i < comboBox.getItemCount(); i++) {
+			String item = ((String) comboBox.getItemAt(i)).toLowerCase();
+			if (item.indexOf(argument) != -1) return i;
+		}
+		return -1;
+	}
 
-    public String getLikelihoodTrace() {
-        return likelihoodTrace;
-    }
+	public String getLikelihoodTrace() {
+		return likelihoodTrace;
+	}
 
-    public int showDialog(TraceList traceList) {
+	public int showDialog(List<TraceList> traceLists) {
 
-        setArguments();
+		setArguments();
 
-        for (int j = 0; j < traceList.getTraceCount(); j++) {
-            String statistic = traceList.getTraceName(j);
-            likelihoodCombo.addItem(statistic);
-        }
-        int index = -1;
-        for (String guess : likelihoodGuesses) {
-            if (index != -1) break;
+		List<String> statistics = new ArrayList<String>();
+		TraceList tl = traceLists.get(0);
+		for (int j = 0; j < tl.getTraceCount(); j++) {
+			statistics.add(tl.getTraceName(j));
+		}
 
-            index = findArgument(likelihoodCombo, guess);
-        }
-        if (index == -1) index = 0;
-        likelihoodCombo.setSelectedIndex(index);
+		for (int i = 1; i < traceLists.size(); i++) {
+			tl = traceLists.get(i);
+			Set<String> statistics2 = new HashSet<String>();
+			for (int j = 0; j < tl.getTraceCount(); j++) {
+				statistics2.add(tl.getTraceName(j));
+			}
 
-        JOptionPane optionPane = new JOptionPane(optionPanel,
-                JOptionPane.QUESTION_MESSAGE,
-                JOptionPane.OK_CANCEL_OPTION,
-                null,
-                null,
-                null);
-        optionPane.setBorder(new EmptyBorder(12, 12, 12, 12));
+			statistics.retainAll(statistics2);
+		}
 
-        final JDialog dialog = optionPane.createDialog(frame, "Marginal Likelihood Analysis");
-        dialog.pack();
+		if (statistics.size() == 0) {
+			JOptionPane.showMessageDialog(frame,
+					"These trace files don't seem to have any traces in common.",
+					"Unable to perform analysis",
+					JOptionPane.ERROR_MESSAGE);
+			return JOptionPane.CANCEL_OPTION;
+		}
 
-        dialog.setVisible(true);
+		for (String statistic : statistics) {
+			likelihoodCombo.addItem(statistic);
+		}
 
-        int result = JOptionPane.CANCEL_OPTION;
-        Integer value = (Integer) optionPane.getValue();
-        if (value != null && value != -1) {
-            result = value;
-        }
+		int index = -1;
+		for (String guess : likelihoodGuesses) {
+			if (index != -1) break;
 
-        if (result == JOptionPane.OK_OPTION) {
-            likelihoodTrace = (String) likelihoodCombo.getSelectedItem();
-        }
+			index = findArgument(likelihoodCombo, guess);
+		}
+		if (index == -1) index = 0;
+		likelihoodCombo.setSelectedIndex(index);
 
-        return result;
-    }
+		JOptionPane optionPane = new JOptionPane(optionPanel,
+				JOptionPane.QUESTION_MESSAGE,
+				JOptionPane.OK_CANCEL_OPTION,
+				null,
+				null,
+				null);
+		optionPane.setBorder(new EmptyBorder(12, 12, 12, 12));
 
-    private void setArguments() {
-        optionPanel.removeAll();
+		final JDialog dialog = optionPane.createDialog(frame, "Marginal Likelihood Analysis");
+		dialog.pack();
 
-        optionPanel.addLabel("Select the trace to analyse:");
+		dialog.setVisible(true);
 
-        optionPanel.addComponents(new JLabel("Likelihood trace:"), likelihoodCombo);
+		int result = JOptionPane.CANCEL_OPTION;
+		Integer value = (Integer) optionPane.getValue();
+		if (value != null && value != -1) {
+			result = value;
+		}
 
-        optionPanel.addSeparator();
+		if (result == JOptionPane.OK_OPTION) {
+			likelihoodTrace = (String) likelihoodCombo.getSelectedItem();
+		}
 
-    }
+		return result;
+	}
 
-    Timer timer = null;
+	private void setArguments() {
+		optionPanel.removeAll();
 
-    public void createBayesFactorsFrame(TraceList traceList, DocumentFrame parent) {
+		optionPanel.addLabel("Select the trace to analyse:");
 
-        final MarginalLikelihoodTask analyseTask = new MarginalLikelihoodTask(traceList);
+		optionPanel.addComponents(new JLabel("Likelihood trace:"), likelihoodCombo);
 
-        final ProgressMonitor progressMonitor = new ProgressMonitor(frame,
-                "Estimating Marginal Likelihoods",
-                "", 0, analyseTask.getLengthOfTask());
-        progressMonitor.setMillisToPopup(0);
-        progressMonitor.setMillisToDecideToPopup(0);
+		optionPanel.addSeparator();
 
-        timer = new Timer(1000, new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-                progressMonitor.setProgress(analyseTask.getCurrent());
-                if (progressMonitor.isCanceled() || analyseTask.done()) {
-                    progressMonitor.close();
-                    analyseTask.stop();
-                    timer.stop();
-                }
-            }
-        });
+	}
 
-        analyseTask.go();
-        timer.start();
-    }
+	Timer timer = null;
 
-    class MarginalLikelihoodTask extends LongTask {
+	public void createBayesFactorsFrame(List<TraceList> traceLists, DocumentFrame parent) {
 
-        TraceList traceList;
+		BayesFactorsFrame frame = new BayesFactorsFrame(parent, "Bayes Factors");
+		frame.initialize();
+		frame.setVisible(true);
 
-        private int lengthOfTask = 0;
-        private int current = 0;
-        private String message;
+		final MarginalLikelihoodTask analyseTask = new MarginalLikelihoodTask(frame, traceLists);
 
-        public MarginalLikelihoodTask(TraceList traceList) {
-            this.traceList = traceList;
+		final ProgressMonitor progressMonitor = new ProgressMonitor(frame,
+				"Estimating Marginal Likelihoods",
+				"", 0, analyseTask.getLengthOfTask());
+		progressMonitor.setMillisToPopup(0);
+		progressMonitor.setMillisToDecideToPopup(0);
 
-        }
+		timer = new Timer(1000, new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				progressMonitor.setProgress(analyseTask.getCurrent());
+				if (progressMonitor.isCanceled() || analyseTask.done()) {
+					progressMonitor.close();
+					analyseTask.stop();
+					timer.stop();
+				}
+			}
+		});
 
-        public int getCurrent() {
-            return current;
-        }
+		analyseTask.go();
+		timer.start();
+	}
 
-        public int getLengthOfTask() {
-            return lengthOfTask;
-        }
+	class MarginalLikelihoodTask extends LongTask {
 
-        public String getDescription() {
-            return "Estimating marginal likelihood...";
-        }
+		List<TraceList> traceLists;
+		BayesFactorsFrame frame;
 
-        public String getMessage() {
-            return message;
-        }
+		private int lengthOfTask = 0;
+		private int current = 0;
+		private String message;
 
-        public Object doWork() {
 
-            current = 0;
+		public MarginalLikelihoodTask(BayesFactorsFrame frame, List<TraceList> traceLists) {
+			this.traceLists = traceLists;
+			this.frame = frame;
+			lengthOfTask = traceLists.size();
+		}
 
-            int index = traceList.getTraceIndex(likelihoodTrace);
-            double[] likelihoods = new double[traceList.getStateCount()];
-            traceList.getValues(index, likelihoods);
+		public int getCurrent() {
+			return current;
+		}
 
-            throw new UnsupportedOperationException("Not implemented yet...");
+		public int getLengthOfTask() {
+			return lengthOfTask;
+		}
 
-            // return null;
-        }
+		public String getDescription() {
+			return "Estimating marginal likelihoods...";
+		}
 
-    }
+		public String getMessage() {
+			return message;
+		}
+
+		public Object doWork() {
+
+			current = 0;
+
+			for (TraceList traceList : traceLists) {
+				int index = traceList.getTraceIndex(likelihoodTrace);
+				double[] likelihoods = new double[traceList.getStateCount()];
+				traceList.getValues(index, likelihoods);
+
+				MarginalLikelihoodAnalysis analysis = new MarginalLikelihoodAnalysis(
+						likelihoods,
+						traceList.getName(), traceList.getBurnIn(),
+						false, true, 1000);
+
+				frame.addMarginalLikelihood(analysis);
+
+				current ++;
+			}
+
+			return null;
+		}
+
+	}
 
 }

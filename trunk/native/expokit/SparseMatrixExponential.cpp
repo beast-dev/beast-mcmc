58338ef295e023a7f9d8dea3bdc6b0372681aeb2
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2007-09-26 15:16:04 +0000

    Map stuff

--- trunk/native/expokit/SparseMatrixExponential.cpp
+++ trunk/native/expokit/SparseMatrixExponential.cpp
@@ -7,13 +7,24 @@ extern "C" void dgexpvms_(long *n, long *nz, long *m, double *t, double *v, doub
 JNIEXPORT void JNICALL Java_dr_math_SparseMatrixExponential_executeDGEXPV
 (JNIEnv *env, jobject, jint order, jint nz, jint maxBasis, jdouble time, jdoubleArray operandVector, jdoubleArray resultVector, jdouble tolerance, jdouble matrixNorm, jdoubleArray rate, jintArray indexX, jintArray indexY, jdoubleArray workspace, jint lengthWorkspace, jintArray intWorkspace, jint lengthIntWorkspace, jint flag) {
 
-  jdouble* operandVectorPtr = env->GetDoubleArrayElements(operandVector,0);
-  jdouble* resultVectorPtr = env->GetDoubleArrayElements(resultVector,0);
-  jdouble* ratePtr = env->GetDoubleArrayElements(rate,0);
-  jdouble* workspacePtr = env->GetDoubleArrayElements(workspace,0);
-  jint* indexXPtr = env->GetIntArrayElements(indexX,0);
-  jint* indexYPtr = env->GetIntArrayElements(indexY,0);
-  jint* intWorkspacePtr = env->GetIntArrayElements(intWorkspace,0);
+
+
+
+//  jdouble* operandVectorPtr = env->GetDoubleArrayElements(operandVector,0);
+//  jdouble* resultVectorPtr = env->GetDoubleArrayElements(resultVector,0);
+//  jdouble* ratePtr = env->GetDoubleArrayElements(rate,0);
+//  jdouble* workspacePtr = env->GetDoubleArrayElements(workspace,0);
+//  jint* indexXPtr = env->GetIntArrayElements(indexX,0);
+//  jint* indexYPtr = env->GetIntArrayElements(indexY,0);
+//  jint* intWorkspacePtr = env->GetIntArrayElements(intWorkspace,0);
+
+  jdouble* operandVectorPtr = (jdouble*)env->GetPrimitiveArrayCritical(operandVector,0);
+  jdouble* resultVectorPtr = (jdouble*)env->GetPrimitiveArrayCritical(resultVector,0);
+  jdouble* ratePtr = (jdouble*)env->GetPrimitiveArrayCritical(rate,0);
+  jdouble* workspacePtr = (jdouble*)env->GetPrimitiveArrayCritical(workspace,0);
+  jint* indexXPtr = (jint*)env->GetPrimitiveArrayCritical(indexX,0);
+  jint* indexYPtr = (jint*)env->GetPrimitiveArrayCritical(indexY,0);
+  jint* intWorkspacePtr = (jint*)env->GetPrimitiveArrayCritical(intWorkspace,0);
 
   long itrace = 0;
 
@@ -22,13 +33,21 @@ JNIEXPORT void JNICALL Java_dr_math_SparseMatrixExponential_executeDGEXPV
   	   workspacePtr, &lengthWorkspace, intWorkspacePtr, 
   	   &lengthIntWorkspace, &itrace, &flag);
 
-  env->ReleaseDoubleArrayElements(operandVector,operandVectorPtr,0);
-  env->ReleaseDoubleArrayElements(resultVector,resultVectorPtr,0);
-  env->ReleaseDoubleArrayElements(rate,ratePtr,0);
-  env->ReleaseDoubleArrayElements(workspace,workspacePtr,0);
-  env->ReleaseIntArrayElements(indexX,indexXPtr,0);
-  env->ReleaseIntArrayElements(indexY,indexYPtr,0);
-  env->ReleaseIntArrayElements(intWorkspace,intWorkspacePtr,0);
+  env->ReleasePrimitiveArrayCritical(operandVector,operandVectorPtr,0);
+  env->ReleasePrimitiveArrayCritical(resultVector,resultVectorPtr,0);
+  env->ReleasePrimitiveArrayCritical(rate,ratePtr,0);
+  env->ReleasePrimitiveArrayCritical(workspace,workspacePtr,0);
+  env->ReleasePrimitiveArrayCritical(indexX,indexXPtr,0);
+  env->ReleasePrimitiveArrayCritical(indexY,indexYPtr,0);
+  env->ReleasePrimitiveArrayCritical(intWorkspace,intWorkspacePtr,0);
+
+//  env->ReleaseDoubleArrayElements(operandVector,operandVectorPtr,0);
+//  env->ReleaseDoubleArrayElements(resultVector,resultVectorPtr,0);
+//  env->ReleaseDoubleArrayElements(rate,ratePtr,0);
+//  env->ReleaseDoubleArrayElements(workspace,workspacePtr,0);
+//  env->ReleaseIntArrayElements(indexX,indexXPtr,0);
+//  env->ReleaseIntArrayElements(indexY,indexYPtr,0);
+//  env->ReleaseIntArrayElements(intWorkspace,intWorkspacePtr,0);
 
 
 }

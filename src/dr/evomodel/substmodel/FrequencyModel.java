d8eedf5b0937557414dffb794e6f1b8896536624
Marc Suchard <msuchard@ucla.edu>
2007-10-02 23:34:52 +0000

    Added "normalize" attribute to FrequencyModel such that starting values can be normalized to sum to 1 if they do not already

--- src/dr/evomodel/substmodel/FrequencyModel.java
+++ src/dr/evomodel/substmodel/FrequencyModel.java
@@ -43,165 +43,175 @@ import java.util.logging.Logger;
  *
  * @author Alexei Drummond
  * @author Andrew Rambaut
+ * @author Marc Suchard
  * @version $Id: FrequencyModel.java,v 1.26 2005/05/24 20:25:58 rambaut Exp $
  */
 public class FrequencyModel extends AbstractModel {
 
-    public static final String FREQUENCIES = "frequencies";
-    public static final String FREQUENCY_MODEL = "frequencyModel";
+	public static final String FREQUENCIES = "frequencies";
+	public static final String FREQUENCY_MODEL = "frequencyModel";
+	public static final String NORMALIZE = "normalize";
 
-    public FrequencyModel(DataType dataType, Parameter frequencyParameter) {
+	public FrequencyModel(DataType dataType, Parameter frequencyParameter) {
 
-        super(FREQUENCY_MODEL);
+		super(FREQUENCY_MODEL);
 
-        double sum = getSumOfFrequencies(frequencyParameter);
+		double sum = getSumOfFrequencies(frequencyParameter);
 
-        if (Math.abs(sum - 1.0) > 1e-8) {
-            throw new IllegalArgumentException("Frequencies do not sum to 1, the sum to " + sum);
-        }
+		if (Math.abs(sum - 1.0) > 1e-8) {
+			throw new IllegalArgumentException("Frequencies do not sum to 1, the sum to " + sum);
+		}
 
-        this.frequencyParameter = frequencyParameter;
-        addParameter(frequencyParameter);
-        frequencyParameter.addBounds(new Parameter.DefaultBounds(1.0, 0.0, frequencyParameter.getDimension()));
-        this.dataType = dataType;
-    }
+		this.frequencyParameter = frequencyParameter;
+		addParameter(frequencyParameter);
+		frequencyParameter.addBounds(new Parameter.DefaultBounds(1.0, 0.0, frequencyParameter.getDimension()));
+		this.dataType = dataType;
+	}
 
-    /**
-     * @param frequencies the frequencies
-     * @return return the sum of frequencies
-     */
-    private double getSumOfFrequencies(Parameter frequencies) {
-        double total = 0.0;
-        for (int i = 0; i < frequencies.getDimension(); i++) {
-            total += frequencies.getParameterValue(i);
-        }
-        return total;
-    }
+	/**
+	 * @param frequencies the frequencies
+	 * @return return the sum of frequencies
+	 */
+	private double getSumOfFrequencies(Parameter frequencies) {
+		double total = 0.0;
+		for (int i = 0; i < frequencies.getDimension(); i++) {
+			total += frequencies.getParameterValue(i);
+		}
+		return total;
+	}
 
-    public void setFrequency(int i, double value) {
-        frequencyParameter.setParameterValue(i, value);
-    }
+	public void setFrequency(int i, double value) {
+		frequencyParameter.setParameterValue(i, value);
+	}
 
-    public double getFrequency(int i) {
-        return frequencyParameter.getParameterValue(i);
-    }
+	public double getFrequency(int i) {
+		return frequencyParameter.getParameterValue(i);
+	}
 
-    public int getFrequencyCount() {
-        return frequencyParameter.getDimension();
-    }
+	public int getFrequencyCount() {
+		return frequencyParameter.getDimension();
+	}
 
-    public double[] getFrequencies() {
-        double[] frequencies = new double[getFrequencyCount()];
-        for (int i = 0; i < frequencies.length; i++) {
-            frequencies[i] = getFrequency(i);
-        }
-        return frequencies;
-    }
+	public double[] getFrequencies() {
+		double[] frequencies = new double[getFrequencyCount()];
+		for (int i = 0; i < frequencies.length; i++) {
+			frequencies[i] = getFrequency(i);
+		}
+		return frequencies;
+	}
 
-    public DataType getDataType() {
-        return dataType;
-    }
+	public DataType getDataType() {
+		return dataType;
+	}
 
-    // *****************************************************************
-    // Interface Model
-    // *****************************************************************
+	// *****************************************************************
+	// Interface Model
+	// *****************************************************************
 
-    protected void handleModelChangedEvent(Model model, Object object, int index) {
-        // no intermediates need recalculating....
-    }
+	protected void handleModelChangedEvent(Model model, Object object, int index) {
+		// no intermediates need recalculating....
+	}
 
-    protected void handleParameterChangedEvent(Parameter parameter, int index) {
-        // no intermediates need recalculating....
-    }
+	protected void handleParameterChangedEvent(Parameter parameter, int index) {
+		// no intermediates need recalculating....
+	}
 
-    protected void storeState() {
-    } // no state apart from parameters to store
+	protected void storeState() {
+	} // no state apart from parameters to store
 
-    protected void restoreState() {
-    } // no state apart from parameters to restore
+	protected void restoreState() {
+	} // no state apart from parameters to restore
 
-    protected void acceptState() {
-    } // no state apart from parameters to accept
+	protected void acceptState() {
+	} // no state apart from parameters to accept
 
-    public Element createElement(Document doc) {
-        throw new RuntimeException("Not implemented!");
-    }
+	public Element createElement(Document doc) {
+		throw new RuntimeException("Not implemented!");
+	}
 
-    /**
-     * Reads a frequency model from an XMLObject.
-     */
-    public static XMLObjectParser PARSER = new AbstractXMLObjectParser() {
+	/**
+	 * Reads a frequency model from an XMLObject.
+	 */
+	public static XMLObjectParser PARSER = new AbstractXMLObjectParser() {
 
-        public String getParserName() {
-            return FREQUENCY_MODEL;
-        }
+		public String getParserName() {
+			return FREQUENCY_MODEL;
+		}
 
-        public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+		public Object parseXMLObject(XMLObject xo) throws XMLParseException {
 
-            DataType dataType = DataTypeUtils.getDataType(xo);
+			DataType dataType = DataTypeUtils.getDataType(xo);
 
-            Parameter freqsParam = (Parameter) xo.getSocketChild(FREQUENCIES);
-            double[] frequencies = null;
+			Parameter freqsParam = (Parameter) xo.getSocketChild(FREQUENCIES);
+			double[] frequencies = null;
 
-            for (int i = 0; i < xo.getChildCount(); i++) {
-                Object obj = xo.getChild(i);
-                if (obj instanceof PatternList) {
-                    frequencies = ((PatternList) obj).getStateFrequencies();
-                }
-            }
+			for (int i = 0; i < xo.getChildCount(); i++) {
+				Object obj = xo.getChild(i);
+				if (obj instanceof PatternList) {
+					frequencies = ((PatternList) obj).getStateFrequencies();
+				}
+			}
 
-            StringBuilder sb = new StringBuilder("Creating state frequencies model: ");
-            if (frequencies != null) {
-                if (freqsParam.getDimension() != frequencies.length) {
-                    throw new XMLParseException("dimension of frequency parameter and number of sequence states don't match!");
-                }
-                for (int j = 0; j < frequencies.length; j++) {
-                    freqsParam.setParameterValue(j, frequencies[j]);
-                }
-                sb.append("Using empirical frequencies from data ");
-            } else {
-                sb.append("Initial frequencies ");
-            }
-            sb.append("= {");
+			StringBuilder sb = new StringBuilder("Creating state frequencies model: ");
+			if (frequencies != null) {
+				if (freqsParam.getDimension() != frequencies.length) {
+					throw new XMLParseException("dimension of frequency parameter and number of sequence states don't match!");
+				}
+				for (int j = 0; j < frequencies.length; j++) {
+					freqsParam.setParameterValue(j, frequencies[j]);
+				}
+				sb.append("Using empirical frequencies from data ");
+			} else {
+				sb.append("Initial frequencies ");
+			}
+			sb.append("= {");
 
-            NumberFormat format = NumberFormat.getNumberInstance();
-            format.setMaximumFractionDigits(5);
+			if (xo.hasAttribute(NORMALIZE) && xo.getBooleanAttribute(NORMALIZE)) {
+				double sum = 0;
+				for (int j = 0; j < freqsParam.getDimension(); j++)
+					sum += freqsParam.getParameterValue(j);
+				for (int j = 0; j < freqsParam.getDimension(); j++)
+					freqsParam.setParameterValue(j, freqsParam.getParameterValue(j) / sum);
+			}
 
-            sb.append(format.format(freqsParam.getParameterValue(0)));
-            for (int j = 1; j < freqsParam.getDimension(); j++) {
-                sb.append(", ");
-                sb.append(format.format(freqsParam.getParameterValue(j)));
-            }
-            sb.append("}");
-            Logger.getLogger("dr.evomodel").info(sb.toString());
+			NumberFormat format = NumberFormat.getNumberInstance();
+			format.setMaximumFractionDigits(5);
 
-            return new FrequencyModel(dataType, freqsParam);
-        }
+			sb.append(format.format(freqsParam.getParameterValue(0)));
+			for (int j = 1; j < freqsParam.getDimension(); j++) {
+				sb.append(", ");
+				sb.append(format.format(freqsParam.getParameterValue(j)));
+			}
+			sb.append("}");
+			Logger.getLogger("dr.evomodel").info(sb.toString());
 
-        public String getParserDescription() {
-            return "A model of equilibrium base frequencies.";
-        }
+			return new FrequencyModel(dataType, freqsParam);
+		}
 
-        public Class getReturnType() {
-            return FrequencyModel.class;
-        }
+		public String getParserDescription() {
+			return "A model of equilibrium base frequencies.";
+		}
 
-        public XMLSyntaxRule[] getSyntaxRules() {
-            return rules;
-        }
+		public Class getReturnType() {
+			return FrequencyModel.class;
+		}
 
-        private XMLSyntaxRule[] rules = new XMLSyntaxRule[]{
-                new XORRule(
-                        new StringAttributeRule(DataType.DATA_TYPE, "The type of sequence data", DataType.getRegisteredDataTypeNames(), false),
-                        new ElementRule(DataType.class)
-                ),
-                new ElementRule(FREQUENCIES,
-                        new XMLSyntaxRule[]{new ElementRule(Parameter.class)})
+		public XMLSyntaxRule[] getSyntaxRules() {
+			return rules;
+		}
 
-        };
-    };
+		private XMLSyntaxRule[] rules = new XMLSyntaxRule[]{
+				new XORRule(
+						new StringAttributeRule(DataType.DATA_TYPE, "The type of sequence data", DataType.getRegisteredDataTypeNames(), false),
+						new ElementRule(DataType.class)
+				),
+				new ElementRule(FREQUENCIES,
+						new XMLSyntaxRule[]{new ElementRule(Parameter.class)}),
+				AttributeRule.newBooleanRule(NORMALIZE, true),
+		};
+	};
 
-    private DataType dataType = null;
-    Parameter frequencyParameter = null;
+	private DataType dataType = null;
+	Parameter frequencyParameter = null;
 
 }

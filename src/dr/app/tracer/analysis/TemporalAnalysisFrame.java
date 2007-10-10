ce9f43445908ce959314ae275644192c0779f854
rambaut <rambaut@users.noreply.github.com>
2007-10-10 23:12:16 +0000

    Made TemporalAnalysisFrame implement TracerFileMenuHandler so it will now export data and PDFs.

--- src/dr/app/tracer/analysis/TemporalAnalysisFrame.java
+++ src/dr/app/tracer/analysis/TemporalAnalysisFrame.java
@@ -7,8 +7,15 @@ import dr.app.tracer.application.TracerFileMenuHandler;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.awt.event.ActionEvent;
+import java.io.*;
 
-public class TemporalAnalysisFrame extends AuxilaryFrame {
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.pdf.*;
+
+public class TemporalAnalysisFrame extends AuxilaryFrame implements TracerFileMenuHandler {
 	private int binCount;
 	private double minTime;
 	private double maxTime;
@@ -135,6 +142,70 @@ public class TemporalAnalysisFrame extends AuxilaryFrame {
 		return rangeSet;
 	}
 
+	public final void doExportData() {
+
+	    FileDialog dialog = new FileDialog(this,
+	            "Export Data...",
+	            FileDialog.SAVE);
+
+	    dialog.setVisible(true);
+	    if (dialog.getFile() != null) {
+	        File file = new File(dialog.getDirectory(), dialog.getFile());
+
+	        try {
+	            FileWriter writer = new FileWriter(file);
+	            writer.write(toString());
+	            writer.close();
+
+
+	        } catch (IOException ioe) {
+	            JOptionPane.showMessageDialog(this, "Unable to write file: " + ioe,
+	                    "Unable to write file",
+	                    JOptionPane.ERROR_MESSAGE);
+	        }
+	    }
+
+	}
+
+	public final void doExportPDF() {
+	    FileDialog dialog = new FileDialog(this,
+	            "Export PDF Image...",
+	            FileDialog.SAVE);
+
+	    dialog.setVisible(true);
+	    if (dialog.getFile() != null) {
+	        File file = new File(dialog.getDirectory(), dialog.getFile());
+
+	        Rectangle2D bounds = temporalAnalysisPlotPanel.getExportableComponent().getBounds();
+	        Document document = new Document(new com.lowagie.text.Rectangle((float)bounds.getWidth(), (float)bounds.getHeight()));
+	        try {
+	            // step 2
+	            PdfWriter writer;
+	            writer = PdfWriter.getInstance(document, new FileOutputStream(file));
+	            // step 3
+	            document.open();
+	            // step 4
+	            PdfContentByte cb = writer.getDirectContent();
+	            PdfTemplate tp = cb.createTemplate((float)bounds.getWidth(), (float)bounds.getHeight());
+	            Graphics2D g2d = tp.createGraphics((float)bounds.getWidth(), (float)bounds.getHeight(), new DefaultFontMapper());
+	            temporalAnalysisPlotPanel.getExportableComponent().print(g2d);
+	            g2d.dispose();
+	            cb.addTemplate(tp, 0, 0);
+	        }
+	        catch(DocumentException de) {
+	            JOptionPane.showMessageDialog(this, "Error writing PDF file: " + de,
+	                    "Export PDF Error",
+	                    JOptionPane.ERROR_MESSAGE);
+	        }
+	        catch (FileNotFoundException e) {
+	            JOptionPane.showMessageDialog(this, "Error writing PDF file: " + e,
+	                    "Export PDF Error",
+	                    JOptionPane.ERROR_MESSAGE);
+	        }
+	        document.close();
+	    }
+	}
+
 	public String toString() {
 		StringBuffer buffer = new StringBuffer();
 
@@ -173,4 +244,25 @@ public class TemporalAnalysisFrame extends AuxilaryFrame {
 
 		return buffer.toString();
 	}
+
+	public Action getExportDataAction() {
+		return exportDataAction;
+	}
+
+	public Action getExportPDFAction() {
+		return exportPDFAction;
+	}
+
+	private AbstractAction exportDataAction = new AbstractAction("Export Data...") {
+	    public void actionPerformed(ActionEvent ae) {
+	        doExportData();
+	    }
+	};
+
+	private AbstractAction exportPDFAction = new AbstractAction("Export PDF...") {
+	    public void actionPerformed(ActionEvent ae) {
+	        doExportPDF();
+	    }
+	};
+
 }

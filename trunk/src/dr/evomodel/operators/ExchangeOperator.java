99340d061864dfeb75efab3f7d7133f9d122b3e7
Sebastian.Hoehna <Sebastian.Hoehna@ca793f91-a31e-0410-b540-2769d408b6a1>
2007-10-23 05:22:49 +0000

    Updated the tree proposal moves. FNPR and Intermediate are added and the other test moves deleted. The CladeMetric is added for counting the swaps for a clade. The ConvergenceListener is added which measures the distance of split frequencies to a golden run. Both are used for statistical analysis for efficiency test on tree proposal distribution.

--- trunk/src/dr/evomodel/operators/ExchangeOperator.java
+++ trunk/src/dr/evomodel/operators/ExchangeOperator.java
@@ -46,15 +46,20 @@ import dr.xml.*;
 public class ExchangeOperator extends SimpleMCMCOperator {
 
     public static final String NARROW_EXCHANGE = "narrowExchange";
-    public static final String WIDE_EXCHANGE = "wideExchange";
+    public static final String WIDE_EXCHANGE = "wideExchange"; 
+    public static final String INTERMEDIATE_EXCHANGE = "intermediateExchange";
 
     public static final int NARROW = 0;
-    public static final int WIDE = 1;
+    public static final int WIDE = 1;  
+    public static final int INTERMEDIATE = 2;
 
     private static final int MAX_TRIES = 10000;
 
     private int mode = NARROW;
     private TreeModel tree;
+    
+
+    private double[] distances;
 
     public ExchangeOperator(int mode, TreeModel tree, double weight) {
         this.mode = mode;
@@ -65,6 +70,7 @@ public class ExchangeOperator extends SimpleMCMCOperator {
     public double doOperation() throws OperatorFailedException {
 
         int tipCount = tree.getExternalNodeCount();
+        double hastingsRatio = 0;
 
         switch (mode) {
             case NARROW:
@@ -73,13 +79,16 @@ public class ExchangeOperator extends SimpleMCMCOperator {
             case WIDE:
                 wide();
                 break;
+            case INTERMEDIATE:
+               hastingsRatio = intermediate();
+               break;
         }
 
         if (tree.getExternalNodeCount() != tipCount) {
             throw new RuntimeException("Lost some tips in " + ((mode == NARROW) ? "NARROW mode." : "WIDE mode."));
         }
 
-        return 0.0;
+        return hastingsRatio;
     }
 
     /**
@@ -157,6 +166,152 @@ public class ExchangeOperator extends SimpleMCMCOperator {
 
         throw new OperatorFailedException("Couldn't find valid wide move on this tree!");
     }
+    
+    /**
+    * WARNING: Assumes strictly bifurcating tree.
+    */
+   public double intermediate() throws OperatorFailedException {
+
+      final int nodeCount = tree.getNodeCount();
+      final NodeRef root = tree.getRoot();
+
+      // I don't know how to prove this but it seems that there are exactly
+      // k(k-1) permissable pairs for k+1
+      // contemporaneous tips, and since the total is 2k * (2k-1) / 2, so the
+      // average number of tries is 2.
+      // With serial data the average number of tries can be made arbitrarily
+      // high as tree becomes less balanced.
+
+      for(int tries = 0; tries < MAX_TRIES; ++tries) {
+      NodeRef i, j;
+      NodeRef[] possibleNodes = null;
+      do {
+
+         // get a random node
+         i = tree.getNode(MathUtils.nextInt(nodeCount));
+         if (root != i) {
+            possibleNodes = tree.getNodes();
+         }
+
+         // check if we got the root
+         while (root == i) {
+            // if so get another one till we haven't got anymore the root
+            i = tree.getNode(MathUtils.nextInt(nodeCount));
+            if (root != i) {
+               possibleNodes =tree.getNodes();
+            }
+         }
+
+         // get another random node
+         // NodeRef j = tree.getNode(MathUtils.nextInt(nodeCount));
+         j = getRandomNode(possibleNodes, i);
+         // check if they are the same and if the new node is the root
+      } while (j == null || j == i || j == root);
+
+      double forward = getWinningChance(indexOf(possibleNodes, j));
+      
+//       possibleNodes = getPossibleNodes(j);
+      calcDistances(possibleNodes, j);
+      forward += getWinningChance(indexOf(possibleNodes, i));
+      
+      // get the parent of both of them
+      final NodeRef iP = tree.getParent(i);
+      final NodeRef jP = tree.getParent(j);
+
+      // check if both parents are equal -> we are siblings :) (this
+      // wouldnt effect a change og topology)
+      // check if I m your parent or vice versa (this would destroy the
+      // tree)
+      // check if you are younger then my father
+      // check if I m younger then your father
+      if ((iP != jP) && (i != jP) && (j != iP)
+            && (tree.getNodeHeight(j) < tree.getNodeHeight(iP))
+            && (tree.getNodeHeight(i) < tree.getNodeHeight(jP))) {
+         // if 1 & 2 are false and 3 & 4 are true then we found a valid
+         // candidate
+         exchangeNodes(tree, i, j, iP, jP);
+
+//          possibleNodes = getPossibleNodes(i);
+         calcDistances(possibleNodes, i);
+         double backward = getWinningChance(indexOf(possibleNodes, j));
+         
+//          possibleNodes = getPossibleNodes(j);
+         calcDistances(possibleNodes, j);
+         backward += getWinningChance(indexOf(possibleNodes, i));
+         
+         // System.out.println("tries = " + tries+1);
+         return Math.log(Math.min(1, (backward) / (forward)));
+         // return 0.0;
+      }
+      }
+
+      throw new OperatorFailedException(
+            "Couldn't find valid wide move on this tree!");
+   }
+   
+   private int indexOf(NodeRef [] a, NodeRef n) {
+      for (int i=0; i<a.length; i++){
+         if (a[i] == n){
+            return i;
+         }
+      }
+      return  -1;
+   }
+
+   private double getWinningChance(int index) {
+
+      double sum = 0;
+      for (int i = 0; i < distances.length; i++) {
+         sum += (1.0 / distances[i]);
+      }
+
+      return (1.0 / distances[index]) / sum;
+
+   }
+   
+   private void calcDistances(NodeRef [] nodes, NodeRef ref) {
+      distances = new double[nodes.length];
+      for (int i = 0; i < nodes.length; i++) {
+         distances[i] = getNodeDistance(ref, nodes[i]) + 1;
+      }
+   }
+   
+   private NodeRef getRandomNode(NodeRef [] nodes, NodeRef ref) {
+      
+      calcDistances(nodes, ref);
+      double sum = 0;
+      for (int i = 0; i < distances.length; i++) {
+         sum += 1.0 / distances[i];
+      }
+
+      double randomValue = MathUtils.nextDouble() * sum;
+      NodeRef n = null;
+      for (int i = 0; i < distances.length; i++) {
+         randomValue -= 1.0 / distances[i];
+
+         if (randomValue <= 0) {
+            n = nodes[i];
+            break;
+         }
+      }
+      return n;
+   }
+
+   private int getNodeDistance(NodeRef i, NodeRef j) {
+      int count = 0;
+
+      while (i != j) {
+         count++;
+         if (tree.getNodeHeight(i) < tree.getNodeHeight(j)) {
+            i = tree.getParent(i);
+         } else {
+            j = tree.getParent(j);
+         }
+      }
+      return count;
+   }
+
+
 
     public int getMode() {
         return mode;
@@ -277,4 +432,42 @@ public class ExchangeOperator extends SimpleMCMCOperator {
         };
 
     };
+    
+    public static XMLObjectParser INTERMEDIATE_EXCHANGE_PARSER = new AbstractXMLObjectParser() {
+
+      public String getParserName() {
+         return INTERMEDIATE_EXCHANGE;
+      }
+
+      public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+
+         TreeModel treeModel = (TreeModel) xo.getChild(TreeModel.class);
+         int weight = xo.getIntegerAttribute("weight");
+
+         return new ExchangeOperator(INTERMEDIATE, treeModel, weight);
+      }
+
+      // ************************************************************************
+      // AbstractXMLObjectParser implementation
+      // ************************************************************************
+
+      public String getParserDescription() {
+         return "This element represents a intermediate exchange operator. "
+               + "This operator swaps two random subtrees.";
+      }
+
+      public Class getReturnType() {
+         return ExchangeOperator.class;
+      }
+
+      public XMLSyntaxRule[] getSyntaxRules() {
+         return rules;
+      }
+
+      private XMLSyntaxRule[] rules = new XMLSyntaxRule[] {
+            AttributeRule.newIntegerRule("weight"),
+            new ElementRule(TreeModel.class) };
+
+   };
+
 }

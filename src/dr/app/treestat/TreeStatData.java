38d5adadc374006900243f5882d58023c4107e20
rambaut <rambaut@users.noreply.github.com>
2007-11-20 09:57:16 +0000

    Fixed up some generification of TreeStatData.

--- src/dr/app/treestat/TreeStatData.java
+++ src/dr/app/treestat/TreeStatData.java
@@ -25,74 +25,73 @@
 
 package dr.app.treestat;
 
+import dr.evolution.util.Taxon;
+import dr.app.treestat.statistics.SummaryStatisticDescription;
 import org.jdom.Document;
 import org.jdom.Element;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class TreeStatData {
 	public static final String version = "1.0";
-	
+
 	public TreeStatData() {
 	}
-	
+
 	// Data options
-	public Set allTaxa = new HashSet();
-	public ArrayList taxonSets = new ArrayList();	
-	public ArrayList characters = new ArrayList();	
-	public ArrayList statistics = new ArrayList();	
-	
+	public Set<String> allTaxa = new HashSet<String>();
+	public List<TaxonSet> taxonSets = new ArrayList<TaxonSet>();
+	public List<Character> characters = new ArrayList<Character>();
+	public List<SummaryStatisticDescription> statistics = new ArrayList<SummaryStatisticDescription>();
+
 	public static class TaxonSet {
 		String name;
 		List taxa;
 		public String toString() { return name; }
 	};
-		
+
 	public static class Character {
 		String name;
 		List states;
 		public String toString() { return name; }
 	};
-		
+
 	public static class State {
 		String name;
 		String description;
 		List taxa;
 		public String toString() { return name; }
 	};
-		
+
 	/**
 	 * Read options from a file
 	 */
 	public Document create() {
-	
+
 		Element root = new Element("treeTracer");
 		root.setAttribute("version", version);
-		
+
 		Element taxonSetsElement = new Element("taxonSets");
 		Element charactersElement = new Element("characters");
 		Element statisticsElement = new Element("statistics");
-		
+
 		root.addContent(taxonSetsElement);
 		root.addContent(charactersElement);
 		root.addContent(statisticsElement);
-		
+
 		Document doc = new Document(root);
 		return doc;
 	}
-		
+
 	/**
 	 * Read options from a file
 	 */
 	public void parse(Document document) throws dr.xml.XMLParseException {
-	
+
 		Element root = document.getRootElement();
 		if (!root.getName().equals("treeTracer")) {
 			throw new dr.xml.XMLParseException("This document does not appear to be a TreeTracer file");
-		}		
+		}
 	}
 }
 

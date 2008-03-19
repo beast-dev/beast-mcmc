f9d5a7e8b6a101bf09687ccaedd2c733fa88edbb
rambaut <rambaut@users.noreply.github.com>
2008-03-19 15:13:25 +0000

    Fix for the Transmission History model.

--- src/dr/evolution/coalescent/Intervals.java
+++ src/dr/evolution/coalescent/Intervals.java
@@ -158,7 +158,7 @@ public class Intervals implements IntervalList {
 			throw new IllegalArgumentException("Too few events to construct intervals");
 		}
 
-		Arrays.sort(events);
+		Arrays.sort(events, 0, eventCount);
 
 		if (events[0].type != IntervalType.SAMPLE) {
 			throw new IllegalArgumentException("First event is not a sample event");

cd4d3a90801e5bbc110f99f7c87ec5f517280b8e
Marc Suchard <msuchard@ucla.edu>
2008-04-10 06:23:03 +0000

    Fixed GMRFSkyrideLikelihood so that it is compatiable with Tracer to make pretty pictures.

--- src/dr/evomodel/coalescent/GMRFTestLikelihood.java
+++ src/dr/evomodel/coalescent/GMRFTestLikelihood.java
@@ -17,60 +17,60 @@ import no.uib.cipr.matrix.SymmTridiagMatrix;
 public class GMRFTestLikelihood extends GMRFSkyrideLikelihood {
 
 
-    public static final String SKYLINE_TEST_LIKELIHOOD = "gmrfTestLikelihood";
-    public static final String INTERVAL_PARAMETER = "intervals";
-    public static final String SUFFSTAT_PARAMETER = "sufficientStatistics";
+	public static final String SKYLINE_TEST_LIKELIHOOD = "gmrfTestLikelihood";
+	public static final String INTERVAL_PARAMETER = "intervals";
+	public static final String SUFFSTAT_PARAMETER = "sufficientStatistics";
 
-    public GMRFTestLikelihood(Tree tree, Parameter popParameter, Parameter precParameter, Parameter lambda, Parameter beta, DesignMatrix design) {
-        super(tree, popParameter, precParameter, lambda, beta,design);
-    }
-
-
-    private Parameter intervalsParameter;
-    private Parameter statsParameter;
-    private Parameter betaParameter;
-    
-    public GMRFTestLikelihood(Parameter inPop, Parameter inPrec, Parameter inLambda, Parameter inBeta, DesignMatrix design, Parameter intervals, Parameter statParameter) {
-        super();
-        popSizeParameter = inPop;
-        precisionParameter = inPrec;
-        lambdaParameter = inLambda;
-        intervalsParameter = intervals;
-        statsParameter = statParameter;
-        betaParameter = inBeta;
-        fieldLength = popSizeParameter.getDimension();
-        addParameter(popSizeParameter);
-        addParameter(precisionParameter);
-        addParameter(lambdaParameter);
-        addParameter(intervalsParameter);
-        addParameter(statsParameter);
-        addParameter(betaParameter);
-        
-        setupGMRFWeights();
-    }
+	public GMRFTestLikelihood(Tree tree, Parameter popParameter, Parameter precParameter, Parameter lambda, Parameter beta, DesignMatrix design) {
+		super(tree, popParameter, null, precParameter, lambda, beta, design);
+	}
 
 
-    protected void storeState() {
-        super.storeState();
-        System.arraycopy(coalescentIntervals, 0, storedCoalescentIntervals, 0, coalescentIntervals.length);
-        System.arraycopy(sufficientStatistics, 0, storedSufficientStatistics, 0, sufficientStatistics.length);
-        storedWeightMatrix = weightMatrix.copy();
-    }
+	private Parameter intervalsParameter;
+	private Parameter statsParameter;
+	private Parameter betaParameter;
+
+	public GMRFTestLikelihood(Parameter inPop, Parameter inPrec, Parameter inLambda, Parameter inBeta, DesignMatrix design, Parameter intervals, Parameter statParameter) {
+		super();
+		popSizeParameter = inPop;
+		precisionParameter = inPrec;
+		lambdaParameter = inLambda;
+		intervalsParameter = intervals;
+		statsParameter = statParameter;
+		betaParameter = inBeta;
+		fieldLength = popSizeParameter.getDimension();
+		addParameter(popSizeParameter);
+		addParameter(precisionParameter);
+		addParameter(lambdaParameter);
+		addParameter(intervalsParameter);
+		addParameter(statsParameter);
+		addParameter(betaParameter);
+
+		setupGMRFWeights();
+	}
 
 
-    protected void restoreState() {
-        super.restoreState();
-        System.arraycopy(storedCoalescentIntervals, 0, coalescentIntervals, 0, storedCoalescentIntervals.length);
-        System.arraycopy(storedSufficientStatistics, 0, sufficientStatistics, 0, storedSufficientStatistics.length);
-        weightMatrix = storedWeightMatrix;
+	protected void storeState() {
+		super.storeState();
+		System.arraycopy(coalescentIntervals, 0, storedCoalescentIntervals, 0, coalescentIntervals.length);
+		System.arraycopy(sufficientStatistics, 0, storedSufficientStatistics, 0, sufficientStatistics.length);
+		storedWeightMatrix = weightMatrix.copy();
+	}
 
-    }
 
-    public double calculateLogLikelihood() {
-        return 0;
-    }
+	protected void restoreState() {
+		super.restoreState();
+		System.arraycopy(storedCoalescentIntervals, 0, coalescentIntervals, 0, storedCoalescentIntervals.length);
+		System.arraycopy(storedSufficientStatistics, 0, sufficientStatistics, 0, storedSufficientStatistics.length);
+		weightMatrix = storedWeightMatrix;
 
-    protected void setupGMRFWeights() {
+	}
+
+	public double calculateLogLikelihood() {
+		return 0;
+	}
+
+	protected void setupGMRFWeights() {
 
 //        int index = 0;
 //
@@ -88,95 +88,95 @@ public class GMRFTestLikelihood extends GMRFSkyrideLikelihood {
 //            }
 //        }
 
-        coalescentIntervals = intervalsParameter.getParameterValues();
-        sufficientStatistics = statsParameter.getParameterValues();
-        storedCoalescentIntervals = new double[coalescentIntervals.length];
-        storedSufficientStatistics = new double[sufficientStatistics.length];
+		coalescentIntervals = intervalsParameter.getParameterValues();
+		sufficientStatistics = statsParameter.getParameterValues();
+		storedCoalescentIntervals = new double[coalescentIntervals.length];
+		storedSufficientStatistics = new double[sufficientStatistics.length];
 
-        //Set up the weight Matrix
-        double[] offdiag = new double[fieldLength - 1];
-        double[] diag = new double[fieldLength];
+		//Set up the weight Matrix
+		double[] offdiag = new double[fieldLength - 1];
+		double[] diag = new double[fieldLength];
 
 //        double precision = precisionParameter.getParameterValue(0);
 
-        //First set up the offdiagonal entries;
-        for (int i = 0; i < fieldLength - 1; i++) {
-            offdiag[i] = -2.0 / (coalescentIntervals[i] + coalescentIntervals[i + 1]);
-        }
+		//First set up the offdiagonal entries;
+		for (int i = 0; i < fieldLength - 1; i++) {
+			offdiag[i] = -2.0 / (coalescentIntervals[i] + coalescentIntervals[i + 1]);
+		}
 
-        //Then set up the diagonal entries;
-        for (int i = 1; i < fieldLength - 1; i++)
-            diag[i] = -(offdiag[i] + offdiag[i - 1]);
+		//Then set up the diagonal entries;
+		for (int i = 1; i < fieldLength - 1; i++)
+			diag[i] = -(offdiag[i] + offdiag[i - 1]);
 
-        //Take care of the endpoints
-        diag[0] = -offdiag[0];
-        diag[fieldLength - 1] = -offdiag[fieldLength - 2];
+		//Take care of the endpoints
+		diag[0] = -offdiag[0];
+		diag[fieldLength - 1] = -offdiag[fieldLength - 2];
 
 
-        weightMatrix = new SymmTridiagMatrix(diag, offdiag);
+		weightMatrix = new SymmTridiagMatrix(diag, offdiag);
 
-    }
+	}
 
-    public static XMLObjectParser PARSER = new AbstractXMLObjectParser() {
+	public static XMLObjectParser PARSER = new AbstractXMLObjectParser() {
 
-        public String getParserName() {
-            return SKYLINE_TEST_LIKELIHOOD;
-        }
+		public String getParserName() {
+			return SKYLINE_TEST_LIKELIHOOD;
+		}
 
-        public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+		public Object parseXMLObject(XMLObject xo) throws XMLParseException {
 
-            XMLObject cxo = (XMLObject) xo.getChild(POPULATION_PARAMETER);
-            Parameter popParameter = (Parameter) cxo.getChild(Parameter.class);
+			XMLObject cxo = (XMLObject) xo.getChild(POPULATION_PARAMETER);
+			Parameter popParameter = (Parameter) cxo.getChild(Parameter.class);
 
-            cxo = (XMLObject) xo.getChild(PRECISION_PARAMETER);
-            Parameter precParameter = (Parameter) cxo.getChild(Parameter.class);
+			cxo = (XMLObject) xo.getChild(PRECISION_PARAMETER);
+			Parameter precParameter = (Parameter) cxo.getChild(Parameter.class);
 
-            cxo = (XMLObject) xo.getChild(INTERVAL_PARAMETER);
-            Parameter intervalParameter = (Parameter) cxo.getChild(Parameter.class);
+			cxo = (XMLObject) xo.getChild(INTERVAL_PARAMETER);
+			Parameter intervalParameter = (Parameter) cxo.getChild(Parameter.class);
 
-            cxo = (XMLObject) xo.getChild(SUFFSTAT_PARAMETER);
-            Parameter statParameter = (Parameter) cxo.getChild(Parameter.class);
+			cxo = (XMLObject) xo.getChild(SUFFSTAT_PARAMETER);
+			Parameter statParameter = (Parameter) cxo.getChild(Parameter.class);
 
-            cxo = (XMLObject) xo.getChild(LAMBDA_PARAMETER);
-            Parameter lambda = (Parameter) cxo.getChild(Parameter.class);
+			cxo = (XMLObject) xo.getChild(LAMBDA_PARAMETER);
+			Parameter lambda = (Parameter) cxo.getChild(Parameter.class);
 
-            cxo = (XMLObject) xo.getChild(BETA_PARAMETER);
-            Parameter betaParameter = (Parameter) cxo.getChild(Parameter.class);
-            
-            DesignMatrix designMatrix = (DesignMatrix)xo.getChild(DesignMatrix.class);
-            
-            
-            return new GMRFTestLikelihood(popParameter, precParameter, lambda, betaParameter, designMatrix, intervalParameter, statParameter);
+			cxo = (XMLObject) xo.getChild(BETA_PARAMETER);
+			Parameter betaParameter = (Parameter) cxo.getChild(Parameter.class);
 
-        }
+			DesignMatrix designMatrix = (DesignMatrix) xo.getChild(DesignMatrix.class);
 
-        //************************************************************************
-        // AbstractXMLObjectParser implementation
-        //************************************************************************
 
-        public String getParserDescription() {
-            return "This element represents the likelihood of the tree given the population size vector.";
-        }
+			return new GMRFTestLikelihood(popParameter, precParameter, lambda, betaParameter, designMatrix, intervalParameter, statParameter);
 
-        public Class getReturnType() {
-            return Likelihood.class;
-        }
+		}
 
-        public XMLSyntaxRule[] getSyntaxRules() {
-            return rules;
-        }
+		//************************************************************************
+		// AbstractXMLObjectParser implementation
+		//************************************************************************
 
-        private XMLSyntaxRule[] rules = new XMLSyntaxRule[]{
-                new ElementRule(POPULATION_PARAMETER, new XMLSyntaxRule[]{
-                        new ElementRule(Parameter.class)
-                }),
-                new ElementRule(PRECISION_PARAMETER, new XMLSyntaxRule[]{
-                        new ElementRule(Parameter.class)
-                }),
+		public String getParserDescription() {
+			return "This element represents the likelihood of the tree given the population size vector.";
+		}
 
-                new ElementRule(LAMBDA_PARAMETER, new XMLSyntaxRule[]{
-                        new ElementRule(Parameter.class)
-                }),
-        };
-    };
+		public Class getReturnType() {
+			return Likelihood.class;
+		}
+
+		public XMLSyntaxRule[] getSyntaxRules() {
+			return rules;
+		}
+
+		private XMLSyntaxRule[] rules = new XMLSyntaxRule[]{
+				new ElementRule(POPULATION_PARAMETER, new XMLSyntaxRule[]{
+						new ElementRule(Parameter.class)
+				}),
+				new ElementRule(PRECISION_PARAMETER, new XMLSyntaxRule[]{
+						new ElementRule(Parameter.class)
+				}),
+
+				new ElementRule(LAMBDA_PARAMETER, new XMLSyntaxRule[]{
+						new ElementRule(Parameter.class)
+				}),
+		};
+	};
 }

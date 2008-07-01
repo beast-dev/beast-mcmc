0dcd0603ba08d9a83fbb8a9792af0e03bf322c40
rambaut <rambaut@users.noreply.github.com>
2008-07-01 14:25:52 +0000

    OK. Automatic scaling is now working. If the initial evaluation of the log likelihood returns -Inf then it turns on. It will impact performance but is better than not working at all. There are a number of improvements that could be made: one is to switch it off again once the tree is significantly improved. The other is to only rescale on certain nodes.
    I tested it by comparing with the ArbitraryPrecisionLikelihoodCore (see ExperimentalTreeLikelihood) which is a form of GeneralLikelihoodCore that uses BigDecimal to a specified precision.

--- src/dr/evomodel/treelikelihood/AncestralStateTreeLikelihood.java
+++ src/dr/evomodel/treelikelihood/AncestralStateTreeLikelihood.java
@@ -49,7 +49,7 @@ public class AncestralStateTreeLikelihood extends TreeLikelihood implements Node
 	                                    boolean useScaling,
 	                                    DataType dataType,
 	                                    String tag) {
-		super(patternList, treeModel, siteModel, branchRateModel, null, useAmbiguities, false, storePartials, 0, 1.0, false);
+		super(patternList, treeModel, siteModel, branchRateModel, null, useAmbiguities, false, storePartials, false);
 		this.dataType = dataType;
 		this.tag = tag;
 

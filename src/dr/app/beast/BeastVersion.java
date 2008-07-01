f8c7906c8bd36337f24e3650dbe6293db64ab2eb
rambaut <rambaut@users.noreply.github.com>
2008-07-01 14:30:09 +0000

    Updated build number

--- src/dr/app/beast/BeastVersion.java
+++ src/dr/app/beast/BeastVersion.java
@@ -53,7 +53,7 @@ public class BeastVersion implements Version {
      * this used to parse the CVS ID string but there is no equivalent
      * for SVN. We must increment this manually.
 	 */
-	private static String BUILD_ID = "Build r949";
+	private static String BUILD_ID = "Build r970";
 
 	public String getVersionString() {
 		return "v" + VERSION;

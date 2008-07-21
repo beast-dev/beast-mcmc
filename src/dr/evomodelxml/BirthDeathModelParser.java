3674359eb7c8430dba75de8d72ffdb63327d00ca
Alexei Drummond <alexeid@users.noreply.github.com>
2008-07-21 15:03:45 +0000

    separated out speciation prior parsers and set yule model XML to silently use birth-death model with zero death rate.

---
+++ src/dr/evomodelxml/BirthDeathModelParser.java
@@ -0,0 +1,58 @@
+package dr.evomodelxml;
+
+import dr.evolution.util.Units;
+import dr.evomodel.speciation.BirthDeathGernhard08Model;
+import dr.evoxml.XMLUnits;
+import dr.inference.model.Parameter;
+import dr.xml.*;
+
+/**
+ * @author Alexei Drummond
+ * @author Joseph Heled
+ */
+public class BirthDeathModelParser extends AbstractXMLObjectParser {
+
+
+    public static final String BIRTH_DEATH_MODEL = "birthDeathModel";
+    public static String BIRTHDIFF_RATE = "birthMinusDeathRate";
+    public static String RELATIVE_DEATH_RATE = "relativeDeathRate";
+
+    public static String BIRTHDIFF_RATE_PARAM_NAME = "birthDeath.BminusDRate";
+    public static String RELATIVE_DEATH_RATE_PARAM_NAME = "birthDeath.DoverB";
+
+    public String getParserName() {
+        return BIRTH_DEATH_MODEL;
+    }
+
+    public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+
+        Units.Type units = XMLParser.Utils.getUnitsAttr(xo);
+
+        Parameter birthParameter = (Parameter) xo.getElementFirstChild(BIRTHDIFF_RATE);
+        Parameter deathParameter = (Parameter) xo.getElementFirstChild(RELATIVE_DEATH_RATE);
+
+        return new BirthDeathGernhard08Model(birthParameter, deathParameter, units);
+    }
+
+    //************************************************************************
+    // AbstractXMLObjectParser implementation
+    //************************************************************************
+
+    public String getParserDescription() {
+        return "Gernhard (2008) model of speciation (equation at bottom of page 19 of draft).";
+    }
+
+    public Class getReturnType() {
+        return BirthDeathGernhard08Model.class;
+    }
+
+    public XMLSyntaxRule[] getSyntaxRules() {
+        return rules;
+    }
+
+    private XMLSyntaxRule[] rules = new XMLSyntaxRule[]{
+            new ElementRule(BIRTHDIFF_RATE, new XMLSyntaxRule[]{new ElementRule(Parameter.class)}),
+            new ElementRule(RELATIVE_DEATH_RATE, new XMLSyntaxRule[]{new ElementRule(Parameter.class)}),
+            XMLUnits.SYNTAX_RULES[0]
+    };
+}

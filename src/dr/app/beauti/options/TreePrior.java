b48272d6bdd8872e699c24e4c5e83082a8f97c1c
rambaut <rambaut@users.noreply.github.com>
2008-07-26 22:35:34 +0000

    Gave the tree prior enums a name which means they can be used directly in a JComboBox. It will make it more robust to changes of order.

--- src/dr/app/beauti/options/TreePrior.java
+++ src/dr/app/beauti/options/TreePrior.java
@@ -5,6 +5,26 @@ package dr.app.beauti.options;
  */
 public enum TreePrior {
 
-    CONSTANT, EXPONENTIAL, LOGISTIC, EXPANSION, SKYLINE, EXTENDED_SKYLINE, YULE, BIRTH_DEATH
+    CONSTANT("Coalescent: Constant Size"),
+    EXPONENTIAL("Coalescent: Exponential Growth"),
+    LOGISTIC("Coalescent: Logistic Growth"),
+    EXPANSION("Coalescent: Expansion Growth"),
+    SKYLINE("Coalescent: Bayesian Skyline"),
+    EXTENDED_SKYLINE("Coalescent: Extended Bayesian Skyline"),
+    YULE("Speciation: Yule Process"),
+    BIRTH_DEATH("Speciation: Birth-Death Process");
 
+    TreePrior(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return super.toString();
+    }
+
+    private final String name;
 }

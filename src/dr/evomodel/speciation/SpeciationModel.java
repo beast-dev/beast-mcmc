b8840c5fad0227b475ae506f73f6c9f3a3b93fc9
rambaut <rambaut@users.noreply.github.com>
2008-08-04 10:42:01 +0000

    Added ability to exclude taxa from the speciation model. This results in the speciation prior being calculated on the subtree of the included taxa only.

--- src/dr/evomodel/speciation/SpeciationModel.java
+++ src/dr/evomodel/speciation/SpeciationModel.java
@@ -28,9 +28,14 @@ package dr.evomodel.speciation;
 import dr.evolution.tree.NodeRef;
 import dr.evolution.tree.Tree;
 import dr.evolution.util.Units;
+import dr.evolution.util.TaxonList;
+import dr.evolution.util.Taxon;
 import dr.inference.model.AbstractModel;
 import dr.inference.model.Model;
 import dr.inference.model.Parameter;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * This interface provides methods that describe a speciation model.
@@ -38,10 +43,10 @@ import dr.inference.model.Parameter;
  */
 public abstract class SpeciationModel extends AbstractModel implements Units {
 
-	public SpeciationModel(String modelName, Type units) {
-		super(modelName);
-		setUnits(units);
-	}
+    public SpeciationModel(String modelName, Type units) {
+        super(modelName);
+        setUnits(units);
+    }
 
     /**
      *  "fixed" part of likelihood. Guaranteed to be called before any logNodeProbability
@@ -88,46 +93,101 @@ public abstract class SpeciationModel extends AbstractModel implements Units {
         return logL;
     }
 
+    /**
+     * Alternative likelihood calculation that uses recursion over the tree and allows
+     * a list of taxa to exclude
+     * @param tree     the tree
+     * @param exclude  a list of taxa to exclude
+     * @return         log-likelihood of density
+     */
+    public double calculateTreeLogLikelihood(Tree tree, Set<Taxon> exclude) {
+        final int taxonCount = tree.getExternalNodeCount() - exclude.size();
+
+        double[] lnL = new double[] { logTreeProbability(taxonCount) };
+
+        calculateNodeLogLikelihood(tree, tree.getRoot(), exclude, lnL);
+
+        return lnL[0];
+    }
+
+    /**
+     * Alternative likelihood calculation that uses recursion over the tree and allows
+     * a list of taxa to exclude
+     * @param tree     the tree
+     * @param exclude  a list of taxa to exclude
+     * @param lnL      a reference to the lnL sum
+     * @return         the number of included daughter nodes
+     */
+    private int calculateNodeLogLikelihood(Tree tree, NodeRef node, Set<Taxon> exclude, double[] lnL) {
+        if (tree.isExternal(node)) {
+            if (exclude.contains(tree.getNodeTaxon(node))) {
+                if (includeExternalNodesInLikelihoodCalculation()) {
+                    lnL[0] += logNodeProbability(tree, node);
+                }
+
+                // this tip is included in the subtree...
+                return 1;
+            }
+
+            // this tip is excluded from the subtree...
+            return 0;
+        } else {
+            int count = 0;
+            for (int i = 0; i < tree.getChildCount(node); i++) {
+                NodeRef child = tree.getChild(node, i);
+                count += calculateNodeLogLikelihood(tree, child, exclude, lnL);
+            }
+
+            if (count == 2) {
+                // this node is included in the subtree...
+                lnL[0] += logNodeProbability(tree, node);
+            }
+
+            // if at least one of the children has included tips then return 1 otherwise 0
+            return count > 0 ? 1 : 0;
+        }
+    }
+
     // **************************************************************
     // Model IMPLEMENTATION
     // **************************************************************
-	
-	protected void handleModelChangedEvent(Model model, Object object, int index) {
-		// no intermediates need to be recalculated...
-	}
-	
-	protected void handleParameterChangedEvent(Parameter parameter, int index) {
-		// no intermediates need to be recalculated...
-	}
-	
-	protected void storeState() {} // no additional state needs storing
-	protected void restoreState() {} // no additional state needs restoring	
-	protected void acceptState() {} // no additional state needs accepting	
+
+    protected void handleModelChangedEvent(Model model, Object object, int index) {
+        // no intermediates need to be recalculated...
+    }
+
+    protected void handleParameterChangedEvent(Parameter parameter, int index) {
+        // no intermediates need to be recalculated...
+    }
+
+    protected void storeState() {} // no additional state needs storing
+    protected void restoreState() {} // no additional state needs restoring
+    protected void acceptState() {} // no additional state needs accepting
 
     // **************************************************************
     // Units IMPLEMENTATION
     // **************************************************************
 
-	/**
-	 * Units in which population size is measured.
-	 */
-	private Type units;
+    /**
+     * Units in which population size is measured.
+     */
+    private Type units;
 
-	/**
-	 * sets units of measurement.
-	 *
-	 * @param u units
-	 */
-	public void setUnits(Type u)
-	{
-		units = u;
-	}
+    /**
+     * sets units of measurement.
+     *
+     * @param u units
+     */
+    public void setUnits(Type u)
+    {
+        units = u;
+    }
 
-	/**
-	 * returns units of measurement.
-	 */
-	public Type getUnits()
-	{
-		return units;
-	}
+    /**
+     * returns units of measurement.
+     */
+    public Type getUnits()
+    {
+        return units;
+    }
 }

c6c0a2e48d92b6fc4d334136f6199d0b64dce5c5
Alexei Drummond <alexeid@users.noreply.github.com>
2008-08-20 14:55:46 +0000

    doubled the speed of RandomLocalYuleModel :-)

--- src/dr/evomodel/speciation/RandomLocalYuleModel.java
+++ src/dr/evomodel/speciation/RandomLocalYuleModel.java
@@ -25,6 +25,7 @@ public class RandomLocalYuleModel extends SpeciationModel implements NodeAttribu
     public static String BIRTH_RATE = "birthRates";
     public static String BIRTH_RATE_INDICATORS = "indicators";
     public static String RATES_AS_MULTIPLIERS = "ratesAsMultipliers";
+    private boolean calculateAllBirthRates = false;
 
     public RandomLocalYuleModel(Parameter birthRates, Parameter indicators, Parameter meanRate,
                                 boolean ratesAsMultipliers, Type units, int dp) {
@@ -51,6 +52,8 @@ public class RandomLocalYuleModel extends SpeciationModel implements NodeAttribu
         System.out.println("  birth rates parameter is named '" + birthRatesName + "'");
         indicatorsName = indicators.getParameterName();
         System.out.println("  indicator parameter is named '" + indicatorsName + "'");
+
+        this.birthRates = new double[birthRates.getDimension() + 1];
     }
 
     public final double getVariable(TreeModel tree, NodeRef node) {
@@ -61,33 +64,93 @@ public class RandomLocalYuleModel extends SpeciationModel implements NodeAttribu
         return tree.getNodeTrait(node, indicatorsName) > 0.5;
     }
 
-    /**
-     * @param tree the tree
-     * @param node the node to retrieve the birth rate of
-     * @return the birth rate of the given node;
-     */
-    double getBirthRate(TreeModel tree, NodeRef node) {
+    //
+    // functions that define a speciation model
+    //
+    public final double logTreeProbability(int taxonCount) {
 
-        double birthRate;
-        if (!tree.isRoot(node)) {
+        // calculate all nodes birth rates
+        calculateAllBirthRates = true;
 
-            double parentRate = getBirthRate(tree, tree.getParent(node));
-            if (isVariableSelected(tree, node)) {
-                birthRate = getVariable(tree, node);
-                if (birthRatesAreMultipliers) {
-                    birthRate *= parentRate;
-                } else {
-                    throw new RuntimeException("Rates must be multipliers in current implementation! " +
-                            "Otherwise root rate might be ignored");
-                }
-            } else {
-                birthRate = parentRate;
-            }
-        } else {
-            birthRate = meanRate.getParameterValue(0);
-        }
-        return birthRate;
+        return 0.0;
+
     }
+
+    //
+    // functions that define a speciation model
+    //
+    public final double logNodeProbability(Tree tree, NodeRef node) {
+
+        if (calculateAllBirthRates) {
+            calculateBirthRates((TreeModel) tree, tree.getRoot(), 0.0);
+            calculateAllBirthRates = false;
+        }
+
+        if (tree.isRoot(node)) {
+            return 0.0;
+        } else {
+
+            double lambda = birthRates[node.getNumber()];
+            double branchLength = tree.getNodeHeight(tree.getParent(node)) - tree.getNodeHeight(node);
+            double logP = -lambda * branchLength;
+
+            if (tree.isExternal(node)) logP += Math.log(lambda);
+
+            return logP;
+        }
+    }
+
+    private void calculateBirthRates(TreeModel tree, NodeRef node, double rate) {
+
+        int nodeNumber = node.getNumber();
+
+        if (tree.isRoot(node)) {
+            rate = meanRate.getParameterValue(0);
+        } else {
+            if (isVariableSelected(tree, node)) {
+                if (birthRatesAreMultipliers) {
+                    rate *= getVariable(tree, node);
+                } else {
+                    rate = getVariable(tree, node);
+                    ;
+                }
+            }
+        }
+        birthRates[nodeNumber] = rate;
+
+        int childCount = tree.getChildCount(node);
+        for (int i = 0; i < childCount; i++) {
+            calculateBirthRates(tree, tree.getChild(node, i), rate);
+        }
+    }
+
+//    /**
+//     * @param tree the tree
+//     * @param node the node to retrieve the birth rate of
+//     * @return the birth rate of the given node;
+//     */
+//    private double getBirthRate(TreeModel tree, NodeRef node) {
+//
+//        double birthRate;
+//        if (!tree.isRoot(node)) {
+//
+//            double parentRate = getBirthRate(tree, tree.getParent(node));
+//            if (isVariableSelected(tree, node)) {
+//                birthRate = getVariable(tree, node);
+//                if (birthRatesAreMultipliers) {
+//                    birthRate *= parentRate;
+//                } else {
+//                    throw new RuntimeException("Rates must be multipliers in current implementation! " +
+//                            "Otherwise root rate might be ignored");
+//                }
+//            } else {
+//                birthRate = parentRate;
+//            }
+//        } else {
+//            birthRate = meanRate.getParameterValue(0);
+//        }
+//        return birthRate;
+//    }
 
     public String[] getNodeAttributeLabel() {
         return new String[]{"I", "b"};
@@ -95,42 +158,13 @@ public class RandomLocalYuleModel extends SpeciationModel implements NodeAttribu
 
     public String[] getAttributeForNode(Tree tree, NodeRef node) {
 
-        String rateString = format.format(getBirthRate((TreeModel) tree, node));
+        String rateString = format.format(birthRates[node.getNumber()]);
 
         if (tree.isRoot(node)) {
             return new String[]{"0", rateString};
         }
 
         return new String[]{(isVariableSelected((TreeModel) tree, node) ? "1" : "0"), rateString};
-    }
-
-    //
-    // functions that define a speciation model
-    //
-    public double logTreeProbability(int taxonCount) {
-        return 0.0;
-    }
-
-    //
-    // functions that define a speciation model
-    //
-    public double logNodeProbability(Tree tree, NodeRef node) {
-
-
-        if (tree.isRoot(node)) {
-            return 0.0;
-        } else {
-
-            double lambda = getBirthRate((TreeModel) tree, node);
-
-            double branchLength = tree.getNodeHeight(tree.getParent(node)) - tree.getNodeHeight(node);
-
-            double logP = -lambda * branchLength;
-
-            if (tree.isExternal(node)) logP += Math.log(lambda);
-
-            return logP;
-        }
     }
 
     public boolean includeExternalNodesInLikelihoodCalculation() {
@@ -202,6 +236,8 @@ public class RandomLocalYuleModel extends SpeciationModel implements NodeAttribu
                 XMLUnits.SYNTAX_RULES[0]
         };
     };
+
+    private double[] birthRates;
 
     private String birthRatesName = "birthRates";
     private String indicatorsName = "birthRateIndicator";

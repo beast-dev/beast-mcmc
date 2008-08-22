62d5429da44195af4d27b285782a79cdf007f49e
Alexei Drummond <alexeid@users.noreply.github.com>
2008-08-22 14:27:07 +0000

    refactoring of ALS stuff with Alex

--- src/dr/evomodel/MSSD/AbstractObservationProcess.java
+++ src/dr/evomodel/MSSD/AbstractObservationProcess.java
@@ -16,8 +16,8 @@ import dr.math.GammaFunction;
 /**
  * Package: AbstractObservationProcess
  * Description:
- *
- *
+ * <p/>
+ * <p/>
  * Created by
  * Alexander V. Alekseyenko (alexander.alekseyenko@gmail.com)
  * Date: Feb 19, 2008
@@ -46,56 +46,65 @@ abstract public class AbstractObservationProcess extends AbstractModel {
     protected MutationDeathType dataType;
     protected int deathState;
     protected SiteModel siteModel;
+    private double logN;
 
     public AbstractObservationProcess(String Name, TreeModel treeModel, PatternList patterns, SiteModel siteModel,
                                       Parameter mu, Parameter lam) {
         super(Name);
         this.treeModel = treeModel;
         this.patterns = patterns;
-        this.mu=mu;
-        this.lam=lam;
-        this.siteModel=siteModel;
+        this.mu = mu;
+        this.lam = lam;
+        this.siteModel = siteModel;
         addModel(treeModel);
         addParameter(mu);
         addParameter(lam);
 
         nodeCount = treeModel.getNodeCount();
-        stateCount= patterns.getDataType().getStateCount();
+        stateCount = patterns.getDataType().getStateCount();
         this.patterns = patterns;
         patternCount = patterns.getPatternCount();
-        patternWeights=patterns.getPatternWeights();
-        totalPatterns=0;
-        for(int i=0; i< patternCount; ++i){
-            totalPatterns+=patternWeights[i];
+        patternWeights = patterns.getPatternWeights();
+        totalPatterns = 0;
+        for (int i = 0; i < patternCount; ++i) {
+            totalPatterns += patternWeights[i];
         }
-        gammaNorm = -GammaFunction.lnGamma(totalPatterns+1);
+        logN = Math.log(totalPatterns);
 
-        dataType =  (MutationDeathType)patterns.getDataType();
-        this.deathState=dataType.DEATHSTATE;
+        gammaNorm = -GammaFunction.lnGamma(totalPatterns + 1);
+
+        dataType = (MutationDeathType) patterns.getDataType();
+        this.deathState = dataType.DEATHSTATE;
         setNodePatternInclusion();
         cumLike = new double[patternCount];
         nodeLikelihoods = new double[patternCount];
-        weightKnown=false;
+        weightKnown = false;
     }
 
-    public Parameter getMuParameter() { return mu; }
-    public Parameter getLamParameter() { return lam; }
+    public Parameter getMuParameter() {
+        return mu;
+    }
+
+    public Parameter getLamParameter() {
+        return lam;
+    }
+
 
     public final double nodePatternLikelihood(double[] freqs, BranchRateModel branchRateModel, LikelihoodCore likelihoodCore) {
-        int i,j;
-        double logL=gammaNorm;
+        int i, j;
+        double logL = gammaNorm;
 
-        double birthRate=lam.getParameterValue(0);
+        double birthRate = lam.getParameterValue(0);
         double prob;
 
-        if(nodePartials==null)
-            nodePartials=new double[patternCount*stateCount];
+        if (nodePartials == null)
+            nodePartials = new double[patternCount * stateCount];
 
         for (j = 0; j < patternCount; ++j) cumLike[j] = 0;
 
         for (i = 0; i < nodeCount; ++i) {
             // get partials for node i
-            likelihoodCore.getPartials(i,nodePartials);
+            likelihoodCore.getPartials(i, nodePartials);
 /*            System.err.println("Partials for node "+i);
             for(int m=0; m<patternCount;++m){
                 System.err.print("[");
@@ -109,66 +118,65 @@ abstract public class AbstractObservationProcess extends AbstractModel {
                     this part could be optimized by first summing
                     and then multiplying by equilibrium probs
             */
-            likelihoodCore.calculateLogLikelihoods(nodePartials,freqs,nodeLikelihoods);
+            likelihoodCore.calculateLogLikelihoods(nodePartials, freqs, nodeLikelihoods);
             prob = Math.log(getNodeSurvivalProbability(i, branchRateModel));
 
-            for (j=0;j<patternCount;++j){
+            for (j = 0; j < patternCount; ++j) {
                 if (nodePatternInclusion[i][j])
-                    cumLike[j]+=Math.exp(nodeLikelihoods[j]+prob);
+                    cumLike[j] += Math.exp(nodeLikelihoods[j] + prob);
             }
         }
 
         double ascertainmentCorrection = getAscertainmentCorrection(cumLike);
 //        System.err.println("AscertainmentCorrection: "+ascertainmentCorrection);
 
-        for (j = 0; j < patternCount; ++j){
-            logL += Math.log(cumLike[j]/ascertainmentCorrection) * patternWeights[j];
+        for (j = 0; j < patternCount; ++j) {
+            logL += Math.log(cumLike[j] / ascertainmentCorrection) * patternWeights[j];
         }
 
-        double deathRate=mu.getParameterValue(0);
+        double deathRate = mu.getParameterValue(0);
 
-        for(i=0;i<siteModel.getCategoryCount();++i){
-            logL+=Math.log(birthRate/(deathRate*siteModel.getRateForCategory(i)))*siteModel.getProportionForCategory(i)*totalPatterns;
-        }
+        double logTreeWeight = getLogTreeWeight(branchRateModel);
 
 /*        System.err.println("Patterns contribution "+logL);
         System.err.println("Patterns less gammanorm "+(logL-gammaNorm));
         System.err.println("TreeWeight "+getLogTreeWeight(branchRateModel));*/
-
-        logL += getLogTreeWeight(branchRateModel);  
+        if (integrateGainRate) {
+            logL -= gammaNorm + logN + Math.log(-logTreeWeight * deathRate / birthRate) * totalPatterns;
+        } else {
+            logL += logTreeWeight + Math.log(birthRate / deathRate) * totalPatterns;
+        }
         return logL;
     }
 
-    protected double getAscertainmentCorrection(double [] patternProbs){
+    protected double getAscertainmentCorrection(double[] patternProbs) {
         // This function probably belongs better to the AscertainedSitePatterns
-        double excludeProb=0, includeProb=0, returnProb=1.0;
-        if(this.patterns instanceof AscertainedSitePatterns){
-            int[] includeIndices = ((AscertainedSitePatterns)patterns).getIncludePatternIndices();
-            int[] excludeIndices = ((AscertainedSitePatterns)patterns).getExcludePatternIndices();
-            for (int i = 0; i < ((AscertainedSitePatterns)patterns).getIncludePatternCount(); i++) {
+        double excludeProb = 0, includeProb = 0, returnProb = 1.0;
+        if (this.patterns instanceof AscertainedSitePatterns) {
+            int[] includeIndices = ((AscertainedSitePatterns) patterns).getIncludePatternIndices();
+            int[] excludeIndices = ((AscertainedSitePatterns) patterns).getExcludePatternIndices();
+            for (int i = 0; i < ((AscertainedSitePatterns) patterns).getIncludePatternCount(); i++) {
                 int index = includeIndices[i];
-                includeProb+=patternProbs[index];
+                includeProb += patternProbs[index];
             }
-            for (int j = 0; j < ((AscertainedSitePatterns)patterns).getExcludePatternCount(); j++) {
+            for (int j = 0; j < ((AscertainedSitePatterns) patterns).getExcludePatternCount(); j++) {
                 int index = excludeIndices[j];
-                excludeProb+=patternProbs[index];
+                excludeProb += patternProbs[index];
             }
-            if(includeProb==0.0){
-                returnProb-=excludeProb;
-            }
-            else if(excludeProb==0.0){
-                returnProb=includeProb;
-            }
-            else{
-                returnProb=includeProb-excludeProb;
+            if (includeProb == 0.0) {
+                returnProb -= excludeProb;
+            } else if (excludeProb == 0.0) {
+                returnProb = includeProb;
+            } else {
+                returnProb = includeProb - excludeProb;
             }
         }
 
         return returnProb;
     }
 
-    final public double getLogTreeWeight(BranchRateModel branchRateModel){
-        if(!weightKnown){
+    final public double getLogTreeWeight(BranchRateModel branchRateModel) {
+        if (!weightKnown) {
             logTreeWeight = calculateLogTreeWeight(branchRateModel);
             weightKnown = true;
         }
@@ -177,17 +185,14 @@ abstract public class AbstractObservationProcess extends AbstractModel {
     }
 
     abstract public double calculateLogTreeWeight(BranchRateModel branchRateModel);
-    /*
-    setNodePatternInclusion populates nodePatternInclusion[][]
-    nodePatternInclusion[i][p] if for pattern p gain event above node i is valid
-     */
+
     abstract void setNodePatternInclusion();
 
-    final public double getAverageRate(){
-        double avgRate=0.0;
+    final public double getAverageRate() {
+        double avgRate = 0.0;
         double proportions[] = siteModel.getCategoryProportions();
-        for(int i=0;i<siteModel.getCategoryCount();++i){
-            avgRate+= proportions[i]*siteModel.getRateForCategory(i);
+        for (int i = 0; i < siteModel.getCategoryCount(); ++i) {
+            avgRate += proportions[i] * siteModel.getRateForCategory(i);
         }
         return avgRate;
     }
@@ -198,7 +203,7 @@ abstract public class AbstractObservationProcess extends AbstractModel {
 
         if (parent == null) return 1.0;
 
-        final double deathRate=mu.getParameterValue(0)*getAverageRate();
+        final double deathRate = mu.getParameterValue(0) * getAverageRate();
         final double branchRate = branchRateModel.getBranchRate(treeModel, node);
         // Get the operational time of the branch
         final double branchTime = branchRate * treeModel.getBranchLength(node);
@@ -206,16 +211,16 @@ abstract public class AbstractObservationProcess extends AbstractModel {
     }
 
     protected void handleModelChangedEvent(Model model, Object object, int index) {
-        if(model==treeModel){
+        if (model == treeModel) {
             weightKnown = false;
         }
     }
 
     protected void handleParameterChangedEvent(Parameter parameter, int index) {
-        if(parameter == mu || parameter == lam){
-            weightKnown=false;
-        }else{
-            System.err.println("AbstractObservationProcess: Got unexpected parameter changed event. (Parameter = "+parameter+")");
+        if (parameter == mu || parameter == lam) {
+            weightKnown = false;
+        } else {
+            System.err.println("AbstractObservationProcess: Got unexpected parameter changed event. (Parameter = " + parameter + ")");
         }
     }
 
@@ -224,9 +229,15 @@ abstract public class AbstractObservationProcess extends AbstractModel {
     }
 
     protected void restoreState() {
-        logTreeWeight=storedLogTreeWeight;
+        logTreeWeight = storedLogTreeWeight;
     }
 
     protected void acceptState() {
     }
+
+    public void setIntegrateGainRate(boolean integrateGainRate) {
+        this.integrateGainRate = integrateGainRate;
+    }
+
+    private boolean integrateGainRate = false;
 }

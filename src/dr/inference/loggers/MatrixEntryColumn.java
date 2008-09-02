b86c5bbb45bd30053e7a341a073482635de56a8d
Marc Suchard <msuchard@ucla.edu>
2008-09-02 16:48:26 +0000

    Implemented BSSVS for possible complex substitution matrices.

--- src/dr/inference/loggers/MatrixEntryColumn.java
+++ src/dr/inference/loggers/MatrixEntryColumn.java
@@ -8,8 +8,9 @@ public class MatrixEntryColumn extends NumberColumn {
 	private int indexI;
 	private int indexJ;
 	private double[][] mat;
+	private Double normalization;
 
-	public MatrixEntryColumn(String name, int indexI, int indexJ, double[][] mat) {
+	public MatrixEntryColumn(String name, int indexI, int indexJ, double[][] mat, Double normalization) {
 		super(name + "_" + indexI + "/" + indexJ);
 
 		this.mat = mat;
@@ -19,9 +20,14 @@ public class MatrixEntryColumn extends NumberColumn {
 
 		this.indexI = indexI;
 		this.indexJ = indexJ;
+		this.normalization = normalization;
+	}
+
+	public MatrixEntryColumn(String name, int indexI, int indexJ, double[][] mat) {
+		this(name, indexI, indexJ, mat, 1.0);
 	}
 
 	public double getDoubleValue() {
-		return mat[indexI][indexJ];
+		return mat[indexI][indexJ] / normalization;
 	}
 }

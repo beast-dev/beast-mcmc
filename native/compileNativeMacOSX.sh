38a6566173fa9df82084c6726fc20d43ebc78a45
Marc Suchard <msuchard@ucla.edu>
2008-10-05 04:23:46 +0000

    As usual, I forgot the C source files

--- native/compileNativeMacOSX.sh
+++ native/compileNativeMacOSX.sh
@@ -3,36 +3,47 @@
 
 
 # This now creates a 'fat' dylib which runs on intel & PPC macs:
-cc -c -arch ppc -O2 -funroll-loops -I/Library/Java/Home/include -o NucleotideLikelihoodCore.PPC.o NucleotideLikelihoodCore.c
-cc -o libNucleotideLikelihoodCore.PPC.jnilib -framework JavaVM -arch ppc -dynamiclib NucleotideLikelihoodCore.PPC.o
+#cc -c -arch ppc -O2 -funroll-loops -I/Library/Java/Home/include -o NucleotideLikelihoodCore.PPC.o NucleotideLikelihoodCore.c
+#cc -o libNucleotideLikelihoodCore.PPC.jnilib -framework JavaVM -arch ppc -dynamiclib NucleotideLikelihoodCore.PPC.o
 
-cc -c -arch i386 -O2 -funroll-loops -I/Library/Java/Home/include -o NucleotideLikelihoodCore.i386.o NucleotideLikelihoodCore.c
-cc -o libNucleotideLikelihoodCore.i386.jnilib -framework JavaVM -arch i386 -dynamiclib NucleotideLikelihoodCore.i386.o
+#cc -c -arch i386 -O2 -funroll-loops -I/Library/Java/Home/include -o NucleotideLikelihoodCore.i386.o NucleotideLikelihoodCore.c
+#cc -o libNucleotideLikelihoodCore.i386.jnilib -framework JavaVM -arch i386 -dynamiclib NucleotideLikelihoodCore.i386.o
 
-lipo -create libNucleotideLikelihoodCore.PPC.jnilib libNucleotideLikelihoodCore.i386.jnilib -output libNucleotideLikelihoodCore.jnilib
+#lipo -create libNucleotideLikelihoodCore.PPC.jnilib libNucleotideLikelihoodCore.i386.jnilib -output libNucleotideLikelihoodCore.jnilib
 
 ##AminoAcidLikelihoodCore
-cc -c -arch ppc -O2 -funroll-loops -I/Library/Java/Home/include -o AminoAcidLikelihoodCore.PPC.o AminoAcidLikelihoodCore.c
-cc -o libAminoAcidLikelihoodCore.PPC.jnilib -framework JavaVM -arch ppc -dynamiclib AminoAcidLikelihoodCore.PPC.o
+#cc -c -arch ppc -O2 -funroll-loops -I/Library/Java/Home/include -o AminoAcidLikelihoodCore.PPC.o AminoAcidLikelihoodCore.c
+#cc -o libAminoAcidLikelihoodCore.PPC.jnilib -framework JavaVM -arch ppc -dynamiclib AminoAcidLikelihoodCore.PPC.o
 
-cc -c -arch i386 -O2 -funroll-loops -I/Library/Java/Home/include -o AminoAcidLikelihoodCore.i386.o AminoAcidLikelihoodCore.c
-cc -o libAminoAcidLikelihoodCore.i386.jnilib -framework JavaVM -arch i386 -dynamiclib AminoAcidLikelihoodCore.i386.o
+#cc -c -arch i386 -O2 -funroll-loops -I/Library/Java/Home/include -o AminoAcidLikelihoodCore.i386.o AminoAcidLikelihoodCore.c
+#cc -o libAminoAcidLikelihoodCore.i386.jnilib -framework JavaVM -arch i386 -dynamiclib AminoAcidLikelihoodCore.i386.o
 
-lipo -create libAminoAcidLikelihoodCore.PPC.jnilib libAminoAcidLikelihoodCore.i386.jnilib -output libAminoAcidLikelihoodCore.jnilib
+#lipo -create libAminoAcidLikelihoodCore.PPC.jnilib libAminoAcidLikelihoodCore.i386.jnilib -output libAminoAcidLikelihoodCore.jnilib
 
 ##GeneralLikelihoodCore
-cc -c -arch i386  -O2 -funroll-loops -I/Library/Java/Home/include \
-   -o GeneralLikelihoodCore.i386.o GeneralLikelihoodCore.c
-
-cc -o libGeneralLikelihoodCore.i386.jnilib -framework JavaVM -arch i386 -dynamiclib GeneralLikelihoodCore.i386.o
-
-lipo -create libGeneralLikelihoodCore.i386.jnilib -output libGeneralLikelihoodCore.jnilib
-
-##NativeMemoryLikelihoodCore -- Generates pointer cast warnings
 #cc -c -arch i386  -O2 -funroll-loops -I/Library/Java/Home/include \
-#   -o NativeMemoryLikelihoodCore.i386.o NativeMemoryLikelihoodCore.c
-#
-#cc -o libNativeMemoryLikelihoodCore.i386.jnilib -framework JavaVM -arch i386 -dynamiclib NativeMemoryLikelihoodCore.i386.o
-#
-#lipo -create libNativeMemoryLikelihoodCore.i386.jnilib -output libNativeMemoryLikelihoodCore.jnilib
+#   -o GeneralLikelihoodCore.i386.o GeneralLikelihoodCore.c
+
+#cc -o libGeneralLikelihoodCore.i386.jnilib -framework JavaVM -arch i386 -dynamiclib GeneralLikelihoodCore.i386.o
+
+#lipo -create libGeneralLikelihoodCore.i386.jnilib -output libGeneralLikelihoodCore.jnilib
+
+#NativeMemoryLikelihoodCore -- Generates pointer cast warnings
+cc -c -arch i386  -O2 -funroll-loops -I/Library/Java/Home/include \
+   -o NativeMemoryLikelihoodCore.i386.o NativeMemoryLikelihoodCore.c 
+
+cc -c -arch i386  -O2 -funroll-loops -I/Library/Java/Home/include \
+   -o NativeSubstitutionModel.i386.o NativeSubstitutionModel.c
+
+cc -o libNativeMemoryLikelihoodCore.i386.jnilib -framework JavaVM -arch i386 \
+    -dynamiclib NativeMemoryLikelihoodCore.i386.o NativeSubstitutionModel.i386.o
+
+
+#cc -o libNativeSubstitutionModel.i386.jnilib -framework JavaVM -arch i386 -dynamiclib NativeSubstitutionModel.i386.o
+
+lipo -create libNativeMemoryLikelihoodCore.i386.jnilib -output libNativeMemoryLikelihoodCore.jnilib 
+    
+
+
+
 

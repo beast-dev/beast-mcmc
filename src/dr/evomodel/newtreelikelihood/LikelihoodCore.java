e373ccf4d6947ac16cec03fd9b4ab8c72ce12b95
rambaut <rambaut@users.noreply.github.com>
2008-10-06 23:12:28 +0000

    newtreelikelihood is a new approach to the likelihood core to minimize communication between Java and native implementations. This should allow faster native code and use of GPUs. This is just a temporary parallel package whilst this is developed.

---
+++ src/dr/evomodel/newtreelikelihood/LikelihoodCore.java
@@ -0,0 +1,106 @@
+/*
+ * LikelihoodCore.java
+ *
+ * Copyright (C) 2002-2007 Alexei Drummond and Andrew Rambaut
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package dr.evomodel.newtreelikelihood;
+
+/**
+ * LikelihoodCore - An interface describing the core likelihood functions.
+ *
+ * @author Andrew Rambaut
+ * @version $Id: LikelihoodCore.java,v 1.15 2006/08/29 18:07:23 rambaut Exp $
+ */
+
+public interface LikelihoodCore {
+
+    /**
+     * initializes partial likelihood arrays.
+     * @param nodeCount the number of nodes in the tree (internal and external)
+     * @param patternCount the number of patterns
+     * @param matrixCount the number of rate categories
+     */
+    void initialize(int nodeCount, int patternCount, int matrixCount);
+
+    /**
+     * cleans up and deallocates arrays.
+     */
+    void finalize() throws Throwable;
+
+    /**
+     * Sets partials for a tip - these are numbered from 0 and remain
+     * constant throughout the run.
+     * @param tipIndex the tip index
+     * @param partials an array of partial likelihoods (patternCount * stateCount)
+     */
+    void setTipPartials(int tipIndex, double[] partials);
+
+    /**
+     * Sets the precursor matrix - this will be
+     * used to produce transition probability matrices for each
+     * branch. This is only called when a parameter of the substitution
+     * model changes.
+     * @param matrix the matrix
+     */
+    void setPrecursorMatrix(double[] matrix);
+
+    /**
+     * Specify the branch lengths that are being updated. These will be used to construct
+     * the transition probability matrices for each branch.
+     * @param branchUpdateIndices the node indices of the branches to be updated
+     * @param branchLengths the branch lengths of the branches to be updated
+     * @param branchUpdateCount the number of branch updates
+     * @param matrixRates the relative rates for each rate category
+     */
+    void updateMatrices(int[] branchUpdateIndices, double[] branchLengths, int branchUpdateCount,
+                        double[] matrixRates);
+
+    /**
+     * Specify the updates to be made. Firstly the branches specified in the
+     * branchIndices array are updated using the branchLengths - these are used
+     * to update the appropriate probability transition matrices.
+     * @param operations an array of partial likelihood calculations. The indices
+     * are [ancestoral dependency, isDependent, source node 1, source node 2, destination node].
+     * @param operationCount the number of operators
+     */
+    void updatePartials(int[] operations, int operationCount);
+
+    /**
+     * Calculates pattern log likelihoods at a node.
+     *
+     * @param frequencies an array of state frequencies
+     * @param proportions the proportion of patterns in each rate category
+     * @param outLogLikelihoods an array into which the log likelihoods will go
+     */
+    void calculateLogLikelihoods(double[] frequencies, double[] proportions, double[] outLogLikelihoods);
+
+    /**
+     * Store current state
+     */
+    void storeState();
+
+    /**
+     * Restore the stored state
+     */
+    void restoreState();
+}

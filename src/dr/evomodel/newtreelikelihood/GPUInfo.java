943476507c949fdbd52e14e5b64719c9669d9ef2
Marc Suchard <msuchard@ucla.edu>
2008-10-29 21:22:47 +0000

    Reads GPU info correctly

--- src/dr/evomodel/newtreelikelihood/GPUInfo.java
+++ src/dr/evomodel/newtreelikelihood/GPUInfo.java
@@ -1,16 +1,55 @@
 package dr.evomodel.newtreelikelihood;
 
+import java.util.ArrayList;
+
 /**
  * @author Marc A. Suchard
  */
 public class GPUInfo {
-
-	GPUInfo(String name, long memorySize) {
-		this.name = name;
-		this.memorySize = memorySize;
+	
+//	GPUInfo() {
+//		
+//	}
+	
+//	GPUInfo(String name, long memorySize) {
+//		numberDevices = 1;
+//		this.name = new String[] {name};
+//		this.memorySize = new long[] {memorySize};
+//	}
+//	
+	GPUInfo(int numberDevices) {
+		this.numberDevices = numberDevices;
+		nameList = new String[numberDevices];
+		memorySizeList = new int[numberDevices];
+		clockSpeedList = new int[numberDevices];
+	}
+	
+	public void addInfo(int deviceNumber, String name, int memorySize, int clockSpeed) {
+		nameList[deviceNumber] = name;
+		memorySizeList[deviceNumber] = memorySize;
+		clockSpeedList[deviceNumber] = clockSpeed;
 	}
 
-	String name;
-	long memorySize;
+	int numberDevices;
+	String[] nameList;
+	int[]	 memorySizeList;
+	int[]	 clockSpeedList;
+
+	
+	public String getName(int deviceNumber) { return nameList[deviceNumber]; }
+	public int getMemorySize(int deviceNumber) { return memorySizeList[deviceNumber]; }
+	public int getClockSpeed(int deviceNumber) { return clockSpeedList[deviceNumber]; }
+	
+	public String toString() {
+		
+		StringBuffer bf = new StringBuffer();
+		bf.append("Total number of GPU devices: "+numberDevices+"\n");
+		for(int i=0; i<numberDevices; i++) {
+			bf.append("\t#"+i+":\t"+getName(i)+"\n");
+			bf.append("\t\tGlobal memory (MB) "+getMemorySize(i)+"\n");
+			bf.append("\t\tClock speed (GHz) "+getClockSpeed(i)+"\n");
+		}						
+		return bf.toString();				
+	}
 	
 }

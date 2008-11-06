60d9901a17f5f650e5acf4715eafefd364c25981
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2008-11-06 02:28:29 +0000

    Started preparing for multiple partitions on a GPU and multiple GPUs driven in different CPU threads

--- trunk/src/dr/evomodel/newtreelikelihood/LikelihoodCoreFactory.java
+++ trunk/src/dr/evomodel/newtreelikelihood/LikelihoodCoreFactory.java
@@ -18,7 +18,8 @@ public class LikelihoodCoreFactory {
 
 		if (coreRegistry == null) {  // Lazy loading
 			coreRegistry = new ArrayList<LikelihoodCoreLoader>();  // List libraries in order of load-priority
-			coreRegistry.add(new GPULikelihoodCore.LikelihoodCoreLoader());
+            coreRegistry.add(new ThreadedGPULikelihoodCore.LikelihoodCoreLoader());
+            coreRegistry.add(new GPULikelihoodCore.LikelihoodCoreLoader());
             coreRegistry.add(new NativeLikelihoodCore.LikelihoodCoreLoader());
 		}
 

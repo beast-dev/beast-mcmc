d59d3a27ffb6226f2fcb8df654502f94c0d03a90
Sebastian.Hoehna <Sebastian.Hoehna@ca793f91-a31e-0410-b540-2769d408b6a1>
2009-03-03 21:14:08 +0000

    Work on the new tree proposal operators. The two Metropolized Gibbs Samplers and the CCP guided samplers.

--- src/dr/evomodel/operators/GibbsPruneAndRegraft.java
+++ src/dr/evomodel/operators/GibbsPruneAndRegraft.java
@@ -28,212 +28,392 @@ import dr.xml.XMLSyntaxRule;
  */
 public class GibbsPruneAndRegraft extends SimpleGibbsOperator {
 
-	public static final String GIBBS_PRUNE_AND_REGRAFT = "GibbsPruneAndRegraft";
+    public static final String GIBBS_PRUNE_AND_REGRAFT = "GibbsPruneAndRegraft";
 
-	private TreeModel tree;
+    private int MAX_DISTANCE = 10;
 
-	/**
+    private TreeModel tree;
+
+    private int[] distances;
+
+    private double[] scores;
+
+    private boolean pruned = true;
+
+    /**
 	 * 
 	 */
-	public GibbsPruneAndRegraft(TreeModel tree, double weight) {
-		this.tree = tree;
-		setWeight(weight);
+    public GibbsPruneAndRegraft(TreeModel tree, boolean pruned, double weight) {
+	this.tree = tree;
+	this.pruned = pruned;
+	setWeight(weight);
+	scores = new double[tree.getNodeCount()];
+	MAX_DISTANCE = tree.getNodeCount() / 10;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * dr.evomodel.operators.SimpleGibbsOperator#doOperation(dr.inference.prior
+     * .Prior, dr.inference.model.Likelihood)
+     */
+    @Override
+    public double doOperation(Prior prior, Likelihood likelihood)
+	    throws OperatorFailedException {
+	if (pruned) {
+	    return prunedGibbsProposal(prior, likelihood);
+	} else {
+	    return GibbsProposal(prior, likelihood);
 	}
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see dr.evomodel.operators.SimpleGibbsOperator#doOperation(dr.inference.prior.Prior,
-	 *      dr.inference.model.Likelihood)
-	 */
-	@Override
-	public double doOperation(Prior prior, Likelihood likelihood)
-			throws OperatorFailedException {
-		return GibbsProposal(prior, likelihood);
-	}
+    private double GibbsProposal(Prior prior, Likelihood likelihood)
+	    throws OperatorFailedException {
+	final int nodeCount = tree.getNodeCount();
+	final NodeRef root = tree.getRoot();
 
-	private double GibbsProposal(Prior prior, Likelihood likelihood)
-			throws OperatorFailedException {
-		final int nodeCount = tree.getNodeCount();
-		final NodeRef root = tree.getRoot();
+	NodeRef i;
 
-		NodeRef i;
+	do {
+	    int indexI = MathUtils.nextInt(nodeCount);
+	    i = tree.getNode(indexI);
+	} while (root == i || tree.getParent(i) == root);
 
-		do {
-			int indexI = MathUtils.nextInt(nodeCount);
-			i = tree.getNode(indexI);
-		} while (root == i || tree.getParent(i) == root);
-
-		List<Integer> secondNodeIndices = new ArrayList<Integer>();
-		List<Double> probabilities = new ArrayList<Double>();
-		NodeRef j, iP, jP;
-		iP = tree.getParent(i);
-		double sum = 0.0;
-		double backwardLikelihood = calculateTreeLikelihood(prior, likelihood,
-				tree);
-		int offset = (int) -backwardLikelihood;
-		double backward = Math.exp(backwardLikelihood + offset);
-		final NodeRef oldBrother = getOtherChild(tree, iP, i);
-		final NodeRef oldGrandfather = tree.getParent(iP);
-		for (int n = 0; n < nodeCount; n++) {
-			j = tree.getNode(n);
-			if (j != root) {
-				jP = tree.getParent(j);
-
-				if (j == oldBrother) {
-//					secondNodeIndices.add(n);
-//					probabilities.add(backward);
-//					sum += backward;
-				} else if ((i != j)
-						&& (tree.getNodeHeight(j) < tree.getNodeHeight(iP))
-						&& (tree.getNodeHeight(iP) < tree.getNodeHeight(jP))) {
-					secondNodeIndices.add(n);
-
-					pruneAndRegraft(tree, i, iP, j, jP);
-					double prob = Math.exp(calculateTreeLikelihood(prior,
-							likelihood, tree)
-							+ offset);
-					probabilities.add(prob);
-					sum += prob;
-
-					pruneAndRegraft(tree, i, iP, oldBrother, oldGrandfather);
-				}
-			}
-		}
-
-		double ran = Math.random() * sum;
-		int index = 0;
-		while (ran > 0.0) {
-			ran -= probabilities.get(index);
-			index++;
-		}
-		index--;
-
-		j = tree.getNode(secondNodeIndices.get(index));
+	List<Integer> secondNodeIndices = new ArrayList<Integer>();
+	List<Double> probabilities = new ArrayList<Double>();
+	NodeRef j, jP;
+	final NodeRef iP = tree.getParent(i);
+	final double heightIP = tree.getNodeHeight(iP);
+	double sum = 0.0;
+	double backwardLikelihood = calculateTreeLikelihood(prior, likelihood,
+		tree);
+	int offset = (int) -backwardLikelihood;
+	double backward = Math.exp(backwardLikelihood + offset);
+	final NodeRef oldBrother = getOtherChild(tree, iP, i);
+	final NodeRef oldGrandfather = tree.getParent(iP);
+	for (int n = 0; n < nodeCount; n++) {
+	    j = tree.getNode(n);
+	    if (j != root) {
 		jP = tree.getParent(j);
 
-		if (iP != jP) {
+		if (j == oldBrother) {
+		    // secondNodeIndices.add(n);
+		    // probabilities.add(backward);
+		    // sum += backward;
+		} else if ((i != j) && (tree.getNodeHeight(j) < heightIP)
+			&& (heightIP < tree.getNodeHeight(jP))) {
+		    secondNodeIndices.add(n);
+
+		    pruneAndRegraft(tree, i, iP, j, jP);
+		    double prob = Math.exp(calculateTreeLikelihood(prior,
+			    likelihood, tree)
+			    + offset);
+		    probabilities.add(prob);
+		    sum += prob;
+
+		    pruneAndRegraft(tree, i, iP, oldBrother, oldGrandfather);
+		}
+	    }
+	}
+
+	if (sum <= 1E-100) {
+	    // hack
+	    // the proposals have such a small likelihood that they can be
+	    // neglected
+	    throw new OperatorFailedException(
+		    "Couldn't find another proposal with a decent likelihood.");
+	}
+
+	double ran = Math.random() * sum;
+	int index = 0;
+	while (ran > 0.0) {
+	    ran -= probabilities.get(index);
+	    index++;
+	}
+	index--;
+
+	j = tree.getNode(secondNodeIndices.get(index));
+	jP = tree.getParent(j);
+
+	// int distance = getNodeDistance(iP, jP);
+	// distances[distance]++;
+
+	pruneAndRegraft(tree, i, iP, j, jP);
+
+	double forward = probabilities.get(index);
+
+	// tree.pushTreeChangedEvent(jP);
+	// tree.pushTreeChangedEvent(oldGrandfather);
+	tree.pushTreeChangedEvent(i);
+
+	double forwardProb = (forward / sum);
+	double backwardProb = (backward / (sum - forward + backward));
+	double hastingsRatio = Math.log(backwardProb / forwardProb);
+
+	return hastingsRatio;
+    }
+
+    private double prunedGibbsProposal(Prior prior, Likelihood likelihood)
+	    throws OperatorFailedException {
+	final int nodeCount = tree.getNodeCount();
+	final NodeRef root = tree.getRoot();
+
+	for (int i = 0; i < nodeCount; i++) {
+	    scores[i] = Double.NEGATIVE_INFINITY;
+	}
+
+	NodeRef i;
+
+	do {
+	    int indexI = MathUtils.nextInt(nodeCount);
+	    i = tree.getNode(indexI);
+	} while (root == i || tree.getParent(i) == root);
+
+	List<Integer> secondNodeIndices = new ArrayList<Integer>();
+	List<Double> probabilities = new ArrayList<Double>();
+	NodeRef j, jP;
+	final NodeRef iP = tree.getParent(i);
+	final double heightIP = tree.getNodeHeight(iP);
+	double sum = 0.0;
+	double backwardLikelihood = calculateTreeLikelihood(prior, likelihood,
+		tree);
+	int offset = (int) -backwardLikelihood;
+	double backward = Math.exp(backwardLikelihood + offset);
+	final NodeRef oldBrother = getOtherChild(tree, iP, i);
+	final NodeRef oldGrandfather = tree.getParent(iP);
+	for (int n = 0; n < nodeCount; n++) {
+	    j = tree.getNode(n);
+	    if (j != root) {
+		jP = tree.getParent(j);
+
+		if (j == oldBrother) {
+		    // secondNodeIndices.add(n);
+		    // probabilities.add(backward);
+		    // sum += backward;
+		} else if ((i != j) && (tree.getNodeHeight(j) < heightIP)
+			&& (heightIP < tree.getNodeHeight(jP))
+			&& getNodeDistance(iP, jP) <= MAX_DISTANCE) {
+		    secondNodeIndices.add(n);
+
+		    pruneAndRegraft(tree, i, iP, j, jP);
+		    double prob = Math.exp(calculateTreeLikelihood(prior,
+			    likelihood, tree)
+			    + offset);
+		    probabilities.add(prob);
+		    scores[n] = prob;
+		    sum += prob;
+
+		    pruneAndRegraft(tree, i, iP, oldBrother, oldGrandfather);
+		}
+	    }
+	}
+
+	if (sum <= 1E-100) {
+	    // hack
+	    // the proposals have such a small likelihood that they can be
+	    // neglected
+	    throw new OperatorFailedException(
+		    "Couldn't find another proposal with a decent likelihood.");
+	}
+
+	double ran = Math.random() * sum;
+	int index = 0;
+	while (ran > 0.0) {
+	    ran -= probabilities.get(index);
+	    index++;
+	}
+	index--;
+
+	j = tree.getNode(secondNodeIndices.get(index));
+	jP = tree.getParent(j);
+
+	// int distance = getNodeDistance(iP, jP);
+	// distances[distance]++;
+
+	pruneAndRegraft(tree, i, iP, j, jP);
+
+	// now simulate the backward move
+	double sumBackward = 0.0;
+	final NodeRef newBrother = j;
+	final NodeRef newGrandfather = jP;
+	for (int n = 0; n < nodeCount; n++) {
+	    j = tree.getNode(n);
+	    if (j != root) {
+		jP = tree.getParent(j);
+
+		if (j == newBrother) {
+		    // secondNodeIndices.add(n);
+		    // probabilities.add(backward);
+		    // sum += backward;
+		} else if ((i != j) && (tree.getNodeHeight(j) < heightIP)
+			&& (heightIP < tree.getNodeHeight(jP))
+			&& getNodeDistance(iP, jP) <= MAX_DISTANCE) {
+
+		    if (scores[n] != Double.NEGATIVE_INFINITY) {
+			sumBackward += scores[n];
+		    } else {
 			pruneAndRegraft(tree, i, iP, j, jP);
+			double prob = Math.exp(calculateTreeLikelihood(prior,
+				likelihood, tree)
+				+ offset);
+			sumBackward += prob;
+
+			pruneAndRegraft(tree, i, iP, newBrother, newGrandfather);
+		    }
 		}
-		double forward = probabilities.get(index);
-
-//		tree.pushTreeChangedEvent(jP);
-//		tree.pushTreeChangedEvent(oldGrandfather);
-		tree.pushTreeChangedEvent(i);
-
-		double forwardProb = (forward / sum);
-		double backwardProb = (backward / (sum - forward + backward));
-		double hastingsRatio = Math.log(backwardProb / forwardProb);
-
-		return hastingsRatio;
+	    }
 	}
 
-	private double calculateTreeLikelihood(Prior prior, Likelihood likelihood,
-			TreeModel tree) {
-		return evaluate(likelihood, prior);
+	double forward = probabilities.get(index);
+
+	// tree.pushTreeChangedEvent(jP);
+	// tree.pushTreeChangedEvent(oldGrandfather);
+	tree.pushTreeChangedEvent(i);
+
+	double forwardProb = (forward / sum);
+	double backwardProb = (backward / (sumBackward));
+	double hastingsRatio = Math.log(backwardProb / forwardProb);
+
+	return hastingsRatio;
+    }
+
+    private int getNodeDistance(NodeRef i, NodeRef j) {
+	int count = 0;
+	double heightI = tree.getNodeHeight(i);
+	double heightJ = tree.getNodeHeight(j);
+
+	while (i != j) {
+	    count++;
+	    if (heightI < heightJ) {
+		i = tree.getParent(i);
+		heightI = tree.getNodeHeight(i);
+	    } else {
+		j = tree.getParent(j);
+		heightJ = tree.getNodeHeight(j);
+	    }
+	}
+	return count;
+    }
+
+    public void printDistances() {
+	System.out.println("Number of proposed trees in distances:");
+	for (int i = 0; i < distances.length; i++) {
+	    System.out.println(i + ")\t\t" + distances[i]);
+	}
+    }
+
+    private double calculateTreeLikelihood(Prior prior, Likelihood likelihood,
+	    TreeModel tree) {
+	return evaluate(likelihood, prior);
+    }
+
+    private void pruneAndRegraft(TreeModel tree, NodeRef i, NodeRef iP,
+	    NodeRef j, NodeRef jP) throws OperatorFailedException {
+	tree.beginTreeEdit();
+
+	// the grandfather
+	NodeRef iG = tree.getParent(iP);
+	// the brother
+	NodeRef iB = getOtherChild(tree, iP, i);
+	// prune
+	tree.removeChild(iP, iB);
+	tree.removeChild(iG, iP);
+	tree.addChild(iG, iB);
+
+	// reattach
+	tree.removeChild(jP, j);
+	tree.addChild(iP, j);
+	tree.addChild(jP, iP);
+
+	// ****************************************************
+
+	try {
+	    tree.endTreeEditUnsafe();
+	} catch (MutableTree.InvalidTreeException ite) {
+	    throw new OperatorFailedException(ite.toString());
 	}
 
-	private void pruneAndRegraft(TreeModel tree, NodeRef i, NodeRef iP,
-			NodeRef j, NodeRef jP) throws OperatorFailedException {
-		tree.beginTreeEdit();
+	tree.pushTreeChangedEvent(i);
+    }
 
-		// the grandfather
-		NodeRef iG = tree.getParent(iP);
-		// the brother
-		NodeRef iB = getOtherChild(tree, iP, i);
-		// prune
-		tree.removeChild(iP, iB);
-		tree.removeChild(iG, iP);
-		tree.addChild(iG, iB);
+    /**
+     * @param tree
+     *            the tree
+     * @param parent
+     *            the parent
+     * @param child
+     *            the child that you want the sister of
+     * @return the other child of the given parent.
+     */
+    protected NodeRef getOtherChild(Tree tree, NodeRef parent, NodeRef child) {
 
-		// reattach
-		tree.removeChild(jP, j);
-		tree.addChild(iP, j);
-		tree.addChild(jP, iP);
+	if (tree.getChild(parent, 0) == child) {
+	    return tree.getChild(parent, 1);
+	} else {
+	    return tree.getChild(parent, 0);
+	}
+    }
 
-		// ****************************************************
+    /*
+     * (non-Javadoc)
+     * 
+     * @see dr.evomodel.operators.SimpleGibbsOperator#getOperatorName()
+     */
+    @Override
+    public String getOperatorName() {
+	return GIBBS_PRUNE_AND_REGRAFT;
+    }
 
-		try {
-			tree.endTreeEdit();
-		} catch (MutableTree.InvalidTreeException ite) {
-			throw new OperatorFailedException(ite.toString());
-		}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see dr.evomodel.operators.SimpleGibbsOperator#getStepCount()
+     */
+    @Override
+    public int getStepCount() {
+	return 0;
+    }
 
-		tree.pushTreeChangedEvent(i);
+    public static XMLObjectParser GIBBS_PRUNE_AND_REGRAFT_PARSER = new AbstractXMLObjectParser() {
+
+	public String getParserName() {
+	    return GIBBS_PRUNE_AND_REGRAFT;
 	}
 
-	/**
-	 * @param tree
-	 *            the tree
-	 * @param parent
-	 *            the parent
-	 * @param child
-	 *            the child that you want the sister of
-	 * @return the other child of the given parent.
-	 */
-	protected NodeRef getOtherChild(Tree tree, NodeRef parent, NodeRef child) {
+	public Object parseXMLObject(XMLObject xo) throws XMLParseException {
 
-		if (tree.getChild(parent, 0) == child) {
-			return tree.getChild(parent, 1);
-		} else {
-			return tree.getChild(parent, 0);
-		}
+	    TreeModel treeModel = (TreeModel) xo.getChild(TreeModel.class);
+	    double weight = xo.getDoubleAttribute("weight");
+	    boolean pruned = true;
+	    if (xo.hasAttribute("pruned")) {
+		pruned = xo.getBooleanAttribute("pruned");
+	    }
+
+	    return new GibbsPruneAndRegraft(treeModel, pruned, weight);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see dr.evomodel.operators.SimpleGibbsOperator#getOperatorName()
-	 */
-	@Override
-	public String getOperatorName() {
-		return GIBBS_PRUNE_AND_REGRAFT;
+	// ************************************************************************
+	// AbstractXMLObjectParser implementation
+	// ************************************************************************
+
+	public String getParserDescription() {
+	    return "This element represents a Gibbs sampler implemented through a prune and regraft operator. "
+		    + "This operator prunes a random subtree and regrafts it below a node chosen by an importance distribution which is the proportion of the likelihoods of the proposals.";
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see dr.evomodel.operators.SimpleGibbsOperator#getStepCount()
-	 */
-	@Override
-	public int getStepCount() {
-		return 0;
+	public Class getReturnType() {
+	    return GibbsPruneAndRegraft.class;
 	}
 
-	public static XMLObjectParser GIBBS_PRUNE_AND_REGRAFT_PARSER = new AbstractXMLObjectParser() {
+	public XMLSyntaxRule[] getSyntaxRules() {
+	    return rules;
+	}
 
-		public String getParserName() {
-			return GIBBS_PRUNE_AND_REGRAFT;
-		}
+	private XMLSyntaxRule[] rules = new XMLSyntaxRule[] {
+		AttributeRule.newDoubleRule("weight"),
+		AttributeRule.newBooleanRule("pruned"),
+		new ElementRule(TreeModel.class) };
 
-		public Object parseXMLObject(XMLObject xo) throws XMLParseException {
-
-			TreeModel treeModel = (TreeModel) xo.getChild(TreeModel.class);
-			double weight = xo.getDoubleAttribute("weight");
-
-			return new GibbsPruneAndRegraft(treeModel, weight);
-		}
-
-		// ************************************************************************
-		// AbstractXMLObjectParser implementation
-		// ************************************************************************
-
-		public String getParserDescription() {
-			return "This element represents a Gibbs sampler implemented through a prune and regraft operator. "
-					+ "This operator prunes a random subtree and regrafts it below a node chosen by an importance distribution which is the proportion of the likelihoods of the proposals.";
-		}
-
-		public Class getReturnType() {
-			return GibbsPruneAndRegraft.class;
-		}
-
-		public XMLSyntaxRule[] getSyntaxRules() {
-			return rules;
-		}
-
-		private XMLSyntaxRule[] rules = new XMLSyntaxRule[] {
-				AttributeRule.newDoubleRule("weight"),
-				new ElementRule(TreeModel.class) };
-
-	};
+    };
 }

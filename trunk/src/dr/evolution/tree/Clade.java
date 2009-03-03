675c4ebedc9b49a75f8ed0c910e3875f7d5d7e42
Sebastian.Hoehna <Sebastian.Hoehna@ca793f91-a31e-0410-b540-2769d408b6a1>
2009-03-03 20:58:45 +0000

    Some work on the conditional clade frequencies.

--- trunk/src/dr/evolution/tree/Clade.java
+++ trunk/src/dr/evolution/tree/Clade.java
@@ -3,9 +3,17 @@
  */
 package dr.evolution.tree;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.List;
+
+import dr.math.MathUtils;
+import dr.math.distributions.Distribution;
+import dr.math.distributions.NormalDistribution;
 
 /**
  * @author Sebastian Hoehna
@@ -14,15 +22,9 @@ import java.util.List;
 public class Clade implements Comparable<Clade> {
 
     private final BitSet bits;
-    private final double height;
+    private double height;
     private List<Double> heights;
     private final int size;
-    private double lognormalMean;
-    private double lognormalStDev;
-    private double lognormalVariance;
-    private boolean dirty = true;
-    
-    private final int PRECISION = 100;
     
     public Clade(final BitSet bits, final double height) {
         this.bits = bits;
@@ -52,11 +54,12 @@ public class Clade implements Comparable<Clade> {
     }
 
     public int compareTo(Clade clade) {
-		int setBitIndexI = 0;
-		int setBitIndexJ = 0;
+		int setBitIndexI = -1;
+		int setBitIndexJ = -1;
+		BitSet otherBits = clade.getBits();
 		do {
 			setBitIndexI = bits.nextSetBit(setBitIndexI + 1);
-			setBitIndexJ = clade.getBits().nextSetBit(setBitIndexJ + 1);
+			setBitIndexJ = otherBits.nextSetBit(setBitIndexJ + 1);
 		} while (setBitIndexI == setBitIndexJ && setBitIndexI != -1);
 
 		return (setBitIndexI < setBitIndexJ ? -1
@@ -71,6 +74,9 @@ public class Clade implements Comparable<Clade> {
     }
     
     public boolean equals(Clade c){
+    	if (size != c.getSize()){
+    		return false;
+    	}
     	return compareTo(c) == 0;
     }
     
@@ -83,178 +89,12 @@ public class Clade implements Comparable<Clade> {
     }
     
     public void addHeight(double height){
-    	dirty = true;
-    	double logHeight = Math.log(height);
-    	heights.add(logHeight);
+    	heights.add(height);
+    	setHeight(height);
     }
     
-    public double getLognormalMean(){
-    	if (dirty){
-    		lognormalMean = estimateLognormalMean();
-    		lognormalStDev = Math.sqrt(getLognormalVariance());
-    		
-    		dirty = false;
-    	}
-    	
-    	return lognormalMean;
+    public void setHeight(double height){
+    	this.height = height;
     }
     
-    private double estimateLognormalMean(){
-    	double max = getLogMax();
-    	double min = getLogMin();
-    	
-    	double slotSize = (max - min) / PRECISION;
-    	int [] frequencies = new int[PRECISION];
-    	
-    	for (double height : heights){
-    		double tmp = (height - min) / slotSize;
-    		int index = (int) tmp;
-    		index = (index == PRECISION ? PRECISION-1 : index);
-    		
-    		if (index - 2 >= 0){
-        		frequencies[index-2]++; 
-        		frequencies[index-1]++;    			
-    		}
-    		else if (index - 1 >= 0){
-        		frequencies[index-1]++;    			
-    		}
-    		
-    		if (index + 2 < PRECISION){
-        		frequencies[index+2]++; 
-        		frequencies[index+1]++;  
-        		frequencies[index]++;   			
-    		}
-    		else if (index + 1 < PRECISION){ 
-        		frequencies[index+1]++;  
-        		frequencies[index]++; 		
-    		}
-    		else if (index < PRECISION){ 
-        		frequencies[index]++;  			
-    		}
-    	}
-    	
-    	double [] smoothedFrequencies = new double[PRECISION];
-    	smoothedFrequencies[0] = frequencies[0]/3.0;
-    	smoothedFrequencies[1] = frequencies[1]/4.0;
-    	for (int i=2; i<PRECISION-2; i++){
-    		smoothedFrequencies[i] = frequencies[i]/5.0;
-    	}
-    	smoothedFrequencies[PRECISION-2] = frequencies[PRECISION-2]/4.0;
-    	smoothedFrequencies[PRECISION-1] = frequencies[PRECISION-1]/3.0;
-    	
-    	// find slot with the max entries
-    	int maxIndex = 0;
-    	for (int i=0; i<PRECISION; i++){
-    		if (smoothedFrequencies[i] > smoothedFrequencies[maxIndex]){
-    			maxIndex = i;
-    		}
-    	}
-    	
-    	double mean = min + (slotSize * (maxIndex + 0.5));
-    	
-    	return mean;
-    }
-    
-    public double getLognormalStDev(){
-    	if (dirty){
-    		lognormalStDev = Math.sqrt(getLognormalVariance());
-    	}
-    	
-    	return lognormalStDev;
-    }
-    
-    private double getLognormalVariance(){
-    	if (dirty){
-    		double sum = 0.0;
-    		double count = 0.0;
-        	for (double height : heights){
-        		if (height <= lognormalMean){
-        			sum += (lognormalMean - height) * (lognormalMean - height);
-        			count++;
-        		}
-        	}    		
-        	lognormalVariance = sum / count;
-        	lognormalStDev = Math.sqrt(lognormalVariance);
-        	
-        	dirty = false;
-    	}
-    	
-    	return lognormalVariance;
-    }
-    
-    public void printLogHeights(){
-    	for (double height : heights){
-    		System.out.println(height);
-    	}
-    }
-    
-    public void printHeights(){
-    	for (double height : heights){
-    		System.out.println(Math.exp(height));
-    	}
-    }
-    
-    public void printLogHistogram(){
-    	double max = getLogMax();
-    	double min = getLogMin();
-    	
-    	double slotSize = (max - min) / 100.0;
-    	int [] frequencies = new int[100];
-    	
-    	for (double height : heights){
-    		double tmp = (height - min) / slotSize;
-    		int index = (int) tmp;
-    		index = (index == 100 ? 99 : index);
-    		frequencies[index]++;
-    	}
-    	
-    	for (int i=0; i<100; i++){
-    		int freq = frequencies[i];
-    		System.out.println(slotSize * i + "\t" + freq);
-    	}
-    }
-    
-    public void printHistogram(){
-    	double max = Math.exp(getLogMax());
-    	double min = Math.exp(getLogMin());
-    	
-    	double slotSize = (max - min) / 100.0;
-    	int [] frequencies = new int[100];
-    	
-    	for (double height : heights){
-    		double tmp = (Math.exp(height) - min) / slotSize;
-    		int index = (int) tmp;
-    		index = (index == 100 ? 99 : index);
-    		frequencies[index]++;
-    	}
-    	
-    	for (int i=0; i<100; i++){
-    		int freq = frequencies[i];
-    		System.out.println(slotSize * i + "\t" + freq);
-    	}
-    }
-    
-    public double getLogMax(){
-    	double max = Double.NEGATIVE_INFINITY;
-    	
-    	for (double height : heights){
-    		if (max < height){
-    			max = height;
-    		}
-    	}
-    	
-    	return max;
-    }
-    
-    public double getLogMin(){
-    	double min = Double.MIN_VALUE;
-    	
-    	for (double height : heights){
-    		if (min > height){
-    			min = height;
-    		}
-    	}
-    	
-    	return min;
-    }
 }

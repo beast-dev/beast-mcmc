1512325ffb813d22eb15f82fe748032d430f466a
alexander.alekseyenko <alexander.alekseyenko@ca793f91-a31e-0410-b540-2769d408b6a1>
2009-04-07 21:36:18 +0000

    minor adjustments to path sampling code 1) user can specify the amount of burn-in through a corresponding attribute 2) the pathParameter column is now labeled as such; this was done by fixing how properties get logged

--- trunk/src/dr/xml/PropertyParser.java
+++ trunk/src/dr/xml/PropertyParser.java
@@ -29,46 +29,52 @@ import dr.util.Property;
 
 public class PropertyParser extends AbstractXMLObjectParser {
 
-	public String getParserName() { return "property"; }
-		
-	public Object parseXMLObject(XMLObject xo) throws XMLParseException {
-		
-		Object object = xo.getChild(0);			
-		String name = xo.getStringAttribute("name");
-				
-		Property property;
-				
-		if (xo.hasAttribute("index")) {
-			int index = xo.getIntegerAttribute("index");					
-			property = new Property(object, name, index);
-		} else if (xo.hasAttribute("label")) {
-			String label = xo.getStringAttribute("label");					
-			property = new Property(object, name, label);
-		} else {
-			property = new Property(object, name);
-		}
-				
-		if (property.getGetter() == null)
-			throw new XMLParseException("unknown property, " + name + ", for object, " + object + ", in property element");
+    public String getParserName() {
+        return "property";
+    }
 
-		return property;
-	}
-	
-	//************************************************************************
-	// AbstractXMLObjectParser implementation
-	//************************************************************************
-	
-	public String getParserDescription() {
-		return "This element returns an object representing the named property of the given child object.";
-	}
-	
-	public Class getReturnType() { return Object.class; }
-	
-	public XMLSyntaxRule[] getSyntaxRules() { return rules; }
-	
-	private XMLSyntaxRule[] rules = new XMLSyntaxRule[] {
-		new StringAttributeRule("name", "name of the property", "length"),
-		new ElementRule(Object.class)
-	};
-	
+    public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+
+        Object object = xo.getChild(0);
+        String name = xo.getStringAttribute("name");
+
+        Property property;
+
+        if (xo.hasAttribute("index")) {
+            int index = xo.getIntegerAttribute("index");
+            property = new Property(object, name, index);
+        } else if (xo.hasAttribute("label")) {
+            String label = xo.getStringAttribute("label");
+            property = new Property(object, name, label);
+        } else {
+            property = new Property(object, name);
+        }
+
+        if (property.getGetter() == null)
+            throw new XMLParseException("unknown property, " + name + ", for object, " + object + ", in property element");
+
+        return property;
+    }
+
+    //************************************************************************
+    // AbstractXMLObjectParser implementation
+    //************************************************************************
+
+    public String getParserDescription() {
+        return "This element returns an object representing the named property of the given child object.";
+    }
+
+    public Class getReturnType() {
+        return Object.class;
+    }
+
+    public XMLSyntaxRule[] getSyntaxRules() {
+        return rules;
+    }
+
+    private XMLSyntaxRule[] rules = new XMLSyntaxRule[]{
+            new StringAttributeRule("name", "name of the property", "length"),
+            new ElementRule(Object.class)
+    };
+
 }

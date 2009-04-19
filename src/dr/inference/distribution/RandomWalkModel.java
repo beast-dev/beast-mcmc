3513dfb0959a6e0462afdbdff358554445fe6509
Marc Suchard <msuchard@ucla.edu>
2009-04-19 01:23:31 +0000

    MultivariateTraitLikelihood now supports (not default) caching of branch-likelihoods; however, caching has a slightly slower run-time for simple 2D Brownian diffusion.  Normalizing by treeLengths means that all tree changes result in a complete recalculation anyway.  Advantages may be seen as diffusion models get more complicated or with treeHeight normalization

---
+++ src/dr/inference/distribution/RandomWalkModel.java
@@ -0,0 +1,174 @@
+package dr.inference.distribution;
+
+import dr.inference.model.AbstractModelLikelihood;
+import dr.inference.model.CompoundParameter;
+import dr.inference.model.Model;
+import dr.inference.model.Parameter;
+import dr.xml.*;
+
+import java.util.logging.Logger;
+
+/**
+ * @author Marc A. Suchard
+ */
+public class RandomWalkModel extends AbstractModelLikelihood {
+
+    public static final String RANDOM_WALK = "randomWalk";
+    public static final String LOG_SCALE = "logScale";
+
+    public RandomWalkModel(
+            ParametricDistributionModel distribution,
+            Parameter data, boolean forwardOrder, boolean logScale) {
+        super(null);
+        this.distribution = distribution;
+        this.forwardOrder = forwardOrder;
+        this.logScale = logScale;
+        this.data = data;
+        if (distribution != null) {
+            addModel(distribution);
+        }
+
+        double lower = Double.NEGATIVE_INFINITY;
+        if (logScale)
+            lower = 0.0;
+
+        addParameter(data);
+        if (data instanceof CompoundParameter) {
+            CompoundParameter cp = (CompoundParameter) data;
+            for (int i = 0; i < cp.getNumberOfParameters(); i++) {
+                Parameter p = cp.getParameter(i);
+                p.addBounds(new Parameter.DefaultBounds(Double.POSITIVE_INFINITY, lower, p.getDimension()));
+            }
+        } else
+            data.addBounds(new Parameter.DefaultBounds(Double.POSITIVE_INFINITY, lower, data.getDimension()));
+
+        Logger.getLogger("dr.inference").info("Setting up a first-order random walk:\n");
+        Logger.getLogger("dr.inference").info("\tData parameter: " + data.getId() + "\n");
+        Logger.getLogger("dr.inference").info("\tOn scale: " + (logScale ? "log" : "real") + "\n");
+        Logger.getLogger("dr.inference").info("\tDistribution: " + distribution.getId() + "\n");
+        Logger.getLogger("dr.inference").info("\tIf you publish results using this model, please cite Suchard and Lemey (in preparation)\n");
+    }
+
+    protected double calculateLogLikelihood() {
+
+        final int dim = data.getDimension();
+
+        double logLikelihood = 0;
+        double previous = data.getParameterValue(0);
+        if (logScale)
+            previous = Math.log(previous);
+        for (int i = 1; i < dim; i++) {
+            double current = data.getParameterValue(i);
+            if (logScale)
+                current = Math.log(current);
+            logLikelihood += distribution.logPdf(current - previous);
+            if (logScale)
+                logLikelihood -= current;
+            previous = current;
+        }
+
+        return logLikelihood;
+    }
+
+    protected void handleModelChangedEvent(Model model, Object object, int index) {
+        likelihoodKnown = false;
+    }
+
+    protected void handleParameterChangedEvent(Parameter parameter, int index, Parameter.ChangeType type) {
+        likelihoodKnown = false;
+    }
+
+    protected void storeState() {
+    }
+
+    protected void restoreState() {
+    }
+
+    protected void acceptState() {
+    }
+
+    private final ParametricDistributionModel distribution;
+    private final boolean logScale;
+    private Parameter data;
+    protected boolean likelihoodKnown;
+    private boolean forwardOrder;
+
+    public static XMLObjectParser PARSER = new AbstractXMLObjectParser() {
+
+        public String getParserName() {
+            return RANDOM_WALK;
+        }
+
+        public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+
+            Parameter data = (Parameter) xo.getChild(Parameter.class);
+            ParametricDistributionModel distribution = (ParametricDistributionModel) xo.getChild(ParametricDistributionModel.class);
+
+            boolean logScale = false;
+            if (xo.hasAttribute(LOG_SCALE))
+                logScale = xo.getBooleanAttribute(LOG_SCALE);
+
+            return new RandomWalkModel(distribution, data, false, logScale);
+
+        }
+
+        //************************************************************************
+        // AbstractXMLObjectParser implementation
+        //************************************************************************
+
+        public XMLSyntaxRule[] getSyntaxRules() {
+            return rules;
+        }
+
+        private XMLSyntaxRule[] rules = new XMLSyntaxRule[]{
+                AttributeRule.newBooleanRule(LOG_SCALE, true),
+                new ElementRule(Parameter.class),
+                new XORRule(
+                        new ElementRule(ParametricDistributionModel.class),
+                        new ElementRule(DistributionLikelihood.class)
+                )
+        };
+
+        public String getParserDescription() {
+            return "Describes a first-order random walk. No prior is assumed on the first data element";
+        }
+
+        public Class getReturnType() {
+            return RandomWalkModel.class;
+        }
+    };
+
+    public Model getModel() {
+        return this;
+    }
+
+    public double getLogLikelihood() {
+        return calculateLogLikelihood();
+    }
+
+    public void makeDirty() {
+    }
+
+    // **************************************************************
+    // Loggable IMPLEMENTATION
+    // **************************************************************
+
+    /**
+     * @return the log columns.
+     */
+    public dr.inference.loggers.LogColumn[] getColumns() {
+        return new dr.inference.loggers.LogColumn[]{
+                new LikelihoodColumn(getId())
+        };
+    }
+
+    private class LikelihoodColumn extends dr.inference.loggers.NumberColumn {
+        public LikelihoodColumn(String label) {
+            super(label);
+        }
+
+        public double getDoubleValue() {
+            return getLogLikelihood();
+        }
+    }
+}

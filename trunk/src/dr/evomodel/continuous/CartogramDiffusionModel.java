3e096bb2bd16890b9983b13debe692c8e85fda14
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2009-05-01 01:14:46 +0000

    Starting a mixture of Cartograms

--- trunk/src/dr/evomodel/continuous/CartogramDiffusionModel.java
+++ trunk/src/dr/evomodel/continuous/CartogramDiffusionModel.java
@@ -17,6 +17,7 @@ public class CartogramDiffusionModel extends MultivariateDiffusionModel {
 
     public static final String DIFFUSION_PROCESS = "cartogramDiffusionModel";
     public static final String FILENAME = "cartogramFileName";
+    public static final String DENSITY = "density";
 
     public static final String BOUNDING_BOX = "boundingBox";
     public static final String MIN_X = "minX";
@@ -27,25 +28,33 @@ public class CartogramDiffusionModel extends MultivariateDiffusionModel {
     public static final String XSIZE = "xGridSize";
     public static final String YSIZE = "yGridSize";
 
-    public CartogramDiffusionModel(String name, CartogramMapping mapping, Parameter precision) {
+    public CartogramDiffusionModel(String name, Parameter precision) {
         super();
-        this.mapping = mapping;
         this.precision = precision;
         addParameter(precision);
         setId(name);
-
         Logger.getLogger("dr.evomodel.continuous").info(
             "Constructing cartogram diffusion model '"+ getId() +"': \n" +
-            "\tMapping  : " + mapping.toString() + "\n" +
-            "\tPrecision: " + precision.getId() + "\n" +
-            "\tIf you use this model, please reference: Lemey, Drummond and Suchard (in preparation)\n"
+            "\tIf you use this model, please reference: Lemey, Drummond and Suchard (in preparation)\n" +
+            "\tPrecision: " + precision.getId()
         );
     }
+
+    public void addMapping(CartogramMapping mapping) {
+        this.mapping = mapping;
+        Logger.getLogger("dr.evomodel.continuous").info(
+            "\tMapping  : " + mapping.toString() + "\n"
+        );
+    }
+
+    protected CartogramMapping getMapping() { return mapping; }
 
     protected double calculateLogDensity(double[] start, double[] stop, double time) {
 
         Point2D realStart = new Point2D.Double(start[0],start[1]);
         Point2D realStop  = new Point2D.Double(stop[0], stop[1]);
+
+        final CartogramMapping mapping = getMapping();
 
         Point2D mappedStart = mapping.map(realStart);
         Point2D mappedStop  = mapping.map(realStop);
@@ -64,6 +73,55 @@ public class CartogramDiffusionModel extends MultivariateDiffusionModel {
     }
 
 
+    public static Rectangle2D parseRectangle2D(XMLObject xo) throws XMLParseException {
+
+        XMLObject cxo = (XMLObject) xo.getChild(BOUNDING_BOX);
+        double minX = cxo.getAttribute(MIN_X, 0.0);
+        double maxX = cxo.getAttribute(MAX_X, 0.0);
+        double minY = cxo.getAttribute(MIN_Y, 0.0);
+        double maxY = cxo.getAttribute(MAX_Y, 0.0);
+
+        if (maxX - minX <= 0 || maxY - minY <= 0)
+            throw new XMLParseException("Bounding box must contain volume");
+
+        return new Rectangle2D.Double(minX, minY, maxX - minX, maxY - minY);
+    }
+
+    public static CartogramMapping parseCartogramMapping(XMLObject xo, Rectangle2D boundingBox) throws XMLParseException {
+        int xGridSize = xo.getAttribute(XSIZE, 0);
+        int yGridSize = xo.getAttribute(YSIZE, 0);
+
+        if (xGridSize <= 1 || yGridSize <= 1)
+            throw new XMLParseException("Strictly positive grid sizes required");
+
+        CartogramMapping mapping = new CartogramMapping(xGridSize, yGridSize, boundingBox);
+
+        double density = xo.getAttribute(DENSITY,0.0);
+        mapping.setAverageDensity(density);
+
+        String fileName = xo.getAttribute(FILENAME, "NONE");
+
+        Logger.getLogger("dr.evomodel.continuous").info(
+                "Loading cartogram file: " + fileName + "\n");
+
+        if (xo.hasAttribute(FILENAME)) {
+
+            try {
+                mapping.readCartogramOutput(fileName);
+            } catch (IOException e) {
+                throw new XMLParseException(e.getMessage());
+            }
+        }
+        return mapping;
+    }
+
+    protected static ElementRule boundingBoxRules = new ElementRule(BOUNDING_BOX, new XMLSyntaxRule[]{
+                    AttributeRule.newDoubleRule(MIN_X),
+                    AttributeRule.newDoubleRule(MAX_X),
+                    AttributeRule.newDoubleRule(MIN_Y),
+                    AttributeRule.newDoubleRule(MAX_Y),
+                 });
+
     public static XMLObjectParser PARSER = new AbstractXMLObjectParser() {
 
          public String getParserName() {
@@ -72,43 +130,16 @@ public class CartogramDiffusionModel extends MultivariateDiffusionModel {
 
          public Object parseXMLObject(XMLObject xo) throws XMLParseException {
 
-             // Parse bounding box
-             XMLObject cxo = (XMLObject) xo.getChild(BOUNDING_BOX);
-             double minX = cxo.getAttribute(MIN_X,0.0);
-             double maxX = cxo.getAttribute(MAX_X,0.0);
-             double minY = cxo.getAttribute(MIN_Y,0.0);
-             double maxY = cxo.getAttribute(MAX_Y,0.0);
+             Rectangle2D boundingBox = parseRectangle2D(xo);
 
-             if (maxX - minX <= 0 || maxY - minY <= 0)
-                throw new XMLParseException("Bounding box must contain volume");
-
-             Rectangle2D boundingBox = new Rectangle2D.Double(minX,minY,maxX-minX,maxY-minY);
-
-             int xGridSize = xo.getAttribute(XSIZE,0);
-             int yGridSize = xo.getAttribute(YSIZE,0);
-
-             if (xGridSize <= 1 || yGridSize <= 1)
-                throw new XMLParseException("Strictly positive grid sizes required");
-
-             CartogramMapping mapping = new CartogramMapping(xGridSize, yGridSize, boundingBox);
-
-             String fileName = xo.getAttribute(FILENAME,"NONE");
-
-             Logger.getLogger("dr.evomodel.continuous").info(
-                    "Loading cartogram file: "+fileName+"\n");
-
-             if (xo.hasAttribute(FILENAME)) {
-                 
-                try {
-                    mapping.readCartogramOutput(fileName);
-                } catch (IOException e) {
-                    throw new XMLParseException(e.getMessage());
-                }
-             }
+             CartogramMapping mapping = parseCartogramMapping(xo, boundingBox);
 
              Parameter diffusionParam = (Parameter) xo.getChild(Parameter.class);
 
-             return new CartogramDiffusionModel(xo.getId(),mapping, diffusionParam);
+             CartogramDiffusionModel model = new CartogramDiffusionModel(xo.getId(), diffusionParam);
+             model.addMapping(mapping);
+
+             return model;
          }
 
          //************************************************************************
@@ -128,12 +159,7 @@ public class CartogramDiffusionModel extends MultivariateDiffusionModel {
                  AttributeRule.newStringRule(FILENAME,true),
                  AttributeRule.newIntegerRule(XSIZE),
                  AttributeRule.newIntegerRule(YSIZE),
-                 new ElementRule(BOUNDING_BOX, new XMLSyntaxRule[]{
-                    AttributeRule.newDoubleRule(MIN_X),
-                    AttributeRule.newDoubleRule(MAX_X),
-                    AttributeRule.newDoubleRule(MIN_Y),
-                    AttributeRule.newDoubleRule(MAX_Y),
-                 })
+                 boundingBoxRules,
          };
 
          public Class getReturnType() {

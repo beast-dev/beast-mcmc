f60836399d59d230768c3be03059d36a41f202a5
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2009-05-12 23:25:12 +0000

    Working on Welch's uniform prior; should now work for < 30 taxa

--- trunk/src/dr/inference/distribution/LinearRegression.java
+++ trunk/src/dr/inference/distribution/LinearRegression.java
@@ -1,7 +1,6 @@
 package dr.inference.distribution;
 
 import dr.inference.model.Parameter;
-import dr.math.matrixAlgebra.Vector;
 
 /**
  * @author Marc Suchard
@@ -25,28 +24,14 @@ public class LinearRegression extends GeneralizedLinearModel {
 		double logLikelihood = 0;
 		double[] xBeta = getXBeta();
 		double[] precision = getScale();
-
-//              System.err.println("N = "+N);
-//              System.exit(-1);
-
-//              System.err.println("XBeta = "+new Vector(xBeta));
-//              System.exit(-1);
                     double[] y = getTransformedDependentParameter();
               
 		for (int i = 0; i < N; i++) {    // assumes that all observations are independent given fixed and random effects
-
-//			double y = dependentParam.getParameterValue(i);
-                              if (logTransform) {
-//                                  y = Math.log(y);
+                              if (logTransform)
                                   logLikelihood -= y[i]; // Jacobian
-                              }
-//                        System.err.println("y = "+y);
-//                        System.err.println("p_Y = "+precision[i]);
 			logLikelihood += 0.5 * Math.log(precision[i]) - 0.5 * (y[i] - xBeta[i]) * (y[i] - xBeta[i]) * precision[i];
 
 		}
-//              System.err.println("");
-//              System.exit(-1);
 		return N * normalizingConstant + logLikelihood;
 	}
 
@@ -57,9 +42,8 @@ public class LinearRegression extends GeneralizedLinearModel {
 	}
 
 	protected double calculateLogLikelihoodAndGradient(double[] beta, double[] gradient) {
-		return 0;
+                    throw new RuntimeException("Optimization not yet implemented.");
 	}
-
 
 	protected boolean requiresScale() {
 		return true;

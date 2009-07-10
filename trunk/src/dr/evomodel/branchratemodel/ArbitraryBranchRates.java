155019aa30411d0448c8b9f20762b745b6bc2981
alexei.drummond <alexei.drummond@ca793f91-a31e-0410-b540-2769d408b6a1>
2009-07-10 23:52:45 +0000

    Added a new TreeParameterModel class, that provides a parameter value associated with each node in the tree (optionally including the root) and tracks any tree changes. Retrofitted DiscretizedBranchRates and ArbitraryBranchRates with the new TreeParameterModel. I have checked that uncorrelated relaxed clocks still work correctly, but I couldn't find a test XML from ArbitraryBranchRates, so Marc Suchard should test this... Also if somebody could check I haven't inadvertantly slowed anything down that would also be good. Once I have change the autocorrelated clocks to use this framework we can remove NodeRates and NodeTraits from TreeModel! Woohoo.

--- trunk/src/dr/evomodel/branchratemodel/ArbitraryBranchRates.java
+++ trunk/src/dr/evomodel/branchratemodel/ArbitraryBranchRates.java
@@ -1,3 +1,28 @@
+/*
+ * ArbitraryBranchRates.java
+ *
+ * Copyright (C) 2002-2009 Alexei Drummond and Andrew Rambaut
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * BEAST is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
 package dr.evomodel.branchratemodel;
 
 import dr.evolution.tree.NodeRef;
@@ -13,85 +38,73 @@ import java.util.logging.Logger;
  * This is useful for forming a scaled mixture of normals for the continuous diffusion model
  *
  * @author Marc A. Suchard
+ * @author Alexei Drummond
  */
 public class ArbitraryBranchRates extends DiscretizedBranchRates {
 
 
-	public static final String ARBITRARY_BRANCH_RATES = "arbitraryBranchRates";
-	public static final String RATES = "rates";
+    public static final String ARBITRARY_BRANCH_RATES = "arbitraryBranchRates";
+    public static final String RATES = "rates";
 
-	//	private TreeModel tree;
-	private Parameter rate;
+    public ArbitraryBranchRates(TreeModel tree, Parameter rate) {
+        super(tree, rate, null, 1);
 
-	public ArbitraryBranchRates(TreeModel tree, Parameter rate) {
-		super(tree, rate, null, 1);
+        //Force the boundaries of rate
+        Parameter.DefaultBounds bound = new Parameter.DefaultBounds(Double.MAX_VALUE, 0, rate.getDimension());
+        rate.addBounds(bound);
+    }
 
-//        this.tree = tree;
-		this.rate = rate;
+    protected void setupRates() {
+    }
 
-		//Force the boundaries of rate
-		Parameter.DefaultBounds bound = new Parameter.DefaultBounds(Double.MAX_VALUE, 0, rate.getDimension());
-		rate.addBounds(bound);
+    public double getBranchRate(Tree tree, NodeRef node) {
 
-	}
+        return rateCategoryParameter.getBranchValue(tree, node);
+    }
 
-	protected void setupRates() {
-	}
+    public static XMLObjectParser PARSER = new AbstractXMLObjectParser() {
 
-	public double getBranchRate(Tree tree, NodeRef node) {
+        public String getParserName() {
+            return ARBITRARY_BRANCH_RATES;
+        }
 
-		int nodeNumber = node.getNumber();
-		return rate.getParameterValue(getCategoryIndexFromNodeNumber(nodeNumber));
-	}
+        public Object parseXMLObject(XMLObject xo) throws XMLParseException {
 
-	public static XMLObjectParser PARSER = new AbstractXMLObjectParser() {
+            TreeModel tree = (TreeModel) xo.getChild(TreeModel.class);
 
-		public String getParserName() {
-			return ARBITRARY_BRANCH_RATES;
-		}
+            Parameter rateCategoryParameter = (Parameter) xo.getChild(RATES);
 
-		public Object parseXMLObject(XMLObject xo) throws XMLParseException {
-
-			TreeModel tree = (TreeModel) xo.getChild(TreeModel.class);
-
-			Parameter rateCategoryParameter = (Parameter) xo.getChild(RATES);
-
-			Logger.getLogger("dr.evomodel").info("Using an scaled mixture of normals model.");
-//	        Logger.getLogger("dr.evomodel").info("  over sampling = " + overSampling);
-//	        Logger.getLogger("dr.evomodel").info("  parametric model = " + distributionModel.getModelName());
-			Logger.getLogger("dr.evomodel").info("  rates = " + rateCategoryParameter.getDimension());
-			Logger.getLogger("dr.evomodel").info("  NB: Make sure you have a prior on " + rateCategoryParameter.getId() + " and not do use this model in a treeLikelihood");
+            Logger.getLogger("dr.evomodel").info("Using an scaled mixture of normals model.");
+            Logger.getLogger("dr.evomodel").info("  rates = " + rateCategoryParameter.getDimension());
+            Logger.getLogger("dr.evomodel").info("  NB: Make sure you have a prior on " + rateCategoryParameter.getId() + " and do not use this model in a treeLikelihood");
 
 
-			return new ArbitraryBranchRates(tree, rateCategoryParameter);
-		}
+            return new ArbitraryBranchRates(tree, rateCategoryParameter);
+        }
 
-		//************************************************************************
-		// AbstractXMLObjectParser implementation
-		//************************************************************************
+        //************************************************************************
+        // AbstractXMLObjectParser implementation
+        //************************************************************************
 
-		public String getParserDescription() {
-			return
-					"This element returns an arbitrary rate model." +
-							"The branch rates are drawn from an arbitrary distribution determine by the prior.";
-		}
+        public String getParserDescription() {
+            return
+                    "This element returns an arbitrary rate model." +
+                            "The branch rates are drawn from an arbitrary distribution determine by the prior.";
+        }
 
-		public Class getReturnType() {
-			return ArbitraryBranchRates.class;
-		}
+        public Class getReturnType() {
+            return ArbitraryBranchRates.class;
+        }
 
-		public XMLSyntaxRule[] getSyntaxRules() {
-			return rules;
-		}
+        public XMLSyntaxRule[] getSyntaxRules() {
+            return rules;
+        }
 
-		private final XMLSyntaxRule[] rules = {
-//	            AttributeRule.newBooleanRule(SINGLE_ROOT_RATE, true, "Whether only a single rate should be used for the two children branches of the root"),
-//	            AttributeRule.newIntegerRule(OVERSAMPLING, true, "The integer factor for oversampling the distribution model (1 means no oversampling)"),
-				new ElementRule(TreeModel.class),
-//	            new ElementRule(DISTRIBUTION, ParametricDistributionModel.class, "The distribution model for rates among branches", false),
-				new ElementRule(RATES, Parameter.class, "The rate parameter"),
-		};
-	};
+        private final XMLSyntaxRule[] rules = {
+                new ElementRule(TreeModel.class),
+                new ElementRule(RATES, Parameter.class, "The rate parameter"),
+        };
+    };
 
 
 }

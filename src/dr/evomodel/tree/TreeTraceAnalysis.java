12f74d344de419fe275eb6ee62e5434434c343fb
Alexei Drummond <alexeid@users.noreply.github.com>
2009-07-13 10:45:55 +0000

    arg.. broken build #2..

--- src/dr/evomodel/tree/TreeTraceAnalysis.java
+++ src/dr/evomodel/tree/TreeTraceAnalysis.java
@@ -1,7 +1,7 @@
 /*
  * TreeTraceAnalysis.java
  *
- * Copyright (C) 2002-2007 Alexei Drummond and Andrew Rambaut
+ * Copyright (C) 2002-2009 Alexei Drummond and Andrew Rambaut
  *
  * This file is part of BEAST.
  * See the NOTICE file distributed with this work for additional
@@ -12,10 +12,10 @@
  * published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  *
- *  BEAST is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
+ * BEAST is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with BEAST; if not, write to the
@@ -28,6 +28,7 @@ package dr.evomodel.tree;
 import dr.app.tools.NexusExporter;
 import dr.evolution.io.Importer;
 import dr.evolution.io.NewickImporter;
+import dr.evolution.io.TreeTrace;
 import dr.evolution.tree.*;
 import dr.util.FrequencySet;
 import dr.util.NumberFormatter;

f2603951e93a69422b86e178feee1ad2836cb353
Alexei Drummond <alexeid@users.noreply.github.com>
2009-07-13 11:07:02 +0000

    Refactoring tidy up. Top level packages should not be added lightly. dr.exporters package contained a single class that was an AbstractXMLObjectParser. Added to dr.evomodelxml package.

--- src/dr/app/beast/BeastParser.java
+++ src/dr/app/beast/BeastParser.java
@@ -1,7 +1,7 @@
 /*
  * BeastParser.java
  *
- * Copyright (C) 2002-2006 Alexei Drummond and Andrew Rambaut
+ * Copyright (C) 2002-2009 Alexei Drummond and Andrew Rambaut
  *
  * This file is part of BEAST.
  * See the NOTICE file distributed with this work for additional
@@ -12,10 +12,10 @@
  * published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  *
- *  BEAST is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
+ * BEAST is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with BEAST; if not, write to the
@@ -362,7 +362,7 @@ public class BeastParser extends XMLParser {
         addXMLObjectParser(new dr.evomodel.tree.TreeTraceAnalysisParser());
         addXMLObjectParser(new dr.inference.trace.TraceAnalysisParser());
         addXMLObjectParser(dr.inference.trace.LogFileTraceExporter.PARSER);
-        addXMLObjectParser(new dr.exporters.CSVExporter());
+        addXMLObjectParser(new dr.evomodelxml.CSVExporter());
 
         addXMLObjectParser(dr.inference.trace.MarginalLikelihoodAnalysis.PARSER);
 

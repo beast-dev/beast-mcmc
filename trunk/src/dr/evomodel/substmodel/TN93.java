fc399aafcc530c596d3e3abb0a9c5d1472120ad0
alexei.drummond <alexei.drummond@ca793f91-a31e-0410-b540-2769d408b6a1>
2009-07-21 05:55:13 +0000

    birth-death-serial-sampling-model

--- trunk/src/dr/evomodel/substmodel/TN93.java
+++ trunk/src/dr/evomodel/substmodel/TN93.java
@@ -1,5 +1,5 @@
 /*
- * HKY.java
+ * TN93.java
  *
  * Copyright (C) 2002-2006 Alexei Drummond and Andrew Rambaut
  *
@@ -32,7 +32,7 @@ import java.util.logging.Logger;
 
 /**
  * Tamura and Nei model of nucleotide evolution.
- *
+ * <p/>
  * Most general model which has an analytical solution.
  *
  * @author Joseph Heled
@@ -48,7 +48,9 @@ public class TN93 extends AbstractNucleotideModel {
 
     private boolean updateIntermediates = true;
 
-    /** Used for precalculations */
+    /**
+     * Used for precalculations
+     */
 
     private double p1a;
     private double p0a;
@@ -60,9 +62,9 @@ public class TN93 extends AbstractNucleotideModel {
     private double p0aa;
     private double p3bb;
     private double p2bb;
-    private double p1aIsa ;
+    private double p1aIsa;
     private double p0aIsa;
-    private double p3bIsb ;
+    private double p3bIsb;
     private double p2bIsb;
     private double k1g;
     private double k1a;
@@ -88,13 +90,16 @@ public class TN93 extends AbstractNucleotideModel {
     /**
      * @return kappa1
      */
-    public final double getKappa1() { return kappa1Parameter.getParameterValue(0); }
+    public final double getKappa1() {
+        return kappa1Parameter.getParameterValue(0);
+    }
 
     /**
-     *
      * @return kappa2
      */
-    public final double getKappa2() { return kappa2Parameter.getParameterValue(0); }
+    public final double getKappa2() {
+        return kappa2Parameter.getParameterValue(0);
+    }
 
 
     protected void frequenciesChanged() {
@@ -111,13 +116,13 @@ public class TN93 extends AbstractNucleotideModel {
 
         // A hack until I get right this boundry case. gives results accurate to 1e-8 in the P matrix
         // so should be OK even like this.
-        if( k1 == 1 ) {
+        if (k1 == 1) {
             k1 += 1E-10;
         }
-        if( k2 == 1 ) {
+        if (k2 == 1) {
             k2 += 1e-10;
         }
-        
+
         double l1 = k1 * k1 * freqR + k1 * (2 * freqY - 1) - freqY;
         double l2 = k2 * k2 * freqY + k2 * (2 * freqR - 1) - freqR;
 
@@ -144,29 +149,28 @@ public class TN93 extends AbstractNucleotideModel {
         k2t = k2 * freqT;
         k2c = k2 * freqC;
 
-        subrateScale = 2 * (k1*freqA*freqG + k2*freqC*freqT + freqR*freqY);
-       // updateMatrix = true;
+        subrateScale = 2 * (k1 * freqA * freqG + k2 * freqC * freqT + freqR * freqY);
+        // updateMatrix = true;
         updateIntermediates = false;
     }
 
     /**
      * get the complete transition probability matrix for the given distance.
-     *
+     * <p/>
      * Based on work I did in my 691 project.
      *
      * @param distance the expected number of substitutions
-     * @param matrix an array to store the matrix
+     * @param matrix   an array to store the matrix
      */
-    public void getTransitionProbabilities(double distance, double[] matrix)
-    {
-        synchronized(this) {
+    public void getTransitionProbabilities(double distance, double[] matrix) {
+        synchronized (this) {
             if (updateIntermediates) {
                 calculateIntermediates();
             }
         }
 
         distance /= subrateScale;
-        
+
         double[] q = {
                 0, k1g, freqC, freqT,
                 k1a, 0, freqC, freqT,
@@ -180,18 +184,18 @@ public class TN93 extends AbstractNucleotideModel {
         q[15] = -(q[12] + q[13] + q[14]);
 
         double[] fa0 = {
-                1 + q[0] - p1aa, q[1] + p1aa,     q[2],             q[3],
-                q[4] + p0aa,     1 + q[5] - p0aa, q[6],             q[7],
-                q[8],            q[9],            1 + q[10] - p3bb, q[11] + p3bb,
-                q[12],           q[13],           q[14] + p2bb,     1 + q[15] - p2bb
+                1 + q[0] - p1aa, q[1] + p1aa, q[2], q[3],
+                q[4] + p0aa, 1 + q[5] - p0aa, q[6], q[7],
+                q[8], q[9], 1 + q[10] - p3bb, q[11] + p3bb,
+                q[12], q[13], q[14] + p2bb, 1 + q[15] - p2bb
         };
 
 
         double[] fa1 = {
-                -q[0] + p1aIsa, -q[1] - p1aIsa, -q[2],           -q[3],
-                -q[4] - p0aIsa, -q[5] + p0aIsa, -q[6],           -q[7],
-                -q[8],          -q[9],          -q[10] + p3bIsb, -q[11] - p3bIsb,
-                -q[12],         -q[13],         -q[14] - p2bIsb, -q[15] + p2bIsb};
+                -q[0] + p1aIsa, -q[1] - p1aIsa, -q[2], -q[3],
+                -q[4] - p0aIsa, -q[5] + p0aIsa, -q[6], -q[7],
+                -q[8], -q[9], -q[10] + p3bIsb, -q[11] - p3bIsb,
+                -q[12], -q[13], -q[14] - p2bIsb, -q[15] + p2bIsb};
 
         double et = Math.exp(-distance);
 
@@ -243,10 +247,11 @@ public class TN93 extends AbstractNucleotideModel {
     /**
      * setup substitution matrix
      */
-    public void setupMatrix()
-    { }
+    public void setupMatrix() {
+    }
 
-     protected void setupRelativeRates() { }
+    protected void setupRelativeRates() {
+    }
 
     // *****************************************************************
     // Interface Model
@@ -281,13 +286,15 @@ public class TN93 extends AbstractNucleotideModel {
      */
     public static XMLObjectParser PARSER = new AbstractXMLObjectParser() {
 
-        public String getParserName() { return TN93_MODEL; }
+        public String getParserName() {
+            return TN93_MODEL;
+        }
 
         public Object parseXMLObject(XMLObject xo) throws XMLParseException {
 
-            Parameter kappa1Param = (Parameter)xo.getElementFirstChild(KAPPA1);
-            Parameter kappa2Param = (Parameter)xo.getElementFirstChild(KAPPA2);
-            FrequencyModel freqModel = (FrequencyModel)xo.getElementFirstChild(FREQUENCIES);
+            Parameter kappa1Param = (Parameter) xo.getElementFirstChild(KAPPA1);
+            Parameter kappa2Param = (Parameter) xo.getElementFirstChild(KAPPA2);
+            FrequencyModel freqModel = (FrequencyModel) xo.getElementFirstChild(FREQUENCIES);
 
             Logger.getLogger("dr.evomodel").info("Creating TN93 substitution model. Initial kappa = "
                     + kappa1Param.getParameterValue(0) + "," + kappa1Param.getParameterValue(1));
@@ -303,9 +310,13 @@ public class TN93 extends AbstractNucleotideModel {
             return "This element represents an instance of the TN93 (Tamura and Nei 1993) model of nucleotide evolution.";
         }
 
-        public Class getReturnType() { return HKY.class; }
+        public Class getReturnType() {
+            return HKY.class;
+        }
 
-        public XMLSyntaxRule[] getSyntaxRules() { return rules; }
+        public XMLSyntaxRule[] getSyntaxRules() {
+            return rules;
+        }
 
         private final XMLSyntaxRule[] rules;{
             rules = new XMLSyntaxRule[]{

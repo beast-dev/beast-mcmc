d25a72148c62c37cd4b5338998b9191c0ebcdc93
Marc Suchard <msuchard@ucla.edu>
2009-07-25 00:13:18 +0000

    Bullet-proofing (or attempting to do so) eigen-decomposition and singular value decomposition.   The Robust* classes are bug-hunting and temporary.

--- src/dr/math/MathUtils.java
+++ src/dr/math/MathUtils.java
@@ -350,4 +350,21 @@ public class MathUtils {
         return dimension * (0.5723649429247001 + Math.log(radius)) +
                 -GammaFunction.lnGamma(dimension / 2.0 + 1.0);
     }
+
+/**
+ * Returns sqrt(a^2 + b^2) without under/overflow.
+ */
+    public static double hypot(double a, double b) {
+	double r;
+	if (Math.abs(a) > Math.abs(b)) {
+		r = b/a;
+		r = Math.abs(a)*Math.sqrt(1+r*r);
+	} else if (b != 0) {
+		r = a/b;
+		r = Math.abs(b)*Math.sqrt(1+r*r);
+	} else {
+		r = 0.0;
+	}
+	return r;
+    }
 }

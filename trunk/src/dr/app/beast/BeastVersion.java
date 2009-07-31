005b5ded77a777214fef5b5588bfd5acb6f8d730
jheled <jheled@ca793f91-a31e-0410-b540-2769d408b6a1>
2009-07-31 03:59:39 +0000

    automatic intelliJ

--- trunk/src/dr/app/beast/BeastVersion.java
+++ trunk/src/dr/app/beast/BeastVersion.java
@@ -45,14 +45,14 @@ public class BeastVersion implements Version {
     /**
      * Version string: assumed to be in format x.x.x
      */
-    private static String VERSION = "1.5beta4";
-    private static String DATE_STRING = "2002-2009";
+    private static final String VERSION = "1.5beta4";
+    private static final String DATE_STRING = "2002-2009";
 
     /**
      * this used to parse the CVS ID string but there is no equivalent
      * for SVN. We must increment this manually.
      */
-    private static String BUILD_ID = "Build r1831";
+    private static final String BUILD_ID = "Build r1831";
 
     public String getVersionString() {
         return "v" + VERSION;

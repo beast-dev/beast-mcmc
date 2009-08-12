bf0404b8c22cebfd154cb6ccdb74889e11399870
dong.w.xie <dong.w.xie@ca793f91-a31e-0410-b540-2769d408b6a1>
2009-08-12 04:57:14 +0000

    Continue Issue #98, need to develop some mouse events late.

--- trunk/src/dr/app/beauti/datapanel/ViewAligmentPanel.java
+++ trunk/src/dr/app/beauti/datapanel/ViewAligmentPanel.java
@@ -1,5 +1,5 @@
 /*
- * ViewAlignmentDialog.java
+ * ViewAligmentPanel.java
  *
  * Copyright (C) 2002-2006 Alexei Drummond and Andrew Rambaut
  *
@@ -33,50 +33,97 @@ import dr.evolution.util.Taxon;
 import java.awt.*;
 import java.awt.event.*;
 
-import java.awt.font.FontRenderContext;
-import java.awt.font.LineBreakMeasurer;
-import java.awt.font.TextAttribute;
-import java.awt.font.TextLayout;
-import java.text.AttributedCharacterIterator;
-import java.text.AttributedString;
 
 /**
  * @author Andrew Rambaut
  * @author Alexei Drummond
  * @author Walter Xie
- * @version $Id: ViewAlignmentDialog.java,v 1.5 2009/08/11 13:29:34 rambaut Exp
- *          $
+ * @version $Id: ViewAligmentPanel.java,v 1.5 2009/08/11 13:29:34 rambaut Exp$
  */
 
 public class ViewAligmentPanel extends JPanel {
+		
+	private static final long serialVersionUID = 6129057786946772468L;
 	
 	private final PartitionData partitionData;
+	private final static int FONT_SIZE = 14;
+	private final static int GAP = 4;
+	private final static int TAXON_NAME_LEN = 300;
+	private final static int Y_BOADER = FONT_SIZE + 3 * GAP + 20;
+	private int width = TAXON_NAME_LEN;
+	private int height = Y_BOADER;
+	
 	 
 	public ViewAligmentPanel (PartitionData partitionData) {
 		this.partitionData = partitionData;
+		
+		getWidthLenth();
 	}
 
 	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		
+		super.paintComponent(g);		
 		setBackground(Color.white);
-
 
 //		Graphics2D g2d = (Graphics2D) g;
 		
-		g.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		g.setFont(new Font("SansSerif", Font.PLAIN, FONT_SIZE));
+		
+		String sequenceString = partitionData.getAlignment().getAlignedSequenceString(0);
+        for (int i = 0; i < sequenceString.length(); i++) {
+        	if (i == 0 || ((i+1)%10) == 0) {
+        		if (i == 0) {
+        			g.drawString(Integer.toString(i+1), TAXON_NAME_LEN + i * (FONT_SIZE + GAP), Y_BOADER - (FONT_SIZE + 3 * GAP)); 
+        		} else if (i < 100) {
+        			g.drawString(Integer.toString(i+1), TAXON_NAME_LEN + i * (FONT_SIZE + GAP) - GAP, Y_BOADER - (FONT_SIZE + 3 * GAP)); 
+        		} else {
+        			g.drawString(Integer.toString(i+1), TAXON_NAME_LEN + i * (FONT_SIZE + GAP) -  2 * GAP, Y_BOADER - (FONT_SIZE + 3 * GAP)); 
+        		} 
+        		
+        		g.drawLine(TAXON_NAME_LEN + i * (FONT_SIZE + GAP) + GAP, Y_BOADER - (FONT_SIZE + 2 * GAP), 
+        				TAXON_NAME_LEN + i * (FONT_SIZE + GAP) + GAP, Y_BOADER - (FONT_SIZE + GAP)); 
+        	}
+        }
+		
 		
 		for (int i = 0; i < partitionData.getAlignment().getTaxonCount(); i++) {
-            Taxon taxon = partitionData.getAlignment().getTaxon(i);
             
+			Taxon taxon = partitionData.getAlignment().getTaxon(i);        
             
-            g.drawString(taxon.getId(), 0, i * 18);
+            g.drawString(taxon.getId(), 0, Y_BOADER + i * (FONT_SIZE + GAP));
             
-            g.drawString(partitionData.getAlignment().getAlignedSequenceString(i), 300, i * 18);
+            sequenceString = partitionData.getAlignment().getAlignedSequenceString(i);
+            
+            int charSpace = TAXON_NAME_LEN;            	
+            for (char c : sequenceString.toCharArray()) {
+            	g.drawString(Character.toString(c), charSpace, Y_BOADER + i * (FONT_SIZE + GAP)); 
+            	charSpace = charSpace + FONT_SIZE + GAP;
+            }
+            
         }
 		
 		
 		
 	}
-
+		
+	public void setPreferredSize(){			
+		super.setPreferredSize(new java.awt.Dimension(width, height));
+//		System.out.println(width + "     " + height);
+	}
+	
+	
+	private void getWidthLenth() {
+		int maxLen = 0;
+		
+		String sequenceString = null;
+		for (int i = 0; i < partitionData.getAlignment().getTaxonCount(); i++) {
+            
+            sequenceString = partitionData.getAlignment().getAlignedSequenceString(i);
+            
+            if (sequenceString.length() > maxLen) maxLen = sequenceString.length();
+                   
+            height = height + FONT_SIZE + GAP;
+        }
+		
+		width = width + maxLen * (FONT_SIZE + GAP);  		
+	}
 }

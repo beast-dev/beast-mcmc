0ad529848b4064909ff9d3b557e9d6de0889d4b7
Marc Suchard <msuchard@ucla.edu>
2009-08-15 23:56:41 +0000

    Implemented BSSVS reversible model in BEAGLE.  I see ~7-fold speed-up for a 40-state phylogeography problem on my midrange iMac-GT120.

---
+++ src/dr/app/beagle/evomodel/substmodel/SVSGeneralSubstitutionModel.java
@@ -0,0 +1,37 @@
+package dr.app.beagle.evomodel.substmodel;
+
+import dr.inference.model.BayesianStochasticSearchVariableSelection;
+import dr.inference.model.Parameter;
+import dr.evolution.datatype.DataType;
+
+/**
+ * @author Marc Suchard
+ */
+
+public class SVSGeneralSubstitutionModel extends GeneralSubstitutionModel implements BayesianStochasticSearchVariableSelection {
+
+    public SVSGeneralSubstitutionModel(DataType dataType, FrequencyModel freqModel,
+                                       Parameter ratesParameter, Parameter indicatorsParameter) {
+        super(dataType, freqModel, ratesParameter, -1);
+        this.indicatorsParameter  = indicatorsParameter;
+        addVariable(indicatorsParameter);
+    }
+
+    @Override
+    protected void setupRelativeRates(double[] rates) {
+        for (int i = 0; i < rates.length; i++) {
+            rates[i] = ratesParameter.getParameterValue(i) * indicatorsParameter.getParameterValue(i);
+        }
+    }
+
+    public Parameter getIndicators() {
+        return indicatorsParameter;
+    }
+
+    public boolean validState() {
+        return true;
+    }
+
+    private Parameter indicatorsParameter;
+
+}

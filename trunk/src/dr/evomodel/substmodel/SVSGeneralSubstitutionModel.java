47e8a48b4a100a397cf8923fe8bfa82c50305a1e
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2009-08-18 09:30:55 +0000

    Standardizing likelihood checks in BSSVS across BEAGLE and BEAST

--- trunk/src/dr/evomodel/substmodel/SVSGeneralSubstitutionModel.java
+++ trunk/src/dr/evomodel/substmodel/SVSGeneralSubstitutionModel.java
@@ -41,8 +41,8 @@ public class SVSGeneralSubstitutionModel extends GeneralSubstitutionModel implem
         return rateIndicator;
     }
 
-      public boolean validState() {
-        return !updateMatrix || checkFullyConnected();
+    public boolean validState() {
+        return !updateMatrix || connectedAndWellConditioned();
     }
 
 
@@ -62,25 +62,19 @@ public class SVSGeneralSubstitutionModel extends GeneralSubstitutionModel implem
      */
     public double getLogLikelihood() {
         if (updateMatrix) {
-            if (!checkFullyConnected()) {
-                System.err.println("SVSGeneralSubstitutionModel is not fully connected.");
+            if (!connectedAndWellConditioned()) {
                 return Double.NEGATIVE_INFINITY;
             }
         }
         return 0;
     }
 
-    private boolean checkFullyConnected() {
+    private boolean connectedAndWellConditioned() {
         if (probability == null)
             probability = new double[stateCount*stateCount];
 
         getTransitionProbabilities(1.0,probability);
-        final int length = stateCount*stateCount;
-        for(int i=0; i<length; i++) {
-            if(probability[i] == 0)
-                return false;
-        }
-        return true;
+        return Utils.connectedAndWellConditioned(probability);
     }
 
     /**
--- trunk/src/dr/inference/model/BayesianStochasticSearchVariableSelection.java
+++ trunk/src/dr/inference/model/BayesianStochasticSearchVariableSelection.java
@@ -10,4 +10,14 @@ public interface BayesianStochasticSearchVariableSelection {
 
     public boolean validState();
 
+    public class Utils {
+
+        public static boolean connectedAndWellConditioned(double[] probability) {
+            for(int i=0; i<probability.length; i++) {
+                if(probability[i] == 0 || probability[i] > 1)
+                    return false;
+            }
+            return true;
+        }
+    }
 }

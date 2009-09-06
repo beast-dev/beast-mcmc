99a9e98437b6c0f4f398c9e263642e21fe236ff3
jheled <jheled@ca793f91-a31e-0410-b540-2769d408b6a1>
2009-09-06 20:12:20 +0000

    comment out unused code

--- trunk/src/dr/util/CollectionHash.java
+++ trunk/src/dr/util/CollectionHash.java
@@ -31,51 +31,53 @@ import java.util.Enumeration;
 import java.util.Hashtable;
 
 
-public class CollectionHash {
-
-    private Hashtable table;
-    
-    public CollectionHash() { table = new Hashtable(); }
-
-    public void put(Object key, Object o) {
-		Collection c = (Collection)table.get(key);
-
-		if (c != null) {
-		    c.add(o);
-		} else {
-		    Collection newc = new ArrayList();
-		    newc.add(o);
-		    table.put(key, newc);
-		}
-    }
-
-    public Object get(Object key) {
-		Collection c = (Collection)table.get(key);
-
-		if (c == null) return null;
-
-		return c.iterator().next();
-    }
-
-    public Enumeration keys() {
-		return table.keys();
-    }
-
-    public Object[] getAll(Object key) {
-		return getCollection(key).toArray();
-    }
-
-    public Collection getCollection(Object key) {
-		return (Collection)table.get(key);
-    }
-
-    public int getSize(Object key) {
-        Collection collection = (Collection)table.get(key);
-        if (collection == null) return 0;
-        return collection.size();
-    }
-
-    public void remove(Object key) {
-    	table.remove(key);
-    }
-}
+// todo not used?
+//
+//public class CollectionHash {
+//
+//    private final Hashtable table;
+//
+//    public CollectionHash() { table = new Hashtable(); }
+//
+//    public void put(Object key, Object o) {
+//		Collection c = (Collection)table.get(key);
+//
+//		if (c != null) {
+//		    c.add(o);
+//		} else {
+//		    Collection newc = new ArrayList();
+//		    newc.add(o);
+//		    table.put(key, newc);
+//		}
+//    }
+//
+//    public Object get(Object key) {
+//		Collection c = (Collection)table.get(key);
+//
+//		if (c == null) return null;
+//
+//		return c.iterator().next();
+//    }
+//
+//    public Enumeration keys() {
+//		return table.keys();
+//    }
+//
+//    public Object[] getAll(Object key) {
+//		return getCollection(key).toArray();
+//    }
+//
+//    public Collection getCollection(Object key) {
+//		return (Collection)table.get(key);
+//    }
+//
+//    public int getSize(Object key) {
+//        Collection collection = (Collection)table.get(key);
+//        if (collection == null) return 0;
+//        return collection.size();
+//    }
+//
+//    public void remove(Object key) {
+//    	table.remove(key);
+//    }
+//}

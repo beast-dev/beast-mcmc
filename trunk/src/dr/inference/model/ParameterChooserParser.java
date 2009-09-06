d070245c40d92719d8a645ecab69beb06fccbe38
jheled <jheled@ca793f91-a31e-0410-b540-2769d408b6a1>
2009-09-06 20:21:37 +0000

    commit development files (serves as a backup and versioning) - may and will substantially change or go away.

---
+++ trunk/src/dr/inference/model/ParameterChooserParser.java
@@ -0,0 +1,38 @@
+package dr.inference.model;
+
+import dr.xml.*;
+
+/**
+ * @author Joseph Heled
+ *         Date: 4/09/2009
+ */
+public class ParameterChooserParser extends dr.xml.AbstractXMLObjectParser {
+    public static String VARIABLE_SELECTOR = "variableSelector";
+    public static String INDEX = "index";
+
+    public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+        String name = xo.getId();
+        final ValuesPool pool = (ValuesPool) xo.getChild(ValuesPool.class);
+        final int[] which = xo.getIntegerArrayAttribute(INDEX);
+        return new ParameterChooser(name, pool, which);
+    }
+
+    public XMLSyntaxRule[] getSyntaxRules() {
+        return new XMLSyntaxRule[] {
+                new ElementRule(ValuesPool.class,1,1),
+                AttributeRule.newIntegerArrayRule(INDEX, false),
+        };
+    }
+
+    public String getParserDescription() {
+        return "";
+    }
+
+    public Class getReturnType() {
+        return ParameterChooser.class;
+    }
+
+    public String getParserName() {
+        return VARIABLE_SELECTOR;
+    }
+}
---
+++ trunk/src/dr/inference/model/ValuesPoolParser.java
@@ -0,0 +1,47 @@
+package dr.inference.model;
+
+import dr.xml.*;
+import sun.awt.X11.XLayerProtocol;
+
+/**
+ * @author Joseph Heled
+ *         Date: 4/09/2009
+ */
+public class ValuesPoolParser extends dr.xml.AbstractXMLObjectParser {
+    public static String VALUES_POOL = "valuesPool";
+    public static String VALUES = "values";
+     public static String SELECTOR = "selector";
+
+    public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+        final Variable pool = (Variable)xo.getElementFirstChild(VALUES);
+        final Variable selector = (Variable)xo.getElementFirstChild(SELECTOR);
+
+        if( pool.getSize() != selector.getSize() ) {
+            throw new XMLParseException("variable Pool and selector must have equal length.");
+        }
+        return new ValuesPool(pool, selector);
+    }
+
+    public XMLSyntaxRule[] getSyntaxRules() {
+          return new XMLSyntaxRule[] {
+                  new ElementRule(VALUES, new XMLSyntaxRule[]{
+                        new ElementRule(Variable.class,1,1) }),
+                   new ElementRule(SELECTOR, new XMLSyntaxRule[]{
+                        new ElementRule(Variable.class,1,1) })
+
+          };
+      }
+
+
+    public String getParserDescription() {
+        return "";
+    }
+
+    public Class getReturnType() {
+        return ValuesPool.class;
+    }
+
+    public String getParserName() {
+        return VALUES_POOL;
+    }
+}

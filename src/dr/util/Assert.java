874b5e5127dc9c69382c1ef75286811871e69e2e
jheled <jheled@ca793f91-a31e-0410-b540-2769d408b6a1>
2009-09-06 20:12:40 +0000

    comment out unused code

--- src/dr/util/Assert.java
+++ src/dr/util/Assert.java
@@ -35,38 +35,38 @@ package dr.util;
  *
  * @author Andrew Rambaut
  */
-public class Assert {
-
-	/* not sure about exactly what this function should display */
-	private static void fail(String msg)
-	{
-		System.err.println("Assertion failed: " + msg);
-
-		Throwable e = new Throwable();
-		e.printStackTrace();
-	}
-
-	public static void test(boolean b)
-	{
-		if (!b)
-			fail("Statement false");
-	}
-
-	public static void test(long lng)
-	{
-		if (lng == 0L)
-			fail("Zero integer");
-	}
-
-	public static void test(double dbl)
-	{
-		if (dbl == 0.0)
-			fail("Zero double");
-	}
-
-	public static void test(Object ref)
-	{
-		if (ref == null)
-			fail("Null reference");
-	}
-}
+//public class Assert {
+//
+//	/* not sure about exactly what this function should display */
+//	private static void fail(String msg)
+//	{
+//		System.err.println("Assertion failed: " + msg);
+//
+//		Throwable e = new Throwable();
+//		e.printStackTrace();
+//	}
+//
+//	public static void test(boolean b)
+//	{
+//		if (!b)
+//			fail("Statement false");
+//	}
+//
+//	public static void test(long lng)
+//	{
+//		if (lng == 0L)
+//			fail("Zero integer");
+//	}
+//
+//	public static void test(double dbl)
+//	{
+//		if (dbl == 0.0)
+//			fail("Zero double");
+//	}
+//
+//	public static void test(Object ref)
+//	{
+//		if (ref == null)
+//			fail("Null reference");
+//	}
+//}

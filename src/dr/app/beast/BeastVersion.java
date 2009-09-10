0f73e6710a7c63519e7928d349642fdfe23d2db2
Walter <walterxie@users.noreply.github.com>
2009-09-10 02:46:26 +0000

    BEAUTi: reduce ModelOptions.

--- src/dr/app/beast/BeastVersion.java
+++ src/dr/app/beast/BeastVersion.java
@@ -46,6 +46,7 @@ public class BeastVersion implements Version {
      * Version string: assumed to be in format x.x.x
      */
     private static final String VERSION = "1.5.1";
+    
     private static final String DATE_STRING = "2002-2009";
 
     /**
@@ -53,6 +54,10 @@ public class BeastVersion implements Version {
      * for SVN. We must increment this manually.
      */
     private static final String BUILD_ID = "Build r2191";
+
+    public String getVersion() {
+        return VERSION;
+    }
 
     public String getVersionString() {
         return "v" + VERSION;

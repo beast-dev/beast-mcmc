b13e2cbe4d1704b8178abc0a79ad25ea917f643a
akaruiws <jessiewu@users.noreply.github.com>
2009-09-23 12:17:28 +0000

    computeStationaryDistribution Method changed. Other new methods added.

--- src/dr/evomodel/substmodel/OnePhaseModel.java
+++ src/dr/evomodel/substmodel/OnePhaseModel.java
@@ -3,12 +3,16 @@ package dr.evomodel.substmodel;
 import dr.evolution.datatype.Microsatellite;
 import dr.inference.model.Parameter;
 
+import java.util.ArrayList;
+
+
 /**
  * @author Chieh-Hsi Wu
  *
  * An abstract for One Phase Microsatellite Models
  */
 public abstract class OnePhaseModel extends MicrosatelliteModel{
+    protected ArrayList<Parameter> nestedParams = null;
 
     /**
      * Constructor
@@ -20,22 +24,46 @@ public abstract class OnePhaseModel extends MicrosatelliteModel{
      */
     public OnePhaseModel(String name, Microsatellite microsatellite, FrequencyModel freqModel, Parameter parameter){
         super(name, microsatellite, freqModel, parameter);
+        nestedParams=new ArrayList<Parameter>();
     }
 
-    /**
+    /*
+     * adding the parameters only if its not a submodel.
+     */
+    protected void addParam(Parameter param){
+        if(isNested){
+            nestedParams.add(param);
+        }else{
+            super.addVariable(param);
+        }
+    }
+
+    /*
+     * get the parameters in this submodel
+     */
+    public Parameter getNestedParameter(int i){
+        return nestedParams.get(i);
+    }
+
+    /*
+     * get number of nested parameters in the submodel
+     */
+    public int getNestedParameterCount(){
+        return nestedParams.size();
+    }
+
+    /*
      * The One Phase Models are special cases of the birth-death chain,
      * and therefore we can use this to calculate the stationay distribution
      * given a infinitesimal rate matrix.
      */
     public void computeStationaryDistribution(){
         double[] pi = new double[stateCount];
-        double[] q = getRates();
-        int k = 0;
+
         pi[0] = 1.0;
         double piSum = 1.0;
         for(int i = 1; i < stateCount; i++){
-            pi[i] = pi[i-1]*q[k]/q[k+stateCount-1];
-            k = k+stateCount;
+            pi[i] = pi[i-1]*infinitesimalRateMatrix[i-1][i]/infinitesimalRateMatrix[i][i-1];
             piSum = piSum+pi[i];
         }
 
@@ -43,8 +71,9 @@ public abstract class OnePhaseModel extends MicrosatelliteModel{
             pi[i] = pi[i]/piSum;
 
         }
-        freqModel = new FrequencyModel(dataType,pi);        
+        freqModel = new FrequencyModel(dataType,pi);
         super.computeStationaryDistribution();
+
     }
 
 

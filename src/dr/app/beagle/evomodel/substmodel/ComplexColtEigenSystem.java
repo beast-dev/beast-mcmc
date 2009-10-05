b3e91f0ec64091e864a767d43108e929edb660f2
Marc Suchard <msuchard@ucla.edu>
2009-10-05 18:04:51 +0000

    Fixes to CachedDistributionLikelihood to work with MultivariateDistributionLikelihood.  Tweaks to eigendecomposition routines, in case of invalid diagonalization

--- src/dr/app/beagle/evomodel/substmodel/ComplexColtEigenSystem.java
+++ src/dr/app/beagle/evomodel/substmodel/ComplexColtEigenSystem.java
@@ -1,6 +1,7 @@
 package dr.app.beagle.evomodel.substmodel;
 
 import dr.math.matrixAlgebra.RobustEigenDecomposition;
+import cern.colt.matrix.DoubleMatrix2D;
 
 /**
  * @author Marc Suchard
@@ -16,5 +17,13 @@ public class ComplexColtEigenSystem extends ColtEigenSystem {
         System.arraycopy(realEval,0,merge,0,dim);
         System.arraycopy(imagEval,0,merge,dim,dim);
         return merge;
+    }
+
+      protected double[] getEmptyAllEigenValues(int dim) {
+        return new double[2 * dim];
+    }
+
+    protected boolean validDecomposition(DoubleMatrix2D eigenV) {
+        return true;
     }
 }

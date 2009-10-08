5125fa87e7576492e68aa7b4c63c217d97acd5e7
rambaut <rambaut@users.noreply.github.com>
2009-10-08 14:42:47 +0000

    Added option to BeastDialog to show list of BEAGLE resources and stop. Added 'Automatic' thread pool option. With BEAGLE, this should be the best option if multiple resources are being used.
    Mac ANT release builder now includes the standard BEAGLE library location in java.library.path (/usr/local/lib) so if it is installed there it should be found. Note that in order to use the current BEAGLE with NVidia Cuda, the application must be run in 32-bit mode (use Get Info in Finder).

--- src/dr/inference/model/CompoundLikelihood.java
+++ src/dr/inference/model/CompoundLikelihood.java
@@ -42,92 +42,94 @@ import java.util.logging.Logger;
  */
 public class CompoundLikelihood implements Likelihood {
 
-	public static final String COMPOUND_LIKELIHOOD = "compoundLikelihood";
-	public static final String THREADS = "threads";
-	public static final String POSTERIOR = "posterior";
-	public static final String PRIOR = "prior";
-	public static final String LIKELIHOOD = "likelihood";
+    public static final String COMPOUND_LIKELIHOOD = "compoundLikelihood";
+    public static final String THREADS = "threads";
+    public static final String POSTERIOR = "posterior";
+    public static final String PRIOR = "prior";
+    public static final String LIKELIHOOD = "likelihood";
 
-	public CompoundLikelihood(int threads) {
-		if (threads > 1) {
-			pool = Executors.newFixedThreadPool(threads);
-//			pool = Executors.newCachedThreadPool();
-		} else {
-			pool = null;
-		}
-	}
+    public CompoundLikelihood(int threads) {
+        if (threads > 0) {
+            pool = Executors.newFixedThreadPool(threads);
+        } else if (threads < 0) {
+            // create a cached thread pool which should create one thread per likelihood...
+            pool = Executors.newCachedThreadPool();
+        } else {
+            pool = null;
+        }
+    }
 
-	public void addLikelihood(Likelihood likelihood) {
+    public void addLikelihood(Likelihood likelihood) {
 
-		if (!likelihoods.contains(likelihood)) {
+        if (!likelihoods.contains(likelihood)) {
 
-			likelihoods.add(likelihood);
-			if (likelihood.getModel() != null) {
-				compoundModel.addModel(likelihood.getModel());
-			}
+            likelihoods.add(likelihood);
+            if (likelihood.getModel() != null) {
+                compoundModel.addModel(likelihood.getModel());
+            }
 
-			likelihoodCallers.add(new LikelihoodCaller(likelihood));
-		}
-	}
+            likelihoodCallers.add(new LikelihoodCaller(likelihood));
+        }
+    }
 
-	public int getLikelihoodCount() {
-		return likelihoods.size();
-	}
+    public int getLikelihoodCount() {
+        return likelihoods.size();
+    }
 
-	public final Likelihood getLikelihood(int i) {
-		return likelihoods.get(i);
-	}
+    public final Likelihood getLikelihood(int i) {
+        return likelihoods.get(i);
+    }
 
-	// **************************************************************
-	// Likelihood IMPLEMENTATION
-	// **************************************************************
+    // **************************************************************
+    // Likelihood IMPLEMENTATION
+    // **************************************************************
 
-	public Model getModel() {
-		return compoundModel;
-	}
+    public Model getModel() {
+        return compoundModel;
+    }
 
     // todo: remove in release
-    static int DEBUG = 0;               
+    static int DEBUG = 0;
 
-	public double getLogLikelihood() {
-		double logLikelihood = 0.0;
+    public double getLogLikelihood() {
+        double logLikelihood = 0.0;
 
-		if (pool == null) {
-			// Single threaded
+        if (pool == null) {
+            // Single threaded
 
-			for (Likelihood likelihood : likelihoods) {
-			   final double l = likelihood.getLogLikelihood();
-				// if the likelihood is zero then short cut the rest of the likelihoods
-				// This means that expensive likelihoods such as TreeLikelihoods should
-				// be put after cheap ones such as BooleanLikelihoods
-				if( l == Double.NEGATIVE_INFINITY )
+            for (Likelihood likelihood : likelihoods) {
+                final double l = likelihood.getLogLikelihood();
+                // if the likelihood is zero then short cut the rest of the likelihoods
+                // This means that expensive likelihoods such as TreeLikelihoods should
+                // be put after cheap ones such as BooleanLikelihoods
+                if( l == Double.NEGATIVE_INFINITY )
                     return Double.NEGATIVE_INFINITY;
-				logLikelihood += l;
-			}
-		} else {
-			try {
+                logLikelihood += l;
+            }
+        } else {
+            try {
 
-				List<Future<Double>> results = pool.invokeAll(likelihoodCallers);
+                List<Future<Double>> results = pool.invokeAll(likelihoodCallers);
 
-				for (Future<Double> result : results) {
-					double logL = result.get();
-					logLikelihood += logL;
-				}
+                for (Future<Double> result : results) {
+                    double logL = result.get();
+                    logLikelihood += logL;
+                }
 
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} catch (ExecutionException e) {
-				e.printStackTrace();
-			}
-		}
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
 
         if( DEBUG > 0 ) {
             int t = DEBUG; DEBUG = 0;
             System.err.println(getId() + ": " + getDiagnosis() + " = " + logLikelihood);
             DEBUG = t;
         }
-		return logLikelihood;
-	}
+        return logLikelihood;
+    }
 
     public void makeDirty() {
         for( Likelihood likelihood : likelihoods ) {
@@ -154,7 +156,7 @@ public class CompoundLikelihood implements Likelihood {
             if( lik instanceof CompoundLikelihood ) {
                 final String d = ((CompoundLikelihood) lik).getDiagnosis();
                 if( d != null && d.length() > 0 ) {
-                    message += "{" + d + "}";
+                    message += "(" + d + ")";
                 }
             } else {
 
@@ -172,136 +174,146 @@ public class CompoundLikelihood implements Likelihood {
         return message;
     }
 
-	public String toString() {
+    public String toString() {
         return getId();
         // really bad for debugging
-		//return Double.toString(getLogLikelihood());
-	}
+        //return Double.toString(getLogLikelihood());
+    }
 
     public String prettyName() {
         return Abstract.getPrettyName(this);
     }
 
-	// **************************************************************
-	// Loggable IMPLEMENTATION
-	// **************************************************************
+    // **************************************************************
+    // Loggable IMPLEMENTATION
+    // **************************************************************
 
-	/**
-	 * @return the log columns.
-	 */
-	public dr.inference.loggers.LogColumn[] getColumns() {
-		return new dr.inference.loggers.LogColumn[]{
-				new LikelihoodColumn(getId())
-		};
-	}
+    /**
+     * @return the log columns.
+     */
+    public dr.inference.loggers.LogColumn[] getColumns() {
+        return new dr.inference.loggers.LogColumn[]{
+                new LikelihoodColumn(getId())
+        };
+    }
 
-	private class LikelihoodColumn extends dr.inference.loggers.NumberColumn {
-		public LikelihoodColumn(String label) {
-			super(label);
-		}
+    private class LikelihoodColumn extends dr.inference.loggers.NumberColumn {
+        public LikelihoodColumn(String label) {
+            super(label);
+        }
 
-		public double getDoubleValue() {
-			return getLogLikelihood();
-		}
-	}
+        public double getDoubleValue() {
+            return getLogLikelihood();
+        }
+    }
 
-	// **************************************************************
-	// Identifiable IMPLEMENTATION
-	// **************************************************************
+    // **************************************************************
+    // Identifiable IMPLEMENTATION
+    // **************************************************************
 
-	private String id = null;
+    private String id = null;
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public String getId() {
-		return id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public static XMLObjectParser PARSER = new AbstractXMLObjectParser() {
+    public static XMLObjectParser PARSER = new AbstractXMLObjectParser() {
 
-		public String getParserName() {
-			return COMPOUND_LIKELIHOOD;
-		}
+        public String getParserName() {
+            return COMPOUND_LIKELIHOOD;
+        }
 
-		public String[] getParserNames() {
-			return new String[]{getParserName(), POSTERIOR, PRIOR, LIKELIHOOD};
-		}
+        public String[] getParserNames() {
+            return new String[]{getParserName(), POSTERIOR, PRIOR, LIKELIHOOD};
+        }
 
-		public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+        public Object parseXMLObject(XMLObject xo) throws XMLParseException {
 
-			int threads = xo.getAttribute(THREADS, 0);
+            // the default is 0 threads but an XML attribute can override it
+            int threads = xo.getAttribute(THREADS, 0);
 
-            if (xo.getName().equalsIgnoreCase(LIKELIHOOD) && System.getProperty("thread_count") != null) {
-                threads = Integer.parseInt(System.getProperty("thread_count"));
-                if (threads < 0 || threads > 1000) {
-                    // put an upper limit here - may be unnecessary?
-                    threads = 0;
+            if (xo.getName().equalsIgnoreCase(LIKELIHOOD)) {
+                // if this is '<likelihood>' then the default is to use a cached thread pool...
+                threads = xo.getAttribute(THREADS, -1);
+
+                // both the XML attribute and a system property can override it
+                if (System.getProperty("thread.count") != null) {
+
+                    threads = Integer.parseInt(System.getProperty("thread.count"));
+                    if (threads < -1 || threads > 1000) {
+                        // put an upper limit here - may be unnecessary?
+                        threads = -1;
+                    }
                 }
             }
 
-			CompoundLikelihood compoundLikelihood = new CompoundLikelihood(threads);
+            CompoundLikelihood compoundLikelihood = new CompoundLikelihood(threads);
 
-			for(int i = 0; i < xo.getChildCount(); i++) {
+            for(int i = 0; i < xo.getChildCount(); i++) {
                 final Object child = xo.getChild(i);
                 if( child instanceof Likelihood ) {
-					compoundLikelihood.addLikelihood((Likelihood) child);
-				} else {
+                    compoundLikelihood.addLikelihood((Likelihood) child);
+                } else {
 
                     throw new XMLParseException("An element (" + child + ") which is not a likelihood has been added to a "
                             + COMPOUND_LIKELIHOOD + " element");
-				}
-			}
+                }
+            }
 
-			if (threads > 1) {
-				Logger.getLogger("dr.evomodel").info("Likelihood is using " + threads + " threads.");
-			}
+            if (threads > 0) {
+                Logger.getLogger("dr.evomodel").info("Likelihood is using " + threads + " threads.");
+            } else if (threads < 0) {
+                Logger.getLogger("dr.evomodel").info("Likelihood is using a cached thread pool.");
+            }
 
-			return compoundLikelihood;
-		}
+            return compoundLikelihood;
+        }
 
-		//************************************************************************
-		// AbstractXMLObjectParser implementation
-		//************************************************************************
+        //************************************************************************
+        // AbstractXMLObjectParser implementation
+        //************************************************************************
 
-		public String getParserDescription() {
-			return "A likelihood function which is simply the product of its component likelihood functions.";
-		}
+        public String getParserDescription() {
+            return "A likelihood function which is simply the product of its component likelihood functions.";
+        }
 
-		public XMLSyntaxRule[] getSyntaxRules() {
-			return rules;
-		}
+        public XMLSyntaxRule[] getSyntaxRules() {
+            return rules;
+        }
 
-		private final XMLSyntaxRule[] rules = {
+        private final XMLSyntaxRule[] rules = {
                 AttributeRule.newIntegerRule(THREADS, true),
-				new ElementRule(Likelihood.class, 1, Integer.MAX_VALUE)				
-		};
+                new ElementRule(Likelihood.class, 1, Integer.MAX_VALUE)
+        };
 
-		public Class getReturnType() {
-			return CompoundLikelihood.class;
-		}
-	};
+        public Class getReturnType() {
+            return CompoundLikelihood.class;
+        }
+    };
 
-	private final ExecutorService pool;
+    private final ExecutorService pool;
 
-	private final ArrayList<Likelihood> likelihoods = new ArrayList<Likelihood>();
-	private final CompoundModel compoundModel = new CompoundModel("compoundModel");
+    private final ArrayList<Likelihood> likelihoods = new ArrayList<Likelihood>();
+    private final CompoundModel compoundModel = new CompoundModel("compoundModel");
 
 
-	private final List<Callable<Double>> likelihoodCallers = new ArrayList<Callable<Double>>();
+    private final List<Callable<Double>> likelihoodCallers = new ArrayList<Callable<Double>>();
 
-	class LikelihoodCaller implements Callable<Double> {
+    class LikelihoodCaller implements Callable<Double> {
 
-		public LikelihoodCaller(Likelihood likelihood) {
-			this.likelihood = likelihood;
-		}
+        public LikelihoodCaller(Likelihood likelihood) {
+            this.likelihood = likelihood;
+        }
 
-		public Double call() throws Exception {
-			return likelihood.getLogLikelihood();
-		}
+        public Double call() throws Exception {
+            return likelihood.getLogLikelihood();
+        }
 
-		private final Likelihood likelihood;
-	}
+        private final Likelihood likelihood;
+    }
 }
 

91ac811739fdf9fc9bf19ea6f4804ab7c3a55bd9
bloomquist <bloomquist@ca793f91-a31e-0410-b540-2769d408b6a1>
2009-10-13 21:25:13 +0000

    Commented my code.

--- trunk/src/test/dr/distibutions/GammaDistributionTest.java
+++ trunk/src/test/dr/distibutions/GammaDistributionTest.java
@@ -12,10 +12,18 @@ public class GammaDistributionTest extends TestCase{
 		gamma = new GammaDistribution(1.0,1.0);
 	}
 	
+	/**
+	 * This test stochastically draws 100 gamma
+	 * variates and compares the coded pdf 
+	 * with the actual pdf.  
+	 * The tolerance is required to be at most 1e-10.
+	 */
 	
 	public void testPdf(){
 		
-		for(int i = 0; i < 200.0; i++){
+		int numberOfTests = 100;
+		
+			for(int i = 0; i < numberOfTests; i++){
 			double j = i/500.0 + 1.0;
 			
 			double shape = 1.0/j;
@@ -29,8 +37,10 @@ public class GammaDistributionTest extends TestCase{
 			double mypdf = Math.pow(value, shape-1)/GammaFunction.gamma(shape)
 					*Math.exp(-value/scale)/Math.pow(scale, shape);
 			
+			
 			assertEquals(mypdf,gamma.pdf(value),1e-10);
 		}
+		
 		gamma.setScale(1.0);
 		gamma.setShape(1.0);
 

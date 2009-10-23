60e4088be8ed6ef35cb2b0e288447ef2869055c3
jheled <jheled@ca793f91-a31e-0410-b540-2769d408b6a1>
2009-10-23 20:37:25 +0000

    Remove deprecated class Yule (superceeded by birth death)
    Add option for conditioning on root height.

--- trunk/src/dr/evomodel/speciation/YuleModel.java
+++ trunk/src/dr/evomodel/speciation/YuleModel.java
@@ -25,10 +25,6 @@
 
 package dr.evomodel.speciation;
 
-import dr.evolution.tree.NodeRef;
-import dr.evolution.tree.Tree;
-import dr.inference.model.Parameter;
-
 /**
  * From Gernhard 2008, Yule density (p; conditioned on n nodes) should be:
  * <p/>
@@ -43,71 +39,71 @@ import dr.inference.model.Parameter;
  * @author Alexei Drummond
  * @author Roald Forsberg
  */
-public class YuleModel extends UltrametricSpeciationModel {
-
-    public static final String YULE_MODEL = "yuleModel";
-
-
-    public YuleModel(Parameter birthRateParameter, Type units) {
-
-        super(YULE_MODEL, units);
-
-        this.birthRateParameter = birthRateParameter;
-        addVariable(birthRateParameter);
-        birthRateParameter.addBounds(new Parameter.DefaultBounds(Double.POSITIVE_INFINITY, 0.0, 1));
-    }
-
-    public double getBirthRate() {
-        return birthRateParameter.getParameterValue(0);
-    }
-
-    public void setBirthRate(double birthRate) {
-
-        birthRateParameter.setParameterValue(0, birthRate);
-    }
-
-    //
-    // functions that define a speciation model
-    //
-    public double logTreeProbability(int taxonCount) {
-        return 0.0;
-    }
-
-    //
-    // functions that define a speciation model
-    //
-    public double logNodeProbability(Tree tree, NodeRef node) {
-
-        double nodeHeight = tree.getNodeHeight(node);
-        final double lambda = getBirthRate();
-
-        double logP = 0;
-
-
-        if (tree.isRoot(node)) {
-            logP += -lambda * nodeHeight;
-        } else {
-        }
-        logP += Math.log(lambda);
-        logP += -lambda * nodeHeight;
-
-        return logP;
-    }
-
-    public boolean includeExternalNodesInLikelihoodCalculation() {
-        return false;
-    }
-
-    // **************************************************************
-    // XMLElement IMPLEMENTATION
-    // **************************************************************
-
-    public org.w3c.dom.Element createElement(org.w3c.dom.Document d) {
-        throw new RuntimeException("createElement not implemented");
-    }
-
-
-    //Protected stuff
-    Parameter birthRateParameter;
-
-}
+//public class YuleModel extends UltrametricSpeciationModel {
+//
+//
+//
+//
+//    public YuleModel(Parameter birthRateParameter, Type units) {
+//
+//        super(YULE_MODEL, units);
+//
+//        this.birthRateParameter = birthRateParameter;
+//        addVariable(birthRateParameter);
+//        birthRateParameter.addBounds(new Parameter.DefaultBounds(Double.POSITIVE_INFINITY, 0.0, 1));
+//    }
+//
+//    public double getBirthRate() {
+//        return birthRateParameter.getParameterValue(0);
+//    }
+//
+//    public void setBirthRate(double birthRate) {
+//
+//        birthRateParameter.setParameterValue(0, birthRate);
+//    }
+//
+//    //
+//    // functions that define a speciation model
+//    //
+//    public double logTreeProbability(int taxonCount) {
+//        return 0.0;
+//    }
+//
+//    //
+//    // functions that define a speciation model
+//    //
+//    public double logNodeProbability(Tree tree, NodeRef node) {
+//
+//        double nodeHeight = tree.getNodeHeight(node);
+//        final double lambda = getBirthRate();
+//
+//        double logP = 0;
+//
+//
+//        if (tree.isRoot(node)) {
+//            logP += -lambda * nodeHeight;
+//        } else {
+//        }
+//        logP += Math.log(lambda);
+//        logP += -lambda * nodeHeight;
+//
+//        return logP;
+//    }
+//
+//    public boolean includeExternalNodesInLikelihoodCalculation() {
+//        return false;
+//    }
+//
+//    // **************************************************************
+//    // XMLElement IMPLEMENTATION
+//    // **************************************************************
+//
+//    public org.w3c.dom.Element createElement(org.w3c.dom.Document d) {
+//        throw new RuntimeException("createElement not implemented");
+//    }
+//
+//
+//    //Protected stuff
+//    Parameter birthRateParameter;
+//
+//}

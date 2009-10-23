cb962edefef6a2163f87db6e27584da89fbae70b
rambaut <rambaut@users.noreply.github.com>
2009-10-23 14:38:16 +0000

    Fixed Issue 218 (http://code.google.com/p/beast-mcmc/issues/detail?id=218).

--- src/dr/app/beast/BeastVersion.java
+++ src/dr/app/beast/BeastVersion.java
@@ -45,7 +45,7 @@ public class BeastVersion implements Version {
     /**
      * Version string: assumed to be in format x.x.x
      */
-    private static final String VERSION = "1.5.2";
+    private static final String VERSION = "1.5.3";
     
     private static final String DATE_STRING = "2002-2009";
 

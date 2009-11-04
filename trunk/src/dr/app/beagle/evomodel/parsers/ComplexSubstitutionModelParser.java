de21d0aee7a17034f4801bf0da3a5e941078e966
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2009-11-04 19:53:33 +0000

    Making BEAGLE ComplexSubstitutionModel as compatible with the old CSM as possible.   Removing duplicated code in SVSCSMParser.

--- trunk/src/dr/app/beagle/evomodel/parsers/ComplexSubstitutionModelParser.java
+++ trunk/src/dr/app/beagle/evomodel/parsers/ComplexSubstitutionModelParser.java
@@ -2,9 +2,11 @@ package dr.app.beagle.evomodel.parsers;
 
 import dr.xml.*;
 import dr.inference.model.Parameter;
+import dr.inference.model.BayesianStochasticSearchVariableSelection;
 import dr.app.beagle.evomodel.substmodel.FrequencyModel;
 import dr.app.beagle.evomodel.substmodel.SubstitutionModel;
 import dr.app.beagle.evomodel.substmodel.ComplexSubstitutionModel;
+import dr.app.beagle.evomodel.substmodel.SVSComplexSubstitutionModel;
 import dr.evolution.datatype.DataType;
 
 import java.util.logging.Logger;
@@ -15,20 +17,33 @@ import java.util.logging.Logger;
 public class ComplexSubstitutionModelParser extends AbstractXMLObjectParser {
 
     public static final String COMPLEX_SUBSTITUTION_MODEL = "complexSubstitutionModel";
+    public static final String SVS_COMPLEX_SUBSTITUTION_MODEL = "svsComplexSubstitutionModel";
     public static final String DATA_TYPE = "dataType";
     public static final String RATES = "rates";
     public static final String FREQUENCIES = "frequencies";
-
+    public static final String ROOT_FREQUENCIES = "rootFrequencies";
+    public static final String RANDOMIZE = "randomizeIndicator";
+    public static final String INDICATOR = "rateIndicator";
 
     public String getParserName() {
         return COMPLEX_SUBSTITUTION_MODEL;
     }
 
+    public String[] getParserNames() {
+        return new String[] {COMPLEX_SUBSTITUTION_MODEL, SVS_COMPLEX_SUBSTITUTION_MODEL};
+    }
+
+
     public Object parseXMLObject(XMLObject xo) throws XMLParseException {
 
         Parameter ratesParameter;
 
-        XMLObject cxo = xo.getChild(FREQUENCIES);
+        XMLObject cxo;
+        if (xo.hasChildNamed(FREQUENCIES)) {
+            cxo = xo.getChild(FREQUENCIES);
+        } else {
+            cxo = xo.getChild(ROOT_FREQUENCIES);
+        }
         FrequencyModel freqModel = (FrequencyModel) cxo.getChild(FrequencyModel.class);
 
         DataType dataType = freqModel.getDataType();
@@ -54,7 +69,24 @@ public class ComplexSubstitutionModelParser extends AbstractXMLObjectParser {
             throw new XMLParseException("Rates parameter in " + getParserName() + " element should have " + rateCount + " dimensions.");
         }
 
-        return new ComplexSubstitutionModel(COMPLEX_SUBSTITUTION_MODEL,dataType, freqModel, ratesParameter);
+        if (!xo.hasChildNamed(INDICATOR)) {
+            return new ComplexSubstitutionModel(COMPLEX_SUBSTITUTION_MODEL,dataType, freqModel, ratesParameter);
+        }
+
+        cxo = xo.getChild(INDICATOR);
+
+        Parameter indicatorParameter = (Parameter) cxo.getChild(Parameter.class);
+        boolean randomize = xo.getAttribute(RANDOMIZE, false);
+        if (randomize) {
+            BayesianStochasticSearchVariableSelection.Utils.randomize(indicatorParameter,
+                    dataType.getStateCount(),false);
+        }
+
+        if (indicatorParameter == null || ratesParameter == null || indicatorParameter.getDimension() != ratesParameter.getDimension())
+            throw new XMLParseException("Rates and indicator parameters in " + getParserName() + " element must be the same dimension.");
+
+        return new SVSComplexSubstitutionModel(SVS_COMPLEX_SUBSTITUTION_MODEL,dataType, freqModel, ratesParameter, indicatorParameter);
+
     }
 
     //************************************************************************
@@ -74,10 +106,17 @@ public class ComplexSubstitutionModelParser extends AbstractXMLObjectParser {
     }
 
     private final XMLSyntaxRule[] rules = {
-            new ElementRule(FREQUENCIES, FrequencyModel.class),
+            AttributeRule.newBooleanRule(RANDOMIZE,true),
+            new XORRule(
+                new ElementRule(FREQUENCIES,FrequencyModel.class),
+                new ElementRule(ROOT_FREQUENCIES,FrequencyModel.class)),
             new ElementRule(RATES,
                 new XMLSyntaxRule[]{
                     new ElementRule(Parameter.class, true)}
-            )
+            ),
+            new ElementRule(INDICATOR,
+                    new XMLSyntaxRule[]{
+                            new ElementRule(Parameter.class)
+                    },true),
     };
 }

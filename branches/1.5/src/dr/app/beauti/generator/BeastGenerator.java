ef6d25fbc17286d5f60fc1ca3ab37ebdd28bd6a3
dong.w.xie <dong.w.xie@ca793f91-a31e-0410-b540-2769d408b6a1>
2009-11-19 21:42:08 +0000

    BRANCH1.5: solve Issue 250: BEAUTi: wrong alignment blocks for multi-gene when choosing "create empty alignment"

--- branches/1.5/src/dr/app/beauti/generator/BeastGenerator.java
+++ branches/1.5/src/dr/app/beauti/generator/BeastGenerator.java
@@ -29,14 +29,11 @@ import dr.app.beast.BeastVersion;
 import dr.app.beauti.components.ComponentFactory;
 import dr.app.beauti.enumTypes.FixRateType;
 import dr.app.beauti.enumTypes.TreePriorType;
-import dr.app.beauti.enumTypes.BinaryModelType;
 import dr.app.beauti.options.*;
 import dr.app.beauti.util.XMLWriter;
 import dr.evolution.alignment.Alignment;
 import dr.evolution.alignment.SitePatterns;
-import dr.evolution.datatype.DataType;
 import dr.evolution.datatype.Nucleotides;
-import dr.evolution.datatype.TwoStateCovarion;
 import dr.evolution.util.Taxa;
 import dr.evolution.util.Taxon;
 import dr.evolution.util.TaxonList;
@@ -70,6 +67,7 @@ public class BeastGenerator extends Generator {
 
     private final static Version version = new BeastVersion();
 
+    private final AlignmentGenerator alignmentGenerator;
     private final TreePriorGenerator treePriorGenerator;
     private final TreeLikelihoodGenerator treeLikelihoodGenerator;
     private final SubstitutionModelGenerator substitutionModelGenerator;
@@ -85,6 +83,7 @@ public class BeastGenerator extends Generator {
     public BeastGenerator(BeautiOptions options, ComponentFactory[] components) {
         super(options, components);
 
+        alignmentGenerator = new AlignmentGenerator (options, components);   
         tmrcaStatisticsGenerator = new TMRCAStatisticsGenerator (options, components);                
         substitutionModelGenerator = new SubstitutionModelGenerator(options, components);
         treePriorGenerator = new TreePriorGenerator(options, components);
@@ -215,58 +214,22 @@ public class BeastGenerator extends Generator {
         if (taxonSets != null && taxonSets.size() > 0) {
             tmrcaStatisticsGenerator.writeTaxonSets(writer, taxonSets); 
         }
-
         generateInsertionPoint(ComponentGenerator.InsertionPoint.AFTER_TAXA, writer);
 
         //++++++++++++++++ Alignments ++++++++++++++++++
-        List<Alignment> alignments = new ArrayList<Alignment>();
+        alignmentGenerator.writeAlignments(writer);
+        generateInsertionPoint(ComponentGenerator.InsertionPoint.AFTER_SEQUENCES, writer);
 
-        for (PartitionData partition : options.dataPartitions) {
-            Alignment alignment = partition.getAlignment();
-            if (!alignments.contains(alignment)) {
-                alignments.add(alignment);
-            }
-        }
-
+        //++++++++++++++++ Pattern Lists ++++++++++++++++++
         if (!options.samplePriorOnly) {
-            int index = 1;
-            for (Alignment alignment : alignments) {
-                if (alignments.size() > 1) {
-                    //if (!options.allowDifferentTaxa) {
-                    alignment.setId(AlignmentParser.ALIGNMENT + index);
-                    //} else { // e.g. alignment_gene1
-                    // alignment.setId("alignment_" + mulitTaxaTagName + index);
-                    //}
-                } else {
-                    alignment.setId(AlignmentParser.ALIGNMENT);
-                }
-                writeAlignment(alignment, writer);
-                index += 1;
-                writer.writeText("");
-            }
-
-            generateInsertionPoint(ComponentGenerator.InsertionPoint.AFTER_SEQUENCES, writer);
-
-            //++++++++++++++++ Pattern Lists ++++++++++++++++++
-//            for (PartitionSubstitutionModel model : options.getPartitionSubstitutionModels()) {
-//                writePatternList(model, writer);
             for (PartitionData partition : options.dataPartitions) { // Each PD has one TreeLikelihood
                 writePatternList(partition, writer);
                 writer.writeText("");
             }
-
-            generateInsertionPoint(ComponentGenerator.InsertionPoint.AFTER_PATTERNS, writer);
-        } else {
-            Alignment alignment = alignments.get(0);
-            alignment.setId(AlignmentParser.ALIGNMENT);
-            writeAlignment(alignment, writer);
-            writer.writeText("");
-
-            generateInsertionPoint(ComponentGenerator.InsertionPoint.AFTER_SEQUENCES, writer);
-            generateInsertionPoint(ComponentGenerator.InsertionPoint.AFTER_PATTERNS, writer);
         }
+        generateInsertionPoint(ComponentGenerator.InsertionPoint.AFTER_PATTERNS, writer);
 
-        //++++++++++++++++ Tree Prior Model ++++++++++++++++++
+            //++++++++++++++++ Tree Prior Model ++++++++++++++++++
         for (PartitionTreePrior prior : options.getPartitionTreePriors()) {
             treePriorGenerator.writeTreePriorModel(prior, writer);
             writer.writeText("");
@@ -442,35 +405,6 @@ public class BeastGenerator extends Generator {
         writer.writeCloseTag(TaxaParser.TAXA);
     }
 
-
-    /**
-     * Determine and return the datatype description for these beast options
-     * note that the datatype in XML may differ from the actual datatype
-     *
-     * @param alignment the alignment to get data type description of
-     * @return description
-     */
-
-    private String getAlignmentDataTypeDescription(Alignment alignment) {
-        String description = alignment.getDataType().getDescription();
-
-        switch (alignment.getDataType().getType()) {
-            case DataType.TWO_STATES: // when dataType="binary"
-//            case DataType.COVARION:
-//                description = alignment.getDataType().getDescription();
-                
-                // if choose Covarion model then should change into dataType="twoStateCovarion"
-                if (options.getPartitionData(alignment).getPartitionSubstitutionModel().getBinarySubstitutionModel()
-                        == BinaryModelType.BIN_COVARION) {
-                    description = TwoStateCovarion.INSTANCE.getDescription(); // dataType="twoStateCovarion"
-                }
-                break;
-        }
-
-        return description;
-    }
-
-
     public void writeDifferentTaxaForMultiGene(PartitionData dataPartition, XMLWriter writer) {
         String data = dataPartition.getName();
         Alignment alignment = dataPartition.getAlignment();
@@ -484,45 +418,6 @@ public class BeastGenerator extends Generator {
         }
 
         writer.writeCloseTag(TaxaParser.TAXA);
-    }
-
-
-    /**
-     * Generate an alignment block from these beast options
-     *
-     * @param alignment the alignment to write
-     * @param writer    the writer
-     */
-    public void writeAlignment(Alignment alignment, XMLWriter writer) {
-
-        writer.writeText("");
-        writer.writeComment("The sequence alignment (each sequence refers to a taxon above).");
-        writer.writeComment("ntax=" + alignment.getTaxonCount() + " nchar=" + alignment.getSiteCount());
-        if (options.samplePriorOnly) {
-            writer.writeComment("Null sequences generated in order to sample from the prior only.");
-        }
-
-        writer.writeOpenTag(
-                AlignmentParser.ALIGNMENT,
-                new Attribute[]{
-                        new Attribute.Default<String>(XMLParser.ID, alignment.getId()),
-                        new Attribute.Default<String>("dataType", getAlignmentDataTypeDescription(alignment))
-                }
-        );
-
-        for (int i = 0; i < alignment.getTaxonCount(); i++) {
-            Taxon taxon = alignment.getTaxon(i);
-
-            writer.writeOpenTag(SequenceParser.SEQUENCE);
-            writer.writeIDref(TaxonParser.TAXON, taxon.getId());
-            if (!options.samplePriorOnly) {
-                writer.writeText(alignment.getAlignedSequenceString(i));
-            } else {
-                writer.writeText("N");
-            }
-            writer.writeCloseTag(SequenceParser.SEQUENCE);
-        }
-        writer.writeCloseTag(AlignmentParser.ALIGNMENT);
     }
 
     /**

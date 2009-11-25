d7f31d6aa8fbc0f5333d333f9e5222acf2b59f54
Marc Suchard <msuchard@ucla.edu>
2009-11-25 05:43:49 +0000

    Fixed MarkovJumps to work when mutation rate is specified in siteModel

---
+++ src/dr/inference/operators/DirtyLikelihoodOperator.java
@@ -0,0 +1,74 @@
+package dr.inference.operators;
+
+import dr.inference.model.Likelihood;
+import dr.xml.*;
+
+/**
+ * @author Marc Suchard
+ */
+public class DirtyLikelihoodOperator extends SimpleMCMCOperator implements GibbsOperator {
+
+    public static final String TOUCH_OPERATOR = "dirtyLikelihood";
+
+    public DirtyLikelihoodOperator(Likelihood likelihood, double weight) {
+        this.likelihood = likelihood;
+        setWeight(weight);
+    }
+
+    public String getPerformanceSuggestion() {
+        return null;
+    }
+
+    public String getOperatorName() {
+        return TOUCH_OPERATOR;
+    }
+
+    public double doOperation() throws OperatorFailedException {
+        likelihood.makeDirty();
+        return 0;
+    }
+
+    public int getStepCount() {
+        return 1;
+    }
+
+        public static dr.xml.XMLObjectParser PARSER = new dr.xml.AbstractXMLObjectParser() {
+
+        public String getParserName() {
+            return TOUCH_OPERATOR;
+        }
+
+        public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+
+            double weight = xo.getDoubleAttribute(WEIGHT);
+
+            Likelihood likelihood = (Likelihood) xo.getChild(Likelihood.class);
+
+            return new DirtyLikelihoodOperator(likelihood,weight);
+
+        }
+
+        //************************************************************************
+        // AbstractXMLObjectParser implementation
+        //************************************************************************
+
+        public String getParserDescription() {
+            return "This element returns a operator on the precision parameter of a normal model with gamma prior.";
+        }
+
+        public Class getReturnType() {
+            return MCMCOperator.class;
+        }
+
+        public XMLSyntaxRule[] getSyntaxRules() {
+            return rules;
+        }
+
+        private final XMLSyntaxRule[] rules = {
+                AttributeRule.newDoubleRule(WEIGHT),
+                new ElementRule(Likelihood.class),
+        };
+    };
+
+    private Likelihood likelihood;
+}

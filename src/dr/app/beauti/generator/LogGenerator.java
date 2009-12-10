974c833b39a7927ba0e79c40a42536eff8ac6a48
Walter <walterxie@users.noreply.github.com>
2009-12-10 03:49:34 +0000

    Trunk: merge BEAUti from branch 1.5 in

--- src/dr/app/beauti/generator/LogGenerator.java
+++ src/dr/app/beauti/generator/LogGenerator.java
@@ -36,7 +36,7 @@ import dr.evomodel.branchratemodel.BranchRateModel;
 import dr.evomodel.branchratemodel.StrictClockBranchRates;
 import dr.evomodel.clock.ACLikelihood;
 import dr.evomodel.coalescent.CoalescentLikelihood;
-import dr.evomodel.coalescent.GMRFFixedGridImportanceSampler;
+
 import dr.evomodel.speciation.MultiSpeciesCoalescent;
 import dr.evomodel.speciation.SpeciationLikelihood;
 import dr.evomodel.speciation.SpeciesTreeModel;
@@ -337,9 +337,9 @@ public class LogGenerator extends Generator {
         for (PartitionTreeModel tree : options.getPartitionTreeModels()) {
             String treeFileName;
             if (options.substTreeLog) {
-                treeFileName = options.fileNameStem + "." + tree.getPrefix() + "(time)." + GMRFFixedGridImportanceSampler.TREE_FILE_NAME;
+                treeFileName = options.fileNameStem + "." + tree.getPrefix() + "(time)." + STARBEASTOptions.TREE_FILE_NAME;
             } else {
-                treeFileName = options.fileNameStem + "." + tree.getPrefix() + GMRFFixedGridImportanceSampler.TREE_FILE_NAME; // stem.partitionName.tree
+                treeFileName = options.fileNameStem + "." + tree.getPrefix() + STARBEASTOptions.TREE_FILE_NAME; // stem.partitionName.tree
             }
 
             List<Attribute> attributes = new ArrayList<Attribute>();
@@ -416,7 +416,7 @@ public class LogGenerator extends Generator {
                                 new Attribute.Default<String>(TreeLoggerParser.LOG_EVERY, options.logEvery + ""),
                                 new Attribute.Default<String>(TreeLoggerParser.NEXUS_FORMAT, "true"),
                                 new Attribute.Default<String>(TreeLoggerParser.FILE_NAME, options.fileNameStem + "." + tree.getPrefix() +
-                                        "(subst)." + GMRFFixedGridImportanceSampler.TREE_FILE_NAME),
+                                        "(subst)." + STARBEASTOptions.TREE_FILE_NAME),
                                 new Attribute.Default<String>(TreeLoggerParser.BRANCH_LENGTHS, TreeLoggerParser.SUBSTITUTIONS)
                         });
                 writer.writeIDref(TreeModel.TREE_MODEL, tree.getPrefix() + TreeModel.TREE_MODEL);

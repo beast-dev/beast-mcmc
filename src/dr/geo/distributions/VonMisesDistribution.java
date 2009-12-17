74cd663326380cc2a5314f14596d521dfd07e8bc
Marc Suchard <msuchard@ucla.edu>
2009-12-17 18:10:00 +0000

    Finished refactoring spatial distributions and implemented an approximate diffusion on a sphere using the von Mises-Fisher distribution

--- src/dr/geo/distributions/VonMisesDistribution.java
+++ src/dr/geo/distributions/VonMisesDistribution.java
@@ -1,16 +1,19 @@
 package dr.geo.distributions;
 
+import cern.jet.math.Bessel;
+import dr.geo.math.Space;
+
 /**
  * @author Marc Suchard
  */
 public class VonMisesDistribution extends HyperSphereDistribution {
-
-    public VonMisesDistribution(int dim, double[] mean) {
-        super(dim, mean);
+        
+    public VonMisesDistribution(int dim, Space space, double[] mean, double kappa) {
+        super(dim, space, mean, kappa);
     }
 
     public double logPdf(double[] x) {
-        return 0;
+        return logPdf(x, mean, kappa, space);
     }
 
     public String getType() {
@@ -19,5 +22,21 @@ public class VonMisesDistribution extends HyperSphereDistribution {
 
     protected int getAllowableDim() {
         return 2;
+    }
+
+    private static double logNormalizationConstant(double kappa) {
+         return -LOG_2_PI - Math.log(Bessel.i0(kappa));
+    }
+
+    public static double logPdf(double[] x, double[] mean, double kappa, Space space) {
+        return logNormalizationConstant(kappa) + kappa * HyperSphereDistribution.innerProduct(x, mean, space);
+    }
+
+    public static void main(String[] arg) {        
+        // Test in radians
+        double kappa = 2;
+        double[] mean = { 1 };
+        double[] x = { -4 };
+        System.err.println("logP = "+logPdf(x, mean, kappa, Space.RADIANS)+" ?= -2.094546");
     }
 }

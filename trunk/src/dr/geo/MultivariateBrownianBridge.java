ba51adf314b27104fc01b0289a066c66baf8057f
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2010-01-28 23:08:15 +0000

    Refactoring multivariate normal distribution to support nice Brownian bridge features

--- trunk/src/dr/geo/MultivariateBrownianBridge.java
+++ trunk/src/dr/geo/MultivariateBrownianBridge.java
@@ -1,5 +1,7 @@
 package dr.geo;
 
+import dr.math.distributions.MultivariateNormalDistribution;
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,54 +14,70 @@ public class MultivariateBrownianBridge {
     /**
      * Divide and conquer brownian bridge
      *
+     * @param normal multivariate normal distribution characterizing Brownian bridge
      * @param start starting spacetime
      * @param end   ending spacetime
      * @param depth depth of divide and conquer recursion resulting in 2^(depth-1) new points
+     * @param rejector invalid space/time rejector
      * @return list of points, containing 2 + 2^(depth-1) points
      */
-    public static List<SpaceTime> divideConquerBrownianBridge(double[][] precision, SpaceTime start, SpaceTime end, int depth, SpaceTimeRejector rejector) {
+    public static List<SpaceTime> divideConquerBrownianBridge(MultivariateNormalDistribution normal, SpaceTime start, SpaceTime end, int depth, SpaceTimeRejector rejector) {
 
         List<SpaceTime> points = new LinkedList<SpaceTime>();
         points.add(start);
         points.add(end);
 
-        divideConquerBrownianBridge(precision, 0, points, depth, rejector);
+        divideConquerBrownianBridge(normal, 0, points, depth, rejector);
         return points;
     }
 
-    public static int divideConquerBrownianBridge(double[][] precision, int point0, List<SpaceTime> points, int depth, SpaceTimeRejector rejector) {
+    public static int divideConquerBrownianBridge(MultivariateNormalDistribution normal, int point0, List<SpaceTime> points, int depth, SpaceTimeRejector rejector) {
 
         if (depth > 0) {
 
-            SpaceTime p0 = points.get(point0);
-            SpaceTime p1 = points.get(point0 + 1);
+            SpaceTime pt0 = points.get(point0);
+            SpaceTime pt1 = points.get(point0 + 1);
 
-            double t0 = p0.getTime();
-            double[] x0 = p0.getX();
+            double t0 = pt0.getTime();
+            double[] x0 = pt0.getX();
 
-            double t1 = p1.getTime();
-            double[] x1 = p1.getX();
+            double t1 = pt1.getTime();
+            double[] x1 = pt1.getX();
 
-            double tm = (t1 - t0) / 2.0 + t0;
+            double tm = (t1 + t0) / 2.0;
 
-            double[] xm = new double[x0.length];
+            double t0m = tm - t1;
+            double p0m = 1.0 / t0m;
+
+            double tm1 = t1 - tm;
+            double pm1 = 1.0 / tm1;
+
+            // p0m + pm1 = precision of pt at tm,
+            double v01 = 1.0 / (p0m + pm1);
+
+            final int dim = x0.length;
+            double[] xm = new double[dim];
+            for (int i = 0; i < dim; i++) {
+                xm[i] = (p0m * x0[i] + pm1 * x1[i]) * v01; 
+            }
+
             SpaceTime s;
             do {
-                getBridgedX(tm, xm, precision, p0, p1);
-                s = new SpaceTime(tm, xm);
+//                getBridgedX(tm, xm, normal, pt0, pt1);
+                s = new SpaceTime(tm, normal.nextScaledMultivariateNormal(xm, v01));
             } while (rejector != null && rejector.reject(s));
 
             points.add(point0 + 1, s);
 
-            int endPoint = divideConquerBrownianBridge(precision, point0, points, depth - 1, rejector);
-            return divideConquerBrownianBridge(precision, endPoint, points, depth - 1, rejector);
+            int endPoint = divideConquerBrownianBridge(normal, point0, points, depth - 1, rejector);
+            return divideConquerBrownianBridge(normal, endPoint, points, depth - 1, rejector);
 
         } else return point0 + 1;
     }
 
     //  fill x by bridge between start and end
-    private static void getBridgedX(double t, double[] x, double[][] precision, SpaceTime start, SpaceTime end) {
-
-    }
+//    private static void getBridgedX(double t, double[] x, MultivariateNormalDistribution normal, SpaceTime start, SpaceTime end) {
+//
+//    }
 
 }

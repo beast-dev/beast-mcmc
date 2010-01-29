d4e65236a17b66a894a6f1ef63a2222da3f9a3db
Alexei Drummond <alexeid@users.noreply.github.com>
2010-01-29 03:41:38 +0000

    Alexei and Marc adding 2-dimensional brownian bridge with rejection-sampling on map of Europe :-)

--- src/dr/geo/SpaceTime.java
+++ src/dr/geo/SpaceTime.java
@@ -17,7 +17,20 @@ public class SpaceTime {
         return space;
     }
 
+    public double getX(int index) {
+        return space[index];
+    }
+
     public double getTime() {
         return time;
+    }
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(time);
+        for (double s : space) {
+            builder.append("\t").append(s);
+        }
+        return builder.toString();
     }
 }

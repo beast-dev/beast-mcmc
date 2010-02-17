6eeff3695602821470161d9f8bb34e205047fc7d
dong.w.xie <dong.w.xie@ca793f91-a31e-0410-b540-2769d408b6a1>
2010-02-17 02:56:28 +0000

    Trunk refactoring: complex substitution model (split parsers).

--- trunk/src/dr/evomodel/substmodel/ComplexSubstitutionModel.java
+++ trunk/src/dr/evomodel/substmodel/ComplexSubstitutionModel.java
@@ -7,17 +7,14 @@ import cern.colt.matrix.linalg.Algebra;
 import cern.colt.matrix.linalg.EigenvalueDecomposition;
 import cern.colt.matrix.linalg.Property;
 import dr.evolution.datatype.DataType;
-import dr.evoxml.DataTypeUtils;
 import dr.inference.loggers.LogColumn;
 import dr.inference.loggers.NumberColumn;
 import dr.inference.model.*;
 import dr.math.matrixAlgebra.Matrix;
 import dr.math.matrixAlgebra.RobustEigenDecomposition;
 import dr.math.matrixAlgebra.RobustSingularValueDecomposition;
-import dr.xml.*;
 
 import java.util.Arrays;
-import java.util.logging.Logger;
 
 /**
  * <b>A general irreversible class for any
@@ -27,18 +24,6 @@ import java.util.logging.Logger;
  */
 
 public class ComplexSubstitutionModel extends AbstractSubstitutionModel implements Likelihood {
-
-    public static final String COMPLEX_SUBSTITUTION_MODEL = "complexSubstitutionModel";
-    public static final String RATES = "rates";
-    public static final String ROOT_FREQUENCIES = "rootFrequencies";
-    public static final String INDICATOR = "rateIndicator";
-    public static final String RANDOMIZE = "randomizeIndicator";
-    public static final String NORMALIZATION = "normalize";
-    public static final String MAX_CONDITION_NUMBER = "maxConditionNumber";
-    public static final String CONNECTED = "mustBeConnected";
-    public static final String MAX_ITERATIONS = "maxIterations";
-    public static final String CHECK_CONDITIONING = "checkConditioning";
-
 
     public ComplexSubstitutionModel(String name, DataType dataType,
                                     FrequencyModel rootFreqModel, Parameter parameter) {
@@ -484,136 +469,6 @@ public class ComplexSubstitutionModel extends AbstractSubstitutionModel implemen
         }
         return mat;
     }
-
-    public static XMLObjectParser PARSER = new AbstractXMLObjectParser() {
-
-        public String[] getParserNames() {
-            return new String[] {
-                    getParserName(),"beast_"+getParserName()
-            };
-        }
-
-        public String getParserName() {
-            return COMPLEX_SUBSTITUTION_MODEL;
-        }
-
-        public Object parseXMLObject(XMLObject xo) throws XMLParseException {
-
-            DataType dataType = DataTypeUtils.getDataType(xo);
-
-            if (dataType == null) dataType = (DataType) xo.getChild(DataType.class);
-
-            XMLObject cxo = (XMLObject) xo.getChild(RATES);
-
-            Parameter ratesParameter = (Parameter) cxo.getChild(Parameter.class);
-
-            int rateCount = (dataType.getStateCount() - 1) * dataType.getStateCount();
-
-            if (ratesParameter.getDimension() != rateCount) {
-                throw new XMLParseException("Rates parameter in " + getParserName() + " element should have " + (rateCount) + " dimensions.  However parameter dimension is " + ratesParameter.getDimension());
-            }
-
-
-            cxo = xo.getChild(ROOT_FREQUENCIES);
-            FrequencyModel rootFreq = (FrequencyModel) cxo.getChild(FrequencyModel.class);
-
-            if (dataType != rootFreq.getDataType()) {
-                throw new XMLParseException("Data type of " + getParserName() + " element does not match that of its rootFrequencyModel.");
-            }
-
-            Parameter indicators = null;
-
-            if (xo.hasChildNamed(INDICATOR)) {
-                indicators = (Parameter) ((XMLObject) xo.getChild(INDICATOR)).getChild(Parameter.class);
-                if (ratesParameter.getDimension() != indicators.getDimension())
-                    throw new XMLParseException("Rate parameter dimension must match indicator parameter dimension");
-            }
-
-            StringBuffer sb = new StringBuffer()
-                    .append("Constructing a complex substitution model using\n")
-                    .append("\tRate parameters: " + ratesParameter.getId() + "\n")
-                    .append("\tRoot frequency model: " + rootFreq.getId() + "\n");
-
-            ComplexSubstitutionModel model;
-
-            if (indicators == null)
-                model = new ComplexSubstitutionModel(xo.getId(), dataType, rootFreq, ratesParameter);
-            else {
-
-                boolean randomize = xo.getAttribute(RANDOMIZE, false);
-                boolean connected = xo.getAttribute(CONNECTED, false);
-                model = new SVSComplexSubstitutionModel(xo.getId(), dataType, rootFreq, ratesParameter, indicators, connected);
-                if (randomize) {
-                    BayesianStochasticSearchVariableSelection.Utils.randomize(indicators,
-                            dataType.getStateCount(),false);
-                }
-                sb.append("\tBSSVS indicators: " + indicators.getId() + "\n");
-                sb.append("\tGraph must be connected: " + connected + "\n");
-            }
-
-            boolean doNormalization = xo.getAttribute(NORMALIZATION, true);
-            model.setNormalization(doNormalization);
-            sb.append("\tNormalized: " + doNormalization + "\n");
-
-            boolean checkConditioning = xo.getAttribute(CHECK_CONDITIONING, true);
-            model.setCheckConditioning(checkConditioning);
-
-            if (checkConditioning) {
-                double maxConditionNumber = xo.getAttribute(MAX_CONDITION_NUMBER, 1000);
-                model.setMaxConditionNumber(maxConditionNumber);
-                sb.append("\tMax. condition number: " + maxConditionNumber + "\n");
-            }
-
-            int maxIterations = xo.getAttribute(MAX_ITERATIONS, 1000);
-            model.setMaxIterations(maxIterations);
-            sb.append("\tMax iterations: " + maxIterations + "\n");
-
-            sb.append("Please cite Lemey, Rambaut, Drummond and Suchard (in submission)\n");
-
-            Logger.getLogger("dr.evomodel.substmodel").info(sb.toString());
-            return model;
-        }
-
-        //************************************************************************
-        // AbstractXMLObjectParser implementation
-        //************************************************************************
-
-        public String getParserDescription() {
-            return "A general reversible model of sequence substitution for any data type with stochastic variable selection.";
-        }
-
-        public Class getReturnType() {
-            return SubstitutionModel.class;
-        }
-
-        public XMLSyntaxRule[] getSyntaxRules() {
-            return rules;
-        }
-
-        private final XMLSyntaxRule[] rules = {
-                new XORRule(
-                        new StringAttributeRule(DataType.DATA_TYPE, "The type of sequence data",
-                                DataType.getRegisteredDataTypeNames(), false),
-                        new ElementRule(DataType.class)
-                ),
-                AttributeRule.newBooleanRule(RANDOMIZE, true),
-                new ElementRule(ROOT_FREQUENCIES, FrequencyModel.class),
-                new ElementRule(RATES,
-                        new XMLSyntaxRule[]{
-                                new ElementRule(Parameter.class)}
-                ),
-                new ElementRule(INDICATOR,
-                        new XMLSyntaxRule[]{
-                                new ElementRule(Parameter.class)
-                        }),
-                AttributeRule.newBooleanRule(NORMALIZATION, true),
-                AttributeRule.newDoubleRule(MAX_CONDITION_NUMBER, true),
-                AttributeRule.newBooleanRule(CONNECTED, true),
-                AttributeRule.newIntegerRule(MAX_ITERATIONS, true),
-                AttributeRule.newBooleanRule(CHECK_CONDITIONING, true),
-        };
-
-    };
 
     private boolean isComplex = false;
     private double[] stationaryDistribution = null;
---
+++ trunk/src/dr/inferencexml/MaskedParameterParser.java
@@ -0,0 +1,55 @@
+package dr.inferencexml;
+
+import dr.inference.model.MaskedParameter;
+import dr.inference.model.Parameter;
+import dr.xml.*;
+
+/**
+ */
+public class MaskedParameterParser extends AbstractXMLObjectParser {
+
+    public static final String MASKED_PARAMETER = "maskedParameter";
+    public static final String MASKING = "mask";
+    public static final String COMPLEMENT = "complement";
+
+    public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+
+        Parameter parameter = (Parameter) xo.getChild(Parameter.class);
+        XMLObject cxo = xo.getChild(MASKING);
+        Parameter mask = (Parameter) cxo.getChild(Parameter.class);
+
+        if (mask.getDimension() != parameter.getDimension())
+            throw new XMLParseException("dim(" + parameter.getId() + ") != dim(" + mask.getId() + ")");
+
+        MaskedParameter maskedParameter = new MaskedParameter(parameter);
+        boolean ones = !xo.getAttribute(COMPLEMENT, false);
+        maskedParameter.addMask(mask, ones);
+
+        return maskedParameter;
+    }
+
+    public XMLSyntaxRule[] getSyntaxRules() {
+        return rules;
+    }
+
+    private final XMLSyntaxRule[] rules = {
+            new ElementRule(Parameter.class),
+            new ElementRule(MASKING,
+                    new XMLSyntaxRule[]{
+                            new ElementRule(Parameter.class)
+                    }),
+            AttributeRule.newBooleanRule(COMPLEMENT, true),
+    };
+
+    public String getParserDescription() {
+        return "A masked parameter.";
+    }
+
+    public Class getReturnType() {
+        return Parameter.class;
+    }
+
+    public String getParserName() {
+        return MASKED_PARAMETER;
+    }
+}

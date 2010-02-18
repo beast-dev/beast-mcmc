a27bc7801450122ac447d024ecd16bd6a6bcdf9a
Walter <walterxie@users.noreply.github.com>
2010-02-18 22:08:23 +0000

    Trunk refactoring: 2nd part of coalescent (split parsers).

--- src/dr/evomodel/coalescent/CoalescentIntervalStatistic.java
+++ src/dr/evomodel/coalescent/CoalescentIntervalStatistic.java
@@ -1,18 +1,10 @@
 package dr.evomodel.coalescent;
 
 import dr.inference.model.Statistic;
-import dr.xml.AbstractXMLObjectParser;
-import dr.xml.ElementRule;
-import dr.xml.XMLObject;
-import dr.xml.XMLObjectParser;
-import dr.xml.XMLParseException;
-import dr.xml.XMLSyntaxRule;
 
 
 public class CoalescentIntervalStatistic extends Statistic.Abstract{
 
-	public static final String COALESCENT_INTERVAL_STATISTIC = "coalescentIntervalStatistic";
-	
 	private GMRFSkyrideLikelihood acl;
 	private int dimension;
 	
@@ -29,33 +21,5 @@ public class CoalescentIntervalStatistic extends Statistic.Abstract{
 	public double getStatisticValue(int dim) {
 			return acl.getCoalescentIntervalHeights()[dim];
 	}
-
-	public static XMLObjectParser PARSER = new AbstractXMLObjectParser(){
-
-		public String getParserDescription() {
-			return "";
-		}
-
-		public Class getReturnType() {
-			return CoalescentIntervalStatistic.class;
-		}
-
-		public XMLSyntaxRule[] getSyntaxRules() {
-			return new XMLSyntaxRule[]{
-					new ElementRule(GMRFSkyrideLikelihood.class),
-			};
-		}
-
-		public Object parseXMLObject(XMLObject xo) throws XMLParseException {
-			return new CoalescentIntervalStatistic(
-					(GMRFSkyrideLikelihood)xo.getChild(GMRFSkyrideLikelihood.class));
-		}
-
-		public String getParserName() {
-			return COALESCENT_INTERVAL_STATISTIC;
-		}
-		
-	};
-	
 
 }

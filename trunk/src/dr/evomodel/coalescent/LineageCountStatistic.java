8608012c990676dbd898867dcad67b8b55efe16b
dong.w.xie <dong.w.xie@ca793f91-a31e-0410-b540-2769d408b6a1>
2010-02-18 22:08:23 +0000

    Trunk refactoring: 2nd part of coalescent (split parsers).

--- trunk/src/dr/evomodel/coalescent/LineageCountStatistic.java
+++ trunk/src/dr/evomodel/coalescent/LineageCountStatistic.java
@@ -1,17 +1,8 @@
 package dr.evomodel.coalescent;
 
 import dr.inference.model.Statistic;
-import dr.xml.AbstractXMLObjectParser;
-import dr.xml.ElementRule;
-import dr.xml.XMLObject;
-import dr.xml.XMLObjectParser;
-import dr.xml.XMLParseException;
-import dr.xml.XMLSyntaxRule;
 
 public class LineageCountStatistic extends Statistic.Abstract{
-
-	public static final String LINEAGE_COUNT_STATISTIC = "lineageCountStatistic";
-	
 	private OldAbstractCoalescentLikelihood acl;
 	
 	public LineageCountStatistic(OldAbstractCoalescentLikelihood acl){
@@ -26,30 +17,4 @@ public class LineageCountStatistic extends Statistic.Abstract{
 			return acl.getLineageCount(dim);
 	}
 
-	public static XMLObjectParser PARSER = new AbstractXMLObjectParser(){
-
-		public String getParserDescription() {
-			return "";
-		}
-
-		public Class getReturnType() {
-			return LineageCountStatistic.class;
-		}
-
-		public XMLSyntaxRule[] getSyntaxRules() {
-			return new XMLSyntaxRule[]{
-					new ElementRule(OldAbstractCoalescentLikelihood.class),
-			};
-		}
-
-		public Object parseXMLObject(XMLObject xo) throws XMLParseException {
-			return new LineageCountStatistic(
-					(OldAbstractCoalescentLikelihood)xo.getChild(OldAbstractCoalescentLikelihood.class));
-		}
-
-		public String getParserName() {
-			return LINEAGE_COUNT_STATISTIC;
-		}
-		
-	};	
 }

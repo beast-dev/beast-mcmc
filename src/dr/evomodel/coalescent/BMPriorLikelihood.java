50c5768ad78bd72b90363f784224a7c9ea9e5156
dong.w.xie <dong.w.xie@ca793f91-a31e-0410-b540-2769d408b6a1>
2010-02-24 21:45:06 +0000

    Trunk refactoring: finish Likelihoods (split parsers)..

--- src/dr/evomodel/coalescent/BMPriorLikelihood.java
+++ src/dr/evomodel/coalescent/BMPriorLikelihood.java
@@ -1,11 +1,11 @@
 package dr.evomodel.coalescent;
 
+import dr.evomodelxml.coalescent.BMPriorLikelihoodParser;
 import dr.inference.distribution.ParametricDistributionModel;
 import dr.inference.model.AbstractModelLikelihood;
 import dr.inference.model.Model;
 import dr.inference.model.Parameter;
 import dr.inference.model.Variable;
-import dr.xml.*;
 
 /**
  * @author Joseph Heled
@@ -26,7 +26,7 @@ public class BMPriorLikelihood extends AbstractModelLikelihood {
     private final VariableDemographicModel m;
 
     private BMPriorLikelihood(Parameter sigma, boolean logSpace, VariableDemographicModel m) {
-        super(BM);
+        super(BMPriorLikelihoodParser.BM);
 
         this.logSpace = logSpace;
         //this.normalize = normalize;
@@ -150,84 +150,6 @@ public class BMPriorLikelihood extends AbstractModelLikelihood {
 
         lastValue = Double.NEGATIVE_INFINITY;
     }
-
-    static final String BM = "BrownianMotion";
-    static final String LOG_SPACE = "logUnits";
-    //static final String NORMALIZE = "normalize";
-
-    //static final String DATA = "data";
-    // static final String TIMES = "times";
-
-    // public static final String MEAN = "mean";
-    public static final String SIGMA = "sigma";
-    //  public static final String LAMBDA = "lambda";
-
-    // (todo) Parser is still in a bad state
-    public static XMLObjectParser PARSER = new AbstractXMLObjectParser() {
-
-        public String getParserDescription() {
-            return "";
-        }
-
-        public Class getReturnType() {
-            return BMPriorLikelihood.class;
-        }
-
-        public String getParserName() {
-            return BM;
-        }
-
-        private Parameter getParam(XMLObject xo, String name) throws XMLParseException {
-            final XMLObject object = xo.getChild(name);
-            // optional
-            if (object == null) {
-                return null;
-            }
-            final Object child = object.getChild(0);
-            if (child instanceof Parameter) {
-                return (Parameter) child;
-            }
-
-            double x = object.getDoubleChild(0);
-            return new Parameter.Default(x);
-        }
-
-        public Object parseXMLObject(XMLObject xo) throws XMLParseException {
-
-            // Parameter mean = getParam(xo, MEAN);
-            Parameter sigma = getParam(xo, SIGMA);
-            //  Parameter lambda = getParam(xo, LAMBDA);
-
-            final boolean logSpace = xo.getAttribute(LOG_SPACE, false);
-            //   final boolean normalize = xo.getAttribute(NORMALIZE, false);
-
-            VariableDemographicModel m = (VariableDemographicModel) xo.getChild(VariableDemographicModel.class);
-
-            ParametricDistributionModel popMeanPrior = (ParametricDistributionModel) xo.getChild(ParametricDistributionModel.class);
-            return new BMPriorLikelihood(sigma, m, logSpace, popMeanPrior);
-        }
-
-        public XMLSyntaxRule[] getSyntaxRules() {
-
-
-            return new XMLSyntaxRule[]{
-                    AttributeRule.newBooleanRule(LOG_SPACE, true),
-
-                    new XORRule(
-                            new ElementRule(SIGMA, Double.class),
-                            new ElementRule(SIGMA, Parameter.class)
-                    ),
-
-                    // you can't have a XOR (b AND c), yikes
-                    // make all optional and check in parser
-                    new ElementRule(VariableDemographicModel.class),
-
-//                    new ElementRule(DATA, new XMLSyntaxRule[]{new ElementRule(Statistic.class)}, true),
-//                    new ElementRule(TIMES, new XMLSyntaxRule[]{new ElementRule(Statistic.class)}, true)
-
-            };
-        }
-    };
 
     // simply saves last value
     double lastValue = Double.NEGATIVE_INFINITY;

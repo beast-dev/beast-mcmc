9eff656479fc04e610dd754ab88371a3799f5142
Marc Suchard <msuchard@ucla.edu>
2010-02-25 04:20:49 +0000

    Made some comments about handling missing trait values for the integrateTraitLikelihood

--- src/test/dr/evomodel/substmodel/ProductChainSubstitutionModelTest.java
+++ src/test/dr/evomodel/substmodel/ProductChainSubstitutionModelTest.java
@@ -2,7 +2,7 @@ package test.dr.evomodel.substmodel;
 
 import junit.framework.TestCase;
 import dr.app.beagle.evomodel.substmodel.*;
-import dr.evolution.datatype.Nucleotides;
+//import dr.evolution.datatype.Nucleotides;
 import dr.evolution.datatype.DataType;
 import dr.evolution.datatype.TwoStates;
 import dr.math.matrixAlgebra.Vector;
@@ -17,12 +17,6 @@ import java.util.Arrays;
  */
 
 public class ProductChainSubstitutionModelTest extends TestCase {
-
-    public void setUp() throws Exception {
-        super.setUp();
-//        setUpNucleotides();
-        setUpTwoStates();
-    }
 
     private void setUpTwoStates() {
         FrequencyModel freqModel0 = new FrequencyModel(TwoStates.INSTANCE,
@@ -79,35 +73,52 @@ public class ProductChainSubstitutionModelTest extends TestCase {
         }
     }
 
-    private void setUpNucleotides() {
+//    private void setUpNucleotides() {
+//
+//        FrequencyModel freqModel0 = new FrequencyModel(Nucleotides.INSTANCE,
+//                new double[]{0.25, 0.25, 0.25, 0.25});
+//        FrequencyModel freqModel1 = new FrequencyModel(Nucleotides.INSTANCE,
+//                new double[]{0.25, 0.25, 0.25, 0.25});
+//        FrequencyModel freqModel2 = new FrequencyModel(Nucleotides.INSTANCE,
+//                new double[]{0.25, 0.25, 0.25, 0.25});
+//
+//        HKY baseModel0 = new HKY(2.0, freqModel0);
+//        HKY baseModel1 = new HKY(2.0, freqModel1);
+//        HKY baseModel2 = new HKY(2.0, freqModel2);
+//
+//        baseModels = new ArrayList<SubstitutionModel>(3);
+//        baseModels.add(baseModel0);
+//        baseModels.add(baseModel1);
+//        baseModels.add(baseModel2);
+//
+//        List<DataType> dataTypes = new ArrayList<DataType>(3);
+//        dataTypes.add(Nucleotides.INSTANCE);
+//        dataTypes.add(Nucleotides.INSTANCE);
+//        dataTypes.add(Nucleotides.INSTANCE);
+//
+//        productChainModel = new ProductChainSubstitutionModel("productChain", dataTypes, baseModels);
+//        stateCount = 4 * 4 * 4;
+//
+//        /*
+//            hky0 = as.eigen(hky.model(2, 1, c(0.25,0.25, 0.25, 0.25), scale = T))
+//            hky1 = as.eigen(hky.model(2, 1, c(0.25,0.25, 0.25, 0.25), scale = T))
+//            hky2 = as.eigen(hky.model(2, 1, c(0.25,0.25, 0.25, 0.25), scale = T))
+//            pc = ind.codon.eigen(hky0, hky1, hky2)
+//         */
+//    }
 
-        FrequencyModel freqModel0 = new FrequencyModel(Nucleotides.INSTANCE,
-                new double[]{0.25, 0.25, 0.25, 0.25});
-        FrequencyModel freqModel1 = new FrequencyModel(Nucleotides.INSTANCE,
-                new double[]{0.25, 0.25, 0.25, 0.25});
-        FrequencyModel freqModel2 = new FrequencyModel(Nucleotides.INSTANCE,
-                new double[]{0.25, 0.25, 0.25, 0.25});
-
-        HKY baseModel0 = new HKY(2.0, freqModel0);
-        HKY baseModel1 = new HKY(2.0, freqModel1);
-        HKY baseModel2 = new HKY(2.0, freqModel2);
-
-        baseModels = new ArrayList<SubstitutionModel>(3);
-        baseModels.add(baseModel0);
-        baseModels.add(baseModel1);
-        baseModels.add(baseModel2);
-
-        List<DataType> dataTypes = new ArrayList<DataType>(3);
-        dataTypes.add(Nucleotides.INSTANCE);
-        dataTypes.add(Nucleotides.INSTANCE);
-        dataTypes.add(Nucleotides.INSTANCE);
-
-        productChainModel = new ProductChainSubstitutionModel("productChain", dataTypes, baseModels);
-        stateCount = 4 * 4 * 4;
+    public void testTwoStateProductChain() {
+        setUpTwoStates();
+        loop("TwoState");
     }
 
-    public void testInfinitesimalRateMatrices() {
-        System.out.println("Running testInfinitesimalRateMatrices...");
+//    public void testNucleotideProductChain() {
+//        setUpNucleotides();
+//        loop("Nucleotides");
+//    }
+
+    private void loop(String name) {
+        System.out.println("Running " + name + "...");
         int m = 0;
         for (SubstitutionModel substModel : baseModels) {
             double[] out = new double[

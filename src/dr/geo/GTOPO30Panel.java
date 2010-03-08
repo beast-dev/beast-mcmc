30b0380f49e6b57ffcb95d555cd03d943050f937
Alexei Drummond <alexeid@users.noreply.github.com>
2010-03-08 04:50:47 +0000

    Added elevation-based random walks on GTOPO30 maps

--- src/dr/geo/GTOPO30Panel.java
+++ src/dr/geo/GTOPO30Panel.java
@@ -26,6 +26,7 @@
 package dr.geo;
 
 import dr.gui.ColorFunction;
+import dr.math.distributions.GammaDistribution;
 
 import javax.swing.*;
 import java.awt.*;
@@ -36,7 +37,7 @@ import java.util.List;
 /**
  * @author Alexei Drummond
  */
-public class GTOPO30Panel extends JPanel {
+public class GTOPO30Panel extends JPanel implements Lattice {
 
     GTOPO30Tile[][] tiles;
 
@@ -93,18 +94,78 @@ public class GTOPO30Panel extends JPanel {
      * @return the height of the pixel at y,x
      */
     public short getHeight(int y, int x) {
-        int tileY = y / GTOPO30Tile.NCOLS;
-        int tileX = x / GTOPO30Tile.NROWS;
+        int tileY = y / GTOPO30Tile.NROWS;
+        int tileX = x / GTOPO30Tile.NCOLS;
 
-        int ty = y % GTOPO30Tile.NCOLS;
-        int tx = x % GTOPO30Tile.NROWS;
+        int ty = y % GTOPO30Tile.NROWS;
+        int tx = x % GTOPO30Tile.NCOLS;
 
-        return tiles[tileY][tileX].getHeight(ty, tx);
+
+        try {
+
+            short height = tiles[tileY][tileX].getHeight(ty, tx);
+            return height;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("Error at y=" + y + ", x=" + x);
+        }
+        return -1;
+    }
+
+    public Location getLocation(double latitude, double longitude) {
+        double relLat = tiles[0][0].getMaxLatitude() - latitude;
+        double relLong = longitude - tiles[0][0].getMinLongitude();
+
+        return new Location(
+                (int) Math.round(relLong / tiles[0][0].xdim),
+                (int) Math.round(relLat / tiles[0][0].ydim)
+        );
     }
 
     public void setScale(double scale) {
         this.scale = scale;
         setSize(getPreferredSize());
         repaint();
+    }
+
+    public int latticeWidth() {
+        return tiles[0].length * GTOPO30Tile.NCOLS;
+    }
+
+    public int latticeHeight() {
+        return tiles.length * GTOPO30Tile.NROWS;
+    }
+
+    public int getState(int i, int j) {
+        return getHeight(j, i) / 100;
+    }
+
+    public void paintLattice(Graphics g) {
+        paint(g);
+    }
+
+    public RateMatrix getRates(double mu) {
+        final double[] toRate = new double[52];
+        for (int j = 0; j < toRate.length; j++) {
+
+            int heightStart = j * 100;
+            int heightEnd = heightStart + 100;
+
+            double shape = 0.55;
+            double scale = 5800.0;
+
+            toRate[j] = mu *
+                    (GammaDistribution.cdf(heightEnd, shape, scale) -
+                            GammaDistribution.cdf(heightStart, shape, scale));
+
+            System.out.println("toRate[" + j + "] = " + toRate[j]);
+        }
+
+        return new RateMatrix() {
+
+            public double getRate(int i, int j) {
+                if (j < 0 || j >= toRate.length) return 0;
+                return toRate[j];
+            }
+        };
     }
 }
---
+++ src/dr/geo/RateMatrix.java
@@ -0,0 +1,9 @@
+package dr.geo;
+
+/**
+ * @author Alexei Drummond
+ */
+public interface RateMatrix {
+
+    public double getRate(int i, int j);
+}

0ef983eac68a0292327c4eb2a6e61a2f3baf0959
dong.w.xie <dong.w.xie@ca793f91-a31e-0410-b540-2769d408b6a1>
2010-03-15 03:34:23 +0000

    TRUNK: time irreversible test (not JUnit).

--- trunk/src/test/dr/integration/TimeIrreversibleTest.java
+++ trunk/src/test/dr/integration/TimeIrreversibleTest.java
@@ -9,13 +9,18 @@ import junit.framework.TestCase;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
  *
  */
 public class TimeIrreversibleTest extends TestCase {
-    private static NumberFormat formatter = new DecimalFormat("###0.000");
+    private static final double time = 0.01;
+
+    private static NumberFormat formatter = new DecimalFormat("###0.000000");
+
+    private static ArrayList<Double> ratioSummary = new ArrayList<Double> ();
 
     static class Original {
         public double[] getRates() {
@@ -69,26 +74,30 @@ public class TimeIrreversibleTest extends TestCase {
     class Test extends Original {
         private final double x;
 
-        public Test (double x) {
+        public Test(double x) {
             this.x = x;
         }
 
-        public double[] getRates() {
+        public double[] getRates(int id) {
             double[] originalRates = super.getRates();
-            System.out.println("original rates:"); 
-            printArrayMatrix(originalRates, getDataType().getStateCount(), getDataType().getStateCount());
+            System.out.println("original rates:");
+            printRateMatrix(originalRates, getDataType().getStateCount());
             double[] newRates = new double[originalRates.length];
 
             double[] uniform = new double[originalRates.length];
 
             for (int r = 0; r < originalRates.length; r++) {
-                uniform[r] = (new Random()).nextDouble() * ((1/x) - x) + x;
-                newRates[r] = originalRates[r] * uniform[r];
+                if (r == id) {
+                    uniform[r] = (new Random()).nextDouble() * ((1 / x) - x) + x;
+                    newRates[r] = originalRates[r] * uniform[r];
+                } else {
+                    newRates[r] = originalRates[r];
+                }
             }
             System.out.println("random ratio:");
-            printArrayMatrix(uniform, getDataType().getStateCount(), getDataType().getStateCount());
+            printRateMatrix(uniform, getDataType().getStateCount());
             System.out.println("new rates:");
-            printArrayMatrix(newRates, getDataType().getStateCount(), getDataType().getStateCount());
+            printRateMatrix(newRates, getDataType().getStateCount());
             return newRates;
         }
 
@@ -99,18 +108,48 @@ public class TimeIrreversibleTest extends TestCase {
 
     public void tests() {
         Original originalTest = new Original();
-        testComplexSubstitutionModel(originalTest);
-        testSVSComplexSubstitutionModel(originalTest);
+        double[] csm_orig = testComplexSubstitutionModel(originalTest, originalTest.getRates());
+        double[] svs_orig = testSVSComplexSubstitutionModel(originalTest, originalTest.getRates());
 
         Test test = new Test(0.8);
-        testComplexSubstitutionModel(test);
-        testSVSComplexSubstitutionModel(test);
+        for (int r = 0; r < test.getRates().length; r++) {
+            System.out.println("==================== changing index = " + r + " (start from 0) ====================");
+
+            double[] newRate = test.getRates(r);
+            double[] csm_test = testComplexSubstitutionModel(test, newRate);
+            reportMatrix(csm_orig, csm_test);
+
+            double[] svs_test = testSVSComplexSubstitutionModel(test, newRate);
+            reportMatrix(svs_orig, svs_test);
+        }
+
+        System.out.println("==================== Biggest Ratio Summary ====================\n");
+        int i = 1;
+        double bigget = 0;
+        int biggetId = 0;
+
+        for (Double r : ratioSummary) {
+            if (i % 2 != 0) {
+                System.out.print(i/2 + "   ");
+            }
+            System.out.print(formatter.format(r) + ",  ");
+            if (bigget < r) {
+                bigget = r;
+                biggetId = i;
+            }
+            if (i % 2 == 0) {
+                System.out.println("");
+            }
+
+            i++;
+        }
+
+        System.out.println("bigget = " + formatter.format(bigget) + ", where index is " + biggetId/2);
     }
 
-    private void testComplexSubstitutionModel(Original test) {
-        System.out.println("Complex Substitution Model Test: " + test);
-                
-        double[] rates = test.getRates();
+    private double[] testComplexSubstitutionModel(Original test, double[] rates) {
+        System.out.println("\n*** Complex Substitution Model Test: " + test + " ***");
+
         Parameter ratesP = new Parameter.Default(rates);
 
         DataType dataType = test.getDataType();
@@ -125,21 +164,20 @@ public class TimeIrreversibleTest extends TestCase {
 
         System.out.println("Prior = " + logL);
 
+        double[] finiteTimeProbs = null;
         if (!Double.isInfinite(logL)) {
-            double[] finiteTimeProbs = new double[substModel.getDataType().getStateCount() * substModel.getDataType().getStateCount()];
-            double time = 1.0;
+            finiteTimeProbs = new double[substModel.getDataType().getStateCount() * substModel.getDataType().getStateCount()];
             substModel.getTransitionProbabilities(time, finiteTimeProbs);
             System.out.println("Probs = ");
-            printArrayMatrix(finiteTimeProbs, substModel.getDataType().getStateCount(), substModel.getDataType().getStateCount());
+            printRateMatrix(finiteTimeProbs, substModel.getDataType().getStateCount());
         }
 //            assertEquals(1, 1, 1e-10);
-
+        return finiteTimeProbs;
     }
 
-    private void testSVSComplexSubstitutionModel(Original test) {
-        System.out.println("SVS Complex Substitution Model Test: " + test);
-                
-        double[] rates = test.getRates();
+    private double[] testSVSComplexSubstitutionModel(Original test, double[] rates) {
+        System.out.println("\n*** SVS Complex Substitution Model Test: " + test + " ***");
+
         double[] indicators = test.getIndicators();
 
         Parameter ratesP = new Parameter.Default(rates);
@@ -158,39 +196,74 @@ public class TimeIrreversibleTest extends TestCase {
 
         System.out.println("Prior = " + logL);
 
+        double[] finiteTimeProbs = null;
         if (!Double.isInfinite(logL)) {
-            double[] finiteTimeProbs = new double[substModel.getDataType().getStateCount() * substModel.getDataType().getStateCount()];
-            double time = 1.0;
+            finiteTimeProbs = new double[substModel.getDataType().getStateCount() * substModel.getDataType().getStateCount()];
             substModel.getTransitionProbabilities(time, finiteTimeProbs);
             System.out.println("Probs = ");
-            printArrayMatrix(finiteTimeProbs, substModel.getDataType().getStateCount(), substModel.getDataType().getStateCount());
+            printRateMatrix(finiteTimeProbs, substModel.getDataType().getStateCount());
         }
 //            assertEquals(1, 1, 1e-10);
+        return finiteTimeProbs;
     }
 
-    public static void printArrayMatrix(double[] m, int a, int b) {
+    public static void printRateMatrix(double[] m, int a) {
+        int id = 0;
         for (int i = 0; i < a; i++) {
-           if (i == 0) {
-               System.out.print("/  ");
-           } else if (i == a - 1) {
-               System.out.print("\\  ");
-           } else {
-               System.out.print("|  ");
-           }
+            if (i == 0) {
+                System.out.print("/  ");
+            } else if (i == a - 1) {
+                System.out.print("\\  ");
+            } else {
+                System.out.print("|  ");
+            }
 
-           for (int j = 0; j < b; j++) {
-               System.out.print(formatter.format(m[i+j]) + "  ");
-           }
+            for (int j = 0; j < a; j++) {
+                if (i == j) {
+                    System.out.print("null");
+                    for (int n = 0; n < formatter.getMaximumFractionDigits(); n++) {
+                        System.out.print(" ");
+                    }
+                } else {
+                    System.out.print(formatter.format(m[id]) + "  ");
+                    id++;                                
+                }
+            }
 
-           if (i == 0) {
-               System.out.print("\\");
-           } else if (i == a - 1) {
-               System.out.print("/");
-           } else {
-               System.out.print("|");
-           }
-           System.out.println();
+            if (i == 0) {
+                System.out.print("\\");
+            } else if (i == a - 1) {
+                System.out.print("/");
+            } else {
+                System.out.print("|");
+            }
+
+            System.out.println();
         }
+        System.out.println("\n");
+    }
+
+    public static void reportMatrix(double[] orig, double[] test) {
+        double bigRatio = 0;
+        double ratio;
+        int index = -1;
+
+        if (orig.length != test.length)
+            System.err.println("Error : 2 matrix should have same length ! " + orig.length + " " + test.length);
+
+        for (int i = 0; i < orig.length; i++) {
+            ratio = Math.abs(orig[i] / test[i]);
+            if (bigRatio < ratio) {
+                bigRatio = ratio;
+                index = i;
+            }
+        }
+
+        ratioSummary.add(bigRatio);
+
+        System.out.println("Biggest Ratio = " + formatter.format(bigRatio) + ", between " + formatter.format(orig[index])
+                + " and " + formatter.format(test[index]));
+        System.out.println("index = " + index + " (start from 0)");
         System.out.println("\n");
     }
 }

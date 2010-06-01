76be2239210d3471db59fd32332306be40d15520
rambaut <rambaut@users.noreply.github.com>
2010-06-01 08:52:25 +0000

    Copied the new all-encompassing GeneralSubstitutionModelParser into dr.app.beagle.parsers to keep BEAST/BEAGLE XML compatibility.

--- src/dr/app/beagle/evomodel/parsers/GeneralSubstitutionModelParser.java
+++ src/dr/app/beagle/evomodel/parsers/GeneralSubstitutionModelParser.java
@@ -1,28 +1,27 @@
 package dr.app.beagle.evomodel.parsers;
 
-import dr.app.beagle.evomodel.substmodel.FrequencyModel;
-import dr.app.beagle.evomodel.substmodel.GeneralSubstitutionModel;
-import dr.app.beagle.evomodel.substmodel.SubstitutionModel;
-import dr.evolution.datatype.DataType;
-import dr.inference.model.Parameter;
 import dr.xml.*;
+import dr.inference.model.Parameter;
+import dr.evolution.datatype.DataType;
+import dr.evoxml.util.DataTypeUtils;
+import dr.app.beagle.evomodel.substmodel.*;
 
 import java.util.logging.Logger;
 
 /**
- * @author Marc Suchard
- * @author Andrew Rambaut
+ * Parses a GeneralSubstitutionModel or one of its more specific descendants.
  */
-
 public class GeneralSubstitutionModelParser extends AbstractXMLObjectParser {
 
-
     public static final String GENERAL_SUBSTITUTION_MODEL = "generalSubstitutionModel";
+    public static final String SVS_GENERAL_SUBSTITUTION_MODEL = "svsGeneralSubstitutionModel";
+
     public static final String DATA_TYPE = "dataType";
     public static final String RATES = "rates";
     public static final String RELATIVE_TO = "relativeTo";
     public static final String FREQUENCIES = "frequencies";
-
+    public static final String INDICATOR = "rateIndicator";
+    public static final String ROOT_FREQ = "rootFrequencies";
 
     public String getParserName() {
         return GENERAL_SUBSTITUTION_MODEL;
@@ -31,52 +30,106 @@ public class GeneralSubstitutionModelParser extends AbstractXMLObjectParser {
     public Object parseXMLObject(XMLObject xo) throws XMLParseException {
 
         Parameter ratesParameter;
+        Parameter indicatorParameter;
 
         XMLObject cxo = xo.getChild(FREQUENCIES);
         FrequencyModel freqModel = (FrequencyModel) cxo.getChild(FrequencyModel.class);
 
-        DataType dataType = freqModel.getDataType();
+        DataType dataType = DataTypeUtils.getDataType(xo);
+
+        if (dataType == null) dataType = (DataType) xo.getChild(DataType.class);
+
+//        if (xo.hasAttribute(DataType.DATA_TYPE)) {
+//            String dataTypeStr = xo.getStringAttribute(DataType.DATA_TYPE);
+//            if (dataTypeStr.equals(Nucleotides.DESCRIPTION)) {
+//                dataType = Nucleotides.INSTANCE;
+//            } else if (dataTypeStr.equals(AminoAcids.DESCRIPTION)) {
+//                dataType = AminoAcids.INSTANCE;
+//            } else if (dataTypeStr.equals(Codons.DESCRIPTION)) {
+//                dataType = Codons.UNIVERSAL;
+//            } else if (dataTypeStr.equals(TwoStates.DESCRIPTION)) {
+//                dataType = TwoStates.INSTANCE;
+//            }
+//        }
+
+//        if (dataType == null) dataType = freqModel.getDataType();
+
+        if (dataType != freqModel.getDataType()) {
+            throw new XMLParseException("Data type of " + getParserName() + " element does not match that of its frequencyModel.");
+        }
 
         cxo = xo.getChild(RATES);
-
-        int states = dataType.getStateCount();
-
-        Logger.getLogger("dr.app.beagle.evomodel").info("  General Substitution Model (stateCount=" + states + ")");
-
-        int relativeTo = cxo.getIntegerAttribute(RELATIVE_TO) - 1;
-        if (relativeTo < 0) {
-            throw new XMLParseException(RELATIVE_TO + " must be 1 or greater");
-        } else {
-            int t = relativeTo;
-            int s = states - 1;
-            int row = 0;
-            while (t >= s) {
-                t -= s;
-                s -= 1;
-                row += 1;
-            }
-            int col = t + row + 1;
-
-            Logger.getLogger("dr.app.beagle.evomodel").info("  Rates relative to "
-                    + dataType.getCode(row) + "<->" + dataType.getCode(col));
-        }
-
         ratesParameter = (Parameter) cxo.getChild(Parameter.class);
 
+        int states = dataType.getStateCount();
+        Logger.getLogger("dr.evomodel").info("  General Substitution Model (stateCount=" + states + ")");
         int rateCount = ((dataType.getStateCount() - 1) * dataType.getStateCount()) / 2;
 
-        if (ratesParameter == null) {
-
-            if (rateCount == 1) {
-                // simplest model for binary traits...
-            } else {
-                throw new XMLParseException("No rates parameter found in " + getParserName());
+        if (xo.hasChildNamed(INDICATOR)) {// has indicator
+            if (ratesParameter.getDimension() != rateCount) {
+                throw new XMLParseException("Rates parameter in " + getParserName() + " element should have " + (rateCount)
+                        + " dimensions.  However parameter dimension is " + ratesParameter.getDimension());
             }
-        } else if (ratesParameter.getDimension() != rateCount - 1) {
-            throw new XMLParseException("Rates parameter in " + getParserName() + " element should have " + (rateCount - 1) + " dimensions.");
-        }
 
-        return new GeneralSubstitutionModel(GENERAL_SUBSTITUTION_MODEL,dataType, freqModel, ratesParameter, relativeTo);
+            cxo = xo.getChild(INDICATOR);
+            indicatorParameter = (Parameter) cxo.getChild(Parameter.class);
+
+            if (indicatorParameter == null || ratesParameter == null || indicatorParameter.getDimension() != ratesParameter.getDimension())
+                throw new XMLParseException("Rates and indicator parameters in " + getParserName() + " element must be the same dimension.");
+
+            if (xo.hasChildNamed(ROOT_FREQ)) {
+                throw new XMLParseException("SVSIrreversibleSubstitutionModel is not implemented for BEAGLE.");
+
+//                cxo = xo.getChild(ROOT_FREQ);
+//                FrequencyModel rootFreq = (FrequencyModel) cxo.getChild(FrequencyModel.class);
+//
+//                if (dataType != rootFreq.getDataType()) {
+//                    throw new XMLParseException("Data type of " + getParserName() + " element does not match that of its rootFrequencyModel.");
+//                }
+//                Logger.getLogger("dr.evomodel").info("  SVS Irreversible Substitution Model is applied with "
+//                        + INDICATOR + " " + indicatorParameter.getParameterName() + " and " + ROOT_FREQ + " " + rootFreq.getId());
+//                return new SVSIrreversibleSubstitutionModel(dataType, freqModel, rootFreq, ratesParameter, indicatorParameter);
+            }
+            Logger.getLogger("dr.evomodel").info("  SVS General Substitution Model is applied with "
+                    + INDICATOR + " " + indicatorParameter.getParameterName());
+            return new SVSGeneralSubstitutionModel(SVS_GENERAL_SUBSTITUTION_MODEL, dataType, freqModel, ratesParameter, indicatorParameter);
+
+        } else {// no indicator
+            if (!cxo.hasAttribute(RELATIVE_TO)) {
+                throw new XMLParseException("The index of the implicit rate (value 1.0) that all other rates are relative to."
+                        + " In DNA this is usually G<->T (6)");
+            }
+            int relativeTo = cxo.getIntegerAttribute(RELATIVE_TO) - 1;
+            if (relativeTo < 0) {
+                throw new XMLParseException(RELATIVE_TO + " must be 1 or greater");
+            } else {
+                int t = relativeTo;
+                int s = states - 1;
+                int row = 0;
+                while (t >= s) {
+                    t -= s;
+                    s -= 1;
+                    row += 1;
+                }
+                int col = t + row + 1;
+
+                Logger.getLogger("dr.evomodel").info("  Rates relative to "
+                        + dataType.getCode(row) + "<->" + dataType.getCode(col));
+            }
+
+            if (ratesParameter == null) {
+                if (rateCount == 1) {
+                    // simplest model for binary traits...
+                } else {
+                    throw new XMLParseException("No rates parameter found in " + getParserName());
+                }
+            } else if (ratesParameter.getDimension() != (rateCount - 1)) {
+                throw new XMLParseException("Rates parameter in " + getParserName() + " element should have ("
+                        + rateCount  + "- 1) dimensions because one of dimensions is fixed.");
+            }
+
+            return new GeneralSubstitutionModel(GENERAL_SUBSTITUTION_MODEL, dataType, freqModel, ratesParameter, relativeTo);
+        }
     }
 
     //************************************************************************
@@ -88,7 +141,7 @@ public class GeneralSubstitutionModelParser extends AbstractXMLObjectParser {
     }
 
     public Class getReturnType() {
-        return SubstitutionModel.class;
+        return GeneralSubstitutionModelParser.class;
     }
 
     public XMLSyntaxRule[] getSyntaxRules() {
@@ -96,12 +149,23 @@ public class GeneralSubstitutionModelParser extends AbstractXMLObjectParser {
     }
 
     private final XMLSyntaxRule[] rules = {
+            new XORRule(
+                    new StringAttributeRule(DataType.DATA_TYPE, "The type of sequence data",
+                            DataType.getRegisteredDataTypeNames(), false),
+                    new ElementRule(DataType.class)
+            ),
             new ElementRule(FREQUENCIES, FrequencyModel.class),
             new ElementRule(RATES,
                     new XMLSyntaxRule[]{
-                            AttributeRule.newIntegerRule(RELATIVE_TO, false, "The index of the implicit rate (value 1.0) that all other rates are relative to. In DNA this is usually G<->T (6)"),
-                            new ElementRule(Parameter.class, true)}
-            )
+                            new ElementRule(Parameter.class)}
+            ),
+            new ElementRule(INDICATOR,
+                    new XMLSyntaxRule[]{
+                            new ElementRule(Parameter.class),
+                    }, true),
+            new ElementRule(ROOT_FREQ,
+                    new XMLSyntaxRule[]{
+                            new ElementRule(FrequencyModel.class)
+                    }, 0, 1)
     };
-
 }

332fe02c8b2240010747c020dd4e451199bee029
rambaut <rambaut@users.noreply.github.com>
2010-06-02 11:10:21 +0000

    In the interests of generalization, AncestralState has been improved to optionally take a trait name, renamed AncestralTrait, moved to dr.evomodel.tree. It has a parser synonym of AncestralState and defaults to getting the 'state' trait so it is backwards compatible with existing XML (namely testAncestralSequenceReconstuction.xml).

--- src/dr/evomodel/tree/AncestralTrait.java
+++ src/dr/evomodel/tree/AncestralTrait.java
@@ -23,7 +23,7 @@
  * Boston, MA  02110-1301  USA
  */
 
-package dr.evomodel.treelikelihood;
+package dr.evomodel.tree;
 
 import dr.evolution.tree.NodeRef;
 import dr.evolution.tree.Tree;
@@ -35,18 +35,18 @@ import dr.inference.loggers.Loggable;
 import java.util.Set;
 
 /**
- * A statistic that logs the inferred state at the MRCA of a set of taxa
+ * A statistic that logs the inferred trait at the MRCA of a set of taxa
  *
  * @author Alexei Drummond
  * @author Andrew Rambaut
  * @version $Id: TMRCAStatistic.java,v 1.21 2005/07/11 14:06:25 rambaut Exp $
  */
-public class AncestralState implements Loggable {
+public class AncestralTrait implements Loggable {
 
-    public AncestralState(String name, TreeTrait ancestralState, Tree tree, TaxonList taxa) throws Tree.MissingTaxonException {
+    public AncestralTrait(String name, TreeTrait ancestralTrait, Tree tree, TaxonList taxa) throws Tree.MissingTaxonException {
         this.name = name;
         this.tree = tree;
-        this.ancestralState = ancestralState;
+        this.ancestralTrait = ancestralTrait;
         this.leafSet = Tree.Utils.getLeavesForTaxa(tree, taxa);
     }
 
@@ -61,7 +61,7 @@ public class AncestralState implements Loggable {
 
         NodeRef node = Tree.Utils.getCommonAncestorNode(tree, leafSet);
         if (node == null) throw new RuntimeException("No node found that is MRCA of " + leafSet);
-        return ancestralState.getTraitString(tree, node)[0];
+        return ancestralTrait.getTraitString(tree, node)[0];
     }
 
     // **************************************************************
@@ -83,7 +83,7 @@ public class AncestralState implements Loggable {
     }
 
     private final Tree tree;
-    private final TreeTrait ancestralState;
+    private final TreeTrait ancestralTrait;
     private final String name;
     private Set<String> leafSet = null;
 

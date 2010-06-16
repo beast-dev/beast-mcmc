5cc1961ae61091fda8a41f6c1168f6dcecd48b52
Marc Suchard <msuchard@ucla.edu>
2010-06-16 01:54:49 +0000

    Add several wrappers to convert automatically between different reductions of TreeTraits (sum over tree, sum across sites, etc.).  One can even double-wrap to get sum over tree + sum across sites.   This should reduce substantial code-duplication and earn me a gold star.

--- src/dr/evolution/tree/TreeTrait.java
+++ src/dr/evolution/tree/TreeTrait.java
@@ -9,23 +9,27 @@ package dr.evolution.tree;
 public interface TreeTrait<T> {
     public enum Intent {
         NODE,
-        BRANCH
+        BRANCH,
+        WHOLE_TREE
     }
 
     /**
      * The human readable name of this trait
+     *
      * @return the name
      */
     String getTraitName();
 
     /**
      * Specifies whether this is a trait of the tree, the nodes or the branch
+     *
      * @return true if a branch property
      */
     Intent getIntent();
 
     /**
      * Return a class object for the trait
+     *
      * @return the class
      */
     Class getTraitClass();
@@ -34,6 +38,7 @@ public interface TreeTrait<T> {
      * Returns the trait values for the given node. If this is a branch trait then
      * it will be for the branch above the specified node (and may not be valid for
      * the root). The array will be the length returned by getDimension().
+     *
      * @param tree a reference to a tree
      * @param node a reference to a node
      * @return the trait value
@@ -42,6 +47,7 @@ public interface TreeTrait<T> {
 
     /**
      * Get a string representations of the trait value.
+     *
      * @param tree a reference to a tree
      * @param node a reference to a node
      * @return the trait string representation
@@ -58,7 +64,10 @@ public interface TreeTrait<T> {
         }
 
         public String getTraitString(Tree tree, NodeRef node) {
-            Double value = getTrait(tree, node);
+            return formatTrait(getTrait(tree, node));
+        }
+
+        public static String formatTrait(Double value) {
             if (value == null) {
                 return null;
             }
@@ -72,11 +81,14 @@ public interface TreeTrait<T> {
     public abstract class I implements TreeTrait<Integer> {
 
         public Class getTraitClass() {
-            return Double.class;
+            return Integer.class;
         }
 
         public String getTraitString(Tree tree, NodeRef node) {
-            Integer value = getTrait(tree, node);
+            return formatTrait(getTrait(tree, node));
+        }
+
+        public static String formatTrait(Integer value) {
             if (value == null) {
                 return null;
             }
@@ -108,7 +120,10 @@ public interface TreeTrait<T> {
         }
 
         public String getTraitString(Tree tree, NodeRef node) {
-            double[] values = getTrait(tree, node);
+            return formatTrait(getTrait(tree, node));
+        }
+
+        public static String formatTrait(double[] values) {
             if (values == null || values.length == 0) return null;
             if (values.length > 1) {
                 StringBuilder sb = new StringBuilder("{");
@@ -136,7 +151,10 @@ public interface TreeTrait<T> {
         }
 
         public String getTraitString(Tree tree, NodeRef node) {
-            int[] values = getTrait(tree, node);
+            return formatTrait(getTrait(tree, node));
+        }
+
+        public static String formatTrait(int[] values) {
             if (values == null || values.length == 0) return null;
             if (values.length > 1) {
                 StringBuilder sb = new StringBuilder("{");
@@ -149,10 +167,157 @@ public interface TreeTrait<T> {
 
                 return sb.toString();
             } else {
-                return Double.toString(values[0]);
+                return Integer.toString(values[0]);
             }
         }
     }
 
+    /**
+     * An abstract wrapper class that sums a TreeTrait<T> over the entire tree
+     */
+    public abstract class SumOverTree<T> implements TreeTrait<T> {
+
+        private static final String NAME_PREFIX = "";
+        private TreeTrait<T> base;
+
+        public SumOverTree(TreeTrait<T> base) {
+            this.base = base;
+        }
+
+        public String getTraitName() {
+            return NAME_PREFIX + base.getTraitName();
+        }
+
+        public Intent getIntent() {
+            return Intent.WHOLE_TREE;
+        }
+
+        public T getTrait(Tree tree, NodeRef node) {
+            T count = null;
+            for (int i = 0; i < tree.getNodeCount(); i++) {
+                addToMatrix(count, base.getTrait(tree, tree.getNode(i)));
+            }
+            return count;
+        }
+
+        protected abstract void addToMatrix(T total, T summant);
+    }
+
+    /**
+     * A wrapper class that sums a TreeTrait.DA over the entire tree
+     */
+    public class SumOverTreeDA extends SumOverTree<double[]> {
+
+        public SumOverTreeDA(TreeTrait<double[]> base) {
+            super(base);
+        }
+
+        public String getTraitString(Tree tree, NodeRef node) {
+            return DA.formatTrait(getTrait(tree, node));
+        }
+
+        public Class getTraitClass() {
+            return double[].class;
+        }
+
+        protected void addToMatrix(double[] total, double[] summant) {
+            if (summant == null) {
+                return;
+            }
+            final int length = summant.length;
+            if (total == null) {
+                total = new double[length];
+            }
+            for (int i = 0; i < length; i++) {
+                total[i] += summant[i];
+            }
+        }
+    }
+
+    /**
+     * A wrapper class that sums a TreeTrait.D over the entire tree
+     */
+    public class SumOverTreeD extends SumOverTree<Double> {
+
+        public SumOverTreeD(TreeTrait<Double> base) {
+            super(base);
+        }
+
+        public String getTraitString(Tree tree, NodeRef node) {
+            return D.formatTrait(getTrait(tree, node));
+        }
+
+        public Class getTraitClass() {
+            return int[].class;
+        }
+
+        protected void addToMatrix(Double total, Double summant) {
+            if (summant == null) {
+                return;
+            }
+            if (total == null) {
+                total = 0.0;
+            }
+            total += summant;
+        }
+    }
+
+    /**
+     * An abstract wrapper class that sums a TreeTrait.Array into a TreeTrait
+     */
+    public abstract class SumAcrossArray<T,TA> implements TreeTrait<T> {
+
+        private TreeTrait<TA> base;
+        public static final String NAME_PREFIX = "";
+
+        public SumAcrossArray(TreeTrait<TA> base) {
+            this.base = base;
+        }
+
+        public String getTraitName() {
+            return NAME_PREFIX + base.getTraitName();
+        }
+
+        public Intent getIntent() {
+            return base.getIntent();
+        }
+
+        public T getTrait(Tree tree, NodeRef node) {
+            TA values = base.getTrait(tree, node);
+            if (values == null) {
+                return null;
+            }
+            return reduce(values);
+
+        }
+
+        protected abstract T reduce(TA values);
+    }
+
+    /**
+     * A wrapper class that sums a TreeTrait.DA into a TreeTrait.D
+     */
+    public class SumAcrossArrayD extends SumAcrossArray<Double, double[]> {
+
+        public SumAcrossArrayD(TreeTrait<double[]> base) {
+            super(base);
+        }
+
+        public Class getTraitClass() {
+            return Double.class;
+        }
+
+        protected Double reduce(double[] values) {
+            double total = 0.0;
+            for (double value : values) {
+                total += value;
+            }
+            return total;
+        }
+
+        public String getTraitString(Tree tree, NodeRef node) {
+            return D.formatTrait(getTrait(tree, node));
+        }
+    }
 }
 

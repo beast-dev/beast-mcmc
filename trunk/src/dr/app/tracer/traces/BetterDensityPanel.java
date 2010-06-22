522529137bd36177d9d7805a1c16ce88a031c45e
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2010-06-22 04:09:21 +0000

    Started work on KDEs in Tracer

---
+++ trunk/src/dr/app/tracer/traces/BetterDensityPanel.java
@@ -0,0 +1,77 @@
+package dr.app.tracer.traces;
+
+import dr.gui.chart.*;
+import dr.inference.trace.Trace;
+import dr.inference.trace.TraceCorrelation;
+import dr.inference.trace.TraceList;
+import dr.stats.Variate;
+import org.virion.jam.framework.Exportable;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Marc A. Suchard
+ * @author Philippe Lemey
+ */
+public class BetterDensityPanel extends DensityPanel {
+
+    private JButton kdeSetupButton;
+    private KDESetupDialog kdeSetupDialog = null;
+      
+    public BetterDensityPanel(final JFrame frame) {
+        super(frame);
+    }
+
+    protected JToolBar setupToolBar(final JFrame frame) {
+        JToolBar toolBar = super.setupToolBar(frame);
+
+        kdeSetupButton = new JButton("KDE...");
+    
+                kdeSetupButton.putClientProperty(
+                "Quaqua.Button.style", "placard"
+        );
+        kdeSetupButton.setFont(UIManager.getFont("SmallSystemFont"));
+        toolBar.add(kdeSetupButton);
+
+        kdeSetupButton.addActionListener(
+                new java.awt.event.ActionListener() {
+                    public void actionPerformed(ActionEvent actionEvent) {
+                        if (kdeSetupDialog == null) {
+                            kdeSetupDialog = new KDESetupDialog(frame, true, false,
+                                    Axis.AT_MAJOR_TICK, Axis.AT_MAJOR_TICK, Axis.AT_ZERO, Axis.AT_MAJOR_TICK);
+                        }
+
+                        kdeSetupDialog.showDialog(traceChart);
+                        validate();
+                        repaint();
+                    }
+                }
+        );
+        
+        return toolBar;
+    }
+
+    protected Plot setupDoubleTrace(TraceList tl, int traceIndex, TraceCorrelation td) {
+        Double values[] = new Double[tl.getStateCount()];
+        tl.getValues(traceIndex, values);
+        boolean[] selected = new boolean[tl.getStateCount()];
+        tl.getSelected(traceIndex, selected);
+
+        Plot plot = new KDENumericalDensityPlot(Trace.arrayConvert(values, selected), minimumBins, td);
+
+        traceChart.setXAxis(false, new HashMap<Integer, String>());// make HashMap empty
+        chartPanel.setYAxisTitle("Density");
+
+        relativeDensityCheckBox.setVisible(true);
+        labelBins.setVisible(true);
+        binsCombo.setVisible(true);
+        return plot;
+    }
+}
+
+    
+

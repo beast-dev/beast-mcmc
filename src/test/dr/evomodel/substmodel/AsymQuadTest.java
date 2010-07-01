5c2791a3b3b9014a98e793525fa553320f1198a0
akaruiws <jessiewu@users.noreply.github.com>
2010-07-01 05:32:07 +0000

    Gibbs sampling for a single msat ancestral state.

--- src/test/dr/evomodel/substmodel/AsymQuadTest.java
+++ src/test/dr/evomodel/substmodel/AsymQuadTest.java
@@ -229,6 +229,12 @@ public class AsymQuadTest extends TestCase {
                 }
             }
 
+            for(int i = 0; i < microsat.getStateCount();i ++){
+                double[] rowTransitionProb = aqm.getRowTransitionProbabilities(test.getDistance(), i);
+                for(int j =0 ; j < microsat.getStateCount(); j++){
+                    assertEquals(result[i*microsat.getStateCount()+j], rowTransitionProb[j], 1e-10);
+                }
+            }
         }
     }
 }

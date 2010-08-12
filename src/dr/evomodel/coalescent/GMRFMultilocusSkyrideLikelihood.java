a18405d15bdbca9313541dbd0168cb78412db4fe
Marc Suchard <msuchard@ucla.edu>
2010-08-12 23:30:44 +0000

    Started work with Mandev on multilociGMRF

--- src/dr/evomodel/coalescent/GMRFMultilocusSkyrideLikelihood.java
+++ src/dr/evomodel/coalescent/GMRFMultilocusSkyrideLikelihood.java
@@ -41,6 +41,15 @@ public class GMRFMultilocusSkyrideLikelihood extends GMRFSkyrideLikelihood imple
         }
     }
 
+    protected int getCorrectFieldLength() {
+        // TODO Fix!
+        int tips = 0;
+        for (Tree tree : treeList) {
+            tips += tree.getExternalNodeCount();
+        }
+        return tips - treeList.size();
+    }
+
     protected void handleModelChangedEvent(Model model, Object object, int index) {
 
         if (model instanceof TreeModel) {
@@ -67,7 +76,166 @@ public class GMRFMultilocusSkyrideLikelihood extends GMRFSkyrideLikelihood imple
     }
 
     protected void setupSufficientStatistics() {
-        // TODO Much to do here
+
+
+//What we want:
+
+//-Matrix with number of lineages present in each tree during each interval
+//-length of each interval
+//-vector which has -1 if corresponding interval does not end with coalescent event, and otherwise has i, where i is the tree in which the coalescent event took place
+
+
+//Add the following method to TreeIntervals.java:
+
+//get time of node which marks start of interval
+//public double getIntervalTime(int i){
+//	return times[indices[i]];
+//}
+
+        System.err.println("Am I here?");
+
+        int numTrees = treeList.size();
+
+        int N = 0;
+
+        for (int i = 0; i < numTrees; i++) {
+            N = N + intervalsList.get(i).getIntervalCount();
+        }
+
+
+        int[][] lineageCounts = new int[numTrees][N];
+        double[] intervalLengths = new double[N];
+        int[] coalEvent = new int[N];
+
+        //vector keeping track of which intervals are next in each tree
+        //should be initialized to all zeros
+        int nextIntervals[] = new int[numTrees];
+
+        int treeFinished[] = new int[numTrees]; //vector indicating whether we have reached end of tree
+        int numTreesFinished = 0;
+
+
+        int index = 0;
+
+        int currentTree = 0;
+        double currentTime = intervalsList.get(0).getIntervalTime(0);
+        double propCurrent;
+
+        for (int i = 1; i < numTrees; i++) {
+            propCurrent = intervalsList.get(i).getIntervalTime(0);
+            if (propCurrent < currentTime) {
+                currentTree = i;
+                currentTime = propCurrent;
+            }
+        }
+
+
+        for (TreeIntervals treeInterval : intervalsList) {
+            System.err.println(treeInterval);
+        }
+
+
+        nextIntervals[currentTree] = nextIntervals[currentTree] + 1;
+
+        System.err.println("Start of while-loop");
+
+
+
+        while (numTreesFinished < numTrees) {
+
+            int nextTree = 0;
+            double nextTime = 0;
+            double length = 0;
+
+            while (nextTree < numTrees) {
+                if (treeFinished[nextTree] == 0) {
+                    nextTime = intervalsList.get(nextTree).getIntervalTime(nextIntervals[nextTree]);
+                    if (nextTime > currentTime) {  // to be sure there are no intervals of length zero
+                        length = nextTime - currentTime;
+                        break;
+                    }
+                } else {
+                    nextTree++;
+                }
+            }
+
+
+            double propNext;
+            int startIndex = nextTree + 1;   //for efficiency
+
+            for (int i = startIndex; i < numTrees; i++) {
+                if (treeFinished[i] == 0) {
+                    propNext = intervalsList.get(i).getIntervalTime(nextIntervals[i]);
+                    if ((propNext - currentTime) < length) {
+                        nextTree = i;
+                        nextTime = propNext;
+                        length = nextTime - currentTime;
+                    }
+                }
+            }
+
+
+            int tempNode;
+
+            for (int i = 0; i < numTrees; i++) {
+                if (treeFinished[i] == 0) {
+                    tempNode = nextIntervals[i] - 1;
+                    if (tempNode < 0) {
+                        lineageCounts[i][index] = 0;
+                    } else {
+                        lineageCounts[i][index] = intervalsList.get(i).getLineageCount(tempNode);
+                    }
+                }
+            }
+
+
+//what if coalescent events in different trees happen at the same time?
+            if (intervalsList.get(nextTree).getCoalescentEvents(nextIntervals[nextTree] - 1) > 0) {
+                coalEvent[index] = nextTree;
+            } else {
+                coalEvent[index] = -1;
+            }
+
+
+            intervalLengths[index] = length;
+
+            currentTime = nextTime;
+            currentTree = nextTree;
+
+            nextIntervals[currentTree] = nextIntervals[currentTree] + 1;
+
+            if (nextIntervals[currentTree] == intervalsList.get(currentTree).getIntervalCount()) {
+                treeFinished[currentTree] = 1;
+                numTreesFinished++;
+            }
+
+            index++;  //when the loop stops, index will have value equal to the total number of intervals
+
+
+        }
+
+         System.err.println("End of while-loop");
+
+
+        double[] sufficientStatistics = new double[index]; // should use smaller array.  count number of coalescent events
+
+        int k = 0;
+        double weight = 0;
+
+
+        //here i indexes intervals, j indexes trees
+
+        for (int i = 0; i < index; i++) {
+            for (int j = 0; j < numTrees; j++) {
+                weight = weight + lineageCounts[j][i] * (lineageCounts[j][i] - 1) * intervalLengths[i];
+            }
+            if (coalEvent[i] > -1) {
+                sufficientStatistics[k] = weight / 2;
+                k++;
+                weight = 0;
+            }
+
+        }
     }
 
     private List<Tree> treeList;

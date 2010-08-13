41ad2d6ff793a05d226258513f7b0abf364008fc
Walter <walterxie@users.noreply.github.com>
2010-08-13 03:50:30 +0000

    Trunk: solve Issue 356: Add instruction to show user how to load specified starting tree from BEAUti

--- src/dr/app/beauti/util/TextUtil.java
+++ src/dr/app/beauti/util/TextUtil.java
@@ -1,6 +1,8 @@
 package dr.app.beauti.util;
 
 import javax.swing.*;
+import javax.swing.text.Document;
+import javax.swing.text.html.HTMLEditorKit;
 import java.awt.*;
 import java.text.BreakIterator;
 import java.util.Locale;
@@ -11,6 +13,7 @@ import java.util.Locale;
 public class TextUtil {
 
     // auto wrap string by given a JComp and the length limit 
+
     public static String wrapText(String someText, JComponent jComp, int lenLimit) {
         BreakIterator iterator = BreakIterator.getWordInstance(Locale.getDefault());
         iterator.setText(someText);
@@ -22,8 +25,8 @@ public class TextUtil {
         String s = "<html>";
         int len = 0;
         while (end != BreakIterator.DONE) {
-            String word = someText.substring(start,end);
-            if( len + fm.stringWidth(word) > lenLimit) {
+            String word = someText.substring(start, end);
+            if (len + fm.stringWidth(word) > lenLimit) {
                 s += "<br> ";
                 len = fm.stringWidth(word);
             } else {
@@ -38,5 +41,19 @@ public class TextUtil {
         return s;
     }
 
+    public static JScrollPane createHTMLScrollPane(String text, Dimension dimension) {
+        JEditorPane jEditorPane = new JEditorPane();
+        jEditorPane.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(jEditorPane);
 
+        HTMLEditorKit kit = new HTMLEditorKit();
+        jEditorPane.setEditorKit(kit);
+        // create a document, set it on the jeditorpane, then add the html
+        Document doc = kit.createDefaultDocument();
+        jEditorPane.setDocument(doc);
+        jEditorPane.setText(text);
+        jEditorPane.setPreferredSize(dimension); // to make html auto wrap
+
+        return scrollPane;
+    }
 }

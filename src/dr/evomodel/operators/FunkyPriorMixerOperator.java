1386418832d7c59317bf4cfb300fa969ff388cac
Marc Suchard <msuchard@ucla.edu>
2010-08-16 16:54:18 +0000

    Placeholders for a new transition kernel to hopefully solve the uniform internal node prior issues

--- src/dr/evomodel/operators/FunkyPriorMixerOperator.java
+++ src/dr/evomodel/operators/FunkyPriorMixerOperator.java
@@ -1,6 +1,9 @@
 package dr.evomodel.operators;
 
+import dr.evolution.tree.NodeRef;
+import dr.evolution.tree.Tree;
 import dr.evomodel.tree.TreeModel;
+import dr.evomodelxml.operators.FunkyPriorMixerOperatorParser;
 import dr.inference.model.Parameter;
 import dr.inference.operators.*;
 import dr.inferencexml.operators.RandomWalkOperatorParser;
@@ -10,14 +13,16 @@ import dr.math.MathUtils;
  * @author Marc A. Suchard
  * @author John J. Welch
  */
-public class FunkyPriorMixerOperator extends AbstractCoercableOperator {
+public class FunkyPriorMixerOperator extends SimpleMCMCOperator //AbstractCoercableOperator
+        implements GibbsOperator {
 
     public FunkyPriorMixerOperator(TreeModel treeModel, double windowSize, RandomWalkOperator.BoundaryCondition bc,
                                    double weight, CoercionMode mode) {
-
-        super(mode);
+      
+//        super(mode);
         this.treeModel = treeModel;
-        this.parameter = treeModel.getRootHeightParameter(); // TODO Temporary hack so everything compiles
+//        this.parameter = treeModel.getRootHeightParameter();
+        this.parameter = null;
 //        this.parameter = parameter;
         this.windowSize = windowSize;
         this.condition = bc;
@@ -27,45 +32,112 @@ public class FunkyPriorMixerOperator extends AbstractCoercableOperator {
 
     public final double doOperation() throws OperatorFailedException {
 
-        // a random dimension to perturb
-        int index;
-        index = MathUtils.nextInt(parameter.getDimension());
+//        // a random dimension to perturb
+//        int index;
+//        index = MathUtils.nextInt(parameter.getDimension());
+//
+//        double newValue = parameter.getParameterValue(index) + ((2.0 * MathUtils.nextDouble() - 1.0) * windowSize);
+//
+////        treeModel.setNodeHeight(node, 0.0);
+////        treeModel.getNodeHeightLower(node);
+//
+//        double lower = parameter.getBounds().getLowerLimit(index);
+//        double upper = parameter.getBounds().getUpperLimit(index);
+//
+//        while (newValue < lower || newValue > upper) {
+//            if (newValue < lower) {
+//                if (condition == RandomWalkOperator.BoundaryCondition.reflecting) {
+//                    newValue = lower + (lower - newValue);
+//                } else {
+//                    throw new OperatorFailedException("proposed value outside boundaries");
+//                }
+//
+//            }
+//            if (newValue > upper) {
+//                if (condition == RandomWalkOperator.BoundaryCondition.reflecting) {
+//                    newValue = upper - (newValue - upper);
+//                } else {
+//                    throw new OperatorFailedException("proposed value outside boundaries");
+//                }
+//
+//            }
+//        }
+//
+//        parameter.setParameterValue(index, newValue);
 
-        double newValue = parameter.getParameterValue(index) + ((2.0 * MathUtils.nextDouble() - 1.0) * windowSize);
+        double[] minNodeHeights = new double[treeModel.getNodeCount()];
+        recursivelyFindNodeMinHeights(treeModel, treeModel.getRoot(), minNodeHeights);
 
-//        treeModel.setNodeHeight(node, 0.0); // TODO How get bounds and set heights properly
-//        treeModel.getNodeHeightLower(node);
+        logForwardDensity = new Double(0.0);
+        logBackwardDensity = new Double(0.0);
 
-        double lower = parameter.getBounds().getLowerLimit(index);
-        double upper = parameter.getBounds().getUpperLimit(index);
+        recursivelyDrawNodeHeights(treeModel, treeModel.getRoot(), 0.0, 0.0, minNodeHeights); //,
+                //logForwardDensity, logBackwardDensity);
 
-        while (newValue < lower || newValue > upper) {
-            if (newValue < lower) {
-                if (condition == RandomWalkOperator.BoundaryCondition.reflecting) {
-                    newValue = lower + (lower - newValue);
-                } else {
-                    throw new OperatorFailedException("proposed value outside boundaries");
-                }
+//        System.err.println("logFD = " + logForwardDensity);
+//        System.err.println("logBD = " + logBackwardDensity);
 
-            }
-            if (newValue > upper) {
-                if (condition == RandomWalkOperator.BoundaryCondition.reflecting) {
-                    newValue = upper - (newValue - upper);
-                } else {
-                    throw new OperatorFailedException("proposed value outside boundaries");
-                }
+        return 0.0; //logBackwardDensity - logForwardDensity;    // TODO Think: Is this a Gibbs operator?
+    }
 
-            }
+
+    private double recursivelyFindNodeMinHeights(Tree tree, NodeRef node, double[] minNodeHeights) {
+
+        // Post-order traversal
+        
+        double minHeight;
+
+        if (tree.isExternal(node))
+            minHeight = tree.getNodeHeight(node);
+        else {
+            double minHeightChild0 = recursivelyFindNodeMinHeights(tree, tree.getChild(node, 0), minNodeHeights);
+            double minHeightChild1 = recursivelyFindNodeMinHeights(tree, tree.getChild(node, 1), minNodeHeights);
+            minHeight = (minHeightChild0 > minHeightChild1) ? minHeightChild0 : minHeightChild1;
         }
 
-        parameter.setParameterValue(index, newValue);
+        minNodeHeights[node.getNumber()] = minHeight;
+        return minHeight;
+    }
 
-        return 0.0;
+    private void recursivelyDrawNodeHeights(TreeModel tree, NodeRef node,
+                                            double oldParentHeight, double newParentHeight, double[] minNodeHeights) {//,
+//                                            Double logForwardDensity, Double logBackwardDensity) {
+        // Pre-order traversal
+
+        if (tree.isExternal(node))
+            return;
+
+        double oldNodeHeight = tree.getNodeHeight(node);
+        double newNodeHeight = oldNodeHeight;
+
+        if (!tree.isRoot(node)) {
+
+
+            double minHeight = minNodeHeights[node.getNumber()];
+
+            double oldDiff = oldParentHeight - minHeight;
+            double newDiff = newParentHeight - minHeight;
+
+            newNodeHeight = MathUtils.nextDouble() * newDiff + minHeight; // Currently uniform
+            logForwardDensity -= Math.log(newDiff);
+            logBackwardDensity -= Math.log(oldDiff);
+
+//            System.err.println("inner logFD = " + logForwardDensity);
+
+            tree.setNodeHeight(node, newNodeHeight);            
+        }
+
+        recursivelyDrawNodeHeights(tree, tree.getChild(node, 0), oldNodeHeight, newNodeHeight, minNodeHeights); //,
+//                logForwardDensity, logBackwardDensity);
+        recursivelyDrawNodeHeights(tree, tree.getChild(node, 1), oldNodeHeight, newNodeHeight, minNodeHeights); //,
+//                logForwardDensity, logBackwardDensity);
+
     }
 
     //MCMCOperator INTERFACE
+
     public final String getOperatorName() {
-        return parameter.getParameterName();
+        return FunkyPriorMixerOperatorParser.FUNKY_OPERATOR;
     }
 
     public double getCoercableParameter() {
@@ -105,7 +177,8 @@ public class FunkyPriorMixerOperator extends AbstractCoercableOperator {
         double prob = MCMCOperator.Utils.getAcceptanceProbability(this);
         double targetProb = getTargetAcceptanceProbability();
 
-        double ws = OperatorUtils.optimizeWindowSize(windowSize, parameter.getParameterValue(0) * 2.0, prob, targetProb);
+//        double ws = OperatorUtils.optimizeWindowSize(windowSize, parameter.getParameterValue(0) * 2.0, prob, targetProb);
+        double ws = 2;
 
         if (prob < getMinimumGoodAcceptanceLevel()) {
             return "Try decreasing windowSize to about " + ws;
@@ -118,8 +191,19 @@ public class FunkyPriorMixerOperator extends AbstractCoercableOperator {
         return RandomWalkOperatorParser.RANDOM_WALK_OPERATOR + "(" + parameter.getParameterName() + ", " + windowSize + ", " + getWeight() + ")";
     }
 
+    public int getStepCount() {
+        return 0;
+    }
+
     private final TreeModel treeModel;
     private final Parameter parameter;
     private double windowSize;
     private final RandomWalkOperator.BoundaryCondition condition;
+
+    private Double logForwardDensity;
+    private Double logBackwardDensity;
+
+    /**
+     * @return the number of steps the operator performs in one go.
+     */
 }

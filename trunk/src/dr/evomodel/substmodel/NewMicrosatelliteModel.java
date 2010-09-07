17312041d225bbf6f532861eced8740ecaa5de1d
akaruiws <akaruiws@ca793f91-a31e-0410-b540-2769d408b6a1>
2010-09-07 21:52:55 +0000

    Added methods for computing the transition probabilities.

--- trunk/src/dr/evomodel/substmodel/NewMicrosatelliteModel.java
+++ trunk/src/dr/evomodel/substmodel/NewMicrosatelliteModel.java
@@ -3,6 +3,7 @@ package dr.evomodel.substmodel;
 import dr.inference.model.Likelihood;
 import dr.inference.model.Parameter;
 import dr.evolution.datatype.Microsatellite;
+import dr.math.ModifiedBesselFirstKind;
 
 /**
  * @author Chieh-Hsi Wu
@@ -15,8 +16,42 @@ public class NewMicrosatelliteModel extends AbstractSubstitutionModel {
 
     }
 
+    public void getTransitionProbabilities(double distance, double[] matrix){
+        int k = 0;
+        double[] rowSums = new double[stateCount];
+        for(int i = 0; i < stateCount; i ++){
+            for(int j = 0; j < stateCount; j++){
+                matrix[k] = Math.exp(-distance)* ModifiedBesselFirstKind.bessi(distance,Math.abs(i-j));
+                rowSums[i] += matrix[k];
+                k++;
+            }
+
+        }
+        k = 0;
+        for(int i = 0; i < stateCount; i ++){
+            for(int j = 0; j < stateCount; j++){
+                matrix[k] =  matrix[k]/rowSums[i];
+                k++;
+            }          
+        }
+    }
+
     protected void ratesChanged() {};
     protected void setupRelativeRates(){};
     protected void frequenciesChanged() {};
+
+    public static void main(String[] args){
+        Microsatellite msat = new Microsatellite(1,30);
+        NewMicrosatelliteModel nmsatModel = new NewMicrosatelliteModel(msat, null);
+        double[] probs = new double[msat.getStateCount()*msat.getStateCount()];
+        nmsatModel.getTransitionProbabilities(1.2,probs);
+        int k =0;
+        for(int i = 0; i < msat.getStateCount(); i++){
+            for(int j = 0; j < msat.getStateCount(); j++){
+                System.out.print(probs[k++]+" ");
+            }
+            System.out.println();
+        }
+    }
 
 }

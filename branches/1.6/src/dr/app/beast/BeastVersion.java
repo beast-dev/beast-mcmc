4c20fc9e9203648be4a0ade688380f3c13356330
dong.w.xie <dong.w.xie@ca793f91-a31e-0410-b540-2769d408b6a1>
2010-09-16 23:40:36 +0000

    BRANCH: update version build number

--- branches/1.6/src/dr/app/beast/BeastVersion.java
+++ branches/1.6/src/dr/app/beast/BeastVersion.java
@@ -53,7 +53,7 @@ public class BeastVersion implements Version {
      * this used to parse the CVS ID string but there is no equivalent
      * for SVN. We must increment this manually.
      */
-    private static final String BUILD_ID = "Build r3548";
+    private static final String BUILD_ID = "Build r3554";
 
     public String getVersion() {
         return VERSION;

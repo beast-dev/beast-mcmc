2922078778e7ee854fb40f43d28483dc3d8c9e08
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2010-09-27 21:02:15 +0000

    Getting aggressive with fixing zero-branch-lengths in starting trees; need comparison of why MutableTree.Utils is not working

--- trunk/src/dr/evoxml/UPGMATreeParser.java
+++ trunk/src/dr/evoxml/UPGMATreeParser.java
@@ -31,11 +31,15 @@ import dr.evolution.tree.NodeRef;
 import dr.evolution.tree.UPGMATree;
 import dr.evolution.util.TimeScale;
 import dr.evomodelxml.tree.TreeModelParser;
+import dr.math.MathUtils;
 import dr.xml.*;
+
+import java.util.logging.Logger;
 
 /**
  * @author Alexei Drummond
  * @author Andrew Rambaut
+ * @author Marc A. Suchard
  * @version $Id: UPGMATreeParser.java,v 1.6 2006/07/28 11:27:32 rambaut Exp $
  */
 public class UPGMATreeParser extends AbstractXMLObjectParser {
@@ -47,6 +51,7 @@ public class UPGMATreeParser extends AbstractXMLObjectParser {
     public static final String UPGMA_TREE = "upgmaTree";
     //public static final String DISTANCES = "distances";
     public static final String ROOT_HEIGHT = TreeModelParser.ROOT_HEIGHT;
+    public static final String RANDOMIZE = "nonzeroBranchLengths";
 
     public String getParserName() {
         return UPGMA_TREE;
@@ -145,9 +150,62 @@ public class UPGMATreeParser extends AbstractXMLObjectParser {
             }
         }
 
+        if (xo.getAttribute(RANDOMIZE, false)) {
+            shakeTree(tree);
+        }
+
 
         return tree;
     }
+
+    private boolean shakeNode(UPGMATree tree, NodeRef node) {
+        if (tree.isRoot(node) || tree.isExternal(node)) {
+            return false;
+        }
+
+        boolean shake = false;
+        if (tree.getBranchLength(node) <= tolerance) {
+            shake = true;
+        }
+        double maxHeight = tree.getNodeHeight(tree.getParent(node));
+        double minHeight = Double.NEGATIVE_INFINITY;
+        for (int i = 0; i < tree.getChildCount(node); i++) {
+            NodeRef child = tree.getChild(node, i);
+            if (tree.getBranchLength(child) <= tolerance) {
+                shake = true;
+            }
+            double thisHeight = tree.getNodeHeight(child);
+            if (thisHeight > minHeight) {
+                minHeight = thisHeight;
+            }
+        }
+        if (shake) {
+            double draw = minHeight + (maxHeight - minHeight) * MathUtils.nextDouble();
+            tree.setNodeHeight(node, draw);
+        }
+        return shake;
+    }
+
+    private void shakeTree(UPGMATree tree) {
+        boolean shake = true;
+        int[] permutation = new int[tree.getNodeCount()];
+        for (int i = 0; i < tree.getNodeCount(); i++) {
+            permutation[i] = i;
+        }
+        while (shake) {
+            Logger.getLogger("dr.evomodelxml").info("Adjusting heights in UPGMA tree");
+            MathUtils.permute(permutation);
+            shake = false;
+            for (int i = 0; i < tree.getNodeCount(); i++) {
+                NodeRef node = tree.getNode(permutation[i]);
+                if (shakeNode(tree, node)) {
+                    shake = true;
+                }
+            }
+        }
+    }
+
+    private static double tolerance = 0.0;
 
     //************************************************************************
     // AbstractXMLObjectParser implementation
@@ -160,6 +218,7 @@ public class UPGMATreeParser extends AbstractXMLObjectParser {
     private final XMLSyntaxRule[] rules = {
             AttributeRule.newBooleanRule(SimpleTreeParser.USING_DATES, true),
             AttributeRule.newDoubleRule(ROOT_HEIGHT, true),
+            AttributeRule.newBooleanRule(RANDOMIZE, true),
             new ElementRule(DistanceMatrix.class)
     };
 

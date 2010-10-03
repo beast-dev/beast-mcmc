820cdf070c524fcf2d2005a7bc1469769bf5bb2e
dong.w.xie <dong.w.xie@ca793f91-a31e-0410-b540-2769d408b6a1>
2010-10-03 22:56:06 +0000

    Trunk: @Override is JDK1.6 code.

--- src/dr/evoxml/LinkageConstraintsParser.java
+++ src/dr/evoxml/LinkageConstraintsParser.java
@@ -1,42 +1,32 @@
 package dr.evoxml;
 
-import java.util.ArrayList;
-
 import dr.evolution.LinkageConstraints;
 import dr.evolution.LinkedGroup;
-import dr.evolution.alignment.Alignment;
-import dr.evolution.tree.Tree;
-import dr.evolution.util.Taxa;
-import dr.evolution.util.Taxon;
-import dr.evolution.util.TaxonList;
-import dr.xml.AbstractXMLObjectParser;
-import dr.xml.ElementRule;
-import dr.xml.XMLObject;
-import dr.xml.XMLParseException;
-import dr.xml.XMLSyntaxRule;
-import dr.xml.XORRule;
+import dr.xml.*;
+
+import java.util.ArrayList;
 
 /**
  * @author Aaron Darling (koadman)
  */
 public class LinkageConstraintsParser extends AbstractXMLObjectParser {
 
-	@Override
+	
 	public String getParserDescription() {
 		return "Data representing metagenome reads that are linked by mate-pair, strobe, or other information";
 	}
 
-	@Override
+
 	public Class getReturnType() {
 		return LinkageConstraints.class;
 	}
 
-	@Override
+
 	public XMLSyntaxRule[] getSyntaxRules() {
 		return rules;
 	}
 
-	@Override
+
 	public Object parseXMLObject(XMLObject xo) throws XMLParseException {
     	ArrayList<LinkedGroup> groups = new ArrayList<LinkedGroup>();
         for (int i = 0; i < xo.getChildCount(); i++) {
@@ -49,7 +39,7 @@ public class LinkageConstraintsParser extends AbstractXMLObjectParser {
 		return lc;
 	}
 
-	@Override
+
 	public String getParserName() {
 		return "LinkageConstraints";
 	}

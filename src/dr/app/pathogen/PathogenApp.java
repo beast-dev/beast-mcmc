43d17d3d7acdf0af5a275d311b2116f5d24ea5f6
rambaut <rambaut@users.noreply.github.com>
2010-10-15 09:41:12 +0000

    Updated revision number of Pathogen

--- src/dr/app/pathogen/PathogenApp.java
+++ src/dr/app/pathogen/PathogenApp.java
@@ -39,7 +39,7 @@ import java.awt.*;
  */
 public class PathogenApp extends MultiDocApplication {
     private final static Version version = new Version() {
-        private static final String VERSION = "1.3";
+        private static final String VERSION = "1.4";
 
         public String getVersion() {
             return VERSION;
@@ -54,7 +54,7 @@ public class PathogenApp extends MultiDocApplication {
         }
 
         public String getBuildString() {
-            return "Build r2010";
+            return "Build r3498";
         }
 
         public String[] getCredits() {

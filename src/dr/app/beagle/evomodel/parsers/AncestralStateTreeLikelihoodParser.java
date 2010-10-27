009d70584ff51b224270d11b623945d66734565f
Marc Suchard <msuchard@ucla.edu>
2010-10-27 15:38:46 +0000

    Enabling tipBranchSubstitutionModel for ancestral state reconstruction

--- src/dr/app/beagle/evomodel/parsers/AncestralStateTreeLikelihoodParser.java
+++ src/dr/app/beagle/evomodel/parsers/AncestralStateTreeLikelihoodParser.java
@@ -77,6 +77,7 @@ public class AncestralStateTreeLikelihoodParser extends TreeLikelihoodParser {
             new ElementRule(PatternList.class),
             new ElementRule(TreeModel.class),
             new ElementRule(GammaSiteRateModel.class),
+            new ElementRule(BranchSubstitutionModel.class, true),     
             new ElementRule(BranchRateModel.class, true),
             new ElementRule(SubstitutionModel.class),
             AttributeRule.newStringRule(TreeLikelihoodParser.SCALING_SCHEME,true),

b5044c3ebced0d7804980f66f2da6daed171728c
rambaut <rambaut@ca793f91-a31e-0410-b540-2769d408b6a1>
2010-11-11 12:52:56 +0000

    Consolidated phylogeography command-line tools in one place.

--- trunk/src/dr/app/phylogeography/tools/ContinuousKML.java
+++ trunk/src/dr/app/phylogeography/tools/ContinuousKML.java
@@ -1,3 +1,28 @@
+/*
+ * ContinuousKML.java
+ *
+ * Copyright (C) 2002-2010 BEAST Development Team
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
 package dr.app.phylogeography.tools ;
 
 import dr.stats.DiscreteStatistics;
@@ -5,11 +30,9 @@ import jebl.evolution.trees.RootedTree;
 import jebl.evolution.graphs.Node;
 
 /**
- * Created by IntelliJ IDEA.
- * User: phil
- * Date: Feb 6, 2008
- * Time: 7:33:37 PM
- * To change this template use File | Settings | File Templates.
+ * @author Philippe Lemey
+ * @author Andrew Rambaut
+ * @author Marc A. Suchard
  */
 public class ContinuousKML {
 

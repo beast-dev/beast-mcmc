1b6cb60399869685c755e664dd0c6e208acf9206
rambaut <rambaut@ca793f91-a31e-0410-b540-2769d408b6a1>
2010-11-16 10:14:53 +0000

    Added an alternative Continous Tree KML generator (TreeKMLGenerator).

---
+++ trunk/src/dr/app/phylogeography/tools/kml/AnalysisType.java
@@ -0,0 +1,11 @@
+package dr.app.phylogeography.tools.kml;
+
+/**
+ * @author Andrew Rambaut
+ * @author Alexei Drummond
+ * @version $Id$
+ */
+public enum AnalysisType {
+    DISCRETE,
+    CONTINUOUS
+}

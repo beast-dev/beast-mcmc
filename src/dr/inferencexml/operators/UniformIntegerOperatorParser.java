7d43d4bf7c5d9bf78763e2dcfa14049f8502a703
dong.w.xie <dong.w.xie@ca793f91-a31e-0410-b540-2769d408b6a1>
2010-11-30 01:42:11 +0000

    Trunk: solve Issue 435: UniformIntegerOperator did not include upper in the value:
    int newValue = MathUtils.nextInt(upper - lower) + lower;
    Because nextInt(n) gives value [0, n-1], if upper = 2 then newValue cannot have 2.
    The correct code should be 
    int newValue = MathUtils.nextInt(upper - lower + 1) + lower;

--- src/dr/inferencexml/operators/UniformIntegerOperatorParser.java
+++ src/dr/inferencexml/operators/UniformIntegerOperatorParser.java
@@ -1,6 +1,7 @@
 package dr.inferencexml.operators;
 
 import dr.inference.model.Parameter;
+import dr.inference.model.Variable;
 import dr.inference.operators.MCMCOperator;
 import dr.inference.operators.UniformIntegerOperator;
 import dr.xml.*;
@@ -19,23 +20,27 @@ public class UniformIntegerOperatorParser extends AbstractXMLObjectParser {
 
         double weight = xo.getDoubleAttribute(MCMCOperator.WEIGHT);
 
-        Parameter parameter = (Parameter) xo.getChild(Parameter.class);
-
-        int lower = (int) (double) parameter.getBounds().getLowerLimit(0);
-        if (xo.hasAttribute("lower")) lower = xo.getIntegerAttribute("lower");
-
-        int upper = (int) (double) parameter.getBounds().getUpperLimit(0);
-        if (xo.hasAttribute("upper")) upper = xo.getIntegerAttribute("upper");
+        Variable parameter = (Variable) xo.getChild(Variable.class);
 
         int count = 1;
         if (xo.hasAttribute("count")) count = xo.getIntegerAttribute("count");
 
-        if (upper == lower || lower == (int) Double.NEGATIVE_INFINITY || upper == (int) Double.POSITIVE_INFINITY) {
-            throw new XMLParseException(this.getParserName() + " boundaries not found in parameter "
-                    + parameter.getParameterName() + " Use operator lower and upper !");
-        }
+        if (parameter instanceof Parameter) {
+            int lower = (int) (double) ((Parameter) parameter).getBounds().getLowerLimit(0);
+            if (xo.hasAttribute("lower")) lower = xo.getIntegerAttribute("lower");
 
-        return new UniformIntegerOperator(parameter, lower, upper, weight, count);
+            int upper = (int) (double) ((Parameter) parameter).getBounds().getUpperLimit(0);
+            if (xo.hasAttribute("upper")) upper = xo.getIntegerAttribute("upper");
+
+            if (upper == lower || lower == (int) Double.NEGATIVE_INFINITY || upper == (int) Double.POSITIVE_INFINITY) {
+                throw new XMLParseException(this.getParserName() + " boundaries not found in parameter "
+                        + parameter.getId() + " Use operator lower and upper !");
+            }
+
+            return new UniformIntegerOperator((Parameter) parameter, lower, upper, weight, count);
+        } else { // Variable<Integer>, Bounds.Staircase
+            return new UniformIntegerOperator(parameter, weight, count);
+        }
     }
 
     //************************************************************************
@@ -60,6 +65,6 @@ public class UniformIntegerOperatorParser extends AbstractXMLObjectParser {
             AttributeRule.newDoubleRule("upper", true),
             AttributeRule.newDoubleRule("lower", true),
             AttributeRule.newDoubleRule("count", true),
-            new ElementRule(Parameter.class)
+            new ElementRule(Variable.class)
     };
 }

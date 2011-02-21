ff2c20b2a75df24a8ea6f82dd2fc0893f4dd91f1
Walter <walterxie@users.noreply.github.com>
2011-02-21 00:20:06 +0000

    Trunk new Trace: update Find Conditional Posterior Distributions... jdialog and jpanel and jtable, but panelParent cannot be repainted.

--- src/dr/app/tracer/traces/FilterContinuousPanel.java
+++ src/dr/app/tracer/traces/FilterContinuousPanel.java
@@ -6,12 +6,13 @@ import java.awt.*;
 /**
  * @author Walter Xie
  */
-public class FilterContinuousPanel extends FilterPanel {
+public class FilterContinuousPanel extends FilterAbstractPanel {
     JTextField minField;
     JTextField maxField;
 
     FilterContinuousPanel(String[] minMax, String[] bound) {
-        setLayout(new GridLayout(2, 3, 1, 10)); // 2 by 3, gap 5 by 1
+        setLayout(new GridLayout(7, 1, 1, 10)); // 2 by 3, gap 5 by 1
+        setSize(new java.awt.Dimension(400, 300));
 
         if (bound == null) {
             bound = new String[2];
@@ -21,13 +22,15 @@ public class FilterContinuousPanel extends FilterPanel {
         minField.setColumns(20);
         add(new JLabel("Set Minimum for Selecting Values : "));
         add(minField);
-        add(new JLabel(", which should > " + minMax[0]));
+        add(new JLabel("which should > " + minMax[0]));
+
+        add(new JLabel(""));
 
         maxField = new JTextField(bound[1]);
         maxField.setColumns(20);
         add(new JLabel("Set Maximum for Selecting Values : "));
         add(maxField);
-        add(new JLabel(", which should < " + minMax[1]));
+        add(new JLabel("which should < " + minMax[1]));
     }
 
     public Object[] getSelectedValues() {

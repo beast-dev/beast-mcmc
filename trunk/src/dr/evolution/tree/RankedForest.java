0e54a32d313601b4eb6b601f4a6c7c2d2ae2bbe0
alexei.drummond <alexei.drummond@ca793f91-a31e-0410-b540-2769d408b6a1>
2011-03-12 03:39:47 +0000

    ranked forest

--- trunk/src/dr/evolution/tree/RankedForest.java
+++ trunk/src/dr/evolution/tree/RankedForest.java
@@ -1,7 +1,31 @@
+/*
+ * RankedForest.java
+ *
+ * Copyright (C) 2002-2011 Alexei Drummond and Andrew Rambaut
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * BEAST is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
 package dr.evolution.tree;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author Alexei Drummond
@@ -14,17 +38,24 @@ public interface RankedForest {
 
     public int rank();
 
-    public boolean compatible(List<RankedNode.Clade> constraints);
+    public boolean compatible(List<BitSet> constraints);
+
+    public Set<BitSet> clades();
+
+    public boolean isClear();
+
 
     public class Default implements RankedForest {
 
         List<RankedNode> nodes;
+        boolean clear;
 
-        public Default(int size) {
+        public Default(int size, boolean clear) {
             nodes = new ArrayList<RankedNode>(size);
             for (int i = size - 1; i >= 0; i--) {
                 nodes.add(new RankedNode(i, size));
             }
+            this.clear = clear;
         }
 
         public int getSize() {
@@ -35,8 +66,17 @@ public interface RankedForest {
             return nodes.get(0).rank;
         }
 
-        public boolean compatible(List<RankedNode.Clade> constraints) {
+        public boolean compatible(List<BitSet> constraints) {
             return true;
+        }
+
+        public Set<BitSet> clades() {
+            return Collections.emptySet();
+
+        }
+
+        public boolean isClear() {
+            return clear;
         }
 
         public List<RankedNode> getNodes() {
@@ -49,6 +89,8 @@ public interface RankedForest {
         RankedForest child;
         RankedNode parent;
         List<RankedNode> nodes;
+        HashSet<BitSet> clades;
+        boolean clear = false;
 
         public Parent(RankedForest child, RankedNode parent) {
             this.child = child;
@@ -58,6 +100,12 @@ public interface RankedForest {
             nodes.addAll(child.getNodes());
             nodes.remove(parent.child1);
             nodes.remove(parent.child2);
+
+            clades = new HashSet<BitSet>();
+            clades.addAll(child.clades());
+            clades.add(parent.cladeBits);
+
+            if (child.isClear()) clear = true;
         }
 
         public List<RankedNode> getNodes() {
@@ -72,20 +120,36 @@ public interface RankedForest {
             return nodes.get(0).rank;
         }
 
-        public boolean compatible(List<RankedNode.Clade> constraints) {
+        public boolean compatible(List<BitSet> constraints) {
+
+            if (constraints.size() == 1) return true;
+
             int rank = 0;
-            for (RankedNode.Clade constraint : constraints) {
+            for (BitSet constraint : constraints) {
                 int newRank = getRank(constraint);
                 if (newRank < rank) return false;
                 rank = newRank;
             }
+            if (rank != Integer.MAX_VALUE) setClear(true);
             return true;
         }
 
-        private int getRank(RankedNode.Clade constraint) {
+        public Set<BitSet> clades() {
+            return clades;
+        }
+
+        public void setClear(boolean clear) {
+            this.clear = clear;
+        }
+
+        public boolean isClear() {
+            return clear;
+        }
+
+        private int getRank(BitSet constraint) {
             //if (constraint.size() > parent.rank) return Integer.MAX_VALUE;
 
-            if (parent.clade.equals(constraint)) {
+            if (parent.cladeBits.equals(constraint)) {
                 return parent.rank;
             } else {
                 if (child instanceof Parent) {

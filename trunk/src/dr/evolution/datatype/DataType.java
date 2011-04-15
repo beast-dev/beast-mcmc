b73e58a7ede2f6f9cd1a94a3db102ddc831a146d
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2011-04-15 14:27:16 +0000

    Missing data registry entry for co-evolution amino acid sites

--- trunk/src/dr/evolution/datatype/DataType.java
+++ trunk/src/dr/evolution/datatype/DataType.java
@@ -47,6 +47,7 @@ public abstract class DataType
     public static final int GENERAL = 4;
     public static final int COVARION = 5;
     public static final int MICRO_SAT = 6;
+    public static final int P2PTYPE = 7;
 
     public static final char UNKNOWN_CHARACTER = '?';
     public static final char GAP_CHARACTER = '-';
@@ -88,6 +89,7 @@ public abstract class DataType
 	        registerDataType(HiddenCodons.DESCRIPTION+"3-"+ GeneticCode.UNIVERSAL.getName(), HiddenCodons.UNIVERSAL_HIDDEN_3);
             registerDataType(GeneralDataType.DESCRIPTION, GeneralDataType.INSTANCE);
             registerDataType(Microsatellite.DESCRIPTION, Microsatellite.INSTANCE);
+            registerDataType(P2P.DESCRIPTION, P2P.INSTANCE);
         }
     }
 

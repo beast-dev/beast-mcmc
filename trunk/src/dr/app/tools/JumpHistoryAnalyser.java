d8adadff32a6917aea8469e7314aafa1e1538e25
rambaut <rambaut@ca793f91-a31e-0410-b540-2769d408b6a1>
2011-06-02 11:12:10 +0000

    Finished up JumpHistoryAnalyser. Command line options allow you to limit the 'from' states and 'to' states to be summarized.

--- trunk/src/dr/app/tools/JumpHistoryAnalyser.java
+++ trunk/src/dr/app/tools/JumpHistoryAnalyser.java
@@ -25,11 +25,14 @@
 
 package dr.app.tools;
 
+import dr.app.util.Arguments;
 import dr.inference.trace.LogFileTraces;
 import dr.inference.trace.TraceDistribution;
 import dr.inference.trace.TraceException;
 import dr.inference.trace.TraceList;
 import dr.stats.Variate;
+import dr.util.DataTable;
+import jam.console.ConsoleApplication;
 import jebl.evolution.coalescent.IntervalList;
 import jebl.evolution.coalescent.Intervals;
 import jebl.evolution.io.ImportException;
@@ -38,13 +41,12 @@ import jebl.evolution.io.NexusImporter;
 import jebl.evolution.io.TreeImporter;
 import jebl.evolution.trees.RootedTree;
 
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -59,13 +61,7 @@ public class JumpHistoryAnalyser {
     private double maxTime;
     private double ageOfYoungest;
 
-    private Variate xData = new Variate.D();
-    private Variate yDataMean = new Variate.D();
-    private Variate yDataMedian = new Variate.D();
-    private Variate yDataUpper = new Variate.D();
-    private Variate yDataLower = new Variate.D();
-
-    public JumpHistoryAnalyser(File logFile, int burnin, int binCount, double minTime, double maxTime, double ageOfYoungest)
+    public JumpHistoryAnalyser(String inputFileName, Set<String> fromStates, Set<String> toStates, int burnin, int binCount, double minTime, double maxTime, double ageOfYoungest)
             throws IOException, ImportException, TraceException {
 
         this.binCount = binCount;
@@ -75,7 +71,7 @@ public class JumpHistoryAnalyser {
 
         double delta = (maxTime - minTime) / (binCount - 1);
 
-        BufferedReader reader = new BufferedReader(new FileReader(logFile));
+        BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
 
         String line = reader.readLine();
         while (line != null && line.startsWith("#")) {
@@ -84,6 +80,8 @@ public class JumpHistoryAnalyser {
 
         // read heading line
         line = reader.readLine();
+
+        int[] bins = new int[binCount];
 
         while (line != null) {
            String[] columns = line.split("\t");
@@ -97,8 +95,21 @@ public class JumpHistoryAnalyser {
                     while (matcher.find()) {
                         String fromState = matcher.group(1);
                         String toState = matcher.group(2);
-                        String time = matcher.group(3);
-                        System.out.println(fromState + " ->" + toState + ": " + time);
+                        String timeString = matcher.group(3);
+
+                        if ((fromStates.size() == 0 || fromStates.contains(fromState)) && (toStates.size() == 0 || toStates.contains(toState))) {
+                            double time = Double.parseDouble(timeString);
+                            time = ageOfYoungest - time;
+                            double binTime = minTime;
+                            int bin = 0;
+                            while (time > binTime) {
+                                binTime += delta;
+                                bin ++;
+                            }
+                            bins[bin] ++;
+                        }
+
+//                        System.out.println(fromState + " ->" + toState + ": " + timeString);
                     }
                 }
             }
@@ -108,44 +119,125 @@ public class JumpHistoryAnalyser {
 
         reader.close();
 
-//        Variate[] bins = new Variate[binCount];
-//        double height = 0.0;
-//
-//        for (int k = 0; k < binCount; k++) {
-//            bins[k] = new Variate.D();
-//            bins[k].add(popsize);
-//        }
-//
-//        double t = 0.0;
-//
-//        for (Variate bin : bins) {
-//            xData.add(t);
-//            if (bin.getCount() > 0) {
-//                yDataMean.add(bin.getMean());
-//                yDataMedian.add(bin.getQuantile(0.5));
-//                yDataLower.add(bin.getQuantile(0.025));
-//                yDataUpper.add(bin.getQuantile(0.975));
-//            } else {
-//                yDataMean.add(Double.NaN);
-//                yDataMedian.add(Double.NaN);
-//                yDataLower.add(Double.NaN);
-//                yDataUpper.add(Double.NaN);
-//            }
-//            t += delta;
-//        }
+        System.out.println("time\tcount");
+
+        double time = minTime;
+        for (int bin = 0; bin < binCount; bin++) {
+            System.out.println(time + "\t" + bins[bin]);
+            time += delta;
+        }
+    }
+
+    public static void printUsage(Arguments arguments) {
+
+        arguments.printUsage("jumphistoryanalyser", "<input-file-name> [<output-file-name>]");
+        System.out.println();
+        System.out.println("  Example: jumphistoryanalyser -burnin 100000 -min 1950 -max 2010 -mrsd 2009.9 jumps.txt");
+        System.out.println("  Example: jumphistoryanalyser -burnin 100000 -from \"usa\" -to \"uk,fr\" -min 1950 -max 2010 -mrsd 2009.9 jumps.txt");
+        System.out.println();
     }
 
 
-    public static void main(String[] argv) {
+    public static void main(String[] args) {
+        String inputFileName = null;
+
+
+//        printTitle();
+
+        Arguments arguments = new Arguments(
+                new Arguments.Option[]{
+                        new Arguments.IntegerOption("burnin", "the number of states to be considered as 'burn-in'"),
+                        new Arguments.StringOption("from", "from_states", "set of 'from' states to limit the history [default all states]"),
+                        new Arguments.StringOption("to", "to_states", "set of 'to' states to limit the history [default all states]"),
+                        new Arguments.IntegerOption("bins", "the number of discrete bins [default 100]"),
+                        new Arguments.RealOption("min", "the minimum bound of the time range"),
+                        new Arguments.RealOption("max", "the maximum bound of the time range"),
+                        new Arguments.RealOption("mrsd", "the date of the most recently sampled tip"),
+                        new Arguments.StringOption("to", "to_states", "set of 'to' states to limit the history [default all states]"),
+                        new Arguments.Option("help", "option to print this message"),
+                });
+
+        try {
+            arguments.parseArguments(args);
+        } catch (Arguments.ArgumentException ae) {
+            System.err.println(ae);
+            printUsage(arguments);
+            System.exit(1);
+        }
+
+        if (arguments.hasOption("help")) {
+            printUsage(arguments);
+            System.exit(0);
+        }
+
+        int burnin = -1;
+        if (arguments.hasOption("burnin")) {
+            burnin = arguments.getIntegerOption("burnin");
+        }
+
+        int binCount = 100;
+        if (arguments.hasOption("bins")) {
+            burnin = arguments.getIntegerOption("bins");
+        }
+
+        double minTime = arguments.getRealOption("min");
+        double maxTime = arguments.getRealOption("max");
+
+        if (minTime >= maxTime) {
+            System.err.println("The minimum time must be less than the maximum time");
+            printUsage(arguments);
+            System.exit(1);
+        }
+        double mrsd = arguments.getRealOption("mrsd");
+
+        Set<String> fromStates = new HashSet<String>();
+        Set<String> toStates = new HashSet<String>();
+
+        if (arguments.hasOption("from")) {
+            String stateString = arguments.getStringOption("from");
+            String[] states = stateString.split("[\"\\s,]");
+            for (String state : states) {
+                if (state.length() > 0) {
+                    fromStates.add(state);
+                }
+            }
+        }
+
+        if (arguments.hasOption("to")) {
+            String stateString = arguments.getStringOption("to");
+            String[] states = stateString.split("[\"\\s,]");
+            for (String state : states) {
+                if (state.length() > 0) {
+                    toStates.add(state);
+                }
+            }
+        }
+
+        final String[] args2 = arguments.getLeftoverArguments();
+
+        switch (args2.length) {
+            case 1:
+                inputFileName = args2[0];
+                break;
+            default: {
+                System.err.println("Unknown option: " + args2[2]);
+                System.err.println();
+                printUsage(arguments);
+                System.exit(1);
+            }
+        }
+
         // command line options to follow shortly...
         try {
             JumpHistoryAnalyser jumpHistory = new JumpHistoryAnalyser(
-                    new File("seg1_ha_type2_SRD06_uncorExp_skyride_asym_traits_expPriors_1_mj.jumpHistory.log"),
-                    1000000,
-                    200,
-                    1950,
-                    2010,
-                    2010
+                    inputFileName,
+                    new HashSet<String>(),
+                    new HashSet<String>(),
+                    burnin,
+                    binCount,
+                    minTime,
+                    maxTime,
+                    mrsd
             );
         } catch (IOException e) {
             e.printStackTrace();
@@ -155,13 +247,7 @@ public class JumpHistoryAnalyser {
             e.printStackTrace();
         }
 
-//        for (int i = 0; i < x.getCount(); i++) {
-//            System.out.print(x.get(i));
-//
-//            for (Variate y : plots) {
-//                System.out.print("\t" + y.get(i));
-//            }
-//            System.out.println();
-//        }
+        System.exit(0);
     }
 }
+

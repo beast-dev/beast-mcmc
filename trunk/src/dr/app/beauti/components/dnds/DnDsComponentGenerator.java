b284002dfa02ea68eb05690cd0e4dc25574e6e42
fbielejec <fbielejec@ca793f91-a31e-0410-b540-2769d408b6a1>
2011-07-01 09:43:43 +0000

    added default tags

--- trunk/src/dr/app/beauti/components/dnds/DnDsComponentGenerator.java
+++ trunk/src/dr/app/beauti/components/dnds/DnDsComponentGenerator.java
@@ -19,13 +19,13 @@ public class DnDsComponentGenerator extends BaseComponentGenerator {
 
 	public boolean usesInsertionPoint(InsertionPoint point) {
 
-		 DnDsComponentOptions component = (DnDsComponentOptions)
-                 options.getComponentOptions(DnDsComponentOptions.class);
+		DnDsComponentOptions component = (DnDsComponentOptions) options
+				.getComponentOptions(DnDsComponentOptions.class);
 
-        if (component.getPartitionList().size() == 0) {
-            // Empty, so do nothing
-            return false;
-        }
+		if (component.getPartitionList().size() == 0) {
+			// Empty, so do nothing
+			return false;
+		}
 
 		switch (point) {
 		case IN_OPERATORS:
@@ -49,12 +49,14 @@ public class DnDsComponentGenerator extends BaseComponentGenerator {
 			writeCodonPartitionedRobustCounting(writer, component);
 			break;
 		case IN_FILE_LOG_PARAMETERS:
-			// TODO
+			writeLogs(writer, component);
 			break;
 		case IN_TREES_LOG:
-			// TODO
+			writeTreeLogs(writer, component);
 			break;
 		case AFTER_TREES_LOG:
+			writeDNdSLogger(writer, component);
+			writeDNdSReport(writer, component);
 			break;
 		default:
 			throw new IllegalArgumentException(
@@ -71,29 +73,156 @@ public class DnDsComponentGenerator extends BaseComponentGenerator {
 	private void writeCodonPartitionedRobustCounting(XMLWriter writer,
 			DnDsComponentOptions component) {
 
-        for (PartitionSubstitutionModel model : component.getPartitionList()) {
-            writeCodonPartitionedRobustCounting(writer, model);
-        }
+		for (PartitionSubstitutionModel model : component.getPartitionList()) {
+			writeCodonPartitionedRobustCounting(writer, model);
+		}
+	}
+
+	// Called for each model that requires robust counting (can be more than
+	// one)
+	private void writeCodonPartitionedRobustCounting(XMLWriter writer,
+			PartitionSubstitutionModel model) {
+
+		System.err.println("DEBUG: Writing RB for " + model.getName());
+
+		writer.writeComment("Robust counting for: " + model.getName());
+
+		// TODO: Hand coding is so 90s
+
+		// S operator
+		writer.writeOpenTag("codonPartitionedRobustCounting",
+				new Attribute[] {
+						new Attribute.Default<String>("id", "robustCounting1"),
+						new Attribute.Default<String>("labeling", "S"),
+						new Attribute.Default<String>("useUniformization",
+								"true"),
+						new Attribute.Default<String>("unconditionedPerBranch",
+								"true") });
+
+		writer.writeIDref("idref", "treeModel");
+		writer.writeOpenTag("firstPosition");
+		writer.writeIDref("ancestralTreeLikelihood", "CP1.treeLikelihood");
+		writer.writeCloseTag("firstPosition");
+
+		writer.writeOpenTag("secondPosition");
+		writer.writeIDref("ancestralTreeLikelihood", "CP2.treeLikelihood");
+		writer.writeCloseTag("secondPosition");
+
+		writer.writeOpenTag("thirdPosition");
+		writer.writeIDref("ancestralTreeLikelihood", "CP3.treeLikelihood");
+		writer.writeCloseTag("thirdPosition");
+
+		writer.writeCloseTag("codonPartitionedRobustCounting");
+
+		writer.writeBlankLine();
+
+		// N operator:
+		writer.writeOpenTag("codonPartitionedRobustCounting",
+				new Attribute[] {
+						new Attribute.Default<String>("id", "robustCounting2"),
+						new Attribute.Default<String>("labeling", "N"),
+						new Attribute.Default<String>("useUniformization",
+								"true"),
+						new Attribute.Default<String>("unconditionedPerBranch",
+								"true") });
+
+		writer.writeIDref("idref", "treeModel");
+		writer.writeOpenTag("firstPosition");
+		writer.writeIDref("ancestralTreeLikelihood", "CP1.treeLikelihood");
+		writer.writeCloseTag("firstPosition");
+
+		writer.writeOpenTag("secondPosition");
+		writer.writeIDref("ancestralTreeLikelihood", "CP2.treeLikelihood");
+		writer.writeCloseTag("secondPosition");
+
+		writer.writeOpenTag("thirdPosition");
+		writer.writeIDref("ancestralTreeLikelihood", "CP3.treeLikelihood");
+		writer.writeCloseTag("thirdPosition");
+
+		writer.writeCloseTag("codonPartitionedRobustCounting");
+
 	}// END: writeCodonPartitionedRobustCounting()
 
-    // Called for each model that requires robust counting (can be more than one)
-    private void writeCodonPartitionedRobustCounting(XMLWriter writer,
-                                                     PartitionSubstitutionModel model) {
+	private void writeLogs(XMLWriter writer, DnDsComponentOptions component) {
 
-        System.err.println("DEBUG: Writing RB for " + model.getName());
-        writer.writeComment("Robust counting for: " + model.getName());
-//		// Just sth to see if it works at all
-//		writer.writeOpenTag("codonPartitionedRobustCounting",
-//				new Attribute[] {
-//						new Attribute.Default<String>("id", "robustCounting1"),
-//						new Attribute.Default<String>("labeling", "S"),
-//						new Attribute.Default<String>("useUniformization",
-//								"true"),
-//						new Attribute.Default<String>("unconditionedPerBranch",
-//								"true") });
-//
-//		writer.writeIDref("treeModel", "");
-//		writer.writeCloseTag("codonPartitionedRobustCounting");
-    }
+		for (PartitionSubstitutionModel model : component.getPartitionList()) {
+			writeLogs(writer, model);
+		}
+	}
 
-}
+	private void writeLogs(XMLWriter writer, PartitionSubstitutionModel model) {
+
+		writer.writeComment("Robust counting for: " + model.getName());
+
+		writer.writeIDref("codonPartitionedRobustCounting", "robustCounting1");
+		writer.writeIDref("codonPartitionedRobustCounting", "robustCounting2");
+	}// END: writeLogs()
+
+	private void writeTreeLogs(XMLWriter writer, DnDsComponentOptions component) {
+
+		for (PartitionSubstitutionModel model : component.getPartitionList()) {
+			// We re-use the same method
+			writeLogs(writer, model);
+		}
+	}
+
+	private void writeDNdSLogger(XMLWriter writer,
+			DnDsComponentOptions component) {
+
+		for (PartitionSubstitutionModel model : component.getPartitionList()) {
+			writeDNdSLogger(writer, model);
+		}
+	}
+
+	private void writeDNdSLogger(XMLWriter writer,
+			PartitionSubstitutionModel model) {
+
+		writer.writeComment("Robust counting for: " + model.getName());
+
+		writer.writeOpenTag("log", new Attribute[] {
+				new Attribute.Default<String>("id", "fileLog_dNdS"),
+				new Attribute.Default<String>("logEvery", "10000"),
+				new Attribute.Default<String>("fileName", model.getName()
+						+ ".log") });
+
+		writer
+				.writeOpenTag("dNdSLogger",
+						new Attribute[] { new Attribute.Default<String>("id",
+								"dNdS") });
+		writer.writeIDref("idref", "treeModel");
+		writer.writeIDref("codonPartitionedRobustCounting", "robustCounting1");
+		writer.writeIDref("codonPartitionedRobustCounting", "robustCounting2");
+		writer.writeCloseTag("dNdSLogger");
+
+		writer.writeCloseTag("log");
+
+	}// END: writeLogs()
+
+	private void writeDNdSReport(XMLWriter writer,
+			DnDsComponentOptions component) {
+
+		for (PartitionSubstitutionModel model : component.getPartitionList()) {
+			writeDNdSReport(writer, model);
+		}
+	}
+
+	private void writeDNdSReport(XMLWriter writer,
+			PartitionSubstitutionModel model) {
+
+		writer.writeComment("Robust counting for: " + model.getName());
+
+		writer.writeOpenTag("report");
+		writer.write("<dNdSPerSiteAnalysis fileName=" + "'" + model.getName()
+				+ ".log'/>");
+		writer.writeCloseTag("report");
+
+	}// END: writeDNdSReport()
+
+	
+	
+	
+	
+	
+	
+	
+}// END: class

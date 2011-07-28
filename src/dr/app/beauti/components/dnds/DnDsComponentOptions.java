45be2498d875be4b015eb961302931651a5db84c
Filip Bielejec <filip.bielejec@rega.kuleuven.be>
2011-07-28 23:36:50 +0000

    added flag setter

--- src/dr/app/beauti/components/dnds/DnDsComponentOptions.java
+++ src/dr/app/beauti/components/dnds/DnDsComponentOptions.java
@@ -14,46 +14,53 @@ public class DnDsComponentOptions implements ComponentOptions {
 
 	final private BeautiOptions options;
 	static public final String CODON_PARTITIONED_ROBUST_COUNTING = "codon.partitioned.robust.counting";
+	private List<PartitionSubstitutionModel> partitionList = new ArrayList<PartitionSubstitutionModel>();
 
 	public DnDsComponentOptions(final BeautiOptions options) {
 		this.options = options;
 	}
 
 	public void createParameters(ModelOptions modelOptions) {
-        // Do nothing; this is only called at launch
+		// Do nothing; this is only called at launch
 	}
 
 	public void selectOperators(ModelOptions modelOptions, List<Operator> ops) {
-        // Do nothing
+		// Do nothing
 	}
 
 	public void selectParameters(ModelOptions modelOptions,
 			List<Parameter> params) {
-        // Do nothing
+		// Do nothing
 	}
 
 	public void selectStatistics(ModelOptions modelOptions,
 			List<Parameter> stats) {
-        // Do nothing
+		// Do nothing
 	}
 
-    public boolean addPartition(PartitionSubstitutionModel partition) {
-        if (!partitionList.contains(partition)) {
-            partitionList.add(partition);
-        }
-        return true; // No error
-    }
+	public boolean addPartition(PartitionSubstitutionModel partition) {
+		if (!partitionList.contains(partition)) {
+			partitionList.add(partition);
+		}
+		return true; // No error
+	}
 
-    public void removePartition(PartitionSubstitutionModel model) {
-        if (partitionList.contains(model)) {
-            partitionList.remove(model);
-        }
-    }
+	public void removePartition(PartitionSubstitutionModel model) {
+		if (partitionList.contains(model)) {
+			partitionList.remove(model);
+		}
+	}
 
-    public List<PartitionSubstitutionModel> getPartitionList() {
-        return partitionList;
-    }
+	public List<PartitionSubstitutionModel> getPartitionList() {
+		return partitionList;
+	}
 
-    private List<PartitionSubstitutionModel> partitionList = new ArrayList<PartitionSubstitutionModel>();
-    
+	public BeautiOptions getOptions() {
+		return options;
+	}
+
+	public boolean doRobustCounting() {
+		return true;
+	}
+
 }

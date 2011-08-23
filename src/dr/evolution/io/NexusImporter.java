179f9b065395c8151a2812ee1684397fe8ae602d
rambaut <rambaut@users.noreply.github.com>
2011-08-23 06:18:33 +0000

    Very minor language improvements.

--- src/dr/evolution/io/NexusImporter.java
+++ src/dr/evolution/io/NexusImporter.java
@@ -700,7 +700,7 @@ public class NexusImporter extends Importer implements SequenceImporter, TreeImp
 
         int duplicateTaxon = TaxonList.Utils.findDuplicateTaxon(taxa);
         if (duplicateTaxon >= 0)
-            throw new IllegalArgumentException("Find duplicate taxon name: " + taxa.getTaxon(duplicateTaxon).getId() +
+            throw new IllegalArgumentException("Tree contains duplicate taxon name: " + taxa.getTaxon(duplicateTaxon).getId() +
                       "!\nAll taxon names should be unique.");
 
         return taxa;

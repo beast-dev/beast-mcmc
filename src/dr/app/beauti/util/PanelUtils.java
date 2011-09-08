354bb10fd842952f3234f826ccd0a286b13850d5
Filip Bielejec <filip.bielejec@rega.kuleuven.be>
2011-09-08 13:33:04 +0000

    method to return active frame

--- src/dr/app/beauti/util/PanelUtils.java
+++ src/dr/app/beauti/util/PanelUtils.java
@@ -8,52 +8,65 @@ import java.awt.*;
  * @version $Id$
  */
 public class PanelUtils {
-    public static JPanel createAddRemoveButtonPanel(Action addAction, Icon addIcon, String addToolTip,
-                                                    Action removeAction, Icon removeIcon, String removeToolTip, int axis) {
+	public static JPanel createAddRemoveButtonPanel(Action addAction,
+			Icon addIcon, String addToolTip, Action removeAction,
+			Icon removeIcon, String removeToolTip, int axis) {
 
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new BoxLayout(buttonPanel, axis));
-        buttonPanel.setOpaque(false);
-        JButton addButton = new JButton(addAction);
-        if (addIcon != null) {
-            addButton.setIcon(addIcon);
-            addButton.setText(null);
-        }
-        addButton.setToolTipText(addToolTip);
-        addButton.putClientProperty("JButton.buttonType", "toolbar");
-        addButton.setOpaque(false);
-        addAction.setEnabled(false);
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new BoxLayout(buttonPanel, axis));
+		buttonPanel.setOpaque(false);
+		JButton addButton = new JButton(addAction);
+		if (addIcon != null) {
+			addButton.setIcon(addIcon);
+			addButton.setText(null);
+		}
+		addButton.setToolTipText(addToolTip);
+		addButton.putClientProperty("JButton.buttonType", "toolbar");
+		addButton.setOpaque(false);
+		addAction.setEnabled(false);
 
-        JButton removeButton = new JButton(removeAction);
-        if (removeIcon != null) {
-            removeButton.setIcon(removeIcon);
-            removeButton.setText(null);
-        }
-        removeButton.setToolTipText(removeToolTip);
-        removeButton.putClientProperty("JButton.buttonType", "toolbar");
-        removeButton.setOpaque(false);
-        removeAction.setEnabled(false);
+		JButton removeButton = new JButton(removeAction);
+		if (removeIcon != null) {
+			removeButton.setIcon(removeIcon);
+			removeButton.setText(null);
+		}
+		removeButton.setToolTipText(removeToolTip);
+		removeButton.putClientProperty("JButton.buttonType", "toolbar");
+		removeButton.setOpaque(false);
+		removeAction.setEnabled(false);
 
-        buttonPanel.add(addButton);
-        buttonPanel.add(new JToolBar.Separator(new Dimension(6, 6)));
-        buttonPanel.add(removeButton);
+		buttonPanel.add(addButton);
+		buttonPanel.add(new JToolBar.Separator(new Dimension(6, 6)));
+		buttonPanel.add(removeButton);
 
-        return buttonPanel;
-    }
+		return buttonPanel;
+	}
 
-    public static void setupComponent(JComponent comp) {
-        comp.setOpaque(false);
+	public static void setupComponent(JComponent comp) {
+		comp.setOpaque(false);
 
-        //comp.setFont(UIManager.getFont("SmallSystemFont"));
-        //comp.putClientProperty("JComponent.sizeVariant", "small");
-        if (comp instanceof JButton) {
-            comp.putClientProperty("JButton.buttonType", "roundRect");
-        }
-        if (comp instanceof JComboBox) {
-            comp.putClientProperty("JComboBox.isPopDown", Boolean.TRUE);
-//            comp.putClientProperty("JComboBox.isSquare", Boolean.TRUE);
-        }
-    }
+		// comp.setFont(UIManager.getFont("SmallSystemFont"));
+		// comp.putClientProperty("JComponent.sizeVariant", "small");
+		if (comp instanceof JButton) {
+			comp.putClientProperty("JButton.buttonType", "roundRect");
+		}
+		if (comp instanceof JComboBox) {
+			comp.putClientProperty("JComboBox.isPopDown", Boolean.TRUE);
+			// comp.putClientProperty("JComboBox.isSquare", Boolean.TRUE);
+		}
+	}
 
+	public static Frame getActiveFrame() {
+		Frame result = null;
+		Frame[] frames = Frame.getFrames();
+		for (int i = 0; i < frames.length; i++) {
+			Frame frame = frames[i];
+			if (frame.isVisible()) {
+				result = frame;
+				break;
+			}
+		}
+		return result;
+	}// END: getActiveFrame
 
 }

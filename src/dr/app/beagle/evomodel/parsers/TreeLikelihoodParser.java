e88ba276c800545d3adc8a65ac661d69d48ebd92
Marc Suchard <msuchard@ucla.edu>
2011-10-16 15:49:24 +0000

    Preliminary star tree implementation as an alternative to the correlated trait models; would function better as a subclass of TreeModel than TreeLikelihood.

--- src/dr/app/beagle/evomodel/parsers/TreeLikelihoodParser.java
+++ src/dr/app/beagle/evomodel/parsers/TreeLikelihoodParser.java
@@ -1,27 +1,56 @@
+/*
+ * TreeLikelihoodParser.java
+ *
+ * Copyright (c) 2002-2011 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
 package dr.app.beagle.evomodel.parsers;
 
 //import dr.app.beagle.evomodel.treelikelihood.RestrictedPartialsSequenceLikelihood;
+
 import dr.app.beagle.evomodel.sitemodel.BranchSubstitutionModel;
+import dr.app.beagle.evomodel.sitemodel.GammaSiteRateModel;
 import dr.app.beagle.evomodel.sitemodel.HomogenousBranchSubstitutionModel;
 import dr.app.beagle.evomodel.substmodel.FrequencyModel;
+import dr.app.beagle.evomodel.treelikelihood.BeagleTreeLikelihood;
+import dr.app.beagle.evomodel.treelikelihood.PartialsRescalingScheme;
 import dr.evolution.alignment.PatternList;
 import dr.evolution.alignment.Patterns;
 import dr.evolution.alignment.SitePatterns;
-import dr.app.beagle.evomodel.sitemodel.GammaSiteRateModel;
-import dr.app.beagle.evomodel.treelikelihood.BeagleTreeLikelihood;
-import dr.app.beagle.evomodel.treelikelihood.PartialsRescalingScheme;
 import dr.evolution.tree.Tree;
 import dr.evolution.util.TaxonList;
 import dr.evomodel.branchratemodel.BranchRateModel;
 import dr.evomodel.newtreelikelihood.TreeLikelihood;
 import dr.evomodel.tree.TreeModel;
 import dr.evomodel.treelikelihood.TipPartialsModel;
-import dr.inference.model.Likelihood;
 import dr.inference.model.CompoundLikelihood;
+import dr.inference.model.Likelihood;
 import dr.inference.model.Parameter;
 import dr.xml.*;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author Andrew Rambaut
@@ -171,8 +200,7 @@ public class TreeLikelihoodParser extends AbstractXMLObjectParser {
         return Likelihood.class;
     }
 
-    public XMLSyntaxRule[] getSyntaxRules() {
-        return new XMLSyntaxRule[] {
+    public static final XMLSyntaxRule[] rules = {
             AttributeRule.newBooleanRule(USE_AMBIGUITIES, true),
             new ElementRule(PatternList.class),
             new ElementRule(TreeModel.class),
@@ -187,5 +215,8 @@ public class TreeLikelihoodParser extends AbstractXMLObjectParser {
             new ElementRule(FrequencyModel.class, true),
                 new ElementRule(TipPartialsModel.class, true)
         };
+
+    public XMLSyntaxRule[] getSyntaxRules() {
+        return rules;
     }
 }

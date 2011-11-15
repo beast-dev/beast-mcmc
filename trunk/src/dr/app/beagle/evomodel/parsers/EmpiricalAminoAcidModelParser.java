b4ad02bd37ab3fe2b91a9ea5b0340e6e225121b8
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2011-11-15 12:36:33 +0000

    Helping FLU model parsing.

--- trunk/src/dr/app/beagle/evomodel/parsers/EmpiricalAminoAcidModelParser.java
+++ trunk/src/dr/app/beagle/evomodel/parsers/EmpiricalAminoAcidModelParser.java
@@ -1,9 +1,34 @@
+/*
+ * EmpiricalAminoAcidModelParser.java
+ *
+ * Copyright (c) 2002-2011 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
 package dr.app.beagle.evomodel.parsers;
 
-import dr.xml.*;
-import dr.app.beagle.evomodel.substmodel.FrequencyModel;
 import dr.app.beagle.evomodel.substmodel.EmpiricalAminoAcidModel;
+import dr.app.beagle.evomodel.substmodel.FrequencyModel;
 import dr.evomodel.substmodel.*;
+import dr.xml.*;
 
 /**
  * BEAGLE parser for empirical amino acid models
@@ -44,7 +69,7 @@ public class EmpiricalAminoAcidModelParser extends AbstractXMLObjectParser {
         } else if (type.equals(AminoAcidModelType.WAG.getXMLName())) {
             rateMatrix = WAG.INSTANCE;
         } else if (type.equals(AminoAcidModelType.FLU.getXMLName())) {
-            rateMatrix = WAG.INSTANCE;
+            rateMatrix = FLU.INSTANCE;
         } else {
             throw new XMLParseException("Unrecognized empirical amino acid model: " + type);
         }

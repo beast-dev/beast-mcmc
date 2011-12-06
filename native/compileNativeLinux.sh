05d897613dd827a52e7294e98fec09ddea09ad4d
Walter <walterxie@users.noreply.github.com>
2011-12-06 23:07:04 +0000

    trunk: delete duplicate native folder

--- native/compileNativeLinux.sh
+++
@@ -1,8 +0,0 @@
-gcc -O4 -march=core2 -funroll-loops -ffast-math -fPIC -fstrict-aliasing -c -I/usr/java/default/include/ -I/usr/java/default/include/linux NucleotideLikelihoodCore.c -o libNucleotideLikelihoodCore.o
-ld -shared -o libNucleotideLikelihoodCore.so libNucleotideLikelihoodCore.o
-
-gcc -O4 -march=pentiumpro -mcpu=pentiumpro -funroll-loops -ffast-math -fstrict-aliasing -c -I/usr/java/j2sdk1.4.1_01/include/ -I/usr/java/j2sdk1.4.1_01/include/linux NucleotideLikelihoodCore.c -o libNucleotideLikelihoodCore.o
-ld -shared -o libNucleotideLikelihoodCore.so libNucleotideLikelihoodCore.o 
-
-gcc -O4 -march=pentiumpro -mcpu=pentiumpro -funroll-loops -ffast-math -fstrict-aliasing -c -I/usr/java/j2sdk1.4.1_01/include/ -I/usr/java/j2sdk1.4.1_01/include/linux AminoAcidLikelihoodCore.c -o libAminoAcidLikelihoodCore.o
-ld -shared -o libAminoAcidLikelihoodCore.so libAminoAcidLikelihoodCore.o 

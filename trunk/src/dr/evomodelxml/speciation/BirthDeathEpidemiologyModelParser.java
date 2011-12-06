efc139a9ce5eea1fc145fb240f982231e20897a6
rambaut <rambaut@ca793f91-a31e-0410-b540-2769d408b6a1>
2011-12-06 23:27:31 +0000

    New parameterization of Tanja's BDSSM for epidemiology

--- trunk/src/dr/evomodelxml/speciation/BirthDeathEpidemiologyModelParser.java
+++ trunk/src/dr/evomodelxml/speciation/BirthDeathEpidemiologyModelParser.java
@@ -1,26 +1,26 @@
 /*
- * BirthDeathSerialSamplingModelParser.java
+ * BirthDeathEpidemiologyModelParser.java
  *
- * Copyright (C) 2002-2011 Alexei Drummond and Andrew Rambaut
+ *  Copyright (c) 2002-2011 BEAST Core Development Team
  *
- * This file is part of BEAST.
- * See the NOTICE file distributed with this work for additional
- * information regarding copyright ownership and licensing.
+ *  This file is part of BEAST.
+ *  See the NOTICE file distributed with this work for additional
+ *  information regarding copyright ownership and licensing.
  *
- * BEAST is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ *  BEAST is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
  *
- * BEAST is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with BEAST; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
- * Boston, MA  02110-1301  USA
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with BEAST; if not, write to the
+ *  Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ *  Boston, MA  02110-1301  USA
  */
 
 package dr.evomodelxml.speciation;

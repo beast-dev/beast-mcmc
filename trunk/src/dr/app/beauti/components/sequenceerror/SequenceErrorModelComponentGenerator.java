c0bc3860f7f3395c5e7ac11332780d6c3dcc2df7
dong.w.xie <dong.w.xie@ca793f91-a31e-0410-b540-2769d408b6a1>
2011-12-06 00:09:34 +0000

    trunk: solve Issue 555:	Does sequence error model have to apply to every sequence data partition?

--- trunk/src/dr/app/beauti/components/sequenceerror/SequenceErrorModelComponentGenerator.java
+++ trunk/src/dr/app/beauti/components/sequenceerror/SequenceErrorModelComponentGenerator.java
@@ -26,7 +26,7 @@ public class SequenceErrorModelComponentGenerator extends BaseComponentGenerator
     }
 
     public boolean usesInsertionPoint(final InsertionPoint point) {
-        SequenceErrorModelComponentOptions comp = (SequenceErrorModelComponentOptions)options.getComponentOptions(SequenceErrorModelComponentOptions.class);
+        SequenceErrorModelComponentOptions comp = (SequenceErrorModelComponentOptions) options.getComponentOptions(SequenceErrorModelComponentOptions.class);
 
 
         if (!comp.usingSequenceErrorModel()) {
@@ -44,7 +44,7 @@ public class SequenceErrorModelComponentGenerator extends BaseComponentGenerator
     }
 
     protected void generate(final InsertionPoint point, final Object item, final XMLWriter writer) {
-        SequenceErrorModelComponentOptions component = (SequenceErrorModelComponentOptions)options.getComponentOptions(SequenceErrorModelComponentOptions.class);
+        SequenceErrorModelComponentOptions component = (SequenceErrorModelComponentOptions) options.getComponentOptions(SequenceErrorModelComponentOptions.class);
 
         switch (point) {
             case AFTER_PATTERNS:
@@ -54,7 +54,7 @@ public class SequenceErrorModelComponentGenerator extends BaseComponentGenerator
                 writeErrorModels(writer, component);
                 break;
             case IN_TREE_LIKELIHOOD:
-                AbstractPartitionData partition = (AbstractPartitionData)item;
+                AbstractPartitionData partition = (AbstractPartitionData) item;
                 SequenceErrorType errorType = component.getSequenceErrorType(partition);
                 if (errorType != SequenceErrorType.NO_ERROR) {
                     writer.writeIDref(SequenceErrorModelParser.SEQUENCE_ERROR_MODEL, partition.getName() + ".errorModel");
@@ -75,7 +75,7 @@ public class SequenceErrorModelComponentGenerator extends BaseComponentGenerator
 
     private void writeHypermutationAlignments(XMLWriter writer, SequenceErrorModelComponentOptions component) {
         for (AbstractPartitionData partition : options.getDataPartitions()) {
-            String prefix = partition.getName() + ".";
+            String prefix = partition.getPrefix();//partition.getName() + ".";
 
             if (component.isHypermutation(partition)) {
                 SequenceErrorType errorType = component.getSequenceErrorType(partition);
@@ -114,7 +114,7 @@ public class SequenceErrorModelComponentGenerator extends BaseComponentGenerator
 
     private void writeErrorModels(XMLWriter writer, SequenceErrorModelComponentOptions component) {
         for (AbstractPartitionData partition : options.getDataPartitions()) {
-            String prefix = partition.getName() + ".";
+            String prefix = partition.getPrefix();//partition.getName() + ".";
 
             SequenceErrorType errorType = component.getSequenceErrorType(partition);
             if (component.isHypermutation(partition)) {
@@ -137,7 +137,7 @@ public class SequenceErrorModelComponentGenerator extends BaseComponentGenerator
                         new Attribute.Default<Boolean>(SumStatisticParser.ELEMENTWISE, true)});
                 writer.writeIDref(ParameterParser.PARAMETER, prefix + SequenceErrorModelComponentOptions.HYPERMUTANT_INDICATOR_PARAMETER);
                 writer.writeCloseTag(SumStatisticParser.SUM_STATISTIC);
-            } else {
+            } else if (errorType != SequenceErrorType.NO_ERROR) {
                 final String errorTypeName = (errorType == SequenceErrorType.AGE_TRANSITIONS ||
                         errorType == SequenceErrorType.BASE_TRANSITIONS ?
                         "transitions" : "all");
@@ -164,23 +164,26 @@ public class SequenceErrorModelComponentGenerator extends BaseComponentGenerator
 
     private void writeLogParameters(final XMLWriter writer, final SequenceErrorModelComponentOptions component) {
         for (AbstractPartitionData partition : options.getDataPartitions()) {
-            String prefix = partition.getName() + ".";
+            String prefix = partition.getPrefix();//partition.getName() + ".";
 
-            if (component.isHypermutation(partition)) {
-                writer.writeIDref(ParameterParser.PARAMETER, prefix + SequenceErrorModelComponentOptions.HYPERMUTION_RATE_PARAMETER);
-                writer.writeIDref(StatisticParser.STATISTIC, prefix + SequenceErrorModelComponentOptions.HYPERMUTANT_COUNT_STATISTIC);
-                writer.writeOpenTag(StatisticParser.STATISTIC,
-                        new Attribute.Default<String>("name", "isHypermutated"));
-                writer.writeIDref(HypermutantErrorModel.HYPERMUTANT_ERROR_MODEL,
-                        prefix + SequenceErrorModelComponentOptions.ERROR_MODEL);
-                writer.writeCloseTag(StatisticParser.STATISTIC);
-            }
+            SequenceErrorType errorType = component.getSequenceErrorType(partition);
+            if (errorType != SequenceErrorType.NO_ERROR) {
+                if (component.isHypermutation(partition)) {
+                    writer.writeIDref(ParameterParser.PARAMETER, prefix + SequenceErrorModelComponentOptions.HYPERMUTION_RATE_PARAMETER);
+                    writer.writeIDref(StatisticParser.STATISTIC, prefix + SequenceErrorModelComponentOptions.HYPERMUTANT_COUNT_STATISTIC);
+                    writer.writeOpenTag(StatisticParser.STATISTIC,
+                            new Attribute.Default<String>("name", "isHypermutated"));
+                    writer.writeIDref(HypermutantErrorModel.HYPERMUTANT_ERROR_MODEL,
+                            prefix + SequenceErrorModelComponentOptions.ERROR_MODEL);
+                    writer.writeCloseTag(StatisticParser.STATISTIC);
+                }
 
-            if (component.hasAgeDependentRate(partition)) {
-                writer.writeIDref(ParameterParser.PARAMETER, prefix + SequenceErrorModelComponentOptions.AGE_RATE_PARAMETER);
-            }
-            if (component.hasBaseRate(partition)) {
-                writer.writeIDref(ParameterParser.PARAMETER, prefix + SequenceErrorModelComponentOptions.BASE_RATE_PARAMETER);
+                if (component.hasAgeDependentRate(partition)) {
+                    writer.writeIDref(ParameterParser.PARAMETER, prefix + SequenceErrorModelComponentOptions.AGE_RATE_PARAMETER);
+                }
+                if (component.hasBaseRate(partition)) {
+                    writer.writeIDref(ParameterParser.PARAMETER, prefix + SequenceErrorModelComponentOptions.BASE_RATE_PARAMETER);
+                }
             }
         }
     }

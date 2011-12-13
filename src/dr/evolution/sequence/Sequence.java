4c28740474767ce7a31840ab029a37f7982409fe
Marc Suchard <msuchard@ucla.edu>
2011-12-13 04:26:06 +0000

    Force all sequences into upper-case.

--- src/dr/evolution/sequence/Sequence.java
+++ src/dr/evolution/sequence/Sequence.java
@@ -1,7 +1,7 @@
 /*
  * Sequence.java
  *
- * Copyright (C) 2002-2009 Alexei Drummond and Andrew Rambaut
+ * Copyright (c) 2002-2011 Alexei Drummond, Andrew Rambaut and Marc Suchard
  *
  * This file is part of BEAST.
  * See the NOTICE file distributed with this work for additional
@@ -12,10 +12,10 @@
  * published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  *
- * BEAST is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with BEAST; if not, write to the
@@ -148,7 +148,7 @@ public class Sequence implements Identifiable, Attributable {
      */
     public void setSequenceString(String sequence) {
         sequenceString.setLength(0);
-        sequenceString.append(sequence);
+        sequenceString.append(sequence.toUpperCase());
     }
 
     /**

ce67967c857b3186bc3e9b3892e0dfe844365f4e
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2012-01-12 17:14:36 +0000

    Adding interface for epoch models using BEAGLE

--- trunk/src/dr/app/beagle/evomodel/sitemodel/BranchSubstitutionModel.java
+++ trunk/src/dr/app/beagle/evomodel/sitemodel/BranchSubstitutionModel.java
@@ -1,5 +1,31 @@
+/*
+ * BranchSubstitutionModel.java
+ *
+ * Copyright (c) 2002-2012 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
 package dr.app.beagle.evomodel.sitemodel;
 
+import beagle.Beagle;
 import dr.app.beagle.evomodel.substmodel.EigenDecomposition;
 import dr.app.beagle.evomodel.substmodel.SubstitutionModel;
 import dr.evolution.tree.NodeRef;
@@ -9,6 +35,7 @@ import dr.inference.model.Model;
 /**
  * @author Andrew Rambaut
  * @author Alexei Drummond
+ * @author Marc A. Suchard
  * @version $Id$
  */
 public interface BranchSubstitutionModel extends Model {
@@ -24,4 +51,14 @@ public interface BranchSubstitutionModel extends Model {
     public int getEigenCount();
 
     boolean canReturnComplexDiagonalization();
+
+    void updateTransitionMatrices(
+            Beagle beagle,
+            int eigenIndex,
+            final int[] probabilityIndices,
+            final int[] firstDerivativeIndices,
+            final int[] secondDervativeIndices,
+            final double[] edgeLengths,
+            int count);
+
 }

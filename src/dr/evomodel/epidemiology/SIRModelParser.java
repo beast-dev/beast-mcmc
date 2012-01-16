1cec441d7a539f276b25f4ce6ab2310b6c2abf8b
Trevor Bedford <trvrb@users.noreply.github.com>
2012-01-16 13:51:34 +0000

    Updated skeleton of SIRModel.  Includes S, I and R parameters and doesn't crash.  Returns dummy likelihood.

--- src/dr/evomodel/epidemiology/SIRModelParser.java
+++ src/dr/evomodel/epidemiology/SIRModelParser.java
@@ -11,10 +11,12 @@ import dr.xml.*;
  */
 public class SIRModelParser extends AbstractXMLObjectParser {
 
-    public static String SIR_MODEL = "sirModel";
-
+    public static String SIR_MODEL = "sirEpidemiology";
     public static String TRANSMISSION_RATE = "transmissionRate";
     public static String RECOVERY_RATE = "recoveryRate";
+    public static String SUSCEPTIBLES = "susceptibles";
+    public static String INFECTEDS = "infecteds";
+    public static String RECOVEREDS = "recovereds";
 
 
     public String getParserName() {
@@ -31,7 +33,17 @@ public class SIRModelParser extends AbstractXMLObjectParser {
         cxo = xo.getChild(RECOVERY_RATE);
         Parameter recoveryRateParameter = (Parameter) cxo.getChild(Parameter.class);
 
-        return new SIRModel(transmissionRateParameter, recoveryRateParameter, units);
+        cxo = xo.getChild(SUSCEPTIBLES);
+        Parameter susceptiblesParameter = (Parameter) cxo.getChild(Parameter.class);
+
+        cxo = xo.getChild(INFECTEDS);
+        Parameter infectedsParameter = (Parameter) cxo.getChild(Parameter.class);
+
+        cxo = xo.getChild(RECOVEREDS);
+        Parameter recoveredsParameter = (Parameter) cxo.getChild(Parameter.class);
+
+        return new SIRModel(transmissionRateParameter, recoveryRateParameter,
+                susceptiblesParameter, infectedsParameter, recoveredsParameter, units);
     }
 
     //************************************************************************
@@ -39,11 +51,11 @@ public class SIRModelParser extends AbstractXMLObjectParser {
     //************************************************************************
 
     public String getParserDescription() {
-        return "A demographic model of exponential growth.";
+        return "A demographic model of epidemic spread.";
     }
 
     public Class getReturnType() {
-        return ExponentialGrowthModel.class;
+        return SIRModel.class;
     }
 
     public XMLSyntaxRule[] getSyntaxRules() {
@@ -54,6 +66,12 @@ public class SIRModelParser extends AbstractXMLObjectParser {
             new ElementRule(TRANSMISSION_RATE,
                     new XMLSyntaxRule[]{new ElementRule(Parameter.class)}),
             new ElementRule(RECOVERY_RATE,
+                    new XMLSyntaxRule[]{new ElementRule(Parameter.class)}),
+            new ElementRule(SUSCEPTIBLES,
+                    new XMLSyntaxRule[]{new ElementRule(Parameter.class)}),
+            new ElementRule(INFECTEDS,
+                    new XMLSyntaxRule[]{new ElementRule(Parameter.class)}),
+            new ElementRule(RECOVEREDS,
                     new XMLSyntaxRule[]{new ElementRule(Parameter.class)}),
             XMLUnits.SYNTAX_RULES[0]
     };

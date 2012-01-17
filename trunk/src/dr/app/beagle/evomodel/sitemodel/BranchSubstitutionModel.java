0934fe166ee0c0f481f8c02851b03175b5067bb9
fbielejec <fbielejec@ca793f91-a31e-0410-b540-2769d408b6a1>
2012-01-17 21:20:24 +0000

    pass BufferIndexHelper to updateTransitionMatrices

--- trunk/src/dr/app/beagle/evomodel/sitemodel/BranchSubstitutionModel.java
+++ trunk/src/dr/app/beagle/evomodel/sitemodel/BranchSubstitutionModel.java
@@ -28,6 +28,7 @@ package dr.app.beagle.evomodel.sitemodel;
 import beagle.Beagle;
 import dr.app.beagle.evomodel.substmodel.EigenDecomposition;
 import dr.app.beagle.evomodel.substmodel.SubstitutionModel;
+import dr.app.beagle.evomodel.treelikelihood.BufferIndexHelper;
 import dr.evolution.tree.NodeRef;
 import dr.evolution.tree.Tree;
 import dr.inference.model.Model;
@@ -46,7 +47,7 @@ public interface BranchSubstitutionModel extends Model {
 
     double[] getStateFrequencies(int categoryIndex);
 
-    public int getBranchIndex(final Tree tree, final NodeRef node);
+    public int getBranchIndex(final Tree tree, final NodeRef node, int bufferIndex);
 
     public int getEigenCount();
 
@@ -55,10 +56,19 @@ public interface BranchSubstitutionModel extends Model {
     void updateTransitionMatrices(
             Beagle beagle,
             int eigenIndex,
+            BufferIndexHelper bufferHelper,
             final int[] probabilityIndices,
             final int[] firstDerivativeIndices,
             final int[] secondDervativeIndices,
             final double[] edgeLengths,
             int count);
+
+	int getExtraBufferCount();
+
+	void setFirstBuffer(int bufferCount);
+
+	void setEigenDecomposition(Beagle beagle, int eigenIndex, BufferIndexHelper bufferHelper, int dummy);
+	
+	
 
 }

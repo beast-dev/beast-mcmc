5299fcfc7f2936d436c4f604d527ff1be35079c2
rambaut <rambaut@ca793f91-a31e-0410-b540-2769d408b6a1>
2012-02-27 16:09:51 +0000

    New cleaner AntigenicLikelihood

--- trunk/src/dr/evomodel/antigenic/AntigenicLikelihood.java
+++ trunk/src/dr/evomodel/antigenic/AntigenicLikelihood.java
@@ -2,6 +2,7 @@ package dr.evomodel.antigenic;
 
 import dr.evolution.util.*;
 import dr.inference.model.*;
+import dr.math.distributions.NormalDistribution;
 import dr.util.*;
 import dr.xml.*;
 
@@ -13,13 +14,21 @@ import java.util.logging.Logger;
  * @author Andrew Rambaut
  * @version $Id$
  */
-public class AntigenicLikelihood implements Citable {
+public class AntigenicLikelihood extends AbstractModelLikelihood implements Citable {
 
     public final static String ANTIGENIC_LIKELIHOOD = "antigenicLikelihood";
 
+    public enum MeasurementType {
+        INTERVAL,
+        POINT,
+        UPPER_BOUND,
+        LOWER_BOUND,
+        MISSING
+    }
+
     public AntigenicLikelihood(
             int mdsDimension,
-            Parameter mdsPrecision,
+            Parameter mdsPrecisionParameter,
             TaxonList strains,
             CompoundParameter tipTraitParameter,
             MatrixParameter locationsParameter,
@@ -28,18 +37,50 @@ public class AntigenicLikelihood implements Citable {
             DataTable<String[]> dataTable,
             List<String> virusLocationStatisticList) {
 
+        super(ANTIGENIC_LIKELIHOOD);
+
+        this.mdsDimension = mdsDimension;
+        this.mdsPrecisionParameter = mdsPrecisionParameter;
+        addVariable(mdsPrecisionParameter);
+
+        this.locationsParameter = locationsParameter;
+        addVariable(locationsParameter);
+
+        this.strains = strains;
+
+        this.tipTraitParameter = tipTraitParameter;
+        if (tipTraitParameter != null) {
+            addVariable(tipTraitParameter);
+        }
+
+        this.columnEffectsParameter = columnEffectsParameter;
+        if (columnEffectsParameter != null) {
+            addVariable(columnEffectsParameter);
+        }
+
+        this.rowEffectsParameter = rowEffectsParameter;
+        if (rowEffectsParameter != null) {
+            addVariable(rowEffectsParameter);
+        }
+
         for (int i = 0; i < dataTable.getRowCount(); i++) {
             String[] values = dataTable.getRow(i);
-            columnLabels.add(values[0]);
-            int column = columnLabels.size() - 1;
+            int column = columnLabels.indexOf(values[0]);
+            if (column == -1) {
+                columnLabels.add(values[0]);
+                column = columnLabels.size() - 1;
+            }
 
             int columnStrain = strains.getTaxonIndex(values[1]);
             if (columnStrain == -1) {
                 throw new IllegalArgumentException("Error reading data table: Unrecognized column strain name, " + values[1] + ", in row " + (i+1));
             }
 
-            rowLabels.add(values[2]);
-            int row = rowLabels.size() - 1;
+            int row = rowLabels.indexOf(values[2]);
+            if (row == -1) {
+                rowLabels.add(values[2]);
+                row = rowLabels.size() - 1;
+            }
 
             int rowStrain = strains.getTaxonIndex(values[3]);
             if (rowStrain == -1) {
@@ -49,7 +90,7 @@ public class AntigenicLikelihood implements Citable {
             double minTitre = Double.NaN;
             if (values[4].length() > 0) {
                 try {
-                  minTitre = Double.parseDouble(values[4]);
+                    minTitre = Double.parseDouble(values[4]);
                 } catch (NumberFormatException nfe) {
                     // do nothing
                 }
@@ -57,25 +98,175 @@ public class AntigenicLikelihood implements Citable {
             double maxTitre = Double.NaN;
             if (values[5].length() > 0) {
                 try {
-                  maxTitre = Double.parseDouble(values[5]);
+                    maxTitre = Double.parseDouble(values[5]);
                 } catch (NumberFormatException nfe) {
                     // do nothing
                 }
             }
 
-            Measurement measurement = new Measurement(column, columnStrain, row, rowStrain, minTitre, maxTitre);
+            MeasurementType type = MeasurementType.INTERVAL;
+            if (minTitre == maxTitre) {
+                type = MeasurementType.POINT;
+            }
+
+            if (Double.isNaN(maxTitre)) {
+                type = MeasurementType.UPPER_BOUND;
+            } else if (Double.isNaN(minTitre)) {
+                type = MeasurementType.LOWER_BOUND;
+            }
+
+            Measurement measurement = new Measurement(column, columnStrain, row, rowStrain, type, minTitre, maxTitre);
             measurements.add(measurement);
         }
     }
 
+    @Override
+    protected void handleModelChangedEvent(Model model, Object object, int index) {
+    }
+
+    @Override
+    protected void handleVariableChangedEvent(Variable variable, int index, Variable.ChangeType type) {
+        if (variable == locationsParameter) {
+        } else if (variable == mdsPrecisionParameter) {
+        } else if (variable == columnEffectsParameter) {
+        } else if (variable == rowEffectsParameter) {
+        } else {
+            // could be a derived class's parameter
+//            throw new IllegalArgumentException("Unknown parameter");
+        }
+        likelihoodKnown = false;
+    }
+
+    @Override
+    protected void storeState() {
+    }
+
+    @Override
+    protected void restoreState() {
+    }
+
+    @Override
+    protected void acceptState() {
+    }
+
+    @Override
+    public Model getModel() {
+        return null;
+    }
+
+    @Override
+    public double getLogLikelihood() {
+        if (!likelihoodKnown) {
+            logLikelihood = computeLogLikelihood();
+        }
+
+        return logLikelihood;
+    }
+
+    // This function can be overwritten to implement other sampling densities, i.e. discrete ranks
+    private double computeLogLikelihood() {
+
+        double precision = mdsPrecisionParameter.getParameterValue(0);
+        double sd = 1.0 / Math.sqrt(precision);
+
+        logLikelihood = 0.0;
+        for (Measurement measurement : measurements) {
+            double distance = computeDistance(measurement.rowStrain, measurement.columnStrain);
+
+            double logNormalization = calculateTruncationNormalization(distance, sd);
+
+            switch (measurement.type) {
+                case INTERVAL: {
+                    double minTitre = transformTitre(measurement.minTitre, measurement.column, measurement.row);
+                    double maxTitre = transformTitre(measurement.maxTitre, measurement.column, measurement.row);
+                    logLikelihood += computeMeasurementIntervalLikelihood(distance, minTitre, maxTitre, sd) - logNormalization;
+                } break;
+                case POINT: {
+                    double titre = transformTitre(measurement.minTitre, measurement.column, measurement.row);
+                    logLikelihood += computeMeasurementLikelihood(distance, titre, sd) - logNormalization;
+                } break;
+                case UPPER_BOUND: {
+                    double minTitre = transformTitre(measurement.minTitre, measurement.column, measurement.row);
+                    logLikelihood += computeMeasurementUpperTailLikelihood(distance, minTitre, sd) - logNormalization;
+                } break;
+                case LOWER_BOUND: {
+                    double maxTitre = transformTitre(measurement.maxTitre, measurement.column, measurement.row);
+                    logLikelihood += computeMeasurementLowerTailLikelihood(distance, maxTitre, sd) - logNormalization;
+                } break;
+                case MISSING:
+                    break;
+            }
+        }
+
+        likelihoodKnown = true;
+
+        return logLikelihood;
+    }
+
+    protected double computeDistance(int rowStrain, int columnStrain) {
+        if (rowStrain == columnStrain) {
+            return 0.0;
+        }
+
+        Parameter X = locationsParameter.getParameter(rowStrain);
+        Parameter Y = locationsParameter.getParameter(columnStrain);
+        double sum = 0.0;
+        for (int i = 0; i < mdsDimension; i++) {
+            double difference = X.getParameterValue(i) - Y.getParameterValue(i);
+            sum += difference * difference;
+        }
+        return Math.sqrt(sum);
+    }
+
+    private double transformTitre(double titre, int column, int row) {
+        double rowEffect = rowEffectsParameter.getParameterValue(row);
+        double columnEffect = columnEffectsParameter.getParameterValue(column);
+
+        return (Math.log(Math.sqrt(rowEffect * columnEffect)) - Math.log(titre)) / Math.log(2);
+    }
+
+    private double computeMeasurementIntervalLikelihood(double distance, double minTitre, double maxTitre, double sd) {
+        double cdf1 = NormalDistribution.cdf(minTitre, distance, sd, false);
+        double cdf2 = NormalDistribution.cdf(maxTitre, distance, sd, false);
+
+        return Math.log(cdf2 - cdf1);
+    }
+
+    private double computeMeasurementLikelihood(double distance, double titre, double sd) {
+        return Math.log(NormalDistribution.pdf(titre, distance, sd));
+    }
+
+    private double computeMeasurementLowerTailLikelihood(double distance, double maxTitre, double sd) {
+        double cdf = NormalDistribution.cdf(maxTitre, distance, sd, false);
+        return Math.log(cdf);
+    }
+
+    private double computeMeasurementUpperTailLikelihood(double distance, double minTitre, double sd) {
+//        double cdf = NormalDistribution.cdf(maxTitre, distance, sd, false);
+//        double tail = 1.0 - cdf;
+        // using special tail function of NormalDistribution (see main() in NormalDistribution for test)
+        double tail = NormalDistribution.tailCDF(minTitre, distance, sd);
+        return Math.log(tail);
+    }
+
+    private double calculateTruncationNormalization(double distance, double sd) {
+        return NormalDistribution.cdf(distance, 0.0, sd, true);
+    }
+
+    @Override
+    public void makeDirty() {
+    }
+
     private class Measurement {
-        private Measurement(final int column, final int columnStrain, final int row, final int rowStrain, final double minTitre, final double maxTitre) {
+        private Measurement(final int column, final int columnStrain, final int row, final int rowStrain, final MeasurementType type, final double minTitre, final double maxTitre) {
             this.column = column;
             this.columnStrain = columnStrain;
-            this.minTitre = minTitre;
-            this.maxTitre = maxTitre;
             this.row = row;
             this.rowStrain = rowStrain;
+
+            this.type = type;
+            this.minTitre = minTitre;
+            this.maxTitre = maxTitre;
         }
 
         final int column;
@@ -83,15 +274,29 @@ public class AntigenicLikelihood implements Citable {
         final int columnStrain;
         final int rowStrain;
 
+        final MeasurementType type;
         final double minTitre;
         final double maxTitre;
 
     };
 
-    private List<Measurement> measurements = new ArrayList<Measurement>();
-    private List<String> columnLabels = new ArrayList<String>();
-    private List<String> rowLabels = new ArrayList<String>();
+    private final List<Measurement> measurements = new ArrayList<Measurement>();
+    private final List<String> columnLabels = new ArrayList<String>();
+    private final List<String> rowLabels = new ArrayList<String>();
 
+
+    private final int mdsDimension;
+    private final Parameter mdsPrecisionParameter;
+    private final MatrixParameter locationsParameter;
+    private final TaxonList strains;
+    private final CompoundParameter tipTraitParameter;
+    private final Parameter columnEffectsParameter;
+    private final Parameter rowEffectsParameter;
+
+    private double logLikelihood = 0.0;
+    private boolean likelihoodKnown = false;
+
+    // **************************************************************
     // XMLObjectParser
     // **************************************************************
 

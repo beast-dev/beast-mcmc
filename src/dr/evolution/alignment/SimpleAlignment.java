8d2f88cf5243d2b2575fddc33d645814a9e38e12
Filip Bielejec <filip.bielejec@rega.kuleuven.be>
2012-03-02 14:34:00 +0000

    setter for count report - safe

--- src/dr/evolution/alignment/SimpleAlignment.java
+++ src/dr/evolution/alignment/SimpleAlignment.java
@@ -102,7 +102,6 @@ public class SimpleAlignment extends Sequences implements Alignment, dr.util.XHT
      */
     public void setDataType(DataType dataType) {
         this.dataType = dataType;
-        countStatistics = !(dataType instanceof Codons) && !(dataType instanceof GeneralDataType);
     }
 
     /**
@@ -499,5 +498,5 @@ public class SimpleAlignment extends Sequences implements Alignment, dr.util.XHT
     private DataType dataType = null;
     private int siteCount = 0;
     private boolean siteCountKnown = false;
-    private boolean countStatistics = true;
+    private boolean countStatistics = !(dataType instanceof Codons) && !(dataType instanceof GeneralDataType);
 }

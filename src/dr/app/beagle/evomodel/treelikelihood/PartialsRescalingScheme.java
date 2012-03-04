c9145938ed153277baab340d935b2e3550bd6a22
Marc Suchard <msuchard@ucla.edu>
2012-03-04 16:46:08 +0000

    Implementing classic BEAST rescaling option (no rescaling until first underflow) in BEAGLE

--- src/dr/app/beagle/evomodel/treelikelihood/PartialsRescalingScheme.java
+++ src/dr/app/beagle/evomodel/treelikelihood/PartialsRescalingScheme.java
@@ -1,3 +1,28 @@
+/*
+ * PartialsRescalingScheme.java
+ *
+ * Copyright (c) 2002-2012 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
 package dr.app.beagle.evomodel.treelikelihood;
 
 /**
@@ -11,6 +36,7 @@ public enum PartialsRescalingScheme {
     DYNAMIC("dynamic"),
     AUTO("auto"),
     KICK_ASS("kickAss"),
+    OLD_SCHOOL("oldSchool"), // TODO Select name
     ALWAYS("always");
 
     PartialsRescalingScheme(String text) {
@@ -24,7 +50,7 @@ public enum PartialsRescalingScheme {
     private final String text;
 
     public static PartialsRescalingScheme parseFromString(String text) {
-        for(PartialsRescalingScheme scheme : PartialsRescalingScheme.values()) {
+        for (PartialsRescalingScheme scheme : PartialsRescalingScheme.values()) {
             if (scheme.getText().compareToIgnoreCase(text) == 0)
                 return scheme;
         }

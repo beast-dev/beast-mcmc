a370f531e70c4b315967e588404380853b8dbc1a
baele.guy@gmail.com <baele.guy@gmail.com@ca793f91-a31e-0410-b540-2769d408b6a1>
2012-03-04 13:52:59 +0000

    Added additional prior parsers (for future use with generalized stepping-stone sampling).

--- trunk/src/dr/inferencexml/distribution/PriorParsers.java
+++ trunk/src/dr/inferencexml/distribution/PriorParsers.java
@@ -25,11 +25,18 @@
 
 package dr.inferencexml.distribution;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 import dr.inference.distribution.DistributionLikelihood;
 import dr.inference.model.Likelihood;
 import dr.inference.model.Statistic;
+import dr.inference.trace.LogFileTraces;
+import dr.inference.trace.TraceException;
+import dr.inferencexml.trace.MarginalLikelihoodAnalysisParser;
 import dr.math.distributions.*;
 import dr.util.DataTable;
+import dr.util.FileHelpers;
 import dr.xml.*;
 
 /**
@@ -39,12 +46,15 @@ public class PriorParsers {
     public static final String EXPONENTIAL_PRIOR = "exponentialPrior";
     public static final String POISSON_PRIOR = "poissonPrior";
     public static final String NORMAL_PRIOR = "normalPrior";
+    public static final String NORMAL_REFERENCE_PRIOR = "normalReferencePrior";
     public static final String LOG_NORMAL_PRIOR = "logNormalPrior";
     public static final String GAMMA_PRIOR = "gammaPrior";
+    public static final String GAMMA_REFERENCE_PRIOR = "gammaReferencePrior";
     public static final String INVGAMMA_PRIOR = "invgammaPrior";
     public static final String INVGAMMA_PRIOR_CORRECT = "inverseGammaPrior";
     public static final String LAPLACE_PRIOR = "laplacePrior";
     public static final String BETA_PRIOR = "betaPrior";
+    public static final String PARAMETER_COLUMN = "parameterColumn";
     public static final String UPPER = "upper";
     public static final String LOWER = "lower";
     public static final String MEAN = "mean";
@@ -243,6 +253,190 @@ public class PriorParsers {
 
         public String getParserDescription() {
             return "Calculates the prior probability of some data under a given normal distribution.";
+        }
+
+        public Class getReturnType() {
+            return Likelihood.class;
+        }
+    };
+    
+    /**
+     * A special parser that reads a convenient short form of reference priors on parameters.
+     */
+    public static XMLObjectParser GAMMA_REFERENCE_PRIOR_PARSER = new AbstractXMLObjectParser() {
+
+        public String getParserName() {
+            return GAMMA_REFERENCE_PRIOR;
+        }
+
+        public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+
+        	String fileName = xo.getStringAttribute(FileHelpers.FILE_NAME);
+        	
+        	try {
+        		
+        		File file = new File(fileName);
+        		String parent = file.getParent();
+        	
+        		if (!file.isAbsolute()) {
+        			parent = System.getProperty("user.dir");
+        		}
+        		file = new File(parent, fileName);
+        		fileName = file.getAbsolutePath();
+        	
+        		String parameterName = xo.getStringAttribute(PARAMETER_COLUMN);
+        	
+        		LogFileTraces traces = new LogFileTraces(fileName, file);
+        		traces.loadTraces();
+        		int maxState = traces.getMaxState();
+        		
+        		// leaving the burnin attribute off will result in 10% being used
+                int burnin = xo.getAttribute("burnin", maxState / 10);
+                if (burnin < 0 || burnin >= maxState) {
+                    burnin = maxState / 10;
+                    System.out.println("WARNING: Burn-in larger than total number of states - using 10%");
+                }
+                traces.setBurnIn(burnin);
+                
+                int traceIndexParameter = -1;
+                for (int i = 0; i < traces.getTraceCount(); i++) {
+                	String traceName = traces.getTraceName(i);
+                	if (traceName.trim().equals(parameterName)) {
+                		traceIndexParameter = i;
+                	}
+                }
+                
+                if (traceIndexParameter == -1) {
+                    throw new XMLParseException("Column '" + parameterName + "' can not be found for " + getParserName() + " element.");
+                }
+
+                Double[] parameterSamples = new Double[traces.getStateCount()];
+                
+                DistributionLikelihood likelihood = new DistributionLikelihood(new GammaKDEDistribution((Double[]) traces.getValues(traceIndexParameter).toArray(parameterSamples)));
+        		for (int j = 0; j < xo.getChildCount(); j++) {
+        			if (xo.getChild(j) instanceof Statistic) {
+        				likelihood.addData((Statistic) xo.getChild(j));
+        			} else {
+        				throw new XMLParseException("illegal element in " + xo.getName() + " element");
+        			}
+        		}
+
+        		return likelihood;
+            
+        	} catch (FileNotFoundException fnfe) {
+        		throw new XMLParseException("File '" + fileName + "' can not be opened for " + getParserName() + " element.");
+        	} catch (java.io.IOException ioe) {
+                throw new XMLParseException(ioe.getMessage());
+            } catch (TraceException e) {
+                throw new XMLParseException(e.getMessage());
+            }
+        }
+
+        public XMLSyntaxRule[] getSyntaxRules() {
+            return rules;
+        }
+
+        private final XMLSyntaxRule[] rules = {
+                AttributeRule.newStringRule("fileName"),
+                AttributeRule.newStringRule("parameterColumn"),
+                AttributeRule.newIntegerRule("burnin"),
+                new ElementRule(Statistic.class, 1, Integer.MAX_VALUE)
+        };
+
+        public String getParserDescription() {
+            return "Calculates the reference prior probability of some data under a given normal distribution.";
+        }
+
+        public Class getReturnType() {
+            return Likelihood.class;
+        }
+    };
+    
+    /**
+     * A special parser that reads a convenient short form of reference priors on parameters.
+     */
+    public static XMLObjectParser NORMAL_REFERENCE_PRIOR_PARSER = new AbstractXMLObjectParser() {
+
+        public String getParserName() {
+            return NORMAL_REFERENCE_PRIOR;
+        }
+
+        public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+
+        	String fileName = xo.getStringAttribute(FileHelpers.FILE_NAME);
+        	
+        	try {
+        		
+        		File file = new File(fileName);
+        		String parent = file.getParent();
+        	
+        		if (!file.isAbsolute()) {
+        			parent = System.getProperty("user.dir");
+        		}
+        		file = new File(parent, fileName);
+        		fileName = file.getAbsolutePath();
+        	
+        		String parameterName = xo.getStringAttribute(PARAMETER_COLUMN);
+        	
+        		LogFileTraces traces = new LogFileTraces(fileName, file);
+        		traces.loadTraces();
+        		int maxState = traces.getMaxState();
+        		
+        		// leaving the burnin attribute off will result in 10% being used
+                int burnin = xo.getAttribute("burnin", maxState / 10);
+                if (burnin < 0 || burnin >= maxState) {
+                    burnin = maxState / 10;
+                    System.out.println("WARNING: Burn-in larger than total number of states - using 10%");
+                }
+                traces.setBurnIn(burnin);
+                
+                int traceIndexParameter = -1;
+                for (int i = 0; i < traces.getTraceCount(); i++) {
+                	String traceName = traces.getTraceName(i);
+                	if (traceName.trim().equals(parameterName)) {
+                		traceIndexParameter = i;
+                	}
+                }
+                
+                if (traceIndexParameter == -1) {
+                    throw new XMLParseException("Column '" + parameterName + "' can not be found for " + getParserName() + " element.");
+                }
+                
+                Double[] parameterSamples = new Double[traces.getStateCount()];
+                
+                DistributionLikelihood likelihood = new DistributionLikelihood(new NormalKDEDistribution((Double[]) traces.getValues(traceIndexParameter).toArray(parameterSamples)));
+        		for (int j = 0; j < xo.getChildCount(); j++) {
+        			if (xo.getChild(j) instanceof Statistic) {
+        				likelihood.addData((Statistic) xo.getChild(j));
+        			} else {
+        				throw new XMLParseException("illegal element in " + xo.getName() + " element");
+        			}
+        		}
+
+        		return likelihood;
+            
+        	} catch (FileNotFoundException fnfe) {
+        		throw new XMLParseException("File '" + fileName + "' can not be opened for " + getParserName() + " element.");
+        	} catch (java.io.IOException ioe) {
+                throw new XMLParseException(ioe.getMessage());
+            } catch (TraceException e) {
+                throw new XMLParseException(e.getMessage());
+            }
+        }
+
+        public XMLSyntaxRule[] getSyntaxRules() {
+            return rules;
+        }
+
+        private final XMLSyntaxRule[] rules = {
+                AttributeRule.newStringRule("fileName"),
+                AttributeRule.newStringRule("parameterColumn"),
+                AttributeRule.newIntegerRule("burnin"),
+                new ElementRule(Statistic.class, 1, Integer.MAX_VALUE)
+        };
+
+        public String getParserDescription() {
+            return "Calculates the reference prior probability of some data under a given normal distribution.";
         }
 
         public Class getReturnType() {

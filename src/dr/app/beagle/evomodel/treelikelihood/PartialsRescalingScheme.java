556e90c8a98c26c219e96e4b9119ba8d7697049f
rambaut <rambaut@users.noreply.github.com>
2012-03-05 12:28:24 +0000

    Default scaling returns to 'dynamic'. 'oldSchool' is renamed 'delayed'.

--- src/dr/app/beagle/evomodel/treelikelihood/PartialsRescalingScheme.java
+++ src/dr/app/beagle/evomodel/treelikelihood/PartialsRescalingScheme.java
@@ -31,13 +31,13 @@ package dr.app.beagle.evomodel.treelikelihood;
  */
 public enum PartialsRescalingScheme {
 
-    DEFAULT("default"),
-    NONE("none"),
-    DYNAMIC("dynamic"),
-    AUTO("auto"),
-    KICK_ASS("kickAss"),
-    OLD_SCHOOL("oldSchool"), // TODO Select name
-    ALWAYS("always");
+    DEFAULT("default"), // what ever our current favourite default is
+    NONE("none"),       // no scaling
+    DYNAMIC("dynamic"), // rescale when needed and reuse scaling factors
+    ALWAYS("always"),   // rescale every node, every site, every time - slow but safe
+    DELAYED("delayed"), // postpone until first underflow then switch to 'always'
+    AUTO("auto");       // BEAGLE automatic scaling - currently playing it safe with 'always'
+//    KICK_ASS("kickAss"),// should be good, probably still to be discovered
 
     PartialsRescalingScheme(String text) {
         this.text = text;

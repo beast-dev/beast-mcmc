6787152fdf69070cb919b73a583d16ce2edf6095
rambaut <rambaut@ca793f91-a31e-0410-b540-2769d408b6a1>
2012-03-23 11:50:29 +0000

    Created a new GUI app for plotting antigenic maps (and other multivariate traits). Currently just a copy of Tracer.

---
+++ trunk/src/dr/app/mapper/application/MapperFileMenuHandler.java
@@ -0,0 +1,14 @@
+package dr.app.mapper.application;
+
+import javax.swing.*;
+
+/**
+ * @author Andrew Rambaut
+ */
+public interface MapperFileMenuHandler {
+
+	Action getExportDataAction();
+
+	Action getExportPDFAction();
+
+}

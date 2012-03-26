52942d51774e8a95cb56d23a4d80bee2400a747f
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2012-03-26 19:45:56 +0000

    Implementing transformed trees and Pagel's lambda estimator

--- trunk/src/dr/evomodel/continuous/IntegratedMultivariateTraitLikelihood.java
+++ trunk/src/dr/evomodel/continuous/IntegratedMultivariateTraitLikelihood.java
@@ -1,3 +1,28 @@
+/*
+ * IntegratedMultivariateTraitLikelihood.java
+ *
+ * Copyright (c) 2002-2012 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
 package dr.evomodel.continuous;
 
 import dr.evolution.tree.NodeRef;
@@ -166,7 +191,7 @@ public abstract class IntegratedMultivariateTraitLikelihood extends AbstractMult
 
         if (computeWishartStatistics) {
 //            if (wishartStatistics == null) {
-                wishartStatistics = new WishartSufficientStatistics(dimTrait);
+            wishartStatistics = new WishartSufficientStatistics(dimTrait);
 //            } else {
 //                wishartStatistics.clear();
 //            }
@@ -468,9 +493,9 @@ public abstract class IntegratedMultivariateTraitLikelihood extends AbstractMult
 
     public double[] getTraitForNode(Tree tree, NodeRef node, String traitName) {
 
-        if (tree != treeModel) {
-            throw new RuntimeException("Can only reconstruct states on treeModel given to constructor");
-        }
+//        if (tree != treeModel) {
+//            throw new RuntimeException("Can only reconstruct states on treeModel given to constructor");
+//        }
 
         getLogLikelihood();
 

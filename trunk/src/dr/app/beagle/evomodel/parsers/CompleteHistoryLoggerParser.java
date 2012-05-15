0972c1d83bc01e6956592305c6ea267ae80bd7eb
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2012-05-15 08:51:39 +0000

    Starting work on a complete history filter

--- trunk/src/dr/app/beagle/evomodel/parsers/CompleteHistoryLoggerParser.java
+++ trunk/src/dr/app/beagle/evomodel/parsers/CompleteHistoryLoggerParser.java
@@ -1,23 +1,52 @@
+/*
+ * CompleteHistoryLoggerParser.java
+ *
+ * Copyright (c) 2002-2012 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
 package dr.app.beagle.evomodel.parsers;
 
 import dr.app.beagle.evomodel.treelikelihood.MarkovJumpsBeagleTreeLikelihood;
 import dr.app.beagle.evomodel.utilities.CompleteHistoryLogger;
+import dr.app.beagle.evomodel.utilities.HistoryFilter;
 import dr.inference.loggers.Logger;
 import dr.xml.*;
 
 /**
- * @author Marc A. Suchrd
+ * @author Marc A. Suchard
  * @author Philippe Lemey
  */
 public class CompleteHistoryLoggerParser extends AbstractXMLObjectParser {
 
     public static final String NAME = "completeHistoryLogger";
- 
+
     public Object parseXMLObject(XMLObject xo) throws XMLParseException {
 
         MarkovJumpsBeagleTreeLikelihood treeLikelihood =
                 (MarkovJumpsBeagleTreeLikelihood) xo.getChild(MarkovJumpsBeagleTreeLikelihood.class);
-        return new CompleteHistoryLogger(treeLikelihood);
+
+        HistoryFilter filter = (HistoryFilter) xo.getChild(HistoryFilter.class);
+
+        return new CompleteHistoryLogger(treeLikelihood, filter);
     }
 
     public String getParserName() {
@@ -38,5 +67,6 @@ public class CompleteHistoryLoggerParser extends AbstractXMLObjectParser {
 
     private final XMLSyntaxRule[] rules = {
             new ElementRule(MarkovJumpsBeagleTreeLikelihood.class),
+            new ElementRule(HistoryFilter.class, true),
     };
 }

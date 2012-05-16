ccc6754cd6fcfa843340744c2e8406a13980fb15
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2012-05-16 08:26:46 +0000

    TreeTransforms are now loggable as branch traits

--- trunk/src/dr/evomodel/tree/TreeTransform.java
+++ trunk/src/dr/evomodel/tree/TreeTransform.java
@@ -26,20 +26,56 @@
 package dr.evomodel.tree;
 
 import dr.evolution.tree.NodeRef;
+import dr.evolution.tree.Tree;
+import dr.evolution.tree.TreeTrait;
+import dr.evolution.tree.TreeTraitProvider;
 import dr.inference.model.AbstractModel;
 import dr.inference.model.Model;
 import dr.inference.model.Variable;
+import dr.util.Author;
+import dr.util.Citable;
+import dr.util.Citation;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Marc A. Suchard
  */
-public abstract class TreeTransform extends AbstractModel {
+public abstract class TreeTransform extends AbstractModel implements TreeTraitProvider, Citable {
+
+    public static final String TREE_TRANSFORM_PREFIX = "treeTransform";
 
     public TreeTransform(String name) {
         super(name);
+        setupTraits();
+    }
+
+    private void setupTraits() {
+        TreeTrait baseTrait = new TreeTrait.D() {
+
+            public String getTraitName() {
+                return TREE_TRANSFORM_PREFIX;
+            }
+
+            public Intent getIntent() {
+                return Intent.BRANCH;
+            }
+
+            public Double getTrait(Tree tree, NodeRef node) {
+                return getScaleForNode(tree, node);
+            }
+
+            public boolean getLoggable() {
+                return true;
+            }
+        };
+        treeTraits.addTrait(baseTrait);
     }
 
     public abstract double transform(TransformedTreeModel tree, NodeRef node, double originalHeight);
+
+    protected abstract double getScaleForNode(Tree tree, NodeRef node);
 
     public abstract String getInfo();
 
@@ -58,6 +94,27 @@ public abstract class TreeTransform extends AbstractModel {
     protected void acceptState() {
     }
 
+    public TreeTrait[] getTreeTraits() {
+        return treeTraits.getTreeTraits();
+    }
 
+    public TreeTrait getTreeTrait(String key) {
+        return treeTraits.getTreeTrait(key);
+    }
+
+    private final Helper treeTraits = new Helper();
+
+    public List<Citation> getCitations() {
+        List<Citation> citations = new ArrayList<Citation>();
+        citations.add(
+                new Citation(
+                        new Author[]{
+                                new Author("P", "Lemey"),
+                                new Author("MA", "Suchard"),
+                        },
+                        Citation.Status.IN_PREPARATION
+                ));
+        return citations;
+    }
 }
 

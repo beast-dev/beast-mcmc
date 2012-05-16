bd7a6ef212b7614c5627a4a7c11a88cb101edb7a
rambaut <rambaut@ca793f91-a31e-0410-b540-2769d408b6a1>
2012-05-16 13:07:18 +0000

    Porting from trunk: http://code.google.com/p/beast-mcmc/issues/detail?id=623

--- branches/1.7/src/dr/app/beauti/generator/LogGenerator.java
+++ branches/1.7/src/dr/app/beauti/generator/LogGenerator.java
@@ -406,28 +406,7 @@ public class LogGenerator extends Generator {
 
             writer.writeIDref(TreeModel.TREE_MODEL, tree.getPrefix() + TreeModel.TREE_MODEL);
 
-            for (PartitionClockModel model : options.getPartitionClockModels(options.getDataPartitions(tree))) {
-                switch (model.getClockType()) {
-                    case STRICT_CLOCK:
-                        writer.writeIDref(StrictClockBranchRatesParser.STRICT_CLOCK_BRANCH_RATES, model.getPrefix() + BranchRateModel.BRANCH_RATES);
-                        break;
-
-                    case UNCORRELATED:
-                        writer.writeIDref(DiscretizedBranchRatesParser.DISCRETIZED_BRANCH_RATES, options.noDuplicatedPrefix(model.getPrefix(), tree.getPrefix()) + BranchRateModel.BRANCH_RATES);
-                        break;
-
-                    case RANDOM_LOCAL_CLOCK:
-                        writer.writeIDref(RandomLocalClockModelParser.LOCAL_BRANCH_RATES, model.getPrefix() + BranchRateModel.BRANCH_RATES);
-                        break;
-
-                    case AUTOCORRELATED:
-                        writer.writeIDref(ACLikelihoodParser.AC_LIKELIHOOD, options.noDuplicatedPrefix(model.getPrefix(), tree.getPrefix()) + BranchRateModel.BRANCH_RATES);
-                        break;
-
-                    default:
-                        throw new IllegalArgumentException("Unknown clock model");
-                }
-            }
+            writeTreeTraits(writer, tree);
 
             if (options.hasData()) {
                 // we have data...
@@ -458,29 +437,7 @@ public class LogGenerator extends Generator {
                         });
                 writer.writeIDref(TreeModel.TREE_MODEL, tree.getPrefix() + TreeModel.TREE_MODEL);
 
-                for (PartitionClockModel model : options.getPartitionClockModels(options.getDataPartitions(tree))) {
-
-                    switch (model.getClockType()) {
-                        case STRICT_CLOCK:
-                            writer.writeIDref(StrictClockBranchRatesParser.STRICT_CLOCK_BRANCH_RATES, model.getPrefix() + BranchRateModel.BRANCH_RATES);
-                            break;
-
-                        case UNCORRELATED:
-                            writer.writeIDref(DiscretizedBranchRatesParser.DISCRETIZED_BRANCH_RATES, options.noDuplicatedPrefix(model.getPrefix(), tree.getPrefix()) + BranchRateModel.BRANCH_RATES);
-                            break;
-
-                        case RANDOM_LOCAL_CLOCK:
-                            writer.writeIDref(RandomLocalClockModelParser.LOCAL_BRANCH_RATES, model.getPrefix() + BranchRateModel.BRANCH_RATES);
-                            break;
-
-                        case AUTOCORRELATED:
-                            writer.writeIDref(ACLikelihoodParser.AC_LIKELIHOOD, options.noDuplicatedPrefix(model.getPrefix(), tree.getPrefix()) + BranchRateModel.BRANCH_RATES);
-                            break;
-
-                        default:
-                            throw new IllegalArgumentException("Unknown clock model");
-                    }
-                }
+                writeTreeTraits(writer, tree);
 
                 writer.writeCloseTag(TreeLoggerParser.LOG_TREE);
             }
@@ -489,4 +446,52 @@ public class LogGenerator extends Generator {
         generateInsertionPoint(ComponentGenerator.InsertionPoint.AFTER_TREES_LOG, writer);
     }
 
+    private void writeTreeTraits(XMLWriter writer, PartitionTreeModel tree) {
+        for (PartitionClockModel model : options.getPartitionClockModels(options.getDataPartitions(tree))) {
+
+            switch (model.getClockType()) {
+                case STRICT_CLOCK:
+                    writeTreeTrait(writer, StrictClockBranchRatesParser.STRICT_CLOCK_BRANCH_RATES,
+                            model.getPrefix() + BranchRateModel.BRANCH_RATES,
+                            BranchRateModel.RATE, model.getPrefix() + BranchRateModel.RATE);
+                    break;
+
+                case UNCORRELATED:
+                    writeTreeTrait(writer, DiscretizedBranchRatesParser.DISCRETIZED_BRANCH_RATES,
+                            options.noDuplicatedPrefix(model.getPrefix(), tree.getPrefix()) + BranchRateModel.BRANCH_RATES,
+                            BranchRateModel.RATE, model.getPrefix() + BranchRateModel.RATE);
+                    break;
+
+                case RANDOM_LOCAL_CLOCK:
+                    writeTreeTrait(writer, RandomLocalClockModelParser.LOCAL_BRANCH_RATES,
+                            model.getPrefix() + BranchRateModel.BRANCH_RATES,
+                            BranchRateModel.RATE, model.getPrefix() + BranchRateModel.RATE);
+                    break;
+
+                case AUTOCORRELATED:
+                    writer.writeIDref(ACLikelihoodParser.AC_LIKELIHOOD,
+                            options.noDuplicatedPrefix(model.getPrefix(), tree.getPrefix()) + BranchRateModel.BRANCH_RATES);
+                    writeTreeTrait(writer, ACLikelihoodParser.AC_LIKELIHOOD,
+                            options.noDuplicatedPrefix(model.getPrefix(), tree.getPrefix()) + BranchRateModel.BRANCH_RATES,
+                            BranchRateModel.RATE, model.getPrefix() + BranchRateModel.RATE);
+                    break;
+
+                default:
+                    throw new IllegalArgumentException("Unknown clock model");
+            }
+        }
+    }
+
+    private void writeTreeTrait(XMLWriter writer, String treeTraitTag, String treeTraitID, String traitName, String traitTag) {
+
+        writer.writeOpenTag(TreeLoggerParser.TREE_TRAIT,
+                new Attribute[]{
+                        new Attribute.Default<String>(TreeLoggerParser.NAME, traitName),
+                        new Attribute.Default<String>(TreeLoggerParser.TAG, traitTag)
+                });
+        writer.writeIDref(treeTraitTag, treeTraitID);
+
+        writer.writeCloseTag(TreeLoggerParser.TREE_TRAIT);
+
+    }
 }

3ed48057e6acae986a063b9349c2f928f5a421f9
Filip Bielejec <filip.bielejec@rega.kuleuven.be>
2012-06-12 11:40:49 +0000

    simulate homogenous sequences

--- src/dr/app/beagle/tools/parsers/BeagleSequenceSimulatorParser.java
+++ src/dr/app/beagle/tools/parsers/BeagleSequenceSimulatorParser.java
@@ -2,7 +2,9 @@ package dr.app.beagle.tools.parsers;
 
 import dr.app.beagle.evomodel.sitemodel.BranchSubstitutionModel;
 import dr.app.beagle.evomodel.sitemodel.GammaSiteRateModel;
+import dr.app.beagle.evomodel.sitemodel.HomogenousBranchSubstitutionModel;
 import dr.app.beagle.evomodel.substmodel.FrequencyModel;
+import dr.app.beagle.evomodel.substmodel.SubstitutionModel;
 import dr.app.beagle.tools.BeagleSequenceSimulator;
 import dr.evolution.alignment.Alignment;
 import dr.evolution.datatype.Codons;
@@ -12,28 +14,20 @@ import dr.evomodel.branchratemodel.BranchRateModel;
 import dr.evomodel.branchratemodel.DefaultBranchRateModel;
 import dr.evomodel.tree.TreeModel;
 import dr.xml.AbstractXMLObjectParser;
-import dr.xml.AttributeRule;
-import dr.xml.ElementRule;
 import dr.xml.XMLObject;
 import dr.xml.XMLParseException;
 import dr.xml.XMLSyntaxRule;
 
+/**
+ * @author Filip Bielejec
+ * @version $Id$
+ */
 public class BeagleSequenceSimulatorParser extends AbstractXMLObjectParser {
 
 	public static final String BEAGLE_SEQUENCE_SIMULATOR = "beagleSequenceSimulator";
 	public static final String REPLICATIONS = "replications";
 	public static final String SITE_MODEL = "siteModel";
 	
-	private XMLSyntaxRule[] rules = new XMLSyntaxRule[] {
-			new ElementRule(TreeModel.class),
-			new ElementRule(BranchSubstitutionModel.class),
-			new ElementRule(GammaSiteRateModel.class),
-//			 new ElementRule(SITE_MODEL, new XMLSyntaxRule[]{new ElementRule(BranchSubstitutionModel.class)}, false),
-			new ElementRule(BranchRateModel.class, true),
-			new ElementRule(FrequencyModel.class),
-			new ElementRule(Sequence.class, true),
-			AttributeRule.newIntegerRule(REPLICATIONS) };
-
 	@Override
 	public String getParserName() {
 		return BEAGLE_SEQUENCE_SIMULATOR;
@@ -51,22 +45,41 @@ public class BeagleSequenceSimulatorParser extends AbstractXMLObjectParser {
 
 	@Override
 	public XMLSyntaxRule[] getSyntaxRules() {
-		return rules;
-	}
+		
+//		return new XMLSyntaxRule[] {
+//				new ElementRule(TreeModel.class),
+//				new ElementRule(BranchSubstitutionModel.class),
+//				new ElementRule(GammaSiteRateModel.class),
+////				 new ElementRule(SITE_MODEL, new XMLSyntaxRule[]{new ElementRule(BranchSubstitutionModel.class)}, false),
+//				new ElementRule(BranchRateModel.class, true),
+//				new ElementRule(FrequencyModel.class),
+//				new ElementRule(Sequence.class, true),
+//				AttributeRule.newIntegerRule(REPLICATIONS) };
+		
+		return null;
+		
+	}//END: getSyntaxRules
 
 	@Override
 	public Object parseXMLObject(XMLObject xo) throws XMLParseException {
 
 		TreeModel tree = (TreeModel) xo.getChild(TreeModel.class);
-		BranchSubstitutionModel branchSubstitutionModel = (BranchSubstitutionModel) xo.getChild(BranchSubstitutionModel.class);
+		
 		GammaSiteRateModel siteModel = (GammaSiteRateModel) xo.getChild(GammaSiteRateModel.class);
-		BranchRateModel rateModel = (BranchRateModel) xo.getChild(BranchRateModel.class);
 		FrequencyModel freqModel = (FrequencyModel) xo.getChild(FrequencyModel.class);
 		Sequence ancestralSequence = (Sequence) xo.getChild(Sequence.class);
 		int replications = xo.getIntegerAttribute(REPLICATIONS);
 
+		BranchRateModel rateModel = (BranchRateModel) xo.getChild(BranchRateModel.class);
 		if (rateModel == null) {
 			rateModel = new DefaultBranchRateModel();
+		}
+		
+		// TODO check this for cast errors
+		BranchSubstitutionModel branchSubstitutionModel = (BranchSubstitutionModel) xo.getChild(BranchSubstitutionModel.class);
+		if (branchSubstitutionModel == null) {
+			SubstitutionModel substitutionModel = (SubstitutionModel) xo.getChild(SubstitutionModel.class);
+			branchSubstitutionModel = new HomogenousBranchSubstitutionModel(substitutionModel, freqModel);
 		}
 		
 		BeagleSequenceSimulator s = new BeagleSequenceSimulator(tree, //
@@ -79,8 +92,7 @@ public class BeagleSequenceSimulatorParser extends AbstractXMLObjectParser {
 
 		if (ancestralSequence != null) {
 
-			if (ancestralSequence.getLength() != 3*replications
-					&& freqModel.getDataType() instanceof Codons) {
+			if (ancestralSequence.getLength() != 3 * replications && freqModel.getDataType() instanceof Codons) {
 
 				throw new RuntimeException("Ancestral codon sequence has "
 						+ ancestralSequence.getLength() + " characters "
@@ -97,7 +109,7 @@ public class BeagleSequenceSimulatorParser extends AbstractXMLObjectParser {
 				s.setAncestralSequence(ancestralSequence);
 
 			}// END: dataType check
-		}
+		}// END: ancestralSequence check
 
 		return s.simulate();
 

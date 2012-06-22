d000e5c69c7b17a503b4d0194fbb7d8aa38888da
fbielejec <fbielejec@ca793f91-a31e-0410-b540-2769d408b6a1>
2012-06-22 23:21:12 +0000

    working on bss UI

--- trunk/src/dr/app/bss/BeagleSequenceSimulatorFrame.java
+++ trunk/src/dr/app/bss/BeagleSequenceSimulatorFrame.java
@@ -6,11 +6,12 @@ import jam.framework.Exportable;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
@@ -23,10 +24,8 @@ import javax.swing.JTabbedPane;
 import javax.swing.SwingUtilities;
 import javax.swing.plaf.BorderUIResource;
 
+import dr.app.beagle.tools.BeagleSequenceSimulator;
 import dr.evolution.io.Importer.ImportException;
-import dr.evolution.io.NewickImporter;
-import dr.evolution.io.NexusImporter;
-import dr.evolution.tree.Tree;
 
 @SuppressWarnings("serial")
 public class BeagleSequenceSimulatorFrame extends DocumentFrame {
@@ -34,80 +33,170 @@ public class BeagleSequenceSimulatorFrame extends DocumentFrame {
 	private JTabbedPane tabbedPane = new JTabbedPane();
 	private TaxaPanel taxaPanel;
 	private TreePanel treePanel;
-	private ModelsPanel modelsPanel;
-	private ClockPanel clockPanel;
+	private BranchSubstitutionModelPanel substModelPanel;
+	private ClockRateModelPanel clockPanel;
+	private FrequencyModelPanel frequencyPanel;
+	private SiteRateModelPanel sitePanel;
+	private SimulationPanel simulationPanels;
 	
 	private BeagleSequenceSimulatorData data = null;
 	private JLabel statusLabel;
 	private File workingDirectory = null;
 	
-	// TODO:
-	// clock model
-	// frequency model
-	// site model (rate cats)
-
 	public BeagleSequenceSimulatorFrame(String title) {
 
 		super();
+		
 		setTitle(title);
 		data = new BeagleSequenceSimulatorData();
 
+	}// END: Constructor
+
+	@Override
+	protected void initializeComponents() {
+
+		setSize(new Dimension(800, 600));
+		setMinimumSize(new Dimension(260, 100));
+
 		taxaPanel = new TaxaPanel(this, data);
 		treePanel = new TreePanel(this, data);
-		modelsPanel = new ModelsPanel(this, data);
-		clockPanel = new ClockPanel(this, data);
+		substModelPanel = new BranchSubstitutionModelPanel(this, data);
+		clockPanel = new ClockRateModelPanel(this, data);
+		frequencyPanel = new FrequencyModelPanel(this, data);
+		sitePanel = new SiteRateModelPanel(this, data);
+		simulationPanels = new SimulationPanel(this, data);
 		
 		tabbedPane.addTab("Taxa", null, taxaPanel);
 		tabbedPane.addTab("Tree", null, treePanel);
-		tabbedPane.addTab("Branch Substitution Model", null, modelsPanel);
-		tabbedPane.addTab("Clock Model", null, clockPanel);
-		
+		tabbedPane.addTab("Branch Substitution Model", null, substModelPanel);
+		tabbedPane.addTab("Clock Rate Model", null, clockPanel);
+		tabbedPane.addTab("Frequency Model", null, frequencyPanel);
+		tabbedPane.addTab("Site Rate Model", null, sitePanel);
+		tabbedPane.addTab("Simulation", null, simulationPanels);
 		
 		statusLabel = new JLabel("No taxa loaded");
 
 		JPanel progressPanel = new JPanel(new BorderLayout(0, 0));
-		JLabel progressLabel = new JLabel("");
 		JProgressBar progressBar = new JProgressBar();
-		progressPanel.add(progressLabel, BorderLayout.NORTH);
 		progressPanel.add(progressBar, BorderLayout.CENTER);
 
-		JPanel panel1 = new JPanel(new BorderLayout(0, 0));
-		panel1.add(statusLabel, BorderLayout.CENTER);
-		panel1.add(progressPanel, BorderLayout.EAST);
-		panel1.setBorder(new BorderUIResource.EmptyBorderUIResource(
-				new java.awt.Insets(0, 6, 0, 6)));
+		JPanel statusPanel = new JPanel(new BorderLayout(0, 0));
+		statusPanel.add(statusLabel, BorderLayout.CENTER);
+		statusPanel.add(progressPanel, BorderLayout.EAST);
+		statusPanel.setBorder(new BorderUIResource.EmptyBorderUIResource(
+				new Insets(0, 6, 0, 6)));
 
-		JPanel panel = new JPanel(new BorderLayout(0, 0));
-		panel.add(tabbedPane, BorderLayout.CENTER);
-		panel.add(panel1, BorderLayout.SOUTH);
-		panel.setBorder(new BorderUIResource.EmptyBorderUIResource(
-				new java.awt.Insets(12, 12, 12, 12)));
+		JPanel tabbedPanePanel = new JPanel(new BorderLayout(0, 0));
+		tabbedPanePanel.add(tabbedPane, BorderLayout.CENTER);
+		tabbedPanePanel.add(statusPanel, BorderLayout.SOUTH);
+		tabbedPanePanel.setBorder(new BorderUIResource.EmptyBorderUIResource(
+				new Insets(12, 12, 12, 12)));
 
 		getContentPane().setLayout(new java.awt.BorderLayout(0, 0));
-		getContentPane().add(panel, BorderLayout.CENTER);
+		getContentPane().add(tabbedPanePanel, BorderLayout.CENTER);
 
-	}// END: Constructor
+	}// END: initializeComponents
 
-	public boolean useImportAction() {
-		return true;
+	// ///////////////////
+	// ---IMPORT TAXA---//
+	// ///////////////////
+
+//	public Action getImportAction() {
+//		return importTaxaAction;
+//	}
+//
+//	private AbstractAction importTaxaAction = new AbstractAction("Import Taxa...") {
+//		public void actionPerformed(ActionEvent ae) {
+//			 doImport();
+//		}
+//	};
+//
+//	public void doImport() {
+//
+//		try {
+//
+//			JFileChooser chooser = new JFileChooser();
+//			chooser.setDialogTitle("Import Taxa...");
+//			chooser.setMultiSelectionEnabled(false);
+//			chooser.setCurrentDirectory(workingDirectory);
+//
+//			chooser.showOpenDialog(Utils.getActiveFrame());
+//			File file = chooser.getSelectedFile();
+//
+//			if (file != null) {
+//
+//				importFromFile(file);
+//
+//				File tmpDir = chooser.getCurrentDirectory();
+//				if (tmpDir != null) {
+//					workingDirectory = tmpDir;
+//				}
+//
+//			}// END: file opened check
+//
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} catch (ImportException e) {
+//			e.printStackTrace();
+//		}// END: try catch block
+//
+//	}// END: doImport
+//	
+//	public void importFromFile(File file) throws IOException, ImportException {
+//		
+//        BufferedReader reader = new BufferedReader(new FileReader(file));
+//
+//        String line = reader.readLine();
+//        Tree tree;
+//
+//        if (line.toUpperCase().startsWith("#NEXUS")) {
+//            NexusImporter importer = new NexusImporter(reader);
+//            tree = importer.importTree(null);
+//        } else {
+//            NewickImporter importer = new NewickImporter(reader);
+//            tree = importer.importTree(null);
+//        }
+//
+//        data.taxonList = tree;
+//        reader.close();
+//
+//        fireTaxaChanged();
+//		
+//	}//END: importFromFile
+
+	public void fireTaxaChanged() {
+		
+		SwingUtilities.invokeLater(new Runnable() {
+		    public void run() {
+
+		        taxaPanel.updateUI();
+		        statusLabel.setText(Integer.toString(data.taxonList.getTaxonCount()) + " taxa loaded.");
+		    
+		    }
+		});
+
+	}// END: fireTaxaChanged
+
+	// ////////////////
+	// ---SIMULATE---//
+	// ////////////////
+
+	public Action getExportAction() {
+		return simulateAction;
 	}
 
-	public Action getImportAction() {
-		return importTaxaAction;
-	}
-
-	private AbstractAction importTaxaAction = new AbstractAction("Import Taxa...") {
+	private AbstractAction simulateAction = new AbstractAction("Simulate...") {
 		public void actionPerformed(ActionEvent ae) {
-			 doImport();
+			doExport();
 		}
 	};
-
-	public final void doImport() {
+	
+	public final void doExport() {
 
 		try {
 
 			JFileChooser chooser = new JFileChooser();
-			chooser.setDialogTitle("Import Tree or Alignment...");
+			chooser.setDialogTitle("Simulate...");
 			chooser.setMultiSelectionEnabled(false);
 			chooser.setCurrentDirectory(workingDirectory);
 
@@ -116,7 +205,8 @@ public class BeagleSequenceSimulatorFrame extends DocumentFrame {
 
 			if (file != null) {
 
-				importFromFile(file);
+				collectAllSettings();
+				generateFile(file);
 
 				File tmpDir = chooser.getCurrentDirectory();
 				if (tmpDir != null) {
@@ -131,54 +221,40 @@ public class BeagleSequenceSimulatorFrame extends DocumentFrame {
 			e.printStackTrace();
 		}// END: try catch block
 
-	}// END: doImport
-	
-	private void importFromFile(File file) throws IOException, ImportException {
-		
-        BufferedReader reader = new BufferedReader(new FileReader(file));
+	}// END: doExport
 
-        String line = reader.readLine();
-        Tree tree;
+	// TODO: simulate
+	private void generateFile(File outFile) throws IOException, ImportException {
 
-        if (line.toUpperCase().startsWith("#NEXUS")) {
-            NexusImporter importer = new NexusImporter(reader);
-            tree = importer.importTree(null);
-        } else {
-            NewickImporter importer = new NewickImporter(reader);
-            tree = importer.importTree(null);
-        }
+		PrintWriter writer = new PrintWriter(new FileWriter(outFile));
 
-        data.taxonList = tree;
-        statusLabel.setText(Integer.toString(data.taxonList.getTaxonCount()) + " taxa loaded.");
-        reader.close();
+		BeagleSequenceSimulator beagleSequenceSimulator = new BeagleSequenceSimulator(
+				data.treeModel, //
+				data.createBranchSubstitutionModel(), //
+				data.createSiteRateModel(), //
+				data.createBranchRateModel(), //
+				data.createFrequencyModel(), //
+				data.replicateCount //
+		);
 
-        fireTaxaChanged();
-		
-	}//END: importFromFile
+		writer.println(beagleSequenceSimulator.simulate().toString());
+		writer.close();
 
+	}// END: generateFile
+	    
 	@Override
 	public JComponent getExportableComponent() {
 		JComponent exportable = null;
-		Component comp = tabbedPane.getSelectedComponent();
+		Component component = tabbedPane.getSelectedComponent();
 
-		if (comp instanceof Exportable) {
-			exportable = ((Exportable) comp).getExportableComponent();
-		} else if (comp instanceof JComponent) {
-			exportable = (JComponent) comp;
+		if (component instanceof Exportable) {
+			exportable = ((Exportable) component).getExportableComponent();
+		} else if (component instanceof JComponent) {
+			exportable = (JComponent) component;
 		}
 
 		return exportable;
 	}// END: getExportableComponent
-
-	@Override
-	protected void initializeComponents() {
-
-		setSize(new Dimension(800, 600));
-		setMinimumSize(new Dimension(260, 100));
-
-		taxaPanel = new TaxaPanel(this, data);
-
-	}// END: initializeComponents
 
 	@Override
 	protected boolean readFromFile(File arg0) throws IOException {
@@ -192,6 +268,15 @@ public class BeagleSequenceSimulatorFrame extends DocumentFrame {
 		return false;
 	}
 
+	private void collectAllSettings() {
+		
+		// TODO: collect all settings from panels
+		frequencyPanel.collectSettings();
+		simulationPanels.collectSettings();
+		substModelPanel.collectSettings();
+		
+	}// END: collectAllSettings
+	
 	public void dataSelectionChanged(boolean isSelected) {
 		if (isSelected) {
 			getDeleteAction().setEnabled(true);
@@ -207,23 +292,9 @@ public class BeagleSequenceSimulatorFrame extends DocumentFrame {
 	public void setWorkingDirectory(File workingDirectory) {
 		this.workingDirectory = workingDirectory;
 	}// END: setWorkingDirectory
-
-	//TODO: does not work
-	public void fireTaxaChanged() {
-		
-		SwingUtilities.invokeLater(new Runnable() {
-		    public void run() {
-
-		        validate();
-		        repaint();
-		    
-		    }
-		});
-
-	}// END: fireTaxaChanged
 	
 	public void fireModelChanged() {
-		modelsPanel.collectSettings();
+		substModelPanel.collectSettings();
 	}
 	
 }// END: class

6ad48d50a42dff27d3582f47de4808fb0ade0e45
rambaut <rambaut@ca793f91-a31e-0410-b540-2769d408b6a1>
2012-08-06 13:38:24 +0000

    DistributionLikelihoods evaluate early if they wrap a UniformDistribution or another distribution with an offset > 0.0. These are likely to return -Inf for some of there parameter range and be used as calibration priors.

--- branches/1.7/src/dr/inferencexml/distribution/PriorParsers.java
+++ branches/1.7/src/dr/inferencexml/distribution/PriorParsers.java
@@ -74,7 +74,7 @@ public class PriorParsers {
                 throw new XMLParseException("Uniform prior " + xo.getName() + " cannot take a bound at infinity, " +
                         "because it returns 1/(high-low) = 1/inf");
 
-            DistributionLikelihood likelihood = new DistributionLikelihood(new UniformDistribution(lower, upper));
+            DistributionLikelihood likelihood = new DistributionLikelihood(new UniformDistribution(lower, upper), true);
             for (int j = 0; j < xo.getChildCount(); j++) {
                 if (xo.getChild(j) instanceof Statistic) {
                     likelihood.addData((Statistic) xo.getChild(j));

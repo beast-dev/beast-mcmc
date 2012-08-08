65f3167ec7ede4652bbaa1f00228f2bbaba2a0b2
rambaut <rambaut@users.noreply.github.com>
2012-08-08 11:06:12 +0000

    Adding attributions.

--- src/dr/evomodel/epidemiology/DynamicalSystem.java
+++ src/dr/evomodel/epidemiology/DynamicalSystem.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
 
+/**
+ * @author Trevor Bedford
+ */
 public class DynamicalSystem {
 
     private List<DynamicalVariable> variables = new ArrayList<DynamicalVariable>();

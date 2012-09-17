3b2adfa2950c8d72ef18d0e85044da4212943aba
Filip Bielejec <filip.bielejec@rega.kuleuven.be>
2012-09-17 08:11:18 +0000

    partition object, no parser yet

---
+++ src/dr/app/bss/Partition.java
@@ -0,0 +1,59 @@
+package dr.app.bss;
+
+import dr.app.beagle.evomodel.sitemodel.BranchSubstitutionModel;
+import dr.app.beagle.evomodel.sitemodel.GammaSiteRateModel;
+import dr.app.beagle.evomodel.substmodel.FrequencyModel;
+import dr.evolution.sequence.Sequence;
+import dr.evomodel.branchratemodel.BranchRateModel;
+import dr.evomodel.tree.TreeModel;
+
+public class Partition {
+
+	public int from;
+	public int to; 
+	public int every;
+	
+	public TreeModel treeModel;
+	public BranchSubstitutionModel branchSubstitutionModel;
+	public GammaSiteRateModel siteModel;
+	public BranchRateModel branchRateModel;
+	public FrequencyModel freqModel;
+	
+	public int partitionSiteCount;
+	
+	public boolean hasAncestralSequence = false;
+	public Sequence ancestralSequence = null;
+	
+	public Partition(
+			TreeModel treeModel, //
+			BranchSubstitutionModel branchSubstitutionModel,
+			GammaSiteRateModel siteModel, //
+			BranchRateModel branchRateModel, //
+			FrequencyModel freqModel, //
+			int from, //
+			int to, //
+			int every //
+	) {
+		
+		this.treeModel = treeModel;
+		this.siteModel = siteModel;
+		this.freqModel = freqModel;
+		this.branchSubstitutionModel = branchSubstitutionModel;
+		this.branchRateModel = branchRateModel;
+		
+		this.from = from - 1;
+		this.to = to;// + 1;
+		this.every = every;
+		
+		partitionSiteCount = ((to - from) / every);
+		
+	}//END: Constructor
+	
+	public void setAncestralSequence(Sequence ancestralSequence) {
+		this.ancestralSequence = ancestralSequence;
+		this.hasAncestralSequence = true;
+	}// END: setAncestralSequence
+	
+	
+	
+}//END: class

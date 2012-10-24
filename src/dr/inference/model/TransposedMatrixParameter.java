080cfb395740ae381ecb2f91bf5bb763daeb2f70
mtolkoff@gmail.com <mtolkoff>
2012-10-24 20:02:15 +0000

    Adds commented out broken code

--- src/dr/inference/model/TransposedMatrixParameter.java
+++ src/dr/inference/model/TransposedMatrixParameter.java
@@ -34,11 +34,37 @@ import java.util.List;
  */
 public class TransposedMatrixParameter extends MatrixParameter {
 
+
+
     public TransposedMatrixParameter(String name) {
         super(name + ".transpose");
     }
 
-    public double getParameterValue(int row, int col) {
+    public TransposedMatrixParameter(String name, MatrixParameter param)
+    {super(name + ".transpose");
+        System.err.println("before");
+        this.setRowDimension(param.getRowDimension());
+        this.setColumnDimension(param.getColumnDimension());
+
+        for(int i=0; i<param.getColumnDimension(); i++)
+        {
+
+            Parameter parameter=param.getParameter(i);
+            System.err.println(param.getParameter(i).getDimension());
+            this.addParameter(parameter);}
+        System.err.println(param.getColumnDimension());
+        System.err.println(param.getRowDimension());
+        System.err.println(getRowDimension());
+        System.err.println(getColumnDimension());
+        System.err.println("middle");
+
+        /*for(int i=1; i<getRowDimension(); i++){
+            for(int j=0; j<getColumnDimension(); j++){
+                this.addParameter(param.getParameter(j*getColumnDimension()+i));
+            }}*/
+     System.err.println("after");}
+
+    /*public double getParameterValue(int row, int col) {
         return super.getParameterValue(col, row);
     }
 
@@ -48,6 +74,11 @@ public class TransposedMatrixParameter extends MatrixParameter {
 
     public int getColumnDimension() {
         return getParameter(0).getDimension();
+    }*/
+
+    public void test()
+    {
+        System.err.println("I can read this!");
     }
 
     public Parameter getParameter(int index) {
@@ -57,6 +88,7 @@ public class TransposedMatrixParameter extends MatrixParameter {
             for (int i = 0; i < getColumnDimension(); ++i) {
                 VectorSliceParameter thisSlice = new VectorSliceParameter("name", i);
                 for (int j = 0; j < getRowDimension(); ++j) {
+                    //does same thing each pass through i?
                     thisSlice.addParameter(super.getParameter(j));
                 }
                 slices.add(thisSlice);
@@ -65,9 +97,21 @@ public class TransposedMatrixParameter extends MatrixParameter {
         return slices.get(index);
     }
 
-    public double getParameterValue(int dim) {
-        throw new RuntimeException("Not yet implemented");
+    /*public double getParameterValue(int row, int col) {
+        return getParameter(row).getParameterValue(col);
     }
+
+    public double[][] getParameterAsMatrix()
+    {   final int J = getRowDimension();
+        final int I = getColumnDimension();
+        double[][] parameterAsMatrix = new double[I][J];
+        for (int i = 0; i < I; i++) {
+            for (int j = 0; j < J; j++)
+                parameterAsMatrix[i][j] = getParameterValue(i, j);
+        }
+        return parameterAsMatrix;
+    } */
+
 
     private List<Parameter> slices = null;
 

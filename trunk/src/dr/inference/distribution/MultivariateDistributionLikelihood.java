d649f05ec9665b54f1de23cfd43e392c50edc1b4
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2012-10-28 02:36:09 +0000

    Implemented transposed matrix representations for multilocus branch rate models

--- trunk/src/dr/inference/distribution/MultivariateDistributionLikelihood.java
+++ trunk/src/dr/inference/distribution/MultivariateDistributionLikelihood.java
@@ -1,7 +1,7 @@
 /*
  * MultivariateDistributionLikelihood.java
  *
- * Copyright (C) 2002-2009 Alexei Drummond and Andrew Rambaut
+ * Copyright (c) 2002-2012 Alexei Drummond, Andrew Rambaut and Marc Suchard
  *
  * This file is part of BEAST.
  * See the NOTICE file distributed with this work for additional
@@ -12,10 +12,10 @@
  * published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  *
- * BEAST is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with BEAST; if not, write to the
@@ -74,7 +74,7 @@ public class MultivariateDistributionLikelihood extends AbstractDistributionLike
         super.addData(data);
 
         if (data instanceof Variable) {
-            ((DefaultModel)getModel()).addVariable((Variable)data);
+            ((DefaultModel) getModel()).addVariable((Variable) data);
         }
     }
 
@@ -280,9 +280,9 @@ public class MultivariateDistributionLikelihood extends AbstractDistributionLike
                     if (cxo.getChild(j) instanceof Parameter) {
                         Parameter data = (Parameter) cxo.getChild(j);
                         if (data instanceof MatrixParameter) {
-                            MatrixParameter matrix = (MatrixParameter)data;
+                            MatrixParameter matrix = (MatrixParameter) data;
                             if (matrix.getParameter(0).getDimension() != mean.getDimension())
-                                throw new XMLParseException("dim(" + data.getStatisticName() + ") = " + matrix.getColumnDimension()
+                                throw new XMLParseException("dim(" + data.getStatisticName() + ") = " + matrix.getParameter(0).getDimension()
                                         + " is not equal to dim(" + mean.getStatisticName() + ") = " + mean.getDimension()
                                         + " in " + xo.getName() + "element");
 

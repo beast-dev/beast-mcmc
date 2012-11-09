9cc5226fb24dbc75a17d71fe382b163b84dbcdec
Filip Bielejec <filip.bielejec@rega.kuleuven.be>
2012-11-09 11:08:34 +0000

    working on UI

---
+++ src/dr/app/bss/PartitionDataList.java
@@ -0,0 +1,35 @@
+package dr.app.bss;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import dr.evolution.util.Taxa;
+import dr.evolution.util.TaxonList;
+import dr.evomodel.tree.TreeModel;
+
+@SuppressWarnings("serial")
+public class PartitionDataList extends ArrayList<PartitionData> {
+
+	public static final String VERSION = "0.0.1";
+	public static final String DATE_STRING = "2012";
+	
+	public TaxonList taxonList = new Taxa();
+	public int sequenceLength = 1000;	
+	
+	public List<File> treeFilesList = new ArrayList<File>();
+	public List<TreeModel> treeModelsList= new ArrayList<TreeModel>();
+	
+	public PartitionDataList() {
+		super();
+	}
+	
+//	public int getSequenceLength() {
+//		return sequenceLength;
+//	}
+//	
+//	public void setSequenceLength(int sequenceLength) {
+//		this.sequenceLength=sequenceLength;
+//	}
+	
+}//END:class

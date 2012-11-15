5650cb4590ccb09f5a1ef6aafd1153ca2c6e5bf1
Filip Bielejec <filip.bielejec@rega.kuleuven.be>
2012-11-15 19:17:55 +0000

    preparing to switch to new interface

--- src/dr/app/beagle/tools/Partition.java
+++ src/dr/app/beagle/tools/Partition.java
@@ -39,51 +39,54 @@ import dr.evomodel.tree.TreeModel;
 public class Partition {
 
 	public int from;
-	public int to; 
+	public int to;
 	public int every;
+
+	private BranchSubstitutionModel branchModel;
 	
 	public TreeModel treeModel;
-	public BranchSubstitutionModel branchSubstitutionModel;
 	public GammaSiteRateModel siteModel;
 	public BranchRateModel branchRateModel;
 	public FrequencyModel freqModel;
-	
+
 	public boolean hasAncestralSequence = false;
 	public Sequence ancestralSequence = null;
 
-//	public static final ThreadLocal threadLocal = new ThreadLocal();
-	
-	public Partition(
-			TreeModel treeModel, //
-			BranchSubstitutionModel branchSubstitutionModel,
-			GammaSiteRateModel siteModel, //
+	// public static final ThreadLocal threadLocal = new ThreadLocal();
+
+	public Partition(TreeModel treeModel, //
+			BranchSubstitutionModel branchModel, GammaSiteRateModel siteModel, //
 			BranchRateModel branchRateModel, //
 			FrequencyModel freqModel, //
 			int from, //
 			int to, //
 			int every //
 	) {
-		
+
 		this.treeModel = treeModel;
 		this.siteModel = siteModel;
 		this.freqModel = freqModel;
-		this.branchSubstitutionModel = branchSubstitutionModel;
+		this.branchModel = branchModel;
 		this.branchRateModel = branchRateModel;
-		
+
 		this.from = from;
 		this.to = to;
 		this.every = every;
-		
-	}//END: Constructor
-	
+
+	}// END: Constructor
+
 	public void setAncestralSequence(Sequence ancestralSequence) {
 		this.ancestralSequence = ancestralSequence;
 		this.hasAncestralSequence = true;
 	}// END: setAncestralSequence
-	
+
 	public int getPartitionSiteCount() {
 		int partitionSiteCount = ((to - from) / every) + 1;
 		return partitionSiteCount;
 	}// END: getPartitionSiteCount
-	
-}//END: class
+
+	public BranchSubstitutionModel getBranchModel() {
+		return this.branchModel;
+	}// END: getBranchModel
+
+}// END: class

bd60c8bd0d3de1630e9b37f8843fbc9069696c49
Trevor Bedford <trvrb@users.noreply.github.com>
2013-01-11 15:04:45 +0000

    Implement statistic that records drifted locations.  This required storing virusDates and serumDates parameters from AntigenicLikelihood and passing these, plus the locationDriftParameter, to the new DriftedLocationsStatistic.

---
+++ src/dr/evomodel/antigenic/DriftedLocationsStatistic.java
@@ -0,0 +1,112 @@
+package dr.evomodel.antigenic;
+
+import dr.inference.model.*;
+import dr.xml.*;
+
+/**
+ * @author Trevor Bedford
+ * @author Marc A. Suchard
+ */
+public class DriftedLocationsStatistic extends Statistic.Abstract implements VariableListener {
+
+    public static final String DRIFTED_LOCATIONS_STATISTIC = "driftedLocationsStatistic";
+
+    public DriftedLocationsStatistic(MatrixParameter locationsParameter, Parameter offsetParameter, Parameter locationDriftParameter) {
+        this.locationsParameter = locationsParameter;
+        locationsParameter.addParameterListener(this);
+        this.offsetParameter = offsetParameter;
+        offsetParameter.addParameterListener(this);
+        this.locationDriftParameter = locationDriftParameter;
+        locationDriftParameter.addParameterListener(this);
+    }
+
+    public int getDimension() {
+        return locationsParameter.getDimension();
+    }
+
+    // strain index
+    public int getColumnIndex(int dim) {
+        return dim / locationsParameter.getRowDimension();
+    }
+
+    // dimension index
+    public int getRowIndex(int dim) {
+        int x = getColumnIndex(dim);
+        return dim - x * locationsParameter.getRowDimension();
+    }
+
+    public double getStatisticValue(int dim) {
+
+        double val;
+
+        // x is location count, y is location dimension
+        int x = getColumnIndex(dim);
+        int y = getRowIndex(dim);
+        Parameter loc = locationsParameter.getParameter(x);
+
+        if (y == 0) {
+            val = loc.getParameterValue(y) + locationDriftParameter.getParameterValue(0) * offsetParameter.getParameterValue(x);
+        }
+        else {
+            val = loc.getParameterValue(y);
+        }
+
+
+        return val;
+
+    }
+
+    public String getDimensionName(int dim) {
+        return locationsParameter.getDimensionName(dim);
+    }
+
+    public void variableChangedEvent(Variable variable, int index, Parameter.ChangeType type) {
+        // do nothing
+    }
+
+    public static XMLObjectParser PARSER = new AbstractXMLObjectParser() {
+
+        public final static String LOCATIONS = "locations";
+        public final static String OFFSET = "offset";
+        public final static String LOCATION_DRIFT = "locationDrift";
+
+        public String getParserName() {
+            return DRIFTED_LOCATIONS_STATISTIC;
+        }
+
+        public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+
+            MatrixParameter locations = (MatrixParameter) xo.getElementFirstChild(LOCATIONS);
+            Parameter offset = (Parameter) xo.getElementFirstChild(OFFSET);
+            Parameter locationDrift = (Parameter) xo.getElementFirstChild(LOCATION_DRIFT);
+            return new DriftedLocationsStatistic(locations, offset, locationDrift);
+
+        }
+
+        //************************************************************************
+        // AbstractXMLObjectParser implementation
+        //************************************************************************
+
+        public String getParserDescription() {
+            return "This element returns a statistic that shifts a matrix of locations by location drift in the first dimension.";
+        }
+
+        public Class getReturnType() {
+            return DriftedLocationsStatistic.class;
+        }
+
+        public XMLSyntaxRule[] getSyntaxRules() {
+            return rules;
+        }
+
+        private XMLSyntaxRule[] rules = new XMLSyntaxRule[]{
+            new ElementRule(LOCATIONS, MatrixParameter.class),
+            new ElementRule(OFFSET, Parameter.class),
+            new ElementRule(LOCATION_DRIFT, Parameter.class)
+        };
+    };
+
+    private MatrixParameter locationsParameter;
+    private Parameter offsetParameter;
+    private Parameter locationDriftParameter;
+}

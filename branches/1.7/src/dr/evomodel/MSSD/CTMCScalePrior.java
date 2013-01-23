c72f527aa714cbcd0051b3ee1df2190cdfad892b
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2013-01-23 18:30:26 +0000

    Merge trunk r5329 into 1.7 branch

--- branches/1.7/src/dr/evomodel/MSSD/CTMCScalePrior.java
+++ branches/1.7/src/dr/evomodel/MSSD/CTMCScalePrior.java
@@ -1,3 +1,28 @@
+/*
+ * CTMCScalePrior.java
+ *
+ * Copyright (c) 2002-2013 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
 package dr.evomodel.MSSD;
 
 import dr.evolution.tree.Tree;
@@ -6,6 +31,7 @@ import dr.inference.model.AbstractModelLikelihood;
 import dr.inference.model.Model;
 import dr.inference.model.Parameter;
 import dr.inference.model.Variable;
+import dr.math.GammaFunction;
 
 /**
  * @author Alexander V. Alekseyenko (alexander.alekseyenko@gmail.com)
@@ -19,6 +45,8 @@ public class CTMCScalePrior extends AbstractModelLikelihood {
     final private TreeModel treeModel;
     private double treeLength;
     private boolean treeLengthKnown;
+
+    private static final double logGammaOneHalf = GammaFunction.lnGamma(0.5);
 
     public CTMCScalePrior(String name, Parameter ctmcScale, TreeModel treeModel) {
         super(name);
@@ -61,8 +89,10 @@ public class CTMCScalePrior extends AbstractModelLikelihood {
 //            updateTreeLength();
 //            treeLengthKnown = true;
 //        }
+//        double totalTreeTime = treeLength;
         double totalTreeTime = Tree.Utils.getTreeLength(treeModel, treeModel.getRoot());
-        return -0.5 * Math.log(ab) - ab * totalTreeTime; // TODO Change to treeLength and confirm results
+        double logNormalization = 0.5 * Math.log(totalTreeTime) - logGammaOneHalf;
+        return logNormalization - 0.5 * Math.log(ab) - ab * totalTreeTime; // TODO Change to treeLength and confirm results
     }
 
     public void makeDirty() {

5de00b1409a105dec9b663e13a5e060842c6c98e
Filip Bielejec <filip.bielejec@rega.kuleuven.be>
2013-03-22 12:04:10 +0000

    reset idrefs after writing to XML

--- src/dr/app/bss/PartitionDataList.java
+++ src/dr/app/bss/PartitionDataList.java
@@ -7,12 +7,13 @@ import java.util.LinkedHashSet;
 import dr.evolution.util.MutableTaxonList;
 import dr.evolution.util.Taxa;
 
-@SuppressWarnings("serial")
+//@SuppressWarnings("serial")
 public class PartitionDataList extends ArrayList<PartitionData> {
+
+	private static final long serialVersionUID = -7703343661531335551L;
 
 	public MutableTaxonList taxonList = new Taxa();
 
-//	public int siteCount = 1000;
 	public int simulationsCount = 1;
 
 	public LinkedHashSet<File> forestList = new LinkedHashSet<File>();

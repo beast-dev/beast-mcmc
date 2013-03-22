f5c17a8d7db865ab4b7f4fd38e9e15edfca1b2fc
julia.pal.r@gmail.com <julia.pal.r@gmail.com@ca793f91-a31e-0410-b540-2769d408b6a1>
2013-03-22 16:33:20 +0000

    added the "tracer" analysis similar to EBSP

---
+++ src/dr/evomodel/coalescent/GPSkytrackAnalysis.java
@@ -0,0 +1,361 @@
+package dr.evomodel.coalescent;
+
+import dr.evolution.io.Importer;
+import dr.inference.model.Parameter;
+
+//import dr.evolution.io.NexusImporter;
+//import dr.evolution.io.TreeImporter;
+//import dr.evolution.tree.Tree;
+import dr.inference.trace.LogFileTraces;
+import dr.inference.trace.TraceException;
+import dr.stats.DiscreteStatistics;
+import dr.util.FileHelpers;
+import dr.util.HeapSort;
+import dr.util.TabularData;
+
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+//import java.io.PrintWriter;
+import java.util.Arrays;
+
+/**
+ * @author Joseph Heled
+ */
+public class GPSkytrackAnalysis extends TabularData {
+
+    private final double[] xPoints;
+    private final double[] means;
+    private final double[] medians;
+    private final double[] hpdLower;
+    private final double[] hpdHigh;
+    final File gvalues = FileHelpers.getFile("gvalues.txt");
+    final File locations = FileHelpers.getFile("locations.txt");
+
+    //    private final double[] HPDLevels;
+    private Parameter numGridpoints;
+    // each bin covers xPoints[-1]/coalBins.length
+//    private int[] coalBins;
+
+//    private final boolean quantiles;
+
+    public GPSkytrackAnalysis(File log,  double burnIn, Parameter numGridPoints) throws IOException, Importer.ImportException, TraceException {
+        this.numGridpoints=numGridPoints;
+        LogFileTraces ltraces = new LogFileTraces(log.getCanonicalPath(), log);
+        System.err.println("path:"+log.getCanonicalPath());
+        ltraces.loadTraces();
+        LogFileTraces gvalTraces=new LogFileTraces(gvalues.getCanonicalPath(),gvalues);
+        LogFileTraces locTraces=new LogFileTraces(locations.getCanonicalPath(),locations);
+
+        ltraces.setBurnIn(0);
+        final int runLengthIncludingBurnin = ltraces.getStateCount();
+
+        int intBurnIn = (int) Math.floor(burnIn < 1 ? runLengthIncludingBurnin * burnIn : burnIn);
+        final int nStates = runLengthIncludingBurnin - intBurnIn;
+//        System.err.println("runl"+runLengthIncludingBurnin+"burnin");
+        //intBurnIn *= ltraces.getStepSize();
+        ltraces.setBurnIn(intBurnIn * ltraces.getStepSize());
+
+        assert ltraces.getStateCount() == nStates;
+
+//        this.quantiles = quantiles;
+//        HPDLevels = new double[]{0.95};
+
+        xPoints = new double[(int) numGridPoints.getParameterValue(0)];
+        means = new double[(int) numGridPoints.getParameterValue(0)];
+        medians = new double[(int) numGridPoints.getParameterValue(0)];
+        hpdHigh = new double[(int) numGridPoints.getParameterValue(0)];
+        hpdLower = new double[(int) numGridPoints.getParameterValue(0)];
+
+        int numbPointsColumn = -1;
+        int lambdaColumn = -1;
+        int precColumn = -1;
+
+        for (int n = 0; n < ltraces.getTraceCount(); ++n) {
+            final String traceName = ltraces.getTraceName(n);
+            System.err.println("trace name"+n+"is"+traceName);
+            if (traceName.equals("skyride.points")) {
+               numbPointsColumn = n;
+            } else if (traceName.equals("skyride.lambda_bound")) {
+                lambdaColumn = n;
+            } else if (traceName.equals("skyride.precision")) {
+                precColumn = n;
+            }
+        }
+
+        if (numbPointsColumn < 0 || lambdaColumn < 0 || precColumn<0) {
+            throw new TraceException("incorrect trace column names: unable to find lambda_bound, points or precision");
+        }
+
+        double binSize = 0;
+//            double hSum = -0;
+        System.err.println("NumPoints Column"+numbPointsColumn);
+        System.err.println("lambda Column"+lambdaColumn);
+        System.err.println("precision Column"+precColumn);
+
+
+            int [] numPoints = new int[nStates];
+            double[] lambda = new double[nStates];
+            double[] kappa= new double[nStates];
+            for (int ns = 0; ns < nStates; ++ns) {
+                lambda[ns]= (Double) ltraces.getTrace(lambdaColumn).getValue(ns);
+                numPoints[ns]=(int)Math.round((Double) ltraces.getTrace(numbPointsColumn).getValue(ns));
+                kappa[ns]=(Double) ltraces.getTrace(precColumn).getValue(ns);
+
+            }
+
+
+//        double[]  test=new double[numPoints[0]];
+//        for (int ns = 0; ns < numPoints[0]; ++ns) {
+//            test[ns]= (Double) gvalTraces.getTrace(0).getValue(ns);
+//            System.err.println("test:"+test[ns]);
+////            numPoints[ns]=(Integer) ltraces.getTrace(numbPointsColumn).getValue(ns);
+////            kappa[ns]=(Double) ltraces.getTrace(precColumn).getValue(ns);
+//
+//        }
+
+
+//        System.exit(-1);
+
+//                hSum += h[0];
+//            }
+//
+//            binSize = hSum / (nStates * coalPointBins);
+//            coalBins = new int[coalPointBins];
+//            Arrays.fill(coalBins, 0);
+//
+
+//        TreeImporter[] treeImporters = new TreeImporter[treeFiles.length];
+//        final boolean isStepWise = modelType == VariableDemographicModel.Type.STEPWISE;
+//
+//        int nIndicators = 0;
+//
+//        for (int k = 0; k < treeFiles.length; ++k) {
+//
+//            // System.err.println("burnin " + treeFiles[k] + "(" + k + ")");
+//
+//            treeImporters[k] = new NexusImporter(new FileReader(treeFiles[k]));
+//            assert intBurnIn > 0;
+//            for (int z = 0; z < intBurnIn - 1; ++z) {
+//                treeImporters[k].importNextTree();
+//            }
+//            nIndicators += treeImporters[k].importNextTree().getExternalNodeCount() - 1;
+//        }
+//
+//        if (isStepWise) {
+//            nIndicators -= 1;
+//        }
+//
+//        final int nXaxisPoints = nIndicators + (isStepWise ? 1 : 0) + 1;
+//        xPoints = new double[nXaxisPoints];
+//        Arrays.fill(xPoints, 0.0);
+//
+//        int nDataPoints = 0;
+//        VDdemographicFunction[] allDemog = new VDdemographicFunction[nStates];
+//        {
+//            double[] indicators = new double[nIndicators];
+//            double[] pop = new double[nIndicators + 1];
+//            Tree[] tt = new Tree[treeFiles.length];
+//
+//            boolean match = true;
+//            for (int ns = 0; ns < nStates; ++ns) {
+//
+//                ltraces.getStateValues(ns, indicators, indicatorsFirstColumn);
+//                ltraces.getStateValues(ns, pop, populationFirstColumn);
+//
+//                if (match) {
+//                    for (int nt = 0; nt < tt.length; ++nt) {
+//                        tt[nt] = treeImporters[nt].importNextTree();
+//                        if( tt[nt] == null ) {
+//                           throw new TraceException("All NEXUS tree files should contain the same number of states");
+//                        }
+//                    }
+//                }
+//                //Get tree state number
+//                final String name1 = tt[0].getId();
+//                final int state1 = Integer.parseInt(name1.substring(name1.indexOf('_') + 1, name1.length()));
+//
+//                for (int j = 1; j < tt.length; ++j) {
+//                    final String name2 = tt[j].getId();
+//                    int state2 = Integer.parseInt(name1.substring(name2.indexOf('_') + 1, name2.length()));
+//                    if (state1 != state2) {
+//                        throw new TraceException("NEXUS tree files have different rates or corrupted!!!!");
+//                    }
+//                }
+//
+//                if ((ns + intBurnIn) * ltraces.getStepSize() == state1) {                   //Check if log state matches tree state
+//                    match = true;
+//                    final VDdemographicFunction demoFunction =
+//                            new VDdemographicFunction(tt, modelType, indicators, pop, logSpace, mid);
+//
+//                    if (restrictToNchanges >= 0 && demoFunction.numberOfChanges() != restrictToNchanges) {
+//                        continue;
+//                    }
+//
+//                    double[] xs = demoFunction.allTimePoints();
+//                    for (int k = 0; k < xs.length; ++k) {
+//                        xPoints[k + 1] += xs[k];
+//                    }
+//                    if (coalPointBins > 0) {
+//                        for (double x : xs) {
+//                            coalBins[Math.min((int) (x / binSize), coalBins.length - 1)]++;
+//                        }
+//                    }
+//                    allDemog[nDataPoints] = demoFunction;
+//                    ++nDataPoints;
+//
+//                    demoFunction.freeze();
+//                } else {
+//                    match = false;
+//                }
+//            }
+//
+//            for (int k = 0; k < xPoints.length; ++k) {
+//                xPoints[k] /= nStates;
+//            }
+//
+//            if (nStates != nDataPoints) {                                                     //Warning if log file and tree files
+//                // have different rates
+//                System.err.println("Different Rates is \"main\" and \"tree\" log files");
+//
+//            }
+//            if (nDataPoints < 10) {                                                           //Warning if number of states is not sufficient
+//                // enough to do the analysis
+//                System.err.println("Warning!!! Not Sufficient number of data points");
+//            }
+//        }
+//
+//        double[] popValues = new double[nDataPoints];
+//        means = new double[nXaxisPoints];
+//        medians = new double[nXaxisPoints];
+//        hpdLower = new double[HPDLevels.length][];
+//        hpdHigh = new double[HPDLevels.length][];
+//
+//        for (int i = 0; i < HPDLevels.length; ++i) {
+//            hpdLower[i] = new double[nXaxisPoints];
+//            hpdHigh[i] = new double[nXaxisPoints];
+//        }
+//
+//        for (int nx = 0; nx < xPoints.length; ++nx) {
+//            final double x = xPoints[nx];
+//
+//            for (int ns = 0; ns < nDataPoints; ++ns) {
+//                popValues[ns] = allDemog[ns].getDemographic(x);
+//            }
+//            int[] indices = new int[popValues.length];
+//            HeapSort.sort(popValues, indices);
+//
+//            means[nx] = DiscreteStatistics.mean(popValues);
+//            for (int i = 0; i < HPDLevels.length; ++i) {
+//                if (quantiles) {
+//                    hpdLower[i][nx] = DiscreteStatistics.quantile((1 - HPDLevels[i]) / 2, popValues, indices);
+//                    hpdHigh[i][nx] = DiscreteStatistics.quantile((1 + HPDLevels[i]) / 2, popValues, indices);
+//                } else {
+//                    final double[] hpd = DiscreteStatistics.HPDInterval(HPDLevels[i], popValues, indices);
+//                    hpdLower[i][nx] = hpd[0];
+//                    hpdHigh[i][nx] = hpd[1];
+//                }
+//            }
+//            medians[nx] = DiscreteStatistics.median(popValues, indices);
+//        }
+//
+//        if( allDemoWriter != null ) {
+//            for(double xPoint : xPoints) {
+//                allDemoWriter.print(xPoint);
+//                allDemoWriter.append(' ');
+//            }
+//
+//            for (int ns = 0; ns < nDataPoints; ++ns) {
+//                allDemoWriter.println();
+//                for(double xPoint : xPoints) {
+//                    allDemoWriter.print(allDemog[ns].getDemographic(xPoint));
+//                    allDemoWriter.append(' ');
+//                }
+//            }
+//            allDemoWriter.close();
+//        }
+    }
+
+    private final String[] columnNames = {"time", "mean", "median","upper","lower"};
+
+    public int nColumns() {
+        return 5;
+    }
+
+    public String columnName(int nColumn) {
+//        final int fixed = columnNames.length;
+//        if (nColumn < fixed) {
+            return columnNames[nColumn];
+        }
+//        nColumn -= fixed;
+//        if (nColumn < 2 * HPDLevels.length) {
+//            final double p = HPDLevels[nColumn / 2];
+//            final String s = (nColumn % 2 == 0) ? "lower" : "upper";
+//            return (quantiles ? "cpd " : "hpd ") + s + " " + Math.round(p * 100);
+//        }
+//        assert (nColumn - 2 * HPDLevels.length) == 0;
+//        return "bins";
+//    }
+
+    public int nRows() {
+        return (int) numGridpoints.getParameterValue(0);
+    }
+
+    public Object data(int nRow, int nColumn) {
+        switch (nColumn) {
+            case 0: {
+                if (nRow < xPoints.length) {
+                    return xPoints[nRow];
+                }
+                break;
+            }
+            case 1: {
+                if (nRow < means.length) {
+                    return means[nRow];
+                }
+                break;
+            }
+            case 2: {
+                if (nRow < medians.length) {
+                    return medians[nRow];
+                }
+                break;
+            }
+            case 3: {
+                if (nRow < hpdLower.length) {
+                    return hpdLower[nRow];
+                }
+                break;
+            }
+            case 4: {
+                if (nRow < hpdHigh.length) {
+                    return hpdHigh[nRow];
+                }
+                break;
+            }
+//            default: {
+//                final int j = nColumn - columnNames.length;
+//                if (j < 2 * HPDLevels.length) {
+//                    if (nRow < xPoints.length) {
+//                        final int k = j / 2;
+//                        if (0 <= k && k < HPDLevels.length) {
+//                            if (j % 2 == 0) {
+//                                return hpdLower[k][nRow];
+//                            } else {
+//                                return hpdHigh[k][nRow];
+//                            }
+//                        }
+//                    }
+//                } else {
+//                    if (nRow < coalBins.length) {
+//                        return coalBins[nRow];
+//                    }
+//                }
+//                break;
+//            }
+        }
+        return "";
+    }
+
+}

121698892a474a51be6ab788cb96da87e82ce52a
fbielejec <fbielejec@ca793f91-a31e-0410-b540-2769d408b6a1>
2013-03-24 20:43:16 +0000

    new tree table

--- trunk/src/dr/app/bss/TreesPanel.java
+++ trunk/src/dr/app/bss/TreesPanel.java
@@ -1,13 +1,12 @@
 package dr.app.bss;
 
-import java.awt.BorderLayout;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.io.File;
-
 import jam.framework.Exportable;
 import jam.panels.ActionPanel;
 import jam.table.TableRenderer;
+
+import java.awt.BorderLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
@@ -16,35 +15,29 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.SwingConstants;
-import javax.swing.event.TableModelEvent;
-import javax.swing.event.TableModelListener;
 import javax.swing.table.TableColumn;
 
-
-
+import dr.evolution.util.Taxon;
 
 @SuppressWarnings("serial")
 public class TreesPanel extends JPanel implements Exportable {
 
 	private PartitionDataList dataList;
 	private MainFrame frame;
-	
+
 	private JTable treesTable = null;
 	private TreesTableModel treesTableModel = null;
-	
+
 	private JScrollPane scrollPane;
 	private TableColumn column;
 	private int treesCount;
-	
+
 	private Action addTreeAction = new AbstractAction("+") {
 		public void actionPerformed(ActionEvent ae) {
 
 			treesTableModel.addDefaultRow();
 			setTrees();
 
-//			Utils.printTreeFileList(dataList);
-//			Utils.printPartitionDataList(dataList);
-			
 		}// END: actionPerformed
 	};
 
@@ -52,33 +45,32 @@ public class TreesPanel extends JPanel implements Exportable {
 		public void actionPerformed(ActionEvent ae) {
 			if (treesCount > 1) {
 
+				String value = dataList.treeFileList.get(treesCount - 1).getName();
+				removeTaxonWithAttributeValue(dataList, Utils.TREE_FILENAME, value);
+				
 				treesTableModel.deleteRow(treesCount - 1);
+				
+				frame.fireTaxaChanged();
 				setTrees();
-
+				
 			}
 		}// END: actionPerformed
 	};
-	
-	public TreesPanel(MainFrame frame,
-			PartitionDataList dataList) {
-		
-//		super();
-		
+
+	public TreesPanel(MainFrame frame, PartitionDataList dataList) {
+
 		this.frame = frame;
 		this.dataList = dataList;
-		
+
 		treesTable = new JTable();
 		treesTable.getTableHeader().setReorderingAllowed(false);
-		treesTable.addMouseListener(new JTableButtonMouseListener(
-				treesTable));
-		
+		treesTable.addMouseListener(new JTableButtonMouseListener(treesTable));
+
 		treesTableModel = new TreesTableModel(this.dataList, this.frame);
-//		treesTableModel.addTableModelListener(new TreesTableModelListener());
 		treesTable.setModel(treesTableModel);
-		
-		
+
 		setLayout(new BorderLayout());
-		
+
 		scrollPane = new JScrollPane(treesTable,
 				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
 				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
@@ -90,34 +82,32 @@ public class TreesPanel extends JPanel implements Exportable {
 		scrollPane.getViewport().setOpaque(false);
 
 		add(scrollPane, BorderLayout.CENTER);
-		
+
 		column = treesTable.getColumnModel().getColumn(
 				TreesTableModel.TREE_FILE_INDEX);
-		//TODO: for labels on buttons
+		// pass dataList for labels on buttons
 		column.setCellRenderer(new JTableButtonCellRenderer(this.dataList));
 		column.setCellEditor(new JTableButtonCellEditor());
 
 		column = treesTable.getColumnModel().getColumn(
 				TreesTableModel.TAXA_INDEX);
-		column.setCellRenderer(
-				new TableRenderer(SwingConstants.LEFT, new Insets(0, 2,
-						0, 2)));
-		
+		column.setCellRenderer(new TableRenderer(SwingConstants.LEFT,
+				new Insets(0, 2, 0, 2)));
+
 		column = treesTable.getColumnModel().getColumn(
 				TreesTableModel.TREES_INDEX);
-		column.setCellRenderer(
-				new TableRenderer(SwingConstants.LEFT, new Insets(0, 2,
-						0, 2)));
-		
+		column.setCellRenderer(new TableRenderer(SwingConstants.LEFT,
+				new Insets(0, 2, 0, 2)));
+
 		ActionPanel actionPanel = new ActionPanel(false);
 		actionPanel.setAddAction(addTreeAction);
 		actionPanel.setRemoveAction(removeTreeAction);
 		add(actionPanel, BorderLayout.SOUTH);
-		
+
 		setTrees();
-		
-	}//END: Constructor
-	
+
+	}// END: Constructor
+
 	private void setTrees() {
 
 		treesCount = dataList.treeFileList.size();
@@ -131,29 +121,23 @@ public class TreesPanel extends JPanel implements Exportable {
 
 		ColumnResizer.adjustColumnPreferredWidths(treesTable);
 	}// END: setPartitions
-	
-	// Listen to tree choices
-//	private class TreesTableModelListener implements TableModelListener {
-//
-//		public void tableChanged(TableModelEvent ev) {
-//
-//			if (ev.getType() == TableModelEvent.UPDATE) {
-//				int row = ev.getFirstRow();
-//				int column = ev.getColumn();
-//
-//				if (column == TreesTableModel.TREE_FILE_INDEX) {
-//
-//					File value = (File) treesTableModel.getValueAt(row,
-//							column);
-//					
-////					dataList.get(row).treeFile = value;
-//					
-//				}
-//
-//			}// END: event check
-//
-//		}// END: tableChanged
-//	}// END: InteractiveTableModelListener
+
+	private void removeTaxonWithAttributeValue(PartitionDataList dataList,
+			String attribute, String value) {
+
+		synchronized (dataList.taxonList) {
+			for (int i = 0; i < dataList.taxonList.getTaxonCount(); i++) {
+
+				Taxon taxon = dataList.taxonList.getTaxon(i);
+				if (taxon.getAttribute(attribute).toString()
+						.equalsIgnoreCase(value)) {
+					dataList.taxonList.removeTaxon(taxon);
+					i--;
+				}
+			}
+		}
+
+	}// END: removeTaxaWithAttributeValue
 	
 	@Override
 	public JComponent getExportableComponent() {

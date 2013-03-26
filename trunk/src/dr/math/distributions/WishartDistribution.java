082129dea4aad8e28dee77996f85d34b792a43c0
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2013-03-26 15:48:29 +0000

    Implemented a log-determinant routine for numerical stability and fixed getParameterValue in DiagonalMatrix

--- trunk/src/dr/math/distributions/WishartDistribution.java
+++ trunk/src/dr/math/distributions/WishartDistribution.java
@@ -1,3 +1,28 @@
+/*
+ * WishartDistribution.java
+ *
+ * Copyright (c) 2002-2013 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
 package dr.math.distributions;
 
 import dr.math.GammaFunction;
@@ -228,16 +253,17 @@ public class WishartDistribution implements MultivariateDistribution {
         double logDensity = 0;
 
         try {
-            if (!W.isPD())
-                return Double.NEGATIVE_INFINITY;
-
-            final double det = W.determinant();
-
-            if (det <= 0) {
+            if (!W.isPD()) {
                 return Double.NEGATIVE_INFINITY;
             }
 
-            logDensity = Math.log(det);
+//            final double det = W.determinant();
+
+            logDensity = W.logDeterminant();
+
+            if (Double.isInfinite(logDensity)) {
+                return Double.NEGATIVE_INFINITY;
+            }
 
             logDensity *= 0.5;
             logDensity *= df - dim - 1;

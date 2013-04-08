6dae7e2534b95049f62b9b3de7e7c7c3400e5e34
Guy Baele <GuyBaele@users.noreply.github.com>
2013-04-08 12:30:00 +0000

    Allows for an independent coalescent to be drawn given a list of taxa and a demographic model. The generated tree is then copied into the existing treeModel.

---
+++ src/dr/inference/operators/IndependentCoalescentSampler.java
@@ -0,0 +1,171 @@
+/*
+ * IndependentCoalescentSampler.java
+ *
+ * Copyright (c) 2002-2013 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package dr.inference.operators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dr.evolution.tree.SimpleTree;
+import dr.evolution.tree.Tree;
+import dr.evolution.util.Taxa;
+import dr.evolution.util.TaxonList;
+import dr.evomodel.coalescent.CoalescentSimulator;
+import dr.evomodel.coalescent.DemographicModel;
+import dr.evomodel.tree.TreeModel;
+import dr.xml.AttributeRule;
+import dr.xml.ElementRule;
+import dr.xml.XMLObject;
+import dr.xml.XMLParseException;
+import dr.xml.XMLSyntaxRule;
+
+/**
+ * An independent coalescent sampler, based on the coalescent simulator parser code.
+ *
+ * @author Guy Baele
+ * 
+ */
+public class IndependentCoalescentSampler extends SimpleMCMCOperator {
+
+	public static final String OPERATOR_NAME = "independentCoalescentSampler";
+    
+    private TreeModel treeModel;
+    private DemographicModel demoModel;
+    private XMLObject xo;
+	
+	public IndependentCoalescentSampler(XMLObject xo, TreeModel treeModel, DemographicModel demoModel, double weight) {
+		
+		this.xo = xo;
+		this.treeModel = treeModel;
+		this.demoModel = demoModel;
+		setWeight(weight);
+		
+	}
+	
+	public String getPerformanceSuggestion() {
+		return "";
+	}
+
+	public String getOperatorName() {
+		return "independentCoalescent(" + treeModel.getModelName() + ")";
+	}
+
+    /**
+     * change the parameter and return the hastings ratio.
+     */
+	public double doOperation() throws OperatorFailedException {
+		
+		CoalescentSimulator simulator = new CoalescentSimulator();
+        
+        List<TaxonList> taxonLists = new ArrayList<TaxonList>();
+
+        double rootHeight = -1.0;
+
+        // should have one child that is node
+        for (int i = 0; i < xo.getChildCount(); i++) {
+            final Object child = xo.getChild(i);
+            
+            //careful: Trees are TaxonLists ... (AER); see CoalescentSimulatorParser
+            if (child instanceof Tree) {
+                //do nothing
+            } else if (child instanceof TaxonList) {
+                //taxonLists.add((TaxonList) child);
+                taxonLists.add((Taxa) child);
+                //taxa added
+                break;
+            } 
+        }
+
+        try {
+        	
+        	Tree[] trees = new Tree[taxonLists.size()];
+            // simulate each taxonList separately
+            for (int i = 0; i < taxonLists.size(); i++) {
+                trees[i] = simulator.simulateTree(taxonLists.get(i), demoModel);
+            }
+
+            SimpleTree simTree = simulator.simulateTree(trees, demoModel, rootHeight, trees.length != 1);
+
+            //this would be the normal way to do it
+            treeModel.beginTreeEdit();
+            //now it's allowed to adjust the tree structure
+            treeModel.adoptTreeStructure(simTree);
+            //endTreeEdit() would then fire the events
+            treeModel.endTreeEdit();
+            
+        } catch (IllegalArgumentException iae) {
+            try {
+				throw new XMLParseException(iae.getMessage());
+			} catch (XMLParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
+		
+		return 0;
+	}
+	
+	public static dr.xml.XMLObjectParser PARSER = new dr.xml.AbstractXMLObjectParser() {
+		
+		public String getParserName() {
+            return OPERATOR_NAME;
+        }
+
+		public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+			
+			TreeModel treeModel = (TreeModel) xo.getChild(TreeModel.class);
+			double weight = xo.getDoubleAttribute(MCMCOperator.WEIGHT);
+	        DemographicModel demoModel = (DemographicModel) xo.getChild(DemographicModel.class);
+	        
+			return new IndependentCoalescentSampler(xo, treeModel, demoModel, weight);
+			
+		}
+		
+		//************************************************************************
+        // AbstractXMLObjectParser implementation
+        //************************************************************************
+
+		public XMLSyntaxRule[] getSyntaxRules() {
+			return rules;
+		}
+		
+		private final XMLSyntaxRule[] rules = {
+				AttributeRule.newDoubleRule(MCMCOperator.WEIGHT),
+				new ElementRule(Taxa.class),
+                new ElementRule(TreeModel.class),
+                new ElementRule(DemographicModel.class)
+        };
+
+		public String getParserDescription() {
+			return "This element returns an independence coalescent sampler from a demographic model.";
+		}
+
+		public Class getReturnType() {
+			return MCMCOperator.class;
+		}
+		
+	};
+
+}

80cece16449336ac5780a9fec22874fcba36b5eb
Filip Bielejec <filip.bielejec@rega.kuleuven.be>
2013-04-12 13:48:35 +0000

    help menu about dialog

--- src/dr/app/bss/DefaultHelpMenuFactory.java
+++ src/dr/app/bss/DefaultHelpMenuFactory.java
@@ -3,8 +3,12 @@ package dr.app.bss;
 import jam.framework.AbstractFrame;
 import jam.framework.MenuFactory;
 
+import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
@@ -18,19 +22,23 @@ public class DefaultHelpMenuFactory implements MenuFactory {
 	public void populateMenu(JMenu menu, AbstractFrame frame) {
 
 		menu.setMnemonic('H');
-		
+
 		JMenuItem item;
-		
+
 		// Setup About
 		item = new JMenuItem();
-		item.setText("About " + BeagleSequenceSimulatorApp.LONG_NAME);
+		item.setText("About " + BeagleSequenceSimulatorApp.LONG_NAME + "...");
 		item.addActionListener(new ListenAboutMenuItem());
 		menu.add(item);
-		
-		
-		
-	}//END: populateMenu
-	
+
+		// Setup Help
+		item = new JMenuItem();
+		item.setText("Help...");
+		item.addActionListener(new ListenHelpMenuItem());
+		menu.add(item);
+
+	}// END: populateMenu
+
 	@Override
 	public String getMenuName() {
 		return "Help";
@@ -44,10 +52,30 @@ public class DefaultHelpMenuFactory implements MenuFactory {
 	private class ListenAboutMenuItem implements ActionListener {
 		public void actionPerformed(ActionEvent ev) {
 
-			 AboutDialog ad = new AboutDialog();
-             ad.setVisible(true);
+			AboutDialog ad = new AboutDialog();
+			ad.setVisible(true);
 
 		}// END: actionPerformed
 	}// END: ListenAboutMenuItem
-	
-}//END: class
+
+	private class ListenHelpMenuItem implements ActionListener {
+		public void actionPerformed(ActionEvent ev) {
+
+			try {
+				Desktop.getDesktop()
+						.browse(new URI(
+								"http://www.kuleuven.ac.be/aidslab/phylogeography/home.html"));
+			} catch (IOException e) {
+				Utils.handleException(
+						e,
+						"Problem occurred while trying to open this link in your system's standard browser.");
+			} catch (URISyntaxException e) {
+				Utils.handleException(
+						e,
+						"Problem occurred while trying to open this link in your system's standard browser.");
+			}
+
+		}// END: actionPerformed
+	}// END: ListenAboutMenuItem
+
+}// END: class

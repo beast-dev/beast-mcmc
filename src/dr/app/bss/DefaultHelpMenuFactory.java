b656f6c1f49c4d306ed3a7649c90bbb2c47be793
Filip Bielejec <filip.bielejec@rega.kuleuven.be>
2013-04-12 09:03:56 +0000

    custom edit menu

---
+++ src/dr/app/bss/DefaultHelpMenuFactory.java
@@ -0,0 +1,28 @@
+package dr.app.bss;
+
+import javax.swing.JMenu;
+
+import jam.framework.AbstractFrame;
+import jam.framework.MenuFactory;
+
+public class DefaultHelpMenuFactory implements MenuFactory {
+
+	@Override
+	public String getMenuName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getPreferredAlignment() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void populateMenu(JMenu arg0, AbstractFrame arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}//END: class

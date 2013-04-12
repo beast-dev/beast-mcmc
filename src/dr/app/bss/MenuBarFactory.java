a216396212b79ec24bc9d565f8d28425661dbe0c
Filip Bielejec <filip.bielejec@rega.kuleuven.be>
2013-04-12 13:37:37 +0000

    help menu

--- src/dr/app/bss/MenuBarFactory.java
+++ src/dr/app/bss/MenuBarFactory.java
@@ -1,7 +1,5 @@
 package dr.app.bss;
 
-//import jam.framework.DefaultEditMenuFactory;
-//import jam.framework.DefaultHelpMenuFactory;
 import jam.framework.DefaultMenuBarFactory;
 
 public class MenuBarFactory extends DefaultMenuBarFactory {
@@ -9,7 +7,6 @@ public class MenuBarFactory extends DefaultMenuBarFactory {
 	public MenuBarFactory() {
 		
 		  registerMenuFactory(new DefaultFileMenuFactory());
-		  //TODO: override Edit & Help menus
 		  registerMenuFactory(new DefaultEditMenuFactory());
 		  registerMenuFactory(new DefaultHelpMenuFactory());
 		  

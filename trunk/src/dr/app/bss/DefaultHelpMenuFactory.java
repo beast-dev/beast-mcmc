403e2a13ffd6d20bfd6e682e7da0221bb4003273
fbielejec <fbielejec@ca793f91-a31e-0410-b540-2769d408b6a1>
2013-04-12 13:37:37 +0000

    help menu

--- trunk/src/dr/app/bss/DefaultHelpMenuFactory.java
+++ trunk/src/dr/app/bss/DefaultHelpMenuFactory.java
@@ -1,28 +1,53 @@
 package dr.app.bss;
 
-import javax.swing.JMenu;
-
 import jam.framework.AbstractFrame;
 import jam.framework.MenuFactory;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
 public class DefaultHelpMenuFactory implements MenuFactory {
 
+	public DefaultHelpMenuFactory() {
+	}// END: Constructor
+
+	@Override
+	public void populateMenu(JMenu menu, AbstractFrame frame) {
+
+		menu.setMnemonic('H');
+		
+		JMenuItem item;
+		
+		// Setup About
+		item = new JMenuItem();
+		item.setText("About " + BeagleSequenceSimulatorApp.LONG_NAME);
+		item.addActionListener(new ListenAboutMenuItem());
+		menu.add(item);
+		
+		
+		
+	}//END: populateMenu
+	
 	@Override
 	public String getMenuName() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Help";
 	}
 
 	@Override
 	public int getPreferredAlignment() {
-		// TODO Auto-generated method stub
-		return 0;
+		return LEFT;
 	}
 
-	@Override
-	public void populateMenu(JMenu arg0, AbstractFrame arg1) {
-		// TODO Auto-generated method stub
-		
-	}
+	private class ListenAboutMenuItem implements ActionListener {
+		public void actionPerformed(ActionEvent ev) {
 
+			 AboutDialog ad = new AboutDialog();
+             ad.setVisible(true);
+
+		}// END: actionPerformed
+	}// END: ListenAboutMenuItem
+	
 }//END: class

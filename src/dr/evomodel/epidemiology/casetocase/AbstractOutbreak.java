e9946309983c897040ac1d29c7d043331567b0e9
mdhall272@gmail.com <mdhall272@gmail.com@ca793f91-a31e-0410-b540-2769d408b6a1>
2013-04-23 17:20:24 +0000

    Big and hopefully final rewrite

--- src/dr/evomodel/epidemiology/casetocase/AbstractOutbreak.java
+++ src/dr/evomodel/epidemiology/casetocase/AbstractOutbreak.java
@@ -1,5 +1,6 @@
 package dr.evomodel.epidemiology.casetocase;
 
+import dr.evolution.tree.NodeRef;
 import dr.inference.model.AbstractModel;
 import dr.inference.model.Variable;
 
@@ -19,23 +20,6 @@ public abstract class AbstractOutbreak extends AbstractModel {
     }
 
     protected ArrayList<AbstractCase> cases;
-
-    /*Likelihood for the root if it is painted with 'farm'  */
-
-    public abstract double noTransmissionBranchLikelihood(AbstractCase farm, Integer farmInfectiousBy);
-
-    public abstract double noTransmissionBranchLogLikelihood(AbstractCase farm, Integer farmInfectiousBy);
-
-
-    /*Likelihood for a branch leading from a node painted 'parent' to a node painted 'child'. This deals with the
-    probability that farm2 is infected at the first time and infectious at the second. The calculations for farm1
-    will be done when that node is considered as a child, or as the root*/
-
-    public abstract double transmissionBranchLikelihood(AbstractCase parent, AbstractCase child, Integer childInfected,
-                                                        Integer childInfectiousBy);
-
-    public abstract double transmissionBranchLogLikelihood(AbstractCase parent, AbstractCase child, Integer childInfected,
-                                                           Integer childInfectiousBy);
 
     public ArrayList<AbstractCase> getCases(){
         return new ArrayList<AbstractCase>(cases);
@@ -57,5 +41,13 @@ public abstract class AbstractOutbreak extends AbstractModel {
         }
         return null;
     }
+
+    public abstract double P(AbstractCase[] paintings, double[] times);
+
+    public abstract double logP(AbstractCase[] paintings, double[] times);
+
+    public abstract double probInfectiousBy(AbstractCase painting, double time);
+
+    public abstract double logProbInfectiousBy(AbstractCase painting, double time);
 
 }

73a68a86341e6a61ef1e50fb2fe35bba8272a592
Guy Baele <GuyBaele@users.noreply.github.com>
2013-06-11 18:02:41 +0000

    Extending CoalescentIntervalStatisticParser towards coalescentLikelihoods.

--- src/dr/evomodelxml/coalescent/CoalescentIntervalStatisticParser.java
+++ src/dr/evomodelxml/coalescent/CoalescentIntervalStatisticParser.java
@@ -1,14 +1,20 @@
 package dr.evomodelxml.coalescent;
 
 import dr.evomodel.coalescent.CoalescentIntervalStatistic;
+import dr.evomodel.coalescent.CoalescentLikelihood;
 import dr.evomodel.coalescent.GMRFSkyrideLikelihood;
-import dr.xml.*;
+import dr.xml.AbstractXMLObjectParser;
+import dr.xml.ElementRule;
+import dr.xml.XMLObject;
+import dr.xml.XMLParseException;
+import dr.xml.XMLSyntaxRule;
+import dr.xml.XORRule;
 
-/**
- */
+
 public class CoalescentIntervalStatisticParser extends AbstractXMLObjectParser {
 
     public static final String COALESCENT_INTERVAL_STATISTIC = "coalescentIntervalStatistic";
+    public static final String GMRFLIKELIHOOD = "gmrfSkyrideLikelihood";
 
     public String getParserDescription() {
         return "";
@@ -20,13 +26,18 @@ public class CoalescentIntervalStatisticParser extends AbstractXMLObjectParser {
 
     public XMLSyntaxRule[] getSyntaxRules() {
         return new XMLSyntaxRule[]{
+        		new XORRule(
                 new ElementRule(GMRFSkyrideLikelihood.class),
+        		new ElementRule(CoalescentLikelihood.class))
         };
     }
 
     public Object parseXMLObject(XMLObject xo) throws XMLParseException {
-        return new CoalescentIntervalStatistic(
-                (GMRFSkyrideLikelihood)xo.getChild(GMRFSkyrideLikelihood.class));
+    	if (xo.getChild(GMRFSkyrideLikelihood.class) != null) {
+    		return new CoalescentIntervalStatistic((GMRFSkyrideLikelihood)xo.getChild(GMRFSkyrideLikelihood.class));
+    	} else {
+    		return new CoalescentIntervalStatistic((CoalescentLikelihood)xo.getChild(CoalescentLikelihood.class));
+    	}
     }
 
     public String getParserName() {

371a83a8af0c82b29bc2804daa70df4ba34cae51
Marc Suchard <msuchard@ucla.edu>
2013-06-19 21:29:51 +0000

    Implemented Muse Gaut 1994 codon model.

---
+++ src/dr/app/beagle/evomodel/substmodel/MG94CodonModel.java
@@ -0,0 +1,98 @@
+/*
+ * MG94CodonModel.java
+ *
+ * Copyright (c) 2002-2013 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package dr.app.beagle.evomodel.substmodel;
+
+import dr.evolution.datatype.Codons;
+import dr.inference.model.Parameter;
+
+/**
+ * Muse-Gaut model of codon evolution
+ *
+ * @author Marc A. Suchard
+ * @author Guy Baele
+ */
+public class MG94CodonModel extends AbstractCodonModel {
+
+    protected Parameter alphaParameter;
+    protected Parameter betaParameter;
+
+    public MG94CodonModel(Codons codonDataType, Parameter alphaParameter, Parameter betaParameter,
+                          FrequencyModel freqModel) {
+        this(codonDataType, alphaParameter, betaParameter, freqModel,
+                new DefaultEigenSystem(codonDataType.getStateCount()));
+    }
+
+    public MG94CodonModel(Codons codonDataType,
+                          Parameter alphaParameter,
+                          Parameter betaParameter,
+                          FrequencyModel freqModel, EigenSystem eigenSystem) {
+        super(codonDataType, freqModel, eigenSystem);
+
+        this.alphaParameter = alphaParameter;
+        addVariable(alphaParameter);
+        alphaParameter.addBounds(new Parameter.DefaultBounds(Double.POSITIVE_INFINITY, 0.0,
+                alphaParameter.getDimension()));
+
+        this.betaParameter = betaParameter;
+        addVariable(betaParameter);
+        betaParameter.addBounds(new Parameter.DefaultBounds(Double.POSITIVE_INFINITY, 0.0,
+                betaParameter.getDimension()));
+
+    }
+
+    public double getAlpha() {
+        return alphaParameter.getParameterValue(0);
+    }
+
+    public double getBeta() {
+        return betaParameter.getParameterValue(0);
+    }
+
+    protected void setupRelativeRates(double[] rates) {
+
+        double alpha = getAlpha();
+        double beta = getBeta();
+        for (int i = 0; i < rateCount; i++) {
+            switch (rateMap[i]) {
+                case 0:
+                    rates[i] = 0.0;
+                    break;            // codon changes in more than one codon position
+                case 1:
+                    rates[i] = alpha;
+                    break;        // synonymous transition
+                case 2:
+                    rates[i] = alpha;
+                    break;        // synonymous transversion
+                case 3:
+                    rates[i] = beta;
+                    break;         // non-synonymous transition
+                case 4:
+                    rates[i] = beta;
+                    break;            // non-synonymous transversion
+            }
+        }
+    }
+}

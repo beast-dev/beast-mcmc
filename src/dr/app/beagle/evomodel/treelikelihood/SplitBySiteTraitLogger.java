475554f0721be7fdfa7860a936794c5f4d6c9336
Marc Suchard <msuchard@ucla.edu>
2013-06-20 20:13:00 +0000

    Removed code duplication through use of AncestralStateTraitProvider interface.

--- src/dr/app/beagle/evomodel/treelikelihood/SplitBySiteTraitLogger.java
+++ src/dr/app/beagle/evomodel/treelikelihood/SplitBySiteTraitLogger.java
@@ -51,62 +51,7 @@ public class SplitBySiteTraitLogger extends TreeTraitProvider.Helper implements 
     public static final String TRAIT_NAME = "traitName";
     public static final String SCALE = "scaleByBranchLength";
 
-    public SplitBySiteTraitLogger(final AncestralStateBeagleTreeLikelihood treeLikelihood, String traitName, boolean scale) throws XMLParseException {
-        TreeTrait trait = treeLikelihood.getTreeTrait(traitName);
-        if (trait == null) {
-            throw new XMLParseException("TreeTraitProvider does not provide trait named '" + traitName + ".");
-        }
-
-        TreeModel tree = treeLikelihood.getTreeModel();
-        int length;
-        Object obj = trait.getTrait(tree, tree.getNode(0));
-
-        boolean isDoubleArray = false;
-        boolean isIntegerArray = false;
-
-        if (obj instanceof double[]) {
-            length = ((double[]) obj).length;
-            isDoubleArray = true;
-        } else if (obj instanceof int[]) {
-            length = ((int[]) obj).length;
-            isIntegerArray = true;
-        } else {
-            throw new XMLParseException("Unknown trait type to split");
-        }
-
-        TreeTrait[] partitionedTraits = new TreeTrait[length];
-        if (isDoubleArray) {
-            for (int i = 0; i < length; i++) {
-                if (scale) {
-                    partitionedTraits[i] = new TreeTrait.PickEntryDAndScale(trait, i);
-                } else {
-                    partitionedTraits[i] = new TreeTrait.PickEntryD(trait, i);
-                }
-            }
-        } else if (isIntegerArray) {
-            for (int i = 0; i < length; ++i) {
-                if (traitName.compareTo(AncestralStateTreeLikelihood.STATES_KEY) == 0) {
-                    partitionedTraits[i] = new TreeTrait.PickEntryI(trait, i) {
-                        public String getTraitString(Tree tree, NodeRef node) {
-                            int[] state = new int[1];
-                            state[0] = getTrait(tree, node);
-                            return treeLikelihood.formattedState(state);
-                        }
-                    };
-                } else {
-                    partitionedTraits[i] = new TreeTrait.PickEntryI(trait, i);
-                }
-            }
-        }
-        addTraits(partitionedTraits);
-
-        Logger.getLogger("dr.app.beagle").info("\tConstructing a split logger with " + length + " partitions;  please cite:\n"
-                + Citable.Utils.getCitationString(this));
-
-    }
-
-    // TODO Horrible temporary code duplication
-    public SplitBySiteTraitLogger(final OldAncestralStateBeagleTreeLikelihood treeLikelihood, String traitName, boolean scale) throws XMLParseException {
+    public SplitBySiteTraitLogger(final AncestralStateTraitProvider treeLikelihood, String traitName, boolean scale) throws XMLParseException {
         TreeTrait trait = treeLikelihood.getTreeTrait(traitName);
         if (trait == null) {
             throw new XMLParseException("TreeTraitProvider does not provide trait named '" + traitName + ".");
@@ -172,15 +117,10 @@ public class SplitBySiteTraitLogger extends TreeTraitProvider.Helper implements 
         public Object parseXMLObject(XMLObject xo) throws XMLParseException {
 
             String traitName = xo.getStringAttribute(TRAIT_NAME);
-            AncestralStateBeagleTreeLikelihood tree = (AncestralStateBeagleTreeLikelihood) xo.getChild(AncestralStateBeagleTreeLikelihood.class);
-            OldAncestralStateBeagleTreeLikelihood oldTree = (OldAncestralStateBeagleTreeLikelihood) xo.getChild(OldAncestralStateBeagleTreeLikelihood.class);
+            AncestralStateTraitProvider tree = (AncestralStateTraitProvider) xo.getChild(AncestralStateTraitProvider.class);
             boolean scale = xo.getAttribute(SCALE, false);
 
-            if (tree != null) {
-                return new SplitBySiteTraitLogger(tree, traitName, scale);
-            } else {
-                return new SplitBySiteTraitLogger(oldTree, traitName, scale);
-            }
+            return new SplitBySiteTraitLogger(tree, traitName, scale);
         }
 
         //************************************************************************
@@ -192,10 +132,7 @@ public class SplitBySiteTraitLogger extends TreeTraitProvider.Helper implements 
         }
 
         private final XMLSyntaxRule[] rules = {
-                new OrRule(
-                        new ElementRule(OldAncestralStateBeagleTreeLikelihood.class),
-                        new ElementRule(AncestralStateBeagleTreeLikelihood.class, "The tree which is to be logged")
-                ),
+                new ElementRule(AncestralStateTraitProvider.class, "The tree which is to be logged"),
                 AttributeRule.newStringRule(TRAIT_NAME),
                 AttributeRule.newBooleanRule(SCALE, true),
         };

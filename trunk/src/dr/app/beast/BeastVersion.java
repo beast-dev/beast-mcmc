051e4320bfdf40727a956047e66863462ccf1c98
rambaut <rambaut@ca793f91-a31e-0410-b540-2769d408b6a1>
2013-10-10 10:57:04 +0000

    Updating version number

--- trunk/src/dr/app/beast/BeastVersion.java
+++ trunk/src/dr/app/beast/BeastVersion.java
@@ -38,13 +38,15 @@ import dr.util.Version;
  *
  * @author Alexei Drummond
  * @author Andrew Rambaut
+ *
+ * $Id$
  */
 public class BeastVersion implements Version {
 
     /**
      * Version string: assumed to be in format x.x.x
      */
-    private static final String VERSION = "1.8.0pre";
+    private static final String VERSION = "1.8.0";
 
     private static final String DATE_STRING = "2002-2013";
 

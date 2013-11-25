dcb93d513b92b17677dc124ce7bdd0d5c8f88861
Guy Baele <GuyBaele@users.noreply.github.com>
2013-11-25 13:06:37 +0000

    CoalescentTreeIntervalStatistic no longer outputs zero-length intervals.

--- src/dr/evomodel/coalescent/CoalescentTreeIntervalStatistic.java
+++ src/dr/evomodel/coalescent/CoalescentTreeIntervalStatistic.java
@@ -33,29 +33,64 @@ import dr.evolution.coalescent.TreeIntervals;
 import dr.evolution.tree.Tree;
 import dr.inference.model.Statistic;
 
-
+/**
+* @author Guy Baele
+*/
 public class CoalescentTreeIntervalStatistic extends Statistic.Abstract {
 
     private Tree tree;
     private TreeIntervals intervals;
+    private int dimension;
+    private double[] nonZeroIntervals;
 
     public CoalescentTreeIntervalStatistic(Tree tree) {
         this.tree = tree;
         this.intervals = new TreeIntervals(tree);
+        //detect here if there are zero-length intervals and set the dimension accordingly
+        setDimension(intervals);
+        this.nonZeroIntervals = new double[this.dimension];
+        removeZeroLengthIntervals(intervals);
     }
 
     public int getDimension() {
-    	return intervals.getIntervalCount();
+    	return this.dimension;
     }
 
     public double getStatisticValue(int i) {
     	//need more elegant way to trigger recalculation of intervals
     	if (i == 0) {
     		intervals.setIntervalsUnknown();
+    		removeZeroLengthIntervals(intervals);
     	}
     	
-        double interval = intervals.getInterval(i);
-
-        return interval;
+        //double interval = intervals.getInterval(i);
+    	//return interval;
+    	
+    	return this.nonZeroIntervals[i];
+    }
+    
+    public String getStatisticName() {
+    	return "coalescentTreeIntervalStatistic";
+    }
+    
+    private void setDimension(TreeIntervals intervals) {
+    	int dim = intervals.getIntervalCount();
+    	for (int i = 0; i < intervals.getIntervalCount(); i++) {
+    		if (intervals.getInterval(i) == 0.0) {
+    			dim--;
+    		}
+    	}
+    	this.dimension = dim;
+    }
+    
+    private void removeZeroLengthIntervals(TreeIntervals intervals) {
+    	int dim = 0;
+    	for (int i = 0; i < intervals.getIntervalCount(); i++) {
+    		double intervalLength = intervals.getInterval(i);
+    		if (intervalLength != 0.0) {
+    			this.nonZeroIntervals[dim] = intervalLength;
+    			dim++;
+    		}
+    	}
     }
 }

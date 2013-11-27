716a695cfb5b8546f22afaf5abf8f7bdd9d03af2
Marc Suchard <msuchard@ucla.edu>
2013-11-27 17:25:33 +0000

    Simple parser example and package structure for latent factor models.

--- src/dr/evomodel/continuous/LatentFactorModel.java
+++ src/dr/evomodel/continuous/LatentFactorModel.java
@@ -1,21 +1,45 @@
+/*
+ * LatentFactorModel.java
+ *
+ * Copyright (c) 2002-2013 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
 package dr.evomodel.continuous;
 
 import dr.inference.model.Parameter;
-import dr.xml.*;
+
 /**
- * Created with IntelliJ IDEA.
- * User: max
- * Date: 10/28/13
- * Time: 1:58 PM
- * To change this template use File | Settings | File Templates.
+ * @author Max Tolkoff
+ * @author Marc Suchard
  */
+
 public class LatentFactorModel {
     private Parameter data;
     private Parameter factor;
     private Parameter latent;
-    public LatentFactorModel(Parameter dataIn, Parameter factorIn, Parameter latentIn){
-        data=dataIn;
-        factor=factorIn;
-        latent=latentIn;
+
+    public LatentFactorModel(Parameter dataIn, Parameter factorIn, Parameter latentIn) {
+        data = dataIn;
+        factor = factorIn;
+        latent = latentIn;
     }
 }

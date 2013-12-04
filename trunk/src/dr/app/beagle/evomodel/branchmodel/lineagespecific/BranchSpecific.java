0220051a6dfe07d6124952e5f89c73e9a6add513
fbielejec <fbielejec@ca793f91-a31e-0410-b540-2769d408b6a1>
2013-12-04 13:21:17 +0000

    main method for future testing

--- trunk/src/dr/app/beagle/evomodel/branchmodel/lineagespecific/BranchSpecific.java
+++ trunk/src/dr/app/beagle/evomodel/branchmodel/lineagespecific/BranchSpecific.java
@@ -1,26 +1,43 @@
 package dr.app.beagle.evomodel.branchmodel.lineagespecific;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import dr.app.beagle.evomodel.branchmodel.BranchModel;
+import dr.app.beagle.evomodel.branchmodel.HomogeneousBranchModel;
 import dr.app.beagle.evomodel.branchmodel.BranchModel.Mapping;
+import dr.app.beagle.evomodel.sitemodel.GammaSiteRateModel;
 import dr.app.beagle.evomodel.substmodel.AbstractCodonModel;
 import dr.app.beagle.evomodel.substmodel.FrequencyModel;
 import dr.app.beagle.evomodel.substmodel.MG94CodonModel;
 import dr.app.beagle.evomodel.substmodel.SubstitutionModel;
+import dr.app.beagle.evomodel.treelikelihood.BeagleTreeLikelihood;
+import dr.app.beagle.evomodel.treelikelihood.PartialsRescalingScheme;
+import dr.app.beagle.tools.BeagleSequenceSimulator;
+import dr.app.beagle.tools.Partition;
+import dr.evolution.alignment.Alignment;
+import dr.evolution.alignment.ConvertAlignment;
+import dr.evolution.datatype.Codons;
+import dr.evolution.datatype.GeneticCode;
+import dr.evolution.datatype.Nucleotides;
+import dr.evolution.io.Importer.ImportException;
+import dr.evolution.io.NewickImporter;
 import dr.evolution.tree.NodeRef;
 import dr.evolution.tree.Tree;
 import dr.evomodel.branchratemodel.AbstractBranchRateModel;
+import dr.evomodel.branchratemodel.BranchRateModel;
 import dr.evomodel.branchratemodel.CountableBranchCategoryProvider;
+import dr.evomodel.branchratemodel.DefaultBranchRateModel;
 import dr.evomodel.tree.TreeModel;
 import dr.inference.model.AbstractModel;
 import dr.inference.model.Model;
 import dr.inference.model.Parameter;
 import dr.inference.model.Variable;
 import dr.inference.model.Variable.ChangeType;
+import dr.math.MathUtils;
 
 public class BranchSpecific 
 extends AbstractModel
@@ -188,5 +205,89 @@ implements BranchModel {
 		
 	}
 
+	 public static void main(String[] args) {
+		 
+	try {	
+		
+		 // the seed of the BEAST
+         MathUtils.setSeed(666);
+		 
+         // create tree
+         NewickImporter importer = new NewickImporter(
+                 "(SimSeq1:73.7468,(SimSeq2:25.256989999999995,SimSeq3:45.256989999999995):18.48981);");
+         TreeModel tree = new TreeModel(importer.importTree(null));
+		 
+      // create site model
+         GammaSiteRateModel siteRateModel = new GammaSiteRateModel(
+                 "siteModel");
+
+         // create branch rate model
+         BranchRateModel branchRateModel = new DefaultBranchRateModel();
+         
+         int sequenceLength = 10;
+         ArrayList<Partition> partitionsList = new ArrayList<Partition>();
+
+         // create Frequency Model
+       Parameter freqs = new Parameter.Default(new double[]{
+     	0.0163936, // 
+     	0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, //
+         0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, //
+     	0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, //
+     	0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, //
+     	0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344, 0.01639344 //
+     	});
+         FrequencyModel freqModel = new FrequencyModel(Codons.UNIVERSAL,
+                 freqs);
+
+         // create substitution model
+         Parameter alpha = new Parameter.Default(1, 10);
+         Parameter beta = new Parameter.Default(1, 5);
+         MG94CodonModel mg94 = new MG94CodonModel(Codons.UNIVERSAL, alpha, beta, freqModel);
+         
+         HomogeneousBranchModel substitutionModel = new HomogeneousBranchModel(mg94);
+
+         // create partition
+         Partition partition1 = new Partition(tree, //
+                 substitutionModel,//
+                 siteRateModel, //
+                 branchRateModel, //
+                 freqModel, //
+                 0, // from
+                 sequenceLength - 1, // to
+                 1 // every
+         );
+
+		partitionsList.add(partition1);
+
+		// feed to sequence simulator and generate data
+		BeagleSequenceSimulator simulator = new BeagleSequenceSimulator(
+				partitionsList);
+
+		Alignment alignment = simulator.simulate(false);
+
+		ConvertAlignment convert = new ConvertAlignment(Nucleotides.INSTANCE,
+				GeneticCode.UNIVERSAL, alignment);
+		
+		Parameter ratesParameter = new Parameter.Default(1, 0.0001);
+		CountableBranchCategoryProvider rateCategories = new CountableBranchCategoryProvider.IndependentBranchCategoryModel(tree, ratesParameter);
+		
+		BranchSpecific branchSpecific = new BranchSpecific(tree, mg94, rateCategories);
+		
+		BeagleTreeLikelihood nbtl = new BeagleTreeLikelihood(convert, //
+				tree, //
+				branchSpecific, //
+				siteRateModel, //
+				branchRateModel, //
+				null, //
+				false, //
+				PartialsRescalingScheme.DEFAULT);
+
+		System.out.println("likelihood = " + nbtl.getLogLikelihood());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}// END: main
 	
 }// END: class

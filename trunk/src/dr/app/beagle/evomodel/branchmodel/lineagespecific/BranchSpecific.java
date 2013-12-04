acd51822b25d40c47d011d89a132f55fc070475d
fbielejec <fbielejec@ca793f91-a31e-0410-b540-2769d408b6a1>
2013-12-04 13:00:48 +0000

    lets do it like this for now

--- trunk/src/dr/app/beagle/evomodel/branchmodel/lineagespecific/BranchSpecific.java
+++ trunk/src/dr/app/beagle/evomodel/branchmodel/lineagespecific/BranchSpecific.java
@@ -7,73 +7,92 @@ import java.util.Map;
 
 import dr.app.beagle.evomodel.branchmodel.BranchModel;
 import dr.app.beagle.evomodel.branchmodel.BranchModel.Mapping;
+import dr.app.beagle.evomodel.substmodel.AbstractCodonModel;
 import dr.app.beagle.evomodel.substmodel.FrequencyModel;
+import dr.app.beagle.evomodel.substmodel.MG94CodonModel;
 import dr.app.beagle.evomodel.substmodel.SubstitutionModel;
 import dr.evolution.tree.NodeRef;
 import dr.evolution.tree.Tree;
 import dr.evomodel.branchratemodel.AbstractBranchRateModel;
 import dr.evomodel.branchratemodel.CountableBranchCategoryProvider;
 import dr.evomodel.tree.TreeModel;
+import dr.inference.model.AbstractModel;
 import dr.inference.model.Model;
 import dr.inference.model.Parameter;
 import dr.inference.model.Variable;
 import dr.inference.model.Variable.ChangeType;
 
-public class BranchSpecific extends AbstractBranchRateModel implements BranchModel {
+public class BranchSpecific 
+extends AbstractModel
+//extends AbstractBranchRateModel 
+implements BranchModel {
 
 	private Parameter rateParameter;
 	
 	private Map<NodeRef, Mapping> nodeMap = new HashMap<NodeRef, Mapping>();
-	private SubstitutionModel codonModel;
+	private SubstitutionModel substModel;
 	private boolean setupMapping = true;
 	
 	private CountableBranchCategoryProvider rateCategories; 
 	private TreeModel treeModel;
+	private FrequencyModel freqModel;
 	
-	public BranchSpecific(TreeModel treeModel, SubstitutionModel codonModel,CountableBranchCategoryProvider rateCategories) {
+	public BranchSpecific(TreeModel treeModel, SubstitutionModel substModel,CountableBranchCategoryProvider rateCategories) {
 		
 		super("");
 		
 		this.treeModel = treeModel;
-		this.codonModel = codonModel;
-
+		this.substModel = substModel;
 		this.rateCategories = rateCategories;
+		
+		this.freqModel = substModel.getFrequencyModel();
+		
 		
 	}
 	
-	@Override
-	public double getBranchRate(Tree tree, NodeRef node) {
-		
-		//form product? or draw from binomial for alpha and beta?
-		
-		
-		int rateCategory = rateCategories.getBranchCategory(treeModel, node);
-        double value = rateParameter.getParameterValue(rateCategory);
-		
-		
-		return value;
-	}
-
-	@Override
-	protected void handleModelChangedEvent(Model model, Object object, int index) {
-	}
-
-	@Override
-	protected void handleVariableChangedEvent(Variable variable, int index,
-			ChangeType type) {
-	}
-
-	@Override
-	protected void storeState() {
-	}
-
-	@Override
-	protected void restoreState() {
-	}
-
-	@Override
-	protected void acceptState() {
-	}
+//	public BranchSpecific(TreeModel treeModel, SubstitutionModel codonModel,CountableBranchCategoryProvider rateCategories) {
+//		
+//		super("");
+//		
+//		this.treeModel = treeModel;
+//		this.codonModel = codonModel;
+//
+//		this.rateCategories = rateCategories;
+//		
+//	}
+//	
+//	@Override
+//	public double getBranchRate(Tree tree, NodeRef node) {
+//		
+//		//form product? or draw from binomial for alpha and beta?
+//		
+//		int rateCategory = rateCategories.getBranchCategory(treeModel, node);
+//        double value = rateParameter.getParameterValue(rateCategory);
+//		
+//		
+//		return value;
+//	}
+//
+//	@Override
+//	protected void handleModelChangedEvent(Model model, Object object, int index) {
+//	}
+//
+//	@Override
+//	protected void handleVariableChangedEvent(Variable variable, int index,
+//			ChangeType type) {
+//	}
+//
+//	@Override
+//	protected void storeState() {
+//	}
+//
+//	@Override
+//	protected void restoreState() {
+//	}
+//
+//	@Override
+//	protected void acceptState() {
+//	}
 
 	@Override
 	public Mapping getBranchModelMapping(NodeRef branch) {
@@ -88,7 +107,17 @@ public class BranchSpecific extends AbstractBranchRateModel implements BranchMod
 	
 	public void setupNodeMap(NodeRef branch) {
 		
+		//form product? or draw from binomial for alpha and beta?
 		
+		int rateCategory = rateCategories.getBranchCategory(treeModel, branch);
+        double value = rateParameter.getParameterValue(rateCategory);
+        
+        Parameter alphaParameter = new Parameter.Default(1,   ((MG94CodonModel) substModel).getAlpha() * value);
+        Parameter betaParameter = new Parameter.Default(1,   ((MG94CodonModel) substModel).getBeta() * value);
+        
+//		codonModel = new MG94CodonModel(codonDataType, alphaParameter, betaParameter, freqModel);
+		
+        
 		nodeMap.put(branch, new Mapping() {
 			@Override
 			public int[] getOrder() {
@@ -105,14 +134,16 @@ public class BranchSpecific extends AbstractBranchRateModel implements BranchMod
 	@Override
 	public List<SubstitutionModel> getSubstitutionModels() {
 		List<SubstitutionModel> list = new ArrayList<SubstitutionModel>();
-		list.add(codonModel);
+		
+		
+		list.add( substModel);
 		
 		return list;
 	}
 
 	@Override
 	public SubstitutionModel getRootSubstitutionModel() {
-		return codonModel;
+		return substModel;
 	}
 
 	@Override
@@ -124,6 +155,37 @@ public class BranchSpecific extends AbstractBranchRateModel implements BranchMod
 	@Override
 	public boolean requiresMatrixConvolution() {
 		return false;
+	}
+
+	@Override
+	protected void handleModelChangedEvent(Model model, Object object, int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void handleVariableChangedEvent(Variable variable, int index,
+			ChangeType type) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void storeState() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void restoreState() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void acceptState() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	

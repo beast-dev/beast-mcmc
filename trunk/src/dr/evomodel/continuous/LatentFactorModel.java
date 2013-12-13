7fd2900c3a0269b1b1b609dee4d59cc5750f584f
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2013-12-13 19:08:25 +0000

    Latent factor model structure

--- trunk/src/dr/evomodel/continuous/LatentFactorModel.java
+++ trunk/src/dr/evomodel/continuous/LatentFactorModel.java
@@ -25,9 +25,13 @@
 
 package dr.evomodel.continuous;
 
-import dr.inference.model.MatrixParameter;
+import dr.inference.model.*;
 import dr.math.matrixAlgebra.IllegalDimension;
 import dr.math.matrixAlgebra.Matrix;
+import dr.util.Citable;
+import dr.util.Citation;
+
+import java.util.List;
 
 
 /**
@@ -35,47 +39,176 @@ import dr.math.matrixAlgebra.Matrix;
  * @author Marc Suchard
  */
 
-public class LatentFactorModel {
-    private Matrix data;
-    private Matrix factors;
-    private Matrix loadings;
+public class LatentFactorModel extends AbstractModelLikelihood implements Citable {
+//    private Matrix data;
+//    private Matrix factors;
+//    private Matrix loadings;
+
     private Matrix residual;
 
+    private final CompoundParameter data;
+    private final CompoundParameter factors;
+    private final MatrixParameter loadings;
+    private final MatrixParameter precision;
 
+    private final int dimFactors;
+    private final int dimData;
+    private final int nTaxa;
 
-    public LatentFactorModel(MatrixParameter dataIn, MatrixParameter factorsIn, MatrixParameter loadingsIn) {
-        data = new Matrix(dataIn.getParameterAsMatrix());
-        factors = new Matrix(factorsIn.getParameterAsMatrix());
-        loadings = new Matrix(loadingsIn.getParameterAsMatrix());
+    private boolean likelihoodKnown = false;
+    private double logLikelihood;
+
+    public LatentFactorModel(CompoundParameter data, CompoundParameter factors, MatrixParameter loadings,
+                             MatrixParameter precision,
+                             int numFactors) {
+        super("");
+//        data = new Matrix(dataIn.getParameterAsMatrix());
+//        factors = new Matrix(factorsIn.getParameterAsMatrix());
+//        loadings = new Matrix(loadingsIn.getParameterAsMatrix());
+        this.data = data;
+        this.factors = factors;
+        this.loadings = loadings;
+        this.precision = precision;
+
+        addVariable(data);
+        addVariable(factors);
+        addVariable(loadings);
+
+        dimFactors = numFactors;
+        nTaxa = factors.getDimension() / dimFactors;
+        dimData = data.getDimension() / nTaxa;
+
+        // TODO Check dimensions of loadings (dimFactors x dimData)
+        // TODO dimData >= dimFactors
+
         computeResiduals();
         System.out.print(residual.toComponents()[0][0]);
     }
 
-    public Matrix getData(){
-        Matrix ans=data;
-        return ans;
+//    public Matrix getData(){
+//        Matrix ans=data;
+//        return ans;
+//    }
+//
+//    public Matrix getFactors(){
+//        Matrix ans=factors;
+//        return ans;
+//    }
+//
+//    public Matrix getLoadings(){
+//        Matrix ans=loadings;
+//        return ans;
+//    }
+//
+//    public Matrix getResidual(){
+//        Matrix ans=residual;
+//        return ans;
+//    }
+
+    private Matrix copy(CompoundParameter parameter, int dimMajor, int dimMinor) {
+        return new Matrix(parameter.getParameterValues(), dimMajor, dimMinor);
     }
 
-    public Matrix getFactors(){
-        Matrix ans=factors;
-        return ans;
-    }
+    private void computeResiduals() {
 
-    public Matrix getLoadings(){
-        Matrix ans=loadings;
-        return ans;
-    }
+        Matrix tLoadings = new Matrix(loadings.getParameterAsMatrix());
 
-    public Matrix getResidual(){
-        Matrix ans=residual;
-        return ans;
-    }
 
-    public void computeResiduals(){
+        Matrix tData = copy(data, nTaxa, dimData);
+        Matrix tFactors = copy(factors, nTaxa, dimFactors);
         try {
-            residual = data.subtract(loadings.product(factors));
+            residual = tData.subtract(tLoadings.product(tFactors));
         } catch (IllegalDimension illegalDimension) {
             illegalDimension.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
+    }
+
+    @Override
+    protected void handleModelChangedEvent(Model model, Object object, int index) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    /**
+     * Additional state information, outside of the sub-model is stored by this call.
+     */
+    @Override
+    protected void storeState() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    /**
+     * After this call the model is guaranteed to have returned its extra state information to
+     * the values coinciding with the last storeState call.
+     * Sub-models are handled automatically and do not need to be considered in this method.
+     */
+    @Override
+    protected void restoreState() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    /**
+     * This call specifies that the current state is accept. Most models will not need to do anything.
+     * Sub-models are handled automatically and do not need to be considered in this method.
+     */
+    @Override
+    protected void acceptState() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    /**
+     * This method is called whenever a parameter is changed.
+     * <p/>
+     * It is strongly recommended that the model component sets a "dirty" flag and does no
+     * further calculations. Recalculation is typically done when the model component is asked for
+     * some information that requires them. This mechanism is 'lazy' so that this method
+     * can be safely called multiple times with minimal computational cost.
+     */
+    @Override
+    protected void handleVariableChangedEvent(Variable variable, int index, Parameter.ChangeType type) {
+        likelihoodKnown = false;
+    }
+
+    /**
+     * @return a list of citations associated with this object
+     */
+    @Override
+    public List<Citation> getCitations() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    /**
+     * Get the model.
+     *
+     * @return the model.
+     */
+    @Override
+    public Model getModel() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    /**
+     * Get the log likelihood.
+     *
+     * @return the log likelihood.
+     */
+    @Override
+    public double getLogLikelihood() {
+        if (!likelihoodKnown) {
+            logLikelihood = calculateLogLikelihood();
+            likelihoodKnown = true;
+        }
+        return logLikelihood;
+    }
+
+    /**
+     * Forces a complete recalculation of the likelihood next time getLikelihood is called
+     */
+    @Override
+    public void makeDirty() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    private double calculateLogLikelihood() {
+        return -2.0;
     }
 }

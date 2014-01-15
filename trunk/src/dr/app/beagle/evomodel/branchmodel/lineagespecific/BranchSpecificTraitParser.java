fd87c11f0e89d7fe0a1914d63aaa367179d7940b
fbielejec <fbielejec@ca793f91-a31e-0410-b540-2769d408b6a1>
2014-01-15 16:08:37 +0000

    cleaned up the code

--- trunk/src/dr/app/beagle/evomodel/branchmodel/lineagespecific/BranchSpecificTraitParser.java
+++ trunk/src/dr/app/beagle/evomodel/branchmodel/lineagespecific/BranchSpecificTraitParser.java
@@ -1,10 +1,6 @@
 package dr.app.beagle.evomodel.branchmodel.lineagespecific;
 
-import dr.app.beagle.evomodel.substmodel.FrequencyModel;
-import dr.evomodel.tree.TreeModel;
-import dr.inference.model.AbstractModel;
 import dr.inference.model.CompoundParameter;
-import dr.inference.model.Parameter;
 import dr.xml.AbstractXMLObjectParser;
 import dr.xml.ElementRule;
 import dr.xml.XMLObject;
@@ -13,18 +9,6 @@ import dr.xml.XMLSyntaxRule;
 
 public class BranchSpecificTraitParser extends AbstractXMLObjectParser {
 
-	
-	/*
-	<fancyNewTreeTraitProvider id=“omegaPerBranch”>  <!— returns parameter(i) per branch —>
-	<branchCategoryModel idref=“filipModel”/>  <!— returns 0, 1, 2, etc per branch —>
-	<compoundParameter>
-		<parameter idref=“omega1”/>
-		<parameter idref=“omega2”/>   <!— implicit contract that parameters are listed in correct order,  could make explicit in XML at some point with an attribute —>
-	</compoundParameter>
-< fancyNewTreeTraitProvider>
-	*/
-	
-	
 	public static final String BRANCH_SPECIFIC_TRAIT = "branchSpecificTrait";
 
 	@Override

73e9261f1cec51d65504b653473e06533a135fe2
Graham853@gmail.com <Graham853@gmail.com@ca793f91-a31e-0410-b540-2769d408b6a1>
2014-03-07 15:39:55 +0000

    Citation updated. GPL notices added.

--- trunk/src/dr/evomodel/speciation/BirthDeathCollapseNClustersStatistic.java
+++ trunk/src/dr/evomodel/speciation/BirthDeathCollapseNClustersStatistic.java
@@ -1,6 +1,23 @@
+/*
+        This file is part of BEAST.
+
+        BEAST is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        BEAST is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with DISSECT.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package dr.evomodel.speciation;
 
-import dr.evolution.tree.NodeRef;
+
 import dr.evomodelxml.speciation.BirthDeathCollapseNClustersStatisticParser;
 import dr.inference.model.Statistic;
 

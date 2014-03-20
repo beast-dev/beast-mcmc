75d292a67c4f47ceacf33788c8d0266d89097c96
Marc Suchard <msuchard@ucla.edu>
2014-03-20 18:36:30 +0000

    Updated BinaryCovarionModelTest to handle hidden frequencies in different ways (delegated to enum); will use to examine Will Chang's model patch.

--- src/test/dr/evomodel/substmodel/BinaryCovarionModelTest.java
+++ src/test/dr/evomodel/substmodel/BinaryCovarionModelTest.java
@@ -1,9 +1,37 @@
+/*
+ * BinaryCovarionModelTest.java
+ *
+ * Copyright (c) 2002-2014 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
 package test.dr.evomodel.substmodel;
 
-import dr.evomodel.substmodel.*;
 import dr.evolution.datatype.DataType;
 import dr.evolution.datatype.TwoStateCovarion;
 import dr.evolution.datatype.TwoStates;
+import dr.evomodel.substmodel.BinaryCovarionModel;
+import dr.evomodel.substmodel.FrequencyModel;
+import dr.evomodel.substmodel.GeneralSubstitutionModel;
+import dr.evomodel.substmodel.SubstitutionModelUtils;
 import dr.inference.model.Parameter;
 import dr.math.matrixAlgebra.IllegalDimension;
 import dr.math.matrixAlgebra.Matrix;
@@ -33,7 +61,8 @@ public class BinaryCovarionModelTest extends TestCase {
         alpha = new Parameter.Default(0.0);
         switchingRate = new Parameter.Default(1.0);
 
-        model = new BinaryCovarionModel(TwoStateCovarion.INSTANCE, frequencies, hiddenFrequencies, alpha, switchingRate);
+        model = new BinaryCovarionModel(TwoStateCovarion.INSTANCE, frequencies, hiddenFrequencies, alpha, switchingRate,
+                BinaryCovarionModel.Version.VERSION1);
         dataType = model.getDataType();
     }
 

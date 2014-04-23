c797a07188e0877cb1636e575f60990c77a8d71a
rambaut <rambaut@users.noreply.github.com>
2014-04-23 15:30:15 +0000

    Added latent state proportions as a loggable tree trait

--- src/dr/evolution/tree/Tree.java
+++ src/dr/evolution/tree/Tree.java
@@ -1057,7 +1057,7 @@ public interface Tree extends TaxonList, Units, Identifiable, Attributable {
                 for (TreeTraitProvider ttp : treeTraitProviders) {
                     TreeTrait[] tts = ttp.getTreeTraits();
                     for (TreeTrait treeTrait: tts) {
-                        if (treeTrait.getIntent() == intent && treeTrait.getLoggable()) {
+                        if (treeTrait.getLoggable() && treeTrait.getIntent() == intent) {
                             String value = treeTrait.getTraitString(tree, node);
 
                             if (value != null) {

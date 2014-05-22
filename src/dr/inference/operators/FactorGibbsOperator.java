a4f5825b03f7ebd0edebe55f92b8c7a6bc724c79
mtolkoff@gmail.com <mtolkoff>
2014-05-22 22:16:52 +0000

    Added a function in matrix to build diagonal matrices Added functions to get elements out of the factor model Added a get precision function for the factor model gibbs sampler

--- src/dr/inference/operators/FactorGibbsOperator.java
+++ src/dr/inference/operators/FactorGibbsOperator.java
@@ -1,6 +1,10 @@
 package dr.inference.operators;
 
 import dr.evomodel.continuous.LatentFactorModel;
+import dr.inference.model.DiagonalMatrix;
+import dr.math.matrixAlgebra.IllegalDimension;
+import dr.math.matrixAlgebra.Matrix;
+import dr.inference.model.Parameter;
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,11 +14,31 @@ import dr.evomodel.continuous.LatentFactorModel;
  * To change this template use File | Settings | File Templates.
  */
 public class FactorGibbsOperator extends SimpleMCMCOperator implements GibbsOperator{
+    private static final String FACTOR_GIBBS_OPERATOR="factorGibbsOperator";
     private LatentFactorModel LFM;
+    private int numFactors=0;
+    private Matrix idMat;
     public FactorGibbsOperator(LatentFactorModel LFM, double weight){
         this.LFM=LFM;
         setWeight(weight);
     }
+
+    private Matrix getPrecision(){
+        if(numFactors!=LFM.getFactors().getRowDimension()){
+            numFactors=LFM.getFactors().getRowDimension();
+            idMat=idMat.buildIdentity(numFactors);
+        }
+        Matrix LoadMat=new Matrix(LFM.getLoadings().getParameterAsMatrix());
+        Matrix colPrec=new Matrix(LFM.getColumnPrecision().getParameterAsMatrix());
+        Matrix answer= null;
+        try {
+            answer = LoadMat.transpose().product(colPrec).product(LoadMat).add(idMat);
+        } catch (IllegalDimension illegalDimension) {
+            illegalDimension.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return answer;
+    }
+
 
 
     public int getStepCount() {
@@ -28,11 +52,13 @@ public class FactorGibbsOperator extends SimpleMCMCOperator implements GibbsOper
 
     @Override
     public String getOperatorName() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return FACTOR_GIBBS_OPERATOR;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
     @Override
     public double doOperation() throws OperatorFailedException {
+
+
         return 0;  //To change body of implemented methods use File | Settings | File Templates.
     }
 

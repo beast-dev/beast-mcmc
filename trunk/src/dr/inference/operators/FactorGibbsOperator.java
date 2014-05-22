d2926be25beef90ee520781d69fcd0e34bdba011
mtolkoff@gmail.com <mtolkoff@gmail.com@ca793f91-a31e-0410-b540-2769d408b6a1>
2014-05-22 22:42:13 +0000

    Lists Factor Gibbs in development parsers Build Diagonal Matrix is now a static Minor changes to FactorGibbsOperator (compiles but does not function)

--- trunk/src/dr/inference/operators/FactorGibbsOperator.java
+++ trunk/src/dr/inference/operators/FactorGibbsOperator.java
@@ -2,6 +2,7 @@ package dr.inference.operators;
 
 import dr.evomodel.continuous.LatentFactorModel;
 import dr.inference.model.DiagonalMatrix;
+import dr.inference.model.MatrixParameter;
 import dr.math.matrixAlgebra.IllegalDimension;
 import dr.math.matrixAlgebra.Matrix;
 import dr.inference.model.Parameter;
@@ -26,13 +27,23 @@ public class FactorGibbsOperator extends SimpleMCMCOperator implements GibbsOper
     private Matrix getPrecision(){
         if(numFactors!=LFM.getFactors().getRowDimension()){
             numFactors=LFM.getFactors().getRowDimension();
-            idMat=idMat.buildIdentity(numFactors);
+            idMat=Matrix.buildIdentity(numFactors);
         }
         Matrix LoadMat=new Matrix(LFM.getLoadings().getParameterAsMatrix());
         Matrix colPrec=new Matrix(LFM.getColumnPrecision().getParameterAsMatrix());
         Matrix answer= null;
         try {
             answer = LoadMat.transpose().product(colPrec).product(LoadMat).add(idMat);
+        } catch (IllegalDimension illegalDimension) {
+            illegalDimension.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return answer;
+    }
+
+    private Matrix getMean(){
+        Matrix answer=null;
+        try {
+            answer=getPrecision().inverse().productWithTransposed(new Matrix(LFM.getLoadings().getParameterAsMatrix())).product(new Matrix(LFM.getColumnPrecision().getParameterAsMatrix())).product(LFM.getScaledData());
         } catch (IllegalDimension illegalDimension) {
             illegalDimension.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
@@ -57,7 +68,10 @@ public class FactorGibbsOperator extends SimpleMCMCOperator implements GibbsOper
 
     @Override
     public double doOperation() throws OperatorFailedException {
-
+        Matrix mean=getMean();
+        System.out.println(mean.columns());
+        System.out.println(mean.rows());
+        MatrixParameter meanList=MatrixParameter.parseFromSymmetricDoubleArray(getMean().toComponents());
 
         return 0;  //To change body of implemented methods use File | Settings | File Templates.
     }

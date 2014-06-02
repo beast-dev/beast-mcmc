f00e2aac897732c328caa22079f527c7ace6dce0
mtolkoff@gmail.com <mtolkoff@gmail.com@ca793f91-a31e-0410-b540-2769d408b6a1>
2014-06-02 19:04:35 +0000

    Factor and loadings gibbs samplers no longer have dimensionality issues Fixed addition error message in Matrix class

--- src/dr/inference/operators/FactorGibbsOperator.java
+++ src/dr/inference/operators/FactorGibbsOperator.java
@@ -32,7 +32,7 @@ public class FactorGibbsOperator extends SimpleMCMCOperator implements GibbsOper
         Matrix colPrec=new Matrix(LFM.getColumnPrecision().getParameterAsMatrix());
         Matrix answer= null;
         try {
-            answer = LoadMat.transpose().product(colPrec).product(LoadMat).add(idMat);
+            answer = LoadMat.product(colPrec).product(LoadMat.transpose()).add(idMat);
         } catch (IllegalDimension illegalDimension) {
             illegalDimension.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
@@ -42,7 +42,7 @@ public class FactorGibbsOperator extends SimpleMCMCOperator implements GibbsOper
     private Matrix getMean(){
         Matrix answer=null;
         try {
-            answer=getPrecision().inverse().productWithTransposed(new Matrix(LFM.getLoadings().getParameterAsMatrix())).product(new Matrix(LFM.getColumnPrecision().getParameterAsMatrix())).product(LFM.getScaledData());
+            answer=getPrecision().inverse().product(new Matrix(LFM.getLoadings().getParameterAsMatrix())).product(new Matrix(LFM.getColumnPrecision().getParameterAsMatrix())).product(LFM.getScaledData());
         } catch (IllegalDimension illegalDimension) {
             illegalDimension.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }

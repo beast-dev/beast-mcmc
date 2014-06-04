b0204b35f1718a9fcda82c71fa14ab40c7beee6a
mtolkoff@gmail.com <mtolkoff>
2014-06-04 17:23:34 +0000

    Focuses the loadings gibbs operator more on Matrix Parameters

--- src/dr/inference/operators/LoadingsGibbsOperator.java
+++ src/dr/inference/operators/LoadingsGibbsOperator.java
@@ -2,6 +2,7 @@ package dr.inference.operators;
 
 import dr.evomodel.continuous.LatentFactorModel;
 import dr.inference.distribution.DistributionLikelihood;
+import dr.inference.model.DiagonalMatrix;
 import dr.inference.model.MatrixParameter;
 import dr.inference.model.Parameter;
 import dr.math.distributions.MultivariateNormalDistribution;
@@ -20,71 +21,117 @@ import dr.math.matrixAlgebra.Vector;
 public class LoadingsGibbsOperator extends SimpleMCMCOperator implements GibbsOperator{
     NormalDistribution prior;
     LatentFactorModel LFM;
+    DiagonalMatrix[] singleElement;
+    MatrixParameter[] innerProductAnswer;
+    MatrixParameter[] vectorProductAnswer;
+    MatrixParameter[] priorMeanVector;
 
         public LoadingsGibbsOperator(LatentFactorModel LFM, DistributionLikelihood prior, double weight){
         setWeight(weight);
 
         this.prior=(NormalDistribution) prior.getDistribution();
         this.LFM=LFM;
+        singleElement=new DiagonalMatrix[LFM.getLoadings().getRowDimension()];
+            for (int i = 0; i <singleElement.length ; i++) {
+                singleElement[i]=DiagonalMatrix.buildIdentityTimesElementMatrix(i+1, 1/(this.prior.getSD()*this.prior.getSD()));
+            }
+        innerProductAnswer=new MatrixParameter[LFM.getLoadings().getRowDimension()];
+            for (int i = 0; i <innerProductAnswer.length ; i++) {
+                innerProductAnswer[i]=new MatrixParameter(null);
+                innerProductAnswer[i].setDimensions(i+1,i+1);
+            }
+
+        vectorProductAnswer=new MatrixParameter[LFM.getLoadings().getRowDimension()];
+            for (int i = 0; i <vectorProductAnswer.length ; i++) {
+                vectorProductAnswer[i]=new MatrixParameter(null);
+                vectorProductAnswer[i].setDimensions(i+1, 1);
+            }
+
+        priorMeanVector=new MatrixParameter[LFM.getLoadings().getRowDimension()];
+            for (int i = 0; i <priorMeanVector.length ; i++) {
+                priorMeanVector[i]=new MatrixParameter(null, i+1, 1, this.prior.getMean()/(this.prior.getSD()*this.prior.getSD()));
+
+
+            }
     }
 
-    private Matrix truncateMatrixParameter(MatrixParameter full, int i){
-        double[][] answerArray=new double[i][full.getColumnDimension()];
-        for (int j = 0; j <i ; j++) {
-        answerArray[j]=full.getRowValues(j);
-        }
-//        System.out.println(answerArray.length);
+    private MatrixParameter truncatedInnerProductInPlace(MatrixParameter full, int newRowDimension, MatrixParameter answer){
 
-        return new Matrix(answerArray);
-    }
+//        MatrixParameter answer=new MatrixParameter(null);
+//        answer.setDimensions(this.getRowDimension(), Right.getRowDimension());
+//        System.out.println(answer.getRowDimension());
+//        System.out.println(answer.getColumnDimension());
 
-    private Matrix getPrecision(int i){
-        Matrix factors=null;
-        int size=LFM.getFactorDimension();
-        Matrix answer=null;
-        if(i<size){
-            factors=truncateMatrixParameter(LFM.getFactors(),i+1);
-            try {
-                answer= Matrix.buildIdentityTimesElementMatrix(i + 1, 1 / (prior.getSD() * prior.getSD())).add(factors.productWithTransposed(factors).product(LFM.getColumnPrecision().getParameterValue(i, i)));
-            } catch (IllegalDimension illegalDimension) {
-                illegalDimension.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        int p = full.getColumnDimension();
+        for (int i = 0; i < newRowDimension; i++) {
+            for (int j = 0; j < newRowDimension; j++) {
+                double sum = 0;
+                for (int k = 0; k < p; k++)
+                    sum += full.getParameterValue(i, k) * full.getParameterValue(j,k);
+                answer.setParameterValueQuietly(i,j, sum);
             }
         }
+
+        return answer;
+
+    }
+
+    private MatrixParameter truncatedMatrixProductWithTransposedWithVectorInPlace(MatrixParameter Left, double[] Right, int newRowDimension, MatrixParameter answer){
+
+//        MatrixParameter answer=new MatrixParameter(null);
+//        answer.setDimensions(this.getRowDimension(), Right.getRowDimension());
+//        System.out.println(answer.getRowDimension());
+//        System.out.println(answer.getColumnDimension());
+
+        int p = Left.getColumnDimension();
+        for (int i = 0; i < newRowDimension; i++) {
+            double sum = 0;
+                for (int k = 0; k < p; k++)
+                    sum += Left.getParameterValue(i, k) * Right[k];
+                answer.setParameterValueQuietly(i,0, sum);
+            }
+
+
+        return answer;
+
+    }
+
+    private MatrixParameter getPrecision(int i){
+        int size=LFM.getFactorDimension();
+        MatrixParameter answer=null;
+        if(i<size){
+            answer= singleElement[i].add(truncatedInnerProductInPlace(LFM.getFactors(), i + 1, innerProductAnswer[i]).product(LFM.getColumnPrecision().getParameterValue(i, i)));
+        }
         else{
-                factors=new Matrix(LFM.getFactors().getParameterAsMatrix());
-                try {
-                    answer= Matrix.buildIdentityTimesElementMatrix(size, 1/(prior.getSD()*prior.getSD())).add(factors.productWithTransposed(factors).product(LFM.getColumnPrecision().getParameterValue(i, i)));
-                } catch (IllegalDimension illegalDimension) {
-                    illegalDimension.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-                }
+             answer= singleElement[size-1].add(innerProductAnswer[size-1].productWithTransposed(innerProductAnswer[size-1]).product(LFM.getColumnPrecision().getParameterValue(i, i)));
             }
         return answer;
     }
 
-    private Vector getMean(int i, Matrix precision){
-        Matrix factors=null;
+    private Matrix getMean(int i, Matrix precision){
+//        Matrix factors=null;
+        MatrixParameter FxY;
         int size=LFM.getFactorDimension();
-        Vector answer=null;
-        Vector dataColumn=null;
-        Vector priorVector=null;
-        Vector temp=null;
-        Matrix data=new Matrix(LFM.getScaledData().getParameterAsMatrix());
+        Matrix answer=null;
+        double[] scaledDataColumn=LFM.getScaledData().getRowValues(i);
+//        Vector dataColumn=null;
+//        Vector priorVector=null;
+//        Vector temp=null;
+//        Matrix data=new Matrix(LFM.getScaledData().getParameterAsMatrix());
         if(i<size){
-            factors=truncateMatrixParameter(LFM.getFactors(), i+1);
-            dataColumn=new Vector(data.toComponents()[i]);
-            priorVector=Vector.buildOneTimesElementVector(i+1, prior.getMean()/(prior.getSD()*prior.getSD()));
+            FxY=truncatedMatrixProductWithTransposedWithVectorInPlace(LFM.getFactors(), scaledDataColumn, i+1, vectorProductAnswer[i]);
+//            dataColumn=new Vector(data.toComponents()[i]);
             try {
-                answer=precision.inverse().product(priorVector.add(factors.product(dataColumn)));
+                answer=precision.inverse().product(new Matrix(priorMeanVector[i].add(FxY).getParameterAsMatrix()));
             } catch (IllegalDimension illegalDimension) {
                 illegalDimension.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             }
         }
         else{
-            factors=new Matrix(LFM.getFactors().getParameterAsMatrix());
-            dataColumn=new Vector(data.toComponents()[i]);
-            priorVector=Vector.buildOneTimesElementVector(size, prior.getMean()/(prior.getSD()*prior.getSD()));
+            FxY=truncatedMatrixProductWithTransposedWithVectorInPlace(LFM.getFactors(), scaledDataColumn, size, vectorProductAnswer[size-1]);
+//            dataColumn=new Vector(data.toComponents()[i]);
             try {
-                answer=precision.inverse().product(priorVector.add(factors.product(dataColumn)));
+                answer=precision.inverse().product(new Matrix(priorMeanVector[size-1].add(FxY).getParameterAsMatrix()));
             } catch (IllegalDimension illegalDimension) {
                 illegalDimension.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             }
@@ -123,12 +170,12 @@ public class LoadingsGibbsOperator extends SimpleMCMCOperator implements GibbsOp
         double[] draws=null;
         int size = LFM.getLoadings().getColumnDimension();
         for (int i = 0; i < size; i++) {
-            Matrix precision=getPrecision(i);
-            Vector mean=getMean(i, precision);
-            draws= MultivariateNormalDistribution.nextMultivariateNormalPrecision(mean.toComponents(),precision.toComponents());
+            MatrixParameter precision=getPrecision(i);
+            Matrix mean=getMean(i, new Matrix(precision.getParameterAsMatrix()));
+            draws= MultivariateNormalDistribution.nextMultivariateNormalPrecision(mean.toComponents()[0],precision.getParameterAsMatrix());
             if(i<draws.length){
                 while(draws[i]<0){
-                    draws= MultivariateNormalDistribution.nextMultivariateNormalPrecision(mean.toComponents(),precision.toComponents());
+                    draws= MultivariateNormalDistribution.nextMultivariateNormalPrecision(mean.toComponents()[0],precision.getParameterAsMatrix());
                 }
             }
             copy(i, draws);

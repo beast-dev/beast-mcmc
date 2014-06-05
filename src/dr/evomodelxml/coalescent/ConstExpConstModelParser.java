3c8c55738f5c22d3a7d8d8a19ca0bd5d44d91594
rambaut <rambaut@users.noreply.github.com>
2014-06-05 14:03:25 +0000

    Attempting to get ConExpCon to work again.

--- src/dr/evomodelxml/coalescent/ConstExpConstModelParser.java
+++ src/dr/evomodelxml/coalescent/ConstExpConstModelParser.java
@@ -16,9 +16,7 @@ public class ConstExpConstModelParser extends AbstractXMLObjectParser {
     public static String POPULATION_SIZE = "populationSize";
     public static String ANCESTRAL_POPULATION_SIZE = "ancestralPopulationSize";
     public static String FINAL_PHASE_START_TIME = "finalPhaseStartTime";
-
-    public static String GROWTH_RATE = "growthRate";
-    public static String DOUBLING_TIME = "doublingTime";
+    public static String GROWTH_PHASE_TIME = "growthPhaseTime";
 
     public String getParserName() {
         return CONST_EXP_CONST_MODEL;
@@ -37,19 +35,10 @@ public class ConstExpConstModelParser extends AbstractXMLObjectParser {
         cxo = xo.getChild(FINAL_PHASE_START_TIME);
         Parameter timeParam = (Parameter) cxo.getChild(Parameter.class);
 
-        Parameter rParam;
-        boolean usingGrowthRate = true;
+        cxo = xo.getChild(GROWTH_PHASE_TIME);
+        Parameter epochParam = (Parameter) cxo.getChild(Parameter.class);
 
-        if (xo.getChild(GROWTH_RATE) != null) {
-            cxo = xo.getChild(GROWTH_RATE);
-            rParam = (Parameter) cxo.getChild(Parameter.class);
-        } else {
-            cxo = xo.getChild(DOUBLING_TIME);
-            rParam = (Parameter) cxo.getChild(Parameter.class);
-            usingGrowthRate = false;
-        }
-
-        return new ConstExpConstModel(N0Param, N1Param, timeParam, rParam, units, usingGrowthRate);
+        return new ConstExpConstModel(N0Param, N1Param, timeParam, epochParam, units);
     }
 
     //************************************************************************
@@ -76,17 +65,8 @@ public class ConstExpConstModelParser extends AbstractXMLObjectParser {
                     new XMLSyntaxRule[]{new ElementRule(Parameter.class)}),
             new ElementRule(FINAL_PHASE_START_TIME,
                     new XMLSyntaxRule[]{new ElementRule(Parameter.class)}),
-            new XORRule(
-
-                    new ElementRule(GROWTH_RATE,
-                            new XMLSyntaxRule[]{new ElementRule(Parameter.class)},
-                            "A value of zero represents a constant population size, negative values represent decline towards the present, " +
-                                    "positive numbers represents exponential growth towards the present. " +
-                                    "A random walk operator is recommended for this parameter with a starting value of 0.0 and no upper or lower limits."),
-                    new ElementRule(DOUBLING_TIME,
-                            new XMLSyntaxRule[]{new ElementRule(Parameter.class)},
-                            "This parameter determines the doubling time.")
-            )
+            new ElementRule(GROWTH_PHASE_TIME,
+                    new XMLSyntaxRule[]{new ElementRule(Parameter.class)}),
     };
 
 

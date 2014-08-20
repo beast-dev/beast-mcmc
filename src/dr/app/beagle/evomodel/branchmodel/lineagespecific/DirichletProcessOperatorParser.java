79b95f097a1f2cc5f84f2e8082dac7fa7705cd97
Filip Bielejec <filip.bielejec@rega.kuleuven.be>
2014-08-20 15:58:26 +0000

    commiting some of the earlier changes

--- src/dr/app/beagle/evomodel/branchmodel/lineagespecific/DirichletProcessOperatorParser.java
+++ src/dr/app/beagle/evomodel/branchmodel/lineagespecific/DirichletProcessOperatorParser.java
@@ -1,6 +1,7 @@
 package dr.app.beagle.evomodel.branchmodel.lineagespecific;
 
 import dr.inference.model.Parameter;
+import dr.inference.operators.MCMCOperator;
 import dr.xml.AbstractXMLObjectParser;
 import dr.xml.AttributeRule;
 import dr.xml.ElementRule;
@@ -11,38 +12,31 @@ import dr.xml.XMLSyntaxRule;
 public class DirichletProcessOperatorParser extends AbstractXMLObjectParser {
 
 	public static final String DIRICHLET_PROCESS_OPERATOR = "dpOperator";
-	public static final String INTENSITY = "intensity";
 	public static final String UNIQUE_REALIZATION_COUNT = "uniqueRealizationCount";
-	public static final String CATEGORY_PROBABILITIES = "categoryProbabilities";
 	
 	@Override
 	public Object parseXMLObject(XMLObject xo) throws XMLParseException {
 
+		DirichletProcessPrior dpp = (DirichletProcessPrior) xo.getChild(DirichletProcessPrior.class);
+		
 		Parameter zParameter = (Parameter) xo.getChild(Parameter.class);
-		double intensity = xo.getDoubleAttribute(INTENSITY);
 		int uniqueRealizationCount = xo
 				.getIntegerAttribute(UNIQUE_REALIZATION_COUNT);
 
-		Parameter categoryProbabilitiesParameter = null;
-		if(xo.hasChildNamed(CATEGORY_PROBABILITIES)) {
-			
-			 categoryProbabilitiesParameter = (Parameter) xo.getElementFirstChild(CATEGORY_PROBABILITIES); 
-		}
+		 final double weight = xo.getDoubleAttribute(MCMCOperator.WEIGHT);
 		
-		return new DirichletProcessOperator(zParameter, categoryProbabilitiesParameter, intensity,
-				uniqueRealizationCount);
+		return new DirichletProcessOperator(dpp, zParameter,
+				uniqueRealizationCount, weight);
 	}// END: parseXMLObject
 
 	@Override
 	public XMLSyntaxRule[] getSyntaxRules() {
 		return new XMLSyntaxRule[] {
 
-				new ElementRule(CATEGORY_PROBABILITIES,
-	                    new XMLSyntaxRule[] { new ElementRule(Parameter.class, false) }, true), // 
-				
+				new ElementRule(DirichletProcessPrior.class, false),
 		new ElementRule(Parameter.class, false), //
-				AttributeRule.newDoubleRule(INTENSITY), //
-				AttributeRule.newIntegerRule(UNIQUE_REALIZATION_COUNT)
+		AttributeRule.newDoubleRule(MCMCOperator.WEIGHT), //
+		AttributeRule.newIntegerRule(UNIQUE_REALIZATION_COUNT)
 
 		};
 

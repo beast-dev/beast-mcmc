13a302a5daf130471c93d3221ef6b687159ce626
rambaut <rambaut@ca793f91-a31e-0410-b540-2769d408b6a1>
2014-08-27 16:39:33 +0000

    Pulling together the Joint Parameter UI.

--- trunk/src/dr/app/beauti/priorsPanel/AbstractPriorDialog.java
+++ trunk/src/dr/app/beauti/priorsPanel/AbstractPriorDialog.java
@@ -3,6 +3,8 @@ package dr.app.beauti.priorsPanel;
 import dr.app.beauti.options.Parameter;
 
 /**
+ * A base class for dialog boxes for setting the prior. Allows code reuse for opening these
+ * in PriorsPanel
  * @author Andrew Rambaut
  * @version $Id$
  */

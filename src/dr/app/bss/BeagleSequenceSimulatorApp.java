8e8ebe4d129fc82e9544c334214a2696e91a5f9d
Filip Bielejec <filip.bielejec@rega.kuleuven.be>
2014-09-17 08:57:54 +0000

    small fixes after testing

--- src/dr/app/bss/BeagleSequenceSimulatorApp.java
+++ src/dr/app/bss/BeagleSequenceSimulatorApp.java
@@ -24,7 +24,7 @@ public class BeagleSequenceSimulatorApp {
 	// Share those if neccessary
 	public static final String SHORT_NAME = "\u03C0BUSS";
 	public static final String LONG_NAME = "Parallel BEAST/BEAGLE Utility for Sequence Simulation";
-	public static final String VERSION = "1.3.6";
+	public static final String VERSION = "1.3.7";
 	public static final String DATE = "2014";
 
 	// Icons

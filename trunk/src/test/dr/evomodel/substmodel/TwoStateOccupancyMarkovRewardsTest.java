d862f2582ce6ecb6533dac90617ae757c2ad54ec
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2014-11-05 22:33:07 +0000

    Fixed normalization in LatentStateBranchRateModel and added necessary Jacobian for sampling over proportion of branch instead of reward value.

--- trunk/src/test/dr/evomodel/substmodel/TwoStateOccupancyMarkovRewardsTest.java
+++ trunk/src/test/dr/evomodel/substmodel/TwoStateOccupancyMarkovRewardsTest.java
@@ -25,10 +25,15 @@
 
 package test.dr.evomodel.substmodel;
 
+import dr.evomodel.branchratemodel.LatentStateBranchRateModel;
 import dr.inference.markovjumps.MarkovReward;
 import dr.inference.markovjumps.SericolaSeriesMarkovReward;
 import dr.inference.markovjumps.TwoStateOccupancyMarkovReward;
+import dr.inference.model.Parameter;
+import org.apache.commons.math.FunctionEvaluationException;
+import org.apache.commons.math.MaxIterationsExceededException;
 import org.apache.commons.math.analysis.UnivariateRealFunction;
+import org.apache.commons.math.analysis.integration.TrapezoidIntegrator;
 import test.dr.math.MathTestCase;
 
 /**
@@ -36,6 +41,8 @@ import test.dr.math.MathTestCase;
  */
 
 public class TwoStateOccupancyMarkovRewardsTest extends MathTestCase {
+
+    private static final double tolerance = 10E-3;
 
     public void testTwoStateSericolaRewards1() {
         final double rate = 0.0015;
@@ -53,15 +60,31 @@ public class TwoStateOccupancyMarkovRewardsTest extends MathTestCase {
 
     public void testTwoStateSericolaRewards2() {
         final double rate = 0.0015;
-//        final double prop = 0.5;
-        final double prop = 0.66666;
+        final double prop = 0.5;
+//        final double prop = 0.66666;
         final double branchLength = 1000.0;
         final boolean print = false;
 
-//        MarkovReward markovReward = createMarkovReward(rate, prop);
-        MarkovReward markovReward = createSericolaMarkovReward(rate, prop);
+        MarkovReward markovReward = createMarkovReward(rate, prop);
+//        MarkovReward markovReward = createSericolaMarkovReward(rate, prop);
 
         run(markovReward, rate, prop, branchLength, print, 1000);
+    }
+
+    public void testLatentStateBranchRateModel() throws FunctionEvaluationException, MaxIterationsExceededException {
+
+        LatentStateBranchRateModel model = new LatentStateBranchRateModel(
+                new Parameter.Default(0.001), new Parameter.Default(0.5));
+
+        TrapezoidIntegrator integator = new TrapezoidIntegrator();
+
+        final double branchLength = 2000;
+        double integral = integator.integrate(new LatentStateDensityFunction(model, branchLength), 0.0, 1.0);
+
+        System.out.println("testLatentStateBeanchRateModel");
+        System.out.println("Integral = " + integral);
+
+        assertEquals(integral, 1.0, tolerance);
     }
 
     private void run(MarkovReward markovReward, double rate, double prop, double branchLength,
@@ -100,16 +123,43 @@ public class TwoStateOccupancyMarkovRewardsTest extends MathTestCase {
         }
         sum *= (branchLength / 2.0 / length);
 
-        double norm = 1.0 - markovReward.computeCdf(0, branchLength, 0, 0) /
-                markovReward.computeConditionalProbability(branchLength, 0, 0);
-        sum /= norm; // TODO Normalization is missing in LatentBranchRateModel
-
+        // TODO Normalization is missing in LatentBranchRateModel
         System.out.println("branchLength = " + branchLength);
         System.out.println("rate = " + rate);
         System.out.println("prop = " + prop);
         System.out.println("Integral = " + sum);
         System.out.println("Mode = " + String.format("%3.2e", modeY) + " at " + modeX);
+
+        assertEquals(sum, 1.0, tolerance);
+
+        TrapezoidIntegrator integrator = new TrapezoidIntegrator();
+        double integral = 0.0;
+        try {
+            integral = integrator.integrate(new UnitDensityFunction(markovReward, branchLength, rate, prop), 0.0, 1.0);
+        } catch (MaxIterationsExceededException e) {
+            e.printStackTrace();
+        } catch (FunctionEvaluationException e) {
+            e.printStackTrace();
+        }
+        System.out.println("unt int = " + integral);
+        assertEquals(integral, 1.0, tolerance);
+
         System.out.println("\n");
+    }
+
+    private class LatentStateDensityFunction implements UnivariateRealFunction {
+
+        private final LatentStateBranchRateModel model;
+        private final double branchLength;
+
+        LatentStateDensityFunction(LatentStateBranchRateModel model, double branchLength) {
+            this.model = model;
+            this.branchLength = branchLength;
+        }
+
+        public double value(double prop) {
+            return model.getBranchRewardDensity(prop, branchLength);
+        }
     }
 
     private class DensityFunction implements UnivariateRealFunction {
@@ -128,8 +178,32 @@ public class TwoStateOccupancyMarkovRewardsTest extends MathTestCase {
 
         @Override
         public double value(double v) { //throws FunctionEvaluationException {
-            return markovReward.computePdf(v, branchLength, 0, 0)
-                    / markovReward.computeConditionalProbability(branchLength, 0, 0);
+            return markovReward.computePdf(v, branchLength, 0, 0) /
+                    (markovReward.computeConditionalProbability(branchLength, 0, 0)
+                            - Math.exp(-rate * prop * branchLength));
+        }
+    }
+
+    private class UnitDensityFunction implements UnivariateRealFunction {
+
+        private final MarkovReward markovReward;
+        private final double branchLength;
+        private final double rate;
+        private final double prop;
+
+        UnitDensityFunction(MarkovReward markovReward, double branchLength, double rate, double prop) {
+            this.markovReward = markovReward;
+            this.branchLength = branchLength;
+            this.rate = rate;
+            this.prop = prop;
+        }
+
+        @Override
+        public double value(double v) { //throws FunctionEvaluationException {
+            double density = markovReward.computePdf(v * branchLength, branchLength, 0, 0) /
+                    (markovReward.computeConditionalProbability(branchLength, 0, 0)
+                            - Math.exp(-rate * prop * branchLength));
+            return density * branchLength;
         }
     }
 

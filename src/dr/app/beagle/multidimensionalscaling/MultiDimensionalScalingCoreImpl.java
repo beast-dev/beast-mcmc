d9b8cb7d94741e2646202b9433d00cfa4640752b
rambaut <rambaut@users.noreply.github.com>
2014-12-03 11:32:46 +0000

    Java skeleton implementation

---
+++ src/dr/app/beagle/multidimensionalscaling/MultiDimensionalScalingCoreImpl.java
@@ -0,0 +1,47 @@
+package dr.app.beagle.multidimensionalscaling;
+
+/**
+ * MultiDimensionalScalingCoreImpl
+ *
+ * @author Andrew Rambaut
+ * @author Marc Suchard
+ * @version $Id$
+ *
+ * $HeadURL$
+ *
+ * $LastChangedBy$
+ * $LastChangedDate$
+ * $LastChangedRevision$
+ */
+
+public class MultiDimensionalScalingCoreImpl implements MultiDimensionalScalingCore {
+    @Override
+    public void initialize(int dimensionCount, int locationCount, int observationCount) {
+
+    }
+
+    @Override
+    public void setData(double[] observations, int[] observationTypes) {
+
+    }
+
+    @Override
+    public void updateLocations(int updateCount, double[] locations) {
+
+    }
+
+    @Override
+    public double calculateLogLikelihood() {
+        return 0;
+    }
+
+    @Override
+    public void storeState() {
+
+    }
+
+    @Override
+    public void restoreState() {
+
+    }
+}

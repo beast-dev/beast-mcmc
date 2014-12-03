9944b5436c794ac35cd0dc2c4daeb4963a921b76
baele.guy@gmail.com <baele.guy@gmail.com@ca793f91-a31e-0410-b540-2769d408b6a1>
2014-12-03 10:21:42 +0000

    Independence samplers disguised as Gibbs operators.

---
+++ trunk/src/dr/evomodel/continuous/GibbsIndependentCoalescentOperator.java
@@ -0,0 +1,191 @@
+/*
+ * GibbsIndependentCoalescentOperator.java
+ *
+ * Copyright (c) 2002-2014 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package dr.evomodel.continuous;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dr.evolution.tree.SimpleTree;
+import dr.evolution.tree.Tree;
+import dr.evolution.util.Taxa;
+import dr.evolution.util.TaxonList;
+import dr.evomodel.coalescent.CoalescentLikelihood;
+import dr.evomodel.coalescent.CoalescentSimulator;
+import dr.evomodel.coalescent.DemographicModel;
+import dr.evomodel.tree.TreeModel;
+import dr.inference.operators.GibbsOperator;
+import dr.inference.operators.MCMCOperator;
+import dr.inference.operators.OperatorFailedException;
+import dr.inference.operators.SimpleMCMCOperator;
+import dr.xml.AttributeRule;
+import dr.xml.ElementRule;
+import dr.xml.XMLObject;
+import dr.xml.XMLParseException;
+import dr.xml.XMLSyntaxRule;
+
+/**
+ * An independent coalescent sampler, based on the coalescent simulator parser code.
+ *
+ * @author Guy Baele
+ * 
+ */
+public class GibbsIndependentCoalescentOperator extends SimpleMCMCOperator implements GibbsOperator {
+
+	public static final String OPERATOR_NAME = "GibbsIndependentCoalescentOperator";
+    
+    private TreeModel treeModel;
+    private DemographicModel demoModel;
+    private CoalescentLikelihood coalescent;
+    private XMLObject xo;
+	
+	public GibbsIndependentCoalescentOperator(XMLObject xo, TreeModel treeModel, DemographicModel demoModel, CoalescentLikelihood coalescent, double weight) {
+		
+		this.xo = xo;
+		this.treeModel = treeModel;
+		this.demoModel = demoModel;
+		this.coalescent = coalescent;
+		setWeight(weight);
+		
+	}
+	
+	public String getPerformanceSuggestion() {
+		return "";
+	}
+
+	public String getOperatorName() {
+		return "GibbsIndependentCoalescent(" + treeModel.getModelName() + ")";
+	}
+	
+	public int getStepCount() {
+        return 1;
+    }
+
+    /**
+     * change the parameter and return the hastings ratio.
+     */
+	public double doOperation() throws OperatorFailedException {
+		
+		CoalescentSimulator simulator = new CoalescentSimulator();
+        
+        List<TaxonList> taxonLists = new ArrayList<TaxonList>();
+
+        double rootHeight = -1.0;
+        double oldLikelihood = 0.0;
+        double newLikelihood = 0.0;
+
+        // should have one child that is node
+        for (int i = 0; i < xo.getChildCount(); i++) {
+            final Object child = xo.getChild(i);
+            
+            //careful: Trees are TaxonLists ... (AER); see OldCoalescentSimulatorParser
+            if (child instanceof Tree) {
+                //do nothing
+            } else if (child instanceof TaxonList) {
+                //taxonLists.add((TaxonList) child);
+                taxonLists.add((Taxa) child);
+                //taxa added
+                break;
+            } 
+        }
+
+        try {
+        	
+        	Tree[] trees = new Tree[taxonLists.size()];
+            // simulate each taxonList separately
+            for (int i = 0; i < taxonLists.size(); i++) {
+                trees[i] = simulator.simulateTree(taxonLists.get(i), demoModel);
+            }
+
+            oldLikelihood = coalescent.getLogLikelihood();
+            
+            SimpleTree simTree = simulator.simulateTree(trees, demoModel, rootHeight, trees.length != 1);
+            
+            //this would be the normal way to do it
+            treeModel.beginTreeEdit();
+            //now it's allowed to adjust the tree structure
+            treeModel.adoptTreeStructure(simTree);
+            //endTreeEdit() would then fire the events
+            treeModel.endTreeEdit();
+            
+            newLikelihood = coalescent.getLogLikelihood();
+            
+        } catch (IllegalArgumentException iae) {
+            try {
+				throw new XMLParseException(iae.getMessage());
+			} catch (XMLParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
+		
+		//return oldLikelihood - newLikelihood;
+        return 0;
+	}
+	
+	public static dr.xml.XMLObjectParser PARSER = new dr.xml.AbstractXMLObjectParser() {
+		
+		public String getParserName() {
+            return OPERATOR_NAME;
+        }
+
+		public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+			
+			TreeModel treeModel = (TreeModel) xo.getChild(TreeModel.class);
+			double weight = xo.getDoubleAttribute(MCMCOperator.WEIGHT);
+	        DemographicModel demoModel = (DemographicModel) xo.getChild(DemographicModel.class);
+	        CoalescentLikelihood coalescent = (CoalescentLikelihood) xo.getChild(CoalescentLikelihood.class);
+	        
+			return new GibbsIndependentCoalescentOperator(xo, treeModel, demoModel, coalescent, weight);
+			
+		}
+		
+		//************************************************************************
+        // AbstractXMLObjectParser implementation
+        //************************************************************************
+
+		public XMLSyntaxRule[] getSyntaxRules() {
+			return rules;
+		}
+		
+		private final XMLSyntaxRule[] rules = {
+				AttributeRule.newDoubleRule(MCMCOperator.WEIGHT),
+				new ElementRule(Taxa.class),
+                new ElementRule(TreeModel.class),
+                new ElementRule(DemographicModel.class),
+                new ElementRule(CoalescentLikelihood.class)
+        };
+
+		public String getParserDescription() {
+			return "This element returns an independence coalescent sampler, disguised as a Gibbs operator, from a demographic model.";
+		}
+
+		public Class getReturnType() {
+			return MCMCOperator.class;
+		}
+		
+	};
+
+}
---
+++ trunk/src/dr/inference/operators/GibbsIndependentGammaOperator.java
@@ -0,0 +1,174 @@
+/*
+ * GibbsIndependentGammaOperator.java
+ *
+ * Copyright (c) 2002-2014 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package dr.inference.operators;
+
+import dr.inference.model.Bounds;
+import dr.inference.model.Parameter;
+import dr.inference.model.Variable;
+import dr.math.distributions.GammaDistribution;
+import dr.xml.AttributeRule;
+import dr.xml.ElementRule;
+import dr.xml.XMLObject;
+import dr.xml.XMLParseException;
+import dr.xml.XMLSyntaxRule;
+
+/**
+ * An independent gamma sampler to propose new (independent) values from a provided gamma prior (distribution).
+ *
+ * @author Guy Baele
+ * 
+ */
+public class GibbsIndependentGammaOperator extends SimpleMCMCOperator implements GibbsOperator {
+
+	public static final String OPERATOR_NAME = "GibbsIndependentGammaOperator";
+    public static final String SHAPE = "shape";
+    public static final String SCALE = "scale";
+	
+	private Variable<Double> variable = null;
+	private GammaDistribution gamma = null;
+	private boolean updateAllIndependently = true;
+	
+	public GibbsIndependentGammaOperator(Variable variable, GammaDistribution gamma) {
+		
+		this(variable, gamma, 1.0);
+		
+	}
+	
+	public GibbsIndependentGammaOperator(Variable variable, GammaDistribution gamma, double weight) {
+		
+		this(variable, gamma, weight, true);
+		
+	}
+	
+	public GibbsIndependentGammaOperator(Variable variable, GammaDistribution gamma, double weight, boolean updateAllIndependently) {
+		
+		this.variable = variable;
+		this.gamma = gamma;
+		this.updateAllIndependently = updateAllIndependently;
+		setWeight(weight);
+		
+	}
+	
+	public String getPerformanceSuggestion() {
+		return "";
+	}
+
+	public String getOperatorName() {
+		return "GibbsIndependentGamma(" + variable.getVariableName() + ")";
+	}
+	
+	public int getStepCount() {
+        return 1;
+    }
+
+    /**
+     * change the parameter and return the hastings ratio.
+     */
+	public double doOperation() throws OperatorFailedException {
+		
+		//double logq = 0;
+		
+		//double currentValue;
+		double newValue;
+		
+		final Bounds<Double> bounds = variable.getBounds();
+		final int dim = variable.getSize();
+		
+		if (updateAllIndependently) {
+			for (int i = 0; i < dim; i++) {
+				
+				//both current and new value of the variable needed for the hastings ratio
+				//currentValue = variable.getValue(i);
+				
+				newValue = gamma.nextGamma();
+				while (newValue == 0.0) {
+					newValue = gamma.nextGamma();
+				}
+				
+				//System.err.println("newValue: " + newValue + " - logPdf: " + gamma.logPdf(newValue));
+				
+				//logq += (gamma.logPdf(currentValue) - gamma.logPdf(newValue));
+				
+				if (newValue < bounds.getLowerLimit(i) || newValue > bounds.getUpperLimit(i)) {
+                    throw new OperatorFailedException("proposed value outside boundaries");
+                }
+				
+				variable.setValue(i, newValue);
+				
+			}
+		}
+		
+		//return logq;
+		return 0;
+	}
+	
+	public static dr.xml.XMLObjectParser PARSER = new dr.xml.AbstractXMLObjectParser() {
+		
+		public String getParserName() {
+            return OPERATOR_NAME;
+        }
+
+		public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+			
+			double weight = xo.getDoubleAttribute(WEIGHT);
+			double shape = xo.getDoubleAttribute(SHAPE);
+			double scale = xo.getDoubleAttribute(SCALE);
+			
+			if (! (shape > 0 && scale > 0)) {
+				throw new XMLParseException("Shape and scale must be positive values.");
+			}
+			
+			Parameter parameter = (Parameter) xo.getChild(Parameter.class);
+			
+			return new GibbsIndependentGammaOperator(parameter, new GammaDistribution(shape, scale), weight);
+		}
+		
+		//************************************************************************
+        // AbstractXMLObjectParser implementation
+        //************************************************************************
+
+		public XMLSyntaxRule[] getSyntaxRules() {
+			return rules;
+		}
+		
+		private final XMLSyntaxRule[] rules = {
+                AttributeRule.newDoubleRule(WEIGHT),
+                AttributeRule.newDoubleRule(SHAPE),
+                AttributeRule.newDoubleRule(SCALE),
+                new ElementRule(Parameter.class)
+        };
+
+		public String getParserDescription() {
+			return "This element returns an independence sampler, disguised as a Gibss operator, from a provided gamma prior.";
+		}
+
+		public Class getReturnType() {
+			return MCMCOperator.class;
+		}
+		
+	};
+
+}

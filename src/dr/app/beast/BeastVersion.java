754b8446a134a2ba10ac384bcaeba0edb6df9df2
rambaut <rambaut@users.noreply.github.com>
2015-03-05 21:37:56 +0000

    Updated build scripts

--- src/dr/app/beast/BeastVersion.java
+++ src/dr/app/beast/BeastVersion.java
@@ -50,7 +50,7 @@ public class BeastVersion implements Version {
 
     private static final String DATE_STRING = "2002-2015";
 
-    private static final boolean IS_PRERELEASE = true;
+    private static final boolean IS_PRERELEASE = false;
 
     private static final String REVISION = "$Rev$";
 

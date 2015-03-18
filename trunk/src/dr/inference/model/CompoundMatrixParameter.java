311b4c1dd8c30b38d49deabee5b0cf3dc9127489
msuchard <msuchard@ca793f91-a31e-0410-b540-2769d408b6a1>
2015-03-18 19:09:50 +0000

    Added compound (column-wise) matrix parameter to wrap multiple matrices

---
+++ trunk/src/dr/inference/model/CompoundMatrixParameter.java
@@ -0,0 +1,100 @@
+/*
+ * CompoundMatrixParameter.java
+ *
+ * Copyright (c) 2002-2015 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package dr.inference.model;
+
+import dr.xml.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Marc A. Suchard
+ */
+public class CompoundMatrixParameter extends MatrixParameter {
+
+    public CompoundMatrixParameter(String name, List<MatrixParameter> matrices) {
+        super(name, compoundMatrices(matrices));
+    }
+
+    private static Parameter[] compoundMatrices(List<MatrixParameter> matrices) {
+        int length = 0;
+        for (MatrixParameter matrix : matrices) {
+            length += matrix.getColumnDimension();
+        }
+
+        Parameter[] parameters = new Parameter[length];
+        int index = 0;
+        for (MatrixParameter matrix : matrices) {
+            for (int i = 0; i < matrix.getColumnDimension(); ++i) {
+                parameters[index] = matrix.getParameter(i);
+                ++index;
+            }
+        }
+        return parameters;
+    }
+
+    public final static String COMPOUND_MATRIX_PARAMETER = "compoundMatrixParameter";
+
+    public static XMLObjectParser PARSER = new AbstractXMLObjectParser() {
+
+        public String getParserName() {
+            return COMPOUND_MATRIX_PARAMETER;
+        }
+
+        public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+
+            List<MatrixParameter> matrices = new ArrayList<MatrixParameter>();
+
+            for (int i = 0; i < xo.getChildCount(); ++i) {
+                matrices.add((MatrixParameter) xo.getChild(i));
+            }
+
+            final String name = xo.hasId() ? xo.getId() : null;
+
+            return new CompoundMatrixParameter(name, matrices);
+        }
+
+        //************************************************************************
+        // AbstractXMLObjectParser implementation
+        //************************************************************************
+
+        public String getParserDescription() {
+            return "A compound matrix parameter constructed from its component parameters.";
+        }
+
+        public XMLSyntaxRule[] getSyntaxRules() {
+            return rules;
+        }
+
+        private final XMLSyntaxRule[] rules = {
+                new ElementRule(MatrixParameter.class, 1, Integer.MAX_VALUE),
+        };
+
+        public Class getReturnType() {
+            return CompoundMatrixParameter.class;
+        }
+    };
+}

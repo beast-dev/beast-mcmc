8b5400de1e53bd741ba4bcafef0445c7f1ed7534
filip <fbielejec@gmail.com>
2015-06-12 16:08:36 +0200

    true/false can now be set for lrc parameters

--- src/dr/app/bss/Utils.java
+++ src/dr/app/bss/Utils.java
@@ -1433,6 +1433,13 @@ public class Utils {
 		}// END: indices loop
 		string += ")";
 
+		if(data.clockModelIndex == data.LRC_INDEX) {
+			
+			String space = (data.lrcParametersInRealSpace[0] == true ? "real" : "log");
+			string += " ( " +  "Parameters in " + space + " space )";
+			
+		}
+		
 		return string;
 	}
 

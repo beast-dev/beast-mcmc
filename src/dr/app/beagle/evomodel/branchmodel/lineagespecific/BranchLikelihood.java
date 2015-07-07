322d2be2ea092207cfd81ec136735fa21c0571ec
filip <fbielejec@gmail.com>
2015-07-07 13:26:00 +0200

    pulling

--- src/dr/app/beagle/evomodel/branchmodel/lineagespecific/BranchLikelihood.java
+++ src/dr/app/beagle/evomodel/branchmodel/lineagespecific/BranchLikelihood.java
@@ -3,12 +3,20 @@ package dr.app.beagle.evomodel.branchmodel.lineagespecific;
 import java.util.LinkedList;
 import java.util.List;
 
+import dr.app.beagle.evomodel.branchmodel.BranchModel;
+import dr.app.beagle.evomodel.sitemodel.SiteRateModel;
+import dr.app.beagle.evomodel.substmodel.FrequencyModel;
+import dr.app.beagle.evomodel.substmodel.SubstitutionModel;
+import dr.app.beagle.evomodel.treelikelihood.BeagleTreeLikelihood;
+import dr.evolution.alignment.PatternList;
 import dr.evolution.tree.NodeRef;
+import dr.evomodel.branchratemodel.BranchRateModel;
 import dr.evomodel.branchratemodel.CountableBranchCategoryProvider;
 import dr.evomodel.tree.TreeModel;
 import dr.inference.loggers.LogColumn;
 import dr.inference.loggers.NumberColumn;
 import dr.inference.model.CompoundModel;
+import dr.inference.model.CompoundParameter;
 import dr.inference.model.Likelihood;
 import dr.inference.model.Model;
 import dr.inference.model.Parameter;
@@ -16,98 +24,94 @@ import dr.inference.model.Parameter;
 @SuppressWarnings("serial")
 public class BranchLikelihood implements Likelihood {
 
+	// Constructor fields
+//	private PatternList patternList;
 	private TreeModel treeModel;
-	private List<Likelihood> uniqueLikelihoods;
-
-	private List<Likelihood> branchLikelihoods;
-	private Parameter categoriesParameter;
-
-	private String id = null;
-	private boolean used = false;
-
-	// for discrete categories
+//	private BranchModel branchModel;
+//	private SiteRateModel siteRateModel;
+//	private FrequencyModel freqModel;
+//	private BranchRateModel branchRateModel;
+	
+    private BeagleTreeLikelihood likelihood;
 	private CountableBranchCategoryProvider categoriesProvider;
 
+	private Parameter categoriesParameter;
+	private CompoundParameter uniqueParameters;
+	private CountableRealizationsParameter allParameters;
+	
 	private final CompoundModel compoundModel = new CompoundModel(
 			"compoundModel");
 
-	public BranchLikelihood(TreeModel treeModel, List<Likelihood> likelihoods,
-			Parameter categoriesParameter) {
+	public BranchLikelihood(	
+//			 PatternList patternList, //
+			 TreeModel treeModel, //
+//			 BranchModel branchModel, //
+//			 SiteRateModel siteRateModel, //
+//			 FrequencyModel freqModel, //
+//			 BranchRateModel branchRateModel, //
+			BeagleTreeLikelihood likelihood,
+			 Parameter categoriesParameter,
+			 CompoundParameter uniqueParameters,
+			 CountableRealizationsParameter allParameters
+			 ) {
 
+//		this.patternList = patternList;
+//		this.treeModel = treeModel;
+//		this.branchModel = branchModel;
+//		this.siteRateModel = siteRateModel;
+//		this.freqModel = freqModel;
+//		this.branchRateModel = branchRateModel;
 		this.treeModel = treeModel;
-		this.uniqueLikelihoods = likelihoods;
 		this.categoriesParameter = categoriesParameter;
-
-		if (this.treeModel != null) {
-
+		this.uniqueParameters = uniqueParameters;
+		this.allParameters = allParameters;
+		
 			this.categoriesProvider = new CountableBranchCategoryProvider.IndependentBranchCategoryModel(
-					treeModel, categoriesParameter);
-
-		}
-
-		this.branchLikelihoods = getBranchLikelihoods();
+					this.treeModel, categoriesParameter);
 
 	}// END: Constructor
 
-	public final Likelihood getLikelihood(int index) {
-		return branchLikelihoods.get(index);
-	}
 	
-	private List<Likelihood> getBranchLikelihoods() {
-
-		// linked list preserves order
-		List<Likelihood> loglikes = new LinkedList<Likelihood>();
-
-		if (treeModel != null) {
-
-			for (NodeRef branch : treeModel.getNodes()) {
-
-				if (!treeModel.isRoot(branch)) {
-
-					int branchCategory = categoriesProvider.getBranchCategory(
-							treeModel, branch);
-					int index = (int) categoriesParameter
-							.getParameterValue(branchCategory);
-
-					Likelihood branchLikelihood = uniqueLikelihoods.get(index);
-
-					loglikes.add(branchLikelihood);
-					compoundModel.addModel(branchLikelihood.getModel());
-
-				}
-			}// END: branch loop
-
-		} else {// if no tree then read them in supplied order
-
-			int dim = categoriesParameter.getDimension();
-			if (dim != uniqueLikelihoods.size()) {
-				throw new RuntimeException("Dimensionality mismatch!");
-			}// END: size of categoriesParameter check
-
-			loglikes.addAll(uniqueLikelihoods);
-
-		}// END: tree check
-
-		return loglikes;
-	}// END: getBranchLikelihoods
+	
 
 	@Override
 	public double getLogLikelihood() {
-
+		
 		double loglike = 0;
-		for (Likelihood like : branchLikelihoods) {
-			loglike += like.getLogLikelihood();
-		}
+
+		for (int i = 0; i < categoriesParameter.getDimension(); i++) {
+			
+			int category = (int) categoriesParameter.getParameterValue(i);			
+			double value = uniqueParameters.getParameterValue(category);
+			
+             allParameters.setParameterValue(i, value);
+			
+		}//END: i loop
+
+		likelihood.makeDirty();
+        loglike = likelihood.getLogLikelihood();
 
 		return loglike;
 	}// END: getLogLikelihood
 
-	public int getLikelihoodCount() {
-		return branchLikelihoods.size();
-	}
-
-	public List<Likelihood> getLikelihoods() {
-		return branchLikelihoods;
+	public double getLogLikelihood(int index) {
+		
+		
+//		likelihood.getBranchModel().getSubstitutionModels().get(index).getVariable(0).setValue(index, value)
+		
+		double tmp1 = likelihood.getLogLikelihood();
+		
+		int category = (int) categoriesParameter.getParameterValue(index);			
+		double value = uniqueParameters.getParameterValue(category);
+		allParameters.setParameterValue(index, value);
+		
+		likelihood.makeDirty();
+		
+		
+		double tmp2 = likelihood.getLogLikelihood();
+		
+		
+		return tmp2-tmp1;
 	}
 
 	@Override
@@ -118,12 +122,11 @@ public class BranchLikelihood implements Likelihood {
 
 	@Override
 	public String getId() {
-		return this.id;
+		return"";
 	}// END: getId
 
 	@Override
 	public void setId(String id) {
-		this.id = id;
 	}// END: setId
 
 	@Override
@@ -134,9 +137,6 @@ public class BranchLikelihood implements Likelihood {
 	@Override
 	public void makeDirty() {
 
-		for (Likelihood likelihood : uniqueLikelihoods) {
-			likelihood.makeDirty();
-		}
 
 	}// END: makeDirty
 
@@ -147,16 +147,11 @@ public class BranchLikelihood implements Likelihood {
 
 	@Override
 	public boolean isUsed() {
-		return used;
+		return true;
 	}// END: isUsed
 
 	@Override
 	public void setUsed() {
-		used = true;
-		for (Likelihood like : branchLikelihoods) {
-			like.setUsed();
-		}
-
 	}// END: setUsed
 
 	@Override
@@ -176,4 +171,39 @@ public class BranchLikelihood implements Likelihood {
 
 	}// END: LikelihoodColumn class
 
+	
+	
+	
+	
+	
+	
+	
+//	private LinkedList<Likelihood> getBranchLikelihoods() {
+//
+//		// linked list preserves order
+//		LinkedList<Likelihood> loglikes = new LinkedList<Likelihood>();
+//
+//			for (NodeRef branch : treeModel.getNodes()) {
+//
+//				if (!treeModel.isRoot(branch)) {
+//
+//					int branchCategory = categoriesProvider.getBranchCategory(
+//							treeModel, branch);
+//					int index = (int) categoriesParameter
+//							.getParameterValue(branchCategory);
+//
+//					Likelihood branchLikelihood = uniqueLikelihoods.get(index);
+//
+//					loglikes.add(branchLikelihood);
+//					compoundModel.addModel(branchLikelihood.getModel());
+//
+//				}
+//			}// END: branch loop
+//
+//		return loglikes;
+//	}// END: getBranchLikelihoods
+	
+	
+	
+	
 }// END: class

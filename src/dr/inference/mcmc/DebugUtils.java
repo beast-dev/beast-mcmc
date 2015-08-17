41abac6791b235deb7097531a78ddaf69be77709
Luiz Max de Carvalho <lmcarvalho@ufrj.br>
2015-08-17 18:25:41 +0100

    small fix to DebugUtils; newline

--- src/dr/inference/mcmc/DebugUtils.java
+++ src/dr/inference/mcmc/DebugUtils.java
@@ -71,7 +71,7 @@ public class DebugUtils {
                 out.print(rngState[i]);
             }
 
-            out.print("state\t");
+            out.print("\nstate\t");
             out.println(state);
 
             out.print("lnL\t");

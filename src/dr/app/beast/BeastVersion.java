d515c1de3254e7992b8e041aa6924b3c98194043
Andrew Rambaut <rambaut@gmail.com>
2016-07-03 21:33:11 +0100

    Updating version

--- src/dr/app/beast/BeastVersion.java
+++ src/dr/app/beast/BeastVersion.java
@@ -52,15 +52,15 @@ public class BeastVersion implements Version, Citable {
     /**
      * Version string: assumed to be in format x.x.x
      */
-    private static final String VERSION = "1.8.4";
+    private static final String VERSION = "1.9.0";
 
     private static final String DATE_STRING = "2002-2016";
 
-    private static final boolean IS_PRERELEASE = false;
+    private static final boolean IS_PRERELEASE = true;
 
     // this is now being manually updated since the move to GitHub. Using date in yyyymmdd format (suffix
     // with b,c,d etc if multiple revisions in a day.
-    private static final String REVISION = "GitHub 20160629";
+    private static final String REVISION = "GitHub 20160703";
 
     public String getVersion() {
         return VERSION;

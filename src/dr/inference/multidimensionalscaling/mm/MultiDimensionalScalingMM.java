aa8522ef3353361020d360b614ff3838c575dcf4
Marc Suchard <msuchard@gmail.com>
2016-07-18 10:32:51 -0700

    manually merge in local branch mm_algo because AR restructured namespaces

--- src/dr/inference/multidimensionalscaling/mm/MultiDimensionalScalingMM.java
+++ src/dr/inference/multidimensionalscaling/mm/MultiDimensionalScalingMM.java
@@ -46,8 +46,15 @@ public class MultiDimensionalScalingMM extends MMAlgorithm {
     private double[] D = null;
     private double[] distance = null;
 
+    final private double tolerance;
+
+    public MultiDimensionalScalingLikelihood getLikelihood() { return likelihood; }
+
+    public GaussianProcessRandomGenerator getGaussianProcess() { return gp; }
+
     public MultiDimensionalScalingMM(MultiDimensionalScalingLikelihood likelihood,
-                                     GaussianProcessRandomGenerator gp) {
+                                     GaussianProcessRandomGenerator gp,
+                                     double tolerance) {
         super();
 
         this.likelihood = likelihood;
@@ -56,34 +63,91 @@ public class MultiDimensionalScalingMM extends MMAlgorithm {
         this.P = likelihood.getMdsDimension();
         this.Q = likelihood.getLocationCount();
 
+        this.tolerance = tolerance;
+    }
+
+    public void run() {
+
         if (gp != null) {
             double[][] precision = gp.getPrecisionMatrix();
             setPrecision(precision);
         }
 
+//        System.err.println("");
+
+//        System.err.println("weight: " + 1.0 / likelihood.getMDSPrecision());
+        weightTree =  1.0 / likelihood.getMDSPrecision();
+
+        double[] start = likelihood.getMatrixParameter().getParameterValues();
+
+        System.err.println("Start: " + printArray(start));
+        double penaltyStart = printLogObjective();
+
+//        EllipticalSliceOperator.transformPoint(mode, true, true, P);
+//        setParameterValues(likelihood.getMatrixParameter(), mode);
+//        printLogObjective();
+
         double[] mode = null;
 
-        System.err.println("Start: " + printArray(likelihood.getMatrixParameter().getParameterValues()));
-
         try {
-            mode = findMode(likelihood.getMatrixParameter().getParameterValues());
+            mode = findMode(likelihood.getMatrixParameter().getParameterValues(),
+                    tolerance, 100000);
+
         } catch (NotConvergedException e) {
             e.printStackTrace();
         }
 
-        System.err.println("Final: " + printArray(mode));
-
-        EllipticalSliceOperator.transformPoint(mode, true, true, P);
-
-        System.err.println("Final: " + printArray(mode));
+//        System.err.println("Final: " + printArray(mode));
 
         setParameterValues(likelihood.getMatrixParameter(), mode);
+        double penaltyEnd = printLogObjective();
+//
+//        if (penaltyStart - penaltyEnd > 1E-1) {
+
+//        if (penaltyEnd < penaltyStart) {
+//            throw new RuntimeException("MM moved up-hill\n\tStart: " + penaltyStart + "\n\tEnd  : " + penaltyEnd);
+//            System.err.println("Revert:  MM moved up-hill?");
+//            setParameterValues(likelihood.getMatrixParameter(), start);
+//            double penaltyRevert = printLogObjective();
+//            System.err.println("End: " + penaltyEnd);
+//            System.err.println("revert : " + penaltyRevert);
+           // throw new RuntimeException("out");
+//
+//        }
+
+//        EllipticalSliceOperator.transformPoint(mode, true, true, P);
+//
+//        System.err.println("Final: " + printArray(mode));
+//
+//        setParameterValues(likelihood.getMatrixParameter(), mode);
+//        printLogObjective();
+
+        throw new RuntimeException("done");
+    }
+
+    private double printLogObjective() {
+        double logLike = likelihood.getLogLikelihood();
+        double logPenalty = gp.getLikelihood().getLogLikelihood();
+        double total = logLike;
+        if (weightTree != 0.0) {
+            total += logPenalty;
+        }
+        System.err.println("obj: " + total + " = " + logLike + " + " + logPenalty);
+//        return logPenalty;
+        return total;
     }
 
     private void setParameterValues(MatrixParameterInterface mat, double[] values) {
 
+//        for (int i = 0; i < values.length; ++i) {
+//            mat.setValue(i, values[i]);
+//        }
         mat.setAllParameterValuesQuietly(values, 0);
         mat.setParameterValueNotifyChangedAll(0, 0, values[0]); // Fire changed
+//        for (int i = 0; i < mat.getUniqueParameterCount(); ++i) {
+//            mat.getUniqueParameter(0).fireParameterChangedEvent();
+//        }
+//        mat.getUniqueParameter(0);
     }
 
     private double[] getDistanceMatrix() {
@@ -153,7 +217,7 @@ public class MultiDimensionalScalingMM extends MMAlgorithm {
             for (int j = i + 1; j < Q; ++j) { // TODO XtX is not a necessary intermediate
                 double norm2 = XtX[i * Q + i] + XtX[j * Q + j] - 2 * XtX[i * Q + j];
                 double norm = norm2 > 0.0 ? Math.sqrt(norm2) : 0.0;
-                D[j * Q + i] = D[i * Q + j] = Math.max(norm, 1E-3);
+                D[j * Q + i] = D[i * Q + j] = Math.max(norm, 1E-10);
 
                 if (Double.isNaN(D[i * Q + j])) {
                     System.err.println("D NaN");
@@ -172,13 +236,20 @@ public class MultiDimensionalScalingMM extends MMAlgorithm {
             for (int k = 0; k < P; ++k) { // TODO Embarrassingly parallel
 
                 final int ik = i * P + k;
-                final int QP = Q * P;
+//                final int QP = Q * P;
 
                 double numerator = 0.0;
                 for (int j = 0; j < Q; ++j) {
-                    int add = (i != j)? 1 : 0;
-                    double inc = distance[i * Q + j] * (current[i * P + k] - current[j * P + k]) / D[i * Q + j]
-                                                + (current[i * P + k] + current[j * P + k]);
+                    double inc = 0.0;
+                    if (i != j) {
+//                        int add = (i != j) ? 1 : 0;
+//                        double inc = add * distance[i * Q + j] * (current[i * P + k] - current[j * P + k]) / D[i * Q + j]
+//                                + (current[i * P + k] + current[j * P + k]);
+                        inc += distance[i * Q + j] * (current[i * P + k] - current[j * P + k]) / D[i * Q + j]
+                                                                        + (current[i * P + k] + current[j * P + k]);
+                    }
+//                    inc = 0.0; // TODO Remove!
+
                     if (Double.isNaN(inc)) {
                         System.err.println("Bomb at " + i + " " + k + " " + j);
                         System.err.println("Distance = " + distance[i * Q + j]);
@@ -193,13 +264,15 @@ public class MultiDimensionalScalingMM extends MMAlgorithm {
                             final int jl = j * P + l;
                             final double prec = precision[ik][jl];
                             final int sign = (prec > 0.0) ? 1 : -1;
-                            inc += weightTree * prec * (current[i * P + k] - sign * current[j * P + k]);
+                            inc += weightTree * Math.abs(prec) * (current[i * P + k] - sign * current[j * P + l]);
                         }
                     }
 
-                    numerator += add * inc;
+                    numerator += inc;
                 }
                 double denominator = 2 * (Q - 1);
+
+//                denominator = 0.0; // TODO Remove
 
                 if (precision != null) {
                     denominator += weightTree * (2 * precision[ik][ik] + precisionStatistics[ik]);
@@ -208,6 +281,9 @@ public class MultiDimensionalScalingMM extends MMAlgorithm {
                 next[i * P + k] = numerator / denominator;
             }
         }
+
+        // Force translation, rotation, reflection symmetry
+        EllipticalSliceOperator.transformPoint(next, true, true, P);
     }
 
    // **************************************************************
@@ -217,6 +293,7 @@ public class MultiDimensionalScalingMM extends MMAlgorithm {
     public static XMLObjectParser PARSER = new AbstractXMLObjectParser() {
 
         public static final String MDS_STARTING_VALUES = "mdsModeFinder";
+        public static final String TOLERANCE = "tolerance";
 
         public String getParserName() {
             return MDS_STARTING_VALUES;
@@ -230,7 +307,11 @@ public class MultiDimensionalScalingMM extends MMAlgorithm {
             GaussianProcessRandomGenerator gp =
                     (GaussianProcessRandomGenerator) xo.getChild(GaussianProcessRandomGenerator.class);
 
-            return new MultiDimensionalScalingMM(likelihood, gp);
+            double tolerance = xo.getAttribute(TOLERANCE, 1E-3);
+
+            MultiDimensionalScalingMM mm = new MultiDimensionalScalingMM(likelihood, gp, tolerance);
+            mm.run();
+            return mm;
         }
 
         //************************************************************************
@@ -248,6 +329,7 @@ public class MultiDimensionalScalingMM extends MMAlgorithm {
         private final XMLSyntaxRule[] rules = {
                 new ElementRule(MultiDimensionalScalingLikelihood.class),
                 new ElementRule(GaussianProcessRandomGenerator.class, true),
+                AttributeRule.newDoubleRule(TOLERANCE, true),
         };
 
         public Class getReturnType() {
@@ -262,5 +344,5 @@ public class MultiDimensionalScalingMM extends MMAlgorithm {
 
     private boolean ignoreGP = false;
 
-    private double weightTree = 0.00001;  // TODO Formally compute
+    private double weightTree;  // TODO Formally compute
 }

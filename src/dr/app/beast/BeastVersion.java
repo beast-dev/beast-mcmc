f5e0bdd4c0ef082bc51651cf7d0e05868890394e
rambaut <rambaut@gmail.com>
2016-09-30 09:51:13 +0100

    Commenting out BEAGLE3 API calls on master - these should be tested on beagle_3_development branch.

--- src/dr/app/beast/BeastVersion.java
+++ src/dr/app/beast/BeastVersion.java
@@ -52,15 +52,14 @@ public class BeastVersion implements Version, Citable {
     /**
      * Version string: assumed to be in format x.x.x
      */
-    private static final String VERSION = "1.9.0";
+    private static final String VERSION = "1.9.0pre20160929";
 
     private static final String DATE_STRING = "2002-2016";
 
     private static final boolean IS_PRERELEASE = true;
 
-    // this is now being manually updated since the move to GitHub. Using date in yyyymmdd format (suffix
-    // with b,c,d etc if multiple revisions in a day.
-    private static final String REVISION = "GitHub 20160703";
+    // this is now being manually updated since the move to GitHub. 7 digits of GitHub hash.
+    private static final String REVISION = "#7d240f1";
 
     public String getVersion() {
         return VERSION;

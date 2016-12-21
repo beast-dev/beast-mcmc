4f13f14d6b91a302bb9db9da4435839ea357895f
rambaut <rambaut@gmail.com>
2016-12-21 12:10:07 +0000

    Use BEAGLE 3 is attempted by default. Turn off by setting System Property "USE_BEAGLE3" to "false"

--- src/dr/evomodelxml/treedatalikelihood/TreeDataLikelihoodParser.java
+++ src/dr/evomodelxml/treedatalikelihood/TreeDataLikelihoodParser.java
@@ -92,7 +92,7 @@ public class TreeDataLikelihoodParser extends AbstractXMLObjectParser {
 //                scalingScheme,
 //                delayRescalingUntilUnderflow);
 
-        boolean useBeagle3 = Boolean.parseBoolean(System.getProperty("USE_BEAGLE3", "false"));
+        boolean useBeagle3 = Boolean.parseBoolean(System.getProperty("USE_BEAGLE3", "true"));
 
         if ( useBeagle3 && MultiPartitionDataLikelihoodDelegate.IS_MULTI_PARTITION_COMPATIBLE() ) {
             DataLikelihoodDelegate dataLikelihoodDelegate = new MultiPartitionDataLikelihoodDelegate(

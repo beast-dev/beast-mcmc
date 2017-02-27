4a50d0b5088f4a6f043a9383cfa897f2f27f78b1
Andrew Rambaut <rambaut@gmail.com>
2017-02-27 18:06:11 +0000

    Adjusted CoalescentSimulator main() to produce single NEXUS files.

--- src/dr/evolution/coalescent/CoalescentSimulator.java
+++ src/dr/evolution/coalescent/CoalescentSimulator.java
@@ -32,6 +32,9 @@ import dr.evolution.util.Date;
 import dr.math.MathUtils;
 import dr.util.HeapSort;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.*;
 
 /**
@@ -292,7 +295,7 @@ public class CoalescentSimulator {
 
 	public static void main(String[] args) {
 
-		int N = 10;
+		int N = 100000000;
 
 		double[] samplingTimes = {
 				0.0, 0.0, 0.0, 0.0, 0.0
@@ -311,11 +314,24 @@ public class CoalescentSimulator {
 		}
 		CoalescentSimulator simulator = new CoalescentSimulator();
 
-		NexusExporter exporter =  new NexusExporter(System.out);
-		for (i = 0; i < N; i++) {
-		Tree tree = simulator.simulateTree(taxa, constantPopulation);
-			exporter.exportTree(tree);
+        PrintStream out = System.out;
+//        try {
+//             out = new PrintStream(new File("out.nex"));
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+        NexusExporter exporter =  new NexusExporter(out);
+        Tree tree = simulator.simulateTree(taxa, constantPopulation);
+        Map<String, Integer> idMap = exporter.writeNexusHeader(tree);
+        out.println("\t\t;");
+        exporter.writeNexusTree(tree, "TREE_" + 1, true, idMap);
+
+		for (i = 1; i < N; i++) {
+            tree = simulator.simulateTree(taxa, constantPopulation);
+            exporter.writeNexusTree(tree, "TREE_" + (i + 1), true, idMap);
 		}
-	}
+        out.println("End;");
+    }
+
 
 }

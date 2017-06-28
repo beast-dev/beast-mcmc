19ed3e1631c32175c8164f0bb68d53f6677c5092
Marc A. Suchard <msuchard@ucla.edu>
2017-06-27 20:23:39 -0700

    bookkeeping for integrated factor model; just need the math now

--- src/dr/evomodel/treedatalikelihood/continuous/IntegratedFactorAnalysisLikelihood.java
+++ src/dr/evomodel/treedatalikelihood/continuous/IntegratedFactorAnalysisLikelihood.java
@@ -25,29 +25,46 @@
 
 package dr.evomodel.treedatalikelihood.continuous;
 
+import dr.evolution.tree.MultivariateTraitTree;
+import dr.evomodel.tree.TreeModel;
 import dr.evomodel.treedatalikelihood.continuous.cdi.PrecisionType;
+import dr.evomodelxml.treelikelihood.TreeTraitParserUtilities;
 import dr.inference.model.*;
 import dr.xml.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * @author Marc A. Suchard
  */
 
-public class IntegratedFactorAnalysisLikelihood extends AbstractModelLikelihood implements ContinuousTraitPartialsProvider {
+public class IntegratedFactorAnalysisLikelihood extends AbstractModelLikelihood
+        implements ContinuousTraitPartialsProvider {
 
     public IntegratedFactorAnalysisLikelihood(String name,
+                                              CompoundParameter traitParameter,
+                                              List<Integer> missingIndices,
                                               MatrixParameterInterface loadings,
                                               Parameter traitPrecision) {
         super(name);
 
+        this.traitParameter = traitParameter;
         this.loadings = loadings;
         this.traitPrecision = traitPrecision;
-        this.missingIndices = null; // TODO
+        this.missingIndices = missingIndices;
 
-        this.numTraits = 0; // TODO
-        this.dimTrait = 0; // TODO
+        this.numTaxa = traitParameter.getParameterCount();
+        this.dimTrait = traitParameter.getParameter(0).getDimension();
+        this.dimPartial =  dimTrait + PrecisionType.FULL.getMatrixLength(dimTrait);
+        this.numFactors = loadings.getRowDimension(); // TODO Confirm this is not column-dimension!
+
+        addVariable(traitParameter);
+        addVariable(loadings);
+        addVariable(traitPrecision);
+
+        this.observedIndicators = setupObservedIndicators(missingIndices, numTaxa, dimTrait);
+        this.observedDimensions = setupObservedDimensions(observedIndicators);
     }
 
     @Override
@@ -57,13 +74,13 @@ public class IntegratedFactorAnalysisLikelihood extends AbstractModelLikelihood 
 
     @Override
     public int getTraitCount() {
-        return numTraits;
+        return 1;
     }
 
     @Override
     public int getTraitDimension() {
-        return dimTrait;
-    }
+        return numFactors;
+    }  // Returns dimension of latent factors
 
     @Override
     public PrecisionType getPrecisionType() {
@@ -72,22 +89,25 @@ public class IntegratedFactorAnalysisLikelihood extends AbstractModelLikelihood 
 
     @Override
     public double[] getTipPartial(int taxonIndex, boolean fullyObserved) {
-        return new double[0];
+        checkStatistics();
+        throw new RuntimeException("To implement");
     }
 
     @Override
     public double[] getTipPartial(int taxonIndex) {
-        return new double[0];
+        checkStatistics();
+        throw new RuntimeException("To implement");
     }
 
     @Override
     public double[] getTipObservation(int taxonIndex, PrecisionType precisionType) {
-        return new double[0];
+        checkStatistics();
+        throw new RuntimeException("To implement");
     }
 
     @Override
     public List<Integer> getMissingIndices() {
-        return missingIndices; // TODO Fix use-case (all values should be missing)
+        return null; // TODO Fix use-case (all tree-tip values (factors) should be missing)
     }
 
     @Override
@@ -97,7 +117,7 @@ public class IntegratedFactorAnalysisLikelihood extends AbstractModelLikelihood 
 
     @Override
     public Model getModel() {
-        return null;
+        return this;
     }
 
     @Override
@@ -109,10 +129,6 @@ public class IntegratedFactorAnalysisLikelihood extends AbstractModelLikelihood 
         return logLikelihood;
     }
 
-    private double calculateLogLikelihood() {
-        return 0.0;
-    }
-
     @Override
     public void makeDirty() {
         likelihoodKnown = false;
@@ -120,14 +136,20 @@ public class IntegratedFactorAnalysisLikelihood extends AbstractModelLikelihood 
 
     @Override
     protected void handleModelChangedEvent(Model model, Object object, int index) {
-
+        // No model dependencies
     }
 
     @Override
     protected void handleVariableChangedEvent(Variable variable, int index, Parameter.ChangeType type) {
         if (variable == loadings || variable == traitPrecision) {
+            statisticsKnown = false;
+            likelihoodKnown = false;
             fireModelChanged(this);
 //            fireModelChanged(this, getTaxonIndex(index));
+        } else if (variable == traitParameter) {
+            statisticsKnown = false;
+            likelihoodKnown = false;
+            fireModelChanged(this);
         } else {
             throw new RuntimeException("Unhandled parameter change type");
         }
@@ -137,12 +159,25 @@ public class IntegratedFactorAnalysisLikelihood extends AbstractModelLikelihood 
     protected void storeState() {
         storedLogLikelihood = logLikelihood;
         storedLikelihoodKnow = likelihoodKnown;
+        storedStatisticsKnown = statisticsKnown;
+
+        System.arraycopy(partials, 0, storedPartials, 0, partials.length);
+        System.arraycopy(remainders, 0, storedRemainders, 0, remainders.length);
     }
 
     @Override
     protected void restoreState() {
         logLikelihood = storedLogLikelihood;
         likelihoodKnown = storedLikelihoodKnow;
+        statisticsKnown = storedStatisticsKnown;
+
+        double[] tmp1 = partials;
+        partials = storedPartials;
+        storedPartials = tmp1;
+
+        double[] tmp2 = remainders;
+        remainders = storedRemainders;
+        storedRemainders = tmp2;
     }
 
     @Override
@@ -150,26 +185,118 @@ public class IntegratedFactorAnalysisLikelihood extends AbstractModelLikelihood 
         // Do nothing
     }
 
+    private double calculateLogLikelihood() {
+        double logLikelihood = 0.0;
+        for (double r : remainders) {
+            logLikelihood += r;
+        }
+        return logLikelihood;
+    }
+
+    private void setupStatistics() {
+        if (partials == null) {
+            partials = new double[numTaxa * dimPartial];
+        }
+
+        if (remainders == null) {
+            remainders = new double[numTaxa];
+        }
+
+        computePartialsAndRemainders();
+    }
+
+    private void computePartialsAndRemainders() {
+        int partialsOffset = 0;
+        for (int taxon = 0; taxon < numTaxa; ++taxon) {
+
+            // TODO Fill in partial means, precisions, (maybe?) variances and normalization constants
+
+            partialsOffset += dimPartial;
+        }
+    }
+
+    private void checkStatistics() {
+        if (!statisticsKnown) {
+            setupStatistics();
+            statisticsKnown = true;
+        }
+    }
+
+    private static double[][] setupObservedIndicators(List<Integer> missingIndices, int nTaxa, int dimTrait) {
+        double[][] observed = new double[nTaxa][dimTrait];
+
+        for (double[] v : observed) {
+            Arrays.fill(v, 1.0);
+        }
+
+        for (Integer idx : missingIndices) {
+            int taxon = idx / dimTrait;
+            int trait = idx % dimTrait;
+            observed[taxon][trait] = 0.0;
+        }
+
+        return observed;
+    }
+
+    private static int[] setupObservedDimensions(double[][] observed) {
+        int length = observed.length;
+        int[] dimensions = new int[length];
+
+        for (int i = 0; i < length; ++i) {
+            double sum = 0;
+            for (double x : observed[i]) {
+                sum += x;
+            }
+            dimensions[i] = (int) sum;
+        }
+
+        return dimensions;
+    }
+
+    private boolean likelihoodKnown = false;
+    private boolean storedLikelihoodKnow;
+    private boolean statisticsKnown = false;
+    private boolean storedStatisticsKnown;
     private double logLikelihood;
     private double storedLogLikelihood;
-    private boolean likelihoodKnown;
-    private boolean storedLikelihoodKnow;
 
-    private final int numTraits;
+    private double[] partials;
+    private double[] storedPartials;
+
+    private double[] remainders;
+    private double[] storedRemainders;
+
+    private final int numTaxa;
     private final int dimTrait;
+    private final int dimPartial;
+    private final int numFactors;
+    private final CompoundParameter traitParameter;
     private final MatrixParameterInterface loadings;
     private final Parameter traitPrecision;
     private final List<Integer> missingIndices;
+
+    private final double[][] observedIndicators;
+    private final int[] observedDimensions;
 
     // TODO Move remainder into separate class file
     public static AbstractXMLObjectParser PARSER = new AbstractXMLObjectParser() {
         @Override
         public Object parseXMLObject(XMLObject xo) throws XMLParseException {
 
+            MultivariateTraitTree treeModel = (MultivariateTraitTree) xo.getChild(TreeModel.class);
+            TreeTraitParserUtilities utilities = new TreeTraitParserUtilities();
+
+            TreeTraitParserUtilities.TraitsAndMissingIndices returnValue =
+                    utilities.parseTraitsFromTaxonAttributes(xo, TreeTraitParserUtilities.DEFAULT_TRAIT_NAME,
+                            treeModel, true);
+            CompoundParameter traitParameter = returnValue.traitParameter;
+            List<Integer> missingIndices = returnValue.missingIndices;
+
             MatrixParameterInterface loadings = (MatrixParameterInterface) xo.getElementFirstChild(LOADINGS);
             Parameter traitPrecision = (Parameter) xo.getElementFirstChild(PRECISION);
 
-            return new IntegratedFactorAnalysisLikelihood(xo.getId(), loadings, traitPrecision);
+            return new IntegratedFactorAnalysisLikelihood(xo.getId(), traitParameter, missingIndices,
+                    loadings, traitPrecision);
         }
 
         @Override
@@ -204,7 +331,15 @@ public class IntegratedFactorAnalysisLikelihood extends AbstractModelLikelihood 
             new ElementRule(PRECISION, new XMLSyntaxRule[] {
                     new ElementRule(Parameter.class),
             }),
-
+            // Tree trait parser
+            new ElementRule(MultivariateTraitTree.class),
+            AttributeRule.newStringRule(TreeTraitParserUtilities.TRAIT_NAME),
+            new ElementRule(TreeTraitParserUtilities.TRAIT_PARAMETER, new XMLSyntaxRule[]{
+                    new ElementRule(Parameter.class)
+            }),
+            new ElementRule(TreeTraitParserUtilities.MISSING, new XMLSyntaxRule[]{
+                    new ElementRule(Parameter.class)
+            }, true),
 
     };
 }

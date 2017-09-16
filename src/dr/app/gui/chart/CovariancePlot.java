67a77456a8bf98dc24fae67053068fb711f39a83
GuyBaele <baele.guy@gmail.com>
2017-09-17 00:52:10 +0200

    more work on covariance plots

--- src/dr/app/gui/chart/CovariancePlot.java
+++ src/dr/app/gui/chart/CovariancePlot.java
@@ -50,6 +50,9 @@ public class CovariancePlot extends Plot.AbstractPlot implements Citable {
     private final double NEGATIVE_CORRELATION_DEGREE = 0.785398163;
     private final double POSITIVE_CORRELATION_DEGREE = 2.35619449;
 
+    private boolean samples = false;
+    private boolean asPoints = false;
+
     //colors from plotcorr R package
     private final Color[] colors = {new Color(165,15,21),
             new Color(222, 45, 38),
@@ -86,6 +89,14 @@ public class CovariancePlot extends Plot.AbstractPlot implements Citable {
         this.plotCount = 1;
     }
 
+    public CovariancePlot(String name, java.util.List<Double> xData, java.util.List<Double> yData, boolean asPoints, boolean samples) {
+        super(xData, yData);
+        setName(name);
+        this.asPoints = asPoints;
+        this.samples = samples;
+        this.plotCount = 1;
+    }
+
     public void setTotalPlotCount(int count) {
         this.plotCount = count;
     }
@@ -102,85 +113,130 @@ public class CovariancePlot extends Plot.AbstractPlot implements Citable {
         int xCount = xData.getCount();
         int yCount = yData.getCount();
         double[] xDataArray = new double[xCount];
+        double minX = 0.0;
+        double maxX = 0.0;
+        double minY = 0.0;
+        double maxY = 0.0;
         double[] yDataArray = new double[yCount];
+        if (xCount > 0) {
+            minX = (Double) xData.get(0);
+            maxX = (Double) xData.get(0);
+            minY = (Double) yData.get(0);
+            maxY = (Double) yData.get(0);
+        }
         for (int i = 0; i < xCount; i++) {
-            xDataArray[i] = (Double)xData.get(i);
-            yDataArray[i] = (Double)yData.get(i);
+            if (this.samples) {
+
+            } else {
+                xDataArray[i] = (Double) xData.get(i);
+                yDataArray[i] = (Double) yData.get(i);
+            }
+            if (xDataArray[i] < minX) {
+                minX = xDataArray[i];
+            }
+            if (maxX > xDataArray[i]) {
+                maxX = xDataArray[i];
+            }
+            if (yDataArray[i] < minY) {
+                minY = yDataArray[i];
+            }
+            if (maxY > yDataArray[i]) {
+                maxY = yDataArray[i];
+            }
         }
 
-        double covariance = DiscreteStatistics.covariance(xDataArray, yDataArray);
+        if (this.asPoints) {
 
-        //System.out.println("plotNumber: " + plotNumber + " ; covariance = " + covariance);
+            double x1 = (plotNumber / (int) Math.sqrt(plotCount));
+            double y1 = (plotNumber % (int) Math.sqrt(plotCount));
+            double x2 = (plotNumber / (int) Math.sqrt(plotCount));
+            double y2 = (plotNumber % (int) Math.sqrt(plotCount));
 
-        Color fillColor = colors[(int)(5-5*covariance)];
-        g2.setColor(fillColor);
+            for (int i = 0; i < xDataArray.length; i++) {
+                //1 unit wide, so divide by entire range on both axes
+                double newX = x1 + xDataArray[i]/(maxX-minX);
+                double newY = x2 + yDataArray[i]/(maxY-minY);
+                //System.out.println("(" + newX + "," + newY + ")");
+                g2.fill(new Ellipse2D.Double(transformX(newX), transformY(newY), 1, 1));
+            }
 
-        //g2.setPaint(linePaint);
-        //g2.setStroke(lineStroke);
+        } else {
 
-        double x1 = (plotNumber/(int)Math.sqrt(plotCount)) + (1.0 - ELLIPSE_HALF_WIDTH);
-        double y1 = (plotNumber%(int)Math.sqrt(plotCount)) + (1.0 - ELLIPSE_HALF_WIDTH);
-        double x2 = (plotNumber/(int)Math.sqrt(plotCount)) + (1.0 + ELLIPSE_HALF_WIDTH);
-        double y2 = (plotNumber%(int)Math.sqrt(plotCount)) + (1.0 + ELLIPSE_HALF_WIDTH);
+            double covariance = DiscreteStatistics.covariance(xDataArray, yDataArray);
 
-        //System.out.println("(" + x1 + "," + y1 + ") (" + x2 + "," + y2 + ")");
+            //System.out.println("plotNumber: " + plotNumber + " ; covariance = " + covariance);
 
-        if (PRINT_VISUAL_AIDES) {
-            drawRect(g2, x1, y1, x2, y2);
-            g2.drawString("plot" + plotNumber, (float) transformX(x1), (float) transformY(y1));
+            Color fillColor = colors[(int) (5 - 5 * covariance)];
+            g2.setColor(fillColor);
+
+            //g2.setPaint(linePaint);
+            //g2.setStroke(lineStroke);
+
+            double x1 = (plotNumber / (int) Math.sqrt(plotCount)) + (1.0 - ELLIPSE_HALF_WIDTH);
+            double y1 = (plotNumber % (int) Math.sqrt(plotCount)) + (1.0 - ELLIPSE_HALF_WIDTH);
+            double x2 = (plotNumber / (int) Math.sqrt(plotCount)) + (1.0 + ELLIPSE_HALF_WIDTH);
+            double y2 = (plotNumber % (int) Math.sqrt(plotCount)) + (1.0 + ELLIPSE_HALF_WIDTH);
+
+            //System.out.println("(" + x1 + "," + y1 + ") (" + x2 + "," + y2 + ")");
+
+            if (PRINT_VISUAL_AIDES) {
+                drawRect(g2, x1, y1, x2, y2);
+                g2.drawString("plot" + plotNumber, (float) transformX(x1), (float) transformY(y1));
+            }
+
+            double rotationDegree = NEGATIVE_CORRELATION_DEGREE;
+            if (covariance > 0) {
+                rotationDegree = POSITIVE_CORRELATION_DEGREE;
+            }
+
+            double selectedHeight;
+            double absCovariance = Math.abs(covariance);
+            selectedHeight = 1.0 - absCovariance;
+
+            //System.out.println("selectedHeight = " + selectedHeight);
+
+            double ellipseWidth = Math.abs(transformX(x2) - transformX(x1));
+            //double ellipseHeight = 0.5 * Math.abs(transformY(y2) - transformY(y1));
+            double ellipseHeight = selectedHeight * Math.abs(transformY(y2) - transformY(y1));
+
+            AffineTransform oldTransform = g2.getTransform();
+
+            //double drawOffset = Math.abs(transformY(y2) - transformY(y1)) - ellipseHeight;
+            //System.out.println("ellipse draw offset = " + drawOffset);
+
+            Shape ellipse = new Ellipse2D.Double(transformX(x1), transformY(y2), ellipseWidth, ellipseHeight);
+
+            //this transformation rotates around the upper right rectangle corner
+            //Shape rotatedEllipse = AffineTransform.getRotateInstance(rotationDegree, transformX(x1), transformY(y2)).createTransformedShape(ellipse);
+
+            //rotate around rectangle center
+            //Shape rotatedEllipse = AffineTransform.getRotateInstance(rotationDegree, transformX((x1+x2)/2.0), transformY((y1+y2)/2.0)).createTransformedShape(ellipse);
+            //rotate around ellipse center
+            Shape rotatedEllipse = AffineTransform.getRotateInstance(rotationDegree, transformX((x1 + x2) / 2.0), transformY(y2) + ellipseHeight / 2.0).createTransformedShape(ellipse);
+
+            //center rotated ellipse within rectangle
+            Shape translatedEllipse = AffineTransform.getTranslateInstance(0.0, transformY((y1 + y2) / 2.0) - (transformY(y2) + ellipseHeight / 2.0)).createTransformedShape(rotatedEllipse);
+
+            //g2.fill(rotatedEllipse);
+            g2.fill(translatedEllipse);
+
+            g2.setColor(Color.BLACK);
+            //g2.draw(rotatedEllipse);
+
+            g2.draw(translatedEllipse);
+
+            if (PRINT_VISUAL_AIDES) {
+                //draw ellipse center in black
+                g2.fill(new Ellipse2D.Double(transformX((x1 + x2) / 2.0), transformY(y2) + ellipseHeight / 2.0, 5, 5));
+
+                //draw rectangle center in black
+                ///g2.fill(new Ellipse2D.Double(transformX((x1+x2)/2.0), transformY((y1+y2)/2.0), 5, 5));
+                g2.draw(new Ellipse2D.Double(transformX((x1 + x2) / 2.0), transformY((y1 + y2) / 2.0), 10, 10));
+            }
+
+            g2.setTransform(oldTransform);
+
         }
-
-        double rotationDegree = NEGATIVE_CORRELATION_DEGREE;
-        if (covariance > 0) {
-            rotationDegree = POSITIVE_CORRELATION_DEGREE;
-        }
-
-        double selectedHeight;
-        double absCovariance = Math.abs(covariance);
-        selectedHeight = 1.0 - absCovariance;
-
-        //System.out.println("selectedHeight = " + selectedHeight);
-
-        double ellipseWidth = Math.abs(transformX(x2)-transformX(x1));
-        //double ellipseHeight = 0.5 * Math.abs(transformY(y2) - transformY(y1));
-        double ellipseHeight = selectedHeight * Math.abs(transformY(y2) - transformY(y1));
-
-        AffineTransform oldTransform = g2.getTransform();
-
-        //double drawOffset = Math.abs(transformY(y2) - transformY(y1)) - ellipseHeight;
-        //System.out.println("ellipse draw offset = " + drawOffset);
-
-        Shape ellipse = new Ellipse2D.Double(transformX(x1), transformY(y2), ellipseWidth, ellipseHeight);
-
-        //this transformation rotates around the upper right rectangle corner
-        //Shape rotatedEllipse = AffineTransform.getRotateInstance(rotationDegree, transformX(x1), transformY(y2)).createTransformedShape(ellipse);
-
-        //rotate around rectangle center
-        //Shape rotatedEllipse = AffineTransform.getRotateInstance(rotationDegree, transformX((x1+x2)/2.0), transformY((y1+y2)/2.0)).createTransformedShape(ellipse);
-        //rotate around ellipse center
-        Shape rotatedEllipse = AffineTransform.getRotateInstance(rotationDegree, transformX((x1+x2)/2.0), transformY(y2)+ellipseHeight/2.0).createTransformedShape(ellipse);
-
-        //center rotated ellipse within rectangle
-        Shape translatedEllipse = AffineTransform.getTranslateInstance(0.0, transformY((y1+y2)/2.0) - (transformY(y2)+ellipseHeight/2.0)).createTransformedShape(rotatedEllipse);
-
-        //g2.fill(rotatedEllipse);
-        g2.fill(translatedEllipse);
-
-        g2.setColor(Color.BLACK);
-        //g2.draw(rotatedEllipse);
-
-        g2.draw(translatedEllipse);
-
-        if (PRINT_VISUAL_AIDES) {
-            //draw ellipse center in black
-            g2.fill(new Ellipse2D.Double(transformX((x1 + x2) / 2.0), transformY(y2) + ellipseHeight / 2.0, 5, 5));
-
-            //draw rectangle center in black
-            ///g2.fill(new Ellipse2D.Double(transformX((x1+x2)/2.0), transformY((y1+y2)/2.0), 5, 5));
-            g2.draw(new Ellipse2D.Double(transformX((x1+x2)/2.0), transformY((y1+y2)/2.0), 10, 10));
-        }
-
-        g2.setTransform(oldTransform);
 
     }
 

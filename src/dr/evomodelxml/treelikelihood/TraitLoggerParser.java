8e942efb10e722dd50143a1c3b63ca5ff3d24b91
Marc A. Suchard <msuchard@ucla.edu>
2017-10-03 13:20:35 +0100

    fix comiling

--- src/dr/evomodelxml/treelikelihood/TraitLoggerParser.java
+++ src/dr/evomodelxml/treelikelihood/TraitLoggerParser.java
@@ -25,73 +25,68 @@
 
 package dr.evomodelxml.treelikelihood;
 
-import dr.evomodel.treelikelihood.utilities.TreeTraitLogger;
+import dr.evolution.tree.Tree;
 import dr.evolution.tree.TreeTrait;
 import dr.evolution.tree.TreeTraitProvider;
-import dr.xml.*;
 import dr.evomodel.tree.TreeModel;
+import dr.evomodel.treelikelihood.utilities.TreeTraitLogger;
+import dr.xml.*;
 
 /**
  * @author Marc A. Suchard
  */
 
-public class StratifiedTraitLoggerParser extends AbstractXMLObjectParser {
+public class TraitLoggerParser extends AbstractXMLObjectParser {
 
-    public static final String PARSER_NAME = "stratifiedTraitLogger";
+    public static final String PARSER_NAME = "traitLogger";
     public static final String TRAIT_NAME = "traitName";
-    public static final String PARTITION = "partition";
-    //public static final String LOG_FORMAT = "format";
 
     public Object parseXMLObject(XMLObject xo) throws XMLParseException {
 
-        TreeModel treeModel = (TreeModel) xo.getChild(TreeModel.class);
+        Tree treeModel = (Tree) xo.getChild(Tree.class);
+        TreeTrait trait = parseTreeTrait(xo, false);
+
+        return new TreeTraitLogger(treeModel, new TreeTrait[] { trait } );
+    }
+
+    static TreeTrait parseTreeTrait(XMLObject xo, boolean wholeTreeOnly) throws XMLParseException {
+
         TreeTraitProvider traitProvider =
                 (TreeTraitProvider) xo.getChild(TreeTraitProvider.class);
 
         String traitName = (String) xo.getAttribute(TRAIT_NAME);
 
         TreeTrait trait = traitProvider.getTreeTrait(traitName);
-        if (trait == null || trait.getIntent() != TreeTrait.Intent.WHOLE_TREE) {
-            StringBuilder sb = new StringBuilder("Unable to find whole tree trait '" + traitName + "' in '" + xo.getId() + "\n");
+
+        if (trait == null || (wholeTreeOnly && trait.getIntent() != TreeTrait.Intent.WHOLE_TREE)) {
+
+            StringBuilder sb = new StringBuilder("Unable to find ");
+
+            if (wholeTreeOnly) {
+                sb.append("whole ");
+            }
+
+            String id = xo.hasId() ? xo.getId() : "null";
+
+            sb.append("tree trait '").append(traitName).append("' in '").append(id).append("\n");
+
             sb.append("\tPossible traits:");
             for (TreeTrait existingTrait : traitProvider.getTreeTraits()) {
-                if (existingTrait.getIntent() == TreeTrait.Intent.WHOLE_TREE) {
-                    sb.append(" " +existingTrait.getTraitName());
+                if (!wholeTreeOnly || existingTrait.getIntent() == TreeTrait.Intent.WHOLE_TREE) {
+                    sb.append(" ").append(existingTrait.getTraitName());
                 }
             }
             sb.append("\n");
+
             throw new XMLParseException(sb.toString());
         }
 
-        boolean partition = xo.getAttribute(PARTITION, false);
-
-        if (trait.getTraitClass() == Double.class || trait.getTraitClass() == Integer.class || !partition) {
-            return new TreeTraitLogger( treeModel, new TreeTrait[] { trait });
-        }
-                                                    
-        int length;
-        Object obj = trait.getTrait(treeModel, treeModel.getNode(0));
-
-        if (obj instanceof double[]) {
-           length = ((double[])obj).length;
-        } else if (obj instanceof int[]) {
-            length = ((int[])obj).length;
-        } else {
-            throw new XMLParseException("Unknown trait type for partitioning");
-        }
-       
-        TreeTrait[] partitionedTraits = new TreeTrait[length];
-        for (int i = 0; i < length; i++) {
-            partitionedTraits[i] = new TreeTrait.PickEntryD(trait, i);
-        }
-        return new TreeTraitLogger( treeModel, partitionedTraits );        
+        return trait;
     }
 
   private static final XMLSyntaxRule[] rules = new XMLSyntaxRule[]{
           AttributeRule.newStringRule(TRAIT_NAME),
-          AttributeRule.newBooleanRule(PARTITION, true),
-//          AttributeRule.newStringRule(LOG_FORMAT, true),
-          new ElementRule(TreeModel.class),
+          new ElementRule(Tree.class),
           new ElementRule(TreeTraitProvider.class),
     };
 
@@ -100,7 +95,7 @@ public class StratifiedTraitLoggerParser extends AbstractXMLObjectParser {
     }
 
     public String getParserDescription() {
-        return "A parser to stratify traits to be logged";
+        return "A parser to log tree traits";
     }
 
     public Class getReturnType() {

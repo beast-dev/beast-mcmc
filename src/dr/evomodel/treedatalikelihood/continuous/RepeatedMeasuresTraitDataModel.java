c7aea067dadf494d297bb497f687fac6a84ac488
Marc A. Suchard <msuchard@ucla.edu>
2018-02-02 13:28:30 -0800

    skeleton structure to start repeated measures model

---
+++ src/dr/evomodel/treedatalikelihood/continuous/RepeatedMeasuresTraitDataModel.java
@@ -0,0 +1,154 @@
+/*
+ * ContinuousTraitData.java
+ *
+ * Copyright (c) 2002-2016 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package dr.evomodel.treedatalikelihood.continuous;
+
+import dr.evolution.tree.MutableTreeModel;
+import dr.evomodel.continuous.MultivariateDiffusionModel;
+import dr.evomodel.tree.TreeModel;
+import dr.evomodel.treedatalikelihood.continuous.cdi.PrecisionType;
+import dr.evomodelxml.treelikelihood.TreeTraitParserUtilities;
+import dr.inference.model.*;
+import dr.xml.*;
+
+import java.util.List;
+
+/**
+ * @author Marc A. Suchard
+ */
+public class RepeatedMeasuresTraitDataModel extends
+        ContinuousTraitDataModel implements ContinuousTraitPartialsProvider {
+
+    private final Parameter samplingPrecision;
+
+    public RepeatedMeasuresTraitDataModel(String name,
+                                          CompoundParameter parameter,
+                                          List<Integer> missingIndices,
+                                          boolean useMissingIndices,
+                                          final int dimTrait,
+                                          PrecisionType precisionType,
+                                          Parameter samplingPrecision) {
+        super(name, parameter, missingIndices, useMissingIndices, dimTrait, precisionType);
+        this.samplingPrecision = samplingPrecision;
+
+        if (samplingPrecision.getDimension() != dimTrait) {
+            throw new RuntimeException("Currently only implemented for diagonal deflation");
+        }
+    }
+
+    @Override
+    public double[] getTipPartial(int taxonIndex, boolean fullyObserved) {
+        double[] partial = super.getTipPartial(taxonIndex, fullyObserved);
+
+        // TODO Deflate partial precision by samplingPrecision
+
+        // Notes: partial lay-out:  0, ..., dim - 1 : mean
+        //                          dim, ..., dim * dim + dim - 1 : precision
+
+        return partial;
+    }
+
+    @Override
+    protected void handleVariableChangedEvent(Variable variable, int index, Parameter.ChangeType type) {
+        super.handleVariableChangedEvent(variable, index, type);
+
+        if (variable == samplingPrecision) {
+            fireModelChanged();
+        }
+    }
+
+
+
+    // TODO Move remainder into separate class file
+    private static final String REPEATED_MEASURES_MODEL = "repeatedMeasuresModel";
+    private static final String PRECISION = "samplingPrecision";
+
+    public static AbstractXMLObjectParser PARSER = new AbstractXMLObjectParser() {
+        @Override
+        public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+
+            MutableTreeModel treeModel = (MutableTreeModel) xo.getChild(TreeModel.class);
+            TreeTraitParserUtilities utilities = new TreeTraitParserUtilities();
+
+            TreeTraitParserUtilities.TraitsAndMissingIndices returnValue =
+                    utilities.parseTraitsFromTaxonAttributes(xo, TreeTraitParserUtilities.DEFAULT_TRAIT_NAME,
+                            treeModel, true);
+            CompoundParameter traitParameter = returnValue.traitParameter;
+            List<Integer> missingIndices = returnValue.missingIndices;
+
+            Parameter samplingPrecision = (Parameter) xo.getElementFirstChild(PRECISION);
+
+            String traitName = returnValue.traitName;
+            MultivariateDiffusionModel diffusionModel = (MultivariateDiffusionModel)
+                    xo.getChild(MultivariateDiffusionModel.class);
+
+            return new RepeatedMeasuresTraitDataModel(
+                    traitName,
+                    traitParameter,
+                    missingIndices,
+                    true,
+                    diffusionModel.getPrecisionParameter().getRowDimension(),
+                    PrecisionType.FULL,
+                    samplingPrecision
+            );
+        }
+
+        @Override
+        public XMLSyntaxRule[] getSyntaxRules() {
+            return rules;
+        }
+
+        @Override
+        public String getParserDescription() {
+            return null;
+        }
+
+        @Override
+        public Class getReturnType() {
+            return RepeatedMeasuresTraitLikelihood.class;
+        }
+
+        @Override
+        public String getParserName() {
+            return REPEATED_MEASURES_MODEL;
+        }
+    };
+
+    private final static XMLSyntaxRule[] rules = new XMLSyntaxRule[] {
+            new ElementRule(PRECISION, new XMLSyntaxRule[] {
+                    new ElementRule(Parameter.class),
+            }),
+            // Tree trait parser
+            new ElementRule(MutableTreeModel.class),
+            AttributeRule.newStringRule(TreeTraitParserUtilities.TRAIT_NAME),
+            new ElementRule(TreeTraitParserUtilities.TRAIT_PARAMETER, new XMLSyntaxRule[]{
+                    new ElementRule(Parameter.class)
+            }),
+            new ElementRule(TreeTraitParserUtilities.MISSING, new XMLSyntaxRule[]{
+                    new ElementRule(Parameter.class)
+            }, true),
+            new ElementRule(MultivariateDiffusionModel.class),
+    };
+}

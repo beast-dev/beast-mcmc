825bd95e10d145b804d0ff54f94bd58aa1e1f4b9
xji3 <xji3@ncsu.edu>
2018-05-31 10:47:55 -0700

    through extension

--- src/dr/evomodel/treedatalikelihood/discrete/BranchRateGradientForDiscreteTrait.java
+++ src/dr/evomodel/treedatalikelihood/discrete/BranchRateGradientForDiscreteTrait.java
@@ -26,19 +26,11 @@
 package dr.evomodel.treedatalikelihood.discrete;
 
 import dr.evolution.tree.*;
-import dr.evomodel.branchratemodel.ArbitraryBranchRates;
-import dr.evomodel.branchratemodel.BranchRateModel;
 import dr.evomodel.treedatalikelihood.*;
-import dr.evomodel.treedatalikelihood.preorder.AbstractDiscreteTraitDelegate;
-import dr.evomodel.treedatalikelihood.preorder.ProcessSimulationDelegate;
 import dr.inference.hmc.GradientWrtParameterProvider;
 import dr.inference.hmc.HessianWrtParameterProvider;
-import dr.inference.loggers.LogColumn;
 import dr.inference.loggers.Loggable;
-import dr.inference.model.Likelihood;
 import dr.inference.model.Parameter;
-import dr.math.MultivariateFunction;
-import dr.math.NumericalDerivative;
 import dr.xml.Reportable;
 
 import static dr.math.MachineAccuracy.SQRT_EPSILON;
@@ -47,71 +39,17 @@ import static dr.math.MachineAccuracy.SQRT_EPSILON;
  * @author Xiang Ji
  * @author Marc A. Suchard
  */
-public class BranchRateGradientForDiscreteTrait
+public class BranchRateGradientForDiscreteTrait extends DiscreteTraitBranchRateGradient
         implements GradientWrtParameterProvider, HessianWrtParameterProvider, Reportable, Loggable {
 
-    private final TreeDataLikelihood treeDataLikelihood;
-    private final TreeTrait treeTraitProvider;
-    private final Tree tree;
-    private final boolean useHessian;
-
-//    private final int nTraits;
-//    private final int dim;
-    private final Parameter rateParameter;
-    private final ArbitraryBranchRates branchRateModel;
-
     // TODO Refactor / remove code duplication with BranchRateGradient
-    // TODO Maybe use:  AbstractBranchRateGradient, DiscreteTraitBranchRateGradient, ContinuousTraitBranchRateGradien
+    // TODO Maybe use:  AbstractBranchRateGradient, DiscreteTraitBranchRateGradient, ContinuousTraitBranchRateGradient
     public BranchRateGradientForDiscreteTrait(String traitName,
                                               TreeDataLikelihood treeDataLikelihood,
                                               BeagleDataLikelihoodDelegate likelihoodDelegate,
                                               Parameter rateParameter,
                                               boolean useHessian) {
-
-        assert (treeDataLikelihood != null);
-
-        this.treeDataLikelihood = treeDataLikelihood;
-        this.tree = treeDataLikelihood.getTree();
-        this.rateParameter = rateParameter;
-        this.useHessian = useHessian;
-
-        BranchRateModel brm = treeDataLikelihood.getBranchRateModel();
-        this.branchRateModel = (brm instanceof ArbitraryBranchRates) ? (ArbitraryBranchRates) brm : null;
-
-        String name = AbstractDiscreteTraitDelegate.getName(traitName);
-        TreeTrait test = treeDataLikelihood.getTreeTrait(name);
-
-        if (test == null) {
-            ProcessSimulationDelegate gradientDelegate = new AbstractDiscreteTraitDelegate(traitName,
-                    treeDataLikelihood.getTree(),
-                    likelihoodDelegate);
-            TreeTraitProvider traitProvider = new ProcessSimulation(treeDataLikelihood, gradientDelegate);
-            treeDataLikelihood.addTraits(traitProvider.getTreeTraits());
-        }
-
-        treeTraitProvider = treeDataLikelihood.getTreeTrait(name);
-        assert (treeTraitProvider != null);
-
-        int nTraits = treeDataLikelihood.getDataLikelihoodDelegate().getTraitCount();
-        if (nTraits != 1) {
-            throw new RuntimeException("Not yet implemented for >1 traits");
-        }
-//        dim = treeDataLikelihood.getDataLikelihoodDelegate().getTraitDim();
-    }
-
-    @Override
-    public Likelihood getLikelihood() {
-        return treeDataLikelihood;
-    }
-
-    @Override
-    public Parameter getParameter() {
-        return rateParameter;
-    }
-
-    @Override
-    public int getDimension() {
-        return getParameter().getDimension();
+        super(traitName, treeDataLikelihood, likelihoodDelegate, rateParameter, useHessian);
     }
 
     @Override
@@ -149,10 +87,6 @@ public class BranchRateGradientForDiscreteTrait
         //Do single call to traitProvider with node == null (get full tree)
         double[] gradient =  (double[]) treeTraitProvider.getTrait(tree, null);
 
-//        if (DEBUG) {
-//            System.err.println(new WrappedVector.Raw(gradient));
-//        }
-
         int v =0;
         for (int i = 0; i < tree.getNodeCount(); ++i) {
             final NodeRef node = tree.getNode(i);
@@ -176,124 +110,4 @@ public class BranchRateGradientForDiscreteTrait
         return result;
     }
 
-    private int getParameterIndexFromNode(NodeRef node) {
-        return (branchRateModel == null) ? node.getNumber() : branchRateModel.getParameterIndexFromNode(node);
-    }
-
-//    private static final boolean DEBUG = true;
-
-    private MultivariateFunction numeric1 = new MultivariateFunction() {
-        @Override
-        public double evaluate(double[] argument) {
-
-            for (int i = 0; i < argument.length; ++i) {
-                rateParameter.setParameterValue(i, argument[i]);
-            }
-
-//            treeDataLikelihood.makeDirty();
-            return treeDataLikelihood.getLogLikelihood();
-        }
-
-        @Override
-        public int getNumArguments() {
-            return rateParameter.getDimension();
-        }
-
-        @Override
-        public double getLowerBound(int n) {
-            return 0;
-        }
-
-        @Override
-        public double getUpperBound(int n) {
-            return Double.POSITIVE_INFINITY;
-        }
-    };
-
-    private boolean valuesAreSufficientlyLarge(double[] vector) {
-        for (double x : vector) {
-            if (x < SQRT_EPSILON * 1.2) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    @Override
-    public String getReport() {
-
-//        treeDataLikelihood.makeDirty();
-
-        double[] savedValues = rateParameter.getParameterValues();
-        double[] testGradient = null;
-        double[] testHessian = null;
-
-        boolean largeEnoughValues = valuesAreSufficientlyLarge(rateParameter.getParameterValues());
-
-        if (DEBUG && largeEnoughValues) {
-            testGradient = NumericalDerivative.gradient(numeric1, rateParameter.getParameterValues());
-        }
-
-        if (DEBUG && useHessian && largeEnoughValues) {
-            testHessian = NumericalDerivative.diagonalHessian(numeric1, rateParameter.getParameterValues());
-        }
-
-
-        for (int i = 0; i < savedValues.length; ++i) {
-            rateParameter.setParameterValue(i, savedValues[i]);
-        }
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("Peeling: ").append(new dr.math.matrixAlgebra.Vector(getGradientLogDensity()));
-        sb.append("\n");
-        
-        if (testGradient != null && largeEnoughValues) {
-            sb.append("numeric: ").append(new dr.math.matrixAlgebra.Vector(testGradient));
-        } else {
-            sb.append("mumeric: too close to 0");
-        }
-        sb.append("\n");
-
-        if (useHessian) {
-            if (largeEnoughValues){
-                sb.append("Peeling: ").append(new dr.math.matrixAlgebra.Vector(getDiagonalHessianLogDensity()));
-                sb.append("\n");
-            }
-
-            if (testHessian != null && largeEnoughValues) {
-                sb.append("numeric: ").append(new dr.math.matrixAlgebra.Vector(testHessian));
-            } else {
-                sb.append("mumeric: too close to 0");
-            }
-            sb.append("\n");
-        }
-
-        if (COUNT_TOTAL_OPERATIONS) {
-            sb.append("\n\tgetGradientLogDensityCount = ").append(getGradientLogDensityCount).append("\n");
-            sb.append(treeTraitProvider.toString()).append("\n");
-            sb.append(treeDataLikelihood.getReport());
-        }
-
-        return sb.toString();
-    }
-
-    private static final boolean DEBUG = true;
-
-    private static final boolean COUNT_TOTAL_OPERATIONS = true;
-    private long getGradientLogDensityCount = 0;
-
-    @Override
-    public LogColumn[] getColumns() {
-
-        LogColumn[] columns = new LogColumn[1];
-        columns[0] = new LogColumn.Default("gradient report", new Object() {
-            @Override
-            public String toString() {
-                return "\n" + getReport();
-            }
-        });
-
-        return columns;
-    }
 }

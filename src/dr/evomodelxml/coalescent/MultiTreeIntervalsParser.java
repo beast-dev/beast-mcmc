8df66b6ab7e5e7db258b32c24a196688ffc6d00b
Andrew Rambaut <rambaut@gmail.com>
2019-02-04 22:57:29 +0000

    Parser for MultiTreeIntervals

--- src/dr/evomodelxml/coalescent/MultiTreeIntervalsParser.java
+++ src/dr/evomodelxml/coalescent/MultiTreeIntervalsParser.java
@@ -1,7 +1,7 @@
 /*
- * CoalescentLikelihoodParser.java
+ * MultiTreeIntervalsParser.java
  *
- * Copyright (c) 2002-2015 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ * Copyright (c) 2002-2019 Alexei Drummond, Andrew Rambaut and Marc Suchard
  *
  * This file is part of BEAST.
  * See the NOTICE file distributed with this work for additional
@@ -29,10 +29,7 @@ import dr.evolution.tree.Tree;
 import dr.evolution.tree.TreeUtils;
 import dr.evolution.util.Taxa;
 import dr.evolution.util.TaxonList;
-import dr.evomodel.coalescent.CoalescentLikelihood;
-import dr.evomodel.coalescent.DemographicModel;
-import dr.evomodel.coalescent.MultiLociTreeSet;
-import dr.evomodel.coalescent.OldAbstractCoalescentLikelihood;
+import dr.evomodel.coalescent.*;
 import dr.evomodel.tree.TreeModel;
 import dr.xml.*;
 
@@ -40,6 +37,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * MultiTreeIntervalsParser
+ *
+ * @author Andrew Rambaut
  */
 public class MultiTreeIntervalsParser extends AbstractXMLObjectParser {
 
@@ -54,74 +54,20 @@ public class MultiTreeIntervalsParser extends AbstractXMLObjectParser {
 
     public Object parseXMLObject(XMLObject xo) throws XMLParseException {
 
-        XMLObject cxo = xo.getChild(TREES);
-        DemographicModel demoModel = (DemographicModel) cxo.getChild(DemographicModel.class);
+        double cutoff = xo.getDoubleAttribute(CUTOFF);
 
-//        List<Tree> trees = new ArrayList<TreeModel>();
-//
-//        for (int k = 0; k < cxo.getChildCount(); ++k) {
-//        }
-//
-//        List<Double> popFactors = new ArrayList<Double>();
-//        MultiLociTreeSet treesSet = demoModel instanceof MultiLociTreeSet ? (MultiLociTreeSet) demoModel : null;
-//
-//            final Object child = xo.getChild(k);
-//            if (child instanceof XMLObject) {
-//                cxo = (XMLObject) child;
-//                if (cxo.getName().equals(POPULATION_TREE)) {
-//                    final TreeModel t = (TreeModel) cxo.getChild(TreeModel.class);
-//                    assert t != null;
-//                    trees.add(t);
-//
-//                    popFactors.add(cxo.getAttribute(POPULATION_FACTOR, 1.0));
-//                }
-//            }
-////                in the future we may have arbitrary multi-loci element
-////                else if( child instanceof MultiLociTreeSet )  {
-////                    treesSet = (MultiLociTreeSet)child;
-////                }
-//        }
-//
-//        TreeModel treeModel = null;
-//        if (trees.size() == 1 && popFactors.get(0) == 1.0) {
-//            treeModel = trees.get(0);
-//        } else if (trees.size() > 1) {
-//            treesSet = new MultiLociTreeSet.Default(trees, popFactors);
-//        } else if (!(trees.size() == 0 && treesSet != null)) {
-//            throw new XMLParseException("Incorrectly constructed likelihood element");
-//        }
-//
-//        TaxonList includeSubtree = null;
-//
-//        if (xo.hasChildNamed(INCLUDE)) {
-//            includeSubtree = (TaxonList) xo.getElementFirstChild(INCLUDE);
-//        }
-//
-//        List<TaxonList> excludeSubtrees = new ArrayList<TaxonList>();
-//
-//        if (xo.hasChildNamed(EXCLUDE)) {
-//            cxo = xo.getChild(EXCLUDE);
-//            for (int i = 0; i < cxo.getChildCount(); i++) {
-//                excludeSubtrees.add((TaxonList) cxo.getChild(i));
-//            }
-//        }
-//
-//        if (treeModel != null) {
-//            try {
-//                return new CoalescentLikelihood(treeModel, includeSubtree, excludeSubtrees, demoModel);
-//            } catch (TreeUtils.MissingTaxonException mte) {
-//                throw new XMLParseException("treeModel missing a taxon from taxon list in " + getParserName() + " element");
-//            }
-//        } else {
-//            if (includeSubtree != null || excludeSubtrees.size() > 0) {
-//                throw new XMLParseException("Include/Exclude taxa not supported for multi locus sets");
-//            }
-//            // Use old code for multi locus sets.
-//            // This is a little unfortunate but the current code is using AbstractCoalescentLikelihood as
-//            // a base - and modifing it will probsbly result in a bigger mess.
-//            return new OldAbstractCoalescentLikelihood(treesSet, demoModel);
-//        }
-             return null;
+        XMLObject cxo = xo.getChild(TREES);
+
+        List<Tree> trees = new ArrayList<Tree>();
+
+        for (int i = 0; i < cxo.getChildCount(); i++) {
+            Tree tree = (Tree)cxo.getChild(Tree.class);
+            trees.add(tree);
+        }
+
+        Taxa singletonTaxa = (Taxa)xo.getElementFirstChild(SINGLETONS);
+
+        return new MultiTreeIntervals(trees, singletonTaxa, cutoff);
     }
 
     //************************************************************************
@@ -129,7 +75,7 @@ public class MultiTreeIntervalsParser extends AbstractXMLObjectParser {
     //************************************************************************
 
     public String getParserDescription() {
-        return "This element represents the likelihood of the tree given the demographic function.";
+        return "This element stores the coalescent intervals across a set of trees.";
     }
 
     public Class getReturnType() {

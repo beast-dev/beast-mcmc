14aea583b6f471f242ed2e653bc9388ac0d0ccfa
Marc Suchard <msuchard@ucla.edu>
2019-02-27 13:55:36 -0800

    impl exponentially tilted stable distribution for Gibbs sampling local shrinkage

--- src/dr/inference/operators/shrinkage/BayesianBridgeShrinkageOperator.java
+++ src/dr/inference/operators/shrinkage/BayesianBridgeShrinkageOperator.java
@@ -1,5 +1,6 @@
 package dr.inference.operators.shrinkage;
 
+import dr.inference.distribution.ExponentialTiltedStableDistribution;
 import dr.inference.distribution.shrinkage.BayesianBridgeLikelihood;
 import dr.inference.model.Parameter;
 import dr.inference.operators.GibbsOperator;
@@ -72,6 +73,18 @@ public class BayesianBridgeShrinkageOperator extends SimpleMCMCOperator implemen
         double draw = Math.pow(phi, -1.0 / exponent);
 
         globalScale.setParameterValue(0, draw);
+
+        //        # Conjugate update for phi = 1 / gshrink ** reg_exponent
+        //        shape = beta_with_shrinkage.size / reg_exponent
+        //        scale = 1 / np.sum(np.abs(beta_with_shrinkage) ** reg_exponent)
+        //        phi = self.rg.np_random.gamma(shape, scale=scale)
+        //        gshrink = 1 / phi ** (1 / reg_exponent)
+        //
+        //   To update the global scale parameter τ, we work directly with the exponential-power density, marginalizing out the latent variables {ωj,uj}. This is a crucial source of efficiency in the bridge MCMC, and leads to the favorable mixing evident in Figure 1. From (1), observe that the posterior for ν ≡ τ−α, given β, is conditionally independent of y, and takes the form
+        //
+        //    p(ν | β) ∝ νp/α exp(−ν 􏰑|βj|α) p(ν).
+        //    j=1
+        //    Therefore if ν has a Gamma(c, d) prior, its conditional posterior will also be a gamma distribution, with hyperparameters c⋆ = c+p/α and d⋆ = d+􏰏pj=1 |βj|α. To sample τ, simply draw ν from this gamma distribution, and use the transformation τ = ν−1/α.
     }
 
     private double absSumBeta() {
@@ -86,32 +99,32 @@ public class BayesianBridgeShrinkageOperator extends SimpleMCMCOperator implemen
     }
 
     private void sampleLocalScale() {
-        // TODO
+
+        final double exponent = regressionExponent.getParameterValue(0);
+        final double global = globalScale.getParameterValue(0);
+
+        for (int i = 0; i < dim; ++i) {
+            double draw = ExponentialTiltedStableDistribution.nextTiltedStable(
+                    exponent / 2, Math.pow(coefficient.getParameterValue(i) / global, 2)
+            );
+
+            localScale.setParameterValueQuietly(i, Math.sqrt(1 / (2 * draw)));
+        }
+
+        localScale.fireParameterChangedEvent();
+
+        //
+        //        lshrink_sq = 1 / np.array([
+        //            2 * self.rg.tilted_stable(reg_exponent / 2, (beta_j / gshrink) ** 2)
+        //            for beta_j in beta_with_shrinkage
+        //        ])
+        //        lshrink = np.sqrt(lshrink_sq)
     }
+
+
 }
 
-//    if self.prior_type['global_shrinkage'] == 'jeffreys':
-//
-//        # Conjugate update for phi = 1 / gshrink ** reg_exponent
-//        shape = beta_with_shrinkage.size / reg_exponent
-//        scale = 1 / np.sum(np.abs(beta_with_shrinkage) ** reg_exponent)
-//        phi = self.rg.np_random.gamma(shape, scale=scale)
-//        gshrink = 1 / phi ** (1 / reg_exponent)
-//
-//    elif self.prior_type['global_shrinkage'] == 'half-cauchy':
-//
-//        gshrink = self.slice_sample_global_shrinkage(
-//            gshrink, beta_with_shrinkage, self.prior_param['global_shrinkage']['scale'], reg_exponent
-//        )
-//
 
-
-
-//    To update the global scale parameter τ , we work directly with the exponential-power density, marginalizing out the latent variables {ωj,uj}. This is a crucial source of efficiency in the bridge MCMC, and leads to the favorable mixing evident in Figure 1. From (1), observe that the posterior for ν ≡ τ−α, given β, is conditionally independent of y, and takes the form
-//    p
-//    p(ν | β) ∝ νp/α exp(−ν 􏰑|βj|α) p(ν).
-//    j=1
-//    Therefore if ν has a Gamma(c, d) prior, its conditional posterior will also be a gamma dis- tribution, with hyperparameters c⋆ = c+p/α and d⋆ = d+􏰏pj=1 |βj|α. To sample τ, simply draw ν from this gamma distribution, and use the transformation τ = ν−1/α.
 
 //    def slice_sample_global_shrinkage(
 //            self, gshrink, beta_with_shrinkage, global_scale, reg_exponent):

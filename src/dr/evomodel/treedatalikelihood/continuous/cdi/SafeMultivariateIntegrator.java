994d3cc143c38b2ec864b9d74b078d5bc216c0a9
Gabe Hassler <gabeh12345@gmail.com>
2019-03-14 13:51:25 -0700

    undoing last commit

--- src/dr/evomodel/treedatalikelihood/continuous/cdi/SafeMultivariateIntegrator.java
+++ src/dr/evomodel/treedatalikelihood/continuous/cdi/SafeMultivariateIntegrator.java
@@ -4,7 +4,6 @@ import dr.math.matrixAlgebra.WrappedVector;
 import dr.math.matrixAlgebra.missingData.InversionResult;
 import org.ejml.data.DenseMatrix64F;
 import org.ejml.ops.CommonOps;
-import java.lang.Double;
 
 import static dr.math.matrixAlgebra.missingData.InversionResult.Code.NOT_OBSERVED;
 import static dr.math.matrixAlgebra.missingData.MissingOps.*;

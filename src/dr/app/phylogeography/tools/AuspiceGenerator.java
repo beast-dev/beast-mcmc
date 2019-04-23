d1f6bb87b14e2fcbbfad30c5d0267f1773707c89
Andrew Rambaut <rambaut@gmail.com>
2019-04-23 16:12:50 +0100

    An option (-m) will create a template metadata json for customization.

--- src/dr/app/phylogeography/tools/AuspiceGenerator.java
+++ src/dr/app/phylogeography/tools/AuspiceGenerator.java
@@ -4,32 +4,33 @@ import jebl.evolution.graphs.Node;
 import jebl.evolution.io.ImportException;
 import jebl.evolution.io.NexusImporter;
 import jebl.evolution.trees.RootedTree;
-import jebl.evolution.trees.SimpleRootedTree;
-import jebl.evolution.trees.Tree;
 import org.apache.commons.cli.*;
 
 import javax.json.Json;
 import javax.json.stream.JsonGenerator;
 import javax.json.stream.JsonGeneratorFactory;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.time.Year;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class AuspiceGenerator {
 
     private final double youngestDate;
+    private final String[] attributeNames;
+    private final Map<String, Set<String>> attributeValues = new HashMap<>();
 
-    public AuspiceGenerator(String treeFileName, String[] attributeNames, String outputStem) {
+    public AuspiceGenerator(String treeFileName, String[] attributeNames, boolean generateMetadata, String outputStem) {
 
         youngestDate = 2019.0;
-        double oldestDate = 1980.0;
+
+        this.attributeNames = attributeNames;
 
         RootedTree tree;
 
@@ -42,7 +43,11 @@ public class AuspiceGenerator {
         }
 
         try {
-            writeTreeJSON(outputStem + "_tree.json", tree, attributeNames);
+            writeTreeJSON(outputStem + "_tree.json", tree);
+
+            if (generateMetadata) {
+                writeMetaJSON(outputStem + "_meta.json", tree);
+            }
         } catch (IOException e) {
             e.printStackTrace();
             return;
@@ -52,7 +57,7 @@ public class AuspiceGenerator {
     private double currentYValue = 0;
     private int cladeNumber = 1;
 
-    private void writeTreeJSON(String filename, RootedTree tree, String[] attributes) throws IOException {
+    private void writeTreeJSON(String filename, RootedTree tree) throws IOException {
         FileWriter writer = new FileWriter(filename);
 
         HashMap<String, Object> config = new HashMap<>();
@@ -63,12 +68,12 @@ public class AuspiceGenerator {
         JsonGenerator generator = factory.createGenerator(writer);
         currentYValue = 0;
 
-        writeNode(generator, tree, tree.getRootNode(), attributes);
+        writeNode(generator, tree, tree.getRootNode());
 
         generator.close();
     }
 
-    private double writeNode(JsonGenerator generator, RootedTree tree, Node node, String[] attributes) {
+    private double writeNode(JsonGenerator generator, RootedTree tree, Node node) {
 
         double yValue;
 
@@ -78,9 +83,11 @@ public class AuspiceGenerator {
                 .writeStartObject("attr")
                 .write("clock_length", tree.getLength(node));
 
-        for (String attribute : attributes) {
+        for (String attribute : attributeNames) {
             if (node.getAttribute(attribute) != null) {
-                generator.write(attribute, node.getAttribute(attribute).toString());
+                String value = node.getAttribute(attribute).toString();
+                generator.write(attribute, value);
+                addAttributeValue(attribute, value);
 
                 if (node.getAttribute(attribute + ".set") != null) {
                     Object[] attributeSet = (Object[])node.getAttribute(attribute + ".set");
@@ -90,6 +97,7 @@ public class AuspiceGenerator {
 
                     int i = 0;
                     for (Object attr :  attributeSet) {
+                        addAttributeValue(attribute, attr.toString());
                         generator.write(attr.toString(), Double.parseDouble(attributeProbs[i].toString()));
                         i++;
                     }
@@ -153,7 +161,7 @@ public class AuspiceGenerator {
 
             yValue = 0.0;
             for (Node child : tree.getChildren(node)) {
-                yValue += writeNode(generator, tree, child, attributes);
+                yValue += writeNode(generator, tree, child);
             }
             yValue /= tree.getChildren(node).size();
 
@@ -181,6 +189,106 @@ public class AuspiceGenerator {
         return yValue;
     }
 
+    private void addAttributeValue(String attribute, String value) {
+        // add the value to the list of possible values
+        Set<String> valueSet = attributeValues.computeIfAbsent(attribute, k -> new HashSet<>());
+        valueSet.add(value);
+    }
+
+    private void writeMetaJSON(String filename, RootedTree tree) throws IOException {
+        FileWriter writer = new FileWriter(filename);
+
+        HashMap<String, Object> config = new HashMap<>();
+        config.put(JsonGenerator.PRETTY_PRINTING, true);
+
+        JsonGeneratorFactory factory = Json.createGeneratorFactory(config);
+
+        JsonGenerator generator = factory.createGenerator(writer);
+
+        generator.writeStartObject();
+
+        generator.writeStartObject("author_info").writeEnd();
+
+        generator.writeStartObject("color_options");
+
+        for (String attribute : attributeNames) {
+            generator.writeStartObject(attribute);
+
+            generator.writeStartArray("color_map");
+            for (String value : attributeValues.get(attribute)) {
+                generator
+                        .writeStartArray()
+                        .write(value)
+                        .write("#000000")
+                        .writeEnd();
+            }
+            generator.writeEnd(); // color_map array
+
+            generator
+                    .write("key", attribute)
+                    .write("legendTitle", attribute)
+                    .write("menuItem", attribute)
+                    .write("type", "discrete");
+
+            generator.writeEnd(); // attribute object
+        }
+
+        generator
+                .writeStartObject("num_date")
+                .write("key", "num_date")
+                .write("legendTitle", "Sampling Date")
+                .write("menuItem", "date")
+                .write("type", "continuous")
+                .writeEnd(); // num_date object
+
+        generator.writeEnd(); // color_options object
+
+        generator
+                .writeStartObject("defaults")
+                .write("mapTriplicate", true)
+                .writeEnd(); // defaults object
+
+        generator.writeStartArray("filters");
+        for (String attribute : attributeNames) {
+            generator.write("attributes");
+        }
+        generator.writeEnd(); // filters array
+
+        generator.writeStartObject("geo");
+        for (String attribute : attributeNames) {
+            generator.writeStartObject(attribute);
+
+            for (String value : attributeValues.get(attribute)) {
+                generator.writeStartObject(value)
+                        .write("latitude", 0.0)
+                        .write("longitude", 0.0)
+                        .writeEnd();
+            }
+
+            generator.writeEnd(); // attribute object
+        }
+        generator.writeEnd(); // geo object
+
+        generator
+                .writeStartArray("maintainer")
+                .writeEnd(); // maintainer array
+
+        generator
+                .writeStartArray("panels")
+                .write("tree")
+                .write("map")
+                .writeEnd(); // panels array
+
+        generator
+                .write("title", "")
+                .write("updated", LocalDate.now().toString())
+                .write("virus_count", tree.getExternalNodes().size());
+
+        generator.writeEnd(); // outer object
+
+        generator.close();
+    }
+
     private String numberToCalendarDate(double numDate) {
         return Year.of( (int)Math.floor(numDate) )
                 .atDay( (int)Math.floor((numDate - Math.floor(numDate)) * 365) + 1 )
@@ -194,6 +302,8 @@ public class AuspiceGenerator {
         options.addOption("h", "help", false, "display help");
         options.addOption("v", "verbose", false, "write analysis details to stderr");
 
+        options.addOption("m", "metadata", false, "generate a metadata file");
+
         options.addOption( Option.builder( "t" )
                 .longOpt("tree")
                 .argName("file name")
@@ -201,14 +311,6 @@ public class AuspiceGenerator {
                 .required(true)
                 .desc( "input tree file" )
                 .type(String.class).build());
-
-//        options.addOption( Option.builder( "m" )
-//                .longOpt("metadata")
-//                .argName("file name")
-//                .hasArg()
-//                .required(false)
-//                .desc( "input metadata tsv/csv file" )
-//                .type(String.class).build());
 
         options.addOption( Option.builder( "a" )
                 .longOpt("attributes")
@@ -246,19 +348,19 @@ public class AuspiceGenerator {
 
         boolean verbose = cmd.hasOption("v");
 
+        boolean generateMetadata = cmd.hasOption("m");
+
         String treeFileName = cmd.getOptionValue("t");
-//        String metadataFileName = cmd.getOptionValue("m");
         String outputFilename = cmd.getOptionValue("o");
 
         String[] attributeNames = cmd.getOptionValues("a");
 
         if (verbose) {
-            System.err.println("    input tree file: " + treeFileName);
-//            System.err.println("input metadata file: " + metadataFileName);
-            System.err.println("        output file: " + outputFilename);
+            System.err.println("     input tree file: " + treeFileName);
+            System.err.println("output filename stem: " + outputFilename);
         }
 
-        new AuspiceGenerator(treeFileName, attributeNames, outputFilename);
+        new AuspiceGenerator(treeFileName, attributeNames, generateMetadata, outputFilename);
 
     }
 

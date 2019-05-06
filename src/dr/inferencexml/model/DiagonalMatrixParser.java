defa9f61120c948697f58b6c9cc2db39c314ced1
xji3 <xji3@ncsu.edu>
2019-05-06 17:13:21 -0500

    allow DiagonalMatrixParser to construct single parameter diagonal matrix to match up old setup

--- src/dr/inferencexml/model/DiagonalMatrixParser.java
+++ src/dr/inferencexml/model/DiagonalMatrixParser.java
@@ -25,6 +25,7 @@
 
 package dr.inferencexml.model;
 
+import dr.inference.model.CompoundParameter;
 import dr.inference.model.DiagonalMatrix;
 import dr.inference.model.MatrixParameter;
 import dr.inference.model.Parameter;
@@ -36,6 +37,7 @@ import dr.xml.*;
 public class DiagonalMatrixParser extends AbstractXMLObjectParser {
 
     public final static String MATRIX_PARAMETER = "diagonalMatrix";
+    public final static String DIMENSION = "dimension";
 
     public String getParserName() {
         return MATRIX_PARAMETER;
@@ -45,6 +47,15 @@ public class DiagonalMatrixParser extends AbstractXMLObjectParser {
 
 
         Parameter diagonalParameter = (Parameter) xo.getChild(Parameter.class);
+
+        final int dim = xo.getAttribute(DIMENSION, diagonalParameter.getDimension());
+        if (dim > diagonalParameter.getDimension() && diagonalParameter.getDimension() == 1) {
+            CompoundParameter newDiagonalParameter = new CompoundParameter(diagonalParameter.getParameterName());
+            for (int i = 0; i < dim; i++) {
+                newDiagonalParameter.addParameter(diagonalParameter);
+            }
+            diagonalParameter = newDiagonalParameter;
+        }
 
         return new DiagonalMatrix(diagonalParameter);
     }
@@ -63,6 +74,7 @@ public class DiagonalMatrixParser extends AbstractXMLObjectParser {
 
     private XMLSyntaxRule[] rules = new XMLSyntaxRule[]{
             new ElementRule(Parameter.class, 1, 1),
+            AttributeRule.newIntegerRule(DIMENSION, true)
     };
 
     public Class getReturnType() {

cdc780883da4eb7eb4989a5ba3cf114cb4116aca
mandevgill <mandevgill@ucla.edu>
2019-05-15 15:25:45 +0100

    Updated parser for Skyride and Skygrid to allow for old XML specification.

--- src/dr/evomodelxml/coalescent/GMRFSkyrideLikelihoodParser.java
+++ src/dr/evomodelxml/coalescent/GMRFSkyrideLikelihoodParser.java
@@ -26,6 +26,8 @@
 package dr.evomodelxml.coalescent;
 
 import dr.evolution.coalescent.IntervalList;
+import dr.evolution.tree.TreeUtils;
+import dr.evomodel.coalescent.TreeIntervals;
 import dr.evolution.tree.Tree;
 import dr.evomodel.coalescent.GMRFMultilocusSkyrideLikelihood;
 import dr.evomodel.coalescent.GMRFSkyrideLikelihood;
@@ -93,27 +95,41 @@ public class GMRFSkyrideLikelihoodParser extends AbstractXMLObjectParser {
         cxo = xo.getChild(PRECISION_PARAMETER);
         Parameter precParameter = (Parameter) cxo.getChild(Parameter.class);
 
-        /*
-        cxo = xo.getChild(POPULATION_TREE);
+        List<IntervalList> intervalsList = new ArrayList<IntervalList>();
 
+        if(xo.getChild(POPULATION_TREE) != null){
+            cxo = xo.getChild(POPULATION_TREE);
+            for (int i = 0; i < cxo.getChildCount(); i++){
+                Object testObject = cxo.getChild(i);
+                if(testObject instanceof Tree){
+                    TreeIntervals treeIntervals;
+                    try {
+                        treeIntervals = new TreeIntervals((Tree) testObject, null, null);
+                    } catch (TreeUtils.MissingTaxonException mte) {
+                        throw new XMLParseException("Taxon, " + mte + ", in " + getParserName() + " was not found in the tree.");
+                    }
+                    intervalsList.add(treeIntervals);
+                }
+            }
+        }
+
+        /*
         List<Tree> treeList = new ArrayList<Tree>();
         for (int i = 0; i < cxo.getChildCount(); i++) {
             Object testObject = cxo.getChild(i);
             if (testObject instanceof Tree) {
                 treeList.add((TreeModel) testObject);
             }
-        }
-        */
+        } */
 
-//        TreeModel treeModel = (TreeModel) cxo.getChild(TreeModel.class);
-
-        cxo = xo.getChild(INTERVALS);
-
-        List<IntervalList> intervalsList = new ArrayList<IntervalList>();
-        for (int i = 0; i < cxo.getChildCount(); i++){
-            Object testObject = cxo.getChild(i);
-            if(testObject instanceof IntervalList){
-                intervalsList.add((IntervalList) testObject);
+        if (xo.getChild(INTERVALS) != null) {
+            cxo = xo.getChild(INTERVALS);
+            intervalsList = new ArrayList<IntervalList>();
+            for (int i = 0; i < cxo.getChildCount(); i++) {
+                Object testObject = cxo.getChild(i);
+                if (testObject instanceof IntervalList) {
+                    intervalsList.add((IntervalList) testObject);
+                }
             }
         }
 
@@ -185,9 +201,7 @@ public class GMRFSkyrideLikelihoodParser extends AbstractXMLObjectParser {
             cxo = xo.getChild(PLOIDY);
             ploidyFactors = (Parameter) cxo.getChild(Parameter.class);
         } else {
-            // ploidyFactors = new Parameter.Default(PLOIDY, treeList.size());
             ploidyFactors = new Parameter.Default(PLOIDY, intervalsList.size());
-            //for (int i = 0; i < treeList.size(); i++) {
             for(int i = 0; i < intervalsList.size(); i++){
                 ploidyFactors.setParameterValue(i, 1.0);
             }
@@ -219,9 +233,6 @@ public class GMRFSkyrideLikelihoodParser extends AbstractXMLObjectParser {
             timeAwareSmoothing = xo.getBooleanAttribute(TIME_AWARE_SMOOTHING);
         }
 
-        // if ((dMatrix != null && beta == null) || (dMatrix == null && beta != null))
-        //     throw new XMLParseException("Must specify both a set of regression coefficients and a design matrix.");
-
         if (dMatrix != null) {
             if (dMatrix.getRowDimension() != popParameter.getDimension())
                 throw new XMLParseException("Design matrix row dimension must equal the population parameter length.");
@@ -249,7 +260,6 @@ public class GMRFSkyrideLikelihoodParser extends AbstractXMLObjectParser {
         }
 
         if (xo.hasChildNamed(COV_PREC_PARAM)){
-            // covPrecParam = new ArrayList<Parameter>();
             if(firstObservedIndex != null) {
                 covPrecParamRecent = new ArrayList<Parameter>();
             }
@@ -259,7 +269,6 @@ public class GMRFSkyrideLikelihoodParser extends AbstractXMLObjectParser {
             cxo = xo.getChild(COV_PREC_PARAM);
 
             for(int i=0; i < cxo.getChildCount(); ++i){
-                //covPrecParam.add((Parameter) cxo.getChild(i));
                 if(firstObservedIndex != null) {
                     covPrecParamRecent.add((Parameter) cxo.getChild(i));
                 }
@@ -370,12 +379,14 @@ public class GMRFSkyrideLikelihoodParser extends AbstractXMLObjectParser {
             new ElementRule(PHI_PARAMETER, new XMLSyntaxRule[]{
                     new ElementRule(Parameter.class)
             }, true), // Optional
-            /*new ElementRule(POPULATION_TREE, new XMLSyntaxRule[]{
-                    new ElementRule(TreeModel.class, 1, Integer.MAX_VALUE)
-            }),*/
-            new ElementRule(INTERVALS, new XMLSyntaxRule[]{
+            new OrRule(
+                new ElementRule(INTERVALS, new XMLSyntaxRule[]{
                     new ElementRule(IntervalList.class, 1, Integer.MAX_VALUE)
-            }),
+                }),
+                new ElementRule(POPULATION_TREE, new XMLSyntaxRule[]{
+                    new ElementRule(TreeModel.class, 1, Integer.MAX_VALUE)
+                })
+            ),
             new ElementRule(GROUP_SIZES, new XMLSyntaxRule[]{
                     new ElementRule(Parameter.class)
             }, true),

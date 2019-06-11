76a423a60c48233c6c96b32a2ce575265b6c7e56
Gabe Hassler <gabeh12345@gmail.com>
2019-06-11 12:36:36 -0700

    adding 'todo'

--- src/dr/evomodel/treedatalikelihood/continuous/WishartStatisticsWrapper.java
+++ src/dr/evomodel/treedatalikelihood/continuous/WishartStatisticsWrapper.java
@@ -135,7 +135,7 @@ public class WishartStatisticsWrapper extends AbstractModel implements Conjugate
                 sampleOffset += dimTrait;
                 bufferOffset += dimPartial;
             }
-            outerProductDelegate.setTipDataDirectly(tip, buffer, dimTrait);
+            outerProductDelegate.setTipDataDirectly(tip, buffer, dimTrait); //TODO: make sure adding the trait dimension doesn't break anything
         }
 
         if (DEBUG) {

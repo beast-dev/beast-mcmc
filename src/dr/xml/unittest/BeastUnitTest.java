873852cacf0145e0b8851866c327ee3958800616
Marc A. Suchard <msuchard@ucla.edu>
2019-06-17 06:04:50 -0700

    more assert types

--- src/dr/xml/unittest/BeastUnitTest.java
+++ src/dr/xml/unittest/BeastUnitTest.java
@@ -12,24 +12,30 @@ import java.util.regex.Pattern;
 
 public class BeastUnitTest {
 
-    private static final double tolerance = 1e-6;
+    private static final double TOLERANCE = 1e-6;
 
     private final String message;
     private final String actual;
     private final String expected;
+    private final double tolerance;
 
     private final AssertType assertType;
 
     public BeastUnitTest(String message, String actual, String expected) {
+        this(message, actual, expected, TOLERANCE);
+    }
+
+    public BeastUnitTest(String message, String actual, String expected, double tolerance) {
         this.message = message;
         this.actual = actual;
         this.expected = expected;
+        this.tolerance = tolerance;
 
         assertType = AssertType.STRING;
     }
 
     public void execute() {
-        if (!assertType.equivalent(actual, expected)) {
+        if (!assertType.equivalent(actual, expected, tolerance)) {
             failCheck();
         }
     }
@@ -44,26 +50,55 @@ public class BeastUnitTest {
     enum AssertType {
         STRING {
             @Override
-            boolean equivalent(String a, String b) {
+            boolean equivalent(String a, String b, double tolerance) {
                 return a.compareTo(b) == 0;
             }
         },
         DOUBLE {
             @Override
-            boolean equivalent(String a, String b) {
-                double aDouble = Double.valueOf(a);
-                double bDouble = Double.valueOf(b);
-                return Math.abs(aDouble - bDouble) < tolerance;
+            boolean equivalent(String a, String b, double tolerance) {
+                double lhs = Double.valueOf(a);
+                double rhs = Double.valueOf(b);
+                return close(lhs, rhs, tolerance);
             }
         },
         DOUBLE_ARRAY {
             @Override
-            boolean equivalent(String a, String b) {
-                return false;
+            boolean equivalent(String a, String b, double tolerance) {
+
+                double[] lhs = parseArray(a);
+                double[] rhs = parseArray(b);
+
+                if (lhs.length != rhs.length) {
+                    return false;
+                }
+
+                for (int i = 0; i < lhs.length; ++i) {
+                    if (!close(lhs[i], rhs[i], tolerance)) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            double[] parseArray(String string) {
+                string = string.replaceAll(",", " ");
+                String[] strings = string.split("\\s+");
+                double[] reals = new double[string.length()];
+                for (int i = 0; i < strings.length; ++i) {
+                    reals[i] = Double.valueOf(strings[i]);
+                }
+
+                return reals;
             }
         };
 
-        abstract boolean equivalent(String a, String b);
+        abstract boolean equivalent(String a, String b, double tolerance);
+
+        private static boolean close(double lhs, double rhs, double tolerance) {
+            return Math.abs(lhs - rhs) < tolerance;
+        }
     }
 
     private static final String CHECK = "assertEqual";
@@ -126,7 +161,7 @@ public class BeastUnitTest {
 
         @Override
         public Class getReturnType() {
-            return null;
+            return BeastUnitTest.class;
         }
 
         @Override

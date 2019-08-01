2971fb7a19eda1b4c089187872345fe93546a832
Gabe Hassler <gabeh12345@gmail.com>
2019-07-31 17:50:04 -0700

    second attempt at loadings hmc operator with reflection

---
+++ src/dr/inference/operators/factorAnalysis/LoadingsReflectionHMCOperator.java
@@ -0,0 +1,79 @@
+package dr.inference.operators.factorAnalysis;
+
+import dr.inference.model.Likelihood;
+import dr.inference.model.MatrixParameterInterface;
+import dr.inference.operators.hmc.HamiltonianMonteCarloOperator;
+import dr.xml.*;
+
+public class LoadingsReflectionHMCOperator extends HamiltonianMonteCarloOperator {
+
+
+    private final MatrixParameterInterface parameter;
+
+    public LoadingsReflectionHMCOperator(HamiltonianMonteCarloOperator hmcOp, MatrixParameterInterface parameter) {
+
+        super(hmcOp);
+        this.parameter = parameter;
+    }
+
+    @Override
+    public double doOperation(Likelihood joint) {
+        double hastingsRatio = super.doOperation(joint);
+
+        boolean changed = false;
+        int dim = Math.min(parameter.getRowDimension(), parameter.getColumnDimension());
+        for (int i = 0; i < dim; i++) {
+
+            if (parameter.getParameterValue(i, i) < 0) {
+                changed = true;
+                for (int j = i; j < parameter.getColumnDimension(); j++) {
+                    parameter.setParameterValueQuietly(i, j, -parameter.getParameterValue(i, j));
+                }
+            }
+        }
+
+        if (changed) {
+            parameter.fireParameterChangedEvent();
+        }
+
+        return hastingsRatio;
+
+    }
+
+    public static final String PARSER_NAME = "loadingsReflectionHMCOperator";
+
+    public static AbstractXMLObjectParser PARSER = new AbstractXMLObjectParser() {
+        @Override
+        public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+            HamiltonianMonteCarloOperator hmcOp = (HamiltonianMonteCarloOperator)
+                    xo.getChild(HamiltonianMonteCarloOperator.class);
+            MatrixParameterInterface parameter = (MatrixParameterInterface) xo.getChild(MatrixParameterInterface.class);
+            return new LoadingsReflectionHMCOperator(hmcOp, parameter);
+        }
+
+        @Override
+        public XMLSyntaxRule[] getSyntaxRules() {
+            return new XMLSyntaxRule[]{
+                    new ElementRule(HamiltonianMonteCarloOperator.class),
+                    new ElementRule(MatrixParameterInterface.class)
+            };
+        }
+
+        @Override
+        public String getParserDescription() {
+            return null;
+        }
+
+        @Override
+        public Class getReturnType() {
+            return LoadingsReflectionHMCOperator.class;
+        }
+
+        @Override
+        public String getParserName() {
+            return PARSER_NAME;
+        }
+    };
+
+
+}

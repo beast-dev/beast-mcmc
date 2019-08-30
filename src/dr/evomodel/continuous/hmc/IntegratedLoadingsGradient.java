bc8e5e38e34650aeadc55c95d7c2ea3f0c16ae50
Gabe Hassler <gabeh12345@gmail.com>
2019-08-30 12:03:18 -0700

    first (probably not working) attempt at avoiding remainder computation

--- src/dr/evomodel/continuous/hmc/IntegratedLoadingsGradient.java
+++ src/dr/evomodel/continuous/hmc/IntegratedLoadingsGradient.java
@@ -40,6 +40,7 @@ public class IntegratedLoadingsGradient implements GradientWrtParameterProvider,
     private final boolean[] missing;
     private final TaxonTaskPool taxonTaskPool;
     private final ThreadUseProvider threadUseProvider;
+    private final ContinuousDataLikelihoodDelegate likelihoodDelegate;
 
     private IntegratedLoadingsGradient(TreeDataLikelihood treeDataLikelihood,
                                        ContinuousDataLikelihoodDelegate likelihoodDelegate,
@@ -55,6 +56,8 @@ public class IntegratedLoadingsGradient implements GradientWrtParameterProvider,
         if (treeDataLikelihood.getTreeTrait(fcdName) == null) {
             likelihoodDelegate.addWrappedFullConditionalDensityTrait(traitName);
         }
+
+        this.likelihoodDelegate = likelihoodDelegate;
 
         this.fullConditionalDensity = castTreeTrait(treeDataLikelihood.getTreeTrait(fcdName));
         this.tree = treeDataLikelihood.getTree();
@@ -197,8 +200,12 @@ public class IntegratedLoadingsGradient implements GradientWrtParameterProvider,
             stopWatches[3].start();
         }
 
+        likelihoodDelegate.setComputePostOrderStatisticsOnly(true); //TODO: do this in process simulation, not here
+
         final List<WrappedNormalSufficientStatistics> allStatistics =
                 fullConditionalDensity.getTrait(tree, null); // TODO Need to test if faster to load inside loop
+
+        likelihoodDelegate.setComputePostOrderStatisticsOnly(false); //TODO: do this in process simulation, not here
 
         if (TIMING) {
             stopWatches[3].stop();
@@ -222,6 +229,8 @@ public class IntegratedLoadingsGradient implements GradientWrtParameterProvider,
                             allStatistics.get(taxon), gradients)
             );
         }
+
+        likelihood.makeDirty(); //TODO: I shouldn't need this. Need to find a way to either only flag on the last evaluation or have the determinant computed on the last step (or something else)
 
 
         return join(gradients);

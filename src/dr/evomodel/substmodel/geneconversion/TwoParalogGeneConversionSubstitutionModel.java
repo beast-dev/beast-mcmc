7a0e6318ec4bb9c6e93c8f1ff1ed8312c408c829
xji3 <xji3@ncsu.edu>
2019-09-10 06:31:19 -0500

    create branch for gene conversion related implementation with initial work on two paralog model

---
+++ src/dr/evomodel/substmodel/geneconversion/TwoParalogGeneConversionSubstitutionModel.java
@@ -0,0 +1,69 @@
+/*
+ * BranchRateGradientForDiscreteTrait.java
+ *
+ * Copyright (c) 2002-2017 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package dr.evomodel.substmodel.geneconversion;
+
+import dr.evolution.datatype.DataType;
+import dr.evolution.datatype.PairedDataType;
+import dr.evomodel.substmodel.*;
+import dr.inference.model.Parameter;
+
+/**
+ * @author Xiang Ji
+ * @author Jeff Thorne
+ * @author Marc A. Suchard
+ */
+public class TwoParalogGeneConversionSubstitutionModel extends AbstractCovarionModel {
+
+    private final BaseSubstitutionModel baseSubstitutionModel;
+    private final Parameter igcRateParameter;
+
+    public TwoParalogGeneConversionSubstitutionModel(String name,
+                                                     BaseSubstitutionModel baseSubstitutionModel,
+                                                     Parameter paralogFrequencyParameter,
+                                                     Parameter relativeGeneConversionRateParameter,
+                                                     PairedDataType dataType) {
+
+        super(name, dataType, baseSubstitutionModel.getFrequencyModel().getFrequencyParameter(), paralogFrequencyParameter);
+        this.baseSubstitutionModel = baseSubstitutionModel;
+        this.igcRateParameter = relativeGeneConversionRateParameter;
+
+    }
+
+    @Override
+    protected void frequenciesChanged() {
+
+    }
+
+    @Override
+    protected void ratesChanged() {
+
+    }
+
+    @Override
+    protected double getNormalizationValue(double[][] matrix, double[] pi) {
+        return 0;
+    }
+}

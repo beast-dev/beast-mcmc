ed2ac7399bade79b8c25e3cce8cc50a3699ba691
xji3 <xji3@ncsu.edu>
2019-11-14 13:40:49 -0800

    preliminary implementation of HomogeneousSubstitutionParameterGradient

--- src/dr/evomodel/treedatalikelihood/discrete/BranchDifferentialMassProvider.java
+++ src/dr/evomodel/treedatalikelihood/discrete/BranchDifferentialMassProvider.java
@@ -11,18 +11,52 @@ import java.util.List;
  */
 class BranchDifferentialMassProvider {
 
-    private TreeParameterModel indexHelper;
+    private IndexHelper indexHelper;
     private List<DifferentialMassProvider> differentialMassProviderList;
 
     BranchDifferentialMassProvider(TreeParameterModel indexHelper,
                                    List<DifferentialMassProvider> differentialMassProviderList) {
 
-        this.indexHelper = indexHelper;
+        this.indexHelper = factory(indexHelper);
         this.differentialMassProviderList = differentialMassProviderList;
 
     }
 
     double[] getDifferentialMassMatrixForBranch(NodeRef node, double time) {
         return differentialMassProviderList.get(indexHelper.getParameterIndexFromNodeNumber(node.getNumber())).getDifferentialMassMatrix(time);
+    }
+
+    private IndexHelper factory(TreeParameterModel indexHelper) {
+        if (indexHelper == null) {
+            return new IndexHelper.HomogeneousIndexHelper();
+        } else {
+            return new IndexHelper.TreeParameterIndexHelper(indexHelper);
+        }
+    }
+
+    private interface IndexHelper {
+        int getParameterIndexFromNodeNumber(int nodeNumber);
+
+        class TreeParameterIndexHelper implements IndexHelper {
+
+            private TreeParameterModel helper;
+
+            TreeParameterIndexHelper(TreeParameterModel helper) {
+                this.helper = helper;
+            }
+
+            @Override
+            public int getParameterIndexFromNodeNumber(int nodeNumber) {
+                return helper.getParameterIndexFromNodeNumber(nodeNumber);
+            }
+        }
+
+        class HomogeneousIndexHelper implements IndexHelper {
+
+            @Override
+            public int getParameterIndexFromNodeNumber(int nodeNumber) {
+                return 0;
+            }
+        }
     }
 }
--- src/dr/evomodelxml/continuous/hmc/BranchSubstitutionParameterGradientParser.java
+++ src/dr/evomodelxml/continuous/hmc/BranchSubstitutionParameterGradientParser.java
@@ -31,8 +31,10 @@ import dr.evomodel.branchratemodel.BranchRateModel;
 import dr.evomodel.treedatalikelihood.BeagleDataLikelihoodDelegate;
 import dr.evomodel.treedatalikelihood.TreeDataLikelihood;
 import dr.evomodel.treedatalikelihood.discrete.BranchSubstitutionParameterGradient;
+import dr.evomodel.treedatalikelihood.discrete.HomogeneousSubstitutionParameterGradient;
 import dr.evomodelxml.treelikelihood.TreeTraitParserUtilities;
 import dr.inference.model.CompoundParameter;
+import dr.inference.model.Parameter;
 import dr.xml.*;
 
 import static dr.evomodelxml.treelikelihood.TreeTraitParserUtilities.DEFAULT_TRAIT_NAME;
@@ -45,6 +47,7 @@ public class BranchSubstitutionParameterGradientParser extends AbstractXMLObject
 
     private static final String NAME = "branchSubstitutionParameterGradient";
     private static final String TRAIT_NAME = TreeTraitParserUtilities.TRAIT_NAME;
+    private static final String HOMOGENEOUS_PROCESS = "homogeneous";
     public static final String USE_HESSIAN = "useHessian";
 
     @Override
@@ -62,11 +65,16 @@ public class BranchSubstitutionParameterGradientParser extends AbstractXMLObject
 
         BeagleDataLikelihoodDelegate beagleData = (BeagleDataLikelihoodDelegate) treeDataLikelihood.getDataLikelihoodDelegate();
 
-        BranchRateModel branchRateModel = (BranchRateModel) xo.getChild(BranchRateModel.class);
-        CompoundParameter branchParameter = branchModel.getBranchSpecificParameters(branchRateModel);
-
-        return new BranchSubstitutionParameterGradient(traitName, treeDataLikelihood, beagleData,
-                branchParameter, branchRateModel, useHessian);
+        Boolean homogeneous = xo.getAttribute(HOMOGENEOUS_PROCESS, false);
+        if (homogeneous) {
+            Parameter parameter = (Parameter) xo.getChild(Parameter.class);
+            return new HomogeneousSubstitutionParameterGradient(traitName, treeDataLikelihood, parameter, beagleData);
+        } else {
+            BranchRateModel branchRateModel = (BranchRateModel) xo.getChild(BranchRateModel.class);
+            CompoundParameter branchParameter = branchModel.getBranchSpecificParameters(branchRateModel);
+            return new BranchSubstitutionParameterGradient(traitName, treeDataLikelihood, beagleData,
+                    branchParameter, branchRateModel, useHessian);
+        }
     }
 
     @Override
@@ -77,8 +85,12 @@ public class BranchSubstitutionParameterGradientParser extends AbstractXMLObject
     private final XMLSyntaxRule[] rules = {
             AttributeRule.newStringRule(TRAIT_NAME),
             new ElementRule(TreeDataLikelihood.class),
-            new ElementRule(BranchSpecificSubstitutionParameterBranchModel.class),
-            new ElementRule(BranchRateModel.class)
+            new XORRule(
+                    new AndRule(
+                            new ElementRule(BranchSpecificSubstitutionParameterBranchModel.class),
+                            new ElementRule(BranchRateModel.class)),
+                    new ElementRule(Parameter.class)),
+            AttributeRule.newBooleanRule(HOMOGENEOUS_PROCESS, true)
     };
 
     @Override

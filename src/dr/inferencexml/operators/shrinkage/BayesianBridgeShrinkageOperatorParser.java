7561c6f3acce4b24e7bd06c30f19d54f4ecbe77b
Gabe Hassler <gabriel.w.hassler@gmail.com>
2020-02-13 09:18:36 -0800

    BayesianBridgeShrinkageOperator doesn't require gamma prior

--- src/dr/inferencexml/operators/shrinkage/BayesianBridgeShrinkageOperatorParser.java
+++ src/dr/inferencexml/operators/shrinkage/BayesianBridgeShrinkageOperatorParser.java
@@ -27,11 +27,17 @@ public class BayesianBridgeShrinkageOperatorParser extends AbstractXMLObjectPars
             bayesianBridge = parseAutoCorrelatedRates(xo);
         }
 
+        GammaDistribution globalScalePrior = null;
+
         DistributionLikelihood prior = (DistributionLikelihood) xo.getChild(DistributionLikelihood.class);
-        if (!(prior.getDistribution() instanceof GammaDistribution)) {
-            throw new XMLParseException("Gibbs sampler only implemented for a gamma distributed global scale");
+        if (prior != null) {
+            if (prior.getDistribution() instanceof GammaDistribution) {
+                globalScalePrior = (GammaDistribution) prior.getDistribution();
+            } else {
+                throw new XMLParseException("Gibbs sampler only implemented for a gamma distributed global scale");
+            }
         }
-        GammaDistribution globalScalePrior = (GammaDistribution) prior.getDistribution();
+
 
         return new BayesianBridgeShrinkageOperator(bayesianBridge, globalScalePrior, weight);
     }
@@ -85,7 +91,7 @@ public class BayesianBridgeShrinkageOperatorParser extends AbstractXMLObjectPars
                     new ElementRule(BayesianBridgeStatisticsProvider.class),
                     new ElementRule(AutoCorrelatedBranchRatesDistribution.class)
             ),
-            new ElementRule(DistributionLikelihood.class),
+            new ElementRule(DistributionLikelihood.class, true),
     };
 
     @Override

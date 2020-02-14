2a80234f09582917d95cacb041d3cd9d50a8ad57
Marc A. Suchard <msuchard@ucla.edu>
2020-02-14 12:02:01 -0800

    better interface

--- src/dr/evomodel/operators/NativeZigZag.java
+++ src/dr/evomodel/operators/NativeZigZag.java
@@ -3,7 +3,49 @@ package dr.evomodel.operators;
 import dr.inference.hmc.PrecisionColumnProvider;
 import dr.inference.operators.hmc.MinimumTravelInformation;
 
-class NativeZigZag {
+public class NativeZigZag {
+
+    public enum Flag {
+        PRECISION_DOUBLE(1L << 1, "double precision computation"),
+        PRECISION_SINGLE(1L << 2, "single precision computation"),
+        FRAMEWORK_TBB(1L << 3, "TBB multi-core threading"),
+        FRAMEWORK_OPENCL(1L << 4, "use OpenCL implementation with CPU or GPU resources"),
+        SIMD_SSE(1L << 7, "use SSE SIMD vectorization"),
+        SIMD_AVX(1L << 8, "use AVX SIMD vectorization"),
+        SIMD_AVX512(1L << 9, "use AVX-512 SIMD vectorization");
+
+        private final long mask;
+        private final String meaning;
+
+        Flag(long mask, String meaning) {
+            this.mask = mask;
+            this.meaning = meaning;
+        }
+
+        @SuppressWarnings("unused")
+        public String getMeaning() {
+            return meaning;
+        }
+
+        public long getMask() {
+            return this.mask;
+        }
+
+        public boolean isSet(long flag) {
+            return (flag & this.mask) != 0L;
+        }
+
+        public static String toString(long flag) {
+            StringBuilder sb = new StringBuilder();
+            for (Flag mask : values()) {
+                if (mask.isSet(flag)) {
+                    sb.append(" ").append(mask.name());
+                }
+            }
+
+            return sb.toString();
+        }
+    }
 
     static NativeZigZag INSTANCE;
     private static int instanceNumber = 0;
@@ -11,17 +53,17 @@ class NativeZigZag {
     private NativeZigZag() { }
 
     int createInstance(int dimension,
-                       PrecisionColumnProvider columnProvider,
+                       NativeZigZagOptions options,
                        double[] mask,
                        double[] observed) {
 
-        double[] column = columnProvider.getColumn(0);
-        if (dimension != column.length || (mask != null && dimension != mask.length) ||
+
+        if ((mask != null && dimension != mask.length) ||
                 (observed != null && dimension != observed.length)) {
             throw new RuntimeException("Invalid dimensions");
         }
 
-        int result = create(dimension, columnProvider, mask, observed);
+        int result = create(dimension, options, mask, observed);
         if (result < 0) {
             throw new RuntimeException("Unable to create instance");
         }
@@ -30,7 +72,7 @@ class NativeZigZag {
     }
 
     private native int create(int dimension,
-                              PrecisionColumnProvider columnProvider,
+                              NativeZigZagOptions options,
                               double[] mask,
                               double[] observed);
 
@@ -87,8 +129,7 @@ class NativeZigZag {
                                                              double[] position,
                                                              double[] velocity,
                                                              double[] action,
-                                                             double[] gradient,
-                                                             long seed);
+                                                             double[] gradient);
     static {
         System.loadLibrary("zig_zag");
         INSTANCE = new NativeZigZag();

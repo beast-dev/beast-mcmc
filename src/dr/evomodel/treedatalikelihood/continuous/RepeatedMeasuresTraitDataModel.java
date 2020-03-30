9cfe4ac3626d6af48b800e931cba1b5608514122
Paul Bastide <paul.bastide@gmail.com>
2020-03-30 15:47:10 +0200

    Move RepeatedMeasureTraitDataModel parser to its own class file.

--- src/dr/evomodel/treedatalikelihood/continuous/RepeatedMeasuresTraitDataModel.java
+++ src/dr/evomodel/treedatalikelihood/continuous/RepeatedMeasuresTraitDataModel.java
@@ -25,24 +25,18 @@
 
 package dr.evomodel.treedatalikelihood.continuous;
 
-import dr.evolution.tree.MutableTreeModel;
 import dr.evolution.tree.Tree;
 import dr.evolution.tree.TreeTrait;
-import dr.evomodel.tree.TreeModel;
 import dr.evomodel.treedatalikelihood.continuous.cdi.PrecisionType;
 import dr.evomodel.treedatalikelihood.preorder.ContinuousExtensionDelegate;
 import dr.evomodel.treedatalikelihood.preorder.ModelExtensionProvider;
-import dr.evomodelxml.treelikelihood.TreeTraitParserUtilities;
 import dr.inference.model.CompoundParameter;
 import dr.inference.model.MatrixParameterInterface;
 import dr.inference.model.Parameter;
 import dr.inference.model.Variable;
-import dr.math.matrixAlgebra.CholeskyDecomposition;
-import dr.math.matrixAlgebra.IllegalDimension;
 import dr.math.matrixAlgebra.Matrix;
 import dr.math.matrixAlgebra.WrappedVector;
 import dr.math.matrixAlgebra.missingData.MissingOps;
-import dr.xml.*;
 import org.ejml.data.DenseMatrix64F;
 
 import java.util.List;
@@ -232,100 +226,5 @@ public class RepeatedMeasuresTraitDataModel extends ContinuousTraitDataModel imp
     }
 
     private static final boolean DEBUG = false;
-
-    // TODO Move remainder into separate class file
-    public static final String REPEATED_MEASURES_MODEL = "repeatedMeasuresModel";
-    private static final String PRECISION = "samplingPrecision";
-
-    public static AbstractXMLObjectParser PARSER = new AbstractXMLObjectParser() {
-        @Override
-        public Object parseXMLObject(XMLObject xo) throws XMLParseException {
-
-            MutableTreeModel treeModel = (MutableTreeModel) xo.getChild(TreeModel.class);
-            TreeTraitParserUtilities utilities = new TreeTraitParserUtilities();
-
-            TreeTraitParserUtilities.TraitsAndMissingIndices returnValue =
-                    utilities.parseTraitsFromTaxonAttributes(xo, TreeTraitParserUtilities.DEFAULT_TRAIT_NAME,
-                            treeModel, true);
-            CompoundParameter traitParameter = returnValue.traitParameter;
-            List<Integer> missingIndices = returnValue.missingIndices;
-
-            XMLObject cxo = xo.getChild(PRECISION);
-            MatrixParameterInterface samplingPrecision = (MatrixParameterInterface)
-                    cxo.getChild(MatrixParameterInterface.class);
-
-            CholeskyDecomposition chol;
-            try {
-                chol = new CholeskyDecomposition(samplingPrecision.getParameterAsMatrix());
-            } catch (IllegalDimension illegalDimension) {
-                throw new XMLParseException(PRECISION + " must be a square matrix.");
-            }
-
-            if (!chol.isSPD()) {
-                throw new XMLParseException(PRECISION + " must be a positive definite matrix.");
-            }
-
-
-            String traitName = returnValue.traitName;
-            //TODO diffusionModel was only used for the dimension.
-            // But this should be the same as the samplingPrecision dimension ?
-//            MultivariateDiffusionModel diffusionModel = (MultivariateDiffusionModel)
-//                    xo.getChild(MultivariateDiffusionModel.class);
-
-            //TODO: This was never used.
-//            final boolean[] missingIndicators = new boolean[returnValue.traitParameter.getDimension()];
-//            for (int i : missingIndices) {
-//                missingIndicators[i] = true;
-//            }
-
-            return new RepeatedMeasuresTraitDataModel(
-                    traitName,
-                    traitParameter,
-                    missingIndices,
-//                    missingIndicators,
-                    true,
-                    samplingPrecision.getColumnDimension(),
-//                    diffusionModel.getPrecisionParameter().getRowDimension(),
-                    samplingPrecision
-            );
-        }
-
-        @Override
-        public XMLSyntaxRule[] getSyntaxRules() {
-            return rules;
-        }
-
-        @Override
-        public String getParserDescription() {
-            return null;
-        }
-
-        @Override
-        public Class getReturnType() {
-            return RepeatedMeasuresTraitDataModel.class;
-        }
-
-        @Override
-        public String getParserName() {
-            return REPEATED_MEASURES_MODEL;
-        }
-    };
-
-    private final static XMLSyntaxRule[] rules = new XMLSyntaxRule[]{
-            new ElementRule(PRECISION, new XMLSyntaxRule[]{
-                    new ElementRule(Parameter.class),
-            }),
-            // Tree trait parser
-            new ElementRule(MutableTreeModel.class),
-            AttributeRule.newStringRule(TreeTraitParserUtilities.TRAIT_NAME),
-            new ElementRule(TreeTraitParserUtilities.TRAIT_PARAMETER, new XMLSyntaxRule[]{
-                    new ElementRule(Parameter.class)
-            }),
-            new ElementRule(TreeTraitParserUtilities.MISSING, new XMLSyntaxRule[]{
-                    new ElementRule(Parameter.class)
-            }, true),
-//            new ElementRule(MultivariateDiffusionModel.class),
-    };
-
 
 }
---
+++ src/dr/evomodel/treedatalikelihood/continuous/RepeatedMeasuresTraitDataModelParser.java
@@ -0,0 +1,131 @@
+/*
+ * RepeatedMeasuresTraitDataModelParser.java
+ *
+ * Copyright (c) 2002-2020 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package dr.evomodel.treedatalikelihood.continuous;
+
+import dr.evolution.tree.MutableTreeModel;
+import dr.evomodel.tree.TreeModel;
+import dr.evomodelxml.treelikelihood.TreeTraitParserUtilities;
+import dr.inference.model.CompoundParameter;
+import dr.inference.model.MatrixParameterInterface;
+import dr.inference.model.Parameter;
+import dr.math.matrixAlgebra.CholeskyDecomposition;
+import dr.math.matrixAlgebra.IllegalDimension;
+import dr.xml.*;
+
+import java.util.List;
+
+public class RepeatedMeasuresTraitDataModelParser extends AbstractXMLObjectParser {
+    public static final String REPEATED_MEASURES_MODEL = "repeatedMeasuresModel";
+    private static final String PRECISION = "samplingPrecision";
+
+    @Override
+    public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+
+        MutableTreeModel treeModel = (MutableTreeModel) xo.getChild(TreeModel.class);
+        TreeTraitParserUtilities utilities = new TreeTraitParserUtilities();
+
+        TreeTraitParserUtilities.TraitsAndMissingIndices returnValue =
+                utilities.parseTraitsFromTaxonAttributes(xo, TreeTraitParserUtilities.DEFAULT_TRAIT_NAME,
+                        treeModel, true);
+        CompoundParameter traitParameter = returnValue.traitParameter;
+        List<Integer> missingIndices = returnValue.missingIndices;
+
+        XMLObject cxo = xo.getChild(PRECISION);
+        MatrixParameterInterface samplingPrecision = (MatrixParameterInterface)
+                cxo.getChild(MatrixParameterInterface.class);
+
+        CholeskyDecomposition chol;
+        try {
+            chol = new CholeskyDecomposition(samplingPrecision.getParameterAsMatrix());
+        } catch (IllegalDimension illegalDimension) {
+            throw new XMLParseException(PRECISION + " must be a square matrix.");
+        }
+
+        if (!chol.isSPD()) {
+            throw new XMLParseException(PRECISION + " must be a positive definite matrix.");
+        }
+
+
+        String traitName = returnValue.traitName;
+        //TODO diffusionModel was only used for the dimension.
+        // But this should be the same as the samplingPrecision dimension ?
+//            MultivariateDiffusionModel diffusionModel = (MultivariateDiffusionModel)
+//                    xo.getChild(MultivariateDiffusionModel.class);
+
+        //TODO: This was never used.
+//            final boolean[] missingIndicators = new boolean[returnValue.traitParameter.getDimension()];
+//            for (int i : missingIndices) {
+//                missingIndicators[i] = true;
+//            }
+
+        return new RepeatedMeasuresTraitDataModel(
+                traitName,
+                traitParameter,
+                missingIndices,
+//                    missingIndicators,
+                true,
+                samplingPrecision.getColumnDimension(),
+//                    diffusionModel.getPrecisionParameter().getRowDimension(),
+                samplingPrecision
+        );
+    }
+
+    @Override
+    public XMLSyntaxRule[] getSyntaxRules() {
+        return rules;
+    }
+
+    @Override
+    public String getParserDescription() {
+        return null;
+    }
+
+    @Override
+    public Class getReturnType() {
+        return RepeatedMeasuresTraitDataModel.class;
+    }
+
+    @Override
+    public String getParserName() {
+        return REPEATED_MEASURES_MODEL;
+    }
+
+    private final static XMLSyntaxRule[] rules = new XMLSyntaxRule[]{
+            new ElementRule(PRECISION, new XMLSyntaxRule[]{
+                    new ElementRule(Parameter.class),
+            }),
+            // Tree trait parser
+            new ElementRule(MutableTreeModel.class),
+            AttributeRule.newStringRule(TreeTraitParserUtilities.TRAIT_NAME),
+            new ElementRule(TreeTraitParserUtilities.TRAIT_PARAMETER, new XMLSyntaxRule[]{
+                    new ElementRule(Parameter.class)
+            }),
+            new ElementRule(TreeTraitParserUtilities.MISSING, new XMLSyntaxRule[]{
+                    new ElementRule(Parameter.class)
+            }, true),
+//            new ElementRule(MultivariateDiffusionModel.class),
+    };
+}

b33beb1c214c7d9500788df2fa674992a3254fb0
GuyBaele <baele.guy@gmail.com>
2020-04-05 21:41:21 +0200

    Some more flexibility in terms of independence sampling from a parametric coalescent model

--- src/dr/evomodel/continuous/GibbsIndependentCoalescentOperator.java
+++ src/dr/evomodel/continuous/GibbsIndependentCoalescentOperator.java
@@ -25,25 +25,19 @@
 
 package dr.evomodel.continuous;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import dr.evolution.tree.SimpleTree;
 import dr.evolution.tree.Tree;
 import dr.evolution.util.Taxa;
 import dr.evolution.util.TaxonList;
-import dr.evomodel.coalescent.CoalescentLikelihood;
 import dr.evomodel.coalescent.CoalescentSimulator;
 import dr.evomodel.coalescent.DemographicModel;
 import dr.evomodel.tree.TreeModel;
 import dr.inference.operators.GibbsOperator;
 import dr.inference.operators.MCMCOperator;
 import dr.inference.operators.SimpleMCMCOperator;
-import dr.xml.AttributeRule;
-import dr.xml.ElementRule;
-import dr.xml.XMLObject;
-import dr.xml.XMLParseException;
-import dr.xml.XMLSyntaxRule;
+import dr.xml.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * An independent coalescent sampler, based on the coalescent simulator parser code.
@@ -54,18 +48,21 @@ import dr.xml.XMLSyntaxRule;
 public class GibbsIndependentCoalescentOperator extends SimpleMCMCOperator implements GibbsOperator {
 
 	public static final String OPERATOR_NAME = "GibbsIndependentCoalescentOperator";
-    
-    private TreeModel treeModel;
+	public static final String HEIGHT = "height";
+
+	private XMLObject xo;
+	private TreeModel treeModel;
     private DemographicModel demoModel;
-    private CoalescentLikelihood coalescent;
-    private XMLObject xo;
-	
-	public GibbsIndependentCoalescentOperator(XMLObject xo, TreeModel treeModel, DemographicModel demoModel, CoalescentLikelihood coalescent, double weight) {
+	private double rootHeight;
+
+    private final dr.evolution.coalescent.CoalescentSimulator simulator = new dr.evolution.coalescent.CoalescentSimulator();
+
+	public GibbsIndependentCoalescentOperator(XMLObject xo, TreeModel treeModel, DemographicModel model, double rootHeight, double weight) {
 
 		this.xo = xo;
 		this.treeModel = treeModel;
-		this.demoModel = demoModel;
-		this.coalescent = coalescent;
+		this.demoModel = model;
+		this.rootHeight = rootHeight;
 		setWeight(weight);
 		
 	}
@@ -91,61 +88,87 @@ public class GibbsIndependentCoalescentOperator extends SimpleMCMCOperator imple
 	 * change the parameter and return the hastings ratio.
      */
 	public double doOperation() {
-		
+
 		CoalescentSimulator simulator = new CoalescentSimulator();
-        
-        List<TaxonList> taxonLists = new ArrayList<TaxonList>();
 
-        double rootHeight = -1.0;
-        double oldLikelihood = 0.0;
-        double newLikelihood = 0.0;
+		//store the generated tree
+		Tree tree;
 
-        // should have one child that is node
-        for (int i = 0; i < xo.getChildCount(); i++) {
-            final Object child = xo.getChild(i);
-            
-            //careful: Trees are TaxonLists ... (AER); see OldCoalescentSimulatorParser
-            if (child instanceof Tree) {
-                //do nothing
-            } else if (child instanceof TaxonList) {
-                //taxonLists.add((TaxonList) child);
-                taxonLists.add((Taxa) child);
-                //taxa added
-                break;
-            } 
-        }
+		List<TaxonList> taxonLists = new ArrayList<TaxonList>();
+		List<Tree> subtrees = new ArrayList<Tree>();
 
-        try {
-        	
-        	Tree[] trees = new Tree[taxonLists.size()];
-            // simulate each taxonList separately
-            for (int i = 0; i < taxonLists.size(); i++) {
-                trees[i] = simulator.simulateTree(taxonLists.get(i), demoModel);
-            }
+		// should have one child that is node
+		for (int i = 0; i < xo.getChildCount(); i++) {
+			final Object child = xo.getChild(i);
 
-            oldLikelihood = coalescent.getLogLikelihood();
-            
-            SimpleTree simTree = simulator.simulateTree(trees, demoModel, rootHeight, trees.length != 1);
-            
-            //this would be the normal way to do it
-            treeModel.beginTreeEdit();
-            //now it's allowed to adjust the tree structure
-            treeModel.adoptTreeStructure(simTree);
-            //endTreeEdit() would then fire the events
-            treeModel.endTreeEdit();
-            
-            newLikelihood = coalescent.getLogLikelihood();
-            
-        } catch (IllegalArgumentException iae) {
-            try {
-				throw new XMLParseException(iae.getMessage());
-			} catch (XMLParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			// AER - swapped the order of these round because Trees are TaxonLists...
+			if (child instanceof TreeModel) {
+				//do nothing
+			} else if (child instanceof Tree) {
+				//don't do this as all subtrees will remain identical
+				//subtrees.add((Tree) child);
+
+				Tree currentTree = (Tree)child;
+				Tree[] newTrees = new Tree[currentTree.getTaxonCount()];
+				for (int j = 0; j < currentTree.getTaxonCount(); j++) {
+					Taxa tip = new Taxa();
+					tip.addTaxon(currentTree.getTaxon(j));
+					newTrees[j] = simulator.simulateTree(tip, demoModel);
+				}
+				Tree newTree = simulator.simulateTree(newTrees, demoModel, rootHeight, newTrees.length != 1);
+				subtrees.add(newTree);
+			} else if (child instanceof TaxonList) {
+				taxonLists.add((TaxonList) child);
 			}
-        }
+		}
+
+		if (taxonLists.size() == 0) {
+			if (subtrees.size() == 1) {
+				//System.out.println(" *** 1 *** ");
+				tree = subtrees.get(0);
+				//this would be the normal way to do it
+				treeModel.beginTreeEdit();
+				//now it's allowed to adjust the tree structure
+				treeModel.adoptTreeStructure(tree);
+				//endTreeEdit() would then fire the events
+				treeModel.endTreeEdit();
+				return 0;
+			}
+			throw new RuntimeException("Expected at least one taxonList or two subtrees in "
+					+ getOperatorName() + " XML specification.");
+		}
+
+		Taxa remainingTaxa = new Taxa();
+		for (int i = 0; i < taxonLists.size(); i++) {
+			remainingTaxa.addTaxa(taxonLists.get(i));
+		}
+
+		for (int i = 0; i < subtrees.size(); i++) {
+			remainingTaxa.removeTaxa(subtrees.get(i));
+		}
+
+		Tree[] trees = new Tree[subtrees.size() + remainingTaxa.getTaxonCount()];
+		// add the preset trees
+		for (int i = 0; i < subtrees.size(); i++) {
+			trees[i] = subtrees.get(i);
+		}
+
+		// add all the remaining taxa in as single tip trees...
+		for (int i = 0; i < remainingTaxa.getTaxonCount(); i++) {
+			Taxa tip = new Taxa();
+			tip.addTaxon(remainingTaxa.getTaxon(i));
+			trees[i + subtrees.size()] = simulator.simulateTree(tip, demoModel);
+		}
+
+		tree = simulator.simulateTree(trees, demoModel, rootHeight, trees.length != 1);
+
+		//this would be the normal way to do it
+		treeModel.beginTreeEdit();
+		//now it's allowed to adjust the tree structure
+		treeModel.adoptTreeStructure(tree);
+		//endTreeEdit() would then fire the events
+		treeModel.endTreeEdit();
 		
-		//return oldLikelihood - newLikelihood;
         return 0;
 	}
 	
@@ -156,14 +179,15 @@ public class GibbsIndependentCoalescentOperator extends SimpleMCMCOperator imple
         }
 
 		public Object parseXMLObject(XMLObject xo) throws XMLParseException {
-			
-			TreeModel treeModel = (TreeModel) xo.getChild(TreeModel.class);
+
 			double weight = xo.getDoubleAttribute(MCMCOperator.WEIGHT);
-	        DemographicModel demoModel = (DemographicModel) xo.getChild(DemographicModel.class);
-	        CoalescentLikelihood coalescent = (CoalescentLikelihood) xo.getChild(CoalescentLikelihood.class);
-	        
-			return new GibbsIndependentCoalescentOperator(xo, treeModel, demoModel, coalescent, weight);
-			
+			TreeModel treeModel = (TreeModel) xo.getChild(TreeModel.class);
+			DemographicModel demoModel = (DemographicModel) xo.getChild(DemographicModel.class);
+			double height = xo.getAttribute(HEIGHT, Double.NaN);
+
+			//return new GibbsIndependentCoalescentOperator(trees, treeModel, demoModel, height, trees.length != 1, weight);
+			return new GibbsIndependentCoalescentOperator(xo, treeModel, demoModel, height, weight);
+
 		}
 		
 		//************************************************************************
@@ -176,10 +200,11 @@ public class GibbsIndependentCoalescentOperator extends SimpleMCMCOperator imple
 		
 		private final XMLSyntaxRule[] rules = {
 				AttributeRule.newDoubleRule(MCMCOperator.WEIGHT),
-				new ElementRule(Taxa.class),
+				AttributeRule.newDoubleRule(HEIGHT, true, ""),
                 new ElementRule(TreeModel.class),
                 new ElementRule(DemographicModel.class),
-                new ElementRule(CoalescentLikelihood.class)
+				new ElementRule(Tree.class, 0, Integer.MAX_VALUE),
+				new ElementRule(TaxonList.class, 0, Integer.MAX_VALUE)
         };
 
 		public String getParserDescription() {

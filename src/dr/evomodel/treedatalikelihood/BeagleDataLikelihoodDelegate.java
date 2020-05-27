bc589ff602796729f5e11305c701cdbe4a05fd17
Andrew Rambaut <rambaut@gmail.com>
2020-05-27 23:34:58 +0100

    Prefer GPU is an XML atribute on TreeDataLikelihood

--- src/dr/evomodel/treedatalikelihood/BeagleDataLikelihoodDelegate.java
+++ src/dr/evomodel/treedatalikelihood/BeagleDataLikelihoodDelegate.java
@@ -133,6 +133,7 @@ public class BeagleDataLikelihoodDelegate extends AbstractModel implements DataL
                                         BranchModel branchModel,
                                         SiteRateModel siteRateModel,
                                         boolean useAmbiguities,
+                                        boolean preferGPU,
                                         PartialsRescalingScheme rescalingScheme,
                                         boolean delayRescalingUntilUnderflow,
                                         PreOrderSettings settings) {
@@ -258,6 +259,9 @@ public class BeagleDataLikelihoodDelegate extends AbstractModel implements DataL
                 if (resourceList[0] > 0) {
                     preferenceFlags |= BeagleFlag.PROCESSOR_GPU.getMask(); // Add preference weight against CPU
                 }
+            }
+            if (preferGPU) {
+                preferenceFlags |= BeagleFlag.PROCESSOR_GPU.getMask(); // Add preference weight against CPU
             }
 
             if (preferredOrder.size() > 0) {

47d0f7f7b74c69e6b2f9c48e8f85c94d5af3a5b8
JT <mccrone@umich.edu>
2020-07-08 12:12:04 +0100

    adds constructor to handle a list of nodes to act on

---
+++ src/test/dr/evomodel/treelikelihood/ApproximatePoissonTreeLikelihoodTest.java
@@ -0,0 +1,11 @@
+package test.dr.evomodel.treelikelihood;
+
+import junit.framework.TestCase;
+
+public class ApproximatePoissonTreeLikelihoodTest extends TestCase {
+    private String treeString = "((A:1,B:1,C:1):1,D:1);";
+
+    public void testMapping(){
+
+    }
+}

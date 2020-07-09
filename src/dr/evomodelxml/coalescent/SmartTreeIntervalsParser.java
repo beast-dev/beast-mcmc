e271d00b3d90f1ad3352a535ac757d2878de0071
jtmccr1 <mccronejt@gmail.com>
2020-07-09 17:13:34 +0100

    adds SmartTreeIntervals for cached Intervals and fixes bug in skygrid intervals.
    Work to do to make these changes more broadly applicable.

---
+++ src/dr/evomodelxml/coalescent/SmartTreeIntervalsParser.java
@@ -0,0 +1,50 @@
+package dr.evomodelxml.coalescent;
+
+import dr.evolution.tree.Tree;
+import dr.evolution.tree.TreeUtils;
+import dr.evomodel.coalescent.SmartTreeIntervals;
+import dr.evomodel.coalescent.TreeIntervals;
+import dr.evomodel.tree.TreeModel;
+import dr.xml.*;
+
+public class SmartTreeIntervalsParser extends AbstractXMLObjectParser{
+
+    public static final String TREE_INTERVALS = "smartTreeIntervals";
+    public static final String TREE = "tree";
+
+    public String getParserName() {
+        return TREE_INTERVALS;
+    }
+
+    public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+
+        Tree tree = (Tree) xo.getChild(Tree.class);
+
+        try {
+            return new SmartTreeIntervals(tree, null, null);
+        } catch (TreeUtils.MissingTaxonException mte) {
+            throw new XMLParseException("Taxon, " + mte + ", in " + getParserName() + " was not found in the tree.");
+        }
+    }
+
+    //************************************************************************
+    // AbstractXMLObjectParser implementation
+    //************************************************************************
+
+    public String getParserDescription() {
+        return "Parser for TreeIntervals.";
+    }
+
+    public Class getReturnType() {
+        return TreeIntervals.class;
+    }
+
+    public XMLSyntaxRule[] getSyntaxRules() {
+        return rules;
+    }
+
+    private final XMLSyntaxRule[] rules = {
+            new ElementRule(TreeModel.class)
+    };
+
+}

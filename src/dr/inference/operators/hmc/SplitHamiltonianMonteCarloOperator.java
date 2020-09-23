07b0219e7c10689cf55e7d8c0c64ba88bff98993
Zhenyu Zhang <zhangzhenyusa@gmail.com>
2020-09-23 10:54:16 -0700

    merge splitHMC into splitHMCoperator

--- src/dr/inference/operators/hmc/SplitHamiltonianMonteCarloOperator.java
+++ src/dr/inference/operators/hmc/SplitHamiltonianMonteCarloOperator.java
@@ -20,12 +20,12 @@ import dr.util.Transform;
  */
 
 public class SplitHamiltonianMonteCarloOperator extends AbstractAdaptableOperator implements GeneralOperator,
-        ReversibleHMCProvider {//todo: implement ReversibleHMCProvider.
+        ReversibleHMCProvider {
 
     private double stepSize;
     private double relativeScale;
-    private ReversibleHMCProvider reversibleHMCProviderA;
-    private ReversibleHMCProvider reversibleHMCProviderB;
+    private ReversibleHMCProvider inner;
+    private ReversibleHMCProvider outer;
     protected final Parameter parameter;
 
     int dimA;
@@ -39,17 +39,17 @@ public class SplitHamiltonianMonteCarloOperator extends AbstractAdaptableOperato
     private double gradientCheckTolerance;
 
 
-    public SplitHamiltonianMonteCarloOperator(double weight, ReversibleHMCProvider reversibleHMCProviderA,
-                                              ReversibleHMCProvider reversibleHMCProviderB, Parameter parameter,
+    public SplitHamiltonianMonteCarloOperator(double weight, ReversibleHMCProvider inner,
+                                              ReversibleHMCProvider outer, Parameter parameter,
                                               double stepSize,
                                               double relativeScale, int nSteps, int innerSteps,
                                               int gradientCheckCount, double gradientCheckTolerance) {
 
         setWeight(weight);
-        this.reversibleHMCProviderA = reversibleHMCProviderA; //todo: better names. Now A = HZZ, B = HMC
-        this.reversibleHMCProviderB = reversibleHMCProviderB;
-        dimA = reversibleHMCProviderA.getInitialPosition().length;
-        dimB = reversibleHMCProviderB.getInitialPosition().length;
+        this.inner = inner; //todo: better names. Now A = HZZ, B = HMC
+        this.outer = outer;
+        dimA = inner.getInitialPosition().length;
+        dimB = outer.getInitialPosition().length;
         this.parameter = parameter;
 
         this.stepSize = stepSize;
@@ -144,36 +144,36 @@ public class SplitHamiltonianMonteCarloOperator extends AbstractAdaptableOperato
     }
 
     private boolean anyTransform() {
-        return (reversibleHMCProviderA.getParameterLogJacobian() == 0 && reversibleHMCProviderB.getParameterLogJacobian() == 0) ? false : true;
+        return (inner.getParameterLogJacobian() == 0 && outer.getParameterLogJacobian() == 0) ? false : true;
     }
 
     private double mergedUpdate() {
 
-        double[] positionAbuffer = reversibleHMCProviderA.getInitialPosition();
-        double[] positionBbuffer = reversibleHMCProviderB.getInitialPosition();
+        double[] positionAbuffer = inner.getInitialPosition();
+        double[] positionBbuffer = outer.getInitialPosition();
 
         WrappedVector positionA = new WrappedVector.Raw(positionAbuffer);
         WrappedVector positionB = new WrappedVector.Raw(positionBbuffer);
 
-        WrappedVector momentumA = reversibleHMCProviderA.drawMomentum();
-        WrappedVector momentumB = reversibleHMCProviderB.drawMomentum();
+        WrappedVector momentumA = inner.drawMomentum();
+        WrappedVector momentumB = outer.drawMomentum();
 
         final double prop =
-                reversibleHMCProviderA.getKineticEnergy(momentumA) + reversibleHMCProviderB.getKineticEnergy(momentumB) + reversibleHMCProviderA.getParameterLogJacobian() + reversibleHMCProviderB.getParameterLogJacobian();
+                inner.getKineticEnergy(momentumA) + outer.getKineticEnergy(momentumB) + inner.getParameterLogJacobian() + outer.getParameterLogJacobian();
 
         for (int i = 0; i < nSteps; i++) {
             for (int j = 0; j < innerSteps; j++) {
-                reversibleHMCProviderB.reversiblePositionMomentumUpdate(positionB, momentumB, 1, stepSize);
+                outer.reversiblePositionMomentumUpdate(positionB, momentumB, 1, stepSize);
             }
-            reversibleHMCProviderA.reversiblePositionMomentumUpdate(positionA, momentumA, 1,
+            inner.reversiblePositionMomentumUpdate(positionA, momentumA, 1,
                     relativeScale * stepSize);
             for (int j = 0; j < innerSteps; j++) {
-                reversibleHMCProviderB.reversiblePositionMomentumUpdate(positionB, momentumB, 1, stepSize);
+                outer.reversiblePositionMomentumUpdate(positionB, momentumB, 1, stepSize);
             }
         }
 
         final double res =
-                reversibleHMCProviderA.getKineticEnergy(momentumA) + reversibleHMCProviderB.getKineticEnergy(momentumB) + reversibleHMCProviderA.getParameterLogJacobian() + reversibleHMCProviderB.getParameterLogJacobian();
+                inner.getKineticEnergy(momentumA) + outer.getKineticEnergy(momentumB) + inner.getParameterLogJacobian() + outer.getParameterLogJacobian();
 
         return prop - res;
     }
@@ -233,15 +233,28 @@ public class SplitHamiltonianMonteCarloOperator extends AbstractAdaptableOperato
 
         //2:update them
         for (int i = 0; i < innerSteps; i++) {
-            reversibleHMCProviderB.reversiblePositionMomentumUpdate(positionB, momentumB, direction, time);
+            outer.reversiblePositionMomentumUpdate(positionB, momentumB, direction, time);
         }
-        reversibleHMCProviderA.reversiblePositionMomentumUpdate(positionA, momentumA, direction, relativeScale * time);
+        inner.reversiblePositionMomentumUpdate(positionA, momentumA, direction, relativeScale * time);
         for (int i = 0; i < innerSteps; i++) {
-            reversibleHMCProviderB.reversiblePositionMomentumUpdate(positionB, momentumB, direction, time);
+            outer.reversiblePositionMomentumUpdate(positionB, momentumB, direction, time);
         }
         //3:merge the position and momentum, update position and momentum
         updateMergedVector(positionA, positionB, position);
         updateMergedVector(momentumA, momentumB, momentum);
+
+        // TODO Avoid all these unncessary copies via
+
+//        WrappedVector positionA = new WrappedVector.View(position, 0, dimA);
+//        WrappedVector momentumA = new WrappedVector.View(momentum, 0, dimA);
+//
+//        WrappedVector positionB = new WrappedVector.View(position, dimA, dimB);
+//        WrappedVector momentumB = new WrappedVector.View(momentum, dimA, dimB);
+//
+//        outer.reversiblePositionMomentumUpdate(positionB, momentumB, direction, time);
+//        inner.reversiblePositionMomentumUpdate(positionA, momentumA, direction, relativeScale * time);
+//        outer.reversiblePositionMomentumUpdate(positionB, momentumB, direction, time);
+
     }
 
     public double[] jointTransformInverse(double[] argument) {
@@ -249,7 +262,7 @@ public class SplitHamiltonianMonteCarloOperator extends AbstractAdaptableOperato
         double[] transformedPositionB = new double[dimB];
 
         System.arraycopy(argument, dimA, transformedPositionB, 0, dimB);
-        double[] unTransformedPositionB = reversibleHMCProviderB.getTransform().inverse(transformedPositionB, 0, dimB);
+        double[] unTransformedPositionB = outer.getTransform().inverse(transformedPositionB, 0, dimB);
 
         System.arraycopy(argument, 0, jointUntransformedPosition, 0, dimA);
         System.arraycopy(unTransformedPositionB, 0, jointUntransformedPosition, dimA, dimB);
@@ -260,7 +273,7 @@ public class SplitHamiltonianMonteCarloOperator extends AbstractAdaptableOperato
     public double transformGetLogJacobian(double[] untransformedValue) {
         double[] untransformedPositionB = new double[dimB];
         System.arraycopy(untransformedValue, dimA, untransformedPositionB, 0, dimB);
-        return reversibleHMCProviderB.getTransform().getLogJacobian(untransformedPositionB, 0, dimB);
+        return outer.getTransform().getLogJacobian(untransformedPositionB, 0, dimB);
     }
 
     public double[] transformupdateGradientLogDensity(double[] analyticalGradientOriginal, Parameter parameter) {
@@ -274,7 +287,7 @@ public class SplitHamiltonianMonteCarloOperator extends AbstractAdaptableOperato
         System.arraycopy(parameter.getParameterValues(), dimA, parameterValueB, 0, dimB);
 
         double[] updatedGradientB =
-                reversibleHMCProviderB.getTransform().updateGradientLogDensity(analyticalGradientOriginalB,
+                outer.getTransform().updateGradientLogDensity(analyticalGradientOriginalB,
                         parameterValueB, 0, dimB);
         System.arraycopy(analyticalGradientOriginalA, 0, updatedGradientAB, 0, dimA);
         System.arraycopy(updatedGradientB, 0, updatedGradientAB, dimA, dimB);
@@ -285,14 +298,14 @@ public class SplitHamiltonianMonteCarloOperator extends AbstractAdaptableOperato
     public double[] getInitialPosition() {
 
         double[] jointPosition = new double[dimA + dimB];
-        System.arraycopy(reversibleHMCProviderA.getInitialPosition(), 0, jointPosition, 0, dimA);
-        System.arraycopy(reversibleHMCProviderB.getInitialPosition(), 0, jointPosition, dimA, dimB);
+        System.arraycopy(inner.getInitialPosition(), 0, jointPosition, 0, dimA);
+        System.arraycopy(outer.getInitialPosition(), 0, jointPosition, dimA, dimB);
         return jointPosition;
     }
 
     @Override
     public double getParameterLogJacobian() {
-        return reversibleHMCProviderA.getParameterLogJacobian() + reversibleHMCProviderB.getParameterLogJacobian();
+        return inner.getParameterLogJacobian() + outer.getParameterLogJacobian();
     }
 
     @Override
@@ -307,9 +320,9 @@ public class SplitHamiltonianMonteCarloOperator extends AbstractAdaptableOperato
 
     private double[] mergeGradient() {
         double[] jointPosition = new double[dimA + dimB];
-        System.arraycopy(reversibleHMCProviderA.getGradientProvider().getGradientLogDensity(), 0, jointPosition, 0,
+        System.arraycopy(inner.getGradientProvider().getGradientLogDensity(), 0, jointPosition, 0,
                 dimA);
-        System.arraycopy(reversibleHMCProviderB.getGradientProvider().getGradientLogDensity(), 0, jointPosition, dimA
+        System.arraycopy(outer.getGradientProvider().getGradientLogDensity(), 0, jointPosition, dimA
                 , dimB);
         return jointPosition;
     }
@@ -323,13 +336,13 @@ public class SplitHamiltonianMonteCarloOperator extends AbstractAdaptableOperato
         System.arraycopy(position, 0, bufferA, 0, dimA);
         System.arraycopy(position, dimA, bufferB, 0, dimB);
 
-        reversibleHMCProviderA.setParameter(bufferA);
-        reversibleHMCProviderB.setParameter(bufferB);
+        inner.setParameter(bufferA);
+        outer.setParameter(bufferB);
     }
 
     @Override
     public WrappedVector drawMomentum() {
-        return mergeWrappedVector(reversibleHMCProviderA.drawMomentum(), reversibleHMCProviderB.drawMomentum());
+        return mergeWrappedVector(inner.drawMomentum(), outer.drawMomentum());
     }
 
     @Override
@@ -339,13 +352,13 @@ public class SplitHamiltonianMonteCarloOperator extends AbstractAdaptableOperato
         //2:split the momentum
         splitWrappedVector(momentum, momentumAbuffer, momentumBbuffer);
         //todo: better solution. Now only part B has a prior.
-        return reversibleHMCProviderA.getJointProbability(new WrappedVector.Raw(momentumAbuffer)) + reversibleHMCProviderB.getJointProbability(new WrappedVector.Raw(momentumBbuffer)) - reversibleHMCProviderA.getLogLikelihood();
+        return inner.getJointProbability(new WrappedVector.Raw(momentumAbuffer)) + outer.getJointProbability(new WrappedVector.Raw(momentumBbuffer)) - inner.getLogLikelihood();
     }
 
     @Override
     public double getLogLikelihood() {
         //todo: better solution. Now only part B has a prior. A only has likelihood.
-        return reversibleHMCProviderA.getLogLikelihood();
+        return inner.getLogLikelihood();
     }
 
     @Override
@@ -358,8 +371,11 @@ public class SplitHamiltonianMonteCarloOperator extends AbstractAdaptableOperato
 
         ReadableVector momentumA = new WrappedVector.Raw(bufferA);
         ReadableVector momentumB = new WrappedVector.Raw(bufferB);
+        // TODO Use ReadableVector.View instead
+//        return inner.getKineticEnergy(new ReadableVector.View(momentum, 0, dimA)) +
+//                outer.getKineticEnergy(new ReadableVector.View(momentum, dimA, dimB));
 
-        return reversibleHMCProviderA.getKineticEnergy(momentumA) + reversibleHMCProviderB.getKineticEnergy(momentumB);
+        return inner.getKineticEnergy(momentumA) + outer.getKineticEnergy(momentumB);
     }
 
     @Override
@@ -367,10 +383,10 @@ public class SplitHamiltonianMonteCarloOperator extends AbstractAdaptableOperato
         return stepSize;
     } //todo: tuning.
 
-    private WrappedVector mergeWrappedVector(WrappedVector vectorA, WrappedVector vectorB) {
-        double[] buffer = new double[dimA + dimB];
-        System.arraycopy(vectorA.getBuffer(), 0, buffer, 0, dimA);
-        System.arraycopy(vectorB.getBuffer(), 0, buffer, dimA, dimB);
+    private WrappedVector mergeWrappedVector(WrappedVector lhs, WrappedVector rhs) {
+        double[] buffer = new double[lhs.getDim() + rhs.getDim()];
+        System.arraycopy(lhs.getBuffer(), 0, buffer, 0, lhs.getDim());
+        System.arraycopy(rhs.getBuffer(), 0, buffer, lhs.getDim(), rhs.getDim());
         return new WrappedVector.Raw(buffer);
     }
 

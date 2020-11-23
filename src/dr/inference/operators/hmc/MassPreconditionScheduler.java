e57b1f9353c3cc38f85bc28dea44dc7a09edbe47
xji3 <xji3@ncsu.edu>
2020-11-23 15:27:05 -0600

    a MassPreconditionScheduler interface

---
+++ src/dr/inference/operators/hmc/MassPreconditionScheduler.java
@@ -0,0 +1,115 @@
+/*
+ * MassPreconditionScheduler.java
+ *
+ * Copyright (c) 2002-2017 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package dr.inference.operators.hmc;
+
+import dr.inference.operators.AdaptableMCMCOperator;
+
+/**
+ * @author Marc A. Suchard
+ * @author Xiang Ji
+ */
+public interface MassPreconditionScheduler {
+
+    boolean shouldUpdatePreconditioning();
+
+    boolean shouldStoreSecant(double[] lastGradient, double[] lastPosition);
+
+    enum Type {
+
+        NONE("none") {
+            @Override
+            public MassPreconditionScheduler factory(HamiltonianMonteCarloOperator.Options options,
+                                                     AdaptableMCMCOperator operator) {
+                return new None();
+            }
+        },
+        DEFAULT("default") {
+            @Override
+            public MassPreconditionScheduler factory(HamiltonianMonteCarloOperator.Options options,
+                                                     AdaptableMCMCOperator operator) {
+                return null;
+            }
+        };
+
+        private final String name;
+
+        Type(String name) {
+            this.name = name;
+        }
+
+        public abstract MassPreconditionScheduler factory(HamiltonianMonteCarloOperator.Options options,
+                                                          AdaptableMCMCOperator operator);
+
+        public String getName() { return name; }
+
+        public static MassPreconditionScheduler.Type parseFromString(String text) {
+            for (MassPreconditionScheduler.Type type : MassPreconditionScheduler.Type.values()) {
+                if (type.name.toLowerCase().compareToIgnoreCase(text) == 0) {
+                    return type;
+                }
+            }
+            return MassPreconditionScheduler.Type.NONE;
+        }
+    }
+
+    class None implements MassPreconditionScheduler {
+
+        @Override
+        public boolean shouldUpdatePreconditioning() {
+            return false;
+        }
+
+        @Override
+        public boolean shouldStoreSecant(double[] lastGradient, double[] lastPosition) {
+            return false;
+        }
+    }
+
+    class Default implements MassPreconditionScheduler {
+
+        private HamiltonianMonteCarloOperator.Options options;
+        private AdaptableMCMCOperator operator;
+
+        Default(HamiltonianMonteCarloOperator.Options options,
+                AdaptableMCMCOperator operator) {
+            this.options = options;
+            this.operator = operator;
+        }
+
+        @Override
+        public boolean shouldUpdatePreconditioning() {
+            return ((options.preconditioningUpdateFrequency > 0)
+                    && (((operator.getCount() % options.preconditioningUpdateFrequency == 0)
+                    && (operator.getCount() > options.preconditioningDelay))));
+        }
+
+        @Override
+        public boolean shouldStoreSecant(double[] lastGradient, double[] lastPosition) {
+            return lastGradient != null && lastPosition != null;
+        }
+    }
+
+}

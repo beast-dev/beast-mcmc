7b212b0064c645671f15216870169ad3536cd32b
athos00 <alexanderfisher@ucla.edu>
2020-12-02 14:17:32 -0500

    fix arbitrary descendents from root node

--- src/dr/inference/model/MaskFromTree.java
+++ src/dr/inference/model/MaskFromTree.java
@@ -6,35 +6,44 @@ import dr.evomodel.tree.TreeModel;
 import dr.evomodel.tree.TreeParameterModel;
 import dr.xml.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Alexander Fisher
  */
 
 
 /**
- * This class masks a branch-specific parameter based on a user-specified descendant taxon.
+ * This class provides a mask for branch-specific parameters based on a user-specified descendant taxon.
+ * It works by automagically masking the 'root-branch' that is ancestor to the specified taxon as well
+ * as an arbitrary number (maskLength) of daughter branches (default = 0)
  */
 
 public class MaskFromTree extends Parameter.Default implements ModelListener {
 
     public static final String MASK_FROM_TREE = "maskFromTree";
+    public static final String MASK_LENGTH = "maskLength";
     private TreeModel tree;
     private Taxon taxon;
     private TreeParameterModel branchRates;
-//    private boolean ancestralMaskBranchKnown = false;
+    private int maskLength; //maskLength determines number of 'daughter' branches to mask as well
 
-    public MaskFromTree(TreeModel tree, Taxon referenceTaxon, int dim) {
+    public MaskFromTree(TreeModel tree, Taxon referenceTaxon, int dim, int maskLength) {
         super(dim);
         this.tree = tree;
         this.taxon = referenceTaxon;
         this.branchRates = new TreeParameterModel(tree, this, false); //default includeRoot = false
         tree.addModelListener(this);
         tree.addModelRestoreListener(this);
+        this.maskLength = maskLength;
+
         updateMask();
     }
 
     void updateMask() {
         // todo: make sure this sets the old 0.0 to 1.0 smarter
+        // todo: remove code duplication from maskLength > 1
         for (int i = 0; i < this.getDimension(); i++) {
             setParameterValueQuietly(i, 1.0);
         }
@@ -45,13 +54,25 @@ public class MaskFromTree extends Parameter.Default implements ModelListener {
         while (tree.getParent(node) != root) {
             node = tree.getParent(node);
         }
-
-//        int maskIndex = node.getNumber();
         int maskIndex = branchRates.getParameterIndexFromNodeNumber(node.getNumber());
-//        if(maskIndex==82){
-//            System.out.println("stop here");
-//        }
         this.setParameterValue(maskIndex, 0.0);
+
+        List<NodeRef> nodeList = new ArrayList<>();
+        // only happens if maskLength > 0 :
+        for (int i = 0; i < maskLength; i++) {
+
+            if (!tree.isExternal(node)) {
+                node = tree.getChild(node, 0);
+                nodeList.add(node);
+            }
+
+//            maskIndex = branchRates.getParameterIndexFromNodeNumber(node.getNumber())
+        }
+        for (int i = 0; i < nodeList.size(); i++) {
+            maskIndex = branchRates.getParameterIndexFromNodeNumber(nodeList.get(i).getNumber());
+            this.setParameterValue(maskIndex, 0.0);
+        }
+
     }
 
     @Override
@@ -113,9 +134,11 @@ public class MaskFromTree extends Parameter.Default implements ModelListener {
 
             Taxon referenceTaxon = (Taxon) xo.getChild(Taxon.class);
 
+            int maskLength = xo.getAttribute(MASK_LENGTH, 0);
+
             int numBranches = tree.getNodeCount() - 1;
 
-            MaskFromTree maskFromTree = new MaskFromTree(tree, referenceTaxon, numBranches);
+            MaskFromTree maskFromTree = new MaskFromTree(tree, referenceTaxon, numBranches, maskLength);
             return maskFromTree;
         }
 
@@ -130,6 +153,7 @@ public class MaskFromTree extends Parameter.Default implements ModelListener {
         private XMLSyntaxRule[] rules = new XMLSyntaxRule[]{
                 new ElementRule(TreeModel.class),
                 new ElementRule(Taxon.class),
+                AttributeRule.newStringRule(MASK_LENGTH, true),
         };
 
         public String getParserDescription() {

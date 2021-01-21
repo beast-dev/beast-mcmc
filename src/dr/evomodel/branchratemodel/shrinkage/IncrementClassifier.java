84bf94fda0eea258d3e9468d4c14fd0592f67bb2
athos00 <alexanderfisher@ucla.edu>
2021-01-21 15:49:08 -0500

    basic classifier + some checks

--- src/dr/evomodel/branchratemodel/shrinkage/IncrementClassifier.java
+++ src/dr/evomodel/branchratemodel/shrinkage/IncrementClassifier.java
@@ -4,6 +4,7 @@ package dr.evomodel.branchratemodel.shrinkage;
 import dr.evolution.tree.TreeTrait;
 import dr.evolution.tree.TreeTraitProvider;
 import dr.evomodel.branchratemodel.AutoCorrelatedBranchRatesDistribution;
+import dr.inference.distribution.ParametricMultivariateDistributionModel;
 import dr.inference.model.Parameter;
 import dr.xml.*;
 
@@ -11,28 +12,54 @@ import dr.xml.*;
  * @author Alexander Fisher
  */
 public class IncrementClassifier implements TreeTraitProvider {
-
+// todo: find magic default epsilon from prior simulation
     public static final String PARAMETER_CLASSIFIER = "parameterClassifier";
     public static final String EPSILON = "epsilon";
     public static final String TARGET_PROBABILITY = "targetProbability";
 
     private AutoCorrelatedBranchRatesDistribution brm;
     private double epsilon;
+    private boolean epsilonKnown = false;
     private double targetProb;
+    private int dim;
+    private ParametricMultivariateDistributionModel distribution;
+
+    private double[] classified;
 
     public IncrementClassifier(AutoCorrelatedBranchRatesDistribution brm, double epsilon, double targetProb) {
         this.brm = brm;
-        if (epsilon != 0.0){
+        this.distribution = brm.getPrior();
+
+        if (epsilon != 0.0) {
             this.epsilon = epsilon;
-        }
-        else if (targetProb != 0.0){
+            epsilonKnown = true;
+        } else if (targetProb != 0.0) {
             this.targetProb = targetProb;
             this.epsilon = findEpsilonFromTargetProb(targetProb);
         }
+
+        this.dim = brm.getDimension();
+        this.classified = new double[dim];
+        classify();
     }
 
-    private double findEpsilonFromTargetProb(double targetProb){
-        return 0.0;
+    private double findEpsilonFromTargetProb(double targetProb) {
+        // finds eps such that p(abs(increment)) < epsilon = targetProb
+        throw new RuntimeException("Target probabiltiy not yet implemented.");
+    }
+
+    private void classify() {
+        double increment;
+        if(!epsilonKnown){
+            findEpsilonFromTargetProb(targetProb);
+        }
+        for (int i = 0; i < dim; i++) {
+            increment = brm.getIncrement(i);
+            classified[i] = 1; // is an increment
+            if (increment < epsilon) {
+                classified[i] = 0; // is not an increment
+            }
+        }
     }
 
     @Override
@@ -61,12 +88,19 @@ public class IncrementClassifier implements TreeTraitProvider {
 //            Parameter parameter = (Parameter) xo.getChild(Parameter.class);
             AutoCorrelatedBranchRatesDistribution brm = (AutoCorrelatedBranchRatesDistribution) xo.getChild(AutoCorrelatedBranchRatesDistribution.class);
 
-            if (xo.hasAttribute(EPSILON) && xo.hasAttribute(TARGET_PROBABILITY)){
+            if (xo.hasAttribute(EPSILON) && xo.hasAttribute(TARGET_PROBABILITY)) {
                 throw new XMLParseException("Cannot set both epsilon and target probability.");
             }
 
             double epsilon = xo.getAttribute(EPSILON, 0);
             double targetProbability = xo.getAttribute(TARGET_PROBABILITY, 0);
+
+            if (epsilon < 0) {
+                throw new XMLParseException("epsilon must be positive.");
+            }
+            else if (targetProbability < 0) {
+                throw new XMLParseException("target probability must be positive.");
+            }
 
             IncrementClassifier incrementClassifier = new IncrementClassifier(brm, epsilon, targetProbability);
             return incrementClassifier;

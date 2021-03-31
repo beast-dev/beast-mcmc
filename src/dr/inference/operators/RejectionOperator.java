ddcc4fbf23037b758b8e0ec15f2c3c3944258c06
Gabe Hassler <gabriel.w.hassler@gmail.com>
2021-03-31 10:56:32 -0700

    new rejection condition

--- src/dr/inference/operators/RejectionOperator.java
+++ src/dr/inference/operators/RejectionOperator.java
@@ -28,17 +28,17 @@ public class RejectionOperator extends SimpleMCMCOperator {
             }
         },
 
-        DescendingAbsoluteValueSpaced("descendingAbsoluteValueSpaced") {
-            @Override
-            public boolean satisfiesCondition(double[] values) {
-                for (int i = 1; i < values.length; i++) {
-                    if (0.9 * Math.abs(values[i - 1]) < Math.abs(values[i])) {
-                        return false;
-                    }
-                }
-                return true;
-            }
-        },
+//        DescendingAbsoluteValueSpaced("descendingAbsoluteValueSpaced") {
+//            @Override
+//            public boolean satisfiesCondition(double[] values) {
+//                for (int i = 1; i < values.length; i++) {
+//                    if (0.9 * Math.abs(values[i - 1]) < Math.abs(values[i])) {
+//                        return false;
+//                    }
+//                }
+//                return true;
+//            }
+//        },
 
         AlternatingSigns("descendingAlternatingSigns") {
             @Override
@@ -66,6 +66,61 @@ public class RejectionOperator extends SimpleMCMCOperator {
 
         @Override
         public abstract boolean satisfiesCondition(double[] values);
+    }
+
+    public static class DescendingAndSpacedCondition implements AcceptCondition {
+        private final double spacing;
+
+        DescendingAndSpacedCondition(double spacing) {
+            this.spacing = spacing;
+        }
+
+        @Override
+        public boolean satisfiesCondition(double[] values) {
+            for (int i = 1; i < values.length; i++) {
+                if (spacing * Math.abs(values[i - 1]) < Math.abs(values[i])) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static final String DESCENDING_AND_SPACED = "descendingAndSpaced";
+        private static final String SPACING = "spacing";
+
+        public static AbstractXMLObjectParser PARSER = new AbstractXMLObjectParser() {
+            @Override
+            public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+                double spacing = xo.getDoubleAttribute(SPACING);
+                if (spacing < 0.0 || spacing > 1.0) {
+                    throw new XMLParseException("Attribute '" + SPACING + "' must be between 0 and 1.");
+                }
+
+                return new DescendingAndSpacedCondition(spacing);
+            }
+
+            @Override
+            public XMLSyntaxRule[] getSyntaxRules() {
+                return new XMLSyntaxRule[]{
+                        AttributeRule.newDoubleRule(SPACING)
+                };
+            }
+
+            @Override
+            public String getParserDescription() {
+                return "Condition requiring parameter to have descending absolute values with some minimum spacing.";
+            }
+
+            @Override
+            public Class getReturnType() {
+                return DescendingAndSpacedCondition.class;
+            }
+
+            @Override
+            public String getParserName() {
+                return DESCENDING_AND_SPACED;
+            }
+        };
     }
 
     private final RejectionProvider gibbsOp;

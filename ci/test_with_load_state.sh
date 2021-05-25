6a65a2aef5ae612c799947183f6098b1a686cd39
Gabe Hassler <gabriel.w.hassler@gmail.com>
2021-05-25 16:43:58 -0700

    fixing path issues

--- ci/test_with_load_state.sh
+++ ci/test_with_load_state.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+cd ci
+
 for file in TestXMLwithLoadState/*\.xml
 do
   filename=$(basename $file .xml)
-  if java -Djava.library.path=${BEAGLE_LIB} -jar build/dist/beast.jar -fail_threads -seed 666 -load_state $filename.chkpt -overwrite $file; then
+  if java -Djava.library.path=${BEAGLE_LIB} -jar ../build/dist/beast.jar -fail_threads -seed 666 -load_state $filename.chkpt -overwrite $file; then
     echo $file passed
   else
     echo $file failed; exit -1

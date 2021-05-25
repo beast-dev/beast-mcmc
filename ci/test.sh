4a3cf79d4fb4811b153da4743cdec20c89315f40
Gabe Hassler <gabriel.w.hassler@gmail.com>
2021-05-25 16:39:15 -0700

    new dirs and permissions

--- ci/test.sh
+++ ci/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for file in TestXML/*\.xml
 do
-  if java -Djava.library.path=${BEAGLE_LIB} -jar ../build/dist/beast.jar -fail_threads -seed 666 -overwrite $file; then
+  if java -Djava.library.path=${BEAGLE_LIB} -jar build/dist/beast.jar -fail_threads -seed 666 -overwrite $file; then
     echo $file passed
   else
     echo $file failed; exit -1
--- ci/test_with_load_state.sh
+++ ci/test_with_load_state.sh
@@ -2,7 +2,7 @@
 for file in TestXMLwithLoadState/*\.xml
 do
   filename=$(basename $file .xml)
-  if java -Djava.library.path=${BEAGLE_LIB} -jar ../build/dist/beast.jar -fail_threads -seed 666 -load_state $filename.chkpt -overwrite $file; then
+  if java -Djava.library.path=${BEAGLE_LIB} -jar build/dist/beast.jar -fail_threads -seed 666 -load_state $filename.chkpt -overwrite $file; then
     echo $file passed
   else
     echo $file failed; exit -1

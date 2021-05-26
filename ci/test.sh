f186128fffac3b5f591a47a33185bd099b81d0ce
Gabe Hassler <gabriel.w.hassler@gmail.com>
2021-05-25 18:03:15 -0700

    fix 3

--- ci/test.sh
+++ ci/test.sh
@@ -3,7 +3,7 @@ cd ci
 
 for file in TestXML/*\.xml
 do
-  if java -Djava.library.path=../${BEAGLE_LIB} -jar ../build/dist/beast.jar -fail_threads -seed 666 -overwrite $file; then
+  if java -Djava.library.path=${BEAGLE_LIB} -jar ../build/dist/beast.jar -fail_threads -seed 666 -overwrite $file; then
     echo $file passed
   else
     echo $file failed; exit -1

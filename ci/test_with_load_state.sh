ec291eadd53793499c5df985fadbe2e31e22d4a8
Gabe Hassler <gabriel.w.hassler@gmail.com>
2021-05-25 18:19:31 -0700

    finishing touches

--- ci/test_with_load_state.sh
+++ ci/test_with_load_state.sh
@@ -5,8 +5,8 @@ java -jar -Djava.library.path=${BEAGLE_LIB} build/dist/beast.jar -beagle_info
 
 for file in ci/TestXMLwithLoadState/*\.xml
 do
-  filename=$(basename $file .xml)
-  if java -Djava.library.path=${BEAGLE_LIB} -jar build/dist/beast.jar -fail_threads -seed 666 -load_state $filename.chkpt -overwrite $file; then
+  checkpoint=ci/TestXMLWithLoadState/$(basename $file .xml).chkpt
+  if java -Djava.library.path=${BEAGLE_LIB} -jar build/dist/beast.jar -fail_threads -seed 666 -load_state $checkpoint -overwrite $file; then
     echo $file passed
   else
     echo $file failed; exit -1

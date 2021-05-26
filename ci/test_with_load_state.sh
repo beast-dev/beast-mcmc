006ca577957f61d2912e2f05a238559356c5a35d
Gabe Hassler <gabriel.w.hassler@gmail.com>
2021-05-25 18:11:29 -0700

    test 5

--- ci/test_with_load_state.sh
+++ ci/test_with_load_state.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-cd ci
 
-BEAGLE_FILES=${GITHUB_WORKSPACE}/${BEAGLE_LIB}
-echo $BEAGLE_FILES
-ls $BEAGLE_FILES
-
-java -jar -Djava.library.path=${BEAGLE_FILES} ../build/dist/beast.jar -beagle_info
+java -jar -Djava.library.path=${BEAGLE_LIB} build/dist/beast.jar -beagle_info
 
 
-for file in TestXMLwithLoadState/*\.xml
+for file in ci/TestXMLwithLoadState/*\.xml
 do
   filename=$(basename $file .xml)
-  if java -Djava.library.path=$BEAGLE_FILES -jar ../build/dist/beast.jar -fail_threads -seed 666 -load_state $filename.chkpt -overwrite $file; then
+  if java -Djava.library.path=$BEAGLE_FILES -jar build/dist/beast.jar -fail_threads -seed 666 -load_state $filename.chkpt -overwrite $file; then
     echo $file passed
   else
     echo $file failed; exit -1

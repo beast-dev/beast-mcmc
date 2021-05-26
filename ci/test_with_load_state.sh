c06a3ddc40f8d7c31d917443afce3398b76b6a42
Gabe Hassler <gabriel.w.hassler@gmail.com>
2021-05-25 17:42:04 -0700

    hoepfully final change

--- ci/test_with_load_state.sh
+++ ci/test_with_load_state.sh
@@ -4,7 +4,7 @@ cd ci
 for file in TestXMLwithLoadState/*\.xml
 do
   filename=$(basename $file .xml)
-  if java -Djava.library.path=${BEAGLE_LIB} -jar ../build/dist/beast.jar -fail_threads -seed 666 -load_state $filename.chkpt -overwrite $file; then
+  if java -Djava.library.path=${GITHUB_WORKSPACE}/${BEAGLE_LIB} -jar ../build/dist/beast.jar -fail_threads -seed 666 -load_state $filename.chkpt -overwrite $file; then
     echo $file passed
   else
     echo $file failed; exit -1

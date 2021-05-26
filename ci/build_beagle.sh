43917042383bfa9d5e32cffd2f17020aae1927b3
Gabe Hassler <gabriel.w.hassler@gmail.com>
2021-05-25 17:05:45 -0700

    trying to find BEAGLE

--- ci/build_beagle.sh
+++ ci/build_beagle.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-git clone -b $BEAGLE_BRANCH --depth 1 https://github.com/beagle-dev/beagle-lib.git $BEAGLE_DIR
-cd $BEAGLE_DIR
+git clone -b ${BEAGLE_BRANCH} --depth 1 https://github.com/beagle-dev/beagle-lib.git ${BEAGLE_DIR}
+cd ${BEAGLE_DIR}
 mkdir build
 cd build
-echo $PWD
 cmake -DBUILD_CUDA=OFF -DBUILD_OPENCL=OFF ..
-make DESTDIR=$BEAGLE_DIR install
+make DESTDIR=${BEAGLE_DIR} install
 export export LD_LIBRARY_PATH=${BEAGLE_LIB}
+cd ${BEAGLE_LIB}
+ls

ec189fdaa424bf80f477067fd6bf6526697e4e25
Andy Magee <andyfmagee@gmail.com>
2021-10-28 13:23:18 -0700

    AA mixture bug

--- src/dr/evomodel/substmodel/AminoAcidMixture.java
+++ src/dr/evomodel/substmodel/AminoAcidMixture.java
@@ -16,7 +16,7 @@ public class AminoAcidMixture extends DesignMatrix {
         Parameter[] p = new Parameter[rateMatrix.size()];
 
         for (int i = 0; i < p.length; ++i) {
-
+            System.err.println("AminoAcidMixture::getParameters()");
             EmpiricalAminoAcidModel model = rateMatrix.get(i);
             // this is a flat representation of the lower triangular order
             double[] rates = model.getEmpiricalRateMatrix().getEmpiricalRates();
@@ -24,12 +24,10 @@ public class AminoAcidMixture extends DesignMatrix {
             // We need upper and lower triangular rates, both logged
             double[] ratesUpperLower = new double[2*rates.length];
             for ( int j = 0; j < rates.length; j++ ) {
-                ratesUpperLower[i] = ratesUpperLower[rates.length + 1] = Math.log(rates[i]);
+                ratesUpperLower[j] = ratesUpperLower[rates.length + j] = Math.log(rates[j]);
             }
-
             p[i] = new Parameter.Default(ratesUpperLower);
 
-            // TODO
         }
         return p;
     }

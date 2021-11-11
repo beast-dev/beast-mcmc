539d1923ae4f231edae74cfcd991707a04d2409f
Marc Suchard <msuchard@ucla.edu>
2021-11-11 08:38:27 -0800

    add option to jitter a subset of taxa

--- src/dr/evomodelxml/treelikelihood/TreeTraitParserUtilities.java
+++ src/dr/evomodelxml/treelikelihood/TreeTraitParserUtilities.java
@@ -26,6 +26,7 @@
 package dr.evomodelxml.treelikelihood;
 
 import dr.evolution.tree.Tree;
+import dr.evolution.util.TaxonList;
 import dr.evomodel.continuous.StandardizeTraits;
 import dr.inference.model.*;
 import dr.math.MathUtils;
@@ -88,7 +89,7 @@ public class TreeTraitParserUtilities {
                 AttributeRule.newDoubleArrayRule(WINDOW),
                 AttributeRule.newBooleanRule(DUPLICATES, true),
                 new ElementRule(Parameter.class),
-
+                new ElementRule(TaxonList.class, true),
         }, optional);
     }
 
@@ -97,7 +98,8 @@ public class TreeTraitParserUtilities {
         Parameter traits = (Parameter) cxo.getChild(Parameter.class);
         double[] window = cxo.getDoubleArrayAttribute(TreeTraitParserUtilities.WINDOW); // Must be included, no default value
         boolean duplicates = cxo.getAttribute(TreeTraitParserUtilities.DUPLICATES, true); // default = true
-        jitter(traits, length, missingIndices, window, duplicates, true);
+        TaxonList taxonList = (TaxonList) cxo.getChild(TaxonList.class);
+        jitter(traits, length, missingIndices, window, taxonList, duplicates, true);
     }
 
     public void randomize(XMLObject xo) throws XMLParseException {
@@ -175,7 +177,9 @@ public class TreeTraitParserUtilities {
         return false;
     }
 
-    public void jitter(Parameter trait, int dim, List<Integer> missingIndices, double[] window, boolean duplicates, boolean verbose) {
+    public void jitter(Parameter trait, int dim, List<Integer> missingIndices, double[] window,
+                       TaxonList taxonList,
+                       boolean duplicates, boolean verbose) {
         int numTraits = trait.getDimension() / dim;
         boolean[] update = new boolean[numTraits];
         if (!duplicates) {
@@ -196,6 +200,27 @@ public class TreeTraitParserUtilities {
                     update[traitArray[i - 1].getIndex()] = true;
                     update[traitArray[i].getIndex()] = true;
                 }
+            }
+        }
+        if (taxonList != null) {
+
+            if (!(trait instanceof CompoundParameter)) {
+                throw new IllegalArgumentException("Currently unable to match taxon names with a FastMatrixParamter");
+            }
+
+            Set<String> includedTaxonNames = new HashSet<>();
+            for (int i = 0; i < taxonList.getTaxonCount(); ++i) {
+                includedTaxonNames.add(taxonList.getTaxonId(i));
+            }
+
+            CompoundParameter cParameter = (CompoundParameter) trait;
+            int offset = 0;
+            for (int i = 0; i < cParameter.getParameterCount(); i += dim) {
+                String name = cParameter.getParameter(i).getParameterName();
+                if (!includedTaxonNames.contains(name)) {
+                    update[offset] = false;
+                }
+                ++offset;
             }
         }
         for (int i = 0; i < numTraits; i++) {

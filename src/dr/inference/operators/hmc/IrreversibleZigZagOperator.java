345bf21c76a91a62eba611ad5483c495920c6133
xji3 <xji3@ncsu.edu>
2022-01-18 10:07:46 -0600

    fix merge conflict

--- src/dr/inference/operators/hmc/IrreversibleZigZagOperator.java
+++ src/dr/inference/operators/hmc/IrreversibleZigZagOperator.java
@@ -34,6 +34,8 @@ import dr.math.matrixAlgebra.ReadableVector;
 import dr.math.matrixAlgebra.WrappedVector;
 import dr.xml.Reportable;
 
+import java.util.Arrays;
+
 /**
  * @author Zhenyu Zhang
  * @author Marc A. Suchard
@@ -119,9 +121,9 @@ public class IrreversibleZigZagOperator extends AbstractZigZagOperator implement
     }
 
     private MinimumTravelInformation testNative(WrappedVector position,
-                            WrappedVector velocity,
-                            WrappedVector action,
-                            WrappedVector gradient) {
+                                                WrappedVector velocity,
+                                                WrappedVector action,
+                                                WrappedVector gradient) {
 
         if (TIMING) {
             timer.startTimer("getNextC++");
@@ -162,28 +164,223 @@ public class IrreversibleZigZagOperator extends AbstractZigZagOperator implement
         double minimumTime = Double.POSITIVE_INFINITY;
         int index = -1;
         Type type = Type.NONE;
+        double gradientTime;
 
-        for (int i = begin; i < end; ++i) {
+        if (NEW_WAY) {
 
-            double boundaryTime = findBoundaryTime(i, position[i], velocity[i]);
+            double[] roots = getRoots(action, gradient);
+            double[] rootsSorted = roots.clone();
+            Arrays.sort(rootsSorted);
 
-            if (boundaryTime < minimumTime) {
-                minimumTime = boundaryTime;
-                index = i;
+            double minBoundaryTime = Double.POSITIVE_INFINITY;
+            gradientTime = getSwitchTimeByMergedProcesses(action, gradient, velocity, roots, rootsSorted);
+            int gradientIndex = getEventDimension(velocity, gradient, action, gradientTime);
+
+            for (int i = 0; i < end; i++) {
+
+                double boundaryTime = findBoundaryTime(i, position[i], velocity[i]);
+
+                if (boundaryTime < minBoundaryTime) {
+                    minBoundaryTime = boundaryTime;
+                    index = i;
+
+                }
+            }
+
+            if (gradientTime < minBoundaryTime) {
+                index = gradientIndex;
+                minimumTime = gradientTime;
+                type = Type.GRADIENT;
+            } else {
+                minimumTime = minBoundaryTime;
                 type = Type.BOUNDARY;
             }
 
-            double T = MathUtils.nextExponential(1);
-            double gradientTime = getSwitchTime(-velocity[i] * gradient[i], velocity[i] * action[i], T);
+        } else {
+            for (int i = begin; i < end; ++i) {
 
-            if (gradientTime < minimumTime) {
-                minimumTime = gradientTime;
+                double boundaryTime = findBoundaryTime(i, position[i], velocity[i]);
+
+                if (boundaryTime < minimumTime) {
+                    minimumTime = boundaryTime;
+                    index = i;
+                    type = Type.BOUNDARY;
+                }
+
+
+                double T = MathUtils.nextExponential(1);
+                gradientTime = getSwitchTime(-velocity[i] * gradient[i], velocity[i] * action[i], T);
+
+
+                if (gradientTime < minimumTime) {
+                    minimumTime = gradientTime;
+                    index = i;
+                    type = Type.GRADIENT;
+                }
+            }
+        }
+        return new MinimumTravelInformation(minimumTime, index, type);
+    }
+
+
+    private double[] getRoots(double[] action, double[] gradient) {
+        // for the linear piece wise line: y = bx + a, where a = -velocity * gradient, b = velocity * action
+        // root = - a / b = gradient / action
+        double[] roots = new double[action.length];
+        double root;
+        for (int i = 0; i < action.length; i++) {
+            root = gradient[i] / action[i];
+            roots[i] = root >= 0 ? root : 0;
+        }
+        return roots;
+    }
+
+    private double getSwitchTimeByMergedProcesses(double[] action, double[] gradient, double[] velocity, double[] roots, double[] rootsSorted) {
+
+        // for the linear piece wise line: y = bx + a, where a = -velocity * gradient (as gradient is negated), b = velocity * action
+        // root = - a / b = gradient / action
+        double T = MathUtils.nextExponential(1);
+        double minFirstEvent = -1;
+        double cumulativeS = 0;
+        PiecewiseLinearEndpoints endPoints;
+
+        if (rootsSorted[rootsSorted.length - 1] == 0) {
+
+            endPoints = getEndpointInfo(0, 0, velocity, gradient, action, roots);
+            minFirstEvent = integrateLinearFunctionToArea(endPoints, T, false);
+        } else {
+
+            int j = 1;
+            double c0;
+            double c1;
+
+            double trapezoidArea;
+            double residualArea;
+
+            c0 = rootsSorted[0];
+
+            while (j < rootsSorted.length) {
+                if (rootsSorted[j] > 0) {
+                    c1 = rootsSorted[j];
+                    endPoints = getEndpointInfo(c0, c1, velocity, gradient, action, roots);
+                    trapezoidArea = getTrapezoidArea(endPoints);
+                    cumulativeS += trapezoidArea;
+                    if (cumulativeS > T) {
+                        residualArea = T - (cumulativeS - trapezoidArea);
+                        minFirstEvent = integrateLinearFunctionToArea(endPoints, residualArea, true);
+                        break;
+                    } else if (j == rootsSorted.length - 1) {
+                        residualArea = T - cumulativeS;
+                        minFirstEvent = integrateLinearFunctionToArea(endPoints, residualArea, false);
+                        break;
+                    } else {
+                        c0 = c1;
+                        j++;
+                    }
+                } else {
+                    j++;
+                }
+            }
+
+        }
+
+        return minFirstEvent;
+    }
+
+    private double integrateLinearFunctionToArea(PiecewiseLinearEndpoints f, double residual, boolean fromC0) {
+        double slope;
+        double intercept;
+
+        if (fromC0) {
+            slope = f.slope0;
+            intercept = f.f0 - slope * f.c0;
+            return onlyPositiveRoot(slope * 0.5, intercept, -(slope * 0.5 * f.c0 * f.c0 + intercept * f.c0 + residual));
+        } else {
+            slope = f.slope1;
+            intercept = f.f1 - slope * f.c1;
+            return onlyPositiveRoot(slope * 0.5, intercept, -(slope * 0.5 * f.c1 * f.c1 + intercept * f.c1 + residual));
+        }
+    }
+
+    private double onlyPositiveRoot(double a, double b, double c) {
+        return (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    }
+
+    private PiecewiseLinearEndpoints getEndpointInfo(double c0, double c1, double[] velocity, double[] gradient,
+                                                     double[] action,
+                                                     double[] roots) {
+
+        double[] c0coeff = new double[2];
+        double[] c1coeff = new double[2];
+
+        for (int i = 0; i < roots.length; i++) {
+
+            accumulateCoef(c0, velocity, gradient, action, roots, c0coeff, i);
+            accumulateCoef(c1, velocity, gradient, action, roots, c1coeff, i);
+        }
+
+        return new PiecewiseLinearEndpoints(c0, c1, c0coeff[0] * c0 + c0coeff[1], c1coeff[0] * c1 + c1coeff[1],
+                c0coeff[0], c1coeff[0]);
+    }
+
+    private void accumulateCoef(double c, double[] velocity, double[] gradient, double[] action, double[] roots,
+                                double[] coef, int i) {
+        if ((roots[i] >= c && velocity[i] * action[i] <= 0) || (roots[i] <= c && velocity[i] * action[i] >= 0)) {
+            coef[0] += velocity[i] * action[i];
+            coef[1] += -velocity[i] * gradient[i];
+        }
+    }
+
+    private double getTrapezoidArea(PiecewiseLinearEndpoints f) {
+        return (f.f0 + f.f1) * (f.c1 - f.c0) / 2.0;
+    }
+
+    private int getEventDimension(double[] velocity, double[] gradient, double[] action, double eventTime) {
+
+        double[] proportions = new double[velocity.length];
+        double rateAtEvent;
+        double rateSum = 0.0;
+
+        for (int i = 0; i < velocity.length; i++) {
+
+            rateAtEvent = eventTime * velocity[i] * action[i] - velocity[i] * gradient[i];
+            proportions[i] = rateAtEvent > 0.0 ? rateAtEvent : 0.0;
+            rateSum += proportions[i];
+        }
+
+        double u = MathUtils.nextDouble();
+        double t = 0.0;
+        int index = -1;
+
+        for (int i = 0; i < proportions.length; i++) {
+            t += proportions[i] / rateSum;
+            if (u <= t) {
                 index = i;
-                type = Type.GRADIENT;
+                break;
             }
         }
 
-        return new MinimumTravelInformation(minimumTime, index, type);
+        return index;
+    }
+
+
+    private class PiecewiseLinearEndpoints {
+
+        final double c0;
+        final double c1;
+        final double f0;
+        final double f1;
+        final double slope0;
+        final double slope1;
+
+        private PiecewiseLinearEndpoints(double c0, double c1, double f0, double f1, double slope0, double slope1) {
+            this.c0 = c0;
+            this.c1 = c1;
+            this.f0 = f0;
+            this.f1 = f1;
+            this.slope0 = slope0;
+            this.slope1 = slope1;
+        }
     }
 
     private double getSwitchTime(double a, double b, double u) {
@@ -282,5 +479,6 @@ public class IrreversibleZigZagOperator extends AbstractZigZagOperator implement
         return "Irreversible zig-zag operator";
     }
 
-    static final boolean CPP_NEXT_BOUNCE = true;
+    private static final boolean CPP_NEXT_BOUNCE = false;
+    private static final boolean NEW_WAY = true;
 }

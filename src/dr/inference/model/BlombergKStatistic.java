4cbe1348f42ce77c352197d1ceb598bd493cd4e8
Gabe Hassler <gabriel.w.hassler@gmail.com>
2022-06-14 18:55:09 -0700

    untested Blomberg's K

---
+++ src/dr/inference/model/BlombergKStatistic.java
@@ -0,0 +1,155 @@
+package dr.inference.model;
+
+import dr.evolution.tree.TreeTrait;
+import dr.evomodel.tree.TreeModel;
+import dr.evomodel.treedatalikelihood.TreeDataLikelihood;
+import dr.evomodel.treedatalikelihood.continuous.ContinuousDataLikelihoodDelegate;
+import dr.evomodel.treedatalikelihood.continuous.MultivariateTraitDebugUtilities;
+import dr.evomodel.treedatalikelihood.continuous.cdi.ContinuousDiffusionIntegrator;
+import dr.evomodel.treedatalikelihood.continuous.cdi.PrecisionType;
+import dr.math.matrixAlgebra.*;
+
+import java.util.Arrays;
+
+
+public class BlombergKStatistic extends Statistic.Abstract implements ModelListener {
+    private final TreeDataLikelihood traitLikelihood;
+    private final TreeModel tree;
+    private final TreeTrait treeTrait;
+    private boolean needToUpdateTree = true;
+    private final int traitDim;
+    private Matrix Linv;
+    private final int treeDim;
+    private double mseExpected;
+    private final ContinuousDiffusionIntegrator integrator;
+    private final ContinuousDataLikelihoodDelegate delegate;
+    private final double[] k;
+
+    public BlombergKStatistic(TreeDataLikelihood traitLikelihood, String traitName) {
+        this.traitLikelihood = traitLikelihood;
+        this.tree = (TreeModel) traitLikelihood.getTree();
+        tree.addModelListener(this);
+
+        this.treeTrait = traitLikelihood.getTreeTrait(traitName);
+        this.traitDim = traitLikelihood.getDataLikelihoodDelegate().getTraitDim();
+        this.treeDim = tree.getTaxonCount();
+        this.delegate = (ContinuousDataLikelihoodDelegate) traitLikelihood.getDataLikelihoodDelegate();
+        this.integrator = delegate.getIntegrator();
+        this.k = new double[traitDim];
+    }
+
+
+    @Override
+    public int getDimension() {
+        return traitDim;
+    }
+
+    @Override
+    public double getStatisticValue(int dim) {
+        if (dim == 0) {
+            computeStatistics();
+        }
+        return k[dim];
+    }
+
+
+    public double computeStatistics() {
+        if (needToUpdateTree) {
+            double[][] treeStructure = MultivariateTraitDebugUtilities.getTreeVariance(tree,
+                    traitLikelihood.getBranchRateModel(),
+                    1.0, Double.POSITIVE_INFINITY); //TODO: make sure order is right
+
+
+            SymmetricMatrix V = new SymmetricMatrix(treeStructure);
+            Matrix L;
+            try {
+                CholeskyDecomposition chol = new CholeskyDecomposition(V);
+                L = new Matrix(chol.getL());
+            } catch (IllegalDimension illegalDimension) {
+                illegalDimension.printStackTrace();
+                throw new RuntimeException();
+            }
+
+            Linv = L.inverse(); //TODO: need some triangular-matix specific inverse
+            double[] ones = new double[traitDim];
+            Arrays.fill(ones, 1);
+
+            Vector l;
+
+            try {
+                l = Linv.product(new Vector(ones));
+            } catch (IllegalDimension illegalDimension) {
+                illegalDimension.printStackTrace();
+                throw new RuntimeException();
+            }
+
+            double sumInverse = 0;
+            for (int i = 0; i < traitDim; i++) {
+                sumInverse += l.component(i) * l.component(i);
+            }
+
+            double trace = 0;
+            for (int i = 0; i < treeDim; i++) {
+                trace += treeStructure[i][i];
+            }
+
+            mseExpected = (trace - treeDim / sumInverse) / (treeDim - 1);
+            needToUpdateTree = false;
+        }
+
+
+        double[] treeTraits = (double[]) treeTrait.getTrait(tree, tree.getRoot());
+
+        PrecisionType type = delegate.getDataModel().getPrecisionType();
+
+        double[] partial = new double[type.getPartialsDimension(traitDim)];
+
+        integrator.getPostOrderPartial(delegate.getActiveNodeIndex(tree.getRoot().getNumber()), partial);
+        double mean[] = new double[traitDim];
+        System.arraycopy(partial, type.getMeanOffset(traitDim), mean, 0, traitDim);
+
+
+        double[] thisTrait = new double[treeDim];
+
+        for (int trait = 0; trait < traitDim; trait++) {
+            for (int taxon = 0; taxon < treeDim; taxon++) {
+                thisTrait[taxon] = treeTraits[taxon * traitDim + trait];
+            }
+
+            Vector contrasts;
+
+            try {
+                contrasts = Linv.product(new Vector(thisTrait));
+            } catch (IllegalDimension illegalDimension) {
+                illegalDimension.printStackTrace();
+                throw new RuntimeException();
+            }
+
+            double ssTrait = sumSquareDiff(thisTrait, mean[trait]);
+            double ssContrasts = sumSquareDiff(contrasts.toComponents(), mean[trait]);
+
+            k[trait] = (ssTrait / ssContrasts) / mseExpected;
+        }
+
+        return 0;
+    }
+
+    private double sumSquareDiff(double[] x, double a) {
+        double ss = 0;
+        for (int i = 0; i < x.length; i++) {
+            double diff = x[i] - a;
+            ss += diff * diff;
+        }
+        return ss;
+    }
+
+    @Override
+    public void modelChangedEvent(Model model, Object object, int index) {
+        needToUpdateTree = true;
+    }
+
+    @Override
+    public void modelRestored(Model model) {
+        needToUpdateTree = true;
+    }
+}

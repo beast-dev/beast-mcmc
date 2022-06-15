5c7f22ebb88f375cb3c323703f3e4142c1d2d10d
yucais <yucais@g.ucla.edu>
2022-06-15 12:58:29 -0700

    add zero sampling rate model

--- src/dr/evomodel/speciation/CriticalBirthDeathSerialSamplingModel.java
+++ src/dr/evomodel/speciation/CriticalBirthDeathSerialSamplingModel.java
@@ -27,26 +27,58 @@ package dr.evomodel.speciation;
 
 import dr.inference.model.Parameter;
 
+import static java.lang.Math.exp;
+import static java.lang.Math.log;
+
 public class CriticalBirthDeathSerialSamplingModel extends NewBirthDeathSerialSamplingModel {
 
-    public CriticalBirthDeathSerialSamplingModel(String modelName,
-                                                 Parameter birthDeathRate,
-                                                 Parameter serialSamplingRate,
-                                                 Parameter treatmentProbability,
-                                                 Parameter samplingFractionAtPresent,
-                                                 Parameter originTime,
-                                                 boolean condition,
-                                                 Type units) {
+    private int n_events;
 
-        super(modelName,
-                birthDeathRate, birthDeathRate, serialSamplingRate,
-                treatmentProbability, samplingFractionAtPresent, originTime,
-                condition, units);
+    public CriticalBirthDeathSerialSamplingModel(
+            String modelName,
+            Parameter birthRate,
+            Parameter deathRate,
+            Parameter serialSamplingRate,
+            Parameter treatmentProbability,
+            Parameter samplingFractionAtPresent,
+            Parameter originTime,
+            boolean condition,
+            Type units) {
+
+        super(modelName, birthRate, deathRate, serialSamplingRate, treatmentProbability, samplingFractionAtPresent, originTime, condition, units);
+        n_events = 0;
     }
+
 
     @Override
     public double processInterval(int model, double tYoung, double tOld, int nLineages) {
         // TODO Do something different
         return super.processInterval(model, tYoung, tOld, nLineages);
+    }
+
+    @Override
+    public double processOrigin(int model, double rootAge) {
+        double lambda = lambda();
+        double rho = rho();
+        double mu = mu();
+        double v = exp(-(lambda - mu) * rootAge);
+        double p_n = log(lambda*rho + (lambda*(1-rho) - mu)* v) - log(1- v);
+        return -2*logq(rootAge) + (n_events-1)*p_n;
+    }
+
+    @Override
+    public double processCoalescence(int model, double tOld) {
+        n_events += 1;
+        return 0;
+    }
+
+    @Override
+    public double processSampling(int model, double tOld) {
+        return 0;
+    }
+
+    @Override
+    public double logConditioningProbability() {
+        return -log(n_events);
     }
 }
--- src/dr/evomodelxml/speciation/NewBirthDeathSerialSamplingModelParser.java
+++ src/dr/evomodelxml/speciation/NewBirthDeathSerialSamplingModelParser.java
@@ -26,6 +26,7 @@
 package dr.evomodelxml.speciation;
 
 import dr.evolution.util.Units;
+import dr.evomodel.speciation.CriticalBirthDeathSerialSamplingModel;
 import dr.evomodel.speciation.NewBirthDeathSerialSamplingModel;
 import dr.evoxml.util.XMLUnits;
 import dr.inference.model.Parameter;
@@ -75,6 +76,10 @@ public class NewBirthDeathSerialSamplingModelParser extends AbstractXMLObjectPar
         }
 
         Logger.getLogger("dr.evomodel").info(citeThisModel);
+
+        if (psi.getParameterValue(0) < Double.MIN_VALUE){
+            return new CriticalBirthDeathSerialSamplingModel(modelName, lambda, mu, psi, r, rho, origin, condition, units);
+        }
 
         return new NewBirthDeathSerialSamplingModel(modelName, lambda, mu, psi, r, rho, origin, condition, units);
     }

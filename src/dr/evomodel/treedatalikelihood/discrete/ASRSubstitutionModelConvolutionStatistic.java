974d5d7f8652cbcc41437de555f699c6d4cfffe2
Andy Magee <andyfmagee@gmail.com>
2022-10-19 20:07:07 -0700

    Migrate code

--- src/dr/evomodel/treedatalikelihood/discrete/ASRSubstitutionModelConvolutionStatistic.java
+++ src/dr/evomodel/treedatalikelihood/discrete/ASRSubstitutionModelConvolutionStatistic.java
@@ -9,7 +9,6 @@ import dr.evomodel.branchratemodel.BranchRateModel;
 import dr.evomodel.substmodel.SubstitutionModel;
 import dr.evomodel.treelikelihood.AncestralStateBeagleTreeLikelihood;
 import dr.inference.distribution.ParametricDistributionModel;
-import dr.inference.model.Parameter;
 import dr.inference.model.Statistic;
 import dr.math.MathUtils;
 import dr.math.UnivariateFunction;
@@ -28,16 +27,20 @@ import java.util.Set;
 public class ASRSubstitutionModelConvolutionStatistic extends Statistic.Abstract implements Reportable {
 
     public ASRSubstitutionModelConvolutionStatistic(String name,
-                                                    AncestralStateBeagleTreeLikelihood asrLike,
-                                                    SubstitutionModel subsModelAncestor,
-                                                    SubstitutionModel subsModelDescendant,
-                                                    BranchRateModel branchRates,
-                                                    Statistic rateAncestor,
-                                                    Statistic rateDescendant,
-                                                    TaxonList mrcaTaxaDescendant,
-                                                    boolean bootstrap,
-                                                    ParametricDistributionModel prior
-                                     ) throws TreeUtils.MissingTaxonException {
+                                                                 AncestralStateBeagleTreeLikelihood asrLike,
+                                                                 SubstitutionModel subsModelAncestor,
+                                                                 SubstitutionModel subsModelDescendant,
+                                                                 int[] pairFirstCharacters,
+                                                                 int[] pairSecondCharacters,
+                                                                 SubstitutionModel pairedSubsModelAncestor,
+                                                                 SubstitutionModel pairedSubsModelDescendant,
+                                                                 BranchRateModel branchRates,
+                                                                 Statistic rateAncestor,
+                                                                 Statistic rateDescendant,
+                                                                 TaxonList mrcaTaxaDescendant,
+                                                                 boolean bootstrap,
+                                                                 ParametricDistributionModel prior
+    ) throws TreeUtils.MissingTaxonException {
         this.name = name;
         this.bootstrap = bootstrap;
         this.prior = prior;
@@ -51,6 +54,19 @@ public class ASRSubstitutionModelConvolutionStatistic extends Statistic.Abstract
         if ( dataType != substitutionModelDescendant.getFrequencyModel().getDataType()) { throw new RuntimeException("Incompatible datatypes in substitution models for ASRSubstitutionModelConvolution.");}
         this.tree = asrLikelihood.getTreeModel();
         this.leafSetDescendant = (mrcaTaxaDescendant != null) ? TreeUtils.getLeavesForTaxa(tree, mrcaTaxaDescendant) : null;
+        // Check validity of paired data model
+        this.pairFirstCharacters = pairFirstCharacters;
+        this.pairSecondCharacters = pairSecondCharacters;
+        this.pairedSubstitutionModelAncestor = pairedSubsModelAncestor;
+        this.pairedSubstitutionModelDescendant = pairedSubsModelDescendant;
+        this.isPartitioned = pairedSubsModelAncestor != null;
+        this.doubletsAreSafe = (!isPartitioned || !doubletsCanOverlap()) ? true : false;
+        if (isPartitioned) {
+            if ( bootstrap) { throw new RuntimeException("Cannot currently bootstrap context-dependent models."); }
+            if ( (pairedSubsModelAncestor != null && pairedSubsModelAncestor == null) || pairedSubsModelAncestor == null && pairedSubsModelAncestor != null) { throw new RuntimeException("If specifying models for doublets must specify ancestral and descendant models."); }
+            if ( pairedSubsModelAncestor.getFrequencyModel().getFrequencies().length != pairedSubsModelDescendant.getFrequencyModel().getFrequencies().length ) { throw new RuntimeException("Doublet models do not match in size."); }
+            if ( pairedSubsModelAncestor.getFrequencyModel().getFrequencies().length != dataType.getStateCount() * dataType.getStateCount() ) { throw new RuntimeException("Doublet models are not sized for doublets."); }
+        }
     }
 
     public int getDimension() {
@@ -77,6 +93,11 @@ public class ASRSubstitutionModelConvolutionStatistic extends Statistic.Abstract
         NodeRef nodeDescendant = getNode(leafSetDescendant);
         NodeRef nodeAncestor = tree.getParent(nodeDescendant);
 
+        if (!doubletsAreSafe && doubletsOverlapOnSequence(nodeAncestor)) {
+            // We cannot technically apply this but to avoid mucking up a whole MCMC chain we'll just return NaN
+            return Double.NaN;
+        }
+
         double[] rates = getRates(nodeDescendant);
 
         double branchTime = tree.getNodeHeight(nodeAncestor) - tree.getNodeHeight(nodeDescendant);
@@ -89,7 +110,7 @@ public class ASRSubstitutionModelConvolutionStatistic extends Statistic.Abstract
     @Override
     public String getReport() {
 
-        StringBuilder sb = new StringBuilder("ASRSubstitutionModelConvolutionStatistic Report\n\n");
+        StringBuilder sb = new StringBuilder("hackyStatespaceStatistic Report\n\n");
 
         sb.append("Estimated time of shift before common ancestor: ").append(getStatisticValue(0)).append("\n");
         sb.append("Using rates: ").append(new dr.math.matrixAlgebra.Vector(getRates(getNode(leafSetDescendant)))).append("\n");
@@ -104,7 +125,7 @@ public class ASRSubstitutionModelConvolutionStatistic extends Statistic.Abstract
         sb.append("\n\n");
         return sb.toString();
     }
-    
+
     // Gets rates for ancestral and descendant (in that order) portions of the branch
     private double[] getRates(NodeRef node) {
         double[] rates = new double[2];
@@ -123,17 +144,8 @@ public class ASRSubstitutionModelConvolutionStatistic extends Statistic.Abstract
 
         return rates;
     }
-    
-    private double computeLogLikelihood(double distance1, double distance2, double rateDescendant, int[] ancestorStates, int[] descendantStates) {
-        int nStates = dataType.getStateCount();
 
-        double[] tpm1 = new double[nStates * nStates];
-        double[] tpm2 = new double[nStates * nStates];
-        substitutionModelAncestor.getTransitionProbabilities(distance1, tpm1);
-        substitutionModelDescendant.getTransitionProbabilities(distance2, tpm2);
-
-        // Do the matrix convolution
-        double[] tpm= new double[nStates * nStates];
+    private void convolveMatrices(double[] tpm1, double[] tpm2, double[] tpm, int nStates) {
         for (int i = 0; i < nStates; i++) {
             for (int j = 0; j < nStates; j++) {
                 for (int k = 0; k < nStates; k++) {
@@ -141,15 +153,105 @@ public class ASRSubstitutionModelConvolutionStatistic extends Statistic.Abstract
                 }
             }
         }
+    }
+
+    private boolean isPair(int first, int second) {
+        if (!isPartitioned) {
+            return false;
+        }
+
+        boolean pair = false;
+
+        for (int i = 0; i < pairFirstCharacters.length; i++) {
+            if ( first == pairFirstCharacters[i] && second == pairSecondCharacters[i] ) {
+                pair = true;
+                break;
+            }
+        }
+        return pair;
+    }
+
+    private boolean doubletsCanOverlap() {
+        boolean overlapPossible = false;
+        for (int i = 0; i < pairFirstCharacters.length; i++) {
+            for (int j= 0; j < pairSecondCharacters.length; j++) {
+                if (pairFirstCharacters[i] == pairSecondCharacters[j]) {
+                    overlapPossible = true;
+                    break;
+                }
+            }
+        }
+        return overlapPossible;
+    }
+
+    private boolean doubletsOverlapOnSequence(NodeRef node) {
+        int[] states = asrLikelihood.getStatesForNode(tree, node);
+        boolean overlapping = false;
+        boolean lastWasPair = false;
+        for (int i = 0; i < states.length - 1; i++) {
+            if ( isPair(states[i], states[i+1]) ) {
+                if ( lastWasPair ) {
+                    overlapping = true;
+                    break;
+                } else {
+                    lastWasPair = true;
+                }
+            } else {
+                lastWasPair = false;
+            }
+        }
+
+        return overlapping;
+    }
+
+    private int getDoublet(int first, int second, int nStates) {
+        return first * nStates + second;
+    }
+
+    private double computeLogLikelihood(double distance1, double distance2, double rateDescendant, int[] ancestorStates, int[] descendantStates) {
+        int nStates = dataType.getStateCount();
+        int nStatesSquared = nStates * nStates;
+
+        double[] tpm1 = new double[nStatesSquared];
+        double[] tpm2 = new double[nStatesSquared];
+        substitutionModelAncestor.getTransitionProbabilities(distance1, tpm1);
+        substitutionModelDescendant.getTransitionProbabilities(distance2, tpm2);
+
+        // Do the matrix convolution
+        double[] tpm= new double[nStates * nStates];
+        convolveMatrices(tpm1, tpm2, tpm, nStates);
 
         double[] logTpm = new double[nStates * nStates];
         for (int i = 0; i < nStates * nStates; i++) {
             logTpm[i] = Math.log(tpm[i]);
         }
 
+        double[] pairedTpm = new double[nStatesSquared * nStatesSquared];
+        double[] logPairedTpm = new double[nStatesSquared * nStatesSquared];
+        if ( isPartitioned ) {
+            double[] pairedTpm1 = new double[nStatesSquared * nStatesSquared];
+            double[] pairedTpm2 = new double[nStatesSquared * nStatesSquared];
+            pairedSubstitutionModelAncestor.getTransitionProbabilities(distance1, pairedTpm1);
+            pairedSubstitutionModelDescendant.getTransitionProbabilities(distance2, pairedTpm2);
+
+            // Do the matrix convolution
+            convolveMatrices(pairedTpm1, pairedTpm2, pairedTpm, nStates);
+
+            for (int i = 0; i < nStatesSquared * nStatesSquared; i++) {
+                logPairedTpm[i] = Math.log(pairedTpm1[i]);
+            }
+        }
+
         double lnL = 0.0;
+        int nextToLast = ancestorStates.length - 1;
         for (int s = 0; s < ancestorStates.length; s++) {
-            lnL += logTpm[ancestorStates[s] * nStates + descendantStates[s]];
+            if (s < nextToLast && isPair(ancestorStates[s],ancestorStates[s+1])) {
+                int from = getDoublet(ancestorStates[s], ancestorStates[s + 1], nStates);
+                int to = getDoublet(descendantStates[s], descendantStates[s + 1], nStates);
+                lnL += logPairedTpm[from * nStatesSquared + to];
+            } else {
+                lnL += logTpm[ancestorStates[s] * nStates + descendantStates[s]];
+            }
         }
 
         if (prior != null) {
@@ -219,10 +321,16 @@ public class ASRSubstitutionModelConvolutionStatistic extends Statistic.Abstract
     private AncestralStateBeagleTreeLikelihood asrLikelihood;
     private SubstitutionModel substitutionModelAncestor;
     private SubstitutionModel substitutionModelDescendant;
+    private SubstitutionModel pairedSubstitutionModelAncestor;
+    private SubstitutionModel pairedSubstitutionModelDescendant;
     private final Set<String> leafSetDescendant;
     private final Tree tree;
     private final DataType dataType;
     private final boolean bootstrap;
     private final ParametricDistributionModel prior;
     private final String name;
+    private final int[] pairFirstCharacters;
+    private final int[] pairSecondCharacters;
+    private final boolean isPartitioned;
+    private final boolean doubletsAreSafe;
 }

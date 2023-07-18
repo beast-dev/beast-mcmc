ff2586a40bdfd59d8e01a612a98f66f782b81a1b
Andy Magee <andyfmagee@gmail.com>
2023-07-18 14:53:47 -0700

    Revert BranchSpecificBranchModelParser to 63b7de6

--- src/dr/evomodelxml/branchmodel/BranchSpecificBranchModelParser.java
+++ src/dr/evomodelxml/branchmodel/BranchSpecificBranchModelParser.java
@@ -31,7 +31,6 @@ import dr.evomodel.substmodel.SubstitutionModel;
 import dr.evolution.util.Taxa;
 import dr.evolution.util.TaxonList;
 import dr.evomodel.tree.TreeModel;
-import dr.inference.model.Parameter;
 import dr.xml.*;
 
 import java.util.logging.Logger;
@@ -67,11 +66,6 @@ public class BranchSpecificBranchModelParser extends AbstractXMLObjectParser {
                 if (xoc.getName().equals(CLADE)) {
 
                     double stemWeight = xoc.getAttribute(STEM_WEIGHT, 0.0);
-                    Parameter stemWeightParameter = null;
-                    if (xoc.hasChildNamed(STEM_WEIGHT)) {
-                        stemWeightParameter = (Parameter) xoc.getElementFirstChild(STEM_WEIGHT);
-                        stemWeight = Double.NaN;
-                    }
 
                     substitutionModel = (SubstitutionModel) xoc.getChild(SubstitutionModel.class);
                     TaxonList taxonList = (TaxonList) xoc.getChild(TaxonList.class);
@@ -82,7 +76,7 @@ public class BranchSpecificBranchModelParser extends AbstractXMLObjectParser {
                     }
 
                     try {
-                        branchModel.addClade(taxonList, substitutionModel, stemWeight, stemWeightParameter);
+                        branchModel.addClade(taxonList, substitutionModel, stemWeight);
 
                     } catch (TreeUtils.MissingTaxonException mte) {
                         throw new XMLParseException("Taxon, " + mte + ", in " + getParserName() + " was not found in the tree.");

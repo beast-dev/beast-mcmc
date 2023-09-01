a3ff593ca70d3d09b0703718a1595cfdb12a77b3
Andrew Rambaut <rambaut@gmail.com>
2023-09-01 13:57:07 +0100

    Hiding Felsenstein weights in Gamma site model for BEAUti for the moment.

--- src/dr/app/beauti/sitemodelspanel/PartitionModelPanel.java
+++ src/dr/app/beauti/sitemodelspanel/PartitionModelPanel.java
@@ -74,7 +74,7 @@ public class PartitionModelPanel extends OptionsPanel {
             .values());
 
     private JComboBox heteroCombo = new JComboBox(new String[] { "None",
-            "Gamma (Felsenstein weights)", "Gamma (equal weights)", "Invariant Sites", "Gamma (equal weights) + Invariant Sites" });
+            /*"Gamma (Felsenstein weights)", */ "Gamma (equal weights)", "Invariant Sites", "Gamma (equal weights) + Invariant Sites" });
 
     private JComboBox gammaCatCombo = new JComboBox(new String[] { "4", "5",
             "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16" });
@@ -216,21 +216,17 @@ public class PartitionModelPanel extends OptionsPanel {
 
         PanelUtils.setupComponent(heteroCombo);
         heteroCombo
-                .setToolTipText("<html>Select the type of site-specific rate<br>heterogeneity model.<br>"
-                + "\"Felsenstein weights\" uses the quadrature method to calculate the category weights described in <br>" +
-                "Felsenstein (2001) <i>J Mol Evol</i> <b>53</b>: 447-455.</html>");
+                .setToolTipText("<html>Select the type of site-specific rate<br>heterogeneity model." +
+//                        "<br>\"Felsenstein weights\" uses the quadrature method to calculate the category weights described in <br>" +
+//                        "Felsenstein (2001) <i>J Mol Evol</i> <b>53</b>: 447-455." +
+                        "</html>");
         heteroCombo.addItemListener(new ItemListener() {
             public void itemStateChanged(ItemEvent ev) {
 
-                boolean gammaHetero = heteroCombo.getSelectedIndex() == 1 ||
-                        heteroCombo.getSelectedIndex() == 2
-                        || heteroCombo.getSelectedIndex() == 4;
-
+                boolean gammaHetero = heteroCombo.getSelectedItem().toString().contains("Gamma");
                 model.setGammaHetero(gammaHetero);
-                model.setInvarHetero(heteroCombo.getSelectedIndex() == 3
-                        || heteroCombo.getSelectedIndex() == 4);
-                model.setGammaHeteroEqualWeights(heteroCombo.getSelectedIndex() == 2
-                        || heteroCombo.getSelectedIndex() == 4);
+                model.setInvarHetero(heteroCombo.getSelectedItem().toString().contains("Invariant"));
+                model.setGammaHeteroEqualWeights(heteroCombo.getSelectedItem().toString().contains("equal"));
 
                 if (gammaHetero) {
                     gammaCatLabel.setEnabled(true);

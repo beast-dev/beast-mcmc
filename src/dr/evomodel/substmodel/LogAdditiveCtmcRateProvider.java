48b35710c8640773d9944136e808ca0fd605aa4d
Marc A. Suchard <msuchard@ucla.edu>
2023-10-04 17:58:02 -0700

    clean up interface a bit

--- src/dr/evomodel/substmodel/LogAdditiveCtmcRateProvider.java
+++ src/dr/evomodel/substmodel/LogAdditiveCtmcRateProvider.java
@@ -9,4 +9,12 @@ public interface LogAdditiveCtmcRateProvider extends Model, Likelihood {
     double[] getXBeta();
 
     LogColumn[] getColumns();
+
+    default double[] getRates() {
+        double[] rates = getXBeta();
+        for (int i = 0; i < rates.length; ++i) {
+            rates[i] = Math.exp(rates[i]);
+        }
+        return rates;
+    }
 }

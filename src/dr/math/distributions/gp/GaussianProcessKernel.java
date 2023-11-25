d504bc4e9bfeda469a7c2561a92144e5f1b75810
Marc A. Suchard <msuchard@ucla.edu>
2023-11-25 06:41:47 -0800

    more GP kernels

--- src/dr/math/distributions/gp/GaussianProcessKernel.java
+++ src/dr/math/distributions/gp/GaussianProcessKernel.java
@@ -7,6 +7,10 @@ import dr.inference.model.Variable;
 
 import java.util.List;
 
+import static dr.math.ModifiedBesselFirstKind.bessi;
+import static dr.math.functionEval.GammaFunction.factorial;
+import static dr.math.functionEval.GammaFunction.gamma;
+
 /**
  * @author Marc A. Suchard
  * @author Filippo Monti
@@ -62,18 +66,73 @@ public interface GaussianProcessKernel {
         }
 
         public double getCorrelation(double[] x, double[] y) {
-            final int dim = x.length;
-
-            double normSquared = 0.0;
-            for (int i = 0; i < dim; ++i) {
-                double diff = x[i] - y[i];
-                normSquared += diff * diff;
-            }
-
-            return functionalForm(normSquared);
+            return functionalForm(normSquared(x, y));
         }
 
         private static final String TYPE = "RadialBasisFunction";
+    }
+
+    class OrnsteinUhlenbeck extends L1Base {
+
+        public OrnsteinUhlenbeck(String name, List<Parameter> parameters) { super(name, parameters); }
+
+        double functionalForm(double norm) {
+            double length = parameters.get(0).getParameterValue(0);
+            return Math.exp(-norm / length);
+        }
+
+        private static final String TYPE = "OrnsteinUhlenbeck";
+    }
+
+    class Matern extends L1Base {
+
+        private final Parameter orderParameter;
+        private final int order;
+        private final double normalization;
+        private final double scale;
+
+        public Matern(String name, List<Parameter> parameters) {
+            super(name, parameters);
+            this.orderParameter = parameters.get(1);
+
+            this.order = (int) orderParameter.getParameterValue(0);
+            this.normalization = Math.pow(2, 1 - order) / factorial(order - 1);
+            this.scale = Math.sqrt(2 * order);
+        }
+
+        double functionalForm(double norm) {
+            double length = parameters.get(0).getParameterValue(0);
+            double argument = scale * norm / length;
+
+            return normalization * Math.pow(argument, order) * bessi(argument, order);
+        }
+
+        @Override
+        protected void handleVariableChangedEvent(Variable variable, int index, Parameter.ChangeType type) {
+            if (variable == orderParameter) {
+                throw new RuntimeException("Not yet implemented");
+            }
+            super.handleVariableChangedEvent(variable, index, type);
+        }
+
+        private static final String TYPE = "Matern";
+    }
+
+    abstract class L1Base extends Base {
+
+        public L1Base(String name, List<Parameter> parameters) { super(name, parameters); }
+
+        abstract double functionalForm(double norm);
+
+        public double getCorrelation(double x, double y) {
+            double norm = Math.abs(x - y);
+            return functionalForm(norm);
+        }
+
+        public double getCorrelation(double[] x, double[] y) {
+            double norm = Math.sqrt(normSquared(x, y));
+            return functionalForm(norm);
+        }
     }
 
     static GaussianProcessKernel factory(String type, String name, List<Parameter> parameters)
@@ -82,6 +141,10 @@ public interface GaussianProcessKernel {
             return new DotProduct(name, parameters);
         } else if (type.equalsIgnoreCase(RadialBasisFunction.TYPE)) {
             return new RadialBasisFunction(name, parameters);
+        } else if (type.equalsIgnoreCase(Matern.TYPE)) {
+            return new Matern(name, parameters);
+        } else if (type.equalsIgnoreCase(OrnsteinUhlenbeck.TYPE)) {
+            return new OrnsteinUhlenbeck(name, parameters);
         } else {
             throw new IllegalArgumentException("Unknown kernel type");
         }
@@ -100,6 +163,18 @@ public interface GaussianProcessKernel {
             for (Parameter p :parameters) {
                 addVariable(p);
             }
+        }
+
+        double normSquared(double[] x, double[] y) {
+            final int dim = x.length;
+
+            double normSquared = 0.0;
+            for (int i = 0; i < dim; ++i) {
+                double diff = x[i] - y[i];
+                normSquared += diff * diff;
+            }
+
+            return normSquared;
         }
 
         @Override

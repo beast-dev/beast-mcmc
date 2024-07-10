453d7a5911122c319fd087b625932ebd14116f34
rambaut <rambaut@gmail.com>
2024-07-10 10:20:07 +0100

    Putting in FreeRate delegate into the SiteModelParser

--- src/dr/evomodelxml/siteratemodel/SiteModelParser.java
+++ src/dr/evomodelxml/siteratemodel/SiteModelParser.java
@@ -29,20 +29,11 @@ package dr.evomodelxml.siteratemodel;
 
 import java.util.logging.Logger;
 
-import dr.evomodel.siteratemodel.DiscretizedSiteRateModel;
-import dr.evomodel.siteratemodel.GammaSiteRateDelegate;
-import dr.evomodel.siteratemodel.HomogeneousRateDelegate;
-import dr.evomodel.siteratemodel.SiteRateDelegate;
+import dr.evomodel.siteratemodel.*;
 import dr.evomodel.substmodel.SubstitutionModel;
 import dr.oldevomodel.sitemodel.SiteModel;
 import dr.inference.model.Parameter;
-import dr.xml.AbstractXMLObjectParser;
-import dr.xml.AttributeRule;
-import dr.xml.ElementRule;
-import dr.xml.XMLObject;
-import dr.xml.XMLParseException;
-import dr.xml.XMLSyntaxRule;
-import dr.xml.XORRule;
+import dr.xml.*;
 
 /**
  * This is a replacement to GammaSiteModelParser to keep old XML that used
@@ -57,7 +48,11 @@ public class SiteModelParser extends AbstractXMLObjectParser {
     public static final String SUBSTITUTION_RATE = "substitutionRate";
     public static final String RELATIVE_RATE = "relativeRate";
     public static final String WEIGHT = "weight";
+    public static final String FREE_RATES = "freeRates";
+    public static final String RATES = "rates";
+    public static final String WEIGHTS = "weights";
     public static final String GAMMA_SHAPE = "gammaShape";
+    public static final String RATE_CATEGORIES = "rateCategories";
     public static final String GAMMA_CATEGORIES = "gammaCategories";
     public static final String PROPORTION_INVARIANT = "proportionInvariant";
     public static final String DISCRETIZATION = "discretization";
@@ -92,36 +87,50 @@ public class SiteModelParser extends AbstractXMLObjectParser {
             }
         }
 
-        GammaSiteRateDelegate.DiscretizationType type = GammaSiteRateDelegate.DEFAULT_DISCRETIZATION;
-
         Parameter shapeParam = null;
+        Parameter invarParam = null;
+        Parameter ratesParam = null;
+        Parameter weightsParam = null;
+        GammaSiteRateDelegate.DiscretizationType type = GammaSiteRateDelegate.DEFAULT_DISCRETIZATION;
         int catCount = 4;
-        if (xo.hasChildNamed(GAMMA_SHAPE)) {
-            XMLObject cxo = xo.getChild(GAMMA_SHAPE);
-            catCount = cxo.getIntegerAttribute(GAMMA_CATEGORIES);
 
-            if ( cxo.hasAttribute(DISCRETIZATION)) {
-                try {
-                    type = GammaSiteRateDelegate.DiscretizationType.valueOf(
-                            cxo.getStringAttribute(DISCRETIZATION).toUpperCase());
-                } catch (IllegalArgumentException eae) {
-                    throw new XMLParseException("Unknown category width type: " + cxo.getStringAttribute(DISCRETIZATION));
+        if (xo.hasChildNamed(FREE_RATES)) {
+            catCount = xo.getIntegerAttribute(RATE_CATEGORIES);
+
+            ratesParam = (Parameter) xo.getElementFirstChild(RATES);
+            weightsParam = (Parameter) xo.getElementFirstChild(WEIGHTS);
+
+            msg += "\n  " + catCount + " category free rate model";
+
+        } else if (xo.hasChildNamed(GAMMA_SHAPE)) {
+
+
+            if (xo.hasChildNamed(GAMMA_SHAPE)) {
+                XMLObject cxo = xo.getChild(GAMMA_SHAPE);
+                catCount = cxo.getIntegerAttribute(GAMMA_CATEGORIES);
+
+                if (cxo.hasAttribute(DISCRETIZATION)) {
+                    try {
+                        type = GammaSiteRateDelegate.DiscretizationType.valueOf(
+                                cxo.getStringAttribute(DISCRETIZATION).toUpperCase());
+                    } catch (IllegalArgumentException eae) {
+                        throw new XMLParseException("Unknown category width type: " + cxo.getStringAttribute(DISCRETIZATION));
+                    }
+                }
+                shapeParam = (Parameter) cxo.getChild(Parameter.class);
+
+                msg += "\n  " + catCount + " category discrete gamma with initial shape = " + shapeParam.getParameterValue(0);
+                if (type == GammaSiteRateDelegate.DiscretizationType.EQUAL) {
+                    msg += "\n  using equal weight discretization of gamma distribution";
+                } else {
+                    msg += "\n  using Gauss-Laguerre quadrature discretization of gamma distribution (Felsenstein, 2012)";
                 }
             }
-            shapeParam = (Parameter) cxo.getChild(Parameter.class);
 
-            msg += "\n  " + catCount + " category discrete gamma with initial shape = " + shapeParam.getParameterValue(0);
-            if (type == GammaSiteRateDelegate.DiscretizationType.EQUAL) {
-                msg += "\n  using equal weight discretization of gamma distribution";
-            } else {
-                msg += "\n  using Gauss-Laguerre quadrature discretization of gamma distribution (Felsenstein, 2012)";
+            if (xo.hasChildNamed(PROPORTION_INVARIANT)) {
+                invarParam = (Parameter) xo.getElementFirstChild(PROPORTION_INVARIANT);
+                msg += "\n  initial proportion of invariant sites = " + invarParam.getParameterValue(0);
             }
-        }
-
-        Parameter invarParam = null;
-        if (xo.hasChildNamed(PROPORTION_INVARIANT)) {
-            invarParam = (Parameter) xo.getElementFirstChild(PROPORTION_INVARIANT);
-            msg += "\n  initial proportion of invariant sites = " + invarParam.getParameterValue(0);
         }
 
         if (msg.length() > 0) {
@@ -194,15 +203,24 @@ public class SiteModelParser extends AbstractXMLObjectParser {
                     }), true
             ),
 
-            new ElementRule(GAMMA_SHAPE, new XMLSyntaxRule[]{
-                    AttributeRule.newIntegerRule(GAMMA_CATEGORIES, true),
-                    AttributeRule.newStringRule(DISCRETIZATION, true),
-                    new ElementRule(Parameter.class)
-            }, true),
-
-            new ElementRule(PROPORTION_INVARIANT, new XMLSyntaxRule[]{
-                    new ElementRule(Parameter.class)
-            }, true)
+            new XORRule(
+                    new AndRule(new XMLSyntaxRule[]{
+                            new ElementRule(GAMMA_SHAPE, new XMLSyntaxRule[]{
+                                    AttributeRule.newIntegerRule(GAMMA_CATEGORIES, true),
+                                    AttributeRule.newIntegerRule(RATE_CATEGORIES, true),
+                                    AttributeRule.newStringRule(DISCRETIZATION, true),
+                                    new ElementRule(Parameter.class)
+                            }, true),
+                            new ElementRule(PROPORTION_INVARIANT, new XMLSyntaxRule[]{
+                                    new ElementRule(Parameter.class)
+                            }, true)
+                    }),
+                    new ElementRule(FREE_RATES, new XMLSyntaxRule[]{
+                            AttributeRule.newIntegerRule(RATE_CATEGORIES, true),
+                            AttributeRule.newStringRule(DISCRETIZATION, true),
+                            new ElementRule(Parameter.class)
+                    }, true)
+            )
     };
 
 }//END: class

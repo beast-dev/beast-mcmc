e6c5a9b0250cacb9087c2c62c59b344c7e760990
xji3 <xji4@tulane.edu>
2024-08-20 15:45:18 -0500

    fix smooth skygrid restore issue for variable changes

--- src/dr/evomodel/coalescent/smooth/SmoothSkygridLikelihood.java
+++ src/dr/evomodel/coalescent/smooth/SmoothSkygridLikelihood.java
@@ -35,6 +35,7 @@ import dr.evomodel.coalescent.AbstractCoalescentLikelihood;
 import dr.evomodel.tree.TreeModel;
 import dr.inference.model.Model;
 import dr.inference.model.Parameter;
+import dr.inference.model.Variable;
 import dr.util.Author;
 import dr.util.Citable;
 import dr.util.Citation;
@@ -645,6 +646,11 @@ public class SmoothSkygridLikelihood extends AbstractCoalescentLikelihood implem
     protected void handleModelChangedEvent(Model model, Object object, int index) {
         super.handleModelChangedEvent(model, object, index);
         tmpSumsKnown = false;
+    }
+
+    protected void handleVariableChangedEvent(Variable variable, int index, Parameter.ChangeType type) {
+        tmpSumsKnown = false;
+        likelihoodKnown = false;
     }
 
     private double getLineageCountEffect(Tree tree, int node) {

a0e9ad5e77284b0a08bd4371898c45621b080b02
Marc Suchard <msuchard@ucla.edu>
2024-10-04 13:33:21 -0700

    intermediate work on Cauchy RVs

--- src/dr/inference/distribution/CauchyDistribution.java
+++ src/dr/inference/distribution/CauchyDistribution.java
@@ -30,38 +30,70 @@ package dr.inference.distribution;
 import dr.inference.model.GradientProvider;
 import dr.math.UnivariateFunction;
 import dr.math.distributions.Distribution;
-import org.apache.commons.math.distribution.CauchyDistributionImpl;
+import org.apache.commons.math.util.FastMath;
 
 public class CauchyDistribution implements Distribution, GradientProvider {
-    private final CauchyDistributionImpl distribution;
     private final double median;
     private final double scale;
 
     public CauchyDistribution(double median, double scale){
-        distribution = new CauchyDistributionImpl(median, scale);
         this.median = median;
         this.scale = scale;
     }
 
+    public static double pdf(double x, double median, double scale) {
+        double dev = x - median;
+        return  scale / (dev * dev + scale * scale) / Math.PI;
+    }
+
+    public static double cdf(double x, double median, double scale) {
+        return 0.5 + FastMath.atan((x - median) / scale) / Math.PI;
+    }
+
+    public static double quantile(double p, double median, double scale) {
+        if (!(p < 0.0) && !(p > 1.0)) {
+            double ret;
+            if (p == 0.0) {
+                ret = Double.NEGATIVE_INFINITY;
+            } else if (p == 1.0) {
+                ret = Double.POSITIVE_INFINITY;
+            } else {
+                ret = median + scale * FastMath.tan(Math.PI * (p - 0.5));
+            }
+
+            return ret;
+        } else {
+            throw new IllegalArgumentException("Out of range");
+        }
+    }
+
+    public static double gradLogPdf(double x, double median, double scale) {
+        double dev = x - median;
+        return 2 * dev / (dev * dev  + scale * scale);
+    }
+
+    public static double logPdf(double x, double median, double scale) {
+        return Math.log(pdf(x, median, scale));
+    }
 
     @Override
     public double pdf(double x) {
-        return distribution.density(x);
+        return pdf(x, median, scale);
     }
 
     @Override
     public double logPdf(double x) {
-        return Math.log(distribution.density(x));
+        return logPdf(x, median, scale);
     }
 
     @Override
     public double cdf(double x) {
-        return distribution.cumulativeProbability(x);
+       return cdf(x, median, scale);
     }
 
     @Override
     public double quantile(double y) {
-        return distribution.inverseCumulativeProbability(y);
+        return quantile(y, median, scale);
     }
 
     @Override
@@ -101,9 +133,6 @@ public class CauchyDistribution implements Distribution, GradientProvider {
     @Override
     public double[] getGradientLogDensity(Object input) {
         double x = (double) input;
-
-        double dev = x - median;
-        double dx = 2 * dev / (dev * dev  + scale * scale);
-        return new double[] { dx };
+        return new double[] { gradLogPdf(x, median, scale) };
     }
 }

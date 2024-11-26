a3a28e164260ad2b87d14c1b4fd6e1a134699051
xji3 <xji4@tulane.edu>
2024-11-26 17:16:40 -0600

    correct formula

--- src/dr/evomodel/treedatalikelihood/ApproximateTreeDataLikelihood.java
+++ src/dr/evomodel/treedatalikelihood/ApproximateTreeDataLikelihood.java
@@ -68,7 +68,7 @@ public class ApproximateTreeDataLikelihood extends AbstractModelLikelihood {
         this.likelihood = maximizer.getLikelihood();
         final GradientWrtParameterProvider gradient = maximizer.getGradient();
         this.parameter = gradient.getParameter();
-        this.marginalLikelihoodConst = (parameter.getDimension() - 1) * Math.log(2 * Math.PI);
+        this.marginalLikelihoodConst = - (parameter.getDimension() / 2.) * Math.log(2 * Math.PI);
         // todo: get Numerical Hessian.
         if (maximizer.getTransform() != null) {
             this.hessianWrtParameterProvider = constructHessian();
@@ -139,7 +139,7 @@ public class ApproximateTreeDataLikelihood extends AbstractModelLikelihood {
             logDiagonalDeterminant += Math.log(Math.abs(diagonalHessian[i]));
         }
         // 2pi^{-k/2} * det(Sigma)^{-1/2} * likelihood(map) * prior(map)
-        this.marginalLikelihood = marginalLikelihoodConst + 0.5 * logDiagonalDeterminant + likelihood.getLogLikelihood()
+        this.marginalLikelihood = marginalLikelihoodConst - 0.5 * logDiagonalDeterminant + likelihood.getLogLikelihood()
          + (maximizer.getTransform() == null ? 0 : maximizer.getTransform().logJacobian(parameter.getParameterValues(), 0, parameter.getDimension()));
         likelihoodKnown = true;
     }

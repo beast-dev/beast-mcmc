0aa84ff1148892e5a0ac02034bacd4dbb4764bf5
Andrew Rambaut <rambaut@gmail.com>
2025-04-21 10:33:32 +0100

    Optimising

--- src/dr/app/tools/treeannotator/CollectionAction.java
+++ src/dr/app/tools/treeannotator/CollectionAction.java
@@ -46,9 +46,9 @@ class CollectionAction implements CladeAction {
 
     @Override
     public void actOnClade(Clade clade, Tree tree, NodeRef node) {
-        synchronized (clade) {
+//        synchronized (clade) {
             collectAttributesForClade(clade, attributeNames, tree, node);
-        }
+//        }
     }
 
     @Override

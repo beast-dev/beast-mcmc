51764e44c5a854ecd6acba79d37eac5e516982a0
xji3 <xji4@tulane.edu>
2025-05-10 18:10:13 -0500

    Revert "Bug fix: pre-order partial index always offset post-order partial index,so that restoring (double buffering) does not cause indexing error"
    This reverts commit 41ce673e96d10472d51d7e016146452153437765.

--- src/dr/evomodel/treedatalikelihood/preorder/AbstractBeagleGradientDelegate.java
+++ src/dr/evomodel/treedatalikelihood/preorder/AbstractBeagleGradientDelegate.java
@@ -229,7 +229,7 @@ public abstract class AbstractBeagleGradientDelegate extends ProcessSimulationDe
     }
 
     protected int getPreOrderPartialIndex(final int nodeNumber) {
-        return preOrderPartialOffset + getPostOrderPartialIndex(nodeNumber);
+        return preOrderPartialOffset + nodeNumber;
     }
 
     @Override

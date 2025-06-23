c92fca4b4c8a2b345af0f6d7e2e582ab8f47777d
Andrew Rambaut <rambaut@gmail.com>
2025-06-23 15:22:26 +0100

    An experimental algorithm for MRHipster (for experimenting - not called by default)

--- src/dr/app/tools/treeannotator/HIPSTRTreeBuilder.java
+++ src/dr/app/tools/treeannotator/HIPSTRTreeBuilder.java
@@ -154,8 +154,95 @@ public class HIPSTRTreeBuilder {
                 // two tips so there will only be one pair and their sum log cred will be 0.0
                 assert clade.getSubClades().size() == 1;
                 Pair<BiClade, BiClade> subClade = clade.getSubClades().stream().findFirst().get();
-                clade.bestLeft = subClade.first;
-                clade.bestRight = subClade.second;
+                if (subClade.first.getIndex() < subClade.second.getIndex()) {
+                    clade.bestLeft = subClade.first;
+                    clade.bestRight = subClade.second;
+                } else {
+                    clade.bestRight = subClade.first;
+                    clade.bestLeft = subClade.second;
+                }
+                cladeScore += 2 * Math.log(1.0);  // yes, I know this is zero - just spelling out why
+            }
+            clade.bestSubTreeScore = cladeScore;
+
+            credibilityCache.put(clade, cladeScore);
+        }
+    }
+
+    private void findHIPSTRTreeMajRule(List<BiClade> clades) {
+
+        for (BiClade clade : clades) {
+
+            double cladeScore = Math.log(clade.getCredibility());
+
+            if (clade.getSize() > 2) {
+                // more than two tips in this clade
+
+                List<Pair<BiClade, BiClade>> candidates = new ArrayList<>();
+                if (clade.getSubClades().size() > 5) {
+                    System.out.println("eek");
+                }
+
+                int majRuleCount = 0;
+                for (Pair<BiClade, BiClade> subClade : clade.getSubClades()) {
+                    BiClade left = subClade.first;
+                    BiClade right = subClade.second;
+
+                    int n = (left.getCredibility() >= 0.5 ? 1 : 0) + (right.getCredibility() >= 0.5 ? 1 : 0);
+
+                    if (n > majRuleCount) {
+                        majRuleCount = n;
+                        candidates.clear();
+                    }
+
+                    if (n == majRuleCount) {
+                        candidates.add(new Pair<>(left, right));
+                    }
+                }
+
+                double bestSubtreeScore = Double.NEGATIVE_INFINITY;
+
+                for (Pair<BiClade, BiClade> subClade : candidates) {
+                    BiClade left = subClade.first;
+                    BiClade right = subClade.second;
+
+                    double leftScore = Math.log(1.0);
+                    if (left.getSize() > 1) {
+                        leftScore = credibilityCache.getOrDefault(left, Double.NaN);
+                        assert !Double.isNaN(leftScore);
+                    }
+
+                    double rightScore = Math.log(1.0);
+                    if (right.getSize() > 1) {
+                        rightScore = credibilityCache.getOrDefault(right, Double.NaN);
+                        assert !Double.isNaN(rightScore);
+                    }
+
+                    if (leftScore + rightScore > bestSubtreeScore) {
+                        bestSubtreeScore = leftScore + rightScore;
+                        if (left.getSize() < right.getSize()) {
+                            // sort by clade size because why not...
+                            clade.bestLeft = left;
+                            clade.bestRight = right;
+                        } else {
+                            clade.bestLeft = right;
+                            clade.bestRight = left;
+                        }
+                    }
+                }
+
+                cladeScore = cladeScore + bestSubtreeScore;
+            } else {
+                // two tips so there will only be one pair and their sum log cred will be 0.0
+                assert clade.getSubClades().size() == 1;
+                Pair<BiClade, BiClade> subClade = clade.getSubClades().stream().findFirst().get();
+                if (subClade.first.getIndex() < subClade.second.getIndex()) {
+                    clade.bestLeft = subClade.first;
+                    clade.bestRight = subClade.second;
+                } else {
+                    clade.bestRight = subClade.first;
+                    clade.bestLeft = subClade.second;
+                }
                 cladeScore += 2 * Math.log(1.0);  // yes, I know this is zero - just spelling out why
             }
             clade.bestSubTreeScore = cladeScore;

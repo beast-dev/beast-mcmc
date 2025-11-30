eb60f53b3c95189fa3e56203f8973764fcad69e8
Marc A. Suchard <msuchard@ucla.edu>
2025-11-30 07:20:42 -0800

    prelim impl of sampled variance for sparse diffusion

--- src/dr/evomodelxml/continuous/SampledCategoricalVarianceModelParser.java
+++ src/dr/evomodelxml/continuous/SampledCategoricalVarianceModelParser.java
@@ -29,37 +29,64 @@ package dr.evomodelxml.continuous;
 
 import dr.evolution.tree.Tree;
 import dr.evomodel.treedatalikelihood.continuous.ContinuousTraitDataModel;
+import dr.evomodelxml.continuous.dr.evomodel.continuous.SampledCategoricalVarianceModel;
 import dr.evomodelxml.treelikelihood.TreeTraitParserUtilities;
 import dr.inference.model.CompoundParameter;
 import dr.inference.model.Parameter;
+import dr.math.MathUtils;
 import dr.xml.*;
 
 public class SampledCategoricalVarianceModelParser extends AbstractXMLObjectParser {
 
     private static final String PARSER_NAME = "sampledCategoricalVarianceModel";
+    private static final String TRAITS = "traitParameter";
+    private static final String VARIANCES = "variances";
+    private static final String ASSIGNMENTS = "assignments";
+    private static final String RANDOMIZE_TRAITS = "randomizeTraits";
 
     @Override
     public Object parseXMLObject(XMLObject xo) throws XMLParseException {
 
-         Tree treeModel = (Tree) xo.getChild(Tree.class);
-        boolean[] missingIndicators;
-        final String traitName;
-
+        Tree treeModel = (Tree) xo.getChild(Tree.class);
         TreeTraitParserUtilities utilities = new TreeTraitParserUtilities();
 
         TreeTraitParserUtilities.TraitsAndMissingIndices returnValue =
                 utilities.parseTraitsFromTaxonAttributes(xo, treeModel, true);
-        CompoundParameter traitParameter = returnValue.traitParameter;
+        CompoundParameter oldMeans = returnValue.traitParameter;
+        Parameter means = new Parameter.Default(oldMeans.getId() + ".mean", oldMeans.getParameterValues());
 
-        return null;
+        Parameter traits = (Parameter) xo.getElementFirstChild(TRAITS);
+        Parameter variances = (Parameter) xo.getElementFirstChild(VARIANCES);
+        Parameter assignments = (Parameter) xo.getElementFirstChild(ASSIGNMENTS);
+
+        if (xo.getAttribute(RANDOMIZE_TRAITS, false)) {
+            double scale = 1.0;
+            for (int i = 0; i < traits.getDimension(); ++i) {
+                traits.setParameterValueQuietly(i,
+                        traits.getParameterValue(i) + scale * MathUtils.nextGaussian());
+            }
+            traits.fireParameterChangedEvent();
+        }
+
+        return new SampledCategoricalVarianceModel(xo.getId(), traits, means, variances, assignments);
     }
 
-    public static final XMLSyntaxRule[] rules = new XMLSyntaxRule[]{
+    private static final XMLSyntaxRule[] rules = new XMLSyntaxRule[]{
             new ElementRule(Tree.class),
             new ElementRule(TreeTraitParserUtilities.TRAIT_PARAMETER, new XMLSyntaxRule[]{
                     new ElementRule(Parameter.class)
             }),
-            AttributeRule.newStringRule(TreeTraitParserUtilities.TRAIT_NAME, true)
+            AttributeRule.newStringRule(TreeTraitParserUtilities.TRAIT_NAME, true),
+            new ElementRule(TRAITS, new XMLSyntaxRule[]{
+                    new ElementRule(Parameter.class)
+            }),
+            new ElementRule(VARIANCES, new XMLSyntaxRule[]{
+                    new ElementRule(Parameter.class)
+            }),
+            new ElementRule(ASSIGNMENTS, new XMLSyntaxRule[]{
+                    new ElementRule(Parameter.class)
+            }),
+            AttributeRule.newBooleanRule(RANDOMIZE_TRAITS, true),
     };
 
     @Override
--- src/dr/evomodelxml/continuous/dr/evomodel/continuous/SampledCategoricalVarianceModel.java
+++ src/dr/evomodelxml/continuous/dr/evomodel/continuous/SampledCategoricalVarianceModel.java
@@ -3,14 +3,17 @@ package dr.evomodelxml.continuous.dr.evomodel.continuous;
 import dr.inference.hmc.GradientWrtParameterProvider;
 import dr.inference.model.*;
 import dr.math.distributions.NormalDistribution;
+import dr.xml.Reportable;
 
 public class SampledCategoricalVarianceModel extends AbstractModelLikelihood
-        implements GradientWrtParameterProvider {
+        implements GradientWrtParameterProvider, Reportable {
 
     private final Parameter traits;
     private final Parameter means;
     private final Parameter variances;
     private final Parameter assignments;
+
+    private final Parametrization parametrization;
 
     private boolean likelihoodKnown;
     private boolean storedLikelihoodKnown;
@@ -30,6 +33,7 @@ public class SampledCategoricalVarianceModel extends AbstractModelLikelihood
         this.means = means;
         this.variances = variances;
         this.assignments = assignments;
+        this.parametrization = new Independent();
 
         addVariable(traits);
         addVariable(means);
@@ -38,43 +42,7 @@ public class SampledCategoricalVarianceModel extends AbstractModelLikelihood
 
         checkParameters();
 
-        this.gradientProvider = makeGradientProvider();
-    }
-
-    GradientWrtParameterProvider makeGradientProvider() {
-
-        final Likelihood likelihood = this;
-        return new GradientWrtParameterProvider() {
-            @Override
-            public Likelihood getLikelihood() {
-                return likelihood;
-            }
-
-            @Override
-            public Parameter getParameter() {
-                return variances;
-            }
-
-            @Override
-            public int getDimension() {
-                return variances.getDimension();
-            }
-
-            @Override
-            public double[] getGradientLogDensity() {
-
-                double[] gradient = new double[getDimension()];
-                for (int i = 0; i < traits.getDimension(); ++i) {
-                    int assignment = getAssignment(i);
-                    double sd = getSd(i);
-
-                    gradient[assignment] += NormalDistribution.gradLogPdf(traits.getParameterValue(i),
-                            means.getParameterValue(i), sd);
-                }
-
-                return gradient;
-            }
-        };
+        this.gradientProvider = makeGradientProviderWrtTraits();
     }
 
     void checkParameters() {
@@ -140,13 +108,165 @@ public class SampledCategoricalVarianceModel extends AbstractModelLikelihood
         return logLikelihood;
     }
 
-    private int getAssignment(int index) {
-        return (int) assignments.getParameterValue(index);
+    GradientWrtParameterProvider makeGradientProviderWrtTraits() {
+
+        final Likelihood likelihood = this;
+        return new GradientWrtParameterProvider() {
+            @Override
+            public Likelihood getLikelihood() {
+                return likelihood;
+            }
+
+            @Override
+            public Parameter getParameter() {
+                return traits;
+            }
+
+            @Override
+            public int getDimension() {
+                return traits.getDimension();
+            }
+
+            @Override
+            public double[] getGradientLogDensity() {
+
+                double[] gradient = new double[getDimension()];
+                for (int i = 0; i < getDimension(); ++i) {
+                    double sd = parametrization.getSd(i);
+                    gradient[i] += NormalDistribution.gradLogPdf(traits.getParameterValue(i),
+                            means.getParameterValue(i), sd);
+                }
+
+                return gradient;
+            }
+        };
     }
 
-    private double getSd(int index) {
-        double variance = variances.getParameterValue(getAssignment(index));
-        return Math.sqrt(variance);
+    private abstract class Parametrization {
+
+        abstract double getSd(int index);
+
+        @SuppressWarnings("unused")
+        abstract GradientWrtParameterProvider makeGradientProvider(Likelihood likelihood);
+
+        int getAssignment(int index) {
+            return (int) assignments.getParameterValue(index);
+        }
+
+    }
+
+    class Multiplicative extends Parametrization {
+
+        @Override
+        double getSd(int index) {
+
+            double var = variances.getParameterValue(0);
+            int assignment = getAssignment(index);
+            for (int i = 1; i < assignment; ++i) {
+                var *= variances.getParameterValue(i);
+            }
+
+            return Math.sqrt(var);
+        }
+
+        @Override
+        GradientWrtParameterProvider makeGradientProvider(Likelihood likelihood) {
+            return null;
+        }
+    }
+
+    class Regression extends Parametrization {
+
+        @Override
+        GradientWrtParameterProvider makeGradientProvider(Likelihood likelihood) {
+
+            return new GradientWrtParameterProvider() {
+                @Override
+                public Likelihood getLikelihood() {
+                    return likelihood;
+                }
+
+                @Override
+                public Parameter getParameter() {
+                    return variances;
+                }
+
+                @Override
+                public int getDimension() {
+                    return variances.getDimension();
+                }
+
+                @Override
+                public double[] getGradientLogDensity() {
+
+                    double[] gradient = new double[getDimension()];
+                    for (int i = 0; i < traits.getDimension(); ++i) {
+
+                        double sd = getSd(i);
+                        double chain = NormalDistribution.gradLogPdf(traits.getParameterValue(i),
+                                means.getParameterValue(i), sd);
+
+                        gradient[0] += chain;
+                        gradient[1] += chain * assignments.getParameterValue(i);
+                    }
+
+                    return gradient;
+                }
+            };
+        }
+
+        @Override
+        double getSd(int index) {
+            double variance =
+                    variances.getParameterValue(0) +
+                    variances.getParameterValue(1) * assignments.getParameterValue(index);
+            return Math.sqrt(variance);
+        }
+    }
+
+    class Independent extends Parametrization {
+
+        @Override
+        GradientWrtParameterProvider makeGradientProvider(Likelihood likelihood) {
+
+            return new GradientWrtParameterProvider() {
+                @Override
+                public Likelihood getLikelihood() {
+                    return likelihood;
+                }
+
+                @Override
+                public Parameter getParameter() {
+                    return variances;
+                }
+
+                @Override
+                public int getDimension() {
+                    return variances.getDimension();
+                }
+
+                @Override
+                public double[] getGradientLogDensity() {
+
+                    double[] gradient = new double[getDimension()];
+                    for (int i = 0; i < traits.getDimension(); ++i) {
+                        int assignment = getAssignment(i);
+                        double sd = getSd(i);
+
+                        gradient[assignment] += NormalDistribution.gradLogPdf(traits.getParameterValue(i),
+                                means.getParameterValue(i), sd);
+                    }
+
+                    return gradient;
+                }
+            };
+        }
+
+        @Override
+        double getSd(int index) {
+            double variance = variances.getParameterValue(getAssignment(index));
+            return Math.sqrt(variance);
+        }
     }
 
     private double computeLogLikelihood() {
@@ -154,14 +274,12 @@ public class SampledCategoricalVarianceModel extends AbstractModelLikelihood
         double logLikelihood = 0;
         for (int i = 0; i < traits.getDimension(); ++i) {
 
-            double sd = getSd(i);
+            double sd = parametrization.getSd(i);
             logLikelihood += NormalDistribution.logPdf(traits.getParameterValue(i), means.getParameterValue(i), sd);
         }
 
         return logLikelihood;
     }
-
-
 
     @Override
     public void makeDirty() {
@@ -186,5 +304,12 @@ public class SampledCategoricalVarianceModel extends AbstractModelLikelihood
     @Override
     public double[] getGradientLogDensity() {
         return gradientProvider.getGradientLogDensity();
+    }
+
+    @Override
+    public String getReport() {
+        return GradientWrtParameterProvider.getReportAndCheckForError(this,
+                getParameter().getBounds().getLowerLimit(0),
+                getParameter().getBounds().getUpperLimit(0), null);
     }
 }

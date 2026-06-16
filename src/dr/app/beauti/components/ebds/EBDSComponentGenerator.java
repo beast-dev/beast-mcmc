27244676c9ebf28d6eaf4480c1330fb2f177aebd
rambaut <rambaut@gmail.com>
2026-06-16 18:18:44 +0100

    Created a EBDS component - not sure whether to do it that way

---
+++ src/dr/app/beauti/components/ebds/EBDSComponentGenerator.java
@@ -0,0 +1,289 @@
+/*
+ * EBDSComponentGenerator.java
+ *
+ * Copyright © 2002-2026 the BEAST Development Team
+ * http://beast.community/about
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ *
+ */
+
+package dr.app.beauti.components.ebds;
+
+import dr.app.beauti.generator.BaseComponentGenerator;
+import dr.app.beauti.options.*;
+import dr.app.beauti.util.XMLWriter;
+import dr.evolution.datatype.DataType;
+import dr.evomodel.branchratemodel.BranchRateModel;
+import dr.evomodel.tree.DefaultTreeModel;
+import dr.evomodelxml.branchratemodel.DiscretizedBranchRatesParser;
+import dr.evomodelxml.branchratemodel.RandomLocalClockModelParser;
+import dr.evomodelxml.branchratemodel.StrictClockBranchRatesParser;
+import dr.evoxml.AlignmentParser;
+import dr.evoxml.MutationDeathTypeParser;
+import dr.evoxml.SitePatternsParser;
+import dr.inference.model.ParameterParser;
+import dr.oldevomodel.sitemodel.GammaSiteModel;
+import dr.oldevomodel.sitemodel.SiteModel;
+import dr.oldevomodelxml.MSSD.ALSSiteModelParser;
+import dr.oldevomodelxml.MSSD.ALSTreeLikelihoodParser;
+import dr.oldevomodelxml.sitemodel.GammaSiteModelParser;
+import dr.oldevomodelxml.substmodel.MutationDeathModelParser;
+import dr.oldevomodelxml.treelikelihood.TreeLikelihoodParser;
+import dr.util.Attribute;
+import dr.xml.XMLParser;
+
+/**
+ * XML Generator for the EpisodicBirthDeathSampling model and likelihood infrastructure
+ *
+ * @author Andrew Rambaut
+ */
+
+public class EBDSComponentGenerator extends BaseComponentGenerator {
+
+    protected EBDSComponentGenerator(BeautiOptions options) {
+        super(options);
+    }
+
+    public boolean usesInsertionPoint(InsertionPoint point) {
+
+        EBDSComponentOptions component = (EBDSComponentOptions) options
+                .getComponentOptions(EBDSComponentOptions.class);
+
+        if (!component.isActive()) {
+            return false;
+        }
+
+        switch (point) {
+            case AFTER_TREE_MODEL:          // after the tree model
+            case IN_OPERATORS:              // in the operator schedule
+            case IN_MCMC_PRIOR:             // in the prior section of the MCMC
+            case IN_MCMC_LIKELIHOOD:        // in the likelihood section of the MCMC
+            case IN_SCREEN_LOG:             // in the screen log
+            case IN_FILE_LOG_PARAMETERS:    // in the file log after the parameters have been logged
+                return true;
+            default:
+                return false;
+        }
+
+    }// END: usesInsertionPoint
+
+    protected void generate(final InsertionPoint point, final Object item, final String prefix, final XMLWriter writer) {
+
+        EBDSComponentOptions component = (EBDSComponentOptions) options
+                .getComponentOptions(EBDSComponentOptions.class);
+
+
+        switch (point) {
+            case AFTER_TREE_MODEL:
+                writeEBDSModel(writer);
+                break;
+            case IN_OPERATORS:
+                writeHamiltonianOperatorModel(writer);
+                break;
+            case IN_MCMC_PRIOR:
+                writeEBDSPriors(writer);
+                break;
+            case IN_MCMC_LIKELIHOOD:
+                writeEBDSLikelihood(writer);
+                break;
+//            case IN_SCREEN_LOG:
+//                writeScreenLogEntries(writer, component);
+//                break;
+            case IN_FILE_LOG_PARAMETERS:
+                writeLog(writer, component);
+                break;
+
+            default:
+                throw new IllegalArgumentException(
+                        "This insertion point is not implemented for "
+                                + this.getClass().getName());
+        }
+
+    }// END: generate
+
+    private void writeEBDSModel(XMLWriter writer) {
+    }
+
+    private void writeScreenLogEntries(XMLWriter writer, EBDSComponentOptions component) {
+    }
+
+    private void writeHamiltonianOperatorModel(XMLWriter writer) {
+
+    }
+
+    private void writeEBDSPriors(XMLWriter writer) {
+
+    }
+
+    private void writeEBDSLikelihood(XMLWriter writer) {
+
+    }
+
+//    private void writeDolloTreeLikelihoodReferences(XMLWriter writer) {
+//
+//        for (AbstractPartitionData partition : options.dataPartitions) {
+//            PartitionSubstitutionModel model = partition.getPartitionSubstitutionModel();
+//            String prefix = partition.getName();
+//            if (model.isDolloModel()) {
+//                writer.writeIDref(ALSTreeLikelihoodParser.LIKE_NAME,
+//                        prefix + ALSTreeLikelihoodParser.LIKE_NAME);
+//            }
+//        }
+//    }
+
+    private void writeLog(XMLWriter writer, EBDSComponentOptions component) {
+        for (AbstractPartitionData partition : options.dataPartitions) {
+            PartitionSubstitutionModel model = partition.getPartitionSubstitutionModel();
+            if (model.isDolloModel()) {
+                String prefix = partition.getName() + ".";
+                writer.writeIDref(ParameterParser.PARAMETER,
+                        component.getOptions().getParameter(prefix + EBDSComponentOptions.DEATH_RATE).getName());
+            }
+        }
+    }
+
+    @Override
+    protected String getCommentLabel() {
+        return "Episodic Birth Death Model";
+    }
+
+    private void writeDolloSubstitutionModels(XMLWriter writer, EBDSComponentOptions component) {
+
+        // generate tree likelihoods for stochastic Dollo partitions
+        for (AbstractPartitionData partition : options.dataPartitions) {
+            PartitionSubstitutionModel model = partition.getPartitionSubstitutionModel();
+            if (model.isDolloModel()) {
+                writeDolloSubstitutionModel(partition, writer, component);
+                writeDolloSiteModel(partition, writer, component);
+                writeDolloTreeLikelihood(TreeLikelihoodParser.TREE_LIKELIHOOD, -1, partition, writer);
+            }
+        }
+    }
+
+    private void writeDolloSubstitutionModel(AbstractPartitionData partition, XMLWriter writer, EBDSComponentOptions component) {
+        String prefix = partition.getName() + ".";
+//        String prefix = partition.getPrefix(); // TODO Fix
+        writer.writeOpenTag(MutationDeathModelParser.MD_MODEL,
+                new Attribute.Default<String>(XMLParser.ID, prefix + EBDSComponentOptions.MODEL_NAME ));
+        writeParameter(prefix + EBDSComponentOptions.DEATH_RATE,
+                component.getOptions().getParameter(prefix + EBDSComponentOptions.DEATH_RATE), writer);
+        writer.writeTag(DataType.DATA_TYPE, new Attribute.Default<String>(XMLParser.IDREF, EBDSComponentOptions.DATA_NAME), true);
+        writer.writeCloseTag(MutationDeathModelParser.MD_MODEL);
+        writer.write("\n");
+    }
+
+    private void writeDolloSiteModel(AbstractPartitionData partition, XMLWriter writer, EBDSComponentOptions components) {
+        String prefix = partition.getName() + ".";
+        writer.writeOpenTag(ALSSiteModelParser.ALS_SITE_MODEL,
+                new Attribute[]{new Attribute.Default<String>(XMLParser.ID, prefix + SiteModel.SITE_MODEL)});
+
+
+        writer.writeOpenTag(GammaSiteModelParser.SUBSTITUTION_MODEL);
+        writer.writeIDref(MutationDeathModelParser.MD_MODEL, prefix + EBDSComponentOptions.MODEL_NAME);
+        writer.writeCloseTag(GammaSiteModelParser.SUBSTITUTION_MODEL);
+
+        PartitionSubstitutionModel model = partition.getPartitionSubstitutionModel();
+        if (model.hasCodonPartitions()) {
+            writeParameter(GammaSiteModelParser.RELATIVE_RATE, "mu", model, writer);
+        }
+
+        if (model.isGammaHetero()) {
+            writer.writeOpenTag(GammaSiteModelParser.GAMMA_SHAPE,
+                    new Attribute.Default<String>(GammaSiteModelParser.GAMMA_CATEGORIES, "" + model.getRateCategories()));
+            writeParameter(prefix + "alpha", model, writer);
+            writer.writeCloseTag(GammaSiteModelParser.GAMMA_SHAPE);
+        }
+
+        if (model.isInvarHetero()) {
+            writeParameter(GammaSiteModelParser.PROPORTION_INVARIANT, "pInv", model, writer);
+        }
+
+        writer.writeCloseTag(ALSSiteModelParser.ALS_SITE_MODEL);
+    }
+
+//    private void writeDolloTreeLikelihoods(XMLWriter writer, DolloComponentOptions component) {
+//
+//        // generate tree likelihoods for stochastic Dollo partitions
+//        for (AbstractPartitionData partition : options.dataPartitions) {
+//            PartitionSubstitutionModel model = partition.getPartitionSubstitutionModel();
+//            if (model.isDolloModel()) {
+//                writeDolloTreeLikelihood(ALSTreeLikelihoodParser.LIKE_NAME, -1, partition, writer);
+//            }
+//        }
+//    }
+
+    private void writeDolloTreeLikelihood(String id, int num, AbstractPartitionData partition, XMLWriter writer) {
+        PartitionSubstitutionModel substModel = partition.getPartitionSubstitutionModel();
+        PartitionTreeModel treeModel = partition.getPartitionTreeModel();
+        PartitionClockModel clockModel = partition.getPartitionClockModel();
+
+        String prefix = partition.getName() + ".";
+        String oldPrefix = partition.getPrefix(); // TODO Get working
+
+        writer.writeComment("Likelihood for tree given a stochastic Dollo model");
+
+        writer.writeOpenTag(
+                ALSTreeLikelihoodParser.LIKE_NAME,
+                new Attribute[]{
+                        new Attribute.Default<String>(XMLParser.ID, oldPrefix + id),
+                        new Attribute.Default<Boolean>(TreeLikelihoodParser.USE_AMBIGUITIES, true),
+                        new Attribute.Default<Boolean>(ALSTreeLikelihoodParser.INTEGRATE_GAIN_RATE, true)}
+        );
+
+        if (!options.samplePriorOnly) {
+            writer.writeIDref(SitePatternsParser.PATTERNS, partition.getPrefix() + SitePatternsParser.PATTERNS);
+        } else {
+            // We just need to use the dummy alignment
+            if (partition instanceof PartitionData) {
+                writer.writeIDref(AlignmentParser.ALIGNMENT, ((PartitionData) partition).getAlignment().getId());
+            }
+        }
+
+        writer.writeIDref(DefaultTreeModel.TREE_MODEL, treeModel.getPrefix() + DefaultTreeModel.TREE_MODEL);
+        writer.writeIDref(GammaSiteModel.SITE_MODEL, prefix + SiteModel.SITE_MODEL);
+
+        writer.writeTag(ALSTreeLikelihoodParser.OBSERVATION_PROCESS,
+                new Attribute.Default<String>(ALSTreeLikelihoodParser.OBSERVATION_TYPE,ALSTreeLikelihoodParser.ANY_TIP),
+                true);
+
+        switch (clockModel.getClockType()) {
+            case STRICT_CLOCK:
+                writer.writeIDref(StrictClockBranchRatesParser.STRICT_CLOCK_BRANCH_RATES, clockModel.getPrefix()
+                        + BranchRateModel.BRANCH_RATES);
+                break;
+            case UNCORRELATED:
+                writer.writeIDref(DiscretizedBranchRatesParser.DISCRETIZED_BRANCH_RATES, clockModel.getPrefix()
+                        + BranchRateModel.BRANCH_RATES);
+                break;
+            case RANDOM_LOCAL_CLOCK:
+                writer.writeIDref(RandomLocalClockModelParser.LOCAL_BRANCH_RATES, clockModel.getPrefix()
+                        + BranchRateModel.BRANCH_RATES);
+                break;
+
+            case AUTOCORRELATED:
+                throw new UnsupportedOperationException("Autocorrelated relaxed clock model not implemented yet");
+            default:
+                throw new IllegalArgumentException("Unknown clock model");
+        }
+        writer.writeCloseTag(ALSTreeLikelihoodParser.LIKE_NAME);
+    }
+
+}// END: class

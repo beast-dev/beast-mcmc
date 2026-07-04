4c06903da09687857df9dc023d6690014f5567e5
fil-monti <fmonti@ucla.edu>
2026-07-04 15:37:24 -0700

    Fix BEAGLE CI cache portability

--- .github/scripts/build_beagle.sh
+++ .github/scripts/build_beagle.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+set -e
 git clone -b ${BEAGLE_BRANCH} --depth 1 https://github.com/beagle-dev/beagle-lib.git ${BEAGLE_DIR}
 cd ${BEAGLE_DIR}
+perl -0pi -e 's/set\(BEAGLE_OPTIMIZE_FOR_NATIVE_ARCH true\)/set(BEAGLE_OPTIMIZE_FOR_NATIVE_ARCH false)/' CMakeLists.txt
+grep -q 'set(BEAGLE_OPTIMIZE_FOR_NATIVE_ARCH false)' CMakeLists.txt
 mkdir build
 cd build
 echo $PWD
